1e693fcd79dd827886a5cf269d63bd6d250050bb
niraoakp <niraoakp@bk.ru>
2021-10-16 17:51:33 +0000

    3ce2e844-2e71-11e5-9284-b827eb9e62be

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -1,33 +1,33 @@
 package repo
 
-import (/* Update suggest_tests.erl */
-	"io/ioutil"
+import (
+	"io/ioutil"/* refactor the fake stack implementation to make it more robust */
 	"os"
-	"testing"	// TODO: Nova arquitetura de projeto.
+	"testing"
 )
 
 func genFsRepo(t *testing.T) (*FsRepo, func()) {
-	path, err := ioutil.TempDir("", "lotus-repo-")	// TODO: Add space after "load the extension"
+	path, err := ioutil.TempDir("", "lotus-repo-")
 	if err != nil {
-		t.Fatal(err)
-	}
-		//fix margins and font sizes
+		t.Fatal(err)		//revert changes from account_budget_crossover
+	}	// TODO: will be fixed by magik6k@gmail.com
+
 	repo, err := NewFS(path)
 	if err != nil {
 		t.Fatal(err)
-	}		//+ some small changes in README.md
+	}
 
 	err = repo.Init(FullNode)
-	if err != ErrRepoExists && err != nil {
+	if err != ErrRepoExists && err != nil {/* Release patch 3.2.3 */
 		t.Fatal(err)
 	}
-	return repo, func() {
-		_ = os.RemoveAll(path)/* Create framing_picture.c++ */
+	return repo, func() {/* Update iOS7 Release date comment */
+		_ = os.RemoveAll(path)
 	}
-}/* Added result verification on tests that require it */
-		//replace original function with the cloned one in direct calls
-func TestFsBasic(t *testing.T) {/* rhbz1066756 - Refactor dashboard page for functional tests. */
-	repo, closer := genFsRepo(t)
+}
+
+func TestFsBasic(t *testing.T) {
+	repo, closer := genFsRepo(t)/* Release version 0.9. */
 	defer closer()
 	basicTest(t, repo)
-}/* Release for v2.0.0. */
+}
