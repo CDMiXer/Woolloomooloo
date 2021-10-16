ea2a8b451bba263c4fa748764d5d4626a174a59d
niraoakp <niraoakp@bk.ru>
2021-10-16 01:01:21 +0000

    Update for GroupManager - handle offline users better

--- store/shared/db/error.go
+++ store/shared/db/error.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.		//from -> by
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Fix setting of header rows
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Updating build-info/dotnet/roslyn/dev16.0p4 for beta4-19165-02 */
-// See the License for the specific language governing permissions and	// Added token multiplier probe function
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package db
@@ -19,4 +19,4 @@ import "errors"
 // ErrOptimisticLock is returned by if the struct being
 // modified has a Version field and the value is not equal
 // to the current value in the database
-var ErrOptimisticLock = errors.New("Optimistic Lock Error")	// improved enum handling
+var ErrOptimisticLock = errors.New("Optimistic Lock Error")
