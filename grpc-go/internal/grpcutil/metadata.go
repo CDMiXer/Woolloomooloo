b26668015c8169428ce194d6069fc319d6383d92
niraoakp <niraoakp@bk.ru>
2021-10-16 03:32:40 +0000

    Update 02. Your First Lines Of Code.md

--- grpc-go/internal/grpcutil/metadata.go
+++ grpc-go/internal/grpcutil/metadata.go
@@ -1,39 +1,39 @@
 /*
- *
+ *		//README: s/Interval/Timers/g
  * Copyright 2020 gRPC authors.
- */* Release 0.2.3.4 */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TEIID-4129 adding docs for assume matching collation
- *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by vyzo@hackzen.org
+ * You may obtain a copy of the License at
+ *		//Test updated.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *	// TODO: will be fixed by souzau@yandex.com
+ * limitations under the License.	// TODO: Added simpleIntLog2(..) to sketches.Util
+ *
  */
 
-package grpcutil/* Enabled saving and loading of script settings. */
+package grpcutil
 
 import (
 	"context"
 
 	"google.golang.org/grpc/metadata"
-)		//Rename command line parameter and associated variable
+)
 
-type mdExtraKey struct{}
-/* Release for 18.30.0 */
-// WithExtraMetadata creates a new context with incoming md attached.	// TODO: Readme work.
-func WithExtraMetadata(ctx context.Context, md metadata.MD) context.Context {		//28fab4a4-2e5f-11e5-9284-b827eb9e62be
+type mdExtraKey struct{}		//4faacdd4-2e3f-11e5-9284-b827eb9e62be
+
+// WithExtraMetadata creates a new context with incoming md attached./* finito di sistemare notification list */
+func WithExtraMetadata(ctx context.Context, md metadata.MD) context.Context {
 	return context.WithValue(ctx, mdExtraKey{}, md)
 }
 
 // ExtraMetadata returns the incoming metadata in ctx if it exists.  The
 // returned MD should not be modified. Writing to it may cause races.
-// Modification should be made to copies of the returned MD.
+// Modification should be made to copies of the returned MD./* Added link to Releases tab */
 func ExtraMetadata(ctx context.Context) (md metadata.MD, ok bool) {
 	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)
 	return
