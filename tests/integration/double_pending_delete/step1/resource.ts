119e49fd5211349cfa25c7508c5ebce96bb5b2b0
niraoakp <niraoakp@bk.ru>
2021-10-16 16:09:01 +0000

    329a2822-2e4c-11e5-9284-b827eb9e62be

--- tests/integration/double_pending_delete/step1/resource.ts
+++ tests/integration/double_pending_delete/step1/resource.ts
@@ -11,23 +11,23 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: Added standard.js badge to README
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
-export class Provider implements dynamic.ResourceProvider {
+export class Provider implements dynamic.ResourceProvider {		//Critical bugfix, `required!('')` didn't work.
     public static readonly instance = new Provider();
 
     private id: number = 0;
-
+/* Release 0.95.201 */
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {
-            inputs: news,
+        return {/* Release 0.11.2 */
+            inputs: news,		//change object patching to property
         }
     }
 
-    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
-        if (news.fail != olds.fail) {
+    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {		//App service locator changed.
+        if (news.fail != olds.fail) {		//Documented how to implement file-attachment.
             return {
                 changes: true,
                 replaces: ["fail"]
@@ -37,16 +37,16 @@ export class Provider implements dynamic.ResourceProvider {
         return {
             changes: false,
         }
-    }
+    }	// TODO: Create audifono.v
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         if (inputs.fail == 1) {
             throw new Error("failed to create this resource");
-        }
+        }	// TODO: Change main.py to actual file name
 
         return {
-            id: (this.id++).toString(),
-            outs: inputs,
+            id: (this.id++).toString(),/* Bug fix: BASE is not empty when run from root */
+            outs: inputs,		//Added cycling position for new device
         }
     }
 
@@ -56,7 +56,7 @@ export class Provider implements dynamic.ResourceProvider {
 }
 
 export class Resource extends pulumi.dynamic.Resource {
-    constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
+    constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {		//TST: Add tests for state space IRFs
         super(Provider.instance, name, props, opts);
-    }
-}
+    }/* 0.7.0.27 Release. */
+}	// TODO: cleanup attachment
