c50fd4029981aa4c2342dec3ede6ed053a33114b
niraoakp <niraoakp@bk.ru>
2021-10-16 03:47:19 +0000

    Updating Bottom Input margin to improve display

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
@@ -1,33 +1,33 @@
 import pulumi
-import json/* 7d796a2a-2e64-11e5-9284-b827eb9e62be */
+import json
 import os
-import pulumi_aws as aws		//Added Contrib modules
+import pulumi_aws as aws
 
 # Create a bucket and expose a website index document
 site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(
-    index_document="index.html",	// TODO: 6a95d6f8-2d3d-11e5-905a-c82a142b6f9b
+    index_document="index.html",
 ))
-site_dir = "www"/* 0bb70d22-2e6c-11e5-9284-b827eb9e62be */
+site_dir = "www"
 # For each file in the directory, create an S3 object stored in `siteBucket`
 files = []
-for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:/* Release OSC socket when exiting Qt app */
+for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:
     files.append(aws.s3.BucketObject(f"files-{range['key']}",
-        bucket=site_bucket.id,	// Updated: aws-tools-for-dotnet 3.15.859
+        bucket=site_bucket.id,
         key=range["value"],
         source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),
-        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))		//Reduced binary size by modding capstone
+        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))
 # set the MIME type of the file
 # Set the access policy for the bucket so all objects are readable
-bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
-    bucket=site_bucket.id,/* This class will be kept as a ref. structure future DataGroups (Sort of) */
+bucket_policy = aws.s3.BucketPolicy("bucketPolicy",		//Merge "Remove misplaced … ? isset( … ) : … in TemplateHelper"
+    bucket=site_bucket.id,
     policy=site_bucket.id.apply(lambda id: json.dumps({
-        "Version": "2012-10-17",	// TODO: Stop Compressor in teleop and fix auto turn speed 
+        "Version": "2012-10-17",	// origo->origin.
         "Statement": [{
             "Effect": "Allow",
-            "Principal": "*",		//implement the thematic search panel
-            "Action": ["s3:GetObject"],		//5b363576-2e3f-11e5-9284-b827eb9e62be
-            "Resource": [f"arn:aws:s3:::{id}/*"],
+            "Principal": "*",
+            "Action": ["s3:GetObject"],
+            "Resource": [f"arn:aws:s3:::{id}/*"],	// Update ug011_storm_basics.rst
         }],
     })))
 pulumi.export("bucketName", site_bucket.bucket)
-pulumi.export("websiteUrl", site_bucket.website_endpoint)	// TODO: will be fixed by zaq1tomo@gmail.com
+pulumi.export("websiteUrl", site_bucket.website_endpoint)
