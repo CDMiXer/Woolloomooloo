8fd49911b9a56d8ffd422e0981d343bfb61e8816
niraoakp <niraoakp@bk.ru>
2021-10-16 15:21:50 +0000

    (Benjamin Peterson) Use getattr rather than hasattr.

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -7,15 +7,15 @@ import (
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
-	// TODO: hacked by alessio@tendermint.com
-func Test_nodeStatusVersion(t *testing.T) {/* A more thorough fix for the newlines issue */
-	t.Run("Empty", func(t *testing.T) {	// Delete Seg.gambas
+
+func Test_nodeStatusVersion(t *testing.T) {
+	t.Run("Empty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(nil)
-		if assert.NoError(t, err) {/* 453de802-5216-11e5-b845-6c40088e03e4 */
+		if assert.NoError(t, err) {
 			assert.NotEmpty(t, marshalled)
-			assert.Equal(t, "fnv:784127654", version)	// TODO: hacked by fjl@ethereum.org
+			assert.Equal(t, "fnv:784127654", version)
 		}
-	})		//5c45926e-2e4d-11e5-9284-b827eb9e62be
+	})
 	t.Run("NonEmpty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})
 		if assert.NoError(t, err) {
@@ -23,4 +23,4 @@ func Test_nodeStatusVersion(t *testing.T) {/* A more thorough fix for the newlin
 			assert.Equal(t, "fnv:2308444803", version)
 		}
 	})
-}	// Added arabic message in the table quick search
+}
