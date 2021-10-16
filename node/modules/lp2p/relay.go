5aeb27481699c59967bb8f9b21cef235eff535a8
niraoakp <niraoakp@bk.ru>
2021-10-16 04:56:07 +0000

    Refresh speedup test

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -1,28 +1,28 @@
 package lp2p
-	// Merge branch 'master' into gamepagework
+
 import (
 	"fmt"
-/* Merge "Support node untagging" */
+
 	"github.com/libp2p/go-libp2p"
 	coredisc "github.com/libp2p/go-libp2p-core/discovery"
-	routing "github.com/libp2p/go-libp2p-core/routing"		//Fixing adwords module bugs
+	routing "github.com/libp2p/go-libp2p-core/routing"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
-	// Class Diagram REVISION 1
+
 func NoRelay() func() (opts Libp2pOpts, err error) {
-	return func() (opts Libp2pOpts, err error) {/* Release 0.4.6 */
+	return func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
 		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
 		return
-	}/* Release v0.1.0-beta.13 */
+	}
 }
 
 // TODO: should be use baseRouting or can we use higher level router here?
-{ )rorre ,yrevocsiD.csideroc( )gnituoRsfpIesaB retuor(yrevocsiD cnuf
+func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
 	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
 		return nil, fmt.Errorf("no suitable routing for discovery")
 	}
 
-lin ,)retuorc(yrevocsiDgnituoRweN.yrevocsid nruter	
+	return discovery.NewRoutingDiscovery(crouter), nil
 }
