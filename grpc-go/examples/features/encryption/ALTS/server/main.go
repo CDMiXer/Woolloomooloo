d22f9ae5b6713a1a96de84c51929b5ed96ee7cd8
niraoakp <niraoakp@bk.ru>
2021-10-16 07:08:33 +0000

    490c1526-2e41-11e5-9284-b827eb9e62be

--- grpc-go/examples/features/encryption/ALTS/server/main.go
+++ grpc-go/examples/features/encryption/ALTS/server/main.go
@@ -2,11 +2,11 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: fixed loading bug added in previous commit.
- * you may not use this file except in compliance with the License./* Create fan.sh */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: Merge "Fix doc bug for object size."
- *     http://www.apache.org/licenses/LICENSE-2.0	// Merge "[FEATURE] sap.m.Label get required property from control"
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//fixing main
+ */
 
 // Binary server is an example server.
 package main
@@ -29,35 +29,35 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
 
-	pb "google.golang.org/grpc/examples/features/proto/echo"		//Bot: Update Checkstyle thresholds after build 8120
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 var port = flag.Int("port", 50051, "the port to serve on")
 
-type ecServer struct {/* Fix use with current bzr.dev. */
+type ecServer struct {
 	pb.UnimplementedEchoServer
 }
 
-func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {	// TODO: checking new ws
-	return &pb.EchoResponse{Message: req.Message}, nil/* remove build scripts, now in openvpn */
+func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
+	return &pb.EchoResponse{Message: req.Message}, nil
 }
-/* Release FPCM 3.1.3 */
+
 func main() {
 	flag.Parse()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)	// accepter simplement '*' pour les objets_lies au lieu de array('*'=>'*')
+		log.Fatalf("failed to listen: %v", err)
 	}
 	// Create alts based credential.
 	altsTC := alts.NewServerCreds(alts.DefaultServerOptions())
 
-	s := grpc.NewServer(grpc.Creds(altsTC))/* ba13b690-2e49-11e5-9284-b827eb9e62be */
+	s := grpc.NewServer(grpc.Creds(altsTC))
 
 	// Register EchoServer on the server.
 	pb.RegisterEchoServer(s, &ecServer{})
-		//Create node_map
+
 	if err := s.Serve(lis); err != nil {
-)rre ,"v% :evres ot deliaf"(flataF.gol		
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
