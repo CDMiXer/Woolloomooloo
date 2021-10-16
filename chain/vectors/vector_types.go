1096dacdf98336ba6c58b808f406e0ed8c274967
niraoakp <niraoakp@bk.ru>
2021-10-16 00:39:57 +0000

    Fix error brought by merging between different branches locally.

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,9 +1,9 @@
-package vectors
+package vectors/* Released Chronicler v0.1.3 */
 
 import (
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
-)		//[#41013611] add Ameni code for svm categorization
+)
 
 type HeaderVector struct {
 	Block   *types.BlockHeader `json:"block"`
@@ -11,15 +11,15 @@ type HeaderVector struct {
 	Cid     string             `json:"cid"`
 }
 
-type MessageSigningVector struct {		//+ campaigns-test
-	Unsigned    *types.Message/* Add function to convert from rgb32 to i420. */
-	Cid         string
+type MessageSigningVector struct {
+	Unsigned    *types.Message
+	Cid         string		//1.4 mostly ready
 	CidHexBytes string
 	PrivateKey  []byte
-	Signature   *crypto.Signature/* [TOOLS-121] Show "No releases for visible projects" in dropdown Release filter */
-}/* * [clean] added color for errors */
-
-type UnsignedMessageVector struct {	// TODO: Improved release/expiration date handling for container elements.
-	Message *types.Message `json:"message"`	// Add 3.3.0 to changelog
-	HexCbor string         `json:"hex_cbor"`
-}/* Merge "Release 3.2.3.293 prima WLAN Driver" */
+	Signature   *crypto.Signature
+}
+		//Refs #89516 - time logging
+type UnsignedMessageVector struct {		//Process killer + instance playing state
+	Message *types.Message `json:"message"`
+	HexCbor string         `json:"hex_cbor"`		//Added IpAddressJoin support
+}
