1ab9d4b062b470d8dcbb3bd9a67b1e6ef7f644a0
niraoakp <niraoakp@bk.ru>
2021-10-16 14:27:12 +0000

    signed updated minidriver cat file

--- core/cron_test.go
+++ core/cron_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 0.0.5 closes #1 and #2 */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Make loopCount writeable */
 // that can be found in the LICENSE file.
 
 // +build !oss
-		//tweak loader animation
-package core	// TODO: will be fixed by nagydani@epointsystem.org
+
+package core
