3ea42dc62e99898f2fa9a1579e3d002b44b562e2
niraoakp <niraoakp@bk.ru>
2021-10-16 11:14:23 +0000

    FIX SVG Text

--- tests/integration/construct_component/go/main.go
+++ tests/integration/construct_component/go/main.go
@@ -1,37 +1,37 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 package main
-	// TODO: hacked by mail@bitpshr.net
+
 import (
 	"reflect"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-/* Release v0.12.2 (#637) */
+
 type componentArgs struct {
 	Echo interface{} `pulumi:"echo"`
-}	// TODO: Permissions, and Bypass Ice Checker
+}
 
-type ComponentArgs struct {/* @Release [io7m-jcanephora-0.23.5] */
+type ComponentArgs struct {
 	Echo pulumi.Input
 }
-/* Moved to movie_without_boxoffice.csv */
-func (ComponentArgs) ElementType() reflect.Type {/* Create compileRelease.bash */
-	return reflect.TypeOf((*componentArgs)(nil)).Elem()/* Update and rename Challenge-1.py to Problem-1.py */
+
+func (ComponentArgs) ElementType() reflect.Type {
+	return reflect.TypeOf((*componentArgs)(nil)).Elem()
 }
-/* Updated Capistrano Version 3 Release Announcement (markdown) */
-type Component struct {	// TODO: Incorrect DOI
+
+type Component struct {
 	pulumi.ResourceState
 
 	Echo    pulumi.AnyOutput    `pulumi:"echo"`
 	ChildID pulumi.StringOutput `pulumi:"childId"`
-}/* changes to site theme */
+}
 
-func NewComponent(		//chore(package): update @types/node to version 8.0.46
-	ctx *pulumi.Context, name string, args *ComponentArgs, opts ...pulumi.ResourceOption) (*Component, error) {/* making afterRelease protected */
+func NewComponent(
+	ctx *pulumi.Context, name string, args *ComponentArgs, opts ...pulumi.ResourceOption) (*Component, error) {
 
 	var resource Component
-	err := ctx.RegisterRemoteComponentResource("testcomponent:index:Component", name, args, &resource, opts...)/* for #339 added docs */
+	err := ctx.RegisterRemoteComponentResource("testcomponent:index:Component", name, args, &resource, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -46,8 +46,8 @@ func main() {
 			return err
 		}
 		_, err = NewComponent(ctx, "b", &ComponentArgs{Echo: componentA.Echo})
-		if err != nil {	// TODO: hacked by nicksavers@gmail.com
-			return err		//Rename preview.html to preview
+		if err != nil {
+			return err
 		}
 		_, err = NewComponent(ctx, "C", &ComponentArgs{Echo: componentA.ChildID})
 		if err != nil {
