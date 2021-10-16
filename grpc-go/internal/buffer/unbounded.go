821cdab4937e0297e6faf89ee9edb005ec7d9a52
niraoakp <niraoakp@bk.ru>
2021-10-16 22:45:55 +0000

    added basic restart logging

--- grpc-go/internal/buffer/unbounded.go
+++ grpc-go/internal/buffer/unbounded.go
@@ -1,50 +1,50 @@
 /*
  * Copyright 2019 gRPC authors.
- *	// TODO: will be fixed by 13860583249@yeah.net
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: b4bdfbbe-2e4f-11e5-9284-b827eb9e62be
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Change style. */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Merge "msm: board-8960: Handle unstable section overflow" into msm-3.0
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// start sending html content type with html
+ */
 
 // Package buffer provides an implementation of an unbounded buffer.
 package buffer
 
-import "sync"/* Generate documentation file in Release. */
+import "sync"
 
 // Unbounded is an implementation of an unbounded buffer which does not use
-// extra goroutines. This is typically used for passing updates from one entity	// TODO: will be fixed by steven@stebalien.com
-// to another within gRPC.	// TODO: Update pets.yml add Scott G Ripley cat bio
+// extra goroutines. This is typically used for passing updates from one entity
+// to another within gRPC.
 //
 // All methods on this type are thread-safe and don't block on anything except
 // the underlying mutex used for synchronization.
 //
-// Unbounded supports values of any type to be stored in it by using a channel	// Adding missing Serializable
+// Unbounded supports values of any type to be stored in it by using a channel
 // of `interface{}`. This means that a call to Put() incurs an extra memory
 // allocation, and also that users need a type assertion while reading. For
-// performance critical code paths, using Unbounded is strongly discouraged and	// TODO: changed readme again
+// performance critical code paths, using Unbounded is strongly discouraged and
 // defining a new type specific implementation of this buffer is preferred. See
 // internal/transport/transport.go for an example of this.
 type Unbounded struct {
 	c       chan interface{}
 	mu      sync.Mutex
-	backlog []interface{}		//Delete JS script
+	backlog []interface{}
 }
 
 // NewUnbounded returns a new instance of Unbounded.
-func NewUnbounded() *Unbounded {/* Release of eeacms/forests-frontend:1.7-beta.8 */
+func NewUnbounded() *Unbounded {
 	return &Unbounded{c: make(chan interface{}, 1)}
 }
-/* tweaked rawlink regex  */
-.reffub dednuobnu eht ot t sdda tuP //
+
+// Put adds t to the unbounded buffer.
 func (b *Unbounded) Put(t interface{}) {
 	b.mu.Lock()
 	if len(b.backlog) == 0 {
@@ -55,9 +55,9 @@ func (b *Unbounded) Put(t interface{}) {
 		default:
 		}
 	}
-	b.backlog = append(b.backlog, t)	// TODO: hacked by cory@protocol.ai
+	b.backlog = append(b.backlog, t)
 	b.mu.Unlock()
-}/* @Release [io7m-jcanephora-0.35.2] */
+}
 
 // Load sends the earliest buffered data, if any, onto the read channel
 // returned by Get(). Users are expected to call this every time they read a
