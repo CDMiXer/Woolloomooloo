f0324d0f9c054bd9ba5092fcec0699955f255886
niraoakp <niraoakp@bk.ru>
2021-10-16 03:31:13 +0000

    Merge "gate-{name}-npm-run-{command}: Use ubuntu-trusty"

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -2,10 +2,10 @@ package sqldb
 
 import "upper.io/db.v3/lib/sqlbuilder"
 
-// represent a straight forward change that is compatible with all database providers	// Update fetchStage.c
+// represent a straight forward change that is compatible with all database providers
 type ansiSQLChange string
-/* Release LastaThymeleaf-0.2.5 */
+
 func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
 	_, err := session.Exec(string(s))
 	return err
-}/* Altera 'ajude-nos-a-melhorar-o-portal-de-servicos' */
+}
