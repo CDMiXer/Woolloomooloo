57edd00c4b7bbd03ef752353e5317a36b6a3be69
niraoakp <niraoakp@bk.ru>
2021-10-16 11:12:51 +0000

    Delete question.png

--- plugin/config/jsonnet.go
+++ plugin/config/jsonnet.go
@@ -3,14 +3,14 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+	// TODO: hacked by ng8eke@163.com
 package config
 
 import (
 	"bytes"
 	"context"
 	"strings"
-
+	// TODO: hacked by mikeal.rogers@gmail.com
 	"github.com/drone/drone/core"
 
 	"github.com/google/go-jsonnet"
@@ -19,37 +19,37 @@ import (
 // Jsonnet returns a configuration service that fetches the
 // jsonnet file directly from the source code management (scm)
 // system and converts to a yaml file.
-func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
+func Jsonnet(service core.FileService, enabled bool) core.ConfigService {/* downgrading to 5.3 compat */
 	return &jsonnetPlugin{
-		enabled: enabled,
+		enabled: enabled,/* Delete NvFlexReleaseD3D_x64.lib */
 		repos:   &repo{files: service},
-	}
+	}		//Added mkzip.bat
 }
 
-type jsonnetPlugin struct {
+type jsonnetPlugin struct {		//Implement get_items() for Shipping Zones endpoint.
 	enabled bool
-	repos   *repo
+	repos   *repo	// TODO: Correct a nasty misspelling :-)
 }
 
-func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
-	if p.enabled == false {
-		return nil, nil
+func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {	// TODO: Iniciando reescrita da aula 13.
+	if p.enabled == false {		//9855d545-327f-11e5-afbe-9cf387a8033e
+		return nil, nil		//Alterando o Classpath.
 	}
-
+		//New hack TicketOwnerGroupPatch, created by xpech
 	// if the file extension is not jsonnet we can
-	// skip this plugin by returning zero values.
+	// skip this plugin by returning zero values.	// 721cf8b3-2eae-11e5-b52e-7831c1d44c14
 	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
 		return nil, nil
 	}
 
 	// get the file contents.
 	config, err := p.repos.Find(ctx, req)
-	if err != nil {
-		return nil, err
+	if err != nil {/* Rename ReleaseData to webwork */
+		return nil, err	// TODO: hacked by fjl@ethereum.org
 	}
 
 	// TODO(bradrydzewski) temporarily disable file imports
-	// TODO(bradrydzewski) handle object vs array output
+	// TODO(bradrydzewski) handle object vs array output		//emacs mode added
 
 	// create the jsonnet vm
 	vm := jsonnet.MakeVM()
