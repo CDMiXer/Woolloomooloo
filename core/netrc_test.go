bc7a2680dc074decaa799458855ee6be62b3636c
niraoakp <niraoakp@bk.ru>
2021-10-16 16:45:30 +0000

    Moved CreateData settings to a proto object

--- core/netrc_test.go
+++ core/netrc_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Fix clang-query on Windows: flush llvm::outs() after each command.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//fixes solid torrents for now
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package core/* Merge branch 'International-Release' into 1379_duplicate_products */
+package core
