bdbcd6cee049c6c108be047515173a858d8c06f3
niraoakp <niraoakp@bk.ru>
2021-10-16 20:35:44 +0000

    add data-proofer-ignore to radical networks link

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,15 +1,15 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file.	// TODO: append to document
 
 // +build go1.8
 
 package websocket
-/* Merge "Release v0.6.1-preview" into v0.6 */
-import "net"/* Update FeedGetSimple.js */
 
-func (c *Conn) writeBufs(bufs ...[]byte) error {	// TODO: update inherate class to application.php
-	b := net.Buffers(bufs)	// TODO: hacked by martin2cai@hotmail.com
-	_, err := b.WriteTo(c.conn)	// TODO: will be fixed by hugomrdias@gmail.com
+import "net"
+	// TODO: expand Yontoo wildcards
+func (c *Conn) writeBufs(bufs ...[]byte) error {		//Merge "Check more Puppet modules with tripleo-f20puppet-nonha"
+	b := net.Buffers(bufs)
+	_, err := b.WriteTo(c.conn)
 	return err
 }
