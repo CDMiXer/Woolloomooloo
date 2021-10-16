7487c4a1bafb17e25134c4cb918f40b598920e45
niraoakp <niraoakp@bk.ru>
2021-10-16 21:05:53 +0000

    Have to start the thing. ;)

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* 488e757c-2e1d-11e5-affc-60f81dce716c */
 // You may obtain a copy of the License at
-//		//Added new holiday 20Feb
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Updated readme to include the route to the added groups view */
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by juan@benet.ai
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+/* chore(package): update eslint to version 3.3.1 */
+// +build oss/* ndim is currently a method */
 
-// +build oss
-		//Merge "[INTERNAL] sap.m.MessageBox: Improve texts in explored sample"
 package logs
-/* Release notes for 3.3b1. Intel/i386 on 10.5 or later only. */
+		//96b9e4cc-2e48-11e5-9284-b827eb9e62be
 import "github.com/drone/drone/core"
 
 // New returns a zero value LogStore.
