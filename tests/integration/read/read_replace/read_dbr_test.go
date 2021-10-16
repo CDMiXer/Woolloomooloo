9b8abea5dc10990044152962efba30740c048a23
niraoakp <niraoakp@bk.ru>
2021-10-16 09:02:07 +0000

    delete wrong name

--- tests/integration/read/read_replace/read_dbr_test.go
+++ tests/integration/read/read_replace/read_dbr_test.go
@@ -3,28 +3,28 @@
 
 package ints
 
-import (	// hbase/client: refactor check to match namespaces
+import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)		//CHANGE: optimizing CSS for app page
+)
 
 // Test that the engine handles the replacement of an external resource with a
-// owned once gracefully.		//f0ebbbce-2e60-11e5-9284-b827eb9e62be
+// owned once gracefully.
 func TestReadReplace(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",/* Remove unnecessary double lookup */
+		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,/* -исправляю ошибки обьединения кода */
-		EditDirs: []integration.EditDir{		//cleanup find_links_new example some more
+		Quick:        true,
+		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
 			},
-			{		//added short summary of results to top of output file.
+			{
 				Dir:      "step3",
 				Additive: true,
 			},
 		},
-	})	// TODO: will be fixed by vyzo@hackzen.org
+	})
 }
