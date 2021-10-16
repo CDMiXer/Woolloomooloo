fe4479ebb8a29ed27bf70f7d5836335bc4c89f86
niraoakp <niraoakp@bk.ru>
2021-10-16 11:18:40 +0000

    Don't build Docker container for Drone or Jenkins.

--- handler/api/repos/secrets/delete_test.go
+++ handler/api/repos/secrets/delete_test.go
@@ -1,67 +1,67 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Automatic changelog generation #7418 [ci skip]
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package secrets
 
-import (	// TODO: hacked by ng8eke@163.com
+import (
 	"context"
-	"encoding/json"
-	"net/http"
+	"encoding/json"		//Initializing Rich-i18n Javascript modules on page load
+	"net/http"		//Revise variables discussion in README.
 	"net/http/httptest"
 	"testing"
-
+		//Maven artifacts for Mental State Interface definition version 2.0.0-SNAPSHOT
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-/* Ultima Release 7* */
+/* Compute the sizes for a few basic objects. */
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
+)	// Bulk performance improvements by using .indexOf over $.inArray and >= 0
 
 func TestHandleDelete(t *testing.T) {
-	controller := gomock.NewController(t)		//70054a1e-2e49-11e5-9284-b827eb9e62be
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)	// TODO: hacked by vyzo@hackzen.org
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
-	secrets := mock.NewMockSecretStore(controller)
+	secrets := mock.NewMockSecretStore(controller)/* Version 2.1.0 Release */
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
 	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(nil)
 
-	c := new(chi.Context)/* Merge "Release 1.0.0.107 QCACLD WLAN Driver" */
-	c.URLParams.Add("owner", "octocat")/* [IMP]project,association: set project menu and for association set member kanban */
+	c := new(chi.Context)	// added and renamed some fields
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()	// TODO: will be fixed by fjl@ethereum.org
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleDelete(repos, secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNoContent; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	if got, want := w.Code, http.StatusNoContent; want != got {		//addition of transportOptions field to Task
+		t.Errorf("Want response code %d, got %d", want, got)	// TODO: hacked by steven@stebalien.com
+	}/* Initiate connection to current subscribers */
 }
 
-func TestHandleDelete_RepoNotFound(t *testing.T) {		//Update default channel link url in rss.js
+func TestHandleDelete_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)	// nodecount3
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
-
-	c := new(chi.Context)
+	// updated to support jruby-1.1.2
+	c := new(chi.Context)	// TODO: 2c59cc3a-2e40-11e5-9284-b827eb9e62be
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")/* Add Release Note for 1.0.5. */
 	c.URLParams.Add("secret", "github_password")
 
-	w := httptest.NewRecorder()/* Add build entry point script */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
@@ -79,17 +79,17 @@ func TestHandleDelete_RepoNotFound(t *testing.T) {		//Update default channel lin
 	}
 }
 
-func TestHandleDelete_SecretNotFound(t *testing.T) {/* Create mainNight.css */
-	controller := gomock.NewController(t)	// Add coverall run script
+func TestHandleDelete_SecretNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)/* Config for working with Releases. */
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
 	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(nil, errors.ErrNotFound)
 
-	c := new(chi.Context)		//Add missing parameter in pom.xml.
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
@@ -100,16 +100,16 @@ func TestHandleDelete_SecretNotFound(t *testing.T) {/* Create mainNight.css */
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleDelete(repos, secrets).ServeHTTP(w, r)/* crypt MD5 value */
+	HandleDelete(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := new(errors.Error), errors.ErrNotFound	// TODO: Implicit .js extension if no fileName provided
+	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}		//#190 - 1 Ajout colonne contact d'urgence dans fiche de présence
+	}
 }
 
 func TestHandleDelete_DeleteError(t *testing.T) {
