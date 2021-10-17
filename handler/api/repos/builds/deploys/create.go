5758c74902b13b88fcc92f0f72c15300d390e80f
niraoakp <niraoakp@bk.ru>
2021-10-17 20:41:45 +0000

    Update spring-cli and spring-cloud-cli to latest

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//always deploy a tag, not only for production apps 
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Merge "[INTERNAL] m.Tree: invalidation is added for tree and treeitems"
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// updated to new fabric8 version
-///* Adding current trunk revision to tag (Release: 0.8) */
-// Unless required by applicable law or agreed to in writing, software	// Attempting to resolve list rendering issues
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* [artifactory-release] Release version 2.3.0 */
+// Unless required by applicable law or agreed to in writing, software	// files can be filtered by folder
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package deploys
+package deploys	// TODO: will be fixed by davidad@alum.mit.edu
