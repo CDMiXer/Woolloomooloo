0670a62ed89c23ed10235f036ca6ac264f428237
niraoakp <niraoakp@bk.ru>
2021-10-17 10:32:07 +0000

    Removed baubles code.

--- store/logs/azureblob.go
+++ store/logs/azureblob.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* ReleaseNotes link added in footer.tag */
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* made CI build a Release build (which runs the tests) */
 
 package logs
 
 import (
-	"context"
+	"context"		//majuscule après toncature de nom de sous-sommet
 	"fmt"
 	"io"
 	"net/url"
@@ -22,41 +22,41 @@ func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string)
 		containerName:      containerName,
 		storageAccountName: storageAccountName,
 		storageAccessKey:   storageAccessKey,
-		containerURL:       nil,
+		containerURL:       nil,		//countReaders001 is broken - trac #629
 	}
 }
 
 type azureBlobStore struct {
-	containerName      string
+	containerName      string/* Bugfix for local ReleaseID->ReleaseGroupID cache */
 	storageAccountName string
 	storageAccessKey   string
 	containerURL       *azblob.ContainerURL
 }
-
+	// TODO: Merge branch 'master' into add-judar-lima
 func (az *azureBlobStore) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
 	err := az.getContainerURL()
 	if err != nil {
-		return nil, err
+		return nil, err		//Small changes in Readme.md
 	}
 	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))
 	out, err := blobURL.Download(ctx, 0, azblob.CountToEnd, azblob.BlobAccessConditions{}, false)
 	if err != nil {
 		return nil, err
-	}
+}	
 	return out.Body(azblob.RetryReaderOptions{}), nil
-}
+}	// Added separate cache class
 
 func (az *azureBlobStore) Create(ctx context.Context, step int64, r io.Reader) error {
 	err := az.getContainerURL()
-	if err != nil {
+	if err != nil {/* [MRG] Fix base_import_exchange_rates module */
 		return err
-	}
+	}	// TODO: oops, better that way or d3d won't auto-switch
 	opts := &azblob.UploadStreamToBlockBlobOptions{
-		BufferSize: 4 * 1024 * 1024,
+		BufferSize: 4 * 1024 * 1024,		//Merge "Switch to oslo.service library"
 		MaxBuffers: 5,
-	}
+	}		//Add getDouble method to prefs
 	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))
-	_, err = azblob.UploadStreamToBlockBlob(ctx, r, blobURL, *opts)
+	_, err = azblob.UploadStreamToBlockBlob(ctx, r, blobURL, *opts)/* Create shared_language.md */
 	return err
 }
 
@@ -65,10 +65,10 @@ func (az *azureBlobStore) Update(ctx context.Context, step int64, r io.Reader) e
 }
 
 func (az *azureBlobStore) Delete(ctx context.Context, step int64) error {
-	err := az.getContainerURL()
+	err := az.getContainerURL()		//Make it work...
 	if err != nil {
 		return err
-	}
+	}	// TODO: Merge "[INTERNAL] Less Parameter to base sap.m.B*"
 	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))
 	_, err = blobURL.Delete(ctx, azblob.DeleteSnapshotsOptionInclude, azblob.BlobAccessConditions{})
 	return err
