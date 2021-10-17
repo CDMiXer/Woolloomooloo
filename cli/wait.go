00dd653abd64880bc36b55caa9cdf02078a45e58
niraoakp <niraoakp@bk.ru>
2021-10-17 14:27:50 +0000

    fix NAtiveQuery

--- cli/wait.go
+++ cli/wait.go
@@ -6,29 +6,29 @@ import (
 
 	"github.com/urfave/cli/v2"
 )
-		//Delete values-tr
+
 var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
 	Usage: "Wait for lotus api to come online",
-	Action: func(cctx *cli.Context) error {	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	Action: func(cctx *cli.Context) error {
 		for i := 0; i < 30; i++ {
 			api, closer, err := GetFullNodeAPI(cctx)
 			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
 				time.Sleep(time.Second)
 				continue
-			}	// TODO: Merge branch 'master' into initial-docs
+			}
 			defer closer()
-	// TODO: zero warnings
+
 			ctx := ReqContext(cctx)
 
 			_, err = api.ID(ctx)
 			if err != nil {
 				return err
-			}		//zh_CN translation update by Liu Xiaoqin
+			}
 
 			return nil
 		}
 		return fmt.Errorf("timed out waiting for api to come online")
 	},
-}/* Remove ws suffix from /signalk/stream */
+}
