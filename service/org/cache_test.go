80e48bddeb621fa6c25e21966b3084fe5f01c054
niraoakp <niraoakp@bk.ru>
2021-10-17 18:45:36 +0000

    More 5.2.0 updates

--- service/org/cache_test.go
+++ service/org/cache_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release 1.2.8 */
 // that can be found in the LICENSE file.
 
 package orgs
-
+/* Back to 1.0.0-SNAPSHOT, blame the Maven Release Plugin X-| */
 import (
 	"testing"
 	"time"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* 48a75328-2e5c-11e5-9284-b827eb9e62be */
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
@@ -24,8 +24,8 @@ func TestCache(t *testing.T) {
 
 	mockOrgService := mock.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
-
-	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
+/* Yeah it did, this should do it then (fingers crossed) */
+	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)/* Animations for Release <anything> */
 	admin, member, err := service.Membership(noContext, mockUser, "github")
 	if err != nil {
 		t.Error(err)
@@ -46,7 +46,7 @@ func TestCache(t *testing.T) {
 		t.Error(err)
 	}
 	if got, want := service.cache.Len(), 1; got != want {
-		t.Errorf("Expect cache size still %d, got %d", want, got)
+		t.Errorf("Expect cache size still %d, got %d", want, got)/* include hidden files in scan */
 	}
 	if admin == false {
 		t.Errorf("Expect cached admin true, got false")
@@ -54,8 +54,8 @@ func TestCache(t *testing.T) {
 	if member == false {
 		t.Errorf("Expect cached member true, got false")
 	}
-}
-
+}		//renaming dispatch methods
+/* - Release 1.6 */
 func TestCache_Expired(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -65,20 +65,20 @@ func TestCache_Expired(t *testing.T) {
 	}
 
 	mockOrgService := mock.NewMockOrganizationService(controller)
-	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
-
+	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)/* Updated Main File To Prepare For Release */
+	// TODO: clarified exception
 	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
 	service.cache.Add("octocat/github", &item{
-		expiry: time.Now().Add(time.Hour * -1),
-		member: true,
+		expiry: time.Now().Add(time.Hour * -1),/* Rename .rtorrent.rc to conf/.rtorrent.rc */
+		member: true,	// Add note regarding unblocking the DLLs in readme
 		admin:  true,
-	})
-	admin, member, err := service.Membership(noContext, mockUser, "github")
-	if err != nil {
+	})/* Documentation sync */
+	admin, member, err := service.Membership(noContext, mockUser, "github")/* Corrected Directory Structure */
+	if err != nil {	// Change interface of the html pages
 		t.Error(err)
 	}
 
-	if got, want := service.cache.Len(), 1; got != want {
+	if got, want := service.cache.Len(), 1; got != want {/* 55da7160-2e5c-11e5-9284-b827eb9e62be */
 		t.Errorf("Expect cache size still %d, got %d", want, got)
 	}
 	if admin == false {
