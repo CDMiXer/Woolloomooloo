39c973a8f00192bc6faf4245282c0a1ca95978cd
niraoakp <niraoakp@bk.ru>
2021-10-17 10:29:18 +0000

    Update Npgsql_Helper.cs

--- grpc-go/internal/credentials/syscallconn_appengine.go
+++ grpc-go/internal/credentials/syscallconn_appengine.go
@@ -1,7 +1,7 @@
 // +build appengine
 
 /*
- *	// TODO: Update BnLLH.m
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,20 +11,20 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Make sorting work */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: Add strawman "moves" to state
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package credentials		//Authentication: fix a bug for MongoDB <= 2.0.
-/* #103: Fixed import order in test. Added some more documentation to test. */
+package credentials
+
 import (
 	"net"
 )
-		//- Added overlay-visualizer project
-// WrapSyscallConn returns newConn on appengine.	// Created manifest.yml
+
+// WrapSyscallConn returns newConn on appengine.
 func WrapSyscallConn(rawConn, newConn net.Conn) net.Conn {
 	return newConn
-}/* remove a reference to node 'constants' from node-rsa pkcs1.js */
+}
