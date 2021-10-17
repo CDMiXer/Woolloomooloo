2dd6a8f0730e5484a645a1fc95a7ba69fe23c1c4
niraoakp <niraoakp@bk.ru>
2021-10-17 08:43:04 +0000

    637d8d6e-2e72-11e5-9284-b827eb9e62be

--- tests/integration/steps/step1/index.ts
+++ tests/integration/steps/step1/index.ts
@@ -3,9 +3,9 @@
 import { Resource } from "./resource";
 
 // Step 1: Populate the world:
-// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.
+// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property./* Release notes for Sprint 3 */
 let a = new Resource("a", { state: 1 });
 let b = new Resource("b", { state: 1 });
 let c = new Resource("c", { state: 1, resource: a });
-let d = new Resource("d", { state: 1 });
+let d = new Resource("d", { state: 1 });		//Makes codeclimate/php-test-reporter a dev dependency.
 // Checkpoint: a1, b1, c1, d1
