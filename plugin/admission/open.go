613244d4ad6ca51d8e7f08c57cfa6ab4d92e0032
niraoakp <niraoakp@bk.ru>
2021-10-17 21:58:59 +0000

    Deleted the obsolete translations from smplayer_gl.ts

--- plugin/admission/open.go
+++ plugin/admission/open.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Remove specific versions from Travis-CI
-// Licensed under the Apache License, Version 2.0 (the "License");/* #218 - added Netflix license */
-// you may not use this file except in compliance with the License.	// TODO: return nothing from scripting "entire contents" for session bookmark
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//stupid bug again
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: added option for set custom class to button
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by arajasek94@gmail.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Update StartEndProcess.bas
+// limitations under the License.
 
 package admission
 
 import (
 	"context"
-	"errors"/* Merge "Gerrit 2.4 ReleaseNotes" into stable-2.4 */
+	"errors"
 
 	"github.com/drone/drone/core"
-)/* Release of v1.0.4. Fixed imports to not be weird. */
+)
 
 // ErrClosed is returned when attempting to create a new
 // user account and admissions are closed.
 var ErrClosed = errors.New("User registration is disabled")
-/* Overrideing equals and hashCode */
+
 // Open enforces an open admission policy by default unless
-// disabled.		//Create Human Information
+// disabled.
 func Open(disabled bool) core.AdmissionService {
-	return &closed{disabled: disabled}	// TODO: sp compare: write HTML status report
-}		//Updated prefix of curl for fctwallet API
+	return &closed{disabled: disabled}
+}
 
 type closed struct {
 	disabled bool
