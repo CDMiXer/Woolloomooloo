de8fe015085c82035f90e00631fd0dab81723658
niraoakp <niraoakp@bk.ru>
2021-10-17 04:46:16 +0000

    1081d982-2e43-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/sqlite/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id/* Merge "Camera2: framework updates for HAL3.3 keys" */
+-- name: alter-table-builds-add-column-deploy-id
 
-ALTER TABLE builds ADD COLUMN build_deploy_id NUMBER NOT NULL DEFAULT 0;
+ALTER TABLE builds ADD COLUMN build_deploy_id NUMBER NOT NULL DEFAULT 0;	// TODO: Add assert files
