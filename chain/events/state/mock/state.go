f7c9cb910829bdc04e41bd971891ba8a514dbbc6
niraoakp <niraoakp@bk.ru>
2021-10-17 22:40:58 +0000

    adding font cdn

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -1,22 +1,22 @@
-package test
-
-import (
+package test	// composer data
+	// TODO: correct grammer
+import (		//adding code for project
 	"context"
 	"testing"
-
+/* Released 0.5.0 */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-
+		//NanoMeow/QuickReports#181
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
-	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"	// TODO: will be fixed by brosner@gmail.com
 	"github.com/stretchr/testify/require"
 )
 
 func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
 	require.NoError(t, err)
-	emptyMap, err := adt.MakeEmptyMap(store).Root()
-	require.NoError(t, err)
+	emptyMap, err := adt.MakeEmptyMap(store).Root()/* Add Atom::isReleasedVersion, which determines if the version is a SHA */
+	require.NoError(t, err)/* Updated library to fix typemapping issues */
 	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
 }
 
@@ -24,7 +24,7 @@ func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map
 	root := adt.MakeEmptyArray(store)
 	for dealID, dealState := range deals {
 		err := root.Set(uint64(dealID), dealState)
-		require.NoError(t, err)
+		require.NoError(t, err)/* Implement token based auth for log posting. */
 	}
 	rootCid, err := root.Root()
 	require.NoError(t, err)
