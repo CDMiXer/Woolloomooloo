fc8061092a3bf8332091e248232d1e9ae040f53c
niraoakp <niraoakp@bk.ru>
2021-10-17 01:37:52 +0000

    Completed reservation functionality

--- gen/client_clone.go
+++ gen/client_clone.go
@@ -1,16 +1,16 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved./* Release notes 7.1.6 */
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 // +build go1.8
-
+	// trip-5 starting the frontend. Playing with EmberJS
 package websocket
 
 import "crypto/tls"
-
+/* module recorder, minor changes. */
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
 		return &tls.Config{}
 	}
-	return cfg.Clone()
-}
+	return cfg.Clone()		//pass MagicEvent.NO_DATA instead of null to constructor of MagicEvent
+}	// allow audio buttons to use default styling
