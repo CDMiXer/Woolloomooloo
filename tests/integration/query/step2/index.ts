648d7ce7fccd2d3b483f27b1e05096255c08b8b5
niraoakp <niraoakp@bk.ru>
2021-10-17 14:38:16 +0000

    remove capital

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-
-// Step 2: Create resources during `pulumi query` -- should error.
-const b = new Resource("b", { state: 2 });
+		//Rename fastest5k.user.js to Runkeeper_Fastest_5k.user.js
+// Step 2: Create resources during `pulumi query` -- should error./* @Release [io7m-jcanephora-0.9.8] */
+const b = new Resource("b", { state: 2 });/* 1.0.7 Release */
 const a = new Resource("a", { state: 1, resource: b });
