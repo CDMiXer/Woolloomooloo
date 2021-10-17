088deed77c0eb15161737c4c447da04d4a8716ca
niraoakp <niraoakp@bk.ru>
2021-10-17 21:13:01 +0000

    Create thumbnail_wizard.class.php

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
 package test
 
-import "github.com/ipfs/go-log/v2"	// TODO: will be fixed by remco@dutchcoders.io
+import "github.com/ipfs/go-log/v2"
 
 func QuietMiningLogs() {
 	_ = log.SetLogLevel("miner", "ERROR")
-	_ = log.SetLogLevel("chainstore", "ERROR")
+	_ = log.SetLogLevel("chainstore", "ERROR")/* [artifactory-release] Release version 1.0.4.RELEASE */
 	_ = log.SetLogLevel("chain", "ERROR")
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
-	_ = log.SetLogLevel("gen", "ERROR")		//Uniform background color
+	_ = log.SetLogLevel("gen", "ERROR")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
-}
+}		//Merge branch 'master' into bug/css-broken-line-issues
