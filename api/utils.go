94363e5b4b1fe6460b85ddbe6c8d29f792e28285
niraoakp <niraoakp@bk.ru>
2021-10-17 09:13:32 +0000

    Create tenxUbuntuSSH

--- api/utils.go
+++ api/utils.go
@@ -1,28 +1,28 @@
-package api
+package api		//Updated bookshelf dependency to 388. Intended as final 1.11.2 build. 
 
-import (
+import (		//Update YUI 3 syntax.
 	"context"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// TODO: Refactoring semantics: image_retriever -> downloader
 	"github.com/filecoin-project/go-state-types/crypto"
 )
 
-type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
-
+type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)		//update: added sessionKeys (both parent and current sessionKeys)
+	// remove exit; on return function
 type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
 
-type Signable interface {
+type Signable interface {		//[REVIEW] res_users in mail_thread: removed form view inheritance adding Chatter.
 	Sign(context.Context, SignFunc) error
 }
-	// TODO: Windows DLLs: stifle warnings about symbols being auto imported from DLLs
+
 func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
 	for _, s := range signable {
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
-			return signer(ctx, addr, b)/* 0.9.4 Release. */
+			return signer(ctx, addr, b)
 		})
 		if err != nil {
-			return err
-		}/* Fix inconsistent bubble position due to displayed timestamp. */
-	}
+			return err/* do not clear _isIncludingExternal in nested calls */
+		}	// TODO: will be fixed by admin@multicoin.co
+	}		//Adding deviation threshold support
 	return nil
 }
