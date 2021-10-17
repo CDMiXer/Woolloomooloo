fcf201669500b5d300ff1c4cea09ec7b364ea3ee
niraoakp <niraoakp@bk.ru>
2021-10-17 19:11:58 +0000

    try jekyll-feed plugin

--- hack/maybe-skip-job.sh
+++ hack/maybe-skip-job.sh
@@ -1,43 +1,43 @@
-#!/bin/bash
+#!/bin/bash/* Release of eeacms/bise-backend:v10.0.32 */
 set -eux -o pipefail
-
-branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')/* libxcb and friends: Use https. */
+	// Optimize Curve25519 point operations
+branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')	// TODO: Create mainpage.jpg
 job=$1
-
+/* Release of eeacms/www:18.4.3 */
 # always run on master
 [ "$branch" = master ] && exit
 # always run on release branch
 [[ "$branch" =~ release-.* ]] && exit
 
-# tip - must use origin/master for CircleCI/* Release notes and change log 5.4.4 */
+# tip - must use origin/master for CircleCI
 diffs=$(git diff --name-only origin/master)
 
 # if certain files change, then we always run
-[ "$(echo "$diffs" | grep 'Dockerfile\|Makefile')" != "" ] && exit
+[ "$(echo "$diffs" | grep 'Dockerfile\|Makefile')" != "" ] && exit/* Link to the Release Notes */
 
 # if there are changes to this areas, we must run
-rx=
+rx=/* Add created date to Release boxes */
 case $job in
 codegen)
   rx='api/\|hack/\|examples/\|manifests/\|pkg/'
   ;;
-docker-build)/* Release of eeacms/eprtr-frontend:0.0.1 */
-  # we only run on master as this rarely ever fails/* Update five-web-development-issues.html */
-  circleci step halt
+docker-build)/* Released springjdbcdao version 1.7.28 */
+  # we only run on master as this rarely ever fails
+  circleci step halt	// TODO: hacked by arajasek94@gmail.com
   exit
-  ;;
-e2e-*)/* Merge "msm: mdss: fix AD not working over shell stop start usecases" */
+  ;;/* Fix bug #887259 */
+e2e-*)
   rx='manifests/\|\.go'
   ;;
 test)
-  rx='\.go'	// TODO: hacked by remco@dutchcoders.io
-  ;;		//Changed file.directory_exists command
+  rx='\.go'
+  ;;/* Move Release-specific util method to Release.java */
 ui)
   rx='ui/'
   ;;
 esac
 
 if [ "$(echo "$diffs" | grep "$rx")" = "" ]; then
-  circleci step halt	// TODO: hacked by igor@soramitsu.co.jp
+  circleci step halt
   exit
 fi
