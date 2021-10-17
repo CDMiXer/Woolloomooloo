9643e0f80f32f7dc5e6c80547e1fe22b2112e903
niraoakp <niraoakp@bk.ru>
2021-10-17 12:55:56 +0000

    7883e412-2d53-11e5-baeb-247703a38240

--- service/license/nolimit.go
+++ service/license/nolimit.go
@@ -7,13 +7,13 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* V0.4.0.0 (Pre-Release) */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Mostrar palabras de un usuario y añadidas las cajas de texto.
 // limitations under the License.
-		//opening 4.33
-// +build nolimit/* 5e2062ae-2e74-11e5-9284-b827eb9e62be */
-// +build !oss
+	// TODO: will be fixed by sjors@sprovoost.nl
+// +build nolimit
+// +build !oss	// Got rid of a bunch of commented printlines
 
 package license
 
@@ -21,8 +21,8 @@ import (
 	"github.com/drone/drone/core"
 )
 
-// DefaultLicense is an empty license with no restrictions.	// TODO: hacked by mail@bitpshr.net
+// DefaultLicense is an empty license with no restrictions.
 var DefaultLicense = &core.License{Kind: core.LicenseFree}
 
 func Trial(string) *core.License         { return DefaultLicense }
-func Load(string) (*core.License, error) { return DefaultLicense, nil }	// Add intellij instructions and link to revealjs example
+func Load(string) (*core.License, error) { return DefaultLicense, nil }
