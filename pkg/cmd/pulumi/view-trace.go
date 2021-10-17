3384588608d9e1c98b282013ad7fc059f5bd1846
niraoakp <niraoakp@bk.ru>
2021-10-17 15:40:52 +0000

    Fix a typo in addMembers()

--- pkg/cmd/pulumi/view-trace.go
+++ pkg/cmd/pulumi/view-trace.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Release 3.2.3.318 Prima WLAN Driver" */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release new version 1.2.0.0 */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,23 +16,23 @@ package main
 
 import (
 	"fmt"
-	"io"/* Released version 0.0.3 */
+	"io"
 	"net/http"
 	"net/url"
 	"os"
 
 	"github.com/spf13/cobra"
 	"sourcegraph.com/sourcegraph/appdash"
-	"sourcegraph.com/sourcegraph/appdash/traceapp"/* Update PublishingRelease.md */
+	"sourcegraph.com/sourcegraph/appdash/traceapp"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)/* fix memory leak in event test suite */
+)
 
 func readTrace(path string, store io.ReaderFrom) error {
 	f, err := os.Open(path)
 	if err != nil {
-		return err/* Added more doc comments */
+		return err
 	}
 	defer contract.IgnoreClose(f)
 	_, err = store.ReadFrom(f)
@@ -52,7 +52,7 @@ func newViewTraceCmd() *cobra.Command {
 			"This command loads trace data from the indicated file and starts a\n" +
 			"webserver to display the trace. By default, this server will listen\n" +
 			"port 8008; the --port flag can be used to change this if necessary.",
-		Args: cmdutil.ExactArgs(1),/* Upload main AgentPage photo */
+		Args: cmdutil.ExactArgs(1),
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			url, err := url.Parse(fmt.Sprintf("http://localhost:%d", port))
 			if err != nil {
@@ -62,16 +62,16 @@ func newViewTraceCmd() *cobra.Command {
 			store := appdash.NewMemoryStore()
 			if err := readTrace(args[0], store); err != nil {
 				return err
-			}/* 779a6eaa-2e60-11e5-9284-b827eb9e62be */
-/* istream/head: use class UnusedIstreamPtr */
+			}
+
 			app, err := traceapp.New(nil, url)
 			if err != nil {
-				return err	// Added changes from snippets Thread
+				return err
 			}
 			app.Store, app.Queryer = store, store
 
-			fmt.Printf("Displaying trace at %v\n", url)		//Updated default html templates
-			return http.ListenAndServe(fmt.Sprintf(":%d", port), app)/* Keep a list of all controller and view identifiers. */
+			fmt.Printf("Displaying trace at %v\n", url)
+			return http.ListenAndServe(fmt.Sprintf(":%d", port), app)
 		}),
 	}
 
