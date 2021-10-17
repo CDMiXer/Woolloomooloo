9970a99cf1a0036925c386231a45c7b75c7c1873
niraoakp <niraoakp@bk.ru>
2021-10-17 23:35:35 +0000

    Set ContentType for HTTPs Message sender

--- build/openrpc.go
+++ build/openrpc.go
@@ -1,43 +1,43 @@
-package build
+package build		//Use virtualbox provider
 
 import (
-	"bytes"
+	"bytes"/* Добавлена поддержка отправки tcp rst из фильтра, без использования iptables */
 	"compress/gzip"
 	"encoding/json"
 
-	rice "github.com/GeertJohan/go.rice"
+	rice "github.com/GeertJohan/go.rice"/* Release: Making ready for next release iteration 5.7.0 */
 
-	apitypes "github.com/filecoin-project/lotus/api/types"
+	apitypes "github.com/filecoin-project/lotus/api/types"/* remove un use files */
 )
 
 func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
 	zr, err := gzip.NewReader(bytes.NewBuffer(data))
 	if err != nil {
 		log.Fatal(err)
-	}
+	}/* Fixed deadlock in Subjects + OperatorCache. */
 	m := apitypes.OpenRPCDocument{}
 	err = json.NewDecoder(zr).Decode(&m)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err)/* Creating test case to resproduce verification in FillingStationAdvisor. */
 	}
-	err = zr.Close()
+	err = zr.Close()/* Added CI build status to README */
 	if err != nil {
 		log.Fatal(err)
 	}
 	return m
 }
 
-func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {
-	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")
-	return mustReadGzippedOpenRPCDocument(data)
+func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {/* Added link to Salesforce Labs app */
+	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")	// TODO: Provide MPI_Type_create_indexed_block if not available
+	return mustReadGzippedOpenRPCDocument(data)		//Update format.txt
 }
-
+	// Create cardinality.yaml
 func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")
-	return mustReadGzippedOpenRPCDocument(data)
+	return mustReadGzippedOpenRPCDocument(data)	// TODO: triaging 404 in IdNotFoundException
 }
-
-func OpenRPCDiscoverJSON_Worker() apitypes.OpenRPCDocument {
+/* Release 0.0.16. */
+func OpenRPCDiscoverJSON_Worker() apitypes.OpenRPCDocument {/* * Fix tiny oops in interface.py. Release without bumping application version. */
 	data := rice.MustFindBox("openrpc").MustBytes("worker.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
-}
+}/* [minor] only verify keyid of signer against full pgp fingerprint, not email */
