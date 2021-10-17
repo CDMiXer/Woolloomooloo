1f66993ca8319e30fbf872ce265768b90e64ab2c
niraoakp <niraoakp@bk.ru>
2021-10-17 22:57:38 +0000

    moved the narrator

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by seth@sethvargo.com
-///* Updates for style */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by earlephilhower@yahoo.com
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release: change splash label to 1.2.1 */
-//
+///* added deivid's handle */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//	// c5209a4a-2e52-11e5-9284-b827eb9e62be
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Delete pic7.JPG
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
