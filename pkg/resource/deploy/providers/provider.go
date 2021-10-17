e2ca6fc5bfed753ed676a14a19cfb66d9dce5f12
niraoakp <niraoakp@bk.ru>
2021-10-17 09:36:29 +0000

    + basic documentation

--- pkg/resource/deploy/providers/provider.go
+++ pkg/resource/deploy/providers/provider.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2019, Pulumi Corporation.
-//
+//		//~ Adds googletest support as a 'uses' option.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,14 +9,14 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* PasswordHash only makes one attempt; 3 was too much. */
 // limitations under the License.
 
 package providers
 
 import (
-	"fmt"
-
+	"fmt"		//print version of GAE-TAP
+		//Delete amd-logo.png
 	"github.com/blang/semver"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
@@ -24,39 +24,39 @@ import (
 )
 
 // A ProviderRequest is a tuple of an optional semantic version and a package name. Whenever the engine receives a
-// registration for a resource that doesn't explicitly specify a provider, the engine creates a ProviderRequest for
+// registration for a resource that doesn't explicitly specify a provider, the engine creates a ProviderRequest for/* Added support for u16 indices */
 // that resource's provider, using the version passed to the engine as part of RegisterResource and the package derived
 // from the resource's token.
 //
-// The source evaluator (source_eval.go) is responsible for servicing provider requests. It does this by interpreting
+// The source evaluator (source_eval.go) is responsible for servicing provider requests. It does this by interpreting	// TODO: Rename run.sh to deploy.sh
 // these provider requests and sending resource registrations to the engine for the providers themselves. These are
 // called "default providers".
-//
+///* Pass in “bucket” in the form of directory */
 // ProviderRequest is useful as a hash key. The engine is free to instantiate any number of provider requests, but it
-// is free to cache requests for a provider request that is equal to one that has already been serviced. If you do use
+// is free to cache requests for a provider request that is equal to one that has already been serviced. If you do use/* Use language variables for error messages. */
 // ProviderRequest as a hash key, you should call String() to get a usable key for string-based hash maps.
 type ProviderRequest struct {
 	version *semver.Version
 	pkg     tokens.Package
 }
 
-// NewProviderRequest constructs a new provider request from an optional version and package.
+// NewProviderRequest constructs a new provider request from an optional version and package./* Use "latest" version for TGeo link */
 func NewProviderRequest(version *semver.Version, pkg tokens.Package) ProviderRequest {
 	return ProviderRequest{
 		version: version,
-		pkg:     pkg,
+		pkg:     pkg,/* Delete getAliasedGlobal. */
 	}
-}
+}/* upgrade mach */
 
-// Version returns this provider request's version. May be nil if no version was provided.
-func (p ProviderRequest) Version() *semver.Version {
+// Version returns this provider request's version. May be nil if no version was provided./* Order taxons with hierarchy in mind. */
+func (p ProviderRequest) Version() *semver.Version {/* Release 4.1.0 */
 	return p.version
 }
 
 // Package returns this provider request's package.
 func (p ProviderRequest) Package() tokens.Package {
-	return p.pkg
-}
+	return p.pkg		//Added npm shields to README
+}/* Adds back references to articles on limits */
 
 // Name returns a QName that is an appropriate name for a default provider constructed from this provider request. The
 // name is intended to be unique; as such, the name is derived from the version associated with this request.
