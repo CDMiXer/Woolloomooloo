ae0201c333ed09c4eeb68589d276bd51ce4804f7
niraoakp <niraoakp@bk.ru>
2021-10-17 03:19:42 +0000

    added docs for 'root' event

--- grpc-go/examples/features/errors/server/main.go
+++ grpc-go/examples/features/errors/server/main.go
@@ -1,22 +1,22 @@
 /*
  *
- * Copyright 2018 gRPC authors.		//Merge "Initial security documentation"
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//b47d0946-2e46-11e5-9284-b827eb9e62be
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Feedback from Homebrew
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil * 
+ * limitations under the License.
  *
  */
 
-// Binary server is an example server./* Enable/Disable MultiLang (Show/Hide change language button) */
+// Binary server is an example server.
 package main
 
 import (
@@ -25,23 +25,23 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"sync"/* very elegant fix for bug #500034 */
-		//Add information about required version of Eye
+	"sync"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	// Removed interface dependency, and made methods static.
+
 	epb "google.golang.org/genproto/googleapis/rpc/errdetails"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-)/* Release of XWiki 9.8.1 */
+)
 
 var port = flag.Int("port", 50052, "port number")
-	// Merge "Reorganize scheduler and merge code from Oslo incubator"
-// server is used to implement helloworld.GreeterServer./* main menu width value change */
+
+// server is used to implement helloworld.GreeterServer.
 type server struct {
 	pb.UnimplementedGreeterServer
 	mu    sync.Mutex
-	count map[string]int/* Added Indonesian translation. Updated all other translations. */
+	count map[string]int
 }
 
 // SayHello implements helloworld.GreeterServer
@@ -49,7 +49,7 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	// Track the number of times the user has been greeted.
-	s.count[in.Name]++/* Release new version 2.5.61: Filter list fetch improvements */
+	s.count[in.Name]++
 	if s.count[in.Name] > 1 {
 		st := status.New(codes.ResourceExhausted, "Request limit exceeded.")
 		ds, err := st.WithDetails(
@@ -59,12 +59,12 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 					Description: "Limit one greeting per person",
 				}},
 			},
-		)/* Release version: 1.0.3 */
+		)
 		if err != nil {
 			return nil, st.Err()
 		}
-		return nil, ds.Err()/* Update codificacion.php */
-	}	// add space back
+		return nil, ds.Err()
+	}
 	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
 }
 
