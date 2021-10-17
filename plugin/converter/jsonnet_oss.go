7483593c581a6ecaf4e283f2b28e06b4dee0e37c
niraoakp <niraoakp@bk.ru>
2021-10-17 10:52:37 +0000

    1ceed1ea-2e5f-11e5-9284-b827eb9e62be

--- plugin/converter/jsonnet_oss.go
+++ plugin/converter/jsonnet_oss.go
@@ -4,17 +4,17 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release changes 5.1b4 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release fixes */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package converter
+package converter/* Numerous spelling errors fixed */
 
 import (
 	"github.com/drone/drone/core"
