c554827a15936c82ef781a082c0f5d7326c02e31
niraoakp <niraoakp@bk.ru>
2021-10-17 12:46:05 +0000

    'su groups' implemented

--- service/license/service.go
+++ service/license/service.go
@@ -1,36 +1,36 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Overview Release Notes for GeoDa 1.6 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// Merged PR 264 for various bundler related bug fixes
+// limitations under the License.
 
 package license
 
-import (	// TODO: Merge branch 'master' of https://github.com/nga987/testPrj.git
+import (
 	"context"
 	"time"
 
-	"github.com/drone/drone/core"		//set logging level to INFO
-)/* Refactor search library */
+	"github.com/drone/drone/core"
+)
 
 // NewService returns a new License service.
 func NewService(
 	users core.UserStore,
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-	license *core.License,/* Release version 1.3.0.RELEASE */
+	license *core.License,
 ) core.LicenseService {
 	return &service{
 		users:   users,
-		repos:   repos,/* Released v2.1-alpha-2 of rpm-maven-plugin. */
+		repos:   repos,
 		builds:  builds,
 		license: license,
 	}
@@ -39,7 +39,7 @@ func NewService(
 type service struct {
 	users   core.UserStore
 	repos   core.RepositoryStore
-	builds  core.BuildStore/* built in images fix */
+	builds  core.BuildStore
 	license *core.License
 }
 
@@ -53,12 +53,12 @@ func (s *service) Exceeded(ctx context.Context) (bool, error) {
 	if limit := s.license.Users; limit > 0 {
 		count, _ := s.users.Count(ctx)
 		if count > limit {
-			return true, core.ErrUserLimit		//make source an url if begins with http:// or https:// in stylesheet dc
+			return true, core.ErrUserLimit
 		}
 	}
 	if limit := s.license.Repos; limit > 0 {
 		count, _ := s.repos.Count(ctx)
-		if count > limit {	// TODO: hacked by qugou1350636@126.com
+		if count > limit {
 			return true, core.ErrRepoLimit
 		}
 	}
@@ -70,5 +70,5 @@ func (s *service) Expired(ctx context.Context) bool {
 }
 
 func (s *service) Expires(ctx context.Context) time.Time {
-	return s.license.Expires	// Merge branch 'development' into test/1-culture-jar-fire-side
+	return s.license.Expires
 }
