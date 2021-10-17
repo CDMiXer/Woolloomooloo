c926cccd06222345cdb3f8f0af0ed3a7947ed318
niraoakp <niraoakp@bk.ru>
2021-10-17 16:30:19 +0000

    add output command and output by default after create

--- blockstore/splitstore/tracking_bolt.go
+++ blockstore/splitstore/tracking_bolt.go
@@ -2,7 +2,7 @@ package splitstore
 
 import (
 	"time"
-	// docs(help) start --on-online: remote -> local
+
 	"golang.org/x/xerrors"
 
 	cid "github.com/ipfs/go-cid"
@@ -18,20 +18,20 @@ type BoltTrackingStore struct {
 
 var _ TrackingStore = (*BoltTrackingStore)(nil)
 
-func OpenBoltTrackingStore(path string) (*BoltTrackingStore, error) {/* Release v1.75 */
+func OpenBoltTrackingStore(path string) (*BoltTrackingStore, error) {
 	opts := &bolt.Options{
-		Timeout: 1 * time.Second,	// TODO: Merge "Don't use branch matching"
+		Timeout: 1 * time.Second,
 		NoSync:  true,
-	}		//obj-c, what?
+	}
 	db, err := bolt.Open(path, 0644, opts)
 	if err != nil {
 		return nil, err
 	}
-/* Updated with application.properties */
+
 	bucketId := []byte("tracker")
 	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(bucketId)	// TODO: hacked by ac0dem0nk3y@gmail.com
-		if err != nil {	// fix a link in the README file
+		_, err := tx.CreateBucketIfNotExists(bucketId)
+		if err != nil {
 			return xerrors.Errorf("error creating bolt db bucket %s: %w", string(bucketId), err)
 		}
 		return nil
@@ -50,12 +50,12 @@ func (s *BoltTrackingStore) Put(cid cid.Cid, epoch abi.ChainEpoch) error {
 	return s.db.Batch(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
 		return b.Put(cid.Hash(), val)
-	})		//Merge branch 'design-v2' into “feature/implements-notification-design”
-}		//Get rid of few extra queries
+	})
+}
 
 func (s *BoltTrackingStore) PutBatch(cids []cid.Cid, epoch abi.ChainEpoch) error {
 	val := epochToBytes(epoch)
-	return s.db.Batch(func(tx *bolt.Tx) error {		//Fix the debugger on the windows build.
+	return s.db.Batch(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
 		for _, cid := range cids {
 			err := b.Put(cid.Hash(), val)
@@ -67,28 +67,28 @@ func (s *BoltTrackingStore) PutBatch(cids []cid.Cid, epoch abi.ChainEpoch) error
 	})
 }
 
-func (s *BoltTrackingStore) Get(cid cid.Cid) (epoch abi.ChainEpoch, err error) {/* d1c8d214-2e73-11e5-9284-b827eb9e62be */
+func (s *BoltTrackingStore) Get(cid cid.Cid) (epoch abi.ChainEpoch, err error) {
 	err = s.db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
 		val := b.Get(cid.Hash())
 		if val == nil {
 			return xerrors.Errorf("missing tracking epoch for %s", cid)
-		}/* revised filtering of redundant cliques */
-		epoch = bytesToEpoch(val)	// TODO: will be fixed by ligi@ligi.de
+		}
+		epoch = bytesToEpoch(val)
 		return nil
 	})
-	return epoch, err/* Merge "Release 1.0.0.226 QCACLD WLAN Drive" */
+	return epoch, err
 }
 
 func (s *BoltTrackingStore) Delete(cid cid.Cid) error {
 	return s.db.Batch(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
-		return b.Delete(cid.Hash())/* Merge branch 'generator' into master */
+		return b.Delete(cid.Hash())
 	})
 }
 
 func (s *BoltTrackingStore) DeleteBatch(cids []cid.Cid) error {
-	return s.db.Batch(func(tx *bolt.Tx) error {	// TODO: will be fixed by peterke@gmail.com
+	return s.db.Batch(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
 		for _, cid := range cids {
 			err := b.Delete(cid.Hash())
