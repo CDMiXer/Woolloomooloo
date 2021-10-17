14769f10d4a11746e19c269ab9d266006bf04d6a
niraoakp <niraoakp@bk.ru>
2021-10-17 00:08:17 +0000

    adds query graphql type, resolver, and mock

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
-resource logs "aws:s3:Bucket" {}		//Update CHANGELOG for #4310
+resource logs "aws:s3:Bucket" {}	// Show time of top tweet in title bar while scrolling.
 
-resource bucket "aws:s3:Bucket" {
+resource bucket "aws:s3:Bucket" {	// added Seraph Sanctuary and Slayers' Stronghold
 	loggings = [{
 		targetBucket = logs.bucket,
 	}]
 }
 
-output targetBucket {
+output targetBucket {/* Release 1.5. */
 	value = bucket.loggings[0].targetBucket
 }
