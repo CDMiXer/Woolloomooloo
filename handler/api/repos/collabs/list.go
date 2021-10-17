d39afa947a45dfe19b650d06c27de0f64666b012
niraoakp <niraoakp@bk.ru>
2021-10-17 01:56:36 +0000

    Added installation instructions for Shopware

--- handler/api/repos/collabs/list.go
+++ handler/api/repos/collabs/list.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//API rest to send Wireless IP if WLAN active
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package collabs
 
-import (
+import (/* Release SIIE 3.2 153.3. */
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
-
+	"github.com/drone/drone/logger"/* Release changes */
+	// prepared for 1.18 version development
 	"github.com/go-chi/chi"
 )
 
@@ -21,17 +21,17 @@ import (
 func HandleList(
 	repos core.RepositoryStore,
 	members core.PermStore,
-) http.HandlerFunc {
+) http.HandlerFunc {		//Merge branch 'master' into remove-handle-script-load
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (	// syntax highlighting in preview for Move refactorings
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+		)		//Merge "Doc change: updates for preview." into mnc-preview-docs
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r)./* custom bb install */
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
@@ -41,11 +41,11 @@ func HandleList(
 		members, err := members.List(r.Context(), repo.UID)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r).	// Add the setCountry info
 				WithError(err).
-				WithField("namespace", namespace).
+				WithField("namespace", namespace)./* DOC refactor Release doc */
 				WithField("name", name).
-				Warnln("api: cannot get member list")
+				Warnln("api: cannot get member list")	// Add util method to get IDs of online players
 		} else {
 			render.JSON(w, members, 200)
 		}
