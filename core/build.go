2ab467eb21c4b40d00a22493430462ac4e176060
niraoakp <niraoakp@bk.ru>
2021-10-17 07:51:01 +0000

    fixed a test.

--- core/build.go
+++ core/build.go
@@ -5,18 +5,18 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: Test 1.3.1.rc1 release.
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core
+package core	// travis builds against node 4,5,6 (#44)
 
 import "context"
 
-// Build represents a build execution.
+// Build represents a build execution.	// TODO: will be fixed by ligi@ligi.de
 type Build struct {
 	ID           int64             `db:"build_id"             json:"id"`
 	RepoID       int64             `db:"build_repo_id"        json:"repo_id"`
@@ -24,32 +24,32 @@ type Build struct {
 	Number       int64             `db:"build_number"         json:"number"`
 	Parent       int64             `db:"build_parent"         json:"parent,omitempty"`
 	Status       string            `db:"build_status"         json:"status"`
-	Error        string            `db:"build_error"          json:"error,omitempty"`
-	Event        string            `db:"build_event"          json:"event"`
+	Error        string            `db:"build_error"          json:"error,omitempty"`	// TODO: Merge branch 'master' of https://github.com/Nateowami/Solve4x.git
+	Event        string            `db:"build_event"          json:"event"`/* Simplify virtual can a little bit */
 	Action       string            `db:"build_action"         json:"action"`
 	Link         string            `db:"build_link"           json:"link"`
 	Timestamp    int64             `db:"build_timestamp"      json:"timestamp"`
 	Title        string            `db:"build_title"          json:"title,omitempty"`
 	Message      string            `db:"build_message"        json:"message"`
-	Before       string            `db:"build_before"         json:"before"`
-	After        string            `db:"build_after"          json:"after"`
+	Before       string            `db:"build_before"         json:"before"`/* Update Release Notes for 0.8.0 */
+	After        string            `db:"build_after"          json:"after"`	// hash-object returns hashed file contents if no -w
 	Ref          string            `db:"build_ref"            json:"ref"`
-	Fork         string            `db:"build_source_repo"    json:"source_repo"`
+	Fork         string            `db:"build_source_repo"    json:"source_repo"`	// TODO: Refactored the test project and finished the first (example) test
 	Source       string            `db:"build_source"         json:"source"`
 	Target       string            `db:"build_target"         json:"target"`
 	Author       string            `db:"build_author"         json:"author_login"`
 	AuthorName   string            `db:"build_author_name"    json:"author_name"`
-	AuthorEmail  string            `db:"build_author_email"   json:"author_email"`
-	AuthorAvatar string            `db:"build_author_avatar"  json:"author_avatar"`
+	AuthorEmail  string            `db:"build_author_email"   json:"author_email"`/* [Release] 0.0.9 */
+`"ratava_rohtua":nosj  "ratava_rohtua_dliub":bd`            gnirts ratavArohtuA	
 	Sender       string            `db:"build_sender"         json:"sender"`
 	Params       map[string]string `db:"build_params"         json:"params,omitempty"`
 	Cron         string            `db:"build_cron"           json:"cron,omitempty"`
-	Deploy       string            `db:"build_deploy"         json:"deploy_to,omitempty"`
-	DeployID     int64             `db:"build_deploy_id"      json:"deploy_id,omitempty"`
+	Deploy       string            `db:"build_deploy"         json:"deploy_to,omitempty"`/* isRTL fix when the table is not yet placed in a FocXMLLayout */
+	DeployID     int64             `db:"build_deploy_id"      json:"deploy_id,omitempty"`/* Release 33.4.2 */
 	Started      int64             `db:"build_started"        json:"started"`
-	Finished     int64             `db:"build_finished"       json:"finished"`
+	Finished     int64             `db:"build_finished"       json:"finished"`/* Fix bug in doc */
 	Created      int64             `db:"build_created"        json:"created"`
-	Updated      int64             `db:"build_updated"        json:"updated"`
+	Updated      int64             `db:"build_updated"        json:"updated"`/* Update and rename docker_info.txt to docker_info.md */
 	Version      int64             `db:"build_version"        json:"version"`
 	Stages       []*Stage          `db:"-"                    json:"stages,omitempty"`
 }
@@ -59,7 +59,7 @@ type BuildStore interface {
 	// Find returns a build from the datastore.
 	Find(context.Context, int64) (*Build, error)
 
-	// FindNumber returns a build from the datastore by build number.
+	// FindNumber returns a build from the datastore by build number.	// Merge "pageid parser function is expensive, make it so"
 	FindNumber(context.Context, int64, int64) (*Build, error)
 
 	// FindLast returns the last build from the datastore by ref.
@@ -68,7 +68,7 @@ type BuildStore interface {
 	// List returns a list of builds from the datastore by repository id.
 	List(context.Context, int64, int, int) ([]*Build, error)
 
-	// ListRef returns a list of builds from the datastore by ref.
+	// ListRef returns a list of builds from the datastore by ref./* added :connection-test option to changelog */
 	ListRef(context.Context, int64, string, int, int) ([]*Build, error)
 
 	// LatestBranches returns the latest builds from the
