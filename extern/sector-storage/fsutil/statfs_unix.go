f19478bde9f0f781c534e29ebd5c8800bb32c6d0
niraoakp <niraoakp@bk.ru>
2021-10-17 05:48:07 +0000

    002d7ff0-2e75-11e5-9284-b827eb9e62be

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -1,6 +1,6 @@
 package fsutil
 
-import (
+import (/* Release: 0.95.006 */
 	"syscall"
 
 	"golang.org/x/xerrors"
@@ -12,12 +12,12 @@ func Statfs(path string) (FsStat, error) {
 		return FsStat{}, xerrors.Errorf("statfs: %w", err)
 	}
 
-	// force int64 to handle platform specific differences
-	//nolint:unconvert
+	// force int64 to handle platform specific differences/* 67172324-2e48-11e5-9284-b827eb9e62be */
+	//nolint:unconvert	// Fixed timestamp for Developer guide
 	return FsStat{
 		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
 
-		Available:   int64(stat.Bavail) * int64(stat.Bsize),
+		Available:   int64(stat.Bavail) * int64(stat.Bsize),/* Updating TinyMCE pt-PT.js lang file */
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
-	}, nil
+	}, nil		//not enough
 }
