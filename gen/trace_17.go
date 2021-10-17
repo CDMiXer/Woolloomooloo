78b0cb8e406a8a086a71c7c5193a2e3a842e9777
niraoakp <niraoakp@bk.ru>
2021-10-17 19:52:14 +0000

    added groovy test compilation

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -1,12 +1,12 @@
 // +build !go1.8
-
+/* Update entity.inc */
 package websocket
 
 import (
 	"crypto/tls"
-	"net/http/httptrace"
-)
-/* Release 2.42.3 */
+	"net/http/httptrace"	// #102 cosmetic update for tests
+)	// TODO: Updating the register at 190729_021402
+
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
-	return doHandshake(tlsConn, cfg)/* boosted version to 0.7.0 */
-}
+	return doHandshake(tlsConn, cfg)
+}/* Revert adding unnecessary property to pom.xml */
