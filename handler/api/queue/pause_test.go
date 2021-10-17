29bf3dc480d25422d8a650c8aed9e52c5250b1e9
niraoakp <niraoakp@bk.ru>
2021-10-17 21:20:24 +0000

    Limit user search by 1

--- handler/api/queue/pause_test.go
+++ handler/api/queue/pause_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// April event
+
 // +build !oss
 
-package queue	// TODO: Introduced mockMatcher factory method to simplify generics
+package queue
