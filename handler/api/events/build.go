4ea9657801f5fb549d4ee5dc8c055e1b4df61a24
niraoakp <niraoakp@bk.ru>
2021-10-17 19:10:24 +0000

    added new unit test, including alias check

--- handler/api/events/build.go
+++ handler/api/events/build.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by onhardev@bk.ru
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Memcache removed from code and a tiny iterator optimalisation
 // limitations under the License.
 
 package events
@@ -18,48 +18,48 @@ import (
 	"context"
 	"io"
 	"net/http"
-	"time"
+	"time"/* Fix SleepyTrousers/EnderIO#2603 crash in creative tab rendering */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Remove Warnings.
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 	"github.com/sirupsen/logrus"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* Rename OLDRenCard.py to Old/OLDRenCard.py */
 )
 
-// interval at which the client is pinged to prevent
-// reverse proxy and load balancers from closing the
-// connection.
+// interval at which the client is pinged to prevent	// Fix image banner
+// reverse proxy and load balancers from closing the	// TODO: so close :)
+// connection./* Merge "Release 3.0.10.005 Prima WLAN Driver" */
 var pingInterval = time.Second * 30
 
-// implements a 24-hour timeout for connections. This
+// implements a 24-hour timeout for connections. This	// TODO: bad path inside facerec filter
 // should not be necessary, but is put in place just
 // in case we encounter dangling connections.
 var timeout = time.Hour * 24
 
 // HandleEvents creates an http.HandlerFunc that streams builds events
-// to the http.Response in an event stream format.
+// to the http.Response in an event stream format./* Release of Verion 0.9.1 */
 func HandleEvents(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,		//added BNC req
 	events core.Pubsub,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
-			namespace = chi.URLParam(r, "owner")
+		var (	// TODO: will be fixed by boringland@protonmail.ch
+			namespace = chi.URLParam(r, "owner")	// support make shared quickfix on member type
 			name      = chi.URLParam(r, "name")
 		)
 		logger := logger.FromRequest(r).WithFields(
 			logrus.Fields{
-				"namespace": namespace,
-				"name":      name,
+				"namespace": namespace,	// TODO: hacked by cory@protocol.ai
+				"name":      name,	// [snomed] exporting id.gen and id.reservations packages
 			},
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.WithError(err).Debugln("events: cannot find repository")
-			return
+			return/* Create Releases.md */
 		}
 
 		h := w.Header()
