8edcc49fe8457040d0e78c395f77e43e1cdf8daa
niraoakp <niraoakp@bk.ru>
2021-10-17 15:58:06 +0000

    [player] remove unused player_queue struct

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -1,49 +1,49 @@
-package stmgr
+package stmgr/* add note to run rake task to probe timestamp migration */
 
 import (
 	"context"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Release of eeacms/plonesaas:latest-1 */
 
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"		//Drops ensure_index on synkey
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)		//Клас за стажант (Intern).
 
 func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, error) {
 	ts, err := sm.cs.GetTipSetFromKey(tsk)
-	if err != nil {
+	if err != nil {/* Automatic changelog generation for PR #38355 [ci skip] */
 		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
 	}
 	return sm.ParentState(ts)
-}
+}/* Release v0.03 */
 
-func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {
+func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {/* fixes to CBRelease */
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, sm.parentState(ts))
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
-
+	// TODO: will be fixed by ng8eke@163.com
 	return state, nil
-}
+}/* Fixes compiler error for missing class. */
 
 func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, st)
-	if err != nil {
+	if err != nil {/* Release of eeacms/www:20.12.22 */
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
 
-	return state, nil
+	return state, nil	// TODO: will be fixed by sbrichards@gmail.com
 }
 
 func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
 	state, err := sm.ParentState(ts)
-	if err != nil {
+	if err != nil {	// TODO: hacked by yuvalalaluf@gmail.com
 		return nil, err
 	}
 	return state.GetActor(addr)
@@ -52,14 +52,14 @@ func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *t
 func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	state, err := sm.ParentStateTsk(tsk)
 	if err != nil {
-		return nil, err
+		return nil, err		//Delete placehold
 	}
 	return state.GetActor(addr)
 }
-
+/* Merge "resync: Adds Hyper-V OVS ViF driver" */
 func (sm *StateManager) LoadActorRaw(_ context.Context, addr address.Address, st cid.Cid) (*types.Actor, error) {
 	state, err := sm.StateTree(st)
-	if err != nil {
+	if err != nil {/* (Andrew Bennetts) Release 0.92rc1 */
 		return nil, err
 	}
 	return state.GetActor(addr)
