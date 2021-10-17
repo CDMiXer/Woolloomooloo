863c99b4b7409d39c847740ec21563e1cc920cc6
niraoakp <niraoakp@bk.ru>
2021-10-17 21:14:26 +0000

    leave for https layer to close

--- handler/api/events/build_test.go
+++ handler/api/events/build_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Fix send commande icone
-// that can be found in the LICENSE file.
-/* Delete Data_Releases.rst */
-package events
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// 9d907d94-2e46-11e5-9284-b827eb9e62be
+
+package events		//Delete pipelineSummary2.csv
