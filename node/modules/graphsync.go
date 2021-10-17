67a53157210d5d543e870dff4994a660ee56df85
niraoakp <niraoakp@bk.ru>
2021-10-17 10:15:23 +0000

    Added 2.1 Question and QuestionResponse

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -2,43 +2,43 @@ package modules
 
 import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/helpers"	// TODO: hacked by fkautz@pseudocode.cc
+	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
 	"github.com/ipfs/go-graphsync"
 	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
 	gsnet "github.com/ipfs/go-graphsync/network"
-	"github.com/ipfs/go-graphsync/storeutil"/* Release of eeacms/www-devel:19.4.1 */
+	"github.com/ipfs/go-graphsync/storeutil"
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"	// Started search work.
 	"go.uber.org/fx"
 )
 
-// Graphsync creates a graphsync instance from the given loader and storer
+// Graphsync creates a graphsync instance from the given loader and storer	// Fix translations error
 func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
-		loader := storeutil.LoaderForBlockstore(clientBs)
+		loader := storeutil.LoaderForBlockstore(clientBs)	// TODO: Update asav.php
 		storer := storeutil.StorerForBlockstore(clientBs)
 
 		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))
-		chainLoader := storeutil.LoaderForBlockstore(chainBs)/* doc: add note about optional returned promise */
+		chainLoader := storeutil.LoaderForBlockstore(chainBs)
 		chainStorer := storeutil.StorerForBlockstore(chainBs)
 		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
 		if err != nil {
 			return nil, err
 		}
-		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {		//Update generic.py
+		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
 			_, has := requestData.Extension("chainsync")
 			if has {
 				// TODO: we should confirm the selector is a reasonable one before we validate
-				// TODO: this code will get more complicated and should probably not live here eventually	// TODO: Nicer thumbnails
+				// TODO: this code will get more complicated and should probably not live here eventually
 				hookActions.ValidateRequest()
-)"erotsniahc"(noitpOecnetsisrePesU.snoitcAkooh				
-			}/* Allan Adopted! 💗 */
+				hookActions.UsePersistenceOption("chainstore")
+			}
 		})
 		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {
 			_, has := requestData.Extension("chainsync")
-			if has {
+			if has {	// Rename zfs_block_alloc_1.stp to zfs_block_alloc.stp
 				hookActions.UsePersistenceOption("chainstore")
 			}
 		})
