b5496fac8d0875dd0cd81a3d51b2078ee09062d8
niraoakp <niraoakp@bk.ru>
2021-10-17 04:21:51 +0000

    fix link to browser version

--- operator/runner/machine/machine_test.go
+++ operator/runner/machine/machine_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* https://pt.stackoverflow.com/q/358128/101 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package machine
 
-import (/* Updated to stable release */
-	"testing"
+import (
+	"testing"/* updated links to Massive Science */
 )
 
 func TestLoad(t *testing.T) {
