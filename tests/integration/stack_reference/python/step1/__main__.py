1e775e2bc52b913db9e11ae2894a8e81442cf093
niraoakp <niraoakp@bk.ru>
2021-10-17 05:01:59 +0000

    Fixed the dropdown.

--- tests/integration/stack_reference/python/step1/__main__.py
+++ tests/integration/stack_reference/python/step1/__main__.py
@@ -1,4 +1,4 @@
-# Copyright 2020, Pulumi Corporation.  All rights reserved.
+# Copyright 2020, Pulumi Corporation.  All rights reserved.		//prompt for the WF credentials, if the stored credentials don't work.
 
 import pulumi
 
@@ -9,7 +9,7 @@ a = pulumi.StackReference(slug)
 
 oldVal = a.get_output('val')
 
-if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':
-    raise Exception('Invalid result')		//Delete vehicles.sp
-
+if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':/* IHTSDO Release 4.5.58 */
+    raise Exception('Invalid result')		//63328f00-2e4d-11e5-9284-b827eb9e62be
+	// TODO: will be fixed by ng8eke@163.com
 pulumi.export('val2', pulumi.Output.secret(['a', 'b']))
