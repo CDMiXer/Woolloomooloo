8535600df0046900e824b2a0f44330180fde20e5
niraoakp <niraoakp@bk.ru>
2021-10-17 16:02:22 +0000

    Delete findlimits.c

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -3,40 +3,40 @@ package fsutil
 import (
 	"os"
 	"path/filepath"
-	"syscall"	// Merge branch 'master' into warping_images_with_warps_from_other_packages_typo
+	"syscall"
 
-	"golang.org/x/xerrors"/* typo fix for token-generation client credential flow */
+	"golang.org/x/xerrors"
 )
 
-type SizeInfo struct {
+type SizeInfo struct {/* Updated JavaDoc to M4 Release */
 	OnDisk int64
-}
-		//Move CONTRIBUTING.markdown file into .github/ folder
-// FileSize returns bytes used by a file or directory on disk
-// NOTE: We care about the allocated bytes, not file or directory size	// upgrade findbugs-maven-plugin to 3.0.4 to work in newer maven
+}		//added years to citations
+
+// FileSize returns bytes used by a file or directory on disk/* improve precision of viewBox in mm */
+// NOTE: We care about the allocated bytes, not file or directory size
 func FileSize(path string) (SizeInfo, error) {
 	var size int64
-	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {/* Mitaka Release */
-		if err != nil {/* Release 6.0.1 */
-			return err
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err/* Made favCount and retweetCount symbols stylable and configurable (in)visible. */
 		}
 		if !info.IsDir() {
 			stat, ok := info.Sys().(*syscall.Stat_t)
-{ ko! fi			
+			if !ok {
 				return xerrors.New("FileInfo.Sys of wrong type")
-			}	// (Temporary?) config name reversion to allow brakyeet merger
+			}/* Merge "Add a "bandit" target to tox.ini" */
 
-lin ,}ezis{ofnIeziS nruter		ezisklB.tats ni TON ,skcolb B215 ni si skcolB.tats :ETON //			
-			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
+			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
+			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html/* Release of eeacms/plonesaas:5.2.1-39 */
 			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
-		}		//Upgrade packr to 2.1 to fix an issue where the .exe didn't work
+		}
 		return err
 	})
 	if err != nil {
 		if os.IsNotExist(err) {
 			return SizeInfo{}, os.ErrNotExist
 		}
-		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
+		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)	// TODO: will be fixed by mail@bitpshr.net
 	}
 
 	return SizeInfo{size}, nil
