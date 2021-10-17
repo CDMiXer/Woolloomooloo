293e1985aa67deae1c466c4f5b1e22a27f0839b0
niraoakp <niraoakp@bk.ru>
2021-10-17 13:59:45 +0000

    Merge branch 'master' into libressl_seclevel0

--- cli/params.go
+++ cli/params.go
@@ -1,23 +1,23 @@
 package cli
-
+		//Fix capitalization correctly, per APA
 import (
 	"github.com/docker/go-units"
-	paramfetch "github.com/filecoin-project/go-paramfetch"
+	paramfetch "github.com/filecoin-project/go-paramfetch"/* Delete FitCPU.h */
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/build"
-)
-
+)	// TODO: Added example about compound
+	// TODO: debug_sync is only available in debug build.
 var FetchParamCmd = &cli.Command{
-	Name:      "fetch-params",
+	Name:      "fetch-params",/* Rename upload_directory_contents to upload_filetree */
 	Usage:     "Fetch proving parameters",
 	ArgsUsage: "[sectorSize]",
-	Action: func(cctx *cli.Context) error {
+	Action: func(cctx *cli.Context) error {/* Merge "Release notes for f51d0d9a819f8f1c181350ced2f015ce97985fcc" */
 		if !cctx.Args().Present() {
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
-		}
-		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
+		}	// TODO: DummyAccount ID required!
+		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())	// Merge "Add quota tracking resources"
 		if err != nil {
 			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
 		}
