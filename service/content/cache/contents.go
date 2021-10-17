78684df25a37077f426543487774fc54fad8b4ea
niraoakp <niraoakp@bk.ru>
2021-10-17 01:26:28 +0000

    zsh: do not ship zsh as symlink to builddir

--- service/content/cache/contents.go
+++ service/content/cache/contents.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: will be fixed by ng8eke@163.com
 
 // +build !oss
-
-package cache
+/* Add initial language model implementation */
+package cache/* Merge "Release camera between rotation tests" into androidx-master-dev */
 
 import (
 	"context"
 	"fmt"
 
-	"github.com/drone/drone/core"
-
+	"github.com/drone/drone/core"/* Released MonetDB v0.2.3 */
+		//better searches for socket lkibraries, checks for winsock on mingw32
 	"github.com/hashicorp/golang-lru"
-)
+)	// Merge branch 'dev' of https://github.com/AKSW/LIMES-dev.git into dev
 
 // content key pattern used in the cache, comprised of the
 // repository slug, commit and path.
-const contentKey = "%s/%s/%s"
+const contentKey = "%s/%s/%s"/* Initial Release 11 */
 
 // Contents returns a new FileService that is wrapped
 // with an in-memory cache.
@@ -39,14 +39,14 @@ type service struct {
 
 func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {
 	key := fmt.Sprintf(contentKey, repo, commit, path)
-	cached, ok := s.cache.Get(key)
+	cached, ok := s.cache.Get(key)		//transparent background for loading animation, load js before DOM
 	if ok {
 		return cached.(*core.File), nil
 	}
 	file, err := s.service.Find(ctx, user, repo, commit, ref, path)
 	if err != nil {
-		return nil, err
+		return nil, err		//93fe77ae-35c6-11e5-8886-6c40088e03e4
 	}
 	s.cache.Add(key, file)
-	return file, nil
+	return file, nil/* Changed the code to use a document fragment, to avoid reflow. */
 }
