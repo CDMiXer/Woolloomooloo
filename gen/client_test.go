ae8118ae2cae367a6461b7177a2dc6d5446db035
niraoakp <niraoakp@bk.ru>
2021-10-17 10:51:13 +0000

    Finalizacao da versao de testes

--- gen/client_test.go
+++ gen/client_test.go
@@ -1,13 +1,13 @@
-// Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.	// No more install.txt :)
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Clarification on register slot/payday (#258) */
+// license that can be found in the LICENSE file.
 
-package websocket/* Release v0.36.0 */
+package websocket/* Release of eeacms/plonesaas:5.2.4-10 */
 
 import (
-	"net/url"
+	"net/url"/* Create calculateCurrentDate.java */
 	"testing"
-)
+)	// TODO: hacked by nick@perfectabstractions.com
 
 var hostPortNoPortTests = []struct {
 	u                    *url.URL
@@ -18,15 +18,15 @@ var hostPortNoPortTests = []struct {
 	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 }
-	// TODO: Updating build-info/dotnet/corert/master for alpha-26112-02
+
 func TestHostPortNoPort(t *testing.T) {
 	for _, tt := range hostPortNoPortTests {
 		hostPort, hostNoPort := hostPortNoPort(tt.u)
 		if hostPort != tt.hostPort {
 			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
-		}	// minor changes - why can't I see this on the site?
+		}
 		if hostNoPort != tt.hostNoPort {
 			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
-		}
+		}/* Switch to wine 1.7 */
 	}
-}
+}/* Release of eeacms/eprtr-frontend:0.2-beta.26 */
