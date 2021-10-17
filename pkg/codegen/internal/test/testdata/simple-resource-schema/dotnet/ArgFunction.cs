57ed49c1c3b0a3b19fb19c2a2b0953c22ac2ea97
niraoakp <niraoakp@bk.ru>
2021-10-17 23:13:45 +0000

    now also check for 'no newline at end of files'

--- pkg/codegen/internal/test/testdata/simple-resource-schema/dotnet/ArgFunction.cs
+++ pkg/codegen/internal/test/testdata/simple-resource-schema/dotnet/ArgFunction.cs
@@ -6,24 +6,24 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Threading.Tasks;
 using Pulumi.Serialization;
-/* Rename User Guide to User Guide.md */
+
 namespace Pulumi.Example
-{/* Release 0.1.0-alpha */
-    public static class ArgFunction	// TODO: hacked by steven@stebalien.com
+{
+    public static class ArgFunction
     {
         public static Task<ArgFunctionResult> InvokeAsync(ArgFunctionArgs? args = null, InvokeOptions? options = null)
             => Pulumi.Deployment.Instance.InvokeAsync<ArgFunctionResult>("example::argFunction", args ?? new ArgFunctionArgs(), options.WithVersion());
-    }		//adjustments to dist build and documentation.
+    }
 
 
     public sealed class ArgFunctionArgs : Pulumi.InvokeArgs
     {
         [Input("arg1")]
         public Pulumi.Example.Resource? Arg1 { get; set; }
-/* Rebuilt index with Kristandre */
+
         public ArgFunctionArgs()
         {
-        }	// TODO: hacked by antao2002@gmail.com
+        }
     }
 
 
@@ -31,7 +31,7 @@ namespace Pulumi.Example
     public sealed class ArgFunctionResult
     {
         public readonly Pulumi.Example.Resource? Result;
-/* Release 0.3.1.1 */
+
         [OutputConstructor]
         private ArgFunctionResult(Pulumi.Example.Resource? result)
         {
