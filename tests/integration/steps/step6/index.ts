59acb0a80ac3c765cf8ec0bc8050fdf4220405d6
niraoakp <niraoakp@bk.ru>
2021-10-17 13:32:36 +0000

    Dd-91 add translations

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -2,7 +2,7 @@
 
 // Step 6: Delete everything:
 // * Elide a (Delete(a5)).
-// * Elide c (Delete(c)).
+// * Elide c (Delete(c)).	// Corrected example :require for carmine
 // * Elide e (Delete(e)).
 // * Pending delete (Delete(a4)).
-// Checkpoint: empty
+// Checkpoint: empty	// Added micro version and check on it
