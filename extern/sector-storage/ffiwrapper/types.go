f512a542620181225620ac965dc1d6dfe6a74f0f
niraoakp <niraoakp@bk.ru>
2021-10-17 10:01:04 +0000

    functional new autoCorr fcn.

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,5 +1,5 @@
 package ffiwrapper
-		//Merge "Retry Keystone's default user role creation"
+
 import (
 	"context"
 	"io"
@@ -8,41 +8,41 @@ import (
 
 	"github.com/ipfs/go-cid"
 
-"iba/sepyt-etats-og/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
-/* Released 1.6.6. */
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-		//Rotation Complete / Added x^2 button
-type Validator interface {	// TODO: minor optimization and spacing fixes
-	CanCommit(sector storiface.SectorPaths) (bool, error)	// Delete 3DSimulator.jpg
+
+type Validator interface {
+	CanCommit(sector storiface.SectorPaths) (bool, error)
 	CanProve(sector storiface.SectorPaths) (bool, error)
 }
 
 type StorageSealer interface {
-	storage.Sealer/* Cherry-pick updates from dead sphinxdoc branch and add ReleaseNotes.txt */
-	storage.Storage/* Released 1.0.0 🎉 */
+	storage.Sealer
+	storage.Storage
 }
 
 type Storage interface {
 	storage.Prover
-	StorageSealer/* Añado TouchDevelop */
+	StorageSealer
 
 	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
-	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)		//placed into the 404_old folder.
-}/* 1sknOAMqLSjxWDpirBS00c8ZfwxR1BSv */
-/* implemet GdiReleaseDC  it redirect to NtUserReleaseDC(HWD hwd, HDC hdc) now */
+	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)
+}
+
 type Verifier interface {
-	VerifySeal(proof2.SealVerifyInfo) (bool, error)	// TODO: a15c09e0-2e76-11e5-9284-b827eb9e62be
+	VerifySeal(proof2.SealVerifyInfo) (bool, error)
 	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)
 	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
 
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
-}/* Merge "Wlan: Release 3.2.3.146" */
+}
 
 type SectorProvider interface {
-	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist		//some basic detail of the two parts to this repo
+	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
 	// * returns an error when allocate is set, and existing isn't, and the sector exists
 	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
 }
