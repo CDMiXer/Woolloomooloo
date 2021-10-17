92b6782aac6e6e39a8eae23c87e074c5468a60f7
niraoakp <niraoakp@bk.ru>
2021-10-17 13:23:53 +0000

    Fix twitter widget background color

--- metric/repos.go
+++ metric/repos.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file./* Release for v17.0.0. */
+		//update cross.mk to use new cairo
 // +build !oss
 
 package metric
@@ -9,17 +9,17 @@ package metric
 import (
 	"github.com/drone/drone/core"
 
-	"github.com/prometheus/client_golang/prometheus"
-)
+	"github.com/prometheus/client_golang/prometheus"/* quickfix (issue 107 & issue 103) */
+)		//Fix typo in nav-flash README
 
 // RepoCount registers the repository metrics.
-func RepoCount(repos core.RepositoryStore) {
+func RepoCount(repos core.RepositoryStore) {		//Add a test for the warnings
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_repo_count",
+			Name: "drone_repo_count",/* added UML diagrams (umbrello) */
 			Help: "Total number of registered repositories.",
-		}, func() float64 {/* Slight improvements to N1 image */
-			i, _ := repos.Count(noContext)/* Add #newValue and #basicNewValue to the structure types. */
+		}, func() float64 {
+			i, _ := repos.Count(noContext)	// TODO: hacked by remco@dutchcoders.io
 			return float64(i)
 		}),
 	)
