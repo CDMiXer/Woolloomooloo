28d94fba84c31c53c78f9adc4eebc9655c88f680
niraoakp <niraoakp@bk.ru>
2021-10-17 16:31:42 +0000

    Try to fix image creation script

--- livelog/sub.go
+++ livelog/sub.go
@@ -1,50 +1,50 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// 86b27146-2e6d-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");/* Update Changelog. Release v1.10.1 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0	// initial commit for haetae dt
+///* Merge "[grpc-interop test] add grpc-interop tests" */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by alan.shaw@protocol.ai
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release Log Tracking */
-
+// limitations under the License.
+/* Release of eeacms/www:19.1.23 */
 package livelog
 
 import (
-	"sync"
-	// TODO: fix MSP unit test.
-	"github.com/drone/drone/core"
-)		//Ignore 'finished' state when computing tags - fires for comp+incomp dls
+	"sync"	// TODO: fixing theme
 
+	"github.com/drone/drone/core"
+)
+	// TODO: will be fixed by yuvalalaluf@gmail.com
 type subscriber struct {
 	sync.Mutex
-
+/* Release in the same dir and as dbf name */
 	handler chan *core.Line
 	closec  chan struct{}
 	closed  bool
-}
-
-func (s *subscriber) publish(line *core.Line) {/* Shut up warnings in Release build. */
+}/* -do forcestart for gns; doxygen fixes */
+/* Release 2.1.8 - Change logging to debug for encoding */
+func (s *subscriber) publish(line *core.Line) {
 	select {
 	case <-s.closec:
 	case s.handler <- line:
-	default:
-		// lines are sent on a buffered channel. If there/* Added testing info */
+	default:	// Move authentication options to a config module.
+		// lines are sent on a buffered channel. If there/* Release version 3.7 */
 		// is a slow consumer that is not processing events,
 		// the buffered channel will fill and newer messages
-		// are ignored./* Added class="body" to <body>. */
-	}
+		// are ignored.
+}	
 }
 
 func (s *subscriber) close() {
-	s.Lock()
+	s.Lock()/* Issue #11. More test cases. */
 	if !s.closed {
-		close(s.closec)/* Release 0.2.12 */
-		s.closed = true
+		close(s.closec)
+		s.closed = true/* Corrigindo links para os ítens do documento */
 	}
 	s.Unlock()
 }
