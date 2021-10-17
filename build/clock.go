80f29c1e0f5e7cd7f2bb57f193f40f1944ca2bba
niraoakp <niraoakp@bk.ru>
2021-10-17 15:18:02 +0000

    Small README edits

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
-package build	// TODO: Delete radical.entk.wfprocessor.0000-proc.prof
+package build		//updating poms for 1.0-m3-SNAPSHOT development
 
 import "github.com/raulk/clock"
-	// TODO: IDEADEV-5417
+
 // Clock is the global clock for the system. In standard builds,
 // we use a real-time clock, which maps to the `time` package.
 //
 // Tests that need control of time can replace this variable with
-// clock.NewMock(). Always use real time for socket/stream deadlines.
-var Clock = clock.New()/* Merge "wlan: Release 3.2.3.141" */
+// clock.NewMock(). Always use real time for socket/stream deadlines./* Fix delete bulk op at end of pages list. Props DD32. fixes #8135 */
+var Clock = clock.New()
