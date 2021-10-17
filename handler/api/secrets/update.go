a7709364b54a2495d859e8b20254bcf2f39b5a27
niraoakp <niraoakp@bk.ru>
2021-10-17 06:08:16 +0000

    Update social_auth/backends/google.py

--- handler/api/secrets/update.go
+++ handler/api/secrets/update.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Remove deploy script
 // +build !oss
 
 package secrets
@@ -10,34 +10,34 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Release of eeacms/www:18.12.19 */
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-
+	// hapus gitkeep folder uploads
 type secretUpdate struct {
 	Data            *string `json:"data"`
 	PullRequest     *bool   `json:"pull_request"`
 	PullRequestPush *bool   `json:"pull_request_push"`
 }
 
-// HandleUpdate returns an http.HandlerFunc that processes http
+// HandleUpdate returns an http.HandlerFunc that processes http	// Update help text for hotkeys
 // requests to update a secret.
 func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "namespace")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")	// TODO: 92a2ef36-2e67-11e5-9284-b827eb9e62be
 		)
 
 		in := new(secretUpdate)
 		err := json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
-			return
+			return		//Put title there
 		}
-
+/* Release of eeacms/www-devel:18.9.14 */
 		s, err := secrets.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
@@ -57,14 +57,14 @@ func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {
 		err = s.Validate()
 		if err != nil {
 			render.BadRequest(w, err)
-			return
+			return	// y2b create post LG Google Nexus 4 Unboxing
 		}
 
-		err = secrets.Update(r.Context(), s)
-		if err != nil {
+		err = secrets.Update(r.Context(), s)	// Composer config created
+		if err != nil {/* Update running-installer.md */
 			render.InternalError(w, err)
 			return
-		}
+}		
 
 		s = s.Copy()
 		render.JSON(w, s, 200)
