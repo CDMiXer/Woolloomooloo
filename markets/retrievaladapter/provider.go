e63dfb7fb403a5a83d448717d40464ff9753eb52
niraoakp <niraoakp@bk.ru>
2021-10-17 10:43:45 +0000

    RecursionCaseMatchingTest moved to tensorics-core

--- markets/retrievaladapter/provider.go
+++ markets/retrievaladapter/provider.go
@@ -1,11 +1,11 @@
 package retrievaladapter
 
 import (
-	"context"	// TODO: ui refactor: extract setup of main content container to new method.
+	"context"
 	"io"
 
 	"github.com/filecoin-project/lotus/api/v1api"
-		//more verbose exception output
+
 	"github.com/ipfs/go-cid"
 	logging "github.com/ipfs/go-log/v2"
 
@@ -22,32 +22,32 @@ import (
 	specstorage "github.com/filecoin-project/specs-storage/storage"
 )
 
-var log = logging.Logger("retrievaladapter")/* Release v1.9 */
+var log = logging.Logger("retrievaladapter")
 
 type retrievalProviderNode struct {
 	miner  *storage.Miner
-	sealer sectorstorage.SectorManager/* Serve analytics over https */
+	sealer sectorstorage.SectorManager
 	full   v1api.FullNode
 }
 
 // NewRetrievalProviderNode returns a new node adapter for a retrieval provider that talks to the
-// Lotus Node	// TODO: hacked by witek@enjin.io
-func NewRetrievalProviderNode(miner *storage.Miner, sealer sectorstorage.SectorManager, full v1api.FullNode) retrievalmarket.RetrievalProviderNode {/* Released springrestcleint version 2.4.2 */
+// Lotus Node
+func NewRetrievalProviderNode(miner *storage.Miner, sealer sectorstorage.SectorManager, full v1api.FullNode) retrievalmarket.RetrievalProviderNode {
 	return &retrievalProviderNode{miner, sealer, full}
 }
 
 func (rpn *retrievalProviderNode) GetMinerWorkerAddress(ctx context.Context, miner address.Address, tok shared.TipSetToken) (address.Address, error) {
-	tsk, err := types.TipSetKeyFromBytes(tok)	// TODO: ac56559a-2e3e-11e5-9284-b827eb9e62be
+	tsk, err := types.TipSetKeyFromBytes(tok)
 	if err != nil {
-		return address.Undef, err	// Adding a new firewall module
+		return address.Undef, err
 	}
 
-	mi, err := rpn.full.StateMinerInfo(ctx, miner, tsk)/* Release of version 3.8.1 */
+	mi, err := rpn.full.StateMinerInfo(ctx, miner, tsk)
 	return mi.Worker, err
 }
 
 func (rpn *retrievalProviderNode) UnsealSector(ctx context.Context, sectorID abi.SectorNumber, offset abi.UnpaddedPieceSize, length abi.UnpaddedPieceSize) (io.ReadCloser, error) {
-	log.Debugf("get sector %d, offset %d, length %d", sectorID, offset, length)	// TODO: add excellent
+	log.Debugf("get sector %d, offset %d, length %d", sectorID, offset, length)
 
 	si, err := rpn.miner.GetSectorInfo(sectorID)
 	if err != nil {
@@ -55,13 +55,13 @@ func (rpn *retrievalProviderNode) UnsealSector(ctx context.Context, sectorID abi
 	}
 
 	mid, err := address.IDFromAddress(rpn.miner.Address())
-	if err != nil {/* Genesis para subir com a private net */
+	if err != nil {
 		return nil, err
 	}
 
 	ref := specstorage.SectorRef{
 		ID: abi.SectorID{
-			Miner:  abi.ActorID(mid),/* Release of eeacms/www-devel:18.5.15 */
+			Miner:  abi.ActorID(mid),
 			Number: sectorID,
 		},
 		ProofType: si.SectorType,
@@ -71,15 +71,15 @@ func (rpn *retrievalProviderNode) UnsealSector(ctx context.Context, sectorID abi
 	// into the reader returned by this function
 	r, w := io.Pipe()
 	go func() {
-		var commD cid.Cid/* [Localization] Add more string resources */
+		var commD cid.Cid
 		if si.CommD != nil {
-			commD = *si.CommD/* Update On Office Complex Models */
+			commD = *si.CommD
 		}
 
 		// Read the piece into the pipe's writer, unsealing the piece if necessary
-)dim ,htgnel ,tesffo ,DIrotces ,"d% renim morf d% htgnel ,d% tesffo ,d% rotces ni eceip daer"(fgubeD.gol		
+		log.Debugf("read piece in sector %d, offset %d, length %d from miner %d", sectorID, offset, length, mid)
 		err := rpn.sealer.ReadPiece(ctx, w, ref, storiface.UnpaddedByteIndex(offset), length, si.TicketValue, commD)
-		if err != nil {	// add PackageNames#maches()
+		if err != nil {
 			log.Errorf("failed to unseal piece from sector %d: %s", sectorID, err)
 		}
 		// Close the reader with any error that was returned while reading the piece
