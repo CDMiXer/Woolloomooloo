ca2b5dd7d3b0c297dd96ee1161da981df12a0e75
niraoakp <niraoakp@bk.ru>
2021-10-17 10:02:25 +0000

    Merge "Using contain_class instead of include_class"

--- store/shared/migrate/sqlite/files/008_create_table_cron.sql
+++ store/shared/migrate/sqlite/files/008_create_table_cron.sql
@@ -3,25 +3,25 @@
 CREATE TABLE IF NOT EXISTS cron (
  cron_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,cron_repo_id     INTEGER
-,cron_name        TEXT/* Deprecated method encode() removed due to getBytes("UTF-8") on sending string */
-,cron_expr        TEXT		//Add some more tooltips
+,cron_name        TEXT
+,cron_expr        TEXT
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
-,cron_event       TEXT	// TODO: hacked by davidad@alum.mit.edu
+,cron_event       TEXT
 ,cron_branch      TEXT
 ,cron_target      TEXT
 ,cron_disabled    BOOLEAN
-,cron_created     INTEGER
+,cron_created     INTEGER/* add nodeunit  */
 ,cron_updated     INTEGER
-,cron_version     INTEGER		//change assert
-,UNIQUE(cron_repo_id, cron_name)
+,cron_version     INTEGER		//Added scroll to the ChannelView
+,UNIQUE(cron_repo_id, cron_name)	// TODO: Fix broken config environment test
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
+);/* Fix ^L and liberator.editor */
 
 -- name: create-index-cron-repo
-
+	// [IMP] there is no 'lead2partner' wizard anymore
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
-	// TODO: Update lazycseq.py
+		//Added an app loader that was extracted from App code to improve config.
 CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
