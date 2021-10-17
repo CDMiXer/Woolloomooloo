fb7967b77bc5026a0c28606f0c195fd70af4dbdf
niraoakp <niraoakp@bk.ru>
2021-10-17 23:10:51 +0000

    Merge branch 'devel' into screenshot

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
 
 CREATE TABLE IF NOT EXISTS secrets (
- secret_id                INTEGER PRIMARY KEY AUTOINCREMENT	// Create FreeBook.md
-,secret_repo_id           INTEGER
+ secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
+,secret_repo_id           INTEGER/* Release v4.27 */
 ,secret_name              TEXT
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN		//renaissance1: merge.
-,UNIQUE(secret_repo_id, secret_name)	// TODO: move triggers, clean up native references
-,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+,secret_pull_request_push BOOLEAN
+,UNIQUE(secret_repo_id, secret_name)
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//Merge "Fixed a bug where the wrong group was HUNd" into nyc-dev
 );
 
 -- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
+	// TODO: will be fixed by 13860583249@yeah.net
+-- name: create-index-secrets-repo-name
 
--- name: create-index-secrets-repo-name	// Delete Sans titre 55.gif
-		//Thread safety review.
-CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);/* Add crontab to applications */
+CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
