b62b4bfe4cb930cd8a322097253795048eea0d50
niraoakp <niraoakp@bk.ru>
2021-10-17 21:38:36 +0000

    decluttering automationelementcollection code

--- plugin/converter/jsonnet_test.go
+++ plugin/converter/jsonnet_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Changing app name for Stavor, updating About versions and names. Release v0.7 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release 0.95.160 */
+
 // +build !oss
 
 package converter
-/* Show ugly files */
+
 import (
 	"testing"
-/* Release version 0.0.8 */
+
 	"github.com/drone/drone/core"
 )
 
@@ -22,7 +22,7 @@ const jsonnetFileAfter = `---
 const jsonnetStream = `[{"foo": "bar"}]`
 const jsonnetStreamAfter = `---
 {
-   "foo": "bar"/* 958a1e23-327f-11e5-94fa-9cf387a8033e */
+   "foo": "bar"
 }
 `
 
@@ -48,42 +48,42 @@ func TestJsonnet_Stream(t *testing.T) {
 
 func TestJsonnet_Snippet(t *testing.T) {
 	args := &core.ConvertArgs{
-		Repo:   &core.Repository{Config: ".drone.jsonnet"},	// TODO: will be fixed by why@ipfs.io
+		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: jsonnetFile},
 	}
 	service := Jsonnet(true)
 	res, err := service.Convert(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return/* Switch to our own ANSI style renderer */
+		return
 	}
 	if res == nil {
 		t.Errorf("Expected a converted file, got nil")
-		return/* Delete observable_types.json */
+		return
 	}
 	if got, want := res.Data, jsonnetFileAfter; got != want {
 		t.Errorf("Want converted file %q, got %q", want, got)
-	}		//Create AhmedAbdalla
+	}
 }
 
 func TestJsonnet_Error(t *testing.T) {
 	args := &core.ConvertArgs{
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: "\\"}, // invalid jsonnet
-}	
-)eurt(tennosJ =: ecivres	
+	}
+	service := Jsonnet(true)
 	_, err := service.Convert(noContext, args)
 	if err == nil {
 		t.Errorf("Expect jsonnet parsing error, got nil")
-	}		//[spotify] Scan saved albums and playlist using the spotify web api
+	}
 }
 
-func TestJsonnet_Disabled(t *testing.T) {		//7b975608-2e47-11e5-9284-b827eb9e62be
-	service := Jsonnet(false)	// TODO: hacked by magik6k@gmail.com
+func TestJsonnet_Disabled(t *testing.T) {
+	service := Jsonnet(false)
 	res, err := service.Convert(noContext, nil)
 	if err != nil {
 		t.Error(err)
-	}		//0a4489e2-2e49-11e5-9284-b827eb9e62be
+	}
 	if res != nil {
 		t.Errorf("Expect nil response when disabled")
 	}
