400fbd2e296cfd58632c748b46b2850020a3433e
niraoakp <niraoakp@bk.ru>
2021-10-17 21:17:25 +0000

    New page configuration property: serialize or unserialize data

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,17 +1,17 @@
 package types
 
-import (	// TODO: Add LocaleContext management in order to specify the language
-	"errors"
-
-	"github.com/ipfs/go-cid"		//Added more debugging information to xia2 main
-)		//Fix not being able to unmarshal signers vatin.
+import (
+	"errors"/* Release 1.0.4 */
+/* Added empty {} insertion to Canvas, resp. pref */
+	"github.com/ipfs/go-cid"
+)/* font awesome 4.5.0 */
 
 var ErrActorNotFound = errors.New("actor not found")
 
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.	// Delete IA.exe
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.	// TODO: 8f4ca87e-2e51-11e5-9284-b827eb9e62be
 	Code    cid.Cid
 	Head    cid.Cid
 	Nonce   uint64
-	Balance BigInt
+	Balance BigInt		//NetKAN generated mods - ModuleManager-4.1.4
 }
