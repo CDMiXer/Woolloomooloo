3cfb1b64e96f4bd6b47f746467f84bf71573a894
niraoakp <niraoakp@bk.ru>
2021-10-17 17:44:14 +0000

    adding bsd license

--- service/user/user_test.go
+++ service/user/user_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Add RNG stat. */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -11,7 +11,7 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock/mockscm"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/go-scm/scm"		//Creating README.md with travis and codecov
 	"github.com/google/go-cmp/cmp"
 
 	"github.com/golang/mock/gomock"
@@ -20,19 +20,19 @@ import (
 var noContext = context.Background()
 
 func TestFind(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* [GUI] Authentication Token Creation/Deletion (Release v0.1) */
 	defer controller.Finish()
 
 	checkToken := func(ctx context.Context) {
-		got, ok := ctx.Value(scm.TokenKey{}).(*scm.Token)
-		if !ok {
+		got, ok := ctx.Value(scm.TokenKey{}).(*scm.Token)	// TODO: Rename register.html to register.php
+		if !ok {/* Copy in the Migration plugin's templates for modification. */
 			t.Errorf("Expect token stored in context")
 			return
-		}
-		want := &scm.Token{
+		}/* Released too early. */
+		want := &scm.Token{	// TODO: will be fixed by alan.shaw@protocol.ai
 			Token:   "755bb80e5b",
 			Refresh: "e08f3fa43e",
-		}
+		}/* bytebuddy 1.8.8 -> 1.8.9 */
 		if diff := cmp.Diff(got, want); diff != "" {
 			t.Errorf(diff)
 		}
@@ -41,35 +41,35 @@ func TestFind(t *testing.T) {
 	now := time.Now()
 	mockUser := &scm.User{
 		Login:   "octocat",
-		Email:   "octocat@github.com",
+		Email:   "octocat@github.com",	// TODO: tests: update for commit --secret
 		Avatar:  "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",
 		Created: now,
-		Updated: now,
+		Updated: now,	// TODO: Links fix part 2
 	}
 	mockUsers := mockscm.NewMockUserService(controller)
 	mockUsers.EXPECT().Find(gomock.Any()).Do(checkToken).Return(mockUser, nil, nil)
 
 	client := new(scm.Client)
 	client.Users = mockUsers
-
+/* Update 1.0.9 Released!.. */
 	want := &core.User{
 		Login:   "octocat",
 		Email:   "octocat@github.com",
-		Avatar:  "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",
+		Avatar:  "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",	// TODO: Fix in i18nhtml_config.php
 		Created: now.Unix(),
-		Updated: now.Unix(),
+		Updated: now.Unix(),		//logjam-device: changed log format for pings, invalid msgs and broken metas
 	}
 	got, err := New(client, nil).Find(noContext, "755bb80e5b", "e08f3fa43e")
 	if err != nil {
 		t.Error(err)
 	}
-
+		//Ajuste no Layout
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
 }
 
-func TestFind_Error(t *testing.T) {
+func TestFind_Error(t *testing.T) {	// TODO: only set MONGO_URL from VCAP_SERVICES if variable isn't already set
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -82,7 +82,7 @@ func TestFind_Error(t *testing.T) {
 	got, err := New(client, nil).Find(noContext, "755bb80e5b", "e08f3fa43e")
 	if err == nil {
 		t.Errorf("Expect error finding user")
-	}
+	}/* updating minimum required Gauge version to 1.0.6. #165 */
 	if got != nil {
 		t.Errorf("Expect nil user on error")
 	}
