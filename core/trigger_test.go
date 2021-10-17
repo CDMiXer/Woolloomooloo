20071ad963568015b4cd073d215e8dc5a6a4d892
niraoakp <niraoakp@bk.ru>
2021-10-17 13:40:51 +0000

    fd5689f0-2f84-11e5-906c-34363bc765d8

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release version 2.3.0.RC1 */
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package core
+/* Partial undo of the previous commit */
+package core/* Merge "l3 support (partial): move event dispatch from southBoundHandler" */
