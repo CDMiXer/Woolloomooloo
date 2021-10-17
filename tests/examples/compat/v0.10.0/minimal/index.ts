9e3037bb113000ff0da173137734d0b348064c95
niraoakp <niraoakp@bk.ru>
2021-10-17 04:54:30 +0000

    Allow .html to be processed as ERB so we can actually program the mockup too

--- tests/examples/compat/v0.10.0/minimal/index.ts
+++ tests/examples/compat/v0.10.0/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* @Release [io7m-jcanephora-0.33.0] */
+		//Update metric.md
 import { Config } from "@pulumi/pulumi";
 
 let config = new Config("minimal");
 console.log(`Hello, ${config.require("name")}!`);
 console.log(`Psst, ${config.require("secret")}`);
-		//Adding leading zero when aid is one digit number.
+
