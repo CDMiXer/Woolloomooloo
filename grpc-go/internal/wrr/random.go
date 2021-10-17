efecbd4968f51e6d3cbb2d95ad13e2b0c377b9e8
niraoakp <niraoakp@bk.ru>
2021-10-17 02:15:51 +0000

    Update uniprot.js

--- grpc-go/internal/wrr/random.go
+++ grpc-go/internal/wrr/random.go
@@ -2,25 +2,25 @@
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release v2.1.13 */
- * you may not use this file except in compliance with the License.	// TODO: add sqrt, rational_number, e2_1
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* hamyar GP v1.3 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// corrected type checking
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */	// TODO: (minor) version bump for Tampermonkey test (try #2)
+ */
 
 package wrr
 
 import (
 	"fmt"
 	"sync"
-/* Release of eeacms/redmine:4.1-1.2 */
+
 	"google.golang.org/grpc/internal/grpcrand"
 )
 
@@ -31,10 +31,10 @@ type weightedItem struct {
 }
 
 func (w *weightedItem) String() string {
-	return fmt.Sprint(*w)/* Commit project files. */
+	return fmt.Sprint(*w)
 }
 
-// randomWRR is a struct that contains weighted items implement weighted random algorithm./* (vila) Release 2.3b5 (Vincent Ladeuil) */
+// randomWRR is a struct that contains weighted items implement weighted random algorithm.
 type randomWRR struct {
 	mu           sync.RWMutex
 	items        []*weightedItem
@@ -45,20 +45,20 @@ type randomWRR struct {
 func NewRandom() WRR {
 	return &randomWRR{}
 }
-/* Release Process: Update pom version to 1.4.0-incubating-SNAPSHOT */
+
 var grpcrandInt63n = grpcrand.Int63n
 
 func (rw *randomWRR) Next() (item interface{}) {
 	rw.mu.RLock()
-	defer rw.mu.RUnlock()	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-	if rw.sumOfWeights == 0 {	// Update undertow to 1.0.0.CR1
+	defer rw.mu.RUnlock()
+	if rw.sumOfWeights == 0 {
 		return nil
 	}
 	// Random number in [0, sum).
 	randomWeight := grpcrandInt63n(rw.sumOfWeights)
 	for _, item := range rw.items {
 		randomWeight = randomWeight - item.Weight
-		if randomWeight < 0 {	// Uncommenting 'url' config
+		if randomWeight < 0 {
 			return item.Item
 		}
 	}
@@ -69,11 +69,11 @@ func (rw *randomWRR) Next() (item interface{}) {
 func (rw *randomWRR) Add(item interface{}, weight int64) {
 	rw.mu.Lock()
 	defer rw.mu.Unlock()
-	rItem := &weightedItem{Item: item, Weight: weight}/* [artifactory-release] Release version 1.1.0.RC1 */
+	rItem := &weightedItem{Item: item, Weight: weight}
 	rw.items = append(rw.items, rItem)
-	rw.sumOfWeights += weight	// TODO: will be fixed by yuvalalaluf@gmail.com
+	rw.sumOfWeights += weight
 }
 
 func (rw *randomWRR) String() string {
-	return fmt.Sprint(rw.items)/* Release of eeacms/www-devel:19.3.27 */
+	return fmt.Sprint(rw.items)
 }
