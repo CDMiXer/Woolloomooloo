0a33d253903f3cd462e56a9239508addd501f5f1
niraoakp <niraoakp@bk.ru>
2021-10-17 05:46:47 +0000

    Create dresden_1794?

--- tests/integration/double_pending_delete/step4/index.ts
+++ tests/integration/double_pending_delete/step4/index.ts
@@ -8,26 +8,26 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//initial attempt at a rewrite
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Character offset computation in TextBox */
+// limitations under the License.
 
-import { Resource } from "./resource";	// TODO: will be fixed by ng8eke@163.com
+import { Resource } from "./resource";
 
 // We'll complete our disaster recovery by triggering replacements of A and B again,
 // but this time the replacement of B will succeed.
-// The engine should generate:/* Release 1.10.6 */
+// The engine should generate:
 //
 // Delete A
 // Create A (mark old A as pending delete)
 const a = new Resource("a", { fail: 4 });
 
 // Create B
-const b = new Resource("b", { fail: 2 }, { dependsOn: a });/* add TODO for YEAR TClass */
-	// TODO: will be fixed by boringland@protonmail.ch
+const b = new Resource("b", { fail: 2 }, { dependsOn: a });
+
 // Delete A
 // Delete B
 
 // Like the last step, this is interesting because we delete A's URN three times in the same plan.
-// This plan should drain all pending deletes and get us back to a state where only the live A and B/* Release notes e link pro sistema Interage */
+// This plan should drain all pending deletes and get us back to a state where only the live A and B
 // exist in the checkpoint.
