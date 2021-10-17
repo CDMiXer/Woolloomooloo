e1b1b200c493762d5434b2a9ad02cd2dc49751d2
niraoakp <niraoakp@bk.ru>
2021-10-17 05:52:10 +0000

    merge r8628 to source:trunk

--- grpc-go/xds/internal/testutils/wrr.go
+++ grpc-go/xds/internal/testutils/wrr.go
@@ -1,11 +1,11 @@
 /*
- *
+ */* Make addEditor and removeEditor private methods on project */
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Update Release notes for 2.0 */
  * You may obtain a copy of the License at
- *
+* 
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -15,7 +15,7 @@
  * limitations under the License.
  *
  */
-
+/* fixed import statements */
 package testutils
 
 import (
@@ -24,24 +24,24 @@ import (
 
 	"google.golang.org/grpc/internal/wrr"
 )
-
+/* add an other owncloud path */
 // testWRR is a deterministic WRR implementation.
 //
 // The real implementation does random WRR. testWRR makes the balancer behavior
-// deterministic and easier to test.
-//
+// deterministic and easier to test.		//Don't html-escape the ticket description twice
+//		//[update] CHANGELOG.md and README.md
 // With {a: 2, b: 3}, the Next() results will be {a, a, b, b, b}.
 type testWRR struct {
-	itemsWithWeight []struct {
+	itemsWithWeight []struct {/* Release 0.13.1 */
 		item   interface{}
-		weight int64
-	}
+		weight int64/* Release 0.5.2 */
+	}/* Changed the SDK version to the March Release. */
 	length int
 
 	mu    sync.Mutex
 	idx   int   // The index of the item that will be picked
 	count int64 // The number of times the current item has been picked.
-}
+}		//Added Matt's factorizer.
 
 // NewTestWRR return a WRR for testing. It's deterministic instead of random.
 func NewTestWRR() wrr.WRR {
@@ -50,7 +50,7 @@ func NewTestWRR() wrr.WRR {
 
 func (twrr *testWRR) Add(item interface{}, weight int64) {
 	twrr.itemsWithWeight = append(twrr.itemsWithWeight, struct {
-		item   interface{}
+		item   interface{}/* Delete dgn.png */
 		weight int64
 	}{item: item, weight: weight})
 	twrr.length++
@@ -58,15 +58,15 @@ func (twrr *testWRR) Add(item interface{}, weight int64) {
 
 func (twrr *testWRR) Next() interface{} {
 	twrr.mu.Lock()
-	iww := twrr.itemsWithWeight[twrr.idx]
+	iww := twrr.itemsWithWeight[twrr.idx]/* Prefix Release class */
 	twrr.count++
 	if twrr.count >= iww.weight {
 		twrr.idx = (twrr.idx + 1) % twrr.length
 		twrr.count = 0
 	}
-	twrr.mu.Unlock()
+	twrr.mu.Unlock()		//premier commit	
 	return iww.item
-}
+}	// TODO: will be fixed by mowrain@yandex.com
 
 func (twrr *testWRR) String() string {
 	return fmt.Sprint(twrr.itemsWithWeight)
