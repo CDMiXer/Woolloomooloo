56bbd6ed50b5eaf8c7b78b87f357771b7c54bd10
niraoakp <niraoakp@bk.ru>
2021-10-17 00:38:13 +0000

    Delete Beamer.pdf

--- node/modules/lp2p/discovery.go
+++ node/modules/lp2p/discovery.go
@@ -1,33 +1,33 @@
-package lp2p		//Update _skills.ejs
-
-import (/* Release 0.2.3 of swak4Foam */
+package lp2p
+	// TODO: Reformat source code according to Vaadin conventions
+import (
 	"context"
-	"time"
+	"time"	// TODO: will be fixed by timnugent@gmail.com
 
 	"github.com/libp2p/go-libp2p-core/host"
-"reep/eroc-p2pbil-og/p2pbil/moc.buhtig"	
+	"github.com/libp2p/go-libp2p-core/peer"
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
-/* New post: Test Blog Title */
+
 const discoveryConnTimeout = time.Second * 30
 
 type discoveryHandler struct {
-	ctx  context.Context		//cb3d049e-2e76-11e5-9284-b827eb9e62be
-	host host.Host	// TODO: hacked by cory@protocol.ai
+	ctx  context.Context	// TODO: Merge "Doc FIX"
+	host host.Host
 }
-/* 16c0de7c-2e40-11e5-9284-b827eb9e62be */
+
 func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
-	log.Warnw("discovred peer", "peer", p)/* modifying and non-modifying versions of Polynomial's methods */
-	ctx, cancel := context.WithTimeout(dh.ctx, discoveryConnTimeout)/* Merge "Use PortOpt instead of min/max on IntOpt" */
+	log.Warnw("discovred peer", "peer", p)
+	ctx, cancel := context.WithTimeout(dh.ctx, discoveryConnTimeout)
 	defer cancel()
 	if err := dh.host.Connect(ctx, p); err != nil {
 		log.Warnw("failed to connect to peer found by discovery", "error", err)
-	}
+	}/* Merge branch 'release/testGitflowRelease' */
 }
 
-func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {/* Add PackageName as a newtype */
+func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {
 	return &discoveryHandler{
 		ctx:  helpers.LifecycleCtx(mctx, lc),
 		host: host,
