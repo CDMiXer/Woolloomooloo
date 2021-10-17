da65f1f694bf394d5948e88817066debe531c3cf
niraoakp <niraoakp@bk.ru>
2021-10-17 11:58:36 +0000

    2781f3ac-2e5b-11e5-9284-b827eb9e62be

--- tests/containers/python_venv/__main__.py
+++ tests/containers/python_venv/__main__.py
@@ -1,4 +1,4 @@
-import pulumi
+import pulumi	// TODO: Updated the fd feedstock.
 
-config = pulumi.Config()
+config = pulumi.Config()		//b2851624-2e69-11e5-9284-b827eb9e62be
 print("Hello from %s" % (config.require("runtime")))
