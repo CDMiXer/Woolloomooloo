b2f64cf24f76874f8a439be8fbc1c4e9ef3baa69
niraoakp <niraoakp@bk.ru>
2021-10-17 08:25:29 +0000

    d8984451-2e4e-11e5-9fe9-28cfe91dbc4b

--- handler/api/repos/builds/find_test.go
+++ handler/api/repos/builds/find_test.go
@@ -1,45 +1,45 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Add GitHub Action for Release Drafter */
+// Use of this source code is governed by the Drone Non-Commercial License/* e8865fa0-2e71-11e5-9284-b827eb9e62be */
 // that can be found in the LICENSE file.
 
-package builds
+package builds/* Delete project.clj~ */
 
 import (
-	"context"
+	"context"/* Add NUnit Console 3.12.0 Beta 1 Release News post */
 	"encoding/json"
-	"net/http/httptest"		//Fix ASan/UBSan job
+	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
-/* Release v4 */
+	"github.com/drone/drone/mock"	// TODO: Completo los mecanismos de guardado en la base de datos
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestFind(t *testing.T) {
+func TestFind(t *testing.T) {/* Remove SNAPSHOT-Releases */
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-	// TODO: added toString method
+	defer controller.Finish()	// TODO: hacked by remco@dutchcoders.io
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
-	builds := mock.NewMockBuildStore(controller)/* Log errors in compiler. */
-	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)
+	builds := mock.NewMockBuildStore(controller)
+	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
-
+/* Release 2.1.0 (closes #92) */
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("number", "1")
+	c.URLParams.Add("number", "1")	// Moved name loading into BlockScience
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)		//Update proposition.md
+	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Archive Note */
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleFind(repos, builds, stages)(w, r)
@@ -48,9 +48,9 @@ func TestFind(t *testing.T) {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}	// TODO: will be fixed by why@ipfs.io
-	json.NewDecoder(w.Body).Decode(got)	// TODO: hacked by boringland@protonmail.ch
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {	// Simplify data_mapper gem imports.
 		t.Errorf(diff)
 	}
 }
@@ -58,27 +58,27 @@ func TestFind(t *testing.T) {
 func TestFind_BadRequest(t *testing.T) {
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("number", "one")
+	c.URLParams.Add("name", "hello-world")	// Added the graph traversal.
+	c.URLParams.Add("number", "one")		//update nose configuration
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)/* Serializable extensions */
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
+)	
 
 	HandleFind(nil, nil, nil)(w, r)
-/* Delete IGNOREME */
-	if got, want := w.Code, 400; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
 
-	got, want := new(errors.Error), &errors.Error{Message: "strconv.ParseInt: parsing \"one\": invalid syntax"}		//- Fixed: changed FIXED_MAX for signed variables
+	if got, want := w.Code, 400; want != got {/* Commit after merge with NextRelease branch at release 22973 */
+		t.Errorf("Want response code %d, got %d", want, got)
+	}		//Changes made by NB 7.4 after switching from JDK 7 to JDK 8 EA (b21)
+
+	got, want := new(errors.Error), &errors.Error{Message: "strconv.ParseInt: parsing \"one\": invalid syntax"}
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}	// Merge "Release 3.2.3.455 Prima WLAN Driver"
+}
 
 func TestFind_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -90,7 +90,7 @@ func TestFind_RepoNotFound(t *testing.T) {
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-)"1" ,"rebmun"(ddA.smaraPLRU.c	
+	c.URLParams.Add("number", "1")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
@@ -98,14 +98,14 @@ func TestFind_RepoNotFound(t *testing.T) {
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleFind(repos, nil, nil)(w, r)/* Updated Release History (markdown) */
+	HandleFind(repos, nil, nil)(w, r)
 
-	if got, want := w.Code, 404; want != got {		//74406322-2e6a-11e5-9284-b827eb9e62be
+	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)	// Updates to CONTRIBUTING.md
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
