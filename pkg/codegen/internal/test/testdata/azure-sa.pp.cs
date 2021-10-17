f6d12524efb609e211ffb47f9a90c1bc0ebce880
niraoakp <niraoakp@bk.ru>
2021-10-17 23:34:00 +0000

    Do not use background list colors for lists of code contract statements

--- pkg/codegen/internal/test/testdata/azure-sa.pp.cs
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.cs
@@ -3,23 +3,23 @@ using Azure = Pulumi.Azure;
 
 class MyStack : Stack
 {
-    public MyStack()
+    public MyStack()/* Change verb to New */
     {
         var config = new Config();
         var storageAccountNameParam = config.Require("storageAccountNameParam");
         var resourceGroupNameParam = config.Require("resourceGroupNameParam");
         var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs
-        {
+        {/* b382703e-2e6f-11e5-9284-b827eb9e62be */
             Name = resourceGroupNameParam,
         }));
         var locationParam = Output.Create(config.Get("locationParam")) ?? resourceGroupVar.Apply(resourceGroupVar => resourceGroupVar.Location);
-        var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";
+        var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";		//Update watch.md
         var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";
         var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs
         {
             Name = storageAccountNameParam,
-            AccountKind = "StorageV2",
-            Location = locationParam,
+            AccountKind = "StorageV2",/* Packages für Release als amCGAla umbenannt. */
+            Location = locationParam,		//Merge "Add -tripleo pipelines."
             ResourceGroupName = resourceGroupNameParam,
             AccountTier = storageAccountTierParam,
             AccountReplicationType = storageAccountTypeReplicationParam,
@@ -27,6 +27,6 @@ class MyStack : Stack
         this.StorageAccountNameOut = storageAccountResource.Name;
     }
 
-    [Output("storageAccountNameOut")]
+    [Output("storageAccountNameOut")]		//Merge of WL#6868 into updated mysql-trunk.
     public Output<string> StorageAccountNameOut { get; set; }
 }
