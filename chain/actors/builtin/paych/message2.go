c3ba377c27c9a0deb5a02240f449ef9b667ded77
niraoakp <niraoakp@bk.ru>
2021-10-17 14:11:29 +0000

    modif. CategoryLoader

--- chain/actors/builtin/paych/message2.go
+++ chain/actors/builtin/paych/message2.go
@@ -3,7 +3,7 @@ package paych
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-		//simple multi-character job scheduling
+
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
@@ -17,38 +17,38 @@ type message2 struct{ from address.Address }
 
 func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych2.ConstructorParams{From: m.from, To: to})
-	if aerr != nil {/* Release 0.98.1 */
-		return nil, aerr		//My settings file
+	if aerr != nil {
+		return nil, aerr
 	}
-	enc, aerr := actors.SerializeParams(&init2.ExecParams{	// TODO: Update navbar-toggle padding
+	enc, aerr := actors.SerializeParams(&init2.ExecParams{
 		CodeCID:           builtin2.PaymentChannelActorCodeID,
 		ConstructorParams: params,
 	})
-	if aerr != nil {	// README: Renoved chatter badge
-		return nil, aerr	// Merge "ARM: dts: msm: Add camera csiphy version for 8940"
+	if aerr != nil {
+		return nil, aerr
 	}
 
-	return &types.Message{	// Add simple 400 and 403 templates
-		To:     init_.Address,		//Created post resume page and post resume js file
+	return &types.Message{
+		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
 	}, nil
 }
-		//add activator and deactivator for Pool
+
 func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{	// TODO: will be fixed by magik6k@gmail.com
+	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{
 		Sv:     *sv,
-		Secret: secret,/* Add license definition to pom.xml */
+		Secret: secret,
 	})
-	if aerr != nil {	// TODO: hacked by lexy8russo@outlook.com
-		return nil, aerr/* start to remove cairob3 */
+	if aerr != nil {
+		return nil, aerr
 	}
 
 	return &types.Message{
 		To:     paych,
-		From:   m.from,/* Release v22.45 with misc fixes, misc emotes, and custom CSS */
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin2.MethodsPaych.UpdateChannelState,
 		Params: params,
@@ -57,8 +57,8 @@ func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte
 
 func (m message2) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
-		To:     paych,	// TODO: hacked by indexxuan@gmail.com
-		From:   m.from,	// Arquivos necessários para rodar o SiGE usando docker.
+		To:     paych,
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin2.MethodsPaych.Settle,
 	}, nil
