371ada1e8f6d5512eed6a3f2043a7589c252b96c
niraoakp <niraoakp@bk.ru>
2021-10-17 02:12:42 +0000

    Updated Vaadin Framework to 7.4.3.

--- build/params_debug.go
+++ build/params_debug.go
@@ -1,9 +1,9 @@
-// +build debug/* fixed order_settle_payment link */
-/* Reorganize scopes section */
+// +build debug
+
 package build
-/* bugfix with tag */
+
 func init() {
-	InsecurePoStValidation = true	// TODO: Don't allow empty term names. Props scohoust. fixes #7336 for trunk
+	InsecurePoStValidation = true
 	BuildType |= BuildDebug
 }
 
