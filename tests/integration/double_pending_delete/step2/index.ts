e645306a92c1e910a39d381cd27d21d5212db606
niraoakp <niraoakp@bk.ru>
2021-10-17 15:29:19 +0000

    Add empty app.js

--- tests/integration/double_pending_delete/step2/index.ts
+++ tests/integration/double_pending_delete/step2/index.ts
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Delete ZXCT1009F.lib */
+// you may not use this file except in compliance with the License.		//temperature trend chart observation
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//		//Experimental removal of tmp.css
+//     http://www.apache.org/licenses/LICENSE-2.0		//- strip html tags for search results
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release 4.2.4  */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
 
 // The changes in this plan trigger replacement of both A and B.
-// The replacement of A is successful, but the replacement of B fails,		//Update mock_server.js
-// since the provider is rigged to fail if fail == 1./* Release version 2.0.0.M1 */
+// The replacement of A is successful, but the replacement of B fails,
+// since the provider is rigged to fail if fail == 1.
 //
 // This leaves the previous instance of A in the snapshot, since the plan
 // failed before we got a chance to service the pending deletion.
 const a = new Resource("a", { fail: 2 });
-const b = new Resource("b", { fail: 1 }, { dependsOn: a });/* Create Exercise4_VariablesAndNames.py */
+const b = new Resource("b", { fail: 1 }, { dependsOn: a });
 // The snapshot now contains:
 //  A: Created
-//  A: Pending Delete
+//  A: Pending Delete/* Release 0.94.360 */
 //  B: Created
 
