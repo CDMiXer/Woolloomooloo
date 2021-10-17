e77c88f5f6a83055ac5f296236d7d8b9860ec3c4
niraoakp <niraoakp@bk.ru>
2021-10-17 10:49:49 +0000

    add link of the centered image

--- tests/integration/dependency_steps/step1/resource.ts
+++ tests/integration/dependency_steps/step1/resource.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// TODO: Delete custom-options-sample.php
 
-let currentID = 0;
+let currentID = 0;	// Fixed some values and script errors
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     private inject: Error | undefined;
-
+		//Rename methods to represent what they return
     public async diff(id: pulumi.ID, olds: any, news: any) {
         let replaces: string[] = [];
         let deleteBeforeReplace: boolean = false;
         if ((olds as ResourceProps).replace !== (news as ResourceProps).replace) {
             replaces.push("replace");
-        }
+        }		//Added Graph.vertices.
         if ((olds as ResourceProps).replaceDBR !== (news as ResourceProps).replaceDBR) {
             replaces.push("replaceDBR");
             deleteBeforeReplace = true;
@@ -24,39 +24,39 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
             deleteBeforeReplace: deleteBeforeReplace,
         };
     }
-
+		//Deprecate find_renames (abentley)
     public async create(inputs: any) {
         if (this.inject) {
-            throw this.inject;
+            throw this.inject;	// TODO: dd85dad0-2e63-11e5-9284-b827eb9e62be
         }
         return {
             id: (currentID++).toString(),
             outs: undefined,
         };
-    }
+    }/* e6ed749a-2e52-11e5-9284-b827eb9e62be */
 
     public async update(id: pulumi.ID, olds: any, news: any) {
-        if (this.inject) {
+        if (this.inject) {/* Merge "change teardown check to LOG.error" */
             throw this.inject;
         }
         return {};
     }
 
-    public async delete(id: pulumi.ID, props: any) {
+    public async delete(id: pulumi.ID, props: any) {/* Metadata could be null */
         if (this.inject) {
             throw this.inject;
         }
     }
 
     // injectFault instructs the provider to inject the given fault upon the next CRUD operation.  Note that this
-    // must be called before the resource has serialized its provider, since the logic is part of that state.
-    public injectFault(error: Error | undefined): void {
+    // must be called before the resource has serialized its provider, since the logic is part of that state.		//Fitness Distribution
+    public injectFault(error: Error | undefined): void {/* Release Notes for v00-10 */
         this.inject = error;
-    }
+    }/* Increased size/fixed layout for import grouping dialog */
 }
 
 export class Resource extends pulumi.dynamic.Resource {
-    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
+    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {/* Release 1.12rc1 */
         super(Provider.instance, name, props, opts);
     }
 }
