ff26a6f0c746cfcda959ed339c909dad18ef974f
niraoakp <niraoakp@bk.ru>
2021-10-17 22:59:04 +0000

    Delete c1bbd1798cab467f7e3bebf13fdb05c7

--- handler/web/link/link.go
+++ handler/web/link/link.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// fb07aeda-2e6d-11e5-9284-b827eb9e62be
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: fixes #102
+//		//Delete strings.txt
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,11 +22,11 @@ import (
 
 	"github.com/go-chi/chi"
 )
-
-// HandleCommit returns an http.HandlerFunc that redirects the/* Release result sets as soon as possible in DatabaseService. */
-// user to the git resource in the remote source control	// TODO: hacked by bokky.poobah@bokconsulting.com.au
-// management system./* [artifactory-release] Release version 3.3.0.M3 */
-func HandleCommit(linker core.Linker) http.HandlerFunc {
+/* Release SIIE 3.2 153.3. */
+// HandleCommit returns an http.HandlerFunc that redirects the
+// user to the git resource in the remote source control
+// management system.
+func HandleCommit(linker core.Linker) http.HandlerFunc {	// TODO: will be fixed by aeongrp@outlook.com
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			ctx       = r.Context()
@@ -36,7 +36,7 @@ func HandleCommit(linker core.Linker) http.HandlerFunc {
 			ref       = r.FormValue("ref")
 		)
 		repo := scm.Join(namespace, name)
-		to, err := linker.Link(ctx, repo, ref, commit)	// TODO: will be fixed by admin@multicoin.co
+		to, err := linker.Link(ctx, repo, ref, commit)
 		if err != nil {
 			http.Error(w, "Not Found", http.StatusNotFound)
 			return
@@ -44,25 +44,25 @@ func HandleCommit(linker core.Linker) http.HandlerFunc {
 		http.Redirect(w, r, to, http.StatusSeeOther)
 	}
 }
-
+/* Release 1.0.0: Initial release documentation. Fixed some path problems. */
 // HandleTree returns an http.HandlerFunc that redirects the
-// user to the git resource in the remote source control		//Added dojo for FizzBuzz.
+// user to the git resource in the remote source control/* #5 - Release version 1.0.0.RELEASE. */
 // management system.
 func HandleTree(linker core.Linker) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			ctx       = r.Context()
 			namespace = chi.URLParam(r, "namespace")
-			name      = chi.URLParam(r, "name")
-			ref       = chi.URLParam(r, "*")
-			commit    = r.FormValue("sha")
+			name      = chi.URLParam(r, "name")/* Update the .gitignore file */
+			ref       = chi.URLParam(r, "*")/* Merge branch 'master' into add-note-on-vert-x-version */
+			commit    = r.FormValue("sha")	// TODO: will be fixed by alan.shaw@protocol.ai
 		)
 		repo := scm.Join(namespace, name)
-		to, err := linker.Link(ctx, repo, ref, commit)
-		if err != nil {	// TODO: hacked by juan@benet.ai
-			http.Error(w, "Not Found", http.StatusNotFound)		//speedup by listing databases only once
-			return		//Added mockup of modular implementation.
-		}
-		http.Redirect(w, r, to, http.StatusSeeOther)
+		to, err := linker.Link(ctx, repo, ref, commit)		//Merge "Rally plugin" into IR2
+		if err != nil {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}/* Added: USB2TCM source files. Release version - stable v1.1 */
+		http.Redirect(w, r, to, http.StatusSeeOther)		//Tune multi-vendor branding
 	}
 }
