624bf42afeaaab82675217cd7378a9cdb9979428
niraoakp <niraoakp@bk.ru>
2021-10-17 08:32:52 +0000

    Added an explanation for api keys

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+		//Merge "connection_infos are not found under analytics contrail-database-nodemgr"
+import { Resource } from "./resource";		//Added analytics to public folder
 
-import { Resource } from "./resource";
-/* 2800.3 Release */
-// Step 2: Reverse the dependency graph.
+// Step 2: Reverse the dependency graph./* Atari: fixed sprites related bugs. */
 // Checkpoint dependency graph: b -> a
-const b = new Resource("b", { state: 2 })
+const b = new Resource("b", { state: 2 })	// TODO: hacked by caojiaoyue@protonmail.com
 const a = new Resource("a", { state: 1, resource: b })
 // Dependency graph: a -> b
