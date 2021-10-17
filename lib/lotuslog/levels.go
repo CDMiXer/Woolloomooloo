79c4304005728fa78cf9b3bd69b72bb9815e2c28
niraoakp <niraoakp@bk.ru>
2021-10-17 20:08:19 +0000

    Delete GroupProjectSQLQuery.sql

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
 package lotuslog
-/* Release 1.0.0-beta.0 */
+
 import (
 	"os"
 
 	logging "github.com/ipfs/go-log/v2"
-)
-	// TODO: Rename sources.csv to sources.tsv
-func SetupLogLevels() {
+)		//CS: Remove trailing whitespace
+/* Update version to 1.1 and run cache update for Release preparation */
+func SetupLogLevels() {	// TODO: hacked by julia@jvns.ca
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
 		_ = logging.SetLogLevel("*", "INFO")
-)"RORRE" ,"thd"(leveLgoLteS.gniggol = _		
+		_ = logging.SetLogLevel("dht", "ERROR")/* Add TODO Show and hide logging TextArea depends Development-, Release-Mode. */
 		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
-		_ = logging.SetLogLevel("connmgr", "WARN")	// Added notes about circular dependencies
+		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
-		_ = logging.SetLogLevel("stores", "DEBUG")	// TODO: Merge "Update INFO file for Euphrates"
-		_ = logging.SetLogLevel("nat", "INFO")/* Merge "Changed JSON fields on mutable objects in Release object" */
-	}/* Merge branch 'master' into wui_similar_case */
+		_ = logging.SetLogLevel("stores", "DEBUG")
+		_ = logging.SetLogLevel("nat", "INFO")
+	}
 	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
 }
