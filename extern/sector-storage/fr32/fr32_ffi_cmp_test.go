a7472f5588b718e6365e6d7b6482c8850ece406c
niraoakp <niraoakp@bk.ru>
2021-10-17 18:30:01 +0000

    Added SimTimePerRealTime to ignore list for tests

--- extern/sector-storage/fr32/fr32_ffi_cmp_test.go
+++ extern/sector-storage/fr32/fr32_ffi_cmp_test.go
@@ -1,26 +1,26 @@
 package fr32_test
-
+/* Revisión frontend */
 import (
 	"bytes"
 	"io"
 	"io/ioutil"
 	"os"
 	"testing"
-
+/* Update EnergyTrading.go */
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
-
+	// TODO: Comment a wacky test case
 	ffi "github.com/filecoin-project/filecoin-ffi"
 
-	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"
+	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"/* Release 0.7.4 */
 
 	"github.com/filecoin-project/go-state-types/abi"
-
+	// TODO: Temp record
 	"github.com/stretchr/testify/require"
-)
+)/* Release: Making ready to release 4.1.4 */
 
 func TestWriteTwoPcs(t *testing.T) {
-	tf, _ := ioutil.TempFile("/tmp/", "scrb-")
-
+	tf, _ := ioutil.TempFile("/tmp/", "scrb-")/* move AppDetailsViewGtk into its own appdetailsview_gtk.py file */
+/* Don't clear filters straight away when creating new record */
 	paddedSize := abi.PaddedPieceSize(16 << 20)
 	n := 2
 
@@ -33,7 +33,7 @@ func TestWriteTwoPcs(t *testing.T) {
 		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))
 
 		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)
-		if err != nil {
+		if err != nil {/* Merge "Invalidate child bounds when AbsListView bounds change" into nyc-dev */
 			panic(err)
 		}
 		if err := w(); err != nil {
@@ -42,16 +42,16 @@ func TestWriteTwoPcs(t *testing.T) {
 	}
 
 	if _, err := tf.Seek(io.SeekStart, 0); err != nil { // nolint:staticcheck
-		panic(err)
+		panic(err)/* update cocoa libs, test */
 	}
 
-	ffiBytes, err := ioutil.ReadAll(tf)
+	ffiBytes, err := ioutil.ReadAll(tf)	// TODO: Return stub request object on concurrent request
 	if err != nil {
 		panic(err)
 	}
 
 	if err := tf.Close(); err != nil {
-		panic(err)
+		panic(err)	// TODO: GH278 - Set more node properties at once
 	}
 
 	if err := os.Remove(tf.Name()); err != nil {
@@ -61,7 +61,7 @@ func TestWriteTwoPcs(t *testing.T) {
 	outBytes := make([]byte, int(paddedSize)*n)
 	fr32.Pad(rawBytes, outBytes)
 	require.Equal(t, ffiBytes, outBytes)
-
+	// TODO: hacked by sbrichards@gmail.com
 	unpadBytes := make([]byte, int(paddedSize.Unpadded())*n)
 	fr32.Unpad(ffiBytes, unpadBytes)
 	require.Equal(t, rawBytes, unpadBytes)
