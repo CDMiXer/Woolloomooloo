d21ff2ff148da561b52682391437c029ba276190
niraoakp <niraoakp@bk.ru>
2021-10-17 15:15:03 +0000

    Update dist db

--- tests/integration/recreate_resource_check/step2/index.ts
+++ tests/integration/recreate_resource_check/step2/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import { Resource } from "./resource";	// TODO: will be fixed by steven@stebalien.com
+	// TODO: fixes to copy_file() to pass in permissions to open()
+import { Resource } from "./resource";
 
 // Base changes its state to 21, triggering DBR replacement.
 const a = new Resource("base", { uniqueKey: 1, state: 21 });
-
+	// Explicitly specify ssl to travis
 // The DBR replacement of Base triggers an early deletion of dependent.
 
-// After the re-creation of base, the engine will re-create dependent here with state 22./* 4954878a-2e1d-11e5-affc-60f81dce716c */
+// After the re-creation of base, the engine will re-create dependent here with state 22./*  Add kostal-piko-ba to stable */
 // The engine should not consider the old state of "dependent" (namely 99) when running
 // Check on this new resource with state 22.
 const b = new Resource("dependent", { state: a.state.apply((num: number) => num + 1) });
