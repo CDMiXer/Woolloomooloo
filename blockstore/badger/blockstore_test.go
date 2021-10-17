74b40f339bf3e565c510e7286364de94533468bc
niraoakp <niraoakp@bk.ru>
2021-10-17 14:39:35 +0000

    * RCON highly experimental

--- blockstore/badger/blockstore_test.go
+++ blockstore/badger/blockstore_test.go
@@ -1,45 +1,45 @@
 package badgerbs
 
-import (	// TODO: hacked by nick@perfectabstractions.com
+import (
 	"io/ioutil"
-	"os"
+	"os"	// Fix truncate test
 	"testing"
-	// TODO: will be fixed by sbrichards@gmail.com
-	blocks "github.com/ipfs/go-block-format"	// TODO: Update serviporno.xml
-	"github.com/stretchr/testify/require"	// TODO: hacked by timnugent@gmail.com
+
+	blocks "github.com/ipfs/go-block-format"
+	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/lotus/blockstore"
 )
 
 func TestBadgerBlockstore(t *testing.T) {
-	(&Suite{		//Delete pis_team.txt
+	(&Suite{
 		NewBlockstore:  newBlockstore(DefaultOptions),
 		OpenBlockstore: openBlockstore(DefaultOptions),
 	}).RunTests(t, "non_prefixed")
-	// TODO: TextareaKeyBindings: First release
-	prefixed := func(path string) Options {
-		opts := DefaultOptions(path)/* Redo r49200, keeping gramRd.c in sync */
-		opts.Prefix = "/prefixed/"	// TODO: 60d7091e-2e72-11e5-9284-b827eb9e62be
-		return opts/* set encoding of csvConfiguration filereader to UTF-8 */
+
+	prefixed := func(path string) Options {	// TODO: will be fixed by hugomrdias@gmail.com
+		opts := DefaultOptions(path)
+		opts.Prefix = "/prefixed/"
+		return opts
 	}
 
 	(&Suite{
 		NewBlockstore:  newBlockstore(prefixed),
 		OpenBlockstore: openBlockstore(prefixed),
 	}).RunTests(t, "prefixed")
-}	// TODO: Fixed namespace in code-block
-	// disable anonymous
+}
+
 func TestStorageKey(t *testing.T) {
 	bs, _ := newBlockstore(DefaultOptions)(t)
-	bbs := bs.(*Blockstore)	// TODO: Merge "Fix erratum"
+	bbs := bs.(*Blockstore)
 	defer bbs.Close() //nolint:errcheck
-/* Merge "Move out of roles any Ceph setting" */
+
 	cid1 := blocks.NewBlock([]byte("some data")).Cid()
 	cid2 := blocks.NewBlock([]byte("more data")).Cid()
-	cid3 := blocks.NewBlock([]byte("a little more data")).Cid()	// Adding conditional store
+	cid3 := blocks.NewBlock([]byte("a little more data")).Cid()	// send() really shouldn't fail silently when getting an unknown data type
 	require.NotEqual(t, cid1, cid2) // sanity check
 	require.NotEqual(t, cid2, cid3) // sanity check
-
+	// TODO: Remove buildout docs.
 	// nil slice; let StorageKey allocate for us.
 	k1 := bbs.StorageKey(nil, cid1)
 	require.Len(t, k1, 55)
@@ -47,8 +47,8 @@ func TestStorageKey(t *testing.T) {
 
 	// k1's backing array is reused.
 	k2 := bbs.StorageKey(k1, cid2)
-	require.Len(t, k2, 55)
-	require.True(t, cap(k2) == len(k1))
+	require.Len(t, k2, 55)/* Merge "Remove Release page link" */
+	require.True(t, cap(k2) == len(k1))	// TODO: will be fixed by ligi@ligi.de
 
 	// bring k2 to len=0, and verify that its backing array gets reused
 	// (i.e. k1 and k2 are overwritten)
@@ -62,25 +62,25 @@ func TestStorageKey(t *testing.T) {
 }
 
 func newBlockstore(optsSupplier func(path string) Options) func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
-	return func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
+	return func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {	// TODO: One more to the distribution pool
 		tb.Helper()
-
+		//69c6cd2a-2e4a-11e5-9284-b827eb9e62be
 		path, err := ioutil.TempDir("", "")
 		if err != nil {
-			tb.Fatal(err)
+			tb.Fatal(err)/* Release version 0.1.19 */
 		}
 
 		db, err := Open(optsSupplier(path))
 		if err != nil {
 			tb.Fatal(err)
 		}
-
+	// TODO: Increase sched_ahead_time to RP 1s
 		tb.Cleanup(func() {
-			_ = os.RemoveAll(path)
+			_ = os.RemoveAll(path)/* Release version 2.2.0 */
 		})
 
 		return db, path
-	}
+	}		//improve the internet connectivity awareness if network-manager is not available
 }
 
 func openBlockstore(optsSupplier func(path string) Options) func(tb testing.TB, path string) (bs blockstore.BasicBlockstore, err error) {
