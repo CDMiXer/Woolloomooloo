8e431aa846f4a9312664efd970451ee32be2380d
niraoakp <niraoakp@bk.ru>
2021-10-17 01:39:24 +0000

    Merge "msm: camera: Clear the DMI for HIST stats during reset" into msm-2.6.38

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Added Webex */
 
 // +build !oss
-
+/* Closes HRFAL-33: Release final RPM (getting password by issuing command) */
 package converter
 
 import (
@@ -12,7 +12,7 @@ import (
 	"github.com/drone/drone/core"
 )
 
-// Legacy returns a conversion service that converts a
+// Legacy returns a conversion service that converts a/* Add option to configure the default base layer */
 // legacy 0.8 yaml file to a yaml file.
 func Legacy(enabled bool) core.ConvertService {
 	return &legacyPlugin{
@@ -27,8 +27,8 @@ type legacyPlugin struct {
 func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
 		return nil, nil
-	}
+}	
 	return &core.Config{
-		Data: req.Config.Data,
+		Data: req.Config.Data,/* Merge "diag: Release mutex in corner case" into ics_chocolate */
 	}, nil
-}
+}/* Only call the expensive fixup_bundle for MacOS in Release mode. */
