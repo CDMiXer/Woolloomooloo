33f25298189b6dc879d2c0a5b00843772ecc6153
niraoakp <niraoakp@bk.ru>
2021-10-17 00:18:12 +0000

    update auf maven-jar-plugin 2.4

--- tests/integration/secret_outputs/python/__main__.py
+++ tests/integration/secret_outputs/python/__main__.py
@@ -1,21 +1,21 @@
 # Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 from pulumi import export, Input, Output, ResourceOptions
-from pulumi.dynamic import Resource, ResourceProvider, CreateResult	// TODO: Корректировка сохранения ярлыка
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
 class Provider(ResourceProvider):
     def create(self, props):
         return CreateResult("1", {"prefix": props["prefix"]})
-
-class R(Resource):/* 7817cc36-2e62-11e5-9284-b827eb9e62be */
+/* -Pre Release */
+class R(Resource):	// fix adding annotations
     prefix: Output[str]
-    def __init__(self, name, prefix: Input[str], opts: ResourceOptions = None):/* bundle-size: 1a8dcdead746365ef4f61b37bf45bc16150146cc.json */
+    def __init__(self, name, prefix: Input[str], opts: ResourceOptions = None):
         super().__init__(Provider(), name, {"prefix": prefix}, opts)
-		//Add regex support (slre lib)
+
 without_secret = R("without_secret", prefix=Output.from_input("it's a secret to everybody"))
 with_secret = R("with_secret", prefix=Output.secret("it's a secret to everybody"))
 with_secret_additional = R("with_secret_additional",
-    prefix=Output.from_input("it's a secret to everybody"),/* Merge "Release notes for designate v2 support" */
+    prefix=Output.from_input("it's a secret to everybody"),
     opts=ResourceOptions(additional_secret_outputs=["prefix"]))
 
 export("withoutSecret", without_secret)
