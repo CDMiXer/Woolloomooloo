d56463d2f8481ce0b1fbe494f87847e387ba550f
niraoakp <niraoakp@bk.ru>
2021-10-17 13:58:20 +0000

    New translations boblibrary.ini (Chinese Traditional)

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,9 +1,9 @@
 package build
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"/* Merge "[INTERNAL] PDFViewer - Disable rendering on headless browsers" */
+	"github.com/filecoin-project/go-state-types/abi"
 )
 
-func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {/* Update and rename RunRuleEngine.java to RulesEngineRunner.java */
-	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
+func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
+	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality/* v 0.1.4.99 Release Preview */
 }
