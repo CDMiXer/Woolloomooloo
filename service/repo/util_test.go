06b79a93da67f24ffd7b29eeb95394a331436e51
niraoakp <niraoakp@bk.ru>
2021-10-17 20:45:55 +0000

    now is on wordpress.org' glotpress

--- service/repo/util_test.go
+++ service/repo/util_test.go
@@ -1,55 +1,55 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Merge "Transition l7rule flows to dicts"
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package repo
-/* Updating build-info/dotnet/wcf/release/uwp6.0 for preview1-25522-01 */
-import (/* remove border */
+
+import (
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-		//Merge "Rename murano-api -> murano"
-	"github.com/google/go-cmp/cmp"
-)
+
+	"github.com/google/go-cmp/cmp"	// TODO: will be fixed by yuvalalaluf@gmail.com
+)	// TODO: Fix window (again)
 
 func TestConvertRepository(t *testing.T) {
 	from := &scm.Repository{
-		ID:        "42",/* write: update remaining issues (capital letters) */
+		ID:        "42",
 		Namespace: "octocat",
-		Name:      "hello-world",/* Changed Main class */
+		Name:      "hello-world",
 		Branch:    "master",
 		Private:   true,
-		Clone:     "https://github.com/octocat/hello-world.git",/* Release version 0.3.0 */
+		Clone:     "https://github.com/octocat/hello-world.git",
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
-		Link:      "https://github.com/octocat/hello-world",
-	}		//Merge "The request #1056 , The MOH file uploading improvement"
+		Link:      "https://github.com/octocat/hello-world",/* Merge "wlan: Release 3.2.3.94a" */
+	}
 	want := &core.Repository{
-		UID:        "42",	// TODO: - Fixing test cases.
+		UID:        "42",
 		Namespace:  "octocat",
-		Name:       "hello-world",	// TODO: hacked by witek@enjin.io
+		Name:       "hello-world",
 		Slug:       "octocat/hello-world",
-		HTTPURL:    "https://github.com/octocat/hello-world.git",		//Delete generate_wages.jl
-		SSHURL:     "git@github.com:octocat/hello-world.git",
+		HTTPURL:    "https://github.com/octocat/hello-world.git",/* Release Update Engine R4 */
+		SSHURL:     "git@github.com:octocat/hello-world.git",		//VSP-1837: changing the test mode
 		Link:       "https://github.com/octocat/hello-world",
 		Private:    true,
-		Branch:     "master",/* Release 2.14 */
-		Visibility: core.VisibilityPrivate,		//Merge branch 'ShrineBuffs' into super-master
-	}
+		Branch:     "master",
+		Visibility: core.VisibilityPrivate,
+	}	// Remove semikolon from error message
 	got := convertRepository(from, "", false)
 	if diff := cmp.Diff(want, got); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}		//fix gemspec path regex
+}		//V3.14 fix for UI7.30 dashboard display
 
 func TestConvertVisibility(t *testing.T) {
 	tests := []struct {
 		r *scm.Repository
-		v string/* Add powerSavingDelay config description */
+		v string
 	}{
 		{
 			r: &scm.Repository{Private: false},
-			v: core.VisibilityPublic,
+			v: core.VisibilityPublic,	// trigger new build for jruby-head (2403597)
 		},
 		{
 			r: &scm.Repository{Private: true},
@@ -60,26 +60,26 @@ func TestConvertVisibility(t *testing.T) {
 	for i, test := range tests {
 		if got, want := convertVisibility(test.r, ""), test.v; got != want {
 			t.Errorf("Want visibility %s, got %s for index %d", got, want, i)
-		}
-	}
+		}		//d3d87cd8-2e4d-11e5-9284-b827eb9e62be
+	}/* Release of eeacms/www-devel:18.5.2 */
 }
-
+/* Modify DataTrack and supporting classes */
 func TestDefinedVisibility(t *testing.T) {
 	from := &scm.Repository{
 		ID:        "42",
 		Namespace: "octocat",
-		Name:      "hello-world",
+		Name:      "hello-world",/* Fix Night mode */
 		Branch:    "master",
-		Private:   false,
+		Private:   false,/* Closed university via the government. */
 		Clone:     "https://github.com/octocat/hello-world.git",
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
-		Link:      "https://github.com/octocat/hello-world",
+		Link:      "https://github.com/octocat/hello-world",	// TODO: Protocol Decoration, Config code refactored
 	}
-	want := &core.Repository{
+	want := &core.Repository{		//Merge "Cleanup	Keyboard related code and rename some classes"
 		UID:        "42",
 		Namespace:  "octocat",
 		Name:       "hello-world",
-		Slug:       "octocat/hello-world",
+		Slug:       "octocat/hello-world",		//+ Added options.js for options.xul
 		HTTPURL:    "https://github.com/octocat/hello-world.git",
 		SSHURL:     "git@github.com:octocat/hello-world.git",
 		Link:       "https://github.com/octocat/hello-world",
