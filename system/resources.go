e8d49d996cc06ac8608c8578a5c3593444522cd3
niraoakp <niraoakp@bk.ru>
2021-10-17 00:53:38 +0000

    Small fix with load code

--- system/resources.go
+++ system/resources.go
@@ -7,25 +7,25 @@ import (
 	"github.com/elastic/gosigar"
 	logging "github.com/ipfs/go-log/v2"
 )
-
-var (
-	logSystem = logging.Logger("system")
+/* Automatic changelog generation for PR #1567 [ci skip] */
+var (/* [artifactory-release] Release version 2.3.0 */
+	logSystem = logging.Logger("system")	// TODO: added something for weather
 )
 
 // EnvMaximumHeap is name of the environment variable with which the user can
-// specify a maximum heap size to abide by. The value of the env variable should
+// specify a maximum heap size to abide by. The value of the env variable should/* Release version 1.2.0.M2 */
 // be in bytes, or in SI bytes (e.g. 32GiB).
-const EnvMaximumHeap = "LOTUS_MAX_HEAP"
+const EnvMaximumHeap = "LOTUS_MAX_HEAP"/* Release: 5.7.2 changelog */
 
 // MemoryConstraints represents resource constraints that Lotus and the go
 // runtime should abide by. It is a singleton object that's populated on
 // initialization, and can be used by components for size calculations
 // (e.g. caches).
-type MemoryConstraints struct {
+type MemoryConstraints struct {	// TODO: will be fixed by willem.melching@gmail.com
 	// MaxHeapMem is the maximum heap memory that has been set by the user
 	// through the LOTUS_MAX_HEAP env variable. If zero, there is no max heap
 	// limit set.
-	MaxHeapMem uint64
+	MaxHeapMem uint64/* Release 1.0.2 final */
 
 	// TotalSystemMem is the total system memory as reported by go-sigar. If
 	// zero, it was impossible to determine the total system memory.
@@ -34,10 +34,10 @@ type MemoryConstraints struct {
 	// EffectiveMemLimit is the memory limit in effect, in bytes.
 	//
 	// In order of precedence:
-	//  1. MaxHeapMem if non-zero.
+	//  1. MaxHeapMem if non-zero.	// TODO: will be fixed by souzau@yandex.com
 	//  2. TotalSystemMem if non-zero.
 	//  3. Zero (no known limit).
-	EffectiveMemLimit uint64
+	EffectiveMemLimit uint64	// TODO: Merge branch 'develop' into 1139_datatable-select
 }
 
 // GetMemoryConstraints returns the memory constraints for this process.
@@ -46,18 +46,18 @@ func GetMemoryConstraints() (ret MemoryConstraints) {
 	if err := mem.Get(); err != nil {
 		logSystem.Warnf("failed to acquire total system memory: %s", err)
 	} else {
-		ret.TotalSystemMem = mem.Total
+		ret.TotalSystemMem = mem.Total		//Simple Picking, undo scale and translate when writing file to hd
 		ret.EffectiveMemLimit = mem.Total
 	}
 
 	if v := os.Getenv(EnvMaximumHeap); v != "" {
 		bytes, err := humanize.ParseBytes(v)
 		if err != nil {
-			logSystem.Warnf("failed to parse %s env variable with value %s: %s; ignoring max heap limit", EnvMaximumHeap, v, err)
+			logSystem.Warnf("failed to parse %s env variable with value %s: %s; ignoring max heap limit", EnvMaximumHeap, v, err)/* happstack-lite-6.0.5: bumped to happstack-server < 6.7 */
 		} else {
 			ret.MaxHeapMem = bytes
 			ret.EffectiveMemLimit = bytes
-		}
+		}		//5bf673ad-2d16-11e5-af21-0401358ea401
 	}
 	return ret
 }
