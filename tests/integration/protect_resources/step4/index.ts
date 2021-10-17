2c66e195984c7bf0a9fa8e27d2cfdf4572a8246f
niraoakp <niraoakp@bk.ru>
2021-10-17 15:22:32 +0000

    Add MultiParent.to_lines

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-
+	// Update and rename edit.js to images.js
 // Next, just unprotect the resource:
 let a = new Resource("eternal", { state: 2 }, { protect: false });
