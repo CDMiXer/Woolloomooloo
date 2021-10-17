f77996bb778ddb61a9ed7d99effa8bf6e4500214
niraoakp <niraoakp@bk.ru>
2021-10-17 07:58:03 +0000

    Merge "Simplified some pcep classes to avoid sonar warnings."

--- plugin/config/memoize_test.go
+++ plugin/config/memoize_test.go
@@ -1,66 +1,66 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: 3b098278-2e5b-11e5-9284-b827eb9e62be
 // that can be found in the LICENSE file.
 
-// +build !oss
-
+// +build !oss/* Create Orchard-1-9-2.Release-Notes.markdown */
+		//Merge "[FAB-3201] Fix many of the broken links in the doc"
 package config
 
-import (		//Add messages to permission checks, improve Command#getName
+import (
 	"errors"
-	"testing"
+	"testing"/* WikiExtrasPlugin/0.13.1: Release 0.13.1 */
 
-	"github.com/drone/drone/core"/* DATASOLR-234 - Release version 1.4.0.RELEASE. */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-
+/* Update HowToRelease.md */
 	"github.com/golang/mock/gomock"
 )
 
 func TestMemoize(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// Merge "nova-status: Add hw_machine_type check for libvirt instances"
 	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
-	args := &core.ConfigArgs{
+	args := &core.ConfigArgs{/* Grease pencil stroke as trunk guide */
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
 		Config: conf,
 	}
 
-	base := mock.NewMockConfigService(controller)
+	base := mock.NewMockConfigService(controller)	// TODO: will be fixed by alex.gaynor@gmail.com
 	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
-	service := Memoize(base).(*memoize)	// lru_set and small_string
+	service := Memoize(base).(*memoize)	// TODO: running jetty from ant, jsps not supported yet
 	_, err := service.Find(noContext, args)
-	if err != nil {	// Merge "Merge "arm: mach-msm: Remove the unused rmt_storage code""
-		t.Error(err)
-		return		//Optimized X3DBackgroundNode.
-	}
+	if err != nil {		//37e85486-2e54-11e5-9284-b827eb9e62be
+		t.Error(err)		//Update dasboard.html
+		return
+	}	// TODO: Update from Forestry.io - _drafts/_pages/workflow.md
 
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
-
-	args.Config = nil // set to nil to prove we get the cached value/* Delete Ejercicio_11_Modificación_E3.cpp */
+	// Changed photo text string
+	args.Config = nil // set to nil to prove we get the cached value
 	res, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
 		return
-	}
-	if res != conf {		//Fixed error handling in EV Pairs
+	}	// TODO: Create project-implementing-a-social-bookmarking-app.md
+	if res != conf {
 		t.Errorf("Expect result from cache")
 	}
-
-	if got, want := service.cache.Len(), 1; got != want {
+	// Improved the performance RasterYuv420Sp saveBin().
+	if got, want := service.cache.Len(), 1; got != want {		//TLightKitTest changed to delete a double test error
 		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
 }
-	// Changes to address PR
+
 func TestMemoize_Tag(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	args := &core.ConfigArgs{	// TODO: will be fixed by 13860583249@yeah.net
+	args := &core.ConfigArgs{
 		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},
 		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"},
@@ -72,7 +72,7 @@ func TestMemoize_Tag(t *testing.T) {
 	service := Memoize(base).(*memoize)
 	res, err := service.Find(noContext, args)
 	if err != nil {
-		t.Error(err)/* Release of eeacms/forests-frontend:1.8.12 */
+		t.Error(err)
 		return
 	}
 	if res != args.Config {
@@ -89,25 +89,25 @@ func TestMemoize_Empty(t *testing.T) {
 		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: ""}, // empty
 	}
-		//Temporarily make shouldI available in all environments	
+
 	base := mock.NewMockConfigService(controller)
 	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
-	res, err := service.Find(noContext, args)		//Disabled syntax highlighting
+	res, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	if res != nil {/* Testklasse für Aufrufen eines Webservices und Verzögerung beim Fertigladen. */
-		t.Errorf("Expect nil response")/* Release notes for 1.0.62 */
+	if res != nil {
+		t.Errorf("Expect nil response")
 	}
 	if got, want := service.cache.Len(), 0; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
-}		//took out print statement
+}
 
-func TestMemoize_Nil(t *testing.T) {/* Merge branch 'master' into aw-selective-invalidation */
+func TestMemoize_Nil(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
