2881b18d603082ad4d7430350bdb2fb5d790baa9
niraoakp <niraoakp@bk.ru>
2021-10-17 07:46:28 +0000

    Remove htp_tx_req_set_query_string(). Update docs.

--- handler/api/repos/builds/purge_oss.go
+++ handler/api/repos/builds/purge_oss.go
@@ -1,23 +1,23 @@
-// Copyright 2019 Drone IO, Inc./* Add const modifier to PCryptoHash API calls' parameters */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* delta matrix initialization ordered by measurement */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// TODO: Rebuilt index with arby85
+// distributed under the License is distributed on an "AS IS" BASIS,		//SO-1699: moved MRCM importer exporter interfaces to mrcm.core.io
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* spacewar grid */
 
 // +build oss
-
-package builds
+/* Merge "Allow display of trial comments, add experiment list item header" */
+package builds/* Part 1 of manual merge w/ danny-milestone4 */
 
 import (
-	"net/http"
+	"net/http"/* Release version [10.7.2] - prepare */
 
 	"github.com/drone/drone/core"
 )
