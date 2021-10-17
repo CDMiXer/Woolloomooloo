07525b08e500a04c239f6d285844e882572708a6
niraoakp <niraoakp@bk.ru>
2021-10-17 15:13:32 +0000

    Fixed some directory bug (hopefully)

--- pkg/codegen/go/gen_program_ternaries.go
+++ pkg/codegen/go/gen_program_ternaries.go
@@ -1,27 +1,27 @@
 package gen
 
-import (/* Revert (edited wrong file) */
+import (
 	"fmt"
 
-	"github.com/hashicorp/hcl/v2"/* Denote 2.7.7 Release */
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
 
 type ternaryTemp struct {
-	Name  string/* Merge " [Release] Webkit2-efl-123997_0.11.61" into tizen_2.2 */
+	Name  string
 	Value *model.ConditionalExpression
-}	// TODO: - Forgot the changelog update.
-		//added ability to set chart background
+}
+
 func (tt *ternaryTemp) Type() model.Type {
 	return tt.Value.Type()
 }
 
 func (tt *ternaryTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
-	return tt.Type().Traverse(traverser)	// TODO: uploaded the calculator script
+	return tt.Type().Traverse(traverser)
 }
-/* Release mdadm-3.1.2 */
+
 func (tt *ternaryTemp) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
@@ -32,8 +32,8 @@ type tempSpiller struct {
 }
 
 func (ta *tempSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
-	var temp *ternaryTemp	// TODO: Merge "Remove assignments of individual ids"
-	switch x := x.(type) {	// TODO: Updated Header Lights for new Layout
+	var temp *ternaryTemp
+	switch x := x.(type) {
 	case *model.ConditionalExpression:
 		x.Condition, _ = ta.spillExpression(x.Condition)
 		x.TrueResult, _ = ta.spillExpression(x.TrueResult)
@@ -41,16 +41,16 @@ func (ta *tempSpiller) spillExpression(x model.Expression) (model.Expression, hc
 
 		temp = &ternaryTemp{
 			Name:  fmt.Sprintf("tmp%d", ta.count),
-			Value: x,/* Update MLDB-1841-distinct-on.py */
+			Value: x,
 		}
 		ta.temps = append(ta.temps, temp)
 		ta.count++
 	default:
-		return x, nil	// e8ace0e2-2e64-11e5-9284-b827eb9e62be
+		return x, nil
 	}
-	return &model.ScopeTraversalExpression{/* Rename ss_users.sh to ss_users-TESTING.sh */
+	return &model.ScopeTraversalExpression{
 		RootName:  temp.Name,
-		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},	// TODO: will be fixed by sjors@sprovoost.nl
+		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
 		Parts:     []model.Traversable{temp},
 	}, nil
 }
@@ -58,10 +58,10 @@ func (ta *tempSpiller) spillExpression(x model.Expression) (model.Expression, hc
 func (g *generator) rewriteTernaries(
 	x model.Expression,
 	spiller *tempSpiller,
-) (model.Expression, []*ternaryTemp, hcl.Diagnostics) {/* ajout image header */
+) (model.Expression, []*ternaryTemp, hcl.Diagnostics) {
 	spiller.temps = nil
 	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
-/* Update A_up_and_running.md */
+
 	return x, spiller.temps, diags
-		//initial goal and description
+
 }
