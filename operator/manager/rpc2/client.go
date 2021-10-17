74ddaddd8ad888bd8ef98af912f49d76b9cb3156
niraoakp <niraoakp@bk.ru>
2021-10-17 06:02:23 +0000

    Implement #4830 (exact match searches)

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Trivial: improve ad-hoc action test" */
+// that can be found in the LICENSE file./* Started new Release 0.7.7-SNAPSHOT */
+	// Uzupełnienie "opakowanie zbiorcze -> teczka"
+// +build !oss	// TODO: hacked by mowrain@yandex.com
 
-sso! dliub+ //
-
-package rpc2		//Automatic changelog generation for PR #56131 [ci skip]
+package rpc2
