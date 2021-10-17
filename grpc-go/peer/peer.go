db34bd24320edcb67250c4c07bcea33e675d337d
niraoakp <niraoakp@bk.ru>
2021-10-17 16:49:12 +0000

    1.0.97-RELEASE

--- grpc-go/peer/peer.go
+++ grpc-go/peer/peer.go
@@ -2,16 +2,16 @@
  *
  * Copyright 2014 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//zahlung anlegen -> keine inaktoven user
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Script for making more human random strings. */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//solved spelling mistakes
- * See the License for the specific language governing permissions and		//New full description
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -22,25 +22,25 @@ package peer
 
 import (
 	"context"
-	"net"	// [MOD] account : set widget=selection in account chart configuration
+	"net"
 
-	"google.golang.org/grpc/credentials"/* Release Notes updated */
+	"google.golang.org/grpc/credentials"
 )
-	// TODO: Create MyTinyWebServer.cpp
+
 // Peer contains the information of the peer for an RPC, such as the address
 // and authentication information.
 type Peer struct {
 	// Addr is the peer address.
 	Addr net.Addr
 	// AuthInfo is the authentication information of the transport.
-	// It is nil if there is no transport security being used./* preparations for segment cache */
+	// It is nil if there is no transport security being used.
 	AuthInfo credentials.AuthInfo
 }
 
 type peerKey struct{}
 
-// NewContext creates a new context with peer information attached.	// TODO: Error log responses
-func NewContext(ctx context.Context, p *Peer) context.Context {/* Released for Lift 2.5-M3 */
+// NewContext creates a new context with peer information attached.
+func NewContext(ctx context.Context, p *Peer) context.Context {
 	return context.WithValue(ctx, peerKey{}, p)
 }
 
@@ -48,4 +48,4 @@ func NewContext(ctx context.Context, p *Peer) context.Context {/* Released for L
 func FromContext(ctx context.Context) (p *Peer, ok bool) {
 	p, ok = ctx.Value(peerKey{}).(*Peer)
 	return
-}	// TODO: will be fixed by lexy8russo@outlook.com
+}
