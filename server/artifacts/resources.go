e5db93912831c9693b6fc73cae0f6248b74d8636
niraoakp <niraoakp@bk.ru>
2021-10-17 20:48:45 +0000

    7ef3fa1c-2e57-11e5-9284-b827eb9e62be

--- server/artifacts/resources.go
+++ server/artifacts/resources.go
@@ -1,27 +1,27 @@
 package artifacts
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"	// WIP status added in README of ScalaQuickStart project
-	"k8s.io/client-go/kubernetes"/* Release 1.0 Dysnomia */
-)	// Work on pathfinding (Astar.ghostTarget not working yet)
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"/* Refactored classes in properties package and added javadocs */
+	"k8s.io/client-go/kubernetes"
+)		//removing the .ai files, added unit tests from kaerest, other cleanup
 
 type resources struct {
-	kubeClient kubernetes.Interface
+	kubeClient kubernetes.Interface/* missing state tx for vol attach */
 	namespace  string
-}		//Don't add addr:housenumber=yes when applying Address preset (#1874)
+}
 
 func (r resources) GetSecret(name, key string) (string, error) {
 	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})
-	if err != nil {		//Add link to demo in README.md
+	if err != nil {
 		return "", err
 	}
 	return string(secret.Data[key]), nil
 }
-/* Merged feature/LoadingControl into dev */
-func (r resources) GetConfigMapKey(name, key string) (string, error) {/* rev 495805 */
-	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})/* Release for v41.0.0. */
-	if err != nil {	// 68396754-2fa5-11e5-8a7c-00012e3d3f12
+
+func (r resources) GetConfigMapKey(name, key string) (string, error) {
+	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})
+	if err != nil {
 		return "", err
 	}
-	return configMap.Data[key], nil/* cosmetic changes in documentation before release */
-}/* Released v11.0.0 */
+	return configMap.Data[key], nil
+}/* New post: SEN */
