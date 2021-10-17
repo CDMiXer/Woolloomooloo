75d549aede61c86ee925391590ab27f584985e9c
niraoakp <niraoakp@bk.ru>
2021-10-17 21:00:12 +0000

    Upss. Descomento.

--- plugin/converter/remote_oss.go
+++ plugin/converter/remote_oss.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by hugomrdias@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,21 +9,21 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: hacked by arajasek94@gmail.com
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* [artifactory-release] Release version 3.1.0.BUILD */
+// +build oss
 
 package converter
 
 import (
 	"time"
-	// Enabled saving and loading of script settings.
-	"github.com/drone/drone/core"		//Delete JWZPhotoViewer.xcscheme
+
+	"github.com/drone/drone/core"
 )
 
-// Remote returns a conversion service that converts the	// TODO: StatusIcon demo
+// Remote returns a conversion service that converts the
 // configuration file using a remote http service.
 func Remote(endpoint, signer, extension string, skipVerify bool, timeout time.Duration) core.ConvertService {
-	return new(noop)	// TODO: hacked by mail@bitpshr.net
+	return new(noop)
 }
