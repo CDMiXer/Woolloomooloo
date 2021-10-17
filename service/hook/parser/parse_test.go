7a2d49b49cb564af48b23a685bca90e70c3a00df
niraoakp <niraoakp@bk.ru>
2021-10-17 01:10:48 +0000

    Rebuilt index with uxster

--- service/hook/parser/parse_test.go
+++ service/hook/parser/parse_test.go
@@ -1,5 +1,5 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: .Bat para iniciar el servicio en local
-	// TODO: will be fixed by witek@enjin.io
-package parser	// TODO: removed old starterpack
+// that can be found in the LICENSE file.
+
+package parser
