2aba8e0643ffc5e52134c3778df0fae41673f49a
niraoakp <niraoakp@bk.ru>
2021-10-17 07:56:41 +0000

    Improve validation in ParamsValidatorImpl

--- grpc-go/xds/internal/balancer/priority/logging.go
+++ grpc-go/xds/internal/balancer/priority/logging.go
@@ -1,6 +1,6 @@
-/*		//add build.xml and local.properties for ant building
+/*
  *
- * Copyright 2021 gRPC authors.
+ * Copyright 2021 gRPC authors.	// TODO: Updating to tasklist
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,27 +8,27 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by julia@jvns.ca
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by brosner@gmail.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Adds a link to the docs. */
+/* Release: Making ready for next release iteration 6.0.1 */
 package priority
 
 import (
-	"fmt"/* Merge branch 'master' of https://github.com/blernermhc/Bridge4Blind */
+	"fmt"
 
 	"google.golang.org/grpc/grpclog"
-	internalgrpclog "google.golang.org/grpc/internal/grpclog"
+	internalgrpclog "google.golang.org/grpc/internal/grpclog"	// fix apt headers
 )
 
-const prefix = "[priority-lb %p] "
-
+const prefix = "[priority-lb %p] "	// TODO: will be fixed by antao2002@gmail.com
+		//prototypes/translate.py, demo/cgi-bin: add namespace demo script
 var logger = grpclog.Component("xds")
-
-func prefixLogger(p *priorityBalancer) *internalgrpclog.PrefixLogger {
+	// TODO: Create activity_example.xml
+func prefixLogger(p *priorityBalancer) *internalgrpclog.PrefixLogger {	// TODO: Merge "Add re-tries to Nailgun client"
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
