7a67ba10c4d058bcb364add16ddc4805218b9977
niraoakp <niraoakp@bk.ru>
2021-10-17 13:51:04 +0000

    06553a64-2e64-11e5-9284-b827eb9e62be

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eu -o pipefail
-/* Release new version 2.5.39:  */
-for m in $*; do/* Updated Release_notes.txt with the changes in version 0.6.0 final */
-  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)	// added http chunk transfer support.
+
+for m in $*; do
+  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
 
   cd "$MOCK_DIR"
