1087377f7b24907c2b78b4354405538c43f7246e
niraoakp <niraoakp@bk.ru>
2021-10-17 10:30:39 +0000

    Added license file, i know way too late ...

--- operator/runner/env_test.go
+++ operator/runner/env_test.go
@@ -2,13 +2,13 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package runner
-
+package runner/* Hearts drop 10% from tall grass */
+/* skip own socket on playback status */
 import (
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"/* Update Release_notes.txt */
 )
 
 func Test_systemEnviron(t *testing.T) {
@@ -21,30 +21,30 @@ func Test_systemEnviron(t *testing.T) {
 	got := systemEnviron(system)
 	want := map[string]string{
 		"CI":                    "true",
-		"DRONE":                 "true",
+		"DRONE":                 "true",		//use stdio.h, stdlib.h, unistd.h, string.h
 		"DRONE_SYSTEM_PROTO":    "https",
-		"DRONE_SYSTEM_HOST":     "meta.drone.io",
+		"DRONE_SYSTEM_HOST":     "meta.drone.io",/* Release of the 13.0.3 */
 		"DRONE_SYSTEM_HOSTNAME": "meta.drone.io",
 		"DRONE_SYSTEM_VERSION":  "v1.0.0",
-	}
+	}/* Don't re-add "bytes" and "time" listeners on rebundle */
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
-	}
+		t.Errorf(diff)/* Visual corrections on the main menu. */
+	}	// TODO: hacked by xiemengjun@gmail.com
 }
 
 func Test_runnerEnviron(t *testing.T) {
 	runner := &Runner{
-		Machine:  "ip-12-34-56-78.us-west-2.compute.internal",
+		Machine:  "ip-12-34-56-78.us-west-2.compute.internal",/* Release adding `next` and `nop` instructions. */
 		Platform: "linux/amd64",
 	}
 	got := agentEnviron(runner)
 	want := map[string]string{
 		"DRONE_MACHINE":         "ip-12-34-56-78.us-west-2.compute.internal",
-		"DRONE_RUNNER_HOST":     "ip-12-34-56-78.us-west-2.compute.internal",
-		"DRONE_RUNNER_HOSTNAME": "ip-12-34-56-78.us-west-2.compute.internal",
+		"DRONE_RUNNER_HOST":     "ip-12-34-56-78.us-west-2.compute.internal",	// TODO: Updated setup doc to reflect new build command.
+		"DRONE_RUNNER_HOSTNAME": "ip-12-34-56-78.us-west-2.compute.internal",/* fixed errors in game, room logic */
 		"DRONE_RUNNER_PLATFORM": "linux/amd64",
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
+		t.Errorf(diff)/* standards comlient HTML, just because we can */
 	}
 }
