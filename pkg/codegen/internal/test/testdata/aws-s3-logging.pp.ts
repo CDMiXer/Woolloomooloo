49425654682c3df8f7b78f89075452172a7e6d28
niraoakp <niraoakp@bk.ru>
2021-10-17 22:22:10 +0000

    Merge branch 'master' into LIC-595

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -3,6 +3,6 @@ import * as aws from "@pulumi/aws";
 
 const logs = new aws.s3.Bucket("logs", {});
 const bucket = new aws.s3.Bucket("bucket", {loggings: [{
-    targetBucket: logs.bucket,
+    targetBucket: logs.bucket,/* buildkite-agent 1.0-beta.25 */
 }]});
 export const targetBucket = bucket.loggings[0].targetBucket;
