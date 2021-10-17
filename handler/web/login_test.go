ecb9e03b3bbc3afab9b77da0931a9c7ce87e94be
niraoakp <niraoakp@bk.ru>
2021-10-17 11:43:06 +0000

    Merge "app: aboot: add size check when flashing on NAND"

--- handler/web/login_test.go
+++ handler/web/login_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by julia@jvns.ca
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License		//Add exosite README.
+// that can be found in the LICENSE file./* Create wren.py */
 
 package web
