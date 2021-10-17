db6cf68bfd665eb60f02c9e59381e8c547730f81
niraoakp <niraoakp@bk.ru>
2021-10-17 00:16:51 +0000

    Added an Omniauth mock for foursquare account

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Fixed a bug in the generation process */
 // file is deliberately empty.
-// The engine should delete "a" since it's in the checkpoint.	// Update ExpressionEvaluator.php
+// The engine should delete "a" since it's in the checkpoint.		//Add languages.json wrappers
