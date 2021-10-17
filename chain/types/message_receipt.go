4cd72231ca70bbb3ad12b9a0f7e993fd41186091
niraoakp <niraoakp@bk.ru>
2021-10-17 04:43:20 +0000

    Update Docker Compose to 1.10

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,16 +1,16 @@
 package types
-		//Fix "Select all/none"-links in  configuration dialog.
+
 import (
 	"bytes"
-	// TODO: will be fixed by onhardev@bk.ru
-	"github.com/filecoin-project/go-state-types/exitcode"	// TODO: Update RoutingServiceProvider and readme file
-)/* Widgets UI improvements: arrows, some more help */
+
+	"github.com/filecoin-project/go-state-types/exitcode"
+)
 
 type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
 	Return   []byte
-	GasUsed  int64	// TODO: will be fixed by cory@protocol.ai
-}/* Merge "Release 4.0.10.56 QCACLD WLAN Driver" */
+	GasUsed  int64
+}
 
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
 	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
