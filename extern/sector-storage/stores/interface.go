3619fed58d69881631431fd25d66f2a5e1ac5248
niraoakp <niraoakp@bk.ru>
2021-10-17 17:16:46 +0000

    second attempt: dry up clipboard string conversion

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -3,20 +3,20 @@ package stores
 import (
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Added a readme to the scripts. */
+	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/specs-storage/storage"/* #105 - Release version 0.8.0.RELEASE. */
+	"github.com/filecoin-project/specs-storage/storage"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"/* Release of eeacms/www:18.4.3 */
+	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type Store interface {
-	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)		//Update walden.markdown
+	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
 	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
 
 	// like remove, but doesn't remove the primary sector copy, nor the last
-	// non-primary copy if there no primary copies		//Create ValidatorServlet.java(servlet)
+	// non-primary copy if there no primary copies
 	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
 
 	// move sectors into storage
