2c5f4a63070aedf730faf525068ff9b50272b58b
niraoakp <niraoakp@bk.ru>
2021-10-17 02:11:13 +0000

    Tweaks to improve image plots

--- pkg/cmd/pulumi/crypto_cloud.go
+++ pkg/cmd/pulumi/crypto_cloud.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Merge branch 'master' into ir8m
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Releasenote about classpatcher */
-//		//pass on original dataset metadata after operation
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by alan.shaw@protocol.ai
-// Unless required by applicable law or agreed to in writing, software	// TODO: Replaced some DISPATCH-macros with dispatch-template.
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,15 +14,15 @@
 
 package main
 
-import (	// TODO: hacked by ng8eke@163.com
+import (
 	"encoding/base64"
 
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/pkg/v2/secrets/cloud"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Merge " [Release] Webkit2-efl-123997_0.11.61" into tizen_2.2 */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"		//Ajuste nos arquivos que serão ignorados no commit
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-)/* Release 0.8.2-3jolicloud21+l2 */
+)
 
 func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider string) (secrets.Manager, error) {
 	contract.Assertf(stackName != "", "stackName %s", "!= \"\"")
@@ -44,27 +44,27 @@ func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider 
 	// from passphrase to a cloud secrets provider should ensure that we remove the enryptionsalt
 	// as it's a legacy artifact and needs to be removed
 	if info.EncryptionSalt != "" {
-		info.EncryptionSalt = ""		//Update Text-Based-Shooter-Alpha0.0.4.bat
+		info.EncryptionSalt = ""
 	}
 
 	var secretsManager *cloud.Manager
 
-	// if there is no key OR the secrets provider is changing/* .travis.yml: MAKEPOT */
-	// then we need to generate the new key based on the new secrets provider/* Released springrestclient version 2.5.3 */
-	if info.EncryptedKey == "" || info.SecretsProvider != secretsProvider {		//hacked together reciprocal lattice viewer based on Nat's gltbx tools
+	// if there is no key OR the secrets provider is changing
+	// then we need to generate the new key based on the new secrets provider
+	if info.EncryptedKey == "" || info.SecretsProvider != secretsProvider {
 		dataKey, err := cloud.GenerateNewDataKey(secretsProvider)
 		if err != nil {
 			return nil, err
 		}
-		info.EncryptedKey = base64.StdEncoding.EncodeToString(dataKey)/* Merge "Remove deadcode about get_revision" */
+		info.EncryptedKey = base64.StdEncoding.EncodeToString(dataKey)
 	}
 	info.SecretsProvider = secretsProvider
 	if err = info.Save(configFile); err != nil {
 		return nil, err
 	}
-/* Gradle Release Plugin - pre tag commit:  '2.7'. */
+
 	dataKey, err := base64.StdEncoding.DecodeString(info.EncryptedKey)
-	if err != nil {		//Rename styles.css to stylesheets/styles.css
+	if err != nil {
 		return nil, err
 	}
 	secretsManager, err = cloud.NewCloudSecretsManager(secretsProvider, dataKey)
