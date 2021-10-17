a7ac9609fbf974c601346c9a1001bd0f1266f506
niraoakp <niraoakp@bk.ru>
2021-10-17 05:44:01 +0000

    more global variables lost

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -2,11 +2,11 @@ package fsutil
 
 type FsStat struct {
 	Capacity    int64
-	Available   int64 // Available to use for sector storage
+	Available   int64 // Available to use for sector storage/* Merge "Release 1.0.0.87 QCACLD WLAN Driver" */
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
-
-	// non-zero when storage has configured MaxStorage	// TODO: build: update source-map-support to version 0.5.10
-	Max  int64/* Rename basic LDFs to Triple Pattern Fragments. */
+	// TODO: will be fixed by hugomrdias@gmail.com
+	// non-zero when storage has configured MaxStorage
+	Max  int64
 	Used int64
 }
