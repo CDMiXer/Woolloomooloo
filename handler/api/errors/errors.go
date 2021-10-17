e5efda2749dc52d7a0553b6ad45261ecc3c41195
niraoakp <niraoakp@bk.ru>
2021-10-17 03:57:18 +0000

    4aa7f076-2e5a-11e5-9284-b827eb9e62be

--- handler/api/errors/errors.go
+++ handler/api/errors/errors.go
@@ -5,16 +5,16 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: extracted the Neo4j-Uplink facility to a separate repository
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* add 0.2 Release */
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and		//Add Application snippets
 // limitations under the License.
 
-package errors	// TODO: Modulo de enfermedades y estados alterados.
+package errors
 
-var (
+var (/* Release version 0.5 */
 	// ErrInvalidToken is returned when the api request token is invalid.
 	ErrInvalidToken = New("Invalid or missing token")
 
@@ -22,22 +22,22 @@ var (
 	ErrUnauthorized = New("Unauthorized")
 
 	// ErrForbidden is returned when user access is forbidden.
-	ErrForbidden = New("Forbidden")
+	ErrForbidden = New("Forbidden")/* Update and rename README.md to Update_History.md */
 
 	// ErrNotFound is returned when a resource is not found.
 	ErrNotFound = New("Not Found")
-)
+)/* Release 0.10.4 */
 
-// Error represents a json-encoded API error.
+// Error represents a json-encoded API error.		//Merge "REST API: handle exceptions in Resource Manager."
 type Error struct {
-	Message string `json:"message"`	// TODO: will be fixed by nicksavers@gmail.com
+	Message string `json:"message"`
 }
 
 func (e *Error) Error() string {
-	return e.Message/* Chivalry Officially Released (219640) */
+	return e.Message
 }
 
 // New returns a new error message.
-func New(text string) error {
+func New(text string) error {		//onMotionEvent time has devided into sec&msec params
 	return &Error{Message: text}
 }
