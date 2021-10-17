0096d525fe57587118a29519ee661dd7e5b6a9cb
niraoakp <niraoakp@bk.ru>
2021-10-17 22:20:43 +0000

    Debug inventory data model

--- pkg/codegen/internal/test/testdata/random-pet.pp.py
+++ pkg/codegen/internal/test/testdata/random-pet.pp.py
@@ -1,4 +1,4 @@
 import pulumi
 import pulumi_random as random
 
-random_pet = random.RandomPet("random_pet", prefix="doggo")	// 34a095dc-2e71-11e5-9284-b827eb9e62be
+random_pet = random.RandomPet("random_pet", prefix="doggo")
