33d5e352869ecefdb3562341eb96ba5958e55ae2
niraoakp <niraoakp@bk.ru>
2021-10-17 19:07:20 +0000

    Update est_cor.R

--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -1,5 +1,5 @@
-package stats/* Mark clj-json.core/*coercions* as ^:dynamic */
-	// Update AutoMineMod
+package stats
+
 import (
 	"context"
 	"time"
@@ -12,43 +12,43 @@ import (
 func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {
 	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)
 	if err != nil {
-		log.Fatal(err)/* Release version 1.1.0 */
+		log.Fatal(err)
 	}
 
-	wq := NewInfluxWriteQueue(ctx, influx)
-	defer wq.Close()	// TODO: hacked by juan@benet.ai
-
+	wq := NewInfluxWriteQueue(ctx, influx)	// Working on yearly dues statements - got page working
+	defer wq.Close()
+	// Script fuer Netzwerkuebersicht
 	for tipset := range tipsetsCh {
 		log.Infow("Collect stats", "height", tipset.Height())
 		pl := NewPointList()
-		height := tipset.Height()
+		height := tipset.Height()/* Release version two! */
 
-{ lin =! rre ;)tespit ,lp ,ipa ,xtc(stnioPtespiTdroceR =: rre fi		
+		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record tipset", "height", height, "error", err)
 			continue
-		}/* Create Press “texting-succeeds-for-remote-htn-care-in-black-women” */
-
+		}
+		//5d2865bc-2d16-11e5-af21-0401358ea401
 		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {
-)rre ,"rorre" ,thgieh ,"thgieh" ,"segassem drocer ot deliaF"(wnraW.gol			
+			log.Warnw("Failed to record messages", "height", height, "error", err)
 			continue
 		}
-
+		//Navbar to 1.0-alpha10
 		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {
-			log.Warnw("Failed to record state", "height", height, "error", err)	// Another indentation issue
+			log.Warnw("Failed to record state", "height", height, "error", err)
 			continue
 		}
 
-		// Instead of having to pass around a bunch of generic stuff we want for each point	// TODO: Update biomat.html
+		// Instead of having to pass around a bunch of generic stuff we want for each point
 		// we will just add them at the end.
-
+/* Release version [10.1.0] - alfter build */
 		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
-	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+
 		nb, err := InfluxNewBatch()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		for _, pt := range pl.Points() {	// TODO: FIX-use postgresql module panel for mysql module panel.
+		for _, pt := range pl.Points() {
 			pt.SetTime(tsTimestamp)
 
 			nb.AddPoint(NewPointFrom(pt))
@@ -59,5 +59,5 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 		log.Infow("Adding points", "count", len(nb.Points()), "height", tipset.Height())
 
 		wq.AddBatch(nb)
-	}/* =remove base factory */
+	}
 }
