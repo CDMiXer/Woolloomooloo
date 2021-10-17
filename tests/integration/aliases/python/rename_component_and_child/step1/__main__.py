189dff1579027ea4645c3eab4346f84b49f97207
niraoakp <niraoakp@bk.ru>
2021-10-17 03:48:41 +0000

    Update LookupCommands.java

--- tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
@@ -1,15 +1,15 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by fkautz@pseudocode.cc
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-	// rename register to singleton in Casser
-class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):/* Update KeyReleaseTrigger.java */
-        super().__init__("my:module:Resource", name, None, opts)	// TODO: Build place holder home page
 
-# Scenario #5 - composing #1 and #3 and making both changes at the same time
-class ComponentFive(ComponentResource):
+class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:ComponentFive", name, None, opts)/* Socket.io NPM update and TTA 1.0.2 */
-        res1 = Resource1("otherchild", ResourceOptions(parent=self))	// TODO: hacked by cory@protocol.ai
+        super().__init__("my:module:Resource", name, None, opts)
+
+# Scenario #5 - composing #1 and #3 and making both changes at the same time/* rev 737645 */
+class ComponentFive(ComponentResource):	// TODO: hacked by souzau@yandex.com
+    def __init__(self, name, opts=None):
+        super().__init__("my:module:ComponentFive", name, None, opts)
+        res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
 comp5 = ComponentFive("comp5")
