56b53cb55fc116cdddca748ed668ca1e057a97e2
niraoakp <niraoakp@bk.ru>
2021-10-17 17:29:46 +0000

    Update saraiva.sql

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.py
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.py
@@ -1,30 +1,30 @@
 import pulumi
 import pulumi_aws as aws
-
+/* Disable Metrics/AbcSize */
 # Create a new security group for port 80.
 security_group = aws.ec2.SecurityGroup("securityGroup", ingress=[aws.ec2.SecurityGroupIngressArgs(
     protocol="tcp",
-    from_port=0,/* Release notes moved on top + link to the 0.1.0 branch */
-    to_port=0,/* When rolling back, just set the Formation to the old Release's formation. */
-    cidr_blocks=["0.0.0.0/0"],/* Technical 2 blog Draft */
-)])		//Fix #925 (Bulk convert (2 files) fails with the errors below.)
+    from_port=0,	// TODO: hacked by mowrain@yandex.com
+    to_port=0,
+    cidr_blocks=["0.0.0.0/0"],
+)])/* Merge branch 'master' into add/instant-preview-media */
 ami = aws.get_ami(filters=[aws.GetAmiFilterArgs(
         name="name",
         values=["amzn-ami-hvm-*-x86_64-ebs"],
     )],
-    owners=["137112412989"],/* Merge "Release 5.3.0 (RC3)" */
+    owners=["137112412989"],
     most_recent=True)
-# Create a simple web server using the startup script for the instance./* Release of eeacms/jenkins-slave-eea:3.21 */
-server = aws.ec2.Instance("server",
-    tags={
-        "Name": "web-server-www",
+# Create a simple web server using the startup script for the instance.
+server = aws.ec2.Instance("server",/* Release of eeacms/plonesaas:5.2.1-54 */
+    tags={/* Release model 9 */
+        "Name": "web-server-www",	// added stringify
     },
     instance_type="t2.micro",
-    security_groups=[security_group.name],
+    security_groups=[security_group.name],	// TODO: 70de14a4-2fa5-11e5-97a6-00012e3d3f12
     ami=ami.id,
-    user_data="""#!/bin/bash
-echo "Hello, World!" > index.html/* Release 1.10.7 */
-nohup python -m SimpleHTTPServer 80 &
-""")		//Update hypothesis from 3.65.3 to 3.66.1
-pulumi.export("publicIp", server.public_ip)	// TODO: dashboard style updates #227
+    user_data="""#!/bin/bash	// TODO: Ignore .c and .h files only in src directory
+echo "Hello, World!" > index.html
+nohup python -m SimpleHTTPServer 80 &		//Add AUTHORS entry
+""")
+pulumi.export("publicIp", server.public_ip)
 pulumi.export("publicHostName", server.public_dns)
