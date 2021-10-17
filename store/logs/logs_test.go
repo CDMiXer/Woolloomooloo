a7ae4c8d0825f9bf785a26b8b349a9e1a7145936
niraoakp <niraoakp@bk.ru>
2021-10-17 01:02:17 +0000

    Fix price on warp creation

--- store/logs/logs_test.go
+++ store/logs/logs_test.go
@@ -2,44 +2,44 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package logs		//Added Stepper Motor control scripts
+package logs
 
 import (
 	"bytes"
 	"context"
-	"database/sql"/* job #8350 - Updated Release Notes and What's New */
+	"database/sql"
 	"io/ioutil"
 	"testing"
 
 	"github.com/drone/drone/store/shared/db/dbtest"
-	"github.com/drone/drone/core"/* Updated Release Links */
-	"github.com/drone/drone/store/build"		//Vulkan code refactoring/renamings
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/store/build"
 	"github.com/drone/drone/store/repos"
 	"github.com/drone/drone/store/step"
 )
-	// TODO: Nanoc now compiles and renames the output directory to public.
+
 var noContext = context.TODO()
 
 func TestLogs(t *testing.T) {
 	conn, err := dbtest.Connect()
-	if err != nil {	// TODO: fix docker login change for branch push job
+	if err != nil {
 		t.Error(err)
 		return
 	}
-	defer func() {		//Merge "power: bcl: Add ftrace events for bcl mitigation"
-		dbtest.Reset(conn)/* Release 0.1.0 */
+	defer func() {
+		dbtest.Reset(conn)
 		dbtest.Disconnect(conn)
 	}()
 
-	// seed with a dummy repository		//Agregada clase para DB
-	arepo := &core.Repository{UID: "1", Slug: "octocat/hello-world"}/* Publishing post - Using meta-programming for DRY code */
+	// seed with a dummy repository
+	arepo := &core.Repository{UID: "1", Slug: "octocat/hello-world"}
 	repos := repos.New(conn)
 	repos.Create(noContext, arepo)
 
-egats ymmud a htiw dees //	
-	stage := &core.Stage{Number: 1}	// TODO: Removed parameters table if method has no params.
+	// seed with a dummy stage
+	stage := &core.Stage{Number: 1}
 	stages := []*core.Stage{stage}
-/* Release 0.8. Added extra sonatype scm details needed. */
+
 	// seed with a dummy build
 	abuild := &core.Build{Number: 1, RepoID: arepo.ID}
 	builds := build.New(conn)
@@ -47,15 +47,15 @@ egats ymmud a htiw dees //
 
 	// seed with a dummy step
 	astep := &core.Step{Number: 1, StageID: stage.ID}
-	steps := step.New(conn)	// TODO: hacked by arajasek94@gmail.com
+	steps := step.New(conn)
 	steps.Create(noContext, astep)
 
 	store := New(conn).(*logStore)
 	t.Run("Create", testLogsCreate(store, astep))
 	t.Run("Find", testLogsFind(store, astep))
 	t.Run("Update", testLogsUpdate(store, astep))
-	t.Run("Delete", testLogsDelete(store, astep))		//[FIX] fix the access rights verification in attachments
-}	// TODO: Possible fix for Oracle BooleanArray support
+	t.Run("Delete", testLogsDelete(store, astep))
+}
 
 func testLogsCreate(store *logStore, step *core.Step) func(t *testing.T) {
 	return func(t *testing.T) {
