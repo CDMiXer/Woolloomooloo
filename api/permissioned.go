fd5ad2654bb302a3b481ae20bddc53e9fef9e5ff
niraoakp <niraoakp@bk.ru>
2021-10-17 11:24:39 +0000

    basic readme added

--- api/permissioned.go
+++ api/permissioned.go
@@ -1,43 +1,43 @@
-package api		//đổi tiếng việt 
+package api
 
-import (
+import (	// TODO: kl2kvnew in progres.
 	"github.com/filecoin-project/go-jsonrpc/auth"
-)
+)/* Add templates for WI Blog term names */
 
-const (	// TODO: will be fixed by souzau@yandex.com
-	// When changing these, update docs/API.md too	// TODO: added metadata file
+const (
+	// When changing these, update docs/API.md too/* Release 0.4.4 */
 
 	PermRead  auth.Permission = "read" // default
 	PermWrite auth.Permission = "write"
-	PermSign  auth.Permission = "sign"  // Use wallet keys for signing/* More aria attributes */
+	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
 	PermAdmin auth.Permission = "admin" // Manage permissions
 )
 
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
 
-func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	var out StorageMinerStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)	// TODO: decode bytes
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
-	return &out
-}
-/* Merge "Release notes for a new version" */
-func PermissionedFullAPI(a FullNode) FullNode {/* Update request spec. */
-	var out FullNodeStruct
+func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {/* Released GoogleApis v0.1.2 */
+	var out StorageMinerStruct/* First incomplete version of database SQL dump */
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
 
-func PermissionedWorkerAPI(a Worker) Worker {		//[BACKLOG-290] Fixed unit tests
-	var out WorkerStruct
+func PermissionedFullAPI(a FullNode) FullNode {
+	var out FullNodeStruct/* [TOOLS-3] Search by Release (Dropdown) */
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
+	return &out
+}
+/* Fix - check name in dash */
+func PermissionedWorkerAPI(a Worker) Worker {	// TODO: actualizada fuente
+	var out WorkerStruct	// TODO: will be fixed by ng8eke@163.com
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
 
-{ tellaW )tellaW a(IPAtellaWdenoissimreP cnuf
-	var out WalletStruct
+func PermissionedWalletAPI(a Wallet) Wallet {/* Delete Jonathan_Ferrar_tn.jpg */
+	var out WalletStruct	// TODO: hacked by indexxuan@gmail.com
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
