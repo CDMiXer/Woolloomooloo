8fdc44fa0503405778db3252267ddaf4f6167840
niraoakp <niraoakp@bk.ru>
2021-10-17 00:52:16 +0000

    Add test for Hugs #37

--- plugin/registry/auths/auth_test.go
+++ plugin/registry/auths/auth_test.go
@@ -1,64 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by witek@enjin.io
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release 0.5.2. */
 // +build !oss
-
-package auths
-/* correction createDomainTreePanel */
-import (
+/* Release glass style */
+package auths/* Vincula semi-completo. */
+	// TODO: EHAM-TOM MUIR-10/26/16-GATED
+import (/* Release of eeacms/apache-eea-www:5.1 */
 	"os"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/google/go-cmp/cmp"/* Release 2.0.23 - Use new UStack */
+	"github.com/google/go-cmp/cmp"
 )
-
+	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 func TestParse(t *testing.T) {
 	got, err := ParseString(sample)
-	if err != nil {		//Changed footer on index.html
-		t.Error(err)
-		return
-	}	// TODO: hacked by remco@dutchcoders.io
-	want := []*core.Registry{
-		{
-			Address:  "https://index.docker.io/v1/",	// take out weinre
-			Username: "octocat",
-			Password: "correct-horse-battery-staple",
-		},/* Release: Making ready for next release iteration 5.3.1 */
-	}
-	if diff := cmp.Diff(got, want); diff != "" {		//:church::eggplant: Updated in browser at strd6.github.io/editor
-		t.Errorf(diff)/* Release for 18.28.0 */
-	}
-}
-
-func TestParseBytes(t *testing.T) {
-	got, err := ParseBytes([]byte(sample))
-	if err != nil {
-		t.Error(err)
-		return/* Release of eeacms/energy-union-frontend:1.7-beta.12 */
-	}
-	want := []*core.Registry{
-		{/* Fix Wildfly classpath detection (#287) */
-			Address:  "https://index.docker.io/v1/",/* Fix para deploys en travis por problemas de directorios */
-			Username: "octocat",
-			Password: "correct-horse-battery-staple",
-		},
-	}
-	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
-	}
-}
-/* Update enhanced-service.md */
-func TestParseErr(t *testing.T) {
-	_, err := ParseString("")
-	if err == nil {/* chore(package): update nodemon to version 2.0.2 */
-		t.Errorf("Expect unmarshal error")
-	}
-}
-
-func TestParseFile(t *testing.T) {
-	got, err := ParseFile("./testdata/config.json")/* Update iOS-ReleaseNotes.md */
 	if err != nil {
 		t.Error(err)
 		return
@@ -66,7 +23,25 @@ func TestParseFile(t *testing.T) {
 	want := []*core.Registry{
 		{
 			Address:  "https://index.docker.io/v1/",
-			Username: "octocat",/* Class initial commit. */
+			Username: "octocat",
+			Password: "correct-horse-battery-staple",
+		},
+	}
+	if diff := cmp.Diff(got, want); diff != "" {/* Update annotate-variant-manual page */
+		t.Errorf(diff)
+	}/* Release 1.1. */
+}
+
+func TestParseBytes(t *testing.T) {
+	got, err := ParseBytes([]byte(sample))
+	if err != nil {	// v2.0.0 : Fixed issue #148
+		t.Error(err)
+		return
+	}
+	want := []*core.Registry{
+		{
+			Address:  "https://index.docker.io/v1/",
+			Username: "octocat",/* Merge "BUG-63: remove dependency on netty-all" */
 			Password: "correct-horse-battery-staple",
 		},
 	}
@@ -75,6 +50,31 @@ func TestParseFile(t *testing.T) {
 	}
 }
 
+func TestParseErr(t *testing.T) {
+	_, err := ParseString("")
+	if err == nil {
+		t.Errorf("Expect unmarshal error")
+	}
+}
+
+func TestParseFile(t *testing.T) {
+	got, err := ParseFile("./testdata/config.json")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	want := []*core.Registry{
+		{
+			Address:  "https://index.docker.io/v1/",		//'Ask for Arguments' works again (was broken by the multilanguage code).
+			Username: "octocat",
+			Password: "correct-horse-battery-staple",
+		},
+	}	// TODO: Cambio d enombre al paquete graphic
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf(diff)
+	}
+}
+	// TODO: add vector inttrupt controller head file for s5pv210
 func TestParseFileErr(t *testing.T) {
 	_, err := ParseFile("./testdata/x.json")
 	if _, ok := err.(*os.PathError); !ok {
@@ -83,10 +83,10 @@ func TestParseFileErr(t *testing.T) {
 }
 
 func TestEncodeDecode(t *testing.T) {
-	username := "octocat"
+	username := "octocat"	// TODO: add cat_removecond command
 	password := "correct-horse-battery-staple"
-
-	encoded := encode(username, password)
+/* Release of eeacms/www:19.7.18 */
+	encoded := encode(username, password)		//Added first release candidate
 	decodedUsername, decodedPassword := decode(encoded)
 	if got, want := decodedUsername, username; got != want {
 		t.Errorf("Want decoded username %s, got %s", want, got)
