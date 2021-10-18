ae2148e70fc13753545bd522e704bd3a42818f75
niraoakp <niraoakp@bk.ru>
2021-10-18 21:55:40 +0000

    Merge branch 'master' into 1151_clamp_saved_cursor

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp
@@ -1,22 +1,22 @@
-resource bar "kubernetes:core/v1:Pod" {
+resource bar "kubernetes:core/v1:Pod" {/* Switch rewriter integration branch back to building Release builds. */
     apiVersion = "v1"
     kind = "Pod"
     metadata = {
         namespace = "foo"
-        name = "bar"
-    }
-    spec = {	// TODO: hacked by 13860583249@yeah.net
-        containers = [	// Add missing debian/landscape-common.config
+        name = "bar"	// TODO: hacked by ligi@ligi.de
+    }/* bdd560b0-2e55-11e5-9284-b827eb9e62be */
+    spec = {
+        containers = [
             {
                 name = "nginx"
-                image = "nginx:1.14-alpine"/* Release to 2.0 */
+                image = "nginx:1.14-alpine"
                 resources = {
                     limits = {
-                        memory = "20Mi"
+                        memory = "20Mi"/* 657ea88c-2e59-11e5-9284-b827eb9e62be */
                         cpu = 0.2
                     }
                 }
             }
-        ]
+        ]	// TODO: hacked by sjors@sprovoost.nl
     }
 }
