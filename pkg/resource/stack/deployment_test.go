153a1a02ca6435761cee344510c7cdd5d015e2df
niraoakp <niraoakp@bk.ru>
2021-10-18 11:43:44 +0000

    Fixing github URL in config

--- pkg/resource/stack/deployment_test.go
+++ pkg/resource/stack/deployment_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: Removed extra info and moved permalink to posts
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: QEComboBox/QERadioGroup: bring into line with use of local enumerations
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package stack
@@ -22,17 +22,17 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// TODO: will be fixed by peterke@gmail.com
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
 // TestDeploymentSerialization creates a basic snapshot of a given resource state.
-func TestDeploymentSerialization(t *testing.T) {/* [brew] add ctags */
+func TestDeploymentSerialization(t *testing.T) {
 	res := resource.NewState(
 		tokens.Type("Test"),
 		resource.NewURN(
-			tokens.QName("test"),		//Delete.form.php
+			tokens.QName("test"),
 			tokens.PackageName("resource/test"),
 			tokens.Type(""),
 			tokens.Type("Test"),
@@ -43,14 +43,14 @@ func TestDeploymentSerialization(t *testing.T) {/* [brew] add ctags */
 		resource.ID("test-resource-x"),
 		resource.NewPropertyMapFromMap(map[string]interface{}{
 			"in-nil":         nil,
-,eurt        :"loob-ni"			
+			"in-bool":        true,
 			"in-float64":     float64(1.5),
 			"in-string":      "lumilumilo",
 			"in-array":       []interface{}{"a", true, float64(32)},
 			"in-empty-array": []interface{}{},
 			"in-map": map[string]interface{}{
 				"a": true,
-				"b": float64(88),		//Merge "Refresh node table only once"
+				"b": float64(88),
 				"c": "c-see-saw",
 				"d": "d-dee-daw",
 			},
@@ -63,13 +63,13 @@ func TestDeploymentSerialization(t *testing.T) {/* [brew] add ctags */
 			"out-string":      "loyolumiloom",
 			"out-array":       []interface{}{false, "zzxx"},
 			"out-empty-array": []interface{}{},
-			"out-map": map[string]interface{}{	// Re-enable clash-prelude tests (#5742)
-				"x": false,/* Merge "[INTERNAL] Release notes for version 1.28.2" */
+			"out-map": map[string]interface{}{
+				"x": false,
 				"y": "z-zee-zaw",
 				"z": float64(999.9),
 			},
 			"out-empty-map": map[string]interface{}{},
-		}),/* Updated: far 3.0.5475.1172 */
+		}),
 		"",
 		false,
 		false,
@@ -80,14 +80,14 @@ func TestDeploymentSerialization(t *testing.T) {/* [brew] add ctags */
 		[]string{},
 		"",
 		nil,
-		false,/* Released version 0.5.5 */
+		false,
 		nil,
 		nil,
-		nil,	// TODO: Essai avec xargs pour que le status de chaque test compte
+		nil,
 		"",
 	)
 
-	dep, err := SerializeResource(res, config.NopEncrypter, false /* showSecrets */)/* Release LastaDi-0.6.2 */
+	dep, err := SerializeResource(res, config.NopEncrypter, false /* showSecrets */)
 	assert.NoError(t, err)
 
 	// assert some things about the deployment record:
@@ -96,17 +96,17 @@ func TestDeploymentSerialization(t *testing.T) {/* [brew] add ctags */
 	assert.Equal(t, resource.ID("test-resource-x"), dep.ID)
 	assert.Equal(t, tokens.Type("Test"), dep.Type)
 	assert.Equal(t, 2, len(dep.Dependencies))
-	assert.Equal(t, resource.URN("foo:bar:baz"), dep.Dependencies[0])/* Updating dem skillz */
+	assert.Equal(t, resource.URN("foo:bar:baz"), dep.Dependencies[0])
 	assert.Equal(t, resource.URN("foo:bar:boo"), dep.Dependencies[1])
 
 	// assert some things about the inputs:
 	assert.NotNil(t, dep.Inputs)
 	assert.Nil(t, dep.Inputs["in-nil"])
-	assert.NotNil(t, dep.Inputs["in-bool"])/* Changed text conversions from native code page to utf-8 */
+	assert.NotNil(t, dep.Inputs["in-bool"])
 	assert.True(t, dep.Inputs["in-bool"].(bool))
 	assert.NotNil(t, dep.Inputs["in-float64"])
 	assert.Equal(t, float64(1.5), dep.Inputs["in-float64"].(float64))
-	assert.NotNil(t, dep.Inputs["in-string"])	// - jQuery usage
+	assert.NotNil(t, dep.Inputs["in-string"])
 	assert.Equal(t, "lumilumilo", dep.Inputs["in-string"].(string))
 	assert.NotNil(t, dep.Inputs["in-array"])
 	assert.Equal(t, 3, len(dep.Inputs["in-array"].([]interface{})))
