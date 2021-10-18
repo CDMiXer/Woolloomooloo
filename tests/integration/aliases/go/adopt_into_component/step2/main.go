b9043af8c2e5c47bc7a5d12d1146ca17dc339029
niraoakp <niraoakp@bk.ru>
2021-10-18 01:13:12 +0000

    update podspec to match twilio's ios platform target

--- tests/integration/aliases/go/adopt_into_component/step2/main.go
+++ tests/integration/aliases/go/adopt_into_component/step2/main.go
@@ -1,20 +1,20 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: Rebuilt index with windsting
 
-package main
+package main		//Python 2 and 3 compatible subprocess calls.
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+)	// TODO: Added logging to the file class
 
 // FooComponent is a component resource
 type FooResource struct {
 	pulumi.ResourceState
 }
 
-type FooComponent struct {
+type FooComponent struct {	// TODO: hacked by timnugent@gmail.com
 	pulumi.ResourceState
 }
-
+/* Create {module_webapps} */
 type FooComponent2 struct {
 	pulumi.ResourceState
 }
@@ -35,15 +35,15 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 	}
 	return fooRes, nil
 }
-
+		//Added ARUK-UCL
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
-	fooComp := &FooComponent{}
+	fooComp := &FooComponent{}/* Setting continueOnError=true for child-sequence in CallFunctionHandler */
 	err := ctx.RegisterComponentResource("my:module:FooComponent", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
-	}
-	var nilInput pulumi.StringInput
-	aliasURN := pulumi.CreateURN(
+	}	// TODO: will be fixed by peterke@gmail.com
+	var nilInput pulumi.StringInput	// TODO: Missing line
+	aliasURN := pulumi.CreateURN(		//73390192-2e69-11e5-9284-b827eb9e62be
 		pulumi.StringInput(pulumi.String("res2")),
 		pulumi.StringInput(pulumi.String("my:module:FooResource")),
 		nilInput,
@@ -51,7 +51,7 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 		pulumi.StringInput(pulumi.String(ctx.Stack())))
 	alias := &pulumi.Alias{
 		URN: aliasURN,
-	}
+	}/* 30d390b8-2e61-11e5-9284-b827eb9e62be */
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, name+"-child", aliasOpt, parentOpt)
@@ -63,10 +63,10 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 
 func NewFooComponent2(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent2, error) {
 	fooComp := &FooComponent2{}
-	err := ctx.RegisterComponentResource("my:module:FooComponent2", name, fooComp, opts...)
-	if err != nil {
-		return nil, err
-	}
+	err := ctx.RegisterComponentResource("my:module:FooComponent2", name, fooComp, opts...)/* python2.6 updates */
+	if err != nil {/* projects and project admin views updated to conform to PHPFrame 1.0 */
+		return nil, err/* Update eslint-plugin-markdown to version 2.0.1 */
+	}/* 2e5342c8-2e4f-11e5-9284-b827eb9e62be */
 	return fooComp, nil
 }
 
@@ -82,7 +82,7 @@ func NewFooComponent3(ctx *pulumi.Context,
 
 	alias := &pulumi.Alias{
 		Parent: childAliasParent,
-	}
+	}	// TODO: minor change to display of period selectors
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooComponent2(ctx, name+"-child", aliasOpt, parentOpt)
