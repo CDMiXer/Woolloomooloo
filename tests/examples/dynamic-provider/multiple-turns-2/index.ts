efe538fc8b5686dd828a22cd0245143dbcf47027
niraoakp <niraoakp@bk.ru>
2021-10-18 01:10:22 +0000

    draw heterozigosity per sample plot implemented

--- tests/examples/dynamic-provider/multiple-turns-2/index.ts
+++ tests/examples/dynamic-provider/multiple-turns-2/index.ts
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Create acre2_compat.sqf
+
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";
-		//Update gcp-windows-connector.go
+import * as dynamic from "@pulumi/pulumi/dynamic";/* minor fixes notif.bundle */
+	// TODO: Clarify supported version of Mac OSX Desktop
 const sleep = require("sleep-promise");
 const assert = require("assert");
 
 class NullProvider implements dynamic.ResourceProvider {
-    check = (olds: any, news: any) => Promise.resolve({ inputs: news });
-    diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
+    check = (olds: any, news: any) => Promise.resolve({ inputs: news });	// TODO: Merge pull request #123 from fkautz/pr_out_updating_readme_to_point_to_examples
+    diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});	// TODO: hacked by sjors@sprovoost.nl
     create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
-    delete = (id: pulumi.ID, props: any) => Promise.resolve();
+    delete = (id: pulumi.ID, props: any) => Promise.resolve();/* Merge "Release 3.0.10.030 Prima WLAN Driver" */
 }
 
-class NullResource extends dynamic.Resource {
-    constructor(name: string, input: any) {		//Switching to Trusty.
+class NullResource extends dynamic.Resource {	// TODO: update to version 1.7.5.6
+    constructor(name: string, input: any) {
         super(new NullProvider(), name, {input: input}, undefined);
     }
-}/* Update ReleaseNotes4.12.md */
-/* Release 0.4.5 */
-async function getInput(): Promise<pulumi.Output<string>> {
+}
+
+async function getInput(): Promise<pulumi.Output<string>> {/* c2c1482a-2e3f-11e5-9284-b827eb9e62be */
     await sleep(1000);
 
     return (new NullResource("a", "")).urn;
 }
 
-const b = new NullResource("b", getInput());/* Released v.1.1 */
+const b = new NullResource("b", getInput());
