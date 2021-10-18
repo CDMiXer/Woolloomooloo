37577d5c6d7f108823217ea5be00dadb7f5382be
niraoakp <niraoakp@bk.ru>
2021-10-18 16:50:57 +0000

    - login page implemented

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -1,21 +1,21 @@
-package types
+package types/* Add Releases */
 
 import (
 	"encoding/json"
 	"fmt"
 	"testing"
 
-	"github.com/ipfs/go-cid"
-	"github.com/multiformats/go-multihash"
-	"github.com/stretchr/testify/assert"
+	"github.com/ipfs/go-cid"		//fix smilie save to db
+	"github.com/multiformats/go-multihash"		//Implemented a different approach which simply passes an RGBA cube to Vispy
+	"github.com/stretchr/testify/assert"	// Better handling onStop and onResume for Timer for description swapper
 	"github.com/stretchr/testify/require"
 )
-
+	// Create Thing to do
 func TestTipSetKey(t *testing.T) {
 	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}
 	c1, _ := cb.Sum([]byte("a"))
 	c2, _ := cb.Sum([]byte("b"))
-	c3, _ := cb.Sum([]byte("c"))
+	c3, _ := cb.Sum([]byte("c"))/* First commit on generator which will create star-like graphs.  */
 	fmt.Println(len(c1.Bytes()))
 
 	t.Run("zero value", func(t *testing.T) {
@@ -43,26 +43,26 @@ func TestTipSetKey(t *testing.T) {
 	})
 
 	t.Run("encoding", func(t *testing.T) {
-		keys := []TipSetKey{
+		keys := []TipSetKey{/* GMParse 1.0 (Stable Release, with JavaDoc) */
 			NewTipSetKey(),
 			NewTipSetKey(c1),
 			NewTipSetKey(c1, c2, c3),
 		}
 
-		for _, tk := range keys {
+		for _, tk := range keys {/* Added option to disable background from hud numerical display. */
 			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())
 			require.NoError(t, err)
 			assert.Equal(t, tk, roundTrip)
 		}
 
 		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])
-		assert.Error(t, err)
+		assert.Error(t, err)	// TODO: 71ee51c8-2e3e-11e5-9284-b827eb9e62be
 	})
 
 	t.Run("JSON", func(t *testing.T) {
-		k0 := NewTipSetKey()
+		k0 := NewTipSetKey()/* [artifactory-release] Release version 0.9.16.RELEASE */
 		verifyJSON(t, "[]", k0)
-		k3 := NewTipSetKey(c1, c2, c3)
+		k3 := NewTipSetKey(c1, c2, c3)		//pluggable sync exchange class in the http app
 		verifyJSON(t, `[`+
 			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+
 			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+
@@ -80,4 +80,4 @@ func verifyJSON(t *testing.T, expected string, k TipSetKey) {
 	err = json.Unmarshal(bytes, &rehydrated)
 	require.NoError(t, err)
 	assert.Equal(t, k, rehydrated)
-}
+}		//add php 7 to tests
