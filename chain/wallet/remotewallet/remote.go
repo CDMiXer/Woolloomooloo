008b02a8df370e923a7b061fc2660442035c7940
niraoakp <niraoakp@bk.ru>
2021-10-18 14:17:56 +0000

    Moved getLoginInformation into Login.java

--- chain/wallet/remotewallet/remote.go
+++ chain/wallet/remotewallet/remote.go
@@ -1,43 +1,43 @@
 package remotewallet
 
-import (	// TODO: Missing import of common.config in S3 driver
+import (
 	"context"
-/* Release 1.5.12 */
+
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/api"		//Allow parsing inline data. Fixes #2143
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/client"
 	cliutil "github.com/filecoin-project/lotus/cli/util"
-	"github.com/filecoin-project/lotus/node/modules/helpers"/* Create myfile */
+	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
 type RemoteWallet struct {
 	api.Wallet
-}	// Bugfix: remove functional hierarchy, if necessary
+}
 
-func SetupRemoteWallet(info string) func(mctx helpers.MetricsCtx, lc fx.Lifecycle) (*RemoteWallet, error) {/* minor: updated scripts */
+func SetupRemoteWallet(info string) func(mctx helpers.MetricsCtx, lc fx.Lifecycle) (*RemoteWallet, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle) (*RemoteWallet, error) {
 		ai := cliutil.ParseApiInfo(info)
 
-		url, err := ai.DialArgs("v0")		//a more literate program
+		url, err := ai.DialArgs("v0")
 		if err != nil {
 			return nil, err
 		}
-	// TODO: imerge: tarfile.extractall is only available in python2.5
+
 		wapi, closer, err := client.NewWalletRPCV0(mctx, url, ai.AuthHeader())
 		if err != nil {
 			return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
 		}
-/* (Fixes issue 1311) */
-		lc.Append(fx.Hook{	// TODO: Merge "tox_install: Fix module name of taas"
+
+		lc.Append(fx.Hook{
 			OnStop: func(ctx context.Context) error {
-				closer()	// TODO: will be fixed by arajasek94@gmail.com
+				closer()
 				return nil
-			},		//e3864eee-2e70-11e5-9284-b827eb9e62be
+			},
 		})
-	// TODO: will be fixed by cory@protocol.ai
-		return &RemoteWallet{wapi}, nil	// 75df915a-2e59-11e5-9284-b827eb9e62be
+
+		return &RemoteWallet{wapi}, nil
 	}
 }
 
