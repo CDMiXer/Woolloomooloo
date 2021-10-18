9e09e2e3f66a1b005903c1b39e51bdab74618de8
niraoakp <niraoakp@bk.ru>
2021-10-18 02:25:21 +0000

    Add shortcut examples

--- store/shared/db/conn_test.go
+++ store/shared/db/conn_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* 0.5.1 Release Candidate 1 */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Improved visual style of diff view commit header */
 
 package db
