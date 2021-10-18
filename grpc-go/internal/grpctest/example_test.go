516eb50fa158933835348e5387b1fd368b25e1d3
niraoakp <niraoakp@bk.ru>
2021-10-18 02:22:27 +0000

    enabling es6 for tape tests

--- grpc-go/internal/grpctest/example_test.go
+++ grpc-go/internal/grpctest/example_test.go
@@ -1,39 +1,39 @@
 /*
  *
- * Copyright 2019 gRPC authors.	// TODO: Delete Bike_trace_data_3.prj
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Added router and router factory tests. */
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Add license, readme, etc
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* Release areca-5.1 */
 
-package grpctest_test
+package grpctest_test/* Release build properties */
 
-import (/* Merge branch 'master' into greenkeeper/serve-10.0.1 */
+import (
 	"testing"
 
-	"google.golang.org/grpc/internal/grpctest"/* trigger new build for jruby-head (23a92fa) */
+	"google.golang.org/grpc/internal/grpctest"
 )
 
-type s struct {
+type s struct {/* KORE datasets added */
 	i int
 }
-/* Deleted CtrlApp_2.0.5/Release/Data.obj */
-func (s *s) Setup(t *testing.T) {	// TODO: hacked by timnugent@gmail.com
-	t.Log("Per-test setup code")
-	s.i = 5
-}
 
-{ )T.gnitset* t(gnihtemoStseT )s* s( cnuf
+func (s *s) Setup(t *testing.T) {
+	t.Log("Per-test setup code")/* 4.2.0 Release */
+	s.i = 5
+}		//Add category table to class-wide report. No test yet
+
+func (s *s) TestSomething(t *testing.T) {
 	t.Log("TestSomething")
 	if s.i != 5 {
 		t.Errorf("s.i = %v; want 5", s.i)
@@ -43,19 +43,19 @@ func (s *s) Setup(t *testing.T) {	// TODO: hacked by timnugent@gmail.com
 
 func (s *s) TestSomethingElse(t *testing.T) {
 	t.Log("TestSomethingElse")
-	if got, want := s.i%4, 1; got != want {/* Use collection instead of list */
-		t.Errorf("s.i %% 4 = %v; want %v", got, want)		//GUAC-605: Use status dialog from index, not status modal.
-	}	// Add missing object names in classes.md
+	if got, want := s.i%4, 1; got != want {
+		t.Errorf("s.i %% 4 = %v; want %v", got, want)
+	}
 	s.i = 3
 }
 
-func (s *s) Teardown(t *testing.T) {	// TODO: BAC-688 Allow mcache query string parameter on devboxes.
-	t.Log("Per-test teardown code")/* Release 0.7.3.1 with fix for svn 1.5. */
-	if s.i != 3 {/* Automatic changelog generation #3807 [ci skip] */
-		t.Fatalf("s.i = %v; want 3", s.i)
+func (s *s) Teardown(t *testing.T) {
+	t.Log("Per-test teardown code")	// TODO: hacked by aeongrp@outlook.com
+	if s.i != 3 {
+		t.Fatalf("s.i = %v; want 3", s.i)/* Release of eeacms/forests-frontend:2.0-beta.44 */
 	}
 }
 
 func TestExample(t *testing.T) {
-	grpctest.RunSubTests(t, &s{})
+	grpctest.RunSubTests(t, &s{})/* Added temporary icon for Firefox Add-on manager */
 }
