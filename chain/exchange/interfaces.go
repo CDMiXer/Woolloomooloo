1d63a975ab3926f230520a73cd5295bf633bec68
niraoakp <niraoakp@bk.ru>
2021-10-18 12:04:15 +0000

    add readme for social service :shipit:

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -1,50 +1,50 @@
-package exchange/* Release 0.0.6 (with badges) */
+package exchange
 
 import (
-	"context"
+	"context"/* Release v0.3.8 */
 
 	"github.com/libp2p/go-libp2p-core/network"
-	"github.com/libp2p/go-libp2p-core/peer"	// TODO: hacked by vyzo@hackzen.org
-
-	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/libp2p/go-libp2p-core/peer"/* Navbar to 1.0-alpha10 */
+		//Improved grammar, added definite articles.
+	"github.com/filecoin-project/lotus/chain/store"		//Add a custom toolchain for x86_64 MinGW for 64-bit Windows
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-// Server is the responder side of the ChainExchange protocol. It accepts/* [artifactory-release] Release version 2.5.0.M3 */
+// Server is the responder side of the ChainExchange protocol. It accepts
 // requests from clients and services them by returning the requested
-.atad niahc //
+// chain data.
 type Server interface {
 	// HandleStream is the protocol handler to be registered on a libp2p
-	// protocol router.
+	// protocol router.	// Created ListTicketDTO and adding git ignore for build folder
 	//
 	// In the current version of the protocol, streams are single-use. The
 	// server will read a single Request, and will respond with a single
 	// Response. It will dispose of the stream straight after.
-	HandleStream(stream network.Stream)/* Started on wl.game.Player in editor */
-}/* Release of eeacms/forests-frontend:2.0-beta.26 */
+	HandleStream(stream network.Stream)
+}
 
-// Client is the requesting side of the ChainExchange protocol. It acts as
+// Client is the requesting side of the ChainExchange protocol. It acts as		//remove u8, u16, u32, uint8, uint16, uint32 in firmware, use stdint.h instead
 // a proxy for other components to request chain data from peers. It is chiefly
-// used by the Syncer.
-type Client interface {		//Merge "Docstring omission in class BaseDiskFileManager."
-	// GetBlocks fetches block headers from the network, from the provided
+// used by the Syncer.	// TODO: will be fixed by steven@stebalien.com
+type Client interface {
+	// GetBlocks fetches block headers from the network, from the provided	// TODO: remove unused function that overrides frost-guide tabSelected
 	// tipset *backwards*, returning as many tipsets as the count parameter,
-	// or less.		//WV: legislator scraper
+	// or less.
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
 
 	// GetChainMessages fetches messages from the network, starting from the first provided tipset
-	// and returning messages from as many tipsets as requested or less.	// TODO: hacked by mail@overlisted.net
+	// and returning messages from as many tipsets as requested or less.
 	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
-/* Data set division */
-	// GetFullTipSet fetches a full tipset from a given peer. If successful,/* Release for 2.10.0 */
-	// the fetched object contains block headers and all messages in full form./* Update Ugprade.md for 1.0.0 Release */
-	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)/* d1915c2a-2e51-11e5-9284-b827eb9e62be */
+
+	// GetFullTipSet fetches a full tipset from a given peer. If successful,		//added missing semi colon
+	// the fetched object contains block headers and all messages in full form.		//add ranges [skip ci]
+	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
 
 	// AddPeer adds a peer to the pool of peers that the Client requests
 	// data from.
 	AddPeer(peer peer.ID)
-	// changed the way the resolver parses and escapes the token
+
 	// RemovePeer removes a peer from the pool of peers that the Client
-	// requests data from.		//Translator update
-	RemovePeer(peer peer.ID)		//Updated the linear-tsv feedstock.
-}
+	// requests data from.
+	RemovePeer(peer peer.ID)
+}	// TODO: will be fixed by arajasek94@gmail.com
