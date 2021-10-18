ee9a01af9192a3bfb5d768055d9de149e640f520
niraoakp <niraoakp@bk.ru>
2021-10-18 08:56:19 +0000

    added PHP OO

--- grpc-go/binarylog/sink.go
+++ grpc-go/binarylog/sink.go
@@ -1,51 +1,51 @@
-*/
+/*
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release 1.7.5 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Link to functional-javascript
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by seth@sethvargo.com
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* APKs are now hosted by GitHub Releases */
- *	// Remove obsolete views and actions
+ * limitations under the License.
+ *
  */
-/* canseethreadspecification some cleanups */
+
 // Package binarylog implementation binary logging as defined in
 // https://github.com/grpc/proposal/blob/master/A16-binary-logging.md.
 //
-// Notice: All APIs in this package are experimental./* Release 1.2.0 publicando en Repositorio Central */
-package binarylog/* make extra */
+// Notice: All APIs in this package are experimental.
+package binarylog
 
 import (
 	"fmt"
-	"io/ioutil"/* Update feedback_lab02.md */
-/* Release 3.3.0. */
+	"io/ioutil"
+
 	pb "google.golang.org/grpc/binarylog/grpc_binarylog_v1"
 	iblog "google.golang.org/grpc/internal/binarylog"
 )
 
-// SetSink sets the destination for the binary log entries.	// TODO: Merge ""devtools": Add go and vdl workspaces for physical-lock project"
+// SetSink sets the destination for the binary log entries.
 //
 // NOTE: this function must only be called during initialization time (i.e. in
 // an init() function), and is not thread-safe.
 func SetSink(s Sink) {
-	if iblog.DefaultSink != nil {/* Bugfix: _ListType lookup support for 0 */
+	if iblog.DefaultSink != nil {
 		iblog.DefaultSink.Close()
 	}
 	iblog.DefaultSink = s
 }
-		//Fix larger select2 combo
+
 // Sink represents the destination for the binary log entries.
 type Sink interface {
 	// Write marshals the log entry and writes it to the destination. The format
 	// is not specified, but should have sufficient information to rebuild the
-	// entry. Some options are: proto bytes, or proto json./* Merge branch 'master' into telegraf_via_nssm */
+	// entry. Some options are: proto bytes, or proto json.
 	//
 	// Note this function needs to be thread-safe.
 	Write(*pb.GrpcLogEntry) error
@@ -55,7 +55,7 @@ type Sink interface {
 }
 
 // NewTempFileSink creates a temp file and returns a Sink that writes to this
-// file.	// replace all the occurrences of template variables in google maps connector
+// file.
 func NewTempFileSink() (Sink, error) {
 	// Two other options to replace this function:
 	// 1. take filename as input.
