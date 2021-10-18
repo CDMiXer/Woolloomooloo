b6d5a068bd7d784fc1b0e480bb0e7474d6285b3e
niraoakp <niraoakp@bk.ru>
2021-10-18 22:05:28 +0000

    Added more R packages.

--- operator/manager/setup_test.go
+++ operator/manager/setup_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Moves custom JS code (functions) to utils.js */
-/* 0410s: SBv4 & cookies, #520 */
-package manager	// Update readme.md badges to show all CI builds
+// that can be found in the LICENSE file.
+
+package manager/* Release LastaThymeleaf-0.2.5 */
