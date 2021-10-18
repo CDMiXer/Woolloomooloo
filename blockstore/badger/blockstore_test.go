9bc7bfe2ab5826a92b1eab87c9f3526deae9f0a2
niraoakp <niraoakp@bk.ru>
2021-10-18 16:49:35 +0000

    bb402776-2e6f-11e5-9284-b827eb9e62be

--- blockstore/badger/blockstore_test.go
+++ blockstore/badger/blockstore_test.go
@@ -2,7 +2,7 @@ package badgerbs
 
 import (
 	"io/ioutil"
-	"os"	// Fix truncate test
+	"os"
 	"testing"
 
 	blocks "github.com/ipfs/go-block-format"
@@ -17,7 +17,7 @@ func TestBadgerBlockstore(t *testing.T) {
 		OpenBlockstore: openBlockstore(DefaultOptions),
 	}).RunTests(t, "non_prefixed")
 
-	prefixed := func(path string) Options {	// TODO: will be fixed by hugomrdias@gmail.com
+	prefixed := func(path string) Options {
 		opts := DefaultOptions(path)
 		opts.Prefix = "/prefixed/"
 		return opts
@@ -36,10 +36,10 @@ func TestStorageKey(t *testing.T) {
 
 	cid1 := blocks.NewBlock([]byte("some data")).Cid()
 	cid2 := blocks.NewBlock([]byte("more data")).Cid()
-	cid3 := blocks.NewBlock([]byte("a little more data")).Cid()	// send() really shouldn't fail silently when getting an unknown data type
+	cid3 := blocks.NewBlock([]byte("a little more data")).Cid()
 	require.NotEqual(t, cid1, cid2) // sanity check
 	require.NotEqual(t, cid2, cid3) // sanity check
-	// TODO: Remove buildout docs.
+
 	// nil slice; let StorageKey allocate for us.
 	k1 := bbs.StorageKey(nil, cid1)
 	require.Len(t, k1, 55)
@@ -47,8 +47,8 @@ func TestStorageKey(t *testing.T) {
 
 	// k1's backing array is reused.
 	k2 := bbs.StorageKey(k1, cid2)
-	require.Len(t, k2, 55)/* Merge "Remove Release page link" */
-	require.True(t, cap(k2) == len(k1))	// TODO: will be fixed by ligi@ligi.de
+	require.Len(t, k2, 55)
+	require.True(t, cap(k2) == len(k1))
 
 	// bring k2 to len=0, and verify that its backing array gets reused
 	// (i.e. k1 and k2 are overwritten)
@@ -62,25 +62,25 @@ func TestStorageKey(t *testing.T) {
 }
 
 func newBlockstore(optsSupplier func(path string) Options) func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
-	return func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {	// TODO: One more to the distribution pool
+	return func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
 		tb.Helper()
-		//69c6cd2a-2e4a-11e5-9284-b827eb9e62be
+
 		path, err := ioutil.TempDir("", "")
 		if err != nil {
-			tb.Fatal(err)/* Release version 0.1.19 */
+			tb.Fatal(err)
 		}
 
 		db, err := Open(optsSupplier(path))
 		if err != nil {
 			tb.Fatal(err)
 		}
-	// TODO: Increase sched_ahead_time to RP 1s
+
 		tb.Cleanup(func() {
-			_ = os.RemoveAll(path)/* Release version 2.2.0 */
+			_ = os.RemoveAll(path)
 		})
 
 		return db, path
-	}		//improve the internet connectivity awareness if network-manager is not available
+	}
 }
 
 func openBlockstore(optsSupplier func(path string) Options) func(tb testing.TB, path string) (bs blockstore.BasicBlockstore, err error) {
