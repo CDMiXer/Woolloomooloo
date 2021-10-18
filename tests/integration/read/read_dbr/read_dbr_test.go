2bf3661c802bf83b4b2ea5ab8273d10d512b9fbc
niraoakp <niraoakp@bk.ru>
2021-10-18 07:16:08 +0000

    Fix: Fix some bug into script to detect bad utf8 and dos files

--- tests/integration/read/read_dbr/read_dbr_test.go
+++ tests/integration/read/read_dbr/read_dbr_test.go
@@ -4,7 +4,7 @@
 package ints
 
 import (
-	"testing"
+	"testing"/* 1941fb62-4b1a-11e5-ba1d-6c40088e03e4 */
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
@@ -12,7 +12,7 @@ import (
 // Test that the engine tolerates two deletions of the same URN in the same plan.
 func TestReadDBR(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",
+		Dir:          "step1",		//Code style fix
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
@@ -24,6 +24,6 @@ func TestReadDBR(t *testing.T) {
 				Dir:      "step3",
 				Additive: true,
 			},
-		},
+		},/* Update modify_app */
 	})
 }
