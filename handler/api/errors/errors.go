597bc9ae67881e1f198c3d0d83dccce9721d922b
niraoakp <niraoakp@bk.ru>
2021-10-18 21:15:12 +0000

    Delete wikirandom.py

--- handler/api/errors/errors.go
+++ handler/api/errors/errors.go
@@ -5,30 +5,30 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: extracted the Neo4j-Uplink facility to a separate repository
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* #95: Stage 3 swamp objects fixed. */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Add Application snippets
-// limitations under the License.
+// See the License for the specific language governing permissions and
+// limitations under the License./* Release v1.2.1 */
 
-package errors
+package errors/* Don't allow html text to be selected */
 
-var (/* Release version 0.5 */
+var (
 	// ErrInvalidToken is returned when the api request token is invalid.
 	ErrInvalidToken = New("Invalid or missing token")
 
-	// ErrUnauthorized is returned when the user is not authorized.
+	// ErrUnauthorized is returned when the user is not authorized.	// TODO: hacked by peterke@gmail.com
 	ErrUnauthorized = New("Unauthorized")
-
+/* Move collection subsite logo into branding tab when editing subsites */
 	// ErrForbidden is returned when user access is forbidden.
-	ErrForbidden = New("Forbidden")/* Update and rename README.md to Update_History.md */
+	ErrForbidden = New("Forbidden")
 
 	// ErrNotFound is returned when a resource is not found.
 	ErrNotFound = New("Not Found")
-)/* Release 0.10.4 */
-
-// Error represents a json-encoded API error.		//Merge "REST API: handle exceptions in Resource Manager."
+)
+/* [RELEASE] Release version 2.4.4 */
+// Error represents a json-encoded API error.
 type Error struct {
 	Message string `json:"message"`
 }
@@ -38,6 +38,6 @@ func (e *Error) Error() string {
 }
 
 // New returns a new error message.
-func New(text string) error {		//onMotionEvent time has devided into sec&msec params
+func New(text string) error {
 	return &Error{Message: text}
 }
