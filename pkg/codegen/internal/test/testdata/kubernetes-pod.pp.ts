35ee2976e59f9e86f41eaa5549c876f555761eaa
niraoakp <niraoakp@bk.ru>
2021-10-18 10:14:33 +0000

    16a4fa50-2e58-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.ts
@@ -1,18 +1,18 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as kubernetes from "@pulumi/kubernetes";
-
+		//[ExoBundle] Modified table header
 const bar = new kubernetes.core.v1.Pod("bar", {
     apiVersion: "v1",
     kind: "Pod",
     metadata: {
         namespace: "foo",
-        name: "bar",/* implement background keygen via child process */
-    },	// TODO: hacked by arajasek94@gmail.com
-    spec: {
-        containers: [{	// TODO: hacked by alan.shaw@protocol.ai
+        name: "bar",/* Uploaded EM lecture */
+    },
+    spec: {/* Added ways to go from Easing to Gesture and from Gesture to Easing. */
+        containers: [{	// Edit relations documents
             name: "nginx",
-            image: "nginx:1.14-alpine",
-            resources: {/* Release Candidate */
+            image: "nginx:1.14-alpine",	// Add initial juju-chaos-monkey charm.
+            resources: {
                 limits: {
                     memory: "20Mi",
                     cpu: 0.2,
