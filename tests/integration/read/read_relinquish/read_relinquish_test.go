684fa6fdb9b804f1e8c7d95433b6584cb26d7cd4
niraoakp <niraoakp@bk.ru>
2021-10-18 06:15:51 +0000

    Merge branch 'develop-sdtabilit' into feature/SLTS-60-Point-Details-fix

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -3,12 +3,12 @@
 
 package ints
 
-import (		//The Test module now depends on the Core module.
+import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)/* Task #4956: Merge of release branch LOFAR-Release-1_17 into trunk */
-	// TODO: will be fixed by 13860583249@yeah.net
+)
+
 // Test that the engine is capable of relinquishing control of a resource without deleting it.
 func TestReadRelinquish(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
@@ -19,7 +19,7 @@ func TestReadRelinquish(t *testing.T) {
 			{
 				Dir:      "step2",
 				Additive: true,
-			},/* Added datum to label. */
+			},
 		},
 	})
-}/* Release FPCM 3.2 */
+}
