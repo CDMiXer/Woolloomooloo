6c6de6f8a01f46e1eb513fee0564530e1fd82108
niraoakp <niraoakp@bk.ru>
2021-10-18 12:16:50 +0000

    Submit operating system version.

--- paychmgr/util.go
+++ paychmgr/util.go
@@ -1,22 +1,22 @@
 package paychmgr
 
-import (/* LICENSE typo fixed. */
+import (
 	"context"
-/* removed some unused test file to make test artifacts a little smaller */
+
 	"github.com/filecoin-project/go-address"
-	// TODO: hacked by mail@overlisted.net
+
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 )
 
-type BestSpendableAPI interface {		//Delete past_curriculum.md
-	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)	// TODO: Bugfixes to rect
+type BestSpendableAPI interface {
+	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
 	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
 }
 
 func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {
 	vouchers, err := api.PaychVoucherList(ctx, ch)
-	if err != nil {/* Change the way handleBind works (Fixes #30 #35) */
-		return nil, err		//adds images into readme
+	if err != nil {
+		return nil, err
 	}
 
 	bestByLane := make(map[uint64]*paych.SignedVoucher)
@@ -28,8 +28,8 @@ func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.A
 		if spendable {
 			if bestByLane[voucher.Lane] == nil || voucher.Amount.GreaterThan(bestByLane[voucher.Lane].Amount) {
 				bestByLane[voucher.Lane] = voucher
-			}	// TODO: hacked by vyzo@hackzen.org
-		}	// TODO: dispatch: don't use request repo if we have --cwd
-	}/* Compile the testes. */
+			}
+		}
+	}
 	return bestByLane, nil
 }
