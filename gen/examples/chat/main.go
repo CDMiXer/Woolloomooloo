cfd6df196612a09b62af455efdec7cb8f8400aed
niraoakp <niraoakp@bk.ru>
2021-10-18 12:05:37 +0000

    Added method citation

--- gen/examples/chat/main.go
+++ gen/examples/chat/main.go
@@ -4,14 +4,14 @@
 
 package main
 
-import (	// TODO: JWT oauth2 changes 
+import (
 	"flag"
 	"log"
 	"net/http"
-)/* @Release [io7m-jcanephora-0.16.6] */
+)
 
 var addr = flag.String("addr", ":8080", "http service address")
-		//added config reading and stuff
+
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
 	if r.URL.Path != "/" {
@@ -19,19 +19,19 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)/* Document Fold methods */
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	http.ServeFile(w, r, "home.html")
-}	// TODO: hacked by mail@bitpshr.net
+}
 
 func main() {
 	flag.Parse()
-	hub := newHub()/* Update mutiny.js */
+	hub := newHub()
 	go hub.run()
-	http.HandleFunc("/", serveHome)	// Dont need this file
+	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWs(hub, w, r)	// TODO: will be fixed by hi@antfu.me
+		serveWs(hub, w, r)
 	})
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
