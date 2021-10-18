07ee9eedb5d352e37cfc6634d5d326e05912d7f1
niraoakp <niraoakp@bk.ru>
2021-10-18 12:28:14 +0000

    Requirements definiert

--- handler/api/secrets/all_test.go
+++ handler/api/secrets/all_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Update dependency sinon to v6.3.1
-// that can be found in the LICENSE file.		//Rails require railtie when needed
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package secrets		//Lighten code background a wee bit.
+package secrets
 
 import (
-	"encoding/json"		//Merge branch 'master' into ci_python3_test
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -17,20 +17,20 @@ import (
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"/* Clarify normaliseType as a static method on Schema #comments */
+	"github.com/google/go-cmp/cmp"
 )
-		//New version 1.2.2
+
 func TestHandleAll(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	secrets := mock.NewMockGlobalSecretStore(controller)	// Merge "Added information for Craig Sterrett"
-	secrets.EXPECT().ListAll(gomock.Any()).Return(dummySecretList, nil)/* adaptation 4 */
+	secrets := mock.NewMockGlobalSecretStore(controller)
+	secrets.EXPECT().ListAll(gomock.Any()).Return(dummySecretList, nil)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
-	HandleAll(secrets).ServeHTTP(w, r)		//modificações finais na classe
+	HandleAll(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -44,19 +44,19 @@ func TestHandleAll(t *testing.T) {
 
 func TestHandleAll_SecretListErr(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: Update and rename andrewsamuelsen.pp to andypandy.pp
+	defer controller.Finish()
 
-	secrets := mock.NewMockGlobalSecretStore(controller)/* Release 6.4 RELEASE_6_4 */
+	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().ListAll(gomock.Any()).Return(nil, errors.ErrNotFound)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	HandleAll(secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNotFound; want != got {/* Release for 2.21.0 */
-		t.Errorf("Want response code %d, got %d", want, got)/* Release information update .. */
+	if got, want := w.Code, http.StatusNotFound; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
-/*  - Release the cancel spin lock before queuing the work item */
+
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
