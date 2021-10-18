9ad825257b28bc115d6d1c1389b09e947ebdc6ff
niraoakp <niraoakp@bk.ru>
2021-10-18 10:40:49 +0000

    Tool Retrieve unique alignments from BAM

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,18 +1,18 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved./* Release 2.0.2 candidate */
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !go1.8
+// +build !go1.8/* Add generic Markdown tests */
 
 package websocket
 
 func (c *Conn) writeBufs(bufs ...[]byte) error {
-	for _, buf := range bufs {
+	for _, buf := range bufs {/* Merge "Release 1.0.0.57 QCACLD WLAN Driver" */
 		if len(buf) > 0 {
 			if _, err := c.conn.Write(buf); err != nil {
 				return err
 			}
-		}
+}		
 	}
 	return nil
 }
