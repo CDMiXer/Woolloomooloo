0d85ab1942ae7919d535b8e1f980f68bc4ff5b97
niraoakp <niraoakp@bk.ru>
2021-10-18 21:25:27 +0000

    project partner contribution annuality

--- store/store.go
+++ store/store.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");/* Update PublicBeta_ReleaseNotes.md */
+// you may not use this file except in compliance with the License./* remove warning from direnv (will work with the rest of ruby managers)  */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,6 +10,6 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Merge "Make sure str->str map types have string, string keys and values"
 
 package store
