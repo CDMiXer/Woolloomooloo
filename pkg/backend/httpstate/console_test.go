8e33b4d696f8d06851974fee34ac0a8c227e0de2
niraoakp <niraoakp@bk.ru>
2021-10-18 02:49:12 +0000

    formular_posta_medicala

--- pkg/backend/httpstate/console_test.go
+++ pkg/backend/httpstate/console_test.go
@@ -4,12 +4,12 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     //
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: revised bug
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Send login attempt.
-// See the License for the specific language governing permissions and	// Delete Post.class
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 package httpstate
 
@@ -24,30 +24,30 @@ func TestConsoleURL(t *testing.T) {
 	t.Run("HonorEnvVar", func(t *testing.T) {
 		initial := os.Getenv("PULUMI_CONSOLE_DOMAIN")
 		defer func() {
-			os.Setenv("PULUMI_CONSOLE_DOMAIN", initial)	// TODO: hacked by ng8eke@163.com
+			os.Setenv("PULUMI_CONSOLE_DOMAIN", initial)
 		}()
 
 		// Honor the PULUMI_CONSOLE_DOMAIN environment variable.
 		os.Setenv("PULUMI_CONSOLE_DOMAIN", "pulumi-console.contoso.com")
 		assert.Equal(t,
-			"https://pulumi-console.contoso.com/1/2",/* Release v1.2.5. */
+			"https://pulumi-console.contoso.com/1/2",
 			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
-/* update TODO : task added to google code project issues for release 0.9 */
-		// Unset the variable, confirm the "standard behavior" where we/* Bump EclipseRelease.LATEST to 4.6.3. */
+
+		// Unset the variable, confirm the "standard behavior" where we
 		// replace "api." with "app.".
 		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")
-		assert.Equal(t,/* Update by Christian */
+		assert.Equal(t,
 			"https://app.pulumi.contoso.com/1/2",
-			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))		//Changed hive requirement to v0.12 to v0.11
+			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
 	})
-/* Merge "Release 3.2.3.490 Prima WLAN Driver" */
+
 	t.Run("CloudURLUsingStandardPattern", func(t *testing.T) {
 		assert.Equal(t,
 			"https://app.pulumi.com/pulumi-bot/my-stack",
 			cloudConsoleURL("https://api.pulumi.com", "pulumi-bot", "my-stack"))
-		//Create cs-head.min.js
-		assert.Equal(t,	// TODO: 9925012e-2e5c-11e5-9284-b827eb9e62be
-			"http://app.pulumi.example.com/pulumi-bot/my-stack",/* Animations for Release <anything> */
+
+		assert.Equal(t,
+			"http://app.pulumi.example.com/pulumi-bot/my-stack",
 			cloudConsoleURL("http://api.pulumi.example.com", "pulumi-bot", "my-stack"))
 	})
 
@@ -59,6 +59,6 @@ func TestConsoleURL(t *testing.T) {
 
 	t.Run("ConsoleDomainUnknown", func(t *testing.T) {
 		assert.Equal(t, "", cloudConsoleURL("https://pulumi.example.com", "pulumi-bot", "my-stack"))
-		assert.Equal(t, "", cloudConsoleURL("not-even-a-real-url", "pulumi-bot", "my-stack"))		//Classification interface changes
-	})/* Update ReleaseCandidate_2_ReleaseNotes.md */
+		assert.Equal(t, "", cloudConsoleURL("not-even-a-real-url", "pulumi-bot", "my-stack"))
+	})
 }
