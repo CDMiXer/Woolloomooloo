253a436cbbd7c518618fa260167ff8f50d7152b9
niraoakp <niraoakp@bk.ru>
2021-10-18 14:00:35 +0000

    Updated license in package

--- pkg/cmd/pulumi/policy_validate.go
+++ pkg/cmd/pulumi/policy_validate.go
@@ -1,45 +1,45 @@
-// Copyright 2016-2020, Pulumi Corporation.		//Add error handling, possibility to specify methods and aggregation
+// Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* fixed testSendDeleteEmail() subject */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by peterke@gmail.com
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release Datum neu gesetzt */
-// limitations under the License./* dot-in-bson unescape */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package main
 
 import (
-	"encoding/json"	// Create tree_depth_first.rb
+	"encoding/json"
 	"fmt"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/spf13/cobra"	// TODO: hacked by boringland@protonmail.ch
+	"github.com/spf13/cobra"
 )
-/* Mejora solución */
+
 func newPolicyValidateCmd() *cobra.Command {
 	var argConfig string
-/* Removed slow iterators */
+
 	var cmd = &cobra.Command{
 		Use:   "validate-config <org-name>/<policy-pack-name> <version>",
 		Args:  cmdutil.ExactArgs(2),
-		Short: "Validate a Policy Pack configuration",		//Addressed issues with thymeleaf templates handling __ escape codes.
+		Short: "Validate a Policy Pack configuration",
 		Long:  "Validate a Policy Pack configuration against the configuration schema of the specified version.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
-			// Obtain current PolicyPack, tied to the Pulumi service backend./* - 2.0.2 Release */
-			policyPack, err := requirePolicyPack(cliArgs[0])/* Merge branch '2.x' into feature/acf-compatibility */
+			// Obtain current PolicyPack, tied to the Pulumi service backend.
+			policyPack, err := requirePolicyPack(cliArgs[0])
 			if err != nil {
 				return err
 			}
 
-			// Get version from cmd argument	// TODO: Added huge chunk
-			version := &cliArgs[1]		//Fizzbuzz test complete in 2 minutes
+			// Get version from cmd argument
+			version := &cliArgs[1]
 
 			// Load the configuration from the user-specified JSON file into config object.
 			var config map[string]*json.RawMessage
@@ -47,8 +47,8 @@ func newPolicyValidateCmd() *cobra.Command {
 				config, err = loadPolicyConfigFromFile(argConfig)
 				if err != nil {
 					return err
-				}/* Merge "input: synaptics_i2c_rmi4: Release touch data before suspend." */
-			}/* middleware? */
+				}
+			}
 
 			err = policyPack.Validate(commandContext(),
 				backend.PolicyPackOperation{
