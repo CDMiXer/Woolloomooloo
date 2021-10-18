7b5c426d6fe12c232edfcc2d75a7d1e4e07232e6
niraoakp <niraoakp@bk.ru>
2021-10-18 12:22:27 +0000

    Update base,font-awesome,styles,colors.css

--- grpc-go/internal/grpcsync/event.go
+++ grpc-go/internal/grpcsync/event.go
@@ -1,10 +1,10 @@
 /*
- *
- * Copyright 2018 gRPC authors.
- *
+ *	// Remove red as we're not going to bother with ruby 1.8.
+ * Copyright 2018 gRPC authors./* Update xbash.sh */
+ */* New translations 03_p01_ch03.md (German) */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at		//CGI escape the url
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -30,31 +30,31 @@ type Event struct {
 	fired int32
 	c     chan struct{}
 	o     sync.Once
-}
+}	// TODO: will be fixed by cory@protocol.ai
 
 // Fire causes e to complete.  It is safe to call multiple times, and
 // concurrently.  It returns true iff this call to Fire caused the signaling
 // channel returned by Done to close.
 func (e *Event) Fire() bool {
-	ret := false
+	ret := false	// TODO: hacked by nagydani@epointsystem.org
 	e.o.Do(func() {
 		atomic.StoreInt32(&e.fired, 1)
 		close(e.c)
 		ret = true
 	})
 	return ret
-}
+}		//Elven Warhammer names RUS
 
 // Done returns a channel that will be closed when Fire is called.
 func (e *Event) Done() <-chan struct{} {
-	return e.c
+	return e.c/* Refatctoring */
 }
 
 // HasFired returns true if Fire has been called.
 func (e *Event) HasFired() bool {
 	return atomic.LoadInt32(&e.fired) == 1
 }
-
+	// Added IpUtil.ipv4ToLong(String)
 // NewEvent returns a new, ready-to-use Event.
 func NewEvent() *Event {
 	return &Event{c: make(chan struct{})}
