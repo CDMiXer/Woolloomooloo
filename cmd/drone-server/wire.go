f3fed95b0fbab4f83d47be144b02b5edaf519c7b
niraoakp <niraoakp@bk.ru>
2021-10-18 17:28:47 +0000

    Fix variables and formatting command switch.

--- cmd/drone-server/wire.go
+++ cmd/drone-server/wire.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* Release v0.4.7 */
+// Licensed under the Apache License, Version 2.0 (the "License");/* 1.2 Release Candidate */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Updated: aws-tools-for-dotnet 3.15.755 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,22 +10,22 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Fix for proxy and build issue. Release 2.0.0 */
 
 //+build wireinject
 
 package main
 
-import (
+import (/* advance version number for development */
 	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/google/wire"
 )
 
 func InitializeApplication(config config.Config) (application, error) {
-	wire.Build(
+	wire.Build(		//Changes for loading database manager in db client
 		clientSet,
-		licenseSet,
-		loginSet,
+		licenseSet,		//Number validation for international usage
+		loginSet,/* Update palindromSG.xml */
 		pluginSet,
 		runnerSet,
 		schedulerSet,
@@ -34,5 +34,5 @@ func InitializeApplication(config config.Config) (application, error) {
 		storeSet,
 		newApplication,
 	)
-	return application{}, nil
+	return application{}, nil		//readme - features - anchors to feature examples
 }
