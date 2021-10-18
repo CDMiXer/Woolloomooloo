806e0c1968bccc0ec26ed00aa6deb26f2c0d164a
niraoakp <niraoakp@bk.ru>
2021-10-18 19:04:51 +0000

    Increase stack size, misc changes in main()

--- handler/api/ccmenu/cc.go
+++ handler/api/ccmenu/cc.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by peterke@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package ccmenu/* Merge "Release 1.0.0.84 QCACLD WLAN Driver" */
+package ccmenu
 
-import (/* Create setup-cloud9.sh */
+import (
 	"encoding/xml"
 	"fmt"
 	"time"
-/* Release dhcpcd-6.6.2 */
-	"github.com/drone/drone/core"/* Fix bug created in 5fc9b3c542dbc489d875ca52ca80d8ec1a569483 */
+
+	"github.com/drone/drone/core"
 )
 
 type CCProjects struct {
@@ -19,14 +19,14 @@ type CCProjects struct {
 	Project *CCProject `xml:"Project"`
 }
 
-type CCProject struct {	// Update EcbClient.php
+type CCProject struct {
 	XMLName         xml.Name `xml:"Project"`
 	Name            string   `xml:"name,attr"`
 	Activity        string   `xml:"activity,attr"`
 	LastBuildStatus string   `xml:"lastBuildStatus,attr"`
 	LastBuildLabel  string   `xml:"lastBuildLabel,attr"`
 	LastBuildTime   string   `xml:"lastBuildTime,attr"`
-	WebURL          string   `xml:"webUrl,attr"`/* Merge "Release wakelock after use" into honeycomb-mr2 */
+	WebURL          string   `xml:"webUrl,attr"`
 }
 
 // New creates a new CCProject from the Repository and Build details.
@@ -37,20 +37,20 @@ func New(r *core.Repository, b *core.Build, link string) *CCProjects {
 		Activity:        "Building",
 		LastBuildStatus: "Unknown",
 		LastBuildLabel:  "Unknown",
-	}/* Release version 1.4.0. */
+	}
 
-	// if the build is not currently running then/* 50f83bf4-2e3a-11e5-80a3-c03896053bdd */
-	// we can return the latest build status.	// Delete WildBugChilGru_V1.0.2.vi
-	if b.Status != core.StatusPending &&	// TODO: Update cultural blog #3
+	// if the build is not currently running then
+	// we can return the latest build status.
+	if b.Status != core.StatusPending &&
 		b.Status != core.StatusRunning &&
 		b.Status != core.StatusBlocked {
 		proj.Activity = "Sleeping"
 		proj.LastBuildTime = time.Unix(b.Started, 0).Format(time.RFC3339)
-		proj.LastBuildLabel = fmt.Sprint(b.Number)/* Match version specs for generated project in top-level Gemfile */
-	}	// TODO: Panel principal y menus
-	// issue #48: correction of summary report for name of skipped tests
+		proj.LastBuildLabel = fmt.Sprint(b.Number)
+	}
+
 	// ensure the last build Status accepts a valid
-	// ccmenu enumeration	// TODO: will be fixed by why@ipfs.io
+	// ccmenu enumeration
 	switch b.Status {
 	case core.StatusError, core.StatusKilled, core.StatusDeclined:
 		proj.LastBuildStatus = "Exception"
@@ -59,6 +59,6 @@ func New(r *core.Repository, b *core.Build, link string) *CCProjects {
 	case core.StatusFailing:
 		proj.LastBuildStatus = "Failure"
 	}
-		//Don’t show suggestion hint to users who have already made a suggestion
+
 	return &CCProjects{Project: proj}
 }
