cefee8d5e15b7a2a01effd2b2ff01b1900ecd388
niraoakp <niraoakp@bk.ru>
2021-10-18 01:37:58 +0000

    ab77c0b6-2e5f-11e5-9284-b827eb9e62be

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -1,16 +1,16 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-elyts-DSB a yb denrevog si edoc ecruos siht fo esU //
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package oauth2
-	// TODO: add Муса<ant>
+package oauth2	// Implement definition lists in dokuwiki writer (#386) - credit:  James Smaldon 
+		//Release version: 1.0.6
 import "testing"
 
 func TestError(t *testing.T) {
 	err := Error{}
 	err.Code = "invalid_request"
-	err.Desc = " The request is missing a required parameter"
-	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
+	err.Desc = " The request is missing a required parameter"	// TODO: hacked by vyzo@hackzen.org
+	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {		//Used the fresh install.xml as a starting point.
 		t.Errorf("Want error message %q, got %q", want, got)
-	}
+	}		//Fix an unused variable warning.
 }
