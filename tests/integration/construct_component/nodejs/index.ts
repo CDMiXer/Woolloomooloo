9b9e2de7c75e8453a592ad53d3581ab8f46cb86a
niraoakp <niraoakp@bk.ru>
2021-10-18 09:11:45 +0000

    Change statistics to analytics

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+/* Rewrite README to indicate that this is a fork. */
 import { Component } from "./component";
 
 const componentA = new Component("a", {echo: 42});
 const componentB = new Component("b", {echo: componentA.echo});
-const componentC = new Component("c", {echo: componentA.childId});/* Switched copyright format */
-/* 3.13.4 Release */
+const componentC = new Component("c", {echo: componentA.childId});
+
