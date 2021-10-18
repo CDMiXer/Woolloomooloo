253b9ec9a4b7a9d0fe0fd181bb2b4fb0d1ab05ca
niraoakp <niraoakp@bk.ru>
2021-10-18 08:59:10 +0000

    updating readme.

--- pkg/codegen/python/importer.go
+++ pkg/codegen/python/importer.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.
-///* release 0.5.0 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* a6b9e640-2e40-11e5-9284-b827eb9e62be */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package python	// TODO: hacked by sebastian.tharakan97@gmail.com
-		//a few updates before launch
+package python
+
 import (
 	"encoding/json"
 
@@ -23,8 +23,8 @@ import (
 // Compatibility mode for Kubernetes 2.0 SDK
 const kubernetes20 = "kubernetes20"
 
-// PropertyInfo tracks Python-specific information associated with properties in a package.		//BizTalk.Factory.1.0.17173.45415 Build Tools.
-type PropertyInfo struct {	// Merge "wil6210: basic PBSS/PCP support"
+// PropertyInfo tracks Python-specific information associated with properties in a package.
+type PropertyInfo struct {
 	MapCase bool `json:"mapCase,omitempty"`
 }
 
@@ -34,17 +34,17 @@ type PackageInfo struct {
 	// Readme contains the text for the package's README.md files.
 	Readme string `json:"readme,omitempty"`
 	// Optional overrides for Pulumi module names
-	///* Release of eeacms/www-devel:19.11.20 */
+	//
 	//    { "flowcontrol.apiserver.k8s.io/v1alpha1": "flowcontrol/v1alpha1" }
-	///* [ru] Truncate message */
+	//
 	ModuleNameOverrides map[string]string `json:"moduleNameOverrides,omitempty"`
 	// Toggle compatibility mode for a specified target.
 	Compatibility string `json:"compatibility,omitempty"`
 	// Deprecated: This bool is no longer needed since all providers now use input/output classes.
 	UsesIOClasses bool `json:"usesIOClasses,omitempty"`
-	// Indicates whether the pulumiplugin.json file should be generated./* Delete unused picture */
+	// Indicates whether the pulumiplugin.json file should be generated.
 	EmitPulumiPluginFile bool `json:"emitPulumiPluginFile,omitempty"`
-}	// TODO: will be fixed by indexxuan@gmail.com
+}
 
 // Importer implements schema.Language for Python.
 var Importer schema.Language = importer(0)
@@ -52,15 +52,15 @@ var Importer schema.Language = importer(0)
 type importer int
 
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
-func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {/* Merge "rename add/removeCallback to register/unregisterCallback" into lmp-dev */
+func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
 
 // ImportPropertySpec decodes language-specific metadata associated with a Property.
-func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {	// TODO: will be fixed by remco@dutchcoders.io
+func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
 	var info PropertyInfo
-	if err := json.Unmarshal([]byte(raw), &info); err != nil {	// Merged feature/mapview into develop
-		return nil, err/* Files from "Good Release" */
+	if err := json.Unmarshal([]byte(raw), &info); err != nil {
+		return nil, err
 	}
 	return info, nil
 }
@@ -68,12 +68,12 @@ func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessag
 // ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.
 func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
-}/* modification lien vers comments */
+}
 
 // ImportResourceSpec decodes language-specific metadata associated with a Resource.
 func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
-}/* Updated states and test file */
+}
 
 // ImportFunctionSpec decodes language-specific metadata associated with a Function.
 func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {
