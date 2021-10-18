c0eea7d8966e9644f453441175da058bbe4e5ff4
niraoakp <niraoakp@bk.ru>
2021-10-18 21:50:10 +0000

    Add current pod version badge to README

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Update gen_specializers.lua */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Merge branch 'master' of https://github.com/hdecarne/certmgr.git */
-package postgres	// Update typo at README.md
+
+package postgres
 
 //go:generate togo ddl -package postgres -dialect postgres
