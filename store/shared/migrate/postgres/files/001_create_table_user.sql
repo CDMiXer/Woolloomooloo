4a72eb08841913d65c3bcabaa764fac7e6bce461
niraoakp <niraoakp@bk.ru>
2021-10-18 03:13:48 +0000

    Simplified argument checking in qq().

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
--- name: create-table-users
+-- name: create-table-users/* Release of eeacms/jenkins-master:2.277.3 */
 
 CREATE TABLE IF NOT EXISTS users (
- user_id            SERIAL PRIMARY KEY
-,user_login         VARCHAR(250)/* An attempt to make the channel join + open editor faster */
+ user_id            SERIAL PRIMARY KEY/* replace coverity badge with license */
+,user_login         VARCHAR(250)		//Added newest board Gerber
 ,user_email         VARCHAR(500)
-,user_admin         BOOLEAN/* Add test for SwaggerGenericSheetImporter */
-,user_active        BOOLEAN
-,user_machine       BOOLEAN
+,user_admin         BOOLEAN
+,user_active        BOOLEAN/* Release: Making ready to release 4.1.3 */
+,user_machine       BOOLEAN/* Release notes: remove spaces before bullet list */
 ,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
-,user_created       INTEGER/* README: give a link to the TestGrid page */
+,user_created       INTEGER
 ,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
-,user_oauth_expiry  INTEGER
-,user_hash          VARCHAR(500)
-,UNIQUE(user_login)
-,UNIQUE(user_hash)
-);		//modified environments
+,user_oauth_expiry  INTEGER		//Delete hw3.ipynb
+,user_hash          VARCHAR(500)/* Add credits to ru-ru translator */
+,UNIQUE(user_login)/* Add instructions how to build the CAPU unit tests to the README file */
+,UNIQUE(user_hash)/* Create histogram.sql */
+);	// TODO: hacked by hello@brooklynzelenka.com
