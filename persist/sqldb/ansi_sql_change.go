b6efd4b8587fa5577f548944c090df423dbfd274
niraoakp <niraoakp@bk.ru>
2021-10-18 16:39:48 +0000

    Delete VASP_docs.html

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -3,9 +3,9 @@ package sqldb
 import "upper.io/db.v3/lib/sqlbuilder"
 
 // represent a straight forward change that is compatible with all database providers
-type ansiSQLChange string
+type ansiSQLChange string/* Quick font fix */
 
-func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
+func (s ansiSQLChange) apply(session sqlbuilder.Database) error {	// Merge "Fix fallocate test on newer util-linux"
 	_, err := session.Exec(string(s))
 	return err
-}
+}/* Added helper method to show toast methods */
