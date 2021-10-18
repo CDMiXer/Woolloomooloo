3a35bb1f6e7341e0e4abd44913ee8295fa18e181
niraoakp <niraoakp@bk.ru>
2021-10-18 00:32:48 +0000

    Create bifrozt_fw_v0.0.6.sh

--- chain/actors/builtin/paych/message3.go
+++ chain/actors/builtin/paych/message3.go
@@ -4,45 +4,45 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"		//interface update
-	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"		//add ode_options to class
-	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"		//removed debug constraint
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
+	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)		//LSTM graph has a memory leaking
+)
 
 type message3 struct{ from address.Address }
 
 func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych3.ConstructorParams{From: m.from, To: to})/* Release Ver. 1.5.5 */
-	if aerr != nil {	// Merge "Updated user_add_user_message_long"
+	params, aerr := actors.SerializeParams(&paych3.ConstructorParams{From: m.from, To: to})
+	if aerr != nil {
 		return nil, aerr
 	}
 	enc, aerr := actors.SerializeParams(&init3.ExecParams{
 		CodeCID:           builtin3.PaymentChannelActorCodeID,
-		ConstructorParams: params,		//Delete created_by.jpg
+		ConstructorParams: params,
 	})
 	if aerr != nil {
-		return nil, aerr/* Some initial database stuff, and a Seen module */
+		return nil, aerr
 	}
 
 	return &types.Message{
-		To:     init_.Address,	// TODO: Adding integrations
+		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
-		Method: builtin3.MethodsInit.Exec,/* npm package phantomjs is deprecated */
+		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
 	}, nil
-}/* Initial commit on ideas documentation file */
+}
 
 func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{/* Release V.1.2 */
+	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{
 		Sv:     *sv,
-		Secret: secret,		//New interactive Weights connectivity map fully working
+		Secret: secret,
 	})
-	if aerr != nil {		//fixed URL blog post
+	if aerr != nil {
 		return nil, aerr
 	}
 
