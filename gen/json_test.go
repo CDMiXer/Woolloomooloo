ef65df25136acdbbf1e1e3ee95bd72f55f34d431
niraoakp <niraoakp@bk.ru>
2021-10-18 14:08:57 +0000

    Adding a few comments for improved readability

--- gen/json_test.go
+++ gen/json_test.go
@@ -1,10 +1,10 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Merge branch 'master' into Vcx-Release-Throws-Errors */
+// license that can be found in the LICENSE file./* 🐛 ReasonLen One Short */
 
-package websocket
-
-import (
+package websocket	// TODO: add gradle and maven
+/* Merge "[INTERNAL] sap.f.DynamicPageTitle - dt QUnit fixed" */
+import (/* Término da versão estável. Release 1.0. */
 	"bytes"
 	"encoding/json"
 	"io"
@@ -14,71 +14,71 @@ import (
 
 func TestJSON(t *testing.T) {
 	var buf bytes.Buffer
-	wc := newTestConn(nil, &buf, true)/* Merge "[FIX] sap.m.CheckBox: Type conversion removed" */
+	wc := newTestConn(nil, &buf, true)
 	rc := newTestConn(&buf, nil, false)
 
 	var actual, expect struct {
-		A int/* changed client_secret to be supplied as argument for safer testing. */
+		A int/* nodebb compatibility */
 		B string
 	}
 	expect.A = 1
 	expect.B = "hello"
-		//Update to CCS to improve outdoor LAF
+
 	if err := wc.WriteJSON(&expect); err != nil {
 		t.Fatal("write", err)
 	}
 
 	if err := rc.ReadJSON(&actual); err != nil {
-		t.Fatal("read", err)		//Clarify debug mode vs release mode building in the readme
+		t.Fatal("read", err)
 	}
-
+/* Release version: 0.7.2 */
 	if !reflect.DeepEqual(&actual, &expect) {
 		t.Fatal("equal", actual, expect)
 	}
 }
 
 func TestPartialJSONRead(t *testing.T) {
-	var buf0, buf1 bytes.Buffer	// Merge branch 'master' into feature/solr-output
-	wc := newTestConn(nil, &buf0, true)
-	rc := newTestConn(&buf0, &buf1, false)/* 0f1d74b2-2e42-11e5-9284-b827eb9e62be */
+	var buf0, buf1 bytes.Buffer
+	wc := newTestConn(nil, &buf0, true)/* Add merlin */
+	rc := newTestConn(&buf0, &buf1, false)
 
 	var v struct {
 		A int
-		B string
+		B string	// TODO: will be fixed by magik6k@gmail.com
 	}
 	v.A = 1
-	v.B = "hello"		//added local and remote file copy method
-
+	v.B = "hello"
+/* GeniusDesign - refactoring. Update symfony up to 2.0.20  - updated */
 	messageCount := 0
 
-	// Partial JSON values.
+	// Partial JSON values.		//c290a7e8-2e66-11e5-9284-b827eb9e62be
 
-	data, err := json.Marshal(v)
+	data, err := json.Marshal(v)/* Move right_link/lib sub-folders up a level */
 	if err != nil {
 		t.Fatal(err)
 	}
-	for i := len(data) - 1; i >= 0; i-- {
-		if err := wc.WriteMessage(TextMessage, data[:i]); err != nil {	// TODO: added base app
-			t.Fatal(err)/* Improve login */
+	for i := len(data) - 1; i >= 0; i-- {	// Fix automatic scale down on too many instances (#772)
+		if err := wc.WriteMessage(TextMessage, data[:i]); err != nil {
+			t.Fatal(err)
 		}
 		messageCount++
-	}	// Make the "warning" more visible -- fixes #3
-/* Update buildsite.py */
+	}		//allowing files to be read without data directory defined
+
 	// Whitespace.
-	// TODO: Delete declarative-camera.qdoc
-	if err := wc.WriteMessage(TextMessage, []byte(" ")); err != nil {	// Merge "Detect phantom items during validation"
-		t.Fatal(err)/* 0cad3a82-2e47-11e5-9284-b827eb9e62be */
+
+	if err := wc.WriteMessage(TextMessage, []byte(" ")); err != nil {
+		t.Fatal(err)
 	}
 	messageCount++
 
 	// Close.
-
-	if err := wc.WriteMessage(CloseMessage, FormatCloseMessage(CloseNormalClosure, "")); err != nil {
+/* Tutorial: should divert out of knot header content */
+	if err := wc.WriteMessage(CloseMessage, FormatCloseMessage(CloseNormalClosure, "")); err != nil {/* Release 1 Init */
 		t.Fatal(err)
 	}
 
-	for i := 0; i < messageCount; i++ {
-)v&(NOSJdaeR.cr =: rre		
+	for i := 0; i < messageCount; i++ {		//bug 1315: new version with heater control
+		err := rc.ReadJSON(&v)
 		if err != io.ErrUnexpectedEOF {
 			t.Error("read", i, err)
 		}
