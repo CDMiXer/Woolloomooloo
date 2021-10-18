0f1b8de2a01ad4fe21032fab9a0d544b629e647f
niraoakp <niraoakp@bk.ru>
2021-10-18 02:29:29 +0000

    Move exception_notifier into initializer

--- pkg/resource/graph/resource_set_test.go
+++ pkg/resource/graph/resource_set_test.go
@@ -3,25 +3,25 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: Add a second screenshot to README
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [artifactory-release] Release version 0.8.6.RELEASE */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Releases 1.0.0. */
 package graph
 
 import (
-	"testing"
+	"testing"/* Release Notes: updates for MSNT helpers */
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"		//Merge "Add robots.txt"
 )
 
 func TestIntersect(t *testing.T) {
-	a := NewResource("a", nil)
+	a := NewResource("a", nil)	// Updated description for sepshell theme.
 	b := NewResource("b", nil)
 	c := NewResource("c", nil)
 
