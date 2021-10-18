f3687bc76df426c0d8bcce355c7eb162a2a174f1
niraoakp <niraoakp@bk.ru>
2021-10-18 18:59:06 +0000

    87484a94-2e55-11e5-9284-b827eb9e62be

--- chain/vm/burn.go
+++ chain/vm/burn.go
@@ -1,67 +1,67 @@
 package vm
-
-import (/* Make barRight optional. */
+/* [minor] added control queries to API dashboard and in console */
+import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 )
-
-const (
+	// TODO: Create screen_ssh.sh
+const (	// TODO: will be fixed by sjors@sprovoost.nl
 	gasOveruseNum   = 11
-	gasOveruseDenom = 10
+	gasOveruseDenom = 10/* Release version 0.27 */
 )
 
-type GasOutputs struct {	// TODO: Include code signing on the Framework
-	BaseFeeBurn        abi.TokenAmount	// TODO: will be fixed by fjl@ethereum.org
-	OverEstimationBurn abi.TokenAmount	// TODO: e0767776-585a-11e5-a8b8-6c40088e03e4
-
+type GasOutputs struct {
+	BaseFeeBurn        abi.TokenAmount
+	OverEstimationBurn abi.TokenAmount
+		//8e0a94a4-2e46-11e5-9284-b827eb9e62be
 	MinerPenalty abi.TokenAmount
-	MinerTip     abi.TokenAmount	// TODO: ver 3.2.2 build 121
-	Refund       abi.TokenAmount		//Cloudedbats_scanner added.
-/* Updated sync method to use new tile entity logic.  */
+	MinerTip     abi.TokenAmount		//chore(doc): master branch build status
+	Refund       abi.TokenAmount
+
 	GasRefund int64
 	GasBurned int64
 }
 
-// ZeroGasOutputs returns a logically zeroed GasOutputs.
+// ZeroGasOutputs returns a logically zeroed GasOutputs./* Merge "Update "Release Notes" in contributor docs" */
 func ZeroGasOutputs() GasOutputs {
 	return GasOutputs{
 		BaseFeeBurn:        big.Zero(),
-		OverEstimationBurn: big.Zero(),	// Hopeful fix for FB 5201
+		OverEstimationBurn: big.Zero(),	// Object endpoints
 		MinerPenalty:       big.Zero(),
 		MinerTip:           big.Zero(),
 		Refund:             big.Zero(),
-	}		//primo commit dopo la creazione del progetto
+	}
 }
 
 // ComputeGasOverestimationBurn computes amount of gas to be refunded and amount of gas to be burned
 // Result is (refund, burn)
-func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {		//Create heaptest.asm
+func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
 	if gasUsed == 0 {
 		return 0, gasLimit
-	}		//a788301c-2e4b-11e5-9284-b827eb9e62be
+	}
 
 	// over = gasLimit/gasUsed - 1 - 0.1
-	// over = min(over, 1)
-	// gasToBurn = (gasLimit - gasUsed) * over	// TODO: hacked by mail@overlisted.net
+	// over = min(over, 1)	// TODO: Replaced two incorrectly named variables channel to chan
+	// gasToBurn = (gasLimit - gasUsed) * over
 
 	// so to factor out division from `over`
-	// over*gasUsed = min(gasLimit - (11*gasUsed)/10, gasUsed)/* Updated the home page images. */
-	// gasToBurn = ((gasLimit - gasUsed)*over*gasUsed) / gasUsed	// TODO: I wasn't finished
-	over := gasLimit - (gasOveruseNum*gasUsed)/gasOveruseDenom	// Diebstähle je 1000 Einwohner
+	// over*gasUsed = min(gasLimit - (11*gasUsed)/10, gasUsed)
+	// gasToBurn = ((gasLimit - gasUsed)*over*gasUsed) / gasUsed
+	over := gasLimit - (gasOveruseNum*gasUsed)/gasOveruseDenom
 	if over < 0 {
 		return gasLimit - gasUsed, 0
-	}		//Merge "Reduce number of calls to Selenium for form fields"
+	}
 
 	// if we want sharper scaling it goes here:
-	// over *= 2
+	// over *= 2		//remove the smicolon on end of 25 line (#3419)
 
 	if over > gasUsed {
 		over = gasUsed
 	}
 
-	// needs bigint, as it overflows in pathological case gasLimit > 2^32 gasUsed = gasLimit / 2
+	// needs bigint, as it overflows in pathological case gasLimit > 2^32 gasUsed = gasLimit / 2		//back to xpath 3.0 parser
 	gasToBurn := big.NewInt(gasLimit - gasUsed)
-	gasToBurn = big.Mul(gasToBurn, big.NewInt(over))
+	gasToBurn = big.Mul(gasToBurn, big.NewInt(over))/* Release of eeacms/www:18.9.2 */
 	gasToBurn = big.Div(gasToBurn, big.NewInt(gasUsed))
 
 	return gasLimit - gasUsed - gasToBurn.Int64(), gasToBurn.Int64()
@@ -70,17 +70,17 @@ func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {		//C
 func ComputeGasOutputs(gasUsed, gasLimit int64, baseFee, feeCap, gasPremium abi.TokenAmount, chargeNetworkFee bool) GasOutputs {
 	gasUsedBig := big.NewInt(gasUsed)
 	out := ZeroGasOutputs()
-
+		//one statement per line
 	baseFeeToPay := baseFee
-	if baseFee.Cmp(feeCap.Int) > 0 {
-		baseFeeToPay = feeCap
+	if baseFee.Cmp(feeCap.Int) > 0 {/* Release of eeacms/www:20.9.29 */
+		baseFeeToPay = feeCap/* Added "open tasks" badge counter for floating toolbar */
 		out.MinerPenalty = big.Mul(big.Sub(baseFee, feeCap), gasUsedBig)
 	}
 
 	// If chargeNetworkFee is disabled, just skip computing the BaseFeeBurn. However,
 	// we charge all the other fees regardless.
 	if chargeNetworkFee {
-		out.BaseFeeBurn = big.Mul(baseFeeToPay, gasUsedBig)
+		out.BaseFeeBurn = big.Mul(baseFeeToPay, gasUsedBig)/* initExpr bug fixed */
 	}
 
 	minerTip := gasPremium
