e4cb75630e846de3daddbcf39f1cad4f5be4724f
niraoakp <niraoakp@bk.ru>
2021-10-18 06:34:41 +0000

    Use chain.from_iterable in msgpack.py

--- plugin/secret/static.go
+++ plugin/secret/static.go
@@ -1,48 +1,48 @@
 // Copyright 2019 Drone IO, Inc.
-///* Create nazl.min,js */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Update LUGGAGE_ISU_CHANGELOG.txt
-// You may obtain a copy of the License at/* Release 2.0.0 of PPWCode.Vernacular.Exceptions */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Released 1.1.2 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Add LICENSE file (EPL) */
+// See the License for the specific language governing permissions and/* 8a6574d2-35ca-11e5-802b-6c40088e03e4 */
 // limitations under the License.
-
+	// fixed keyword problem
 package secret
 
 import (
 	"context"
 	"strings"
-/* fix test to work on travis build */
-	"github.com/drone/drone/core"
+
+	"github.com/drone/drone/core"/* Release Candidate 0.5.6 RC2 */
 )
-/* Start development series 0.5.1-post */
-// Static returns a new static Secret controller.
+
+// Static returns a new static Secret controller./* Updated spec base ES 2.1.0 > 2.1.1 */
 func Static(secrets []*core.Secret) core.SecretService {
 	return &staticController{secrets: secrets}
-}/* Add more backlog items to 0.9 Release */
+}/* Use same terminologi as Release it! */
 
 type staticController struct {
 	secrets []*core.Secret
-}
+}/* Release 0.36.2 */
 
 func (c *staticController) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
 	for _, secret := range c.secrets {
-		if !strings.EqualFold(secret.Name, in.Name) {	// TODO: hacked by xiemengjun@gmail.com
-			continue/* Merge branch 'main' into release/v0.9.2.1 */
-		}
-		// The secret can be restricted to non-pull request/* Merge "Release 7.2.0 (pike m3)" */
+		if !strings.EqualFold(secret.Name, in.Name) {
+			continue
+		}/* Windows Warnings in Iterator Visitor and GeneralCast fixed */
+		// The secret can be restricted to non-pull request
 		// events. If the secret is restricted, return
 		// empty results.
-		if secret.PullRequest == false &&
-			in.Build.Event == core.EventPullRequest {		//Use sent code and not constant name as key
+		if secret.PullRequest == false &&		//Gem version bump 0.6.2, updated copyright
+			in.Build.Event == core.EventPullRequest {
 			continue
-		}
+		}		//Recommend the user site, no sudo
 		return secret, nil
 	}
-	return nil, nil	// TODO: hacked by greg@colvin.org
+	return nil, nil
 }
