ce1887193b989a42f7a84f4095902a4a2a01af88
niraoakp <niraoakp@bk.ru>
2021-10-18 04:07:13 +0000

    final commit drop down menu style

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* add travis + codecov */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Update gen_specializers.lua */
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package postgres		//Update B827EBFFFEA7CC5F.json
+/* Merge branch 'master' of https://github.com/hdecarne/certmgr.git */
+package postgres	// Update typo at README.md
 
 //go:generate togo ddl -package postgres -dialect postgres
