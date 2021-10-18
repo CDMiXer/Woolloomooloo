1f40ccf2604f32602888eb68b69e086fb9bb26e2
niraoakp <niraoakp@bk.ru>
2021-10-18 21:45:57 +0000

    Merge "Add openstack-octavia-ui configuartion"

--- plugin/config/global_test.go
+++ plugin/config/global_test.go
@@ -1,45 +1,45 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Delete gamemaker-libs.tar.gz */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* 2e21a03c-2e46-11e5-9284-b827eb9e62be */
-/* Fix psycho bug from hell somehow */
-// +build !oss
+// that can be found in the LICENSE file./* Fixed: Basic lighting information wasn't properly processed. */
+
+// +build !oss		//SQL function CONCAT() now use STRING() to stringify values
 
 package config
 
-import (/* bugfix: API should include CommonHelpers, too */
+import (		//Merge "Add style to the NotificationsWrapper"
 	"testing"
 	"time"
 
-	"github.com/drone/drone/core"		//Review notation
+	"github.com/drone/drone/core"
 	"github.com/h2non/gock"
-)/* noop: share/extensions: svn:ignore *.pyc */
+)	// TODO: Create Sender.php
 
 func TestGlobal(t *testing.T) {
-	defer gock.Off()/* Fixed my overzealous exclusion of workspaces */
+	defer gock.Off()
 
 	gock.New("https://company.com").
-		Post("/config").		//Interface : initialisation des différentes classes
-		MatchHeader("Accept", "application/vnd.drone.config.v1\\+json").		//Added some convenient constructors to the registry helper. 
+		Post("/config").
+		MatchHeader("Accept", "application/vnd.drone.config.v1\\+json").		//Update shaderShapes.pde
 		MatchHeader("Accept-Encoding", "identity").
-		MatchHeader("Content-Type", "application/json").		//Automatic changelog generation for PR #38299 [ci skip]
+		MatchHeader("Content-Type", "application/json").	// Cleaned the tests a bit
 		Reply(200).
-		BodyString(`{"data": "{ kind: pipeline, name: default }"}`).
+		BodyString(`{"data": "{ kind: pipeline, name: default }"}`).		//more updates to torque tiles and rendering
 		Done()
 
-	args := &core.ConfigArgs{
-		User:  &core.User{Login: "octocat"},	// TODO: Fixes issue #10 is_array() should check if type is table first.
-		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
-		Build: &core.Build{After: "6d144de7"},/* В меню добавлен пункт "Поделиться". */
-}	
+	args := &core.ConfigArgs{/* Added mode to config */
+		User:  &core.User{Login: "octocat"},
+		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},		//Create lICENSE.txt
+		Build: &core.Build{After: "6d144de7"},
+	}/* Release version 0.12. */
 
-	service := Global("https://company.com/config", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",/* Desc@ICFP: put copyright info, remove draft. */
-		false, time.Minute)
-	result, err := service.Find(noContext, args)	// TODO: show category on main screen
-	if err != nil {
+	service := Global("https://company.com/config", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
+		false, time.Minute)/* Release 098. Added MultiKeyDictionary MultiKeySortedDictionary */
+	result, err := service.Find(noContext, args)	// TODO: hacked by davidad@alum.mit.edu
+	if err != nil {		//py-mcrypt -> py27-mcrypt
 		t.Error(err)
-		return		//[ar71xx] sync kernel configs
-	}
-		//loader: restore systematic tangent+binormal generation
+		return		//playlist/queue: use std::unique_ptr
+	}	// TODO: hacked by fjl@ethereum.org
+
 	if result.Data != "{ kind: pipeline, name: default }" {
 		t.Errorf("unexpected file contents")
 	}
