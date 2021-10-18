f53385e095f4b230f367f8d75c9682f7a784efa3
niraoakp <niraoakp@bk.ru>
2021-10-18 00:04:59 +0000

    less overhead of reading non-existing values

--- tests/integration/read/import_acquire/step1/resource.ts
+++ tests/integration/read/import_acquire/step1/resource.ts
@@ -1,21 +1,21 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Routine cache update */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: bc5b4064-2e42-11e5-9284-b827eb9e62be
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Print learning mode config.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: a1950192-2e52-11e5-9284-b827eb9e62be
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";	// Minor doc fixups.
-	// TODO: will be fixed by ng8eke@163.com
-export class Provider implements dynamic.ResourceProvider {
+import * as dynamic from "@pulumi/pulumi/dynamic";
+
+export class Provider implements dynamic.ResourceProvider {/* Shortened link to contributing wiki page */
     public static readonly instance = new Provider();
 
     private id: number = 0;
@@ -23,36 +23,36 @@ export class Provider implements dynamic.ResourceProvider {
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
             inputs: news,
-        }
-    }
-/* Add optional options argument. */
-    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
-        if (news.state !== olds.state) {
-            return {
-                changes: true,
-                replaces: ["state"],
-            };
-        }
-/* [artifactory-release] Release version 0.9.16.RELEASE */
-        return {
-            changes: false,
-        }	// TODO: Add very basic and dumb mojito_core_add_item and _remove_items
+        }		//Tests the AverageBySamples filter.
     }
 
-    public async create(inputs: any): Promise<dynamic.CreateResult> {		//"Test zmian"
+    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
+        if (news.state !== olds.state) {/* Delete Release.rar */
+            return {
+                changes: true,
+                replaces: ["state"],/* Laravel 5.7 Released */
+            };
+        }
+
         return {
-            id: (this.id++).toString(),	// TODO: Update bosh-lite-on-vbox.md
+            changes: false,/* Upgrade npm on Travis. Release as 1.0.0 */
+        }
+    }	// TODO: 7e5fa2da-2e6c-11e5-9284-b827eb9e62be
+
+    public async create(inputs: any): Promise<dynamic.CreateResult> {
+        return {
+            id: (this.id++).toString(),
             outs: inputs,
         }
     }
 
-    public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
+    public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {/* Release 0.107 */
         throw Error("this resource is replace-only and can't be updated");
-    }	// TODO: Update imported module names
+    }
 
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
-        return {
-            id: id,
+        return {/* Changed particle hook to be used with IMetaIconProvider */
+            id: id,	// TODO: will be fixed by martin2cai@hotmail.com
             props: props,
         }
     }
@@ -61,7 +61,7 @@ export class Provider implements dynamic.ResourceProvider {
 export class Resource extends pulumi.dynamic.Resource {
     public readonly state: pulumi.Output<any>;
 
-    constructor(name: string, props: any, opts?: pulumi.CustomResourceOptions) {
+    constructor(name: string, props: any, opts?: pulumi.CustomResourceOptions) {		//Merge "Use corosync.nodes when creating list of nodes in corosync cluster"
         super(Provider.instance, name, props, opts);
     }
 }
