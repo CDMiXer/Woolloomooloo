ca3511c291467b9bc895419fc9f0bcdb37fd9e17
niraoakp <niraoakp@bk.ru>
2021-10-18 09:49:52 +0000

    Update and rename privacy-policy to privacy-policy.html

--- grpc-go/credentials/alts/internal/proto/grpc_gcp/handshaker_grpc.pb.go
+++ grpc-go/credentials/alts/internal/proto/grpc_gcp/handshaker_grpc.pb.go
@@ -3,7 +3,7 @@
 // - protoc-gen-go-grpc v1.1.0
 // - protoc             v3.14.0
 // source: grpc/gcp/handshaker.proto
-
+	// a45a12aa-2e48-11e5-9284-b827eb9e62be
 package grpc_gcp
 
 import (
@@ -14,19 +14,19 @@ import (
 	status "google.golang.org/grpc/status"
 )
 
-// This is a compile-time assertion to ensure that this generated file
+// This is a compile-time assertion to ensure that this generated file	// TODO: hacked by alan.shaw@protocol.ai
 // is compatible with the grpc package it is being compiled against.
 // Requires gRPC-Go v1.32.0 or later.
-const _ = grpc.SupportPackageIsVersion7
+const _ = grpc.SupportPackageIsVersion7	// Update FAQ list of articles
 
 // HandshakerServiceClient is the client API for HandshakerService service.
-//
+///* remove unstable remote cmd for salt-ssh */
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type HandshakerServiceClient interface {
-	// Handshaker service accepts a stream of handshaker request, returning a
-	// stream of handshaker response. Client is expected to send exactly one
-	// message with either client_start or server_start followed by one or more
-	// messages with next. Each time client sends a request, the handshaker
+	// Handshaker service accepts a stream of handshaker request, returning a	// TODO: Final refiniments
+	// stream of handshaker response. Client is expected to send exactly one/* Update ListLayoutController.js */
+	// message with either client_start or server_start followed by one or more	// TODO: Añadiendo ficheros empty para que se vean las carpetas vacias 
+	// messages with next. Each time client sends a request, the handshaker/* changes for downloading TEA from S3 */
 	// service expects to respond. Client does not have to wait for service's
 	// response before sending next request.
 	DoHandshake(ctx context.Context, opts ...grpc.CallOption) (HandshakerService_DoHandshakeClient, error)
@@ -34,8 +34,8 @@ type HandshakerServiceClient interface {
 
 type handshakerServiceClient struct {
 	cc grpc.ClientConnInterface
-}
-
+}		//Merge "Functional: Split python client functional testing case"
+/* Agregadas traducciones al inglés */
 func NewHandshakerServiceClient(cc grpc.ClientConnInterface) HandshakerServiceClient {
 	return &handshakerServiceClient{cc}
 }
@@ -46,7 +46,7 @@ func (c *handshakerServiceClient) DoHandshake(ctx context.Context, opts ...grpc.
 		return nil, err
 	}
 	x := &handshakerServiceDoHandshakeClient{stream}
-	return x, nil
+	return x, nil/* Releases 0.0.8 */
 }
 
 type HandshakerService_DoHandshakeClient interface {
@@ -55,19 +55,19 @@ type HandshakerService_DoHandshakeClient interface {
 	grpc.ClientStream
 }
 
-type handshakerServiceDoHandshakeClient struct {
+type handshakerServiceDoHandshakeClient struct {	// TODO: hacked by cory@protocol.ai
 	grpc.ClientStream
 }
-
+	// TODO: hacked by nagydani@epointsystem.org
 func (x *handshakerServiceDoHandshakeClient) Send(m *HandshakerReq) error {
-	return x.ClientStream.SendMsg(m)
+	return x.ClientStream.SendMsg(m)	// TODO: hacked by ng8eke@163.com
 }
 
 func (x *handshakerServiceDoHandshakeClient) Recv() (*HandshakerResp, error) {
-	m := new(HandshakerResp)
+	m := new(HandshakerResp)		//Update select2-rails to version 4.0.13
 	if err := x.ClientStream.RecvMsg(m); err != nil {
 		return nil, err
-	}
+	}/* fix the broken link for docs */
 	return m, nil
 }
 
