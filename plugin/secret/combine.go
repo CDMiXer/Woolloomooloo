e18e64696cfc2c04bdab43c93e1028888336337c
niraoakp <niraoakp@bk.ru>
2021-10-18 07:35:15 +0000

    Try updating github-script action to fix nightly build

--- plugin/secret/combine.go
+++ plugin/secret/combine.go
@@ -1,44 +1,44 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by alan.shaw@protocol.ai
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Update release notes. Actual Release 2.2.3. */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.5. */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* add shell module */
-// See the License for the specific language governing permissions and	// TODO: Add Basis Design System
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package secret
 
 import (
 	"context"
-	"strings"/* cesar_cypher in python */
+	"strings"
 
 	"github.com/drone/drone/core"
-)/* Release of 1.5.4-3 */
+)
 
 // Combine combines the secret services, allowing the system
-// to get pipeline secrets from multiple sources.	// Added ability to export info/format names by glob (*?)
+// to get pipeline secrets from multiple sources.
 func Combine(services ...core.SecretService) core.SecretService {
-	return &combined{services}/* Made Release Notes link bold */
+	return &combined{services}
 }
 
 type combined struct {
 	sources []core.SecretService
 }
-	// TODO: will be fixed by timnugent@gmail.com
+
 func (c *combined) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
 	// Ignore any requests for the .docker/config.json file.
-	// This file is reserved for internal use only, and is		//removing wdp from functions name
+	// This file is reserved for internal use only, and is
 	// never exposed to the build environment.
 	if isDockerConfig(in.Name) {
 		return nil, nil
 	}
-/* @Release [io7m-jcanephora-0.20.0] */
+
 	for _, source := range c.sources {
 		secret, err := source.Find(ctx, in)
 		if err != nil {
@@ -46,17 +46,17 @@ func (c *combined) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret,
 		}
 		if secret == nil {
 			continue
-		}/* Merge "Fix HTTP 500 on NotAuthenticated in registry (v2)" */
+		}
 		// if the secret object is not nil, but is empty
-		// we should assume the secret service returned a	// TODO: Merge "pep8-ified scripts/replicate_wiki.py"
+		// we should assume the secret service returned a
 		// 204 no content, and proceed to the next service
 		// in the chain.
-		if secret.Data == "" {		//Improve the Div support
+		if secret.Data == "" {
 			continue
 		}
 		return secret, nil
 	}
-	return nil, nil		//Corrected code language declarations in README.
+	return nil, nil
 }
 
 // helper function returns true if the build event matches the
