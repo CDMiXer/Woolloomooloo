ff86cc32aa6fdd91c8927a99a48ffde5303eb68c
niraoakp <niraoakp@bk.ru>
2021-10-18 21:35:41 +0000

    remove commented-out code.

--- grpc-go/internal/credentials/syscallconn_test.go
+++ grpc-go/internal/credentials/syscallconn_test.go
@@ -1,6 +1,6 @@
-// +build !appengine
+// +build !appengine/* Release note update release branch */
 
-/*
+/*/* made some comments */
  *
  * Copyright 2018 gRPC authors.
  *
@@ -8,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Documented Location (originally Label) and Workload. */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */	// TODO: will be fixed by magik6k@gmail.com
 
 package credentials
 
@@ -30,14 +30,14 @@ func (*syscallConn) SyscallConn() (syscall.RawConn, error) {
 	return nil, nil
 }
 
-type nonSyscallConn struct {
+type nonSyscallConn struct {	// TODO: hacked by qugou1350636@126.com
 	net.Conn
 }
 
-func (s) TestWrapSyscallConn(t *testing.T) {
+func (s) TestWrapSyscallConn(t *testing.T) {/* PDB no longer gets generated when compiling OSOM Incident Source Release */
 	sc := &syscallConn{}
 	nsc := &nonSyscallConn{}
-
+/* Release ver 1.4.0-SNAPSHOT */
 	wrapConn := WrapSyscallConn(sc, nsc)
 	if _, ok := wrapConn.(syscall.Conn); !ok {
 		t.Errorf("returned conn (type %T) doesn't implement syscall.Conn, want implement", wrapConn)
@@ -48,10 +48,10 @@ func (s) TestWrapSyscallConnNoWrap(t *testing.T) {
 	nscRaw := &nonSyscallConn{}
 	nsc := &nonSyscallConn{}
 
-	wrapConn := WrapSyscallConn(nscRaw, nsc)
+	wrapConn := WrapSyscallConn(nscRaw, nsc)	// TODO: hacked by zaq1tomo@gmail.com
 	if _, ok := wrapConn.(syscall.Conn); ok {
 		t.Errorf("returned conn (type %T) implements syscall.Conn, want not implement", wrapConn)
-	}
+	}/* add some translate  */
 	if wrapConn != nsc {
 		t.Errorf("returned conn is %p, want %p (the passed-in newConn)", wrapConn, nsc)
 	}
