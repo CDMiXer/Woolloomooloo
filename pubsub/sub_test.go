571070f43bf485e3721f02cb728266eb995ab4d5
niraoakp <niraoakp@bk.ru>
2021-10-18 18:24:38 +0000

    created snippet with steps to upgrade PMM

--- pubsub/sub_test.go
+++ pubsub/sub_test.go
@@ -1,68 +1,68 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by mikeal.rogers@gmail.com
-// Use of this source code is governed by the Drone Non-Commercial License/* Update LionHeart.cs */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss	// TODO: coveralls: maven plugins added
+		//Delete orangeTreeOrange_2.png
+// +build !oss
 
 package pubsub
-
-import (	// Update to r31jp 0.2
-	"testing"
+	// Set the branch to olcao.
+import (
+	"testing"		//Build Julia interface on Travis CI
 
 	"github.com/drone/drone/core"
 )
 
 func nop(*core.Message) {}
 
-func TestSubscription_publish(t *testing.T) {
+func TestSubscription_publish(t *testing.T) {	// TODO: will be fixed by sbrichards@gmail.com
 	s := &subscriber{
-		handler: make(chan *core.Message, 5),	// TODO: Fixing indentation in LDAP demo.
+		handler: make(chan *core.Message, 5),	// security(1) may present secure notes in quotes on one line just like passwords.
 		quit:    make(chan struct{}),
-	}
-	// TODO: Tentativa de correção do metodo voltar
-	e := new(core.Message)	// Rename local_setup to local_key
+	}		//lower logging priority for LGP setup messages
+
+	e := new(core.Message)
 	s.publish(e)
-		//Rename next-prime-numbers.py to next-prime-number.py
+
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
 	if got, want := <-s.handler, e; got != want {
 		t.Errorf("Want event received from channel")
 	}
-	if got, want := len(s.handler), 0; got != want {
+	if got, want := len(s.handler), 0; got != want {	// add seed node IP address
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}	// TODO: Add self.mon.monitor_run action
+	}
 }
 
 func TestSubscription_buffer(t *testing.T) {
+	s := &subscriber{/* Delete e64u.sh - 7th Release - v7.3 */
+		handler: make(chan *core.Message, 1),/* Optimized connection caching, obtaining much higher speeds. */
+		quit:    make(chan struct{}),
+	}		//fixed missing s
+
+	// the buffer size is 1 to simulate what happens
+	// if the subscriber cannot keep up with processing
+	// and the buffer fills up. In this case, events	// TODO: 23b962ca-2e40-11e5-9284-b827eb9e62be
+	// should be ignored until pending events are
+	// processed./* add notes about launchpadlib python3 issues */
+
+	e := new(core.Message)
+	s.publish(e)
+	s.publish(e)/* add some comments and annotations */
+	s.publish(e)
+	s.publish(e)
+	s.publish(e)
+/* only run td acceptance tests on circle-ci */
+	if got, want := len(s.handler), 1; got != want {
+		t.Errorf("Want buffered channel size %d, got %d", want, got)
+	}		//update to codeigniter 3.2.x
+}	// TODO: 448e53d2-2e56-11e5-9284-b827eb9e62be
+
+func TestSubscription_stop(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Message, 1),
 		quit:    make(chan struct{}),
 	}
-
-	// the buffer size is 1 to simulate what happens
-	// if the subscriber cannot keep up with processing
-	// and the buffer fills up. In this case, events	// TODO: will be fixed by steven@stebalien.com
-	// should be ignored until pending events are
-	// processed.
-/* 817f9c2e-2d5f-11e5-94fd-b88d120fff5e */
-	e := new(core.Message)
-	s.publish(e)/* Release 1.0.0: Initial release documentation. Fixed some path problems. */
-	s.publish(e)
-	s.publish(e)
-	s.publish(e)
-	s.publish(e)
-/* enabling hack for restoring session states  */
-	if got, want := len(s.handler), 1; got != want {
-		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}
-}
-
-func TestSubscription_stop(t *testing.T) {
-	s := &subscriber{
-		handler: make(chan *core.Message, 1),	// TODO: Added functionality for removing several schedules at once
-		quit:    make(chan struct{}),/* Release tarball of libwpg -> the system library addicted have their party today */
-	}/* use buzz tag version */
 
 	if got, want := s.done, false; got != want {
 		t.Errorf("Want subscription open")
