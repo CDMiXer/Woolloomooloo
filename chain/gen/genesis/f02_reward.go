800c260ad564261d633a7525e82a6aed9bb90739
niraoakp <niraoakp@bk.ru>
2021-10-18 21:22:33 +0000

    Verbose Object#toString

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,27 +1,27 @@
 package genesis
 
 import (
-	"context"
+	"context"/* Preparing for Market Release 1.2 */
+		//Merge "Add system trust agents on first boot or when adding user" into lmp-dev
+	"github.com/filecoin-project/go-state-types/big"/* Merge branch 'develop' into bug/T213450 */
 
-	"github.com/filecoin-project/go-state-types/big"
-
-	"github.com/filecoin-project/specs-actors/actors/builtin"
+	"github.com/filecoin-project/specs-actors/actors/builtin"		//recognize `__dispose` and `__disposeAsync` as magic methods.
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//Fix issue with undefined index
 )
-
+/* update symengine version */
 func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
-	cst := cbor.NewCborStore(bs)
+	cst := cbor.NewCborStore(bs)		//Upgrade dentaku to version 3.0.0
 
-	st := reward0.ConstructState(qaPower)
-
+	st := reward0.ConstructState(qaPower)	// Some Gui tweaks.
+/* update gradle.build */
 	hcid, err := cst.Put(context.TODO(), st)
 	if err != nil {
-		return nil, err
+		return nil, err/* Fix yarn link */
 	}
 
 	return &types.Actor{
