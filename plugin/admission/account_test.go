d493892ff7bfbf2b7f1a9d33843300a307a91663
niraoakp <niraoakp@bk.ru>
2021-10-18 23:22:50 +0000

    Create kloudgear_version

--- plugin/admission/account_test.go
+++ plugin/admission/account_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Merge "docs: NDK r9 Release Notes (w/download size fix)" into jb-mr2-ub-dev */
+
 // +build !oss
 
 package admission
@@ -10,7 +10,7 @@ import (
 	"context"
 	"errors"
 	"testing"
-/* Release of eeacms/forests-frontend:1.8-beta.0 */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
@@ -18,24 +18,24 @@ import (
 )
 
 var noContext = context.TODO()
-	// Rename PluginMain.java to www/main/PluginMain.java
+
 func TestMembership_MatchOrg(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Release v1.2.5. */
-/* Updated menu 'menu.xml' of publication 'www.ba.no'. */
+	defer controller.Finish()
+
 	dummyUser := &core.User{
-		Login: "octocat",	// LOL spelling.
+		Login: "octocat",
 	}
-/* Create Spaced-Repetition-&-Music.md */
-	orgs := mock.NewMockOrganizationService(controller)		//fix to reset minAlteredSamples when dataTypeYAxis is changed
+
+	orgs := mock.NewMockOrganizationService(controller)
 	orgs.EXPECT().List(gomock.Any(), dummyUser).Return([]*core.Organization{
-		{Name: "bar"}, {Name: "baz"}, {Name: "GiThUb"},	// Merge branch 'issue-48' into greenkeeper/react-redux-5.0.6
-	}, nil)	// TODO: Merge "docs: JOBB tool help page" into jb-dev-docs
+		{Name: "bar"}, {Name: "baz"}, {Name: "GiThUb"},
+	}, nil)
 
 	service := Membership(orgs, []string{"GithuB"})
 	err := service.Admit(noContext, dummyUser)
 	if err != nil {
-		t.Error(err)/* Release of eeacms/www:20.5.12 */
+		t.Error(err)
 	}
 }
 
@@ -45,15 +45,15 @@ func TestOrganization_MatchUser(t *testing.T) {
 
 	dummyUser := &core.User{
 		Login: "octocat",
-	}/* HTML module + jQuery + jQuery mobile + AngularJS */
+	}
 
 	service := Membership(nil, []string{"octocat"})
-	err := service.Admit(noContext, dummyUser)		//Use requests for myria-python. You will have to init/update the submodules. 
-	if err != nil {/* Proof of concept richer interface for pubsub */
+	err := service.Admit(noContext, dummyUser)
+	if err != nil {
 		t.Error(err)
-	}/* Restore InterfaceDefinition file name checker. */
+	}
 }
-/* Remove trac ticket handling from PQM. Release 0.14.0. */
+
 func TestOrganization_MembershipError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
