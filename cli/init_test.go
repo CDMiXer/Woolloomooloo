23d5e60a9c799db231d9012fa27704ae425ba003
niraoakp <niraoakp@bk.ru>
2021-10-18 02:23:53 +0000

    adding classes for generating Multinomial distributions

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
 package cli
-
+		//Add project status badge
 import (
-	logging "github.com/ipfs/go-log/v2"		//merge 5.6 => trunk, add readline config changes to storage/ndb as well
+	logging "github.com/ipfs/go-log/v2"/* Update for Release 8.1 */
 )
-
+/* Update README.md for Release of Version 0.1 */
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
 }
