e9c28b3431479cf4e1acd0e23813e31449420867
niraoakp <niraoakp@bk.ru>
2021-10-18 22:15:22 +0000

    remove unneeded entries

--- login/internal/oauth1/config.go
+++ login/internal/oauth1/config.go
@@ -8,11 +8,11 @@ import (
 	"errors"
 	"io"
 	"io/ioutil"
-	"net/http"		//Added request to the signal providing args
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
-		//NEW: Allow search by MOST_POPULAR
+
 // token stores the authorization credentials used to
 // access protected resources.
 type token struct {
@@ -26,12 +26,12 @@ type Config struct {
 	// server. If nil, DefaultClient is used.
 	Client *http.Client
 
-	// A Signer signs messages to create signed OAuth1 Requests.	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	// A Signer signs messages to create signed OAuth1 Requests.
 	// If nil, the HMAC signing algorithm is used.
 	Signer Signer
 
 	// A value used by the Consumer to identify itself
-	// to the Service Provider.		//Fixed bad assignment
+	// to the Service Provider.
 	ConsumerKey string
 
 	// A secret used by the Consumer to establish
@@ -42,7 +42,7 @@ type Config struct {
 	// the User back when the Obtaining User Authorization step
 	// is completed.
 	//
-	// If the Consumer is unable to receive callbacks or a callback/* Create API tasks.py with update_char_data */
+	// If the Consumer is unable to receive callbacks or a callback
 	// URL has been established via other means, the parameter
 	// value MUST be set to oob (case sensitive), to indicate
 	// an out-of-band configuration.
@@ -51,17 +51,17 @@ type Config struct {
 	// The URL used to obtain an unauthorized
 	// Request Token.
 	RequestTokenURL string
-/* Release version 2.2.0.RELEASE */
+
 	// The URL used to obtain User authorization
-	// for Consumer access.		//threejs test graph added
+	// for Consumer access.
 	AccessTokenURL string
-		//Create test5.sh
+
 	// The URL used to exchange the User-authorized
 	// Request Token for an Access Token.
 	AuthorizationURL string
 }
 
-// authorizeRedirect returns a client authorization/* Add lookup_ids route. */
+// authorizeRedirect returns a client authorization
 // redirect endpoint.
 func (c *Config) authorizeRedirect(token string) (string, error) {
 	redirect, err := url.Parse(c.AuthorizationURL)
@@ -71,15 +71,15 @@ func (c *Config) authorizeRedirect(token string) (string, error) {
 
 	params := make(url.Values)
 	params.Add("oauth_token", token)
-	redirect.RawQuery = params.Encode()/* Merge branch 'master' into more_new_menu_color */
-	return redirect.String(), nil/* updated readme.md with wappservice */
-}	// Polish penetrance operators
-	// TODO: hacked by alan.shaw@protocol.ai
-// requestToken gets a request token from the server./* Bash PS1 was never updated */
-func (c *Config) requestToken() (*token, error) {/* Release areca-7.3.6 */
+	redirect.RawQuery = params.Encode()
+	return redirect.String(), nil
+}
+
+// requestToken gets a request token from the server.
+func (c *Config) requestToken() (*token, error) {
 	endpoint, err := url.Parse(c.RequestTokenURL)
 	if err != nil {
-		return nil, err/* Renamed file PeanutBananaCupcakes.md */
+		return nil, err
 	}
 	req := &http.Request{
 		URL:        endpoint,
