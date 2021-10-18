8c1cd25f4442433d5016f37dfa2666f007373253
niraoakp <niraoakp@bk.ru>
2021-10-18 20:19:41 +0000

    Install make as depends on

--- handler/api/users/repos.go
+++ handler/api/users/repos.go
@@ -2,53 +2,53 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// flip over to row-major matrices
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Enhance commons generation
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Merge "In Python3.7 async is a keyword [1]"
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Create udp.c
+// limitations under the License.
 
-package users	// TODO: will be fixed by mowrain@yandex.com
-
+package users
+	// Create make.rb
 import (
-	"net/http"
-/* Release 1.0.0 !! */
+	"net/http"	// Add numeral system for user management
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// Add StringReceiver
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
-
-// HandleRepoList returns an http.HandlerFunc that writes a json-encoded		//Corrected modif date
-// list of all user repositories to the response body.
-func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.HandlerFunc {		//Merge "iPXE ISO Ramdisk booting"
+/* Release v0.0.8 */
+// HandleRepoList returns an http.HandlerFunc that writes a json-encoded
+// list of all user repositories to the response body.	// TODO: hacked by 13860583249@yeah.net
+func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		login := chi.URLParam(r, "user")		//discrete probability distributions
+		login := chi.URLParam(r, "user")
 
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
-			render.NotFound(w, err)	// TODO: 75f6b4e2-2e73-11e5-9284-b827eb9e62be
+			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("user", login).
 				Debugln("api: cannot find user")
-			return/* getItemObject now looks through the inventory as well as the room */
+			return
 		}
-
+	// TODO: Move npm install before code upload
 		repos, err := repos.List(r.Context(), user.ID)
-		if err != nil {
+		if err != nil {/* Rename README.md to 1strand004.py */
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("user", login).
 				Warnln("api: cannot list user repositories")
-		} else {
+		} else {		//- typing mistake fix
 			render.JSON(w, repos, 200)
-		}	// Sorting links switch between asc and desc
-	}/* Recognize contributions from Ngô Huy, Nguyễn Hà Dương, Hà Quang Dương */
+		}
+	}
 }
