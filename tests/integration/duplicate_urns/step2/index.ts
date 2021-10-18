0945b34e4811036eaff5cdaf96afe9607af24f9b
niraoakp <niraoakp@bk.ru>
2021-10-18 04:03:02 +0000

    language tweak for reminder emails

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// Update vib.py
+//	// TODO: will be fixed by timnugent@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Create Valgrind suppression file for library memory issues. */
 // You may obtain a copy of the License at
-//	// TODO: Set SQLite as default database and convert actual flat to sqlite
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release LastaThymeleaf-0.2.2 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";	// TODO: Comment out unused AudacityProject::OnSplitLabelsToTracks().
-
-// Setup for the next test.
+import { Resource } from "./resource";
+	// refactored translations in JS
+// Setup for the next test.	// 40811156-2e3f-11e5-9284-b827eb9e62be
 const a = new Resource("a", { state: 4 });
 
