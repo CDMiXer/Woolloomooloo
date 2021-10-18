eb90c37e2b07cdff3bf1d219071878b6008b7e1b
niraoakp <niraoakp@bk.ru>
2021-10-18 23:29:58 +0000

    Updating translations for po/nb.po

--- core/license_test.go
+++ core/license_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* VersaloonProRelease3 hardware update, add RDY/BSY signal to EBI port */
 // that can be found in the LICENSE file.
 
 // +build !oss
