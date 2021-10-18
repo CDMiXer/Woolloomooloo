7bf31fa55a5246dbcb8dd0af2fa13f4cf9ad3abc
niraoakp <niraoakp@bk.ru>
2021-10-18 17:45:58 +0000

    englishify

--- handler/api/system/none.go
+++ handler/api/system/none.go
@@ -4,38 +4,38 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* cleanup the field allocate to form ajax message */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* add widget API calls */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-/* Release 0.8.1 Alpha */
+
 package system
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: will be fixed by witek@enjin.io
+	"github.com/drone/drone/handler/api/render"
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {/* Changed name from info to Helper, to better define the scope. */
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
 // HandleLicense returns a no-op http.HandlerFunc.
 func HandleLicense(license core.License) http.HandlerFunc {
 	return notImplemented
-}/* Mitaka Release */
+}
 
 // HandleStats returns a no-op http.HandlerFunc.
 func HandleStats(
 	core.BuildStore,
-	core.StageStore,	// TODO: hacked by why@ipfs.io
+	core.StageStore,
 	core.UserStore,
 	core.RepositoryStore,
 	core.Pubsub,
