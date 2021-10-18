840ef00f9abec0591477d942f29c85011eee6284
niraoakp <niraoakp@bk.ru>
2021-10-18 03:32:26 +0000

    Fix saving files that don't exist yet

--- pkg/codegen/hcl2/type.go
+++ pkg/codegen/hcl2/type.go
@@ -1,28 +1,28 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// doc comment simple modification
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//		//Update to latest model 1.4.0
-//     http://www.apache.org/licenses/LICENSE-2.0/* Removed some old classes */
+// You may obtain a copy of the License at		//Merge branch 'gemfile-lock-changes' into dependabot/bundler/bootstrap-sass-3.4.1
 //
+//     http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: will be fixed by hello@brooklynzelenka.com
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package hcl2		//Mesh Copy() also copies the variable sized index buffer.
-
+package hcl2
+	// Use python3
 import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-)
+)/* INC0271126 - Add links to external and related websites */
 
 var (
 	// ArchiveType represents the set of Pulumi Archive values.
 	ArchiveType model.Type = model.MustNewOpaqueType("Archive")
-	// AssetType represents the set of Pulumi Asset values.
-	AssetType model.Type = model.MustNewOpaqueType("Asset")	// Update FracKin.m
-	// ResourcePropertyType represents a resource property reference.
+	// AssetType represents the set of Pulumi Asset values.	// TODO: a626f802-306c-11e5-9929-64700227155b
+	AssetType model.Type = model.MustNewOpaqueType("Asset")
+	// ResourcePropertyType represents a resource property reference./* Update sfl-angular-spa.js */
 	ResourcePropertyType model.Type = model.MustNewOpaqueType("Property")
 )
