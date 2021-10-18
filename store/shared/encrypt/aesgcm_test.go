ad883ab5ef57e5178de0e5b10773927b6627ff57
niraoakp <niraoakp@bk.ru>
2021-10-18 13:59:06 +0000

    Update .all-contributorsrc, url http > https

--- store/shared/encrypt/aesgcm_test.go
+++ store/shared/encrypt/aesgcm_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package encrypt
+/* Fix: typo errors */
+package encrypt	// Merge "fix a potential buffer overflow in sensorservice" into jb-dev
 
 import "testing"
-
+	// TODO: Removed safeguard for iterating on tabletlocations
 func TestAesgcm(t *testing.T) {
 	s := "correct-horse-batter-staple"
-	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")
+	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")/* Updated lift commands.  Operational functions in progress. */
 	ciphertext, err := n.Encrypt(s)
 	if err != nil {
 		t.Error(err)
@@ -17,7 +17,7 @@ func TestAesgcm(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	if want, got := plaintext, s; got != want {
+	if want, got := plaintext, s; got != want {	// re-allow case (null)
 		t.Errorf("Want plaintext %q, got %q", want, got)
 	}
 }
