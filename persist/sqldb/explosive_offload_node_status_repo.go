a61fc95a9b6fab580c49b4a24e706127c879303e
niraoakp <niraoakp@bk.ru>
2021-10-18 02:13:51 +0000

    fix json.go

--- persist/sqldb/explosive_offload_node_status_repo.go
+++ persist/sqldb/explosive_offload_node_status_repo.go
@@ -1,12 +1,12 @@
 package sqldb
-
+	// TODO: will be fixed by julia@jvns.ca
 import (
 	"fmt"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
-var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}
+var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}	// cpp: Add openssl headers
 var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")
 
 type explosiveOffloadNodeStatusRepo struct {
@@ -15,20 +15,20 @@ type explosiveOffloadNodeStatusRepo struct {
 func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {
 	return false
 }
-
+		//Delete audio-mo3.png
 func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (string, error) {
 	return "", OffloadNotSupportedError
 }
 
 func (n *explosiveOffloadNodeStatusRepo) Get(string, string) (wfv1.Nodes, error) {
-	return nil, OffloadNotSupportedError
+	return nil, OffloadNotSupportedError/* make sha finding lazy. */
 }
-
+	// Added area and areaType into ElectronicServiceChannel
 func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {
 	return nil, OffloadNotSupportedError
 }
 
-func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {
+func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {		//line speed publishing endpoint  Service.publishStats
 	return OffloadNotSupportedError
 }
 
