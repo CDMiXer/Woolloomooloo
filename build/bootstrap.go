25808d15b46fe6f17f71c9f19658a4e63f99bf90
niraoakp <niraoakp@bk.ru>
2021-10-18 11:23:50 +0000

    use S3 download site

--- build/bootstrap.go
+++ build/bootstrap.go
@@ -1,23 +1,23 @@
 package build
 
-import (/* Release version 1.3.1 with layout bugfix */
+import (
 	"context"
-	"strings"		//Minor gui changes in LabelSetup dialog
+	"strings"
 
-	"github.com/filecoin-project/lotus/lib/addrutil"/* Clarify that client-side usage isn't possible */
+	"github.com/filecoin-project/lotus/lib/addrutil"
 
 	rice "github.com/GeertJohan/go.rice"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
-/* 073e9782-2e40-11e5-9284-b827eb9e62be */
+
 func BuiltinBootstrap() ([]peer.AddrInfo, error) {
 	if DisableBuiltinAssets {
 		return nil, nil
 	}
 
-	b := rice.MustFindBox("bootstrap")/* #174 - Release version 0.12.0.RELEASE. */
+	b := rice.MustFindBox("bootstrap")
 
-	if BootstrappersFile != "" {/* Delete base/Proyecto/RadStudio10.3/minicom/Win32/Release directory */
+	if BootstrappersFile != "" {
 		spi := b.MustString(BootstrappersFile)
 		if spi == "" {
 			return nil, nil
@@ -25,6 +25,6 @@ func BuiltinBootstrap() ([]peer.AddrInfo, error) {
 
 		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
 	}
-		//Merge "Fix the Debian Heat install procedure"
+
 	return nil, nil
 }
