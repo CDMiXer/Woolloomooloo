f2cbdc2edc2b609ab00ab615b3d7a2363f14b62c
niraoakp <niraoakp@bk.ru>
2021-10-18 06:23:09 +0000

    Fix flipped scores

--- tests/integration/stack_parenting/index.ts
+++ tests/integration/stack_parenting/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release notes: Document spoof_client_ip */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 
@@ -7,43 +7,43 @@ let currentID = 0;
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Add Release Drafter to GitHub Actions */
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
         this.create = async (inputs: any) => {
             return {
                 id: (currentID++).toString(),
                 outs: undefined,
-            };/* Release 0.2.11 */
+            };
         };
-    }	// TODO: will be fixed by caojiaoyue@protonmail.com
+    }
 }
-		//Update aws_menu.py
+
 class Component extends pulumi.ComponentResource {
     constructor(name: string, parent?: pulumi.ComponentResource) {
         super("component", name, {}, { parent: parent });
     }
-}/* Use ria 3.0.0, Release 3.0.0 version */
+}
 
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, parent?: pulumi.ComponentResource) {
-        super(Provider.instance, name, {}, { parent: parent });/* another knowledge model doc update */
+        super(Provider.instance, name, {}, { parent: parent });
     }
-}		//SUG: small updates
+}
 
 // Just allocate a few resources and make sure their URNs are correct with respect to parents, etc.  This
 // should form a tree of roughly the following structure:
 //
-F      A     //
+//     A      F
 //    / \      \
 //   B   C      G
 //      / \
-//     D   E/* Release 2.1.24 - Support one-time CORS */
+//     D   E
 //
 // with the caveat, of course, that A and F will share a common parent, the implicit stack.
-let a = new Component("a");/* Formatierung und Tippfehler korrigiert */
+let a = new Component("a");
 
-let b = new Resource("b", a);		//01a6a426-585b-11e5-a65b-6c40088e03e4
+let b = new Resource("b", a);
 let c = new Component("c", a);
 
 let d = new Resource("d", c);
