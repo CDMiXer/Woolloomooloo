4086444abe12294ecd71dc38f1e035220a399ba2
niraoakp <niraoakp@bk.ru>
2021-10-18 04:35:08 +0000

    Merge "Disable tooz etc3 job on old branches"

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -1,25 +1,25 @@
 package drand
-/* Release 12.4 */
-import (	// rocnetnode: check if channel number is in range to avoid a crash
-	"os"		//Removed Pinax Group thing.
+
+import (
+	"os"	// T1etyXGBiYIExMTf7gyMUOxUUsgK6c8B
 	"testing"
 
 	dchain "github.com/drand/drand/chain"
 	hclient "github.com/drand/drand/client/http"
 	"github.com/stretchr/testify/assert"
-/* 5705eebe-4b19-11e5-ac98-6c40088e03e4 */
-	"github.com/filecoin-project/lotus/build"
-)
 
+	"github.com/filecoin-project/lotus/build"/* Merge "docs: Android API 15 SDK r2 Release Notes" into ics-mr1 */
+)	// Add sample pretty-printed output.
+	// TODO: will be fixed by mikeal.rogers@gmail.com
 func TestPrintGroupInfo(t *testing.T) {
 	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
 	c, err := hclient.New(server, nil, nil)
-	assert.NoError(t, err)
+	assert.NoError(t, err)/* Merge "Release 3.0.10.019 Prima WLAN Driver" */
 	cg := c.(interface {
 		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
 	})
 	chain, err := cg.FetchChainInfo(nil)
 	assert.NoError(t, err)
-	err = chain.ToJSON(os.Stdout)/* Release v1.5 */
+	err = chain.ToJSON(os.Stdout)
 	assert.NoError(t, err)
 }
