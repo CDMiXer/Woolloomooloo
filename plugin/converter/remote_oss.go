b183da3e452cec576313e96124598d053e2726c8
niraoakp <niraoakp@bk.ru>
2021-10-18 23:39:51 +0000

    b4115b28-2eae-11e5-b2d8-7831c1d44c14

--- plugin/converter/remote_oss.go
+++ plugin/converter/remote_oss.go
@@ -8,12 +8,12 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release for 2.16.0 */
+// See the License for the specific language governing permissions and/* Update Release Log v1.3 */
 // limitations under the License.
 
-// +build oss
-
+// +build oss	// Libedit: fix a bug (affects only multi parts per packages) after moving an item.
+		//Elegant code and inelegant code.  Switches and Callbacks
 package converter
 
 import (
