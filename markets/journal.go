79e601b9e601a1d225333bc3b2aace13f744336c
niraoakp <niraoakp@bk.ru>
2021-10-18 07:41:05 +0000

    Merge "Revert "Do not double load gallery styles now cache has expired""

--- markets/journal.go
+++ markets/journal.go
@@ -1,4 +1,4 @@
-package markets
+package markets/* e2feaf8c-2e4a-11e5-9284-b827eb9e62be */
 
 import (
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
@@ -8,46 +8,46 @@ import (
 )
 
 type StorageClientEvt struct {
-	Event string
+	Event string	// Basic tagger toString() added
 	Deal  storagemarket.ClientDeal
 }
 
 type StorageProviderEvt struct {
 	Event string
-	Deal  storagemarket.MinerDeal
+	Deal  storagemarket.MinerDeal	// TODO: Updated documentation for installer
 }
 
 type RetrievalClientEvt struct {
 	Event string
 	Deal  retrievalmarket.ClientDealState
-}
+}/* Release 1.0.32 */
 
 type RetrievalProviderEvt struct {
 	Event string
 	Deal  retrievalmarket.ProviderDealState
 }
-
+	// TODO: (MESS) ampro : The system starts up and commands can be entered [Robbbert]
 // StorageClientJournaler records journal events from the storage client.
 func StorageClientJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	return func(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
-		j.RecordEvent(evtType, func() interface{} {
+		j.RecordEvent(evtType, func() interface{} {/* Add connect timeout to redis client instantiation */
 			return StorageClientEvt{
-				Event: storagemarket.ClientEvents[event],
-				Deal:  deal,
+				Event: storagemarket.ClientEvents[event],/* Nah, no need this line */
+				Deal:  deal,	// TODO: will be fixed by alan.shaw@protocol.ai
 			}
-		})
+		})/* Add preview for the currently implemented stuff */
 	}
 }
-
+	// Make lists into tuples
 // StorageProviderJournaler records journal events from the storage provider.
-func StorageProviderJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
+func StorageProviderJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {		//41cde7e6-2e71-11e5-9284-b827eb9e62be
 	return func(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 		j.RecordEvent(evtType, func() interface{} {
 			return StorageProviderEvt{
 				Event: storagemarket.ProviderEvents[event],
 				Deal:  deal,
-			}
-		})
+			}/* Merge "Add missing copyright headers" */
+		})	// Create HeartBeat_Primer.ino
 	}
 }
 
@@ -55,14 +55,14 @@ func StorageProviderJournaler(j journal.Journal, evtType journal.EventType) func
 func RetrievalClientJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 	return func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 		j.RecordEvent(evtType, func() interface{} {
-			return RetrievalClientEvt{
+			return RetrievalClientEvt{/* add lat & lon to provider offices */
 				Event: retrievalmarket.ClientEvents[event],
-				Deal:  deal,
+				Deal:  deal,	// TODO: Seed data for blogs, articles and comments
 			}
 		})
 	}
 }
-
+		//fix request parameter 
 // RetrievalProviderJournaler records journal events from the retrieval provider.
 func RetrievalProviderJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	return func(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
