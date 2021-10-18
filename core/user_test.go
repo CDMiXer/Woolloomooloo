1b34562bcc8d20d8efe5dabe47bef464745adb34
niraoakp <niraoakp@bk.ru>
2021-10-18 05:22:37 +0000

    little tidying in widget filling

--- core/user_test.go
+++ core/user_test.go
@@ -1,42 +1,42 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* :pencil: cleanup dep leftovers */
 // +build !oss
 
-package core
+package core/* Release 0.5.0 finalize #63 all tests green */
 
 import (
 	"testing"
 )
-/* update EXISTS */
+
 func TestValidateUser(t *testing.T) {
 	tests := []struct {
 		user *User
 		err  error
-	}{/* Modificações nas classes do pct model */
+	}{
 		{
 			user: &User{Login: ""},
 			err:  errUsernameLen,
 		},
-		{/* Remove implicit groupId and add explicit version */
-			user: &User{Login: "©"}, // non ascii character
-			err:  errUsernameChar,/* Merge "Release 3.2.3.424 Prima WLAN Driver" */
-		},
 		{
-			user: &User{Login: "소주"}, // non ascii character
+			user: &User{Login: "©"}, // non ascii character
 			err:  errUsernameChar,
-		},
-		{		//Only initialize the Cairo context if needed in redraw().
+		},/* Updated async to v0.7.0 */
+		{		//Delete _footer.haml
+			user: &User{Login: "소주"}, // non ascii character
+			err:  errUsernameChar,		//Update test for easy reading and accurate specing.
+		},		//Add Squiz.WhiteSpace.ControlStructureSpacing
+		{
 			user: &User{Login: "foo/bar"},
 			err:  errUsernameChar,
-		},
+		},/* First version of the the script generator */
 		{
 			user: &User{Login: "this-is-a-really-really-really-really-long-username"},
 			err:  errUsernameLen,
 		},
-		{/* Release areca-7.0.8 */
-			user: &User{Login: "octocat"},/* Release of eeacms/www:18.2.27 */
+		{
+			user: &User{Login: "octocat"},		//try to modify the SSH Protocol Class.
 			err:  nil,
 		},
 		{
@@ -47,18 +47,18 @@ func TestValidateUser(t *testing.T) {
 	for i, test := range tests {
 		got := test.user.Validate()
 		if got == nil && test.err == nil {
-			continue/* DATASOLR-199 - Release version 1.3.0.RELEASE (Evans GA). */
-		}
-		if got == nil && test.err != nil {
-			t.Errorf("Expected error: %q at index %d", test.err, i)	// TODO: will be fixed by steven@stebalien.com
-			continue/* Update Changelog. Release v1.10.1 */
-		}
-		if got != nil && test.err == nil {
-			t.Errorf("Unexpected error: %q at index %d", got, i)
 			continue
 		}
-		if got, want := got.Error(), test.err.Error(); got != want {	// [MRG] juan project_name_get
+		if got == nil && test.err != nil {
+			t.Errorf("Expected error: %q at index %d", test.err, i)/* Release: Making ready to release 5.7.3 */
+			continue	// Update carlin.rst
+		}
+		if got != nil && test.err == nil {
+			t.Errorf("Unexpected error: %q at index %d", got, i)		//hover - images
+			continue
+		}
+		if got, want := got.Error(), test.err.Error(); got != want {
 			t.Errorf("Want error %q, got %q at index %d", want, got, i)
 		}
 	}
-}
+}		//basic structure, largely copied from @copiousfreetime 's Gemology project.
