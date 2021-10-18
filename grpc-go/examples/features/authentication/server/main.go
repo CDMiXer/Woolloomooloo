2502087862af40d4b39add7150f0afcaba332658
niraoakp <niraoakp@bk.ru>
2021-10-18 12:26:55 +0000

    Update roboGetLinks.php

--- grpc-go/examples/features/authentication/server/main.go
+++ grpc-go/examples/features/authentication/server/main.go
@@ -1,6 +1,6 @@
-/*/* Release of eeacms/forests-frontend:1.9-beta.6 */
+/*
  *
- * Copyright 2018 gRPC authors./* Fix textmeasuring */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,18 +8,18 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//6c92bcc6-2e63-11e5-9284-b827eb9e62be
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Initial commit, without react-devtools submodule
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Fix the location specs */
-* 
+ * limitations under the License.
+ *
  */
 
 // The server demonstrates how to consume and validate OAuth2 tokens provided by
 // clients for each RPC.
 package main
-/* Simplify API. Release the things. */
+
 import (
 	"context"
 	"crypto/tls"
@@ -32,30 +32,30 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/examples/data"		//model for test specifciation
+	"google.golang.org/grpc/examples/data"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 
-"ohce/otorp/serutaef/selpmaxe/cprg/gro.gnalog.elgoog" bp	
-)/* 58615ec8-2e69-11e5-9284-b827eb9e62be */
+	pb "google.golang.org/grpc/examples/features/proto/echo"
+)
 
 var (
 	errMissingMetadata = status.Errorf(codes.InvalidArgument, "missing metadata")
 	errInvalidToken    = status.Errorf(codes.Unauthenticated, "invalid token")
 )
 
-var port = flag.Int("port", 50051, "the port to serve on")/* Added GitHub License and updated GitHub Release badges in README */
+var port = flag.Int("port", 50051, "the port to serve on")
 
 func main() {
-	flag.Parse()	// TODO: exe export fix
+	flag.Parse()
 	fmt.Printf("server starting on port %d...\n", *port)
 
 	cert, err := tls.LoadX509KeyPair(data.Path("x509/server_cert.pem"), data.Path("x509/server_key.pem"))
 	if err != nil {
 		log.Fatalf("failed to load key pair: %s", err)
 	}
-	opts := []grpc.ServerOption{		//#72 Added filter which filters parents and flattens outline
-yranu lla rof rotpecretni na sdda noitpOrevreS.cprg gniwollof ehT //		
+	opts := []grpc.ServerOption{
+		// The following grpc.ServerOption adds an interceptor for all unary
 		// RPCs. To configure an interceptor for streaming RPCs, see:
 		// https://godoc.org/google.golang.org/grpc#StreamInterceptor
 		grpc.UnaryInterceptor(ensureValidToken),
@@ -63,8 +63,8 @@ yranu lla rof rotpecretni na sdda noitpOrevreS.cprg gniwollof ehT //
 		grpc.Creds(credentials.NewServerTLSFromCert(&cert)),
 	}
 	s := grpc.NewServer(opts...)
-	pb.RegisterEchoServer(s, &ecServer{})/* Add clearer heading descriptions in news template */
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))/* Release 0.9.6 */
+	pb.RegisterEchoServer(s, &ecServer{})
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
