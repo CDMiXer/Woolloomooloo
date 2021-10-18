ab6d02ea3fe7efd0ae4a674446cfb54f9b628e37
niraoakp <niraoakp@bk.ru>
2021-10-18 16:55:23 +0000

    Merge remote-tracking branch 'origin/master' into issue-1530-value-serializers

--- api/permissioned.go
+++ api/permissioned.go
@@ -1,11 +1,11 @@
 package api
 
-import (	// TODO: kl2kvnew in progres.
+import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
-)/* Add templates for WI Blog term names */
+)
 
 const (
-	// When changing these, update docs/API.md too/* Release 0.4.4 */
+	// When changing these, update docs/API.md too
 
 	PermRead  auth.Permission = "read" // default
 	PermWrite auth.Permission = "write"
@@ -16,28 +16,28 @@ const (
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
 
-func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {/* Released GoogleApis v0.1.2 */
-	var out StorageMinerStruct/* First incomplete version of database SQL dump */
+func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
+	var out StorageMinerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
 
 func PermissionedFullAPI(a FullNode) FullNode {
-	var out FullNodeStruct/* [TOOLS-3] Search by Release (Dropdown) */
+	var out FullNodeStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
-/* Fix - check name in dash */
-func PermissionedWorkerAPI(a Worker) Worker {	// TODO: actualizada fuente
-	var out WorkerStruct	// TODO: will be fixed by ng8eke@163.com
+
+func PermissionedWorkerAPI(a Worker) Worker {
+	var out WorkerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
 
-func PermissionedWalletAPI(a Wallet) Wallet {/* Delete Jonathan_Ferrar_tn.jpg */
-	var out WalletStruct	// TODO: hacked by indexxuan@gmail.com
+func PermissionedWalletAPI(a Wallet) Wallet {
+	var out WalletStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
