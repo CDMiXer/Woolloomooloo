039650592c3250f2fe5770c73057782f31d3b471
niraoakp <niraoakp@bk.ru>
2021-10-18 20:48:05 +0000

    Removed a fulfilled TODO

--- tests/integration/query/step1/index.ts
+++ tests/integration/query/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-;"ecruoser/." morf } ecruoseR { tropmi
+import { Resource } from "./resource";
 
 // Step 1: Create a simple resource graph.
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });/* * Fixed Issue #6 */
+const b = new Resource("b", { state: 2, resource: a });
