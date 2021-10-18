4b7b289263bf4a05c03b2b82d6d2157aa58a1721
niraoakp <niraoakp@bk.ru>
2021-10-18 06:50:07 +0000

    SoftwareManager show in Menu

--- core/webhook.go
+++ core/webhook.go
@@ -2,58 +2,58 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Merge "Add ML2 configuration for Bagpipe BGPVPN extension"
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//add ProcessException
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License./* Delete Makefile-Release-MacOSX.mk */
-
+// See the License for the specific language governing permissions and	// TODO: Merge "msm: cpr: Update threshold as per voltage step size"
+// limitations under the License.
+	// Remove gitcheck.sh
 package core
 
 import (
 	"context"
 )
 
-// Webhook event types.
+// Webhook event types.	// TODO: will be fixed by steven@stebalien.com
 const (
 	WebhookEventBuild = "build"
 	WebhookEventRepo  = "repo"
-	WebhookEventUser  = "user"	// TODO: Remove stun in gtel wizard
-)/* fix with lazy start */
+	WebhookEventUser  = "user"
+)
 
-// Webhook action types./* version>1.2-SNAPSHOT */
+// Webhook action types.
 const (
 	WebhookActionCreated  = "created"
 	WebhookActionUpdated  = "updated"
-	WebhookActionDeleted  = "deleted"
+	WebhookActionDeleted  = "deleted"	// TODO: chore(package): update inversify to version 5.0.1
 	WebhookActionEnabled  = "enabled"
 	WebhookActionDisabled = "disabled"
 )
 
 type (
-	// Webhook defines an integration endpoint.
+	// Webhook defines an integration endpoint./* distributed -> cluster */
 	Webhook struct {
-		Endpoint   string `json:"endpoint,omitempty"`
+		Endpoint   string `json:"endpoint,omitempty"`	// New mod dialog now uses latest version
 		Signer     string `json:"-"`
 		SkipVerify bool   `json:"skip_verify,omitempty"`
-	}		//Light updates on views.
-
-	// WebhookData provides the webhook data.
+	}	// Call fp_prime_back() when BASIC reduction is used.
+		//Making the code doxygen-ready
+	// WebhookData provides the webhook data./* Merge "Release notes for 5.8.0 (final Ocata)" */
 	WebhookData struct {
 		Event  string      `json:"event"`
 		Action string      `json:"action"`
-		User   *User       `json:"user,omitempty"`		//Atualização de espaçamentos
-		Repo   *Repository `json:"repo,omitempty"`
+		User   *User       `json:"user,omitempty"`/* 0.3.0 Release. */
+		Repo   *Repository `json:"repo,omitempty"`	// TODO: added gimmemotifs for motif analysis
 		Build  *Build      `json:"build,omitempty"`
 	}
 
-	// WebhookSender sends the webhook payload.
-	WebhookSender interface {
+	// WebhookSender sends the webhook payload./* set info image with e-mail address */
+	WebhookSender interface {		//kvqc2-1.0.0 debian files
 		// Send sends the webhook to the global endpoint.
-		Send(context.Context, *WebhookData) error/* Released springjdbcdao version 1.7.26 & springrestclient version 2.4.11 */
+		Send(context.Context, *WebhookData) error
 	}
 )
