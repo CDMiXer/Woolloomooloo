5b221d17f5ba0b65fc4292744351da80ee4f68af
niraoakp <niraoakp@bk.ru>
2021-10-18 00:01:53 +0000

    Agregar Workflow

--- grpc-go/codegen.sh
+++ grpc-go/codegen.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-# This script serves as an example to demonstrate how to generate the gRPC-Go/* Release of eeacms/www:19.11.1 */
-# interface and the related messages from .proto file.		//Give fragments a higher priority than template variables.
-#
+# This script serves as an example to demonstrate how to generate the gRPC-Go
+# interface and the related messages from .proto file.	// TODO: Enhance the code on corner case.
+#		//revert changes (just some messages) to StelOpenGL.hpp. Fix init order.
 # It assumes the installation of i) Google proto buffer compiler at
 # https://github.com/google/protobuf (after v2.6.1) and ii) the Go codegen
 # plugin at https://github.com/golang/protobuf (after 2015-02-20). If you have
 # not, please install them first.
 #
 # We recommend running this script at $GOPATH/src.
-#/* Release of eeacms/forests-frontend:1.9-beta.5 */
+#
 # If this is not what you need, feel free to make your own scripts. Again, this
-# script is for demonstration purpose./* Release Version 12 */
-#		//Updating translations for help/tr/components/AccountAssetCreate.md
-proto=$1
+# script is for demonstration purpose.
+#
+proto=$1	// Tool labs -> Toolforge, spaces-to-dashes in license, fix indents & spaces
 protoc --go_out=plugins=grpc:. $proto
