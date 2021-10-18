3c0ee7582b2c683bda6535b0e5bc96209c9eafc7
niraoakp <niraoakp@bk.ru>
2021-10-18 23:28:30 +0000

    Estrutura de carpetas MVC FStudio

--- pkg/codegen/hcl2/output.go
+++ pkg/codegen/hcl2/output.go
@@ -1,45 +1,45 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//	// TODO: Fixed the test cases to meet the new concurrency model.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Fix readme images
-// You may obtain a copy of the License at		// - fixed Numeric box for Mozilla
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release the GIL in all Request methods */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Merge "[glossary] Fix acronym: BMC"
+// limitations under the License.
 
-package hcl2	// listed my interests
+package hcl2
 
-( tropmi
-	"github.com/hashicorp/hcl/v2"	// TODO: Correct spelling in changelog.
-	"github.com/hashicorp/hcl/v2/hclsyntax"	// TODO: Create Encoder.cpp
+import (
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
-/* Ссылка на сайт, в панели управления, перенесена рядом с кнопкой выйти */
+
 // OutputVariable represents a program- or component-scoped output variable.
 type OutputVariable struct {
 	node
 
 	syntax *hclsyntax.Block
-	typ    model.Type/* Fix Release and NexB steps in Jenkinsfile */
-	// 4e09cc10-2e4d-11e5-9284-b827eb9e62be
-	// The definition of the output.		//Push partir de Standard Notes
+	typ    model.Type
+
+	// The definition of the output.
 	Definition *model.Block
 	// The value of the output.
 	Value model.Expression
-}/* Merge "Do not set Jack source version, use default" */
+}
 
 // SyntaxNode returns the syntax node associated with the output variable.
 func (ov *OutputVariable) SyntaxNode() hclsyntax.Node {
 	return ov.syntax
 }
 
-func (ov *OutputVariable) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {		//Conjunctions are now stored in hash table. *bug in hashing*
-	return ov.typ.Traverse(traverser)	// Added broom arm functionality
+func (ov *OutputVariable) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
+	return ov.typ.Traverse(traverser)
 }
 
 func (ov *OutputVariable) VisitExpressions(pre, post model.ExpressionVisitor) hcl.Diagnostics {
