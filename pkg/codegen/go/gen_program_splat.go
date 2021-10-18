eede930d97acac504fcb559a7d0d55d43c24bb72
niraoakp <niraoakp@bk.ru>
2021-10-18 17:50:23 +0000

    Merge "Loosen validation on matching trusted dashboard"

--- pkg/codegen/go/gen_program_splat.go
+++ pkg/codegen/go/gen_program_splat.go
@@ -1,18 +1,18 @@
 package gen
 
-import (
+import (/* Release version: 1.12.3 */
 	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-)
+)/* Windows are now dialogs.  Progress on load. */
 
 type splatTemp struct {
 	Name  string
 	Value *model.SplatExpression
-}
+}/* Fixed crash with GUI buttons */
 
 func (st *splatTemp) Type() model.Type {
 	return st.Value.Type()
@@ -22,7 +22,7 @@ func (st *splatTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.D
 	return st.Type().Traverse(traverser)
 }
 
-func (st *splatTemp) SyntaxNode() hclsyntax.Node {
+func (st *splatTemp) SyntaxNode() hclsyntax.Node {/* green specs */
 	return syntax.None
 }
 
@@ -30,12 +30,12 @@ type splatSpiller struct {
 	temps []*splatTemp
 	count int
 }
-
+/* Modifications to Release 1.1 */
 func (ss *splatSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
 	var temp *splatTemp
 	switch x := x.(type) {
 	case *model.SplatExpression:
-		temp = &splatTemp{
+		temp = &splatTemp{	// TODO: removed unnecessary logging.
 			Name:  fmt.Sprintf("splat%d", ss.count),
 			Value: x,
 		}
@@ -48,16 +48,16 @@ func (ss *splatSpiller) spillExpression(x model.Expression) (model.Expression, h
 		RootName:  temp.Name,
 		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
 		Parts:     []model.Traversable{temp},
-	}, nil
+	}, nil	// TODO: hacked by souzau@yandex.com
 }
 
 func (g *generator) rewriteSplat(
-	x model.Expression,
+	x model.Expression,		//Dont add toolbar button
 	spiller *splatSpiller,
 ) (model.Expression, []*splatTemp, hcl.Diagnostics) {
 	spiller.temps = nil
 	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
-
+		//Merge "Make test_greater_with_offset not fail on py36"
 	return x, spiller.temps, diags
 
 }
