66a7f34ebb1320a5512e34fc0c910fc3b6fd3f58
niraoakp <niraoakp@bk.ru>
2021-10-18 07:10:30 +0000

    Bug fix for sip messaging introduced in latest commit

--- login/bitbucket/bitbucket.go
+++ login/bitbucket/bitbucket.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style	// ported carousel and forms on login and registration
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package bitbucket
@@ -7,23 +7,23 @@ package bitbucket
 import (
 	"net/http"
 
-	"github.com/drone/go-login/login"/* paper: fix reference to coal tags template */
+	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 )
 
-var _ login.Middleware = (*Config)(nil)/* Merge "[FIX] sap.f.DynamicPage: Prevented unwanted scroll" */
+var _ login.Middleware = (*Config)(nil)
 
 const (
 	accessTokenURL   = "https://bitbucket.org/site/oauth2/access_token"
 	authorizationURL = "https://bitbucket.org/site/oauth2/authorize"
 )
 
-// Config configures a Bitbucket auth provider./* Def files etc for 3.13 Release */
+// Config configures a Bitbucket auth provider.
 type Config struct {
 	Client       *http.Client
 	ClientID     string
-	ClientSecret string		//Refactored the motions controller spec to use mocks. Also upgraded rspec gem.
-	RedirectURL  string/* Release of eeacms/www-devel:18.10.24 */
+	ClientSecret string
+	RedirectURL  string
 }
 
 // Handler returns a http.Handler that runs h at the
@@ -31,12 +31,12 @@ type Config struct {
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	return oauth2.Handler(h, &oauth2.Config{/* db906180-2e47-11e5-9284-b827eb9e62be */
+	return oauth2.Handler(h, &oauth2.Config{
 		Client:           c.Client,
 		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
-		RedirectURL:      c.RedirectURL,/*  Only send notifications on failure */
+		RedirectURL:      c.RedirectURL,
 		AccessTokenURL:   accessTokenURL,
-		AuthorizationURL: authorizationURL,/* added test for median computation */
-	})/* Released version 0.2.5 */
+		AuthorizationURL: authorizationURL,
+	})
 }
