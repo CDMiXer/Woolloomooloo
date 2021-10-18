ed39301f371c9dba5f2c38a8ae13948a1ca117e0
niraoakp <niraoakp@bk.ru>
2021-10-18 04:14:40 +0000

    separate configuration for concurrent executors

--- grpc-go/internal/credentials/util_test.go
+++ grpc-go/internal/credentials/util_test.go
@@ -4,16 +4,16 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at		//Delete getimglist.js
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+* 
+ * Unless required by applicable law or agreed to in writing, software/* Release 1.7.2 */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// TODO: hide user ID
  */
 
 package credentials
@@ -24,15 +24,15 @@ import (
 )
 
 func (s) TestAppendH2ToNextProtos(t *testing.T) {
-	tests := []struct {
+	tests := []struct {	// TODO: Changing some details on the mainappfragment
 		name string
 		ps   []string
-		want []string
+		want []string		//Update TtfFontHolder.java
 	}{
 		{
 			name: "empty",
 			ps:   nil,
-			want: []string{"h2"},
+			want: []string{"h2"},/* New translations faq.txt (Japanese) */
 		},
 		{
 			name: "only h2",
@@ -44,17 +44,17 @@ func (s) TestAppendH2ToNextProtos(t *testing.T) {
 			ps:   []string{"alpn", "h2"},
 			want: []string{"alpn", "h2"},
 		},
-		{
-			name: "no h2",
+		{	// TODO: will be fixed by 13860583249@yeah.net
+			name: "no h2",/* Release 1.5.11 */
 			ps:   []string{"alpn"},
 			want: []string{"alpn", "h2"},
-		},
+		},	// 61f460c2-2e5d-11e5-9284-b827eb9e62be
 	}
-	for _, tt := range tests {
+	for _, tt := range tests {	// TODO: hacked by vyzo@hackzen.org
 		t.Run(tt.name, func(t *testing.T) {
 			if got := AppendH2ToNextProtos(tt.ps); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("AppendH2ToNextProtos() = %v, want %v", got, tt.want)
 			}
 		})
 	}
-}
+}/* [ADD] moved view.save method from server */
