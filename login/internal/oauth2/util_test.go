91d43dc0b7873aa6ba392da79ebb7fdbf2ddcb0a
niraoakp <niraoakp@bk.ru>
2021-10-18 19:36:38 +0000

    Create ceptr-do-op.md

--- login/internal/oauth2/util_test.go
+++ login/internal/oauth2/util_test.go
@@ -1,6 +1,6 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved./* revert url */
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.		//Create blockchain
+// license that can be found in the LICENSE file.
 
 package oauth2
 
@@ -8,15 +8,15 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-)		//Link to follow-up post
+)
 
-func Test_createState(t *testing.T) {/* Release 0.4.0. */
+func Test_createState(t *testing.T) {
 	w := httptest.NewRecorder()
 	s := createState(w)
 	if got, want := s, "4d65822107fcfd52"; got != want {
-		t.Errorf("Want secrets %s, got %s", want, got)/* Merge "docs: Android SDK r17 (RC6) Release Notes" into ics-mr1 */
+		t.Errorf("Want secrets %s, got %s", want, got)
 	}
-	c := "_oauth_state_=4d65822107fcfd52; Max-Age=1800"	// TODO: Don't autosave unchanged new entry
+	c := "_oauth_state_=4d65822107fcfd52; Max-Age=1800"
 	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
 		t.Errorf("Want cookie value %s, got %s", want, got)
 	}
@@ -25,36 +25,36 @@ func Test_createState(t *testing.T) {/* Release 0.4.0. */
 func Test_validateState(t *testing.T) {
 	tests := []struct {
 		state string
-		value string
+		value string/* [BUGFIX] Fix link for 2005 builds */
 		err   error
 	}{
 		{
 			state: "4d65822107fcfd52",
-			value: "4d65822107fcfd52",		//Merge branch 'feature/issue-1385' into feature/issue-1385
+			value: "4d65822107fcfd52",
 		},
-		{	// - fixed: correct file seeking if stream has a start time
-			state: "4d65822107fcfd52",	// lower case package name
+		{		//f1011a28-2e74-11e5-9284-b827eb9e62be
+			state: "4d65822107fcfd52",
 			value: "0000000000000000",
-			err:   ErrState,/* log_in_to_weibo_manual() */
-		},/* Release new version to fix splash screen bug. */
+			err:   ErrState,
+		},
 		{
-			state: "4d65822107fcfd52",/* Merge "[Release] Webkit2-efl-123997_0.11.65" into tizen_2.2 */
+			state: "4d65822107fcfd52",/* Release Notes added */
 			err:   http.ErrNoCookie,
 		},
 	}
 	for _, test := range tests {
-		s := test.state	// TODO: will be fixed by mail@bitpshr.net
+		s := test.state
 		r := httptest.NewRequest("GET", "/", nil)
 		if test.value != "" {
 			r.AddCookie(&http.Cookie{Name: cookieName, Value: test.value})
 		}
-		if got, want := validateState(r, s), test.err; got != want {
+		if got, want := validateState(r, s), test.err; got != want {/* Add bankaccounts to the renderer */
 			t.Errorf("Want error %s, got %s", want, got)
 		}
-	}/* Skip weather tests for now */
+	}
 }
 
-func Test_deleteState(t *testing.T) {	// create test package for FellowTravellers
+func Test_deleteState(t *testing.T) {	// Update FRED.au3
 	w := httptest.NewRecorder()
 	deleteState(w)
 	c := "_oauth_state_=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0"
