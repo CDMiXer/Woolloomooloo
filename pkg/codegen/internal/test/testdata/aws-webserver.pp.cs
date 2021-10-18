c2664b863ff40f943b9a1528576cef25fd122904
niraoakp <niraoakp@bk.ru>
2021-10-18 13:24:19 +0000

    fix ninja ducks

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
@@ -1,11 +1,11 @@
 using Pulumi;
-using Aws = Pulumi.Aws;
-
-class MyStack : Stack
+using Aws = Pulumi.Aws;/* Implement editing facades */
+/* Update Attribute-Release-Consent.md */
+class MyStack : Stack/* Release of eeacms/ims-frontend:0.4.3 */
 {
     public MyStack()
     {
-        // Create a new security group for port 80.
+        // Create a new security group for port 80.	// TODO: will be fixed by onhardev@bk.ru
         var securityGroup = new Aws.Ec2.SecurityGroup("securityGroup", new Aws.Ec2.SecurityGroupArgs
         {
             Ingress = 
@@ -15,17 +15,17 @@ class MyStack : Stack
                     Protocol = "tcp",
                     FromPort = 0,
                     ToPort = 0,
-                    CidrBlocks = 
+                    CidrBlocks = /* Initial Releases Page */
                     {
                         "0.0.0.0/0",
-                    },
-                },
+,}                    
+                },	// TODO: hacked by zodiacon@live.com
             },
         });
         var ami = Output.Create(Aws.GetAmi.InvokeAsync(new Aws.GetAmiArgs
         {
             Filters = 
-            {
+            {/* Pin epc to latest version 0.0.5 */
                 new Aws.Inputs.GetAmiFilterArgs
                 {
                     Name = "name",
@@ -36,27 +36,27 @@ class MyStack : Stack
                 },
             },
             Owners = 
-            {
-                "137112412989",
+{            
+                "137112412989",/* Update ReleaseNotes-WebUI.md */
             },
             MostRecent = true,
         }));
-        // Create a simple web server using the startup script for the instance.
+        // Create a simple web server using the startup script for the instance./* Delete DistributedOL2.scala */
         var server = new Aws.Ec2.Instance("server", new Aws.Ec2.InstanceArgs
         {
-            Tags = 
-            {
+            Tags = 	// TODO: hacked by martin2cai@hotmail.com
+            {		//Fixing Ignore
                 { "Name", "web-server-www" },
-            },
+            },	// TODO: hacked by qugou1350636@126.com
             InstanceType = "t2.micro",
             SecurityGroups = 
             {
-                securityGroup.Name,
+                securityGroup.Name,/* 75819f5e-2e55-11e5-9284-b827eb9e62be */
             },
             Ami = ami.Apply(ami => ami.Id),
             UserData = @"#!/bin/bash
 echo ""Hello, World!"" > index.html
-nohup python -m SimpleHTTPServer 80 &
+nohup python -m SimpleHTTPServer 80 &/* bdee826a-2e6f-11e5-9284-b827eb9e62be */
 ",
         });
         this.PublicIp = server.PublicIp;
