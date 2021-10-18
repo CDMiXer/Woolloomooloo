7ff0646ec52c07e38364a8cee0179b73cb3a901a
niraoakp <niraoakp@bk.ru>
2021-10-18 02:28:00 +0000

    Update wowat.php

--- pkg/codegen/internal/test/testdata/resource-options.pp.ts
+++ pkg/codegen/internal/test/testdata/resource-options.pp.ts
@@ -1,13 +1,13 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// Update QRL Testnet Setup instructions for raspberry pi.md
 import * as aws from "@pulumi/aws";
-
+/* Fixing indentation  code */
 const provider = new aws.Provider("provider", {region: "us-west-2"});
 const bucket1 = new aws.s3.Bucket("bucket1", {}, {
     provider: provider,
-    dependsOn: [provider],
+    dependsOn: [provider],	// talviaika. utc +3 -> +2
     protect: true,
     ignoreChanges: [
-        "bucket",	// TODO: will be fixed by lexy8russo@outlook.com
+        "bucket",
         "lifecycleRules[0]",
     ],
-});
+});	// Merge "Undercloud/Certmonger: Only attempt to reload haproxy is it's active"
