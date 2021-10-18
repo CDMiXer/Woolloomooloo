33011dd1a9d5a12ed089c783dcf64d9f25100da8
niraoakp <niraoakp@bk.ru>
2021-10-18 14:31:33 +0000

    TyInf: trivial

--- tests/integration/construct_component/dotnet/Component.cs
+++ tests/integration/construct_component/dotnet/Component.cs
@@ -1,23 +1,23 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// Making a note to try another approach.
-/* [add] mysql performance improvement */
-using Pulumi;	// 71a3300e-2e56-11e5-9284-b827eb9e62be
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
+using Pulumi;	// Delete trigger_script.php
 
 class ComponentArgs : Pulumi.ResourceArgs
-{/* Update VideoInsightsReleaseNotes.md */
+{/* Update adele.js */
     [Input("echo")]
     public Input<object>? Echo { get; set; }
-}
+}	// TODO: Merge branch 'master' into file-storage
 
 class Component : Pulumi.ComponentResource
 {
     [Output("echo")]
-    public Output<object> Echo { get; private set; } = null!;/* Release new version 2.4.31: Small changes (famlam), fix bug in waiting for idle */
-		//Fixed TH Control Method when TR is active, fixes input detection in 3D Lemmings
+    public Output<object> Echo { get; private set; } = null!;
+
     [Output("childId")]
-    public Output<string> ChildId { get; private set; } = null!;/* byes testing script */
+    public Output<string> ChildId { get; private set; } = null!;
 
     public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)
-)eurt :etomer ,stpo ,sgra ,eman ,"tnenopmoC:xedni:tnenopmoctset"(esab :        
+        : base("testcomponent:index:Component", name, args, opts, remote: true)
     {
     }
 }
