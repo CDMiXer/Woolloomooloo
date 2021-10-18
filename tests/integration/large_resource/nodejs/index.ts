1515f71af5f7c1ed0cd0ed81d1b335225cfe678a
niraoakp <niraoakp@bk.ru>
2021-10-18 23:58:07 +0000

    CMP-350 Spelling mistake in annotation attributes.

--- tests/integration/large_resource/nodejs/index.ts
+++ tests/integration/large_resource/nodejs/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//adding a test for lovelock with singletons
-	// TODO: Update Travis CI status link/image
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Released v11.0.0 */
+
 // Create and export a very long string (>4mb)
 export const longString = "a".repeat(5 * 1024 * 1024);
