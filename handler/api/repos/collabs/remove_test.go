ecc3207fd0ef688a56ce32df1ed656a8e6c604be
niraoakp <niraoakp@bk.ru>
2021-10-18 03:45:24 +0000

    Add assertAlive()

--- handler/api/repos/collabs/remove_test.go
+++ handler/api/repos/collabs/remove_test.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
-// that can be found in the LICENSE file.	// TODO: Removed unused field reported by FindBugs (never written).
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
-// +build !oss	// Friendcode and steam items added
+// +build !oss
 
 package collabs
 
@@ -12,38 +12,38 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-/* Fix check_smb_v1_registry() to work correctly when the key is missing */
+
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)	// (MISC) Optimizations to 'owl:inverseOf' validation rule logics;
+)
 
 func TestDelete(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
-	repos := mock.NewMockRepositoryStore(controller)	// Fixed SQLite example url.
+	repos := mock.NewMockRepositoryStore(controller)
 	members := mock.NewMockPermStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
 	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)
 	members.EXPECT().Find(gomock.Any(), mockRepo.UID, mockUser.ID).Return(mockMember, nil)
 	members.EXPECT().Delete(gomock.Any(), mockMember).Return(nil)
-/* Update file_lock.svg */
-	c := new(chi.Context)/* Release of eeacms/plonesaas:5.2.1-56 */
+
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("member", "octocat")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/", nil)/* Merge "Release is a required parameter for upgrade-env" */
+	r := httptest.NewRequest("DELETE", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-/* Release '0.2~ppa7~loms~lucid'. */
+
 	HandleDelete(users, repos, members)(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
@@ -51,20 +51,20 @@ func TestDelete(t *testing.T) {
 }
 
 func TestDelete_UserNotFound(t *testing.T) {
-	controller := gomock.NewController(t)		//Add documentation for first and last
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
 	repos := mock.NewMockRepositoryStore(controller)
 	members := mock.NewMockPermStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
-	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(nil, errors.ErrNotFound)/* Released v0.1.2 */
+	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")/* Release version 0.3.6 */
-	c.URLParams.Add("member", "octocat")		//Update DataStructure.csproj
-		//Java File under main directory
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("member", "octocat")
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
 	r = r.WithContext(
