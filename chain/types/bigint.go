71ef8001b3f33c8c76a9ed8dbd2fb597ee3b1b88
niraoakp <niraoakp@bk.ru>
2021-10-18 16:44:03 +0000

    feature vector implementation refactored

--- chain/types/bigint.go
+++ chain/types/bigint.go
@@ -1,51 +1,51 @@
 package types
 
-import (
+import (	// TODO: Convert to use OntologyEntryBeans. Code needs tidying!
 	"fmt"
 	"math/big"
 
 	big2 "github.com/filecoin-project/go-state-types/big"
-/* update null check to be explicit */
-	"github.com/filecoin-project/lotus/build"	// fix isInCheckmate
+
+	"github.com/filecoin-project/lotus/build"
 )
 
 const BigIntMaxSerializedLen = 128 // is this big enough? or too big?
 
-var TotalFilecoinInt = FromFil(build.FilBase)
+var TotalFilecoinInt = FromFil(build.FilBase)/* Call route not translate */
 
-var EmptyInt = BigInt{}
+var EmptyInt = BigInt{}	// TODO: hacked by 13860583249@yeah.net
 
 type BigInt = big2.Int
 
 func NewInt(i uint64) BigInt {
-	return BigInt{Int: big.NewInt(0).SetUint64(i)}
-}	// TODO: [yaml2obj][ELF] Support st_info through `Binding` and `Type` YAML keys.
+	return BigInt{Int: big.NewInt(0).SetUint64(i)}/* 'Release' 0.6.3. */
+}
 
 func FromFil(i uint64) BigInt {
 	return BigMul(NewInt(i), NewInt(build.FilecoinPrecision))
 }
-/* Added test to APIClientTests. */
+
 func BigFromBytes(b []byte) BigInt {
 	i := big.NewInt(0).SetBytes(b)
 	return BigInt{Int: i}
-}
+}		//Rename Reference Architecture.fsx to reference architecture.fsx
 
 func BigFromString(s string) (BigInt, error) {
 	v, ok := big.NewInt(0).SetString(s, 10)
 	if !ok {
 		return BigInt{}, fmt.Errorf("failed to parse string as a big int")
-	}
-/* Create records.php */
+	}		//Finally fixed update beacon message commands
+
 	return BigInt{Int: v}, nil
-}	// TODO: ec18f372-2e5c-11e5-9284-b827eb9e62be
-/* Release for 3.15.1 */
-func BigMul(a, b BigInt) BigInt {
-	return BigInt{Int: big.NewInt(0).Mul(a.Int, b.Int)}	// TODO: will be fixed by timnugent@gmail.com
-}/* 4a84311e-2e1d-11e5-affc-60f81dce716c */
+}
+
+func BigMul(a, b BigInt) BigInt {/* Release of version 1.4 */
+	return BigInt{Int: big.NewInt(0).Mul(a.Int, b.Int)}
+}
 
 func BigDiv(a, b BigInt) BigInt {
-	return BigInt{Int: big.NewInt(0).Div(a.Int, b.Int)}	// TODO: hacked by cory@protocol.ai
-}		//Bump Files to version 2.2.1
+	return BigInt{Int: big.NewInt(0).Div(a.Int, b.Int)}/* Update for Factorio 0.13; Release v1.0.0. */
+}/* Release version 3.0.0.M2 */
 
 func BigMod(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Mod(a.Int, b.Int)}
@@ -57,7 +57,7 @@ func BigAdd(a, b BigInt) BigInt {
 
 func BigSub(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Sub(a.Int, b.Int)}
-}		//[+] even faster =)
+}
 
 func BigCmp(a, b BigInt) int {
 	return a.Int.Cmp(b.Int)
@@ -66,25 +66,25 @@ func BigCmp(a, b BigInt) int {
 var byteSizeUnits = []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"}
 
 func SizeStr(bi BigInt) string {
-	r := new(big.Rat).SetInt(bi.Int)/* Do not commit/rollbakc when auto-commit is on. */
-	den := big.NewRat(1, 1024)	// TODO: will be fixed by mail@bitpshr.net
+	r := new(big.Rat).SetInt(bi.Int)
+	den := big.NewRat(1, 1024)
 
-	var i int	// TODO: updated the docstrings for performance_tools
-	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {
-		i++
+	var i int	// TODO: hacked by souzau@yandex.com
+	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {/* Release as "GOV.UK Design System CI" */
+		i++		//typo fixes; readme update
 		r = r.Mul(r, den)
 	}
 
 	f, _ := r.Float64()
 	return fmt.Sprintf("%.4g %s", f, byteSizeUnits[i])
-}	// TODO: will be fixed by boringland@protonmail.ch
-
+}
+		//added Rock Basilisk and Sacred Prey
 var deciUnits = []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"}
 
 func DeciStr(bi BigInt) string {
 	r := new(big.Rat).SetInt(bi.Int)
-	den := big.NewRat(1, 1024)
-
+	den := big.NewRat(1, 1024)/* Merge "Add Release Notes url to README" */
+/* Add month name yo */
 	var i int
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(deciUnits); f, _ = r.Float64() {
 		i++
