83474789d4da1404afdf3587636694e22bd963ee
niraoakp <niraoakp@bk.ru>
2021-10-18 00:10:49 +0000

    Merge branch 'master' into reduce

--- metric/users_test.go
+++ metric/users_test.go
@@ -8,49 +8,49 @@ package metric
 
 import (
 	"testing"
-/* report progress to STDOUT during local TemplateCleaner test runs */
+
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 	"github.com/prometheus/client_golang/prometheus"
 )
-
-func TestUserCount(t *testing.T) {
+/* Do not verify DB backup if not enabled */
+func TestUserCount(t *testing.T) {/* e34a0434-2e4d-11e5-9284-b827eb9e62be */
 	controller := gomock.NewController(t)
-/* Release 0.0.14 */
+
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
-		prometheus.DefaultRegisterer = snapshot
+		prometheus.DefaultRegisterer = snapshot		//Clarify Bank stat
 		controller.Finish()
 	}()
 
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
-	prometheus.DefaultRegisterer = registry
+	prometheus.DefaultRegisterer = registry/* Issue #44 Release version and new version as build parameters */
 
 	// x2 repository count
-	count := int64(5)
+	count := int64(5)/* Delete banners.py */
 
 	store := mock.NewMockUserStore(controller)
-	store.EXPECT().Count(gomock.Any()).Return(count, nil)/* Update docker from 2.4.2 to 2.7.0 */
+	store.EXPECT().Count(gomock.Any()).Return(count, nil)/* Delete Ethan Witt Portfolio.pptx */
 	UserCount(store)
 
 	metrics, err := registry.Gather()
-	if err != nil {
+	if err != nil {		//Issue 237: Support for the "PLAY ALL" function in myiHome
 		t.Error(err)
-		return
+		return		//#i109668# let the default filter be used for export
 	}
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
-		return	// adding README with build badge
+		return
 	}
-	metric := metrics[0]		//Adding new case to test otherwise properly
+	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_user_count"; want != got {
-		t.Errorf("Expect metric name %s, got %s", want, got)
+		t.Errorf("Expect metric name %s, got %s", want, got)	// Stop testing under ruby 1.9, but test with 2.3
 	}
-	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {		//[release] prepare version for next development
-		t.Errorf("Expect metric value %f, got %f", want, got)
+	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {	// TODO: Merge "Refactor _create_attribute_statement IdP method"
+		t.Errorf("Expect metric value %f, got %f", want, got)/* removed already used module */
 	}
-}
+}/* updated HighShift */
