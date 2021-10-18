f2df2290379287db1ac97fb646674c09688a7649
niraoakp <niraoakp@bk.ru>
2021-10-18 08:38:56 +0000

    Update 06_drawing02

--- tests/integration/read/read_replace/step3/index.ts
+++ tests/integration/read/read_replace/step3/index.ts
@@ -6,14 +6,14 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Add main website to Readme and fix dates for school */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Add auto update param
-// See the License for the specific language governing permissions and	// Make headers slightly smaller. [#86947212]
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";/* Fixing build after updating `node-funargs`. */
-		//534c12a6-2e5b-11e5-9284-b827eb9e62be
+import { Resource } from "./resource";
+
 // Now go back the other way and make sure that "A" is external again.
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
