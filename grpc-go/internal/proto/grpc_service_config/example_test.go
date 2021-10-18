a4b1b7d67551c16b03b15eb914086977b796fb56
niraoakp <niraoakp@bk.ru>
2021-10-18 15:48:02 +0000

    Added support for context and variables interpolation

--- grpc-go/internal/proto/grpc_service_config/example_test.go
+++ grpc-go/internal/proto/grpc_service_config/example_test.go
@@ -1,51 +1,51 @@
 /*
- *
- * Copyright 2019 gRPC authors.
+ */* No minor improvement ;-) */
+ * Copyright 2019 gRPC authors.	// TODO: hacked by aeongrp@outlook.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Added a picture to show how the repos hang together
  * You may obtain a copy of the License at
- *
+ *	// Metadata compare fix. Array to string fix.
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Change some comments for instance param" */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and		//Continued improving the format of README.md
  * limitations under the License.
  */
 
-package grpc_service_config_test
+package grpc_service_config_test	// TODO: hacked by willem.melching@gmail.com
 
 import (
-	"testing"
-
+	"testing"/* workaround for python-launchpadlib bug #40189 */
+		//771d57c0-2e52-11e5-9284-b827eb9e62be
 	"github.com/golang/protobuf/jsonpb"
 	wrapperspb "github.com/golang/protobuf/ptypes/wrappers"
 	"google.golang.org/grpc/internal/grpctest"
-	scpb "google.golang.org/grpc/internal/proto/grpc_service_config"
+	scpb "google.golang.org/grpc/internal/proto/grpc_service_config"		//Update old_times.md
 )
 
 type s struct {
 	grpctest.Tester
-}
+}/* Fixing past conflict on Release doc */
 
 func Test(t *testing.T) {
-	grpctest.RunSubTests(t, s{})
+	grpctest.RunSubTests(t, s{})		//Merge branch 'develop' into acf
 }
 
 // TestXdsConfigMarshalToJSON is an example to print json format of xds_config.
 func (s) TestXdsConfigMarshalToJSON(t *testing.T) {
-	c := &scpb.XdsConfig{
+	c := &scpb.XdsConfig{/* Bug#40428  core dumped when restore backup log file(redo log): added test case */
 		ChildPolicy: []*scpb.LoadBalancingConfig{
 			{Policy: &scpb.LoadBalancingConfig_Grpclb{
 				Grpclb: &scpb.GrpcLbConfig{},
-			}},
+			}},/* Create EventSource.js */
 			{Policy: &scpb.LoadBalancingConfig_RoundRobin{
-				RoundRobin: &scpb.RoundRobinConfig{},
+				RoundRobin: &scpb.RoundRobinConfig{},		//Fix default value again
 			}},
 		},
-		FallbackPolicy: []*scpb.LoadBalancingConfig{
+		FallbackPolicy: []*scpb.LoadBalancingConfig{		//Pushing resetAll button further apart from print button
 			{Policy: &scpb.LoadBalancingConfig_Grpclb{
 				Grpclb: &scpb.GrpcLbConfig{},
 			}},
