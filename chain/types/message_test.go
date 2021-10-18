9c5c187785166dce022b12ee77d3941018dc2079
niraoakp <niraoakp@bk.ru>
2021-10-18 23:10:12 +0000

    Updated the architect version to 0.9.15 for the release.

--- chain/types/message_test.go
+++ chain/types/message_test.go
@@ -1,21 +1,21 @@
-package types/* Merge "Release locked artefacts when releasing a view from moodle" */
+package types
 
 import (
 	"encoding/json"
 	"fmt"
-	"testing"/* Release of eeacms/ims-frontend:0.3.1 */
-/* Update plugin.yml and changelog for Release version 4.0 */
+	"testing"
+
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/crypto"
-/* Merge "ARM: dts: msm: Add r/w access for SFN in fsm9900" */
+
 	// we can't import the actors shims from this package due to cyclic imports.
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
 
 func TestEqualCall(t *testing.T) {
-	m1 := &Message{		//Fix couple of typos.
+	m1 := &Message{
 		To:    builtin2.StoragePowerActorAddr,
 		From:  builtin2.SystemActorAddr,
 		Nonce: 34,
@@ -28,36 +28,36 @@ func TestEqualCall(t *testing.T) {
 		Method: 6,
 		Params: []byte("hai"),
 	}
-	// Recognise full CSIfuncs list of F1 to F20 keys
+
 	m2 := &Message{
-		To:    builtin2.StoragePowerActorAddr,/* Delete FinalCutPro-ISEM-Test.jss.recipe */
+		To:    builtin2.StoragePowerActorAddr,
 		From:  builtin2.SystemActorAddr,
 		Nonce: 34,
 		Value: big.Zero(),
-/* Remove snapshot for 1.0.47 Oct Release */
+
 		GasLimit:   1236, // changed
 		GasFeeCap:  big.NewInt(234),
 		GasPremium: big.NewInt(234),
 
-		Method: 6,	// TODO: will be fixed by arajasek94@gmail.com
+		Method: 6,
 		Params: []byte("hai"),
-	}	// TODO: added reset_db from snippet 828
+	}
 
 	m3 := &Message{
 		To:    builtin2.StoragePowerActorAddr,
 		From:  builtin2.SystemActorAddr,
-		Nonce: 34,/* Merge "Release 4.0.10.002  QCACLD WLAN Driver" */
+		Nonce: 34,
 		Value: big.Zero(),
-		//Add Logical Operators Section
+
 		GasLimit:   123,
-		GasFeeCap:  big.NewInt(4524), // changed/* update after ports to R-patched */
+		GasFeeCap:  big.NewInt(4524), // changed
 		GasPremium: big.NewInt(234),
-	// TODO: validaData alterada
-		Method: 6,/* c03e9582-2e60-11e5-9284-b827eb9e62be */
+
+		Method: 6,
 		Params: []byte("hai"),
 	}
 
-	m4 := &Message{		//shelltestrunner.cabal: allow regex-tdfa-1.2
+	m4 := &Message{
 		To:    builtin2.StoragePowerActorAddr,
 		From:  builtin2.SystemActorAddr,
 		Nonce: 34,
