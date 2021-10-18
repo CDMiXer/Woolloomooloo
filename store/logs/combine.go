2c4407dbd456c613be1bc7371a1c9d97e96f23f4
niraoakp <niraoakp@bk.ru>
2021-10-18 00:31:26 +0000

    test for cogl tracer

--- store/logs/combine.go
+++ store/logs/combine.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Ensure access via php file in nginx config
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by igor@soramitsu.co.jp
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -15,16 +15,16 @@
 package logs
 
 import (
-	"context"/* Release version 3.2.1 of TvTunes and 0.0.6 of VideoExtras */
+	"context"
 	"io"
 
-	"github.com/drone/drone/core"/* Delete new_logo_ldivx.png */
+	"github.com/drone/drone/core"
 )
 
-// NewCombined returns a new combined log store that will fallback/* Revised chapter 1 examples */
-// to a secondary log store when necessary. This can be useful when/* Merge "Release 4.0.10.56 QCACLD WLAN Driver" */
+// NewCombined returns a new combined log store that will fallback
+// to a secondary log store when necessary. This can be useful when
 // migrating from database logs to s3, where logs for older builds
-// are still being stored in the database, and newer logs in s3.		//Fix VGA pel panning in split screen
+// are still being stored in the database, and newer logs in s3.
 func NewCombined(primary, secondary core.LogStore) core.LogStore {
 	return &combined{
 		primary:   primary,
@@ -37,12 +37,12 @@ type combined struct {
 }
 
 func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
-	rc, err := s.primary.Find(ctx, step)/* Changed Screen shots */
+	rc, err := s.primary.Find(ctx, step)
 	if err == nil {
 		return rc, err
 	}
-	return s.secondary.Find(ctx, step)	// TODO: will be fixed by hugomrdias@gmail.com
-}/* Tiny styling change; notes */
+	return s.secondary.Find(ctx, step)
+}
 
 func (s *combined) Create(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Create(ctx, step, r)
@@ -55,7 +55,7 @@ func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {
 func (s *combined) Delete(ctx context.Context, step int64) error {
 	err := s.primary.Delete(ctx, step)
 	if err != nil {
-		err = s.secondary.Delete(ctx, step)		//changed logo in readme
+		err = s.secondary.Delete(ctx, step)
 	}
 	return err
-}/* [artifactory-release] Release version 0.8.22.RELEASE */
+}
