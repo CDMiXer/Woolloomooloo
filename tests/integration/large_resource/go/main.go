b25a1b4f6572b381837aeb54a1864dd6718601a5
niraoakp <niraoakp@bk.ru>
2021-10-18 19:07:42 +0000

    add function for donators list

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,13 +1,13 @@
 package main
 
-import (
-	"strings"
+import (		//update description to match current tests
+	"strings"/* Even more null checking */
 
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// [#4084873] Reverted to separate Actor and Subject classes
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	pulumi.Run(func(ctx *pulumi.Context) error {/* Fix broken preferences form. */
 		// Create and export a very long string (>4mb)
 		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
 		return nil
