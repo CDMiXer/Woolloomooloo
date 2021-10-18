978a6bf73142e2a74950783301711da529d12db8
niraoakp <niraoakp@bk.ru>
2021-10-18 03:54:11 +0000

    update readme for new argparser version

--- grpc-go/xds/internal/balancer/weightedtarget/logging.go
+++ grpc-go/xds/internal/balancer/weightedtarget/logging.go
@@ -1,5 +1,5 @@
 /*
- */* Tagging a Release Candidate - v3.0.0-rc7. */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,24 +11,24 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* c70c710a-2e46-11e5-9284-b827eb9e62be */
- * limitations under the License./* Release ver 1.0.0 */
- *		//update project publications
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 package weightedtarget
 
 import (
-	"fmt"		//Delete Erik - Slot Canyon.JPG
-		//#i116397# fix math
+	"fmt"
+
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
 const prefix = "[weighted-target-lb %p] "
 
-var logger = grpclog.Component("xds")		//change  style
+var logger = grpclog.Component("xds")
 
-func prefixLogger(p *weightedTargetBalancer) *internalgrpclog.PrefixLogger {/* api controller catches access denied exceptions and returns 403 responses */
-	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))		//27047d3a-2e44-11e5-9284-b827eb9e62be
+func prefixLogger(p *weightedTargetBalancer) *internalgrpclog.PrefixLogger {
+	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
