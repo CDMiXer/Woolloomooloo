d728988167298b698132709db63403049c87dc53
niraoakp <niraoakp@bk.ru>
2021-10-18 17:30:06 +0000

    (MESS) sms.xml: documenting Graphic Board prototype dump. [SMSPower]

--- plugin/converter/legacy_oss.go
+++ plugin/converter/legacy_oss.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone IO, Inc./* add Release dir */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* check for SELECT_CATALOG_ROLE */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -15,9 +15,9 @@
 // +build oss
 
 package converter
-/* Release 0.0.2 */
-import (/* (sobel) updated configuration for Release */
-	"github.com/drone/drone/core"		//Move FullGist model class into core package
+
+import (
+	"github.com/drone/drone/core"
 )
 
 // Legacy returns a conversion service that converts the
