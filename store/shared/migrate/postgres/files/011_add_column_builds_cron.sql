a63bc3999f6d4449153edff4352e2f406553dee9
niraoakp <niraoakp@bk.ru>
2021-10-18 15:11:06 +0000

    Add testenv for python 2.7 and django 1.7

--- store/shared/migrate/postgres/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/postgres/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
 -- name: alter-table-builds-add-column-cron
 
-ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';/* Passage en V.0.3.0 Release */
+ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';
