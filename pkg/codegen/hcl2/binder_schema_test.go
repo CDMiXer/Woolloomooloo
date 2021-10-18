a880ced539fc2ca9bea0448ebdb675f949aa407b
niraoakp <niraoakp@bk.ru>
2021-10-18 19:09:07 +0000

    towards model selection for distance machines

--- pkg/codegen/hcl2/binder_schema_test.go
+++ pkg/codegen/hcl2/binder_schema_test.go
@@ -1,18 +1,18 @@
 package hcl2
-
-import (/* Release of eeacms/eprtr-frontend:1.3.0-0 */
+		//License to match API
+import (/* Released 3.1.3.RELEASE */
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-
+	// TODO: upload new headshot
 func BenchmarkLoadPackage(b *testing.B) {
 	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
-	// TODO: Update outputs.rst
+
 	for n := 0; n < b.N; n++ {
 		_, err := NewPackageCache().loadPackageSchema(loader, "aws")
 		contract.AssertNoError(err)
 	}
-}
+}/* Renaming few classes */
