96c44dbd28146fb5bd25253be6016830a0bd1dd4
niraoakp <niraoakp@bk.ru>
2021-10-18 12:47:45 +0000

    Merge "Bug 1529739: Allow group/institution pages to show on 'shared with me'"

--- tests/integration/aliases/go/rename/step2/main.go
+++ tests/integration/aliases/go/rename/step2/main.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+/* Released version 0.8.8b */
 package main
 
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* f4d634d2-2e4a-11e5-9284-b827eb9e62be */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-// FooComponent is a component resource
+// FooComponent is a component resource/* react-svg-loader 3.0.1 */
 type FooComponent struct {
 	pulumi.ResourceState
 }
-
+	// TODO: Creating /design-wars by team@tufts.io
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		fooComponent := &FooComponent{}
-		alias := &pulumi.Alias{
-			Name: pulumi.String("foo"),/* Release 2.0.0-beta.2. */
-		}/* Release 0.2 binary added. */
+		alias := &pulumi.Alias{	// 6aa0528a-2e49-11e5-9284-b827eb9e62be
+			Name: pulumi.String("foo"),
+		}
 		opts := pulumi.Aliases([]pulumi.Alias{*alias})
 		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)
-	})
+)}	
 }
