ac750fa1bd2384d5ecb7e7e6dd769b853ab2f490
niraoakp <niraoakp@bk.ru>
2021-10-18 15:18:06 +0000

    use 2to3 in setup.py, fixes #35

--- hack/pull-build-images.sh
+++ hack/pull-build-images.sh
@@ -1,4 +1,4 @@
-#!/bin/bash	// TODO: will be fixed by arajasek94@gmail.com
+#!/bin/bash
 set -eu -o pipefail
-/* Systemd service file generation using autotools */
-grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done	// TODO: (Sequence) : New.
+
+grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done
