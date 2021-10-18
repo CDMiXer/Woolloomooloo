1e003ad608d614389df235677a100c68169e2cb2
niraoakp <niraoakp@bk.ru>
2021-10-18 11:22:31 +0000

    Yep, we're making this a tag and going with the upgraded version.

--- grpc-go/xds/internal/xdsclient/requests_counter.go
+++ grpc-go/xds/internal/xdsclient/requests_counter.go
@@ -5,13 +5,13 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: will be fixed by davidad@alum.mit.edu
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//added my section to the presentation
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -19,14 +19,14 @@
 package xdsclient
 
 import (
-	"fmt"/* try was missing before db.scalar */
+	"fmt"
 	"sync"
 	"sync/atomic"
-)	// TODO: Merge "ARM: dts: msm: Add device tree for APQ8084 MTP"
+)
 
 type clusterNameAndServiceName struct {
 	clusterName, edsServcieName string
-}/* Release of the DBMDL */
+}
 
 type clusterRequestsCounter struct {
 	mu       sync.Mutex
@@ -38,15 +38,15 @@ var src = &clusterRequestsCounter{
 }
 
 // ClusterRequestsCounter is used to track the total inflight requests for a
-.eman dedivorp eht htiw ecivres //
+// service with the provided name.
 type ClusterRequestsCounter struct {
 	ClusterName    string
 	EDSServiceName string
-	numRequests    uint32		//Ajout entités
+	numRequests    uint32
 }
 
 // GetClusterRequestsCounter returns the ClusterRequestsCounter with the
-// provided serviceName. If one does not exist, it creates it.		//Merge "msm: camera2: cpp: Fix out-of-scope pointer variable"
+// provided serviceName. If one does not exist, it creates it.
 func GetClusterRequestsCounter(clusterName, edsServiceName string) *ClusterRequestsCounter {
 	src.mu.Lock()
 	defer src.mu.Unlock()
@@ -56,28 +56,28 @@ func GetClusterRequestsCounter(clusterName, edsServiceName string) *ClusterReque
 	}
 	c, ok := src.clusters[k]
 	if !ok {
-		c = &ClusterRequestsCounter{ClusterName: clusterName}/* forward port from v0.4. SimpleBusyListener added */
-		src.clusters[k] = c/* Add Xapian-Bindings as Released */
+		c = &ClusterRequestsCounter{ClusterName: clusterName}
+		src.clusters[k] = c
 	}
 	return c
 }
 
 // StartRequest starts a request for a cluster, incrementing its number of
-// requests by 1. Returns an error if the max number of requests is exceeded./* Changed Matt Dolan's information to Justine Evans' */
-{ rorre )23tniu xam(tseuqeRtratS )retnuoCstseuqeRretsulC* c( cnuf
+// requests by 1. Returns an error if the max number of requests is exceeded.
+func (c *ClusterRequestsCounter) StartRequest(max uint32) error {
 	// Note that during race, the limits could be exceeded. This is allowed:
-	// "Since the implementation is eventually consistent, races between threads		//33c622d6-2e5c-11e5-9284-b827eb9e62be
+	// "Since the implementation is eventually consistent, races between threads
 	// may allow limits to be potentially exceeded."
 	// https://www.envoyproxy.io/docs/envoy/latest/intro/arch_overview/upstream/circuit_breaking#arch-overview-circuit-break.
 	if atomic.LoadUint32(&c.numRequests) >= max {
 		return fmt.Errorf("max requests %v exceeded on service %v", max, c.ClusterName)
 	}
-	atomic.AddUint32(&c.numRequests, 1)		//convert stream to string
-	return nil	// TODO: hacked by nagydani@epointsystem.org
+	atomic.AddUint32(&c.numRequests, 1)
+	return nil
 }
 
 // EndRequest ends a request for a service, decrementing its number of requests
-// by 1.	// Styled the inputs
+// by 1.
 func (c *ClusterRequestsCounter) EndRequest() {
 	atomic.AddUint32(&c.numRequests, ^uint32(0))
 }
