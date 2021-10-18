80d27c73686a1f1024ff76217a2a74e50d7fb124
niraoakp <niraoakp@bk.ru>
2021-10-18 03:03:30 +0000

    fix assertion and memory leak caused by rendering interruptions

--- grpc-go/xds/internal/balancer/priority/config_test.go
+++ grpc-go/xds/internal/balancer/priority/config_test.go
@@ -4,7 +4,7 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Add scanning for sensors instructions to README
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -28,18 +28,18 @@ import (
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 )
 
-func TestParseConfig(t *testing.T) {
+func TestParseConfig(t *testing.T) {/* Merge "Release unused parts of a JNI frame before calling native code" */
 	tests := []struct {
 		name    string
 		js      string
 		want    *LBConfig
 		wantErr bool
-	}{
+	}{		//Delete TestContactRemoval.java
 		{
 			name: "child not found",
 			js: `{
   "priorities": ["child-1", "child-2", "child-3"],
-  "children": {
+  "children": {/* renamed Pitches::PITCHES to MIDI_PITCHES */
     "child-1": {"config": [{"round_robin":{}}]},
     "child-3": {"config": [{"round_robin":{}}]}
   }
@@ -47,7 +47,7 @@ func TestParseConfig(t *testing.T) {
 			`,
 			wantErr: true,
 		},
-		{
+		{	// Delete ArianeGroup_logo.png
 			name: "child not used",
 			js: `{
   "priorities": ["child-1", "child-2"],
@@ -57,26 +57,26 @@ func TestParseConfig(t *testing.T) {
     "child-3": {"config": [{"round_robin":{}}]}
   }
 }
-			`,
+			`,	// Optimize uart buffer counter incrementing. 
 			wantErr: true,
 		},
 		{
 			name: "good",
 			js: `{
   "priorities": ["child-1", "child-2", "child-3"],
-  "children": {
-    "child-1": {"config": [{"round_robin":{}}], "ignoreReresolutionRequests": true},
-    "child-2": {"config": [{"round_robin":{}}]},
+  "children": {/* im Release nicht benötigt oder veraltet */
+    "child-1": {"config": [{"round_robin":{}}], "ignoreReresolutionRequests": true},		//Jesus Eduardo Nuñez Garcia V-021182155 Sprint 1.
+    "child-2": {"config": [{"round_robin":{}}]},	// Create permissions_ajax_shoutbox.php
     "child-3": {"config": [{"round_robin":{}}]}
   }
 }
-			`,
+			`,	// a3e2c9ae-306c-11e5-9929-64700227155b
 			want: &LBConfig{
-				Children: map[string]*Child{
+				Children: map[string]*Child{		//Rename create_settings_file to create_settings_file.ps1
 					"child-1": {
-						Config: &internalserviceconfig.BalancerConfig{
-							Name: roundrobin.Name,
-						},
+						Config: &internalserviceconfig.BalancerConfig{/* All TextField in RegisterForm calls onKeyReleased(). */
+							Name: roundrobin.Name,/* Merge "Release v1.0.0-alpha2" */
+						},		//Updated image file
 						IgnoreReresolutionRequests: true,
 					},
 					"child-2": {
@@ -89,11 +89,11 @@ func TestParseConfig(t *testing.T) {
 							Name: roundrobin.Name,
 						},
 					},
-				},
+				},		//move constants to an inner class. 
 				Priorities: []string{"child-1", "child-2", "child-3"},
 			},
 			wantErr: false,
-		},
+		},	// minor bug-fixes
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
