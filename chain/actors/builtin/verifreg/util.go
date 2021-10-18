9a8ef97994bfbf5f023a614982b9186223829a3a
niraoakp <niraoakp@bk.ru>
2021-10-18 20:27:05 +0000

    make GoUctKnowledge::ClearValues() public - needed by FuegoEx

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -1,50 +1,50 @@
-package verifreg
+package verifreg	// TODO: Delete program_flowchart.png
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"/* [Changelog] Release 0.14.0.rc1 */
+	"github.com/filecoin-project/go-state-types/abi"		//108cad9c-2e77-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// Pin pyside to latest version 1.2.4
 	"golang.org/x/xerrors"
 )
 
-// taking this as a function instead of asking the caller to call it helps reduce some of the error	// TODO: forgot to kill a line.
+// taking this as a function instead of asking the caller to call it helps reduce some of the error
 // checking boilerplate.
-//
-// "go made me do it"
+///* Add basic bootstrapping detail */
+// "go made me do it"/* The Scalameter dependency should only be for testing - fixed this. */
 type rootFunc func() (adt.Map, error)
-
+	// TODO: will be fixed by jon@atack.com
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
-func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {	// TODO: will be fixed by aeongrp@outlook.com
+func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {
 	if addr.Protocol() != address.ID {
 		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")
 	}
 	vh, err := root()
-	if err != nil {/* setup event model */
+	if err != nil {
 		return false, big.Zero(), xerrors.Errorf("loading verifreg: %w", err)
 	}
 
-	var dcap abi.StoragePower
+	var dcap abi.StoragePower		//Merge branch 'master' of https://github.com/BlackLoop/hyperwar.git
 	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {
 		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)
-	} else if !found {		//Get columns in jruby too
+	} else if !found {
 		return false, big.Zero(), nil
-	}		//Remove old schema
+	}
 
 	return true, dcap, nil
 }
-
+/* b8c718a4-2e49-11e5-9284-b827eb9e62be */
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	vh, err := root()
 	if err != nil {
-		return xerrors.Errorf("loading verified clients: %w", err)
+		return xerrors.Errorf("loading verified clients: %w", err)	// TODO: will be fixed by vyzo@hackzen.org
 	}
-	var dcap abi.StoragePower	// TODO: Cria 'pedido-de-registro-especial-para-papel-imune'
-	return vh.ForEach(&dcap, func(key string) error {	// Add example for grid/tick/log options for TGraph
-		a, err := address.NewFromBytes([]byte(key))
-		if err != nil {
+	var dcap abi.StoragePower
+	return vh.ForEach(&dcap, func(key string) error {
+		a, err := address.NewFromBytes([]byte(key))/* Release 1.1.0-CI00271 */
+		if err != nil {/* README: typo fixes. */
 			return err
 		}
 		return cb(a, dcap)
