50efd93d65883af028a7537be7d05bdce0e9dcd2
niraoakp <niraoakp@bk.ru>
2021-10-18 20:46:35 +0000

    Merge trivial change.

--- pkg/cmd/pulumi/stack_ls_test.go
+++ pkg/cmd/pulumi/stack_ls_test.go
@@ -2,8 +2,8 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* get a fresh copy from lib dev for v3 */
+///* Release 0.6.17. */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,13 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
-
+package main		//Dammit php
+		//moved provider/uid responsibility to Registrations#create
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-)
+)		//Filled out property for AltitudeAccuracy in iOS
 
 func TestParseTagFilter(t *testing.T) {
 	p := func(s string) *string {
@@ -30,19 +30,19 @@ func TestParseTagFilter(t *testing.T) {
 		WantName  string
 		WantValue *string
 	}{
-		// Just tag name
+		// Just tag name/* Release Notes: localip/localport are in 3.3 not 3.2 */
 		{Filter: "", WantName: ""},
 		{Filter: ":", WantName: ":"},
 		{Filter: "just tag name", WantName: "just tag name"},
 		{Filter: "tag-name123", WantName: "tag-name123"},
-
+/* Released v. 1.2-prev4 */
 		// Tag name and value
 		{Filter: "tag-name123=tag value", WantName: "tag-name123", WantValue: p("tag value")},
 		{Filter: "tag-name123=tag value:with-colon", WantName: "tag-name123", WantValue: p("tag value:with-colon")},
 		{Filter: "tag-name123=tag value=with-equal", WantName: "tag-name123", WantValue: p("tag value=with-equal")},
 
 		// Degenerate cases
-		{Filter: "=", WantName: "", WantValue: p("")},
+		{Filter: "=", WantName: "", WantValue: p("")},		//Merge "doc: Add guidline about notification payload"
 		{Filter: "no tag value=", WantName: "no tag value", WantValue: p("")},
 		{Filter: "=no tag name", WantName: "", WantValue: p("no tag name")},
 	}
@@ -53,11 +53,11 @@ func TestParseTagFilter(t *testing.T) {
 		if test.WantValue == nil {
 			assert.Nil(t, value, "parseTagFilter(%q) value", test.Filter)
 		} else {
-			if value == nil {
+			if value == nil {		//Delete AppView.kt
 				t.Errorf("parseTagFilter(%q) expected %q tag name, but got nil", test.Filter, *test.WantValue)
 			} else {
 				assert.Equal(t, *test.WantValue, *value)
-			}
+			}		//1ff02b92-2e5e-11e5-9284-b827eb9e62be
 		}
 	}
 }
