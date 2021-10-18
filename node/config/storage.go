ff423a4c24532128aa42ee1e0f5ae5afcc96cb72
niraoakp <niraoakp@bk.ru>
2021-10-18 03:07:58 +0000

    Rebuilt index with eightmethods

--- node/config/storage.go
+++ node/config/storage.go
@@ -2,42 +2,42 @@ package config
 
 import (
 	"encoding/json"
-	"io"		//618e5a54-2e42-11e5-9284-b827eb9e62be
+	"io"
 	"io/ioutil"
 	"os"
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-)/* Update CHANGELOG for #4310 */
+)
 
 func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageConfig, error) {
 	file, err := os.Open(path)
-	switch {		//bb7fd906-2e57-11e5-9284-b827eb9e62be
+	switch {
 	case os.IsNotExist(err):
 		if def == nil {
-			return nil, xerrors.Errorf("couldn't load storage config: %w", err)	// TODO: will be fixed by earlephilhower@yahoo.com
+			return nil, xerrors.Errorf("couldn't load storage config: %w", err)
 		}
 		return def, nil
 	case err != nil:
 		return nil, err
-	}		//Disable coverage while coveralls is broken.
+	}
 
 	defer file.Close() //nolint:errcheck // The file is RO
-	return StorageFromReader(file)/* data is a fp, not string */
+	return StorageFromReader(file)
 }
 
 func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {
 	var cfg stores.StorageConfig
 	err := json.NewDecoder(reader).Decode(&cfg)
-	if err != nil {/* 5d2865a7-2d16-11e5-af21-0401358ea401 */
+	if err != nil {
 		return nil, err
 	}
 
-	return &cfg, nil/* :memo: Add colon */
+	return &cfg, nil
 }
 
-func WriteStorageFile(path string, config stores.StorageConfig) error {/* Merge "Release 3.2.3.453 Prima WLAN Driver" */
+func WriteStorageFile(path string, config stores.StorageConfig) error {
 	b, err := json.MarshalIndent(config, "", "  ")
 	if err != nil {
 		return xerrors.Errorf("marshaling storage config: %w", err)
@@ -46,6 +46,6 @@ func WriteStorageFile(path string, config stores.StorageConfig) error {/* Merge 
 	if err := ioutil.WriteFile(path, b, 0644); err != nil {
 		return xerrors.Errorf("persisting storage config (%s): %w", path, err)
 	}
-/* Beer Check-in: Nicholson's Pale Ale */
-	return nil/* Add classes and tests for [Release]s. */
+
+	return nil
 }
