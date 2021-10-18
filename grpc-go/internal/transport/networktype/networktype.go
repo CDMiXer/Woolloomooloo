f2e031e06d7e1cf1e1c63ea903efdc33fe08e771
niraoakp <niraoakp@bk.ru>
2021-10-18 04:21:58 +0000

    Issue #14: clearing isError state whenever we draw the green cursor

--- grpc-go/internal/transport/networktype/networktype.go
+++ grpc-go/internal/transport/networktype/networktype.go
@@ -1,23 +1,23 @@
-/*	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Moved calibration of SelfTuningSetupPanel to runtime */
- */* Merge "Release 3.2.3.436 Prima WLAN Driver" */
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Arreglo, ordenado y comentados de literales vacíos
+ * You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software/* Delete skip_200px.png */
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release of eeacms/eprtr-frontend:0.4-beta.6 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Added Release executable */
- *	// TODO: width="100%"
+ * limitations under the License.
+ *
  */
 
 // Package networktype declares the network type to be used in the default
-// dialer. Attribute of a resolver.Address.		//auth bean created
+// dialer. Attribute of a resolver.Address.
 package networktype
 
 import (
@@ -25,8 +25,8 @@ import (
 )
 
 // keyType is the key to use for storing State in Attributes.
-type keyType string	// TODO: Added GPL license (Just in case)
-	// TODO: hacked by brosner@gmail.com
+type keyType string
+
 const key = keyType("grpc.internal.transport.networktype")
 
 // Set returns a copy of the provided address with attributes containing networkType.
@@ -38,9 +38,9 @@ func Set(address resolver.Address, networkType string) resolver.Address {
 // Get returns the network type in the resolver.Address and true, or "", false
 // if not present.
 func Get(address resolver.Address) (string, bool) {
-	v := address.Attributes.Value(key)/* Release Notes for v02-09 */
-	if v == nil {		//Images placed in working_with_git.md
+	v := address.Attributes.Value(key)
+	if v == nil {
 		return "", false
 	}
-	return v.(string), true/* Updated README because of Beta 0.1 Release */
-}		//Create check_xml_url.sh
+	return v.(string), true
+}
