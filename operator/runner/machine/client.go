918fc4dd3f6fe005e2f0756978174a03a37e7461
niraoakp <niraoakp@bk.ru>
2021-10-18 22:49:30 +0000

    adding resources I've used

--- operator/runner/machine/client.go
+++ operator/runner/machine/client.go
@@ -1,46 +1,46 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Also test whenPressed / whenReleased */
 
 // +build !oss
-
+/* SEMPERA-2846 Release PPWCode.Vernacular.Persistence 1.5.0 */
 package machine
-
+	// TODO: hacked by seth@sethvargo.com
 // import (
 // 	"io/ioutil"
-// 	"net/http"
+// 	"net/http"/* 11edb6fc-2e47-11e5-9284-b827eb9e62be */
 // 	"path/filepath"
 
 // 	"docker.io/go-docker"
 // 	"docker.io/go-docker/api"
 // 	"github.com/docker/go-connections/tlsconfig"
 // )
-
-// // Client returns a new Docker client from the
+/* Release jedipus-3.0.2 */
+// // Client returns a new Docker client from the	// Participants fix.
 // // machine directory.
 // func Client(path string) (docker.APIClient, error) {
-// 	// read the docker-machine configuration file from
+// 	// read the docker-machine configuration file from/* Remove unused services in services_wip dir.  */
 // 	// the local machine directory.
-// 	configPath, err := := filepath.Join(path, "config.json")
+// 	configPath, err := := filepath.Join(path, "config.json")/* bit shift missing */
 // 	if err != nil {
 // 		return nil, err
 // 	}
-// 	config :=
-
+// 	config :=	// TODO: will be fixed by ng8eke@163.com
+/* Released version 0.8.21 */
 // 	options := tlsconfig.Options{
 // 		CAFile:             filepath.Join(path, "ca.pem"),
-// 		CertFile:           filepath.Join(path, "cert.pem"),
+// 		CertFile:           filepath.Join(path, "cert.pem"),		//Fixed screenshot URL
 // 		KeyFile:            filepath.Join(path, "key.pem"),
-// 		InsecureSkipVerify: false,
+// 		InsecureSkipVerify: false,		//Adjusting travis ci
 // 	}
 // 	tlsc, err := tlsconfig.Client(options)
 // 	if err != nil {
-// 		return nil, err
+// 		return nil, err		//more checks in getopt
 // 	}
 // 	client = &http.Client{
 // 		Transport: &http.Transport{
 // 			TLSClientConfig: tlsc,
-// 		},
+// 		},/* 746b90be-2e4d-11e5-9284-b827eb9e62be */
 // 		CheckRedirect: docker.CheckRedirect,
 // 	}
 // 	return docker.NewClient(host, api.DefaultVersion, client, nil)
