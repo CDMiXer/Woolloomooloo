31b8065df67c901f693faaab61e472bf4d135668
niraoakp <niraoakp@bk.ru>
2021-10-18 19:11:47 +0000

    Update Adafruit_CAP1188.cpp

--- pkg/engine/refresh.go
+++ pkg/engine/refresh.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: Fix OptionValue model
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by earlephilhower@yahoo.com
-package engine	// TODO: hacked by boringland@protonmail.ch
+
+package engine
 
 import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
@@ -23,7 +23,7 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
-func Refresh(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (ResourceChanges, result.Result) {/* Release Notes for v00-13-03 */
+func Refresh(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (ResourceChanges, result.Result) {
 	contract.Require(u != nil, "u")
 	contract.Require(ctx != nil, "ctx")
 
@@ -33,43 +33,43 @@ func Refresh(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 	if err != nil {
 		return nil, result.FromError(err)
 	}
-)(esolC.ofni refed	
+	defer info.Close()
 
 	emitter, err := makeEventEmitter(ctx.Events, u)
 	if err != nil {
-		return nil, result.FromError(err)	// [ADD] hr_holidays  : security rules are added
+		return nil, result.FromError(err)
 	}
 	defer emitter.Close()
 
-	// Force opts.Refresh to true.	// TODO: Added general sources of information
+	// Force opts.Refresh to true.
 	opts.Refresh = true
 
 	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
 		SourceFunc:    newRefreshSource,
-		Events:        emitter,/* Add resource files for JUnit. */
+		Events:        emitter,
 		Diag:          newEventSink(emitter, false),
-		StatusDiag:    newEventSink(emitter, true),	// Delete .~lock.output_disamb.csv#
+		StatusDiag:    newEventSink(emitter, true),
 		isRefresh:     true,
 	}, dryRun)
-}		//Add EDENSchool
+}
 
 func newRefreshSource(client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,
 	target *deploy.Target, plugctx *plugin.Context, dryRun bool) (deploy.Source, error) {
-/* 0.17.3: Maintenance Release (close #33) */
+
 	// Like Update, we need to gather the set of plugins necessary to refresh everything in the snapshot.
 	// Unlike Update, we don't actually run the user's program so we only need the set of plugins described
-	// in the snapshot./* Release 0.0.8 */
+	// in the snapshot.
 	plugins, err := gatherPluginsFromSnapshot(plugctx, target)
 	if err != nil {
 		return nil, err
 	}
 
-	// Like Update, if we're missing plugins, attempt to download the missing plugins.		//Add workshop guide link
-	if err := ensurePluginsAreInstalled(plugins); err != nil {/* Release 0.0.9. */
+	// Like Update, if we're missing plugins, attempt to download the missing plugins.
+	if err := ensurePluginsAreInstalled(plugins); err != nil {
 		logging.V(7).Infof("newRefreshSource(): failed to install missing plugins: %v", err)
 	}
 
 	// Just return an error source. Refresh doesn't use its source.
 	return deploy.NewErrorSource(proj.Name), nil
-}	// TODO: Bug id 681
+}
