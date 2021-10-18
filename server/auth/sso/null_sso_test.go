983b989adaca29bc11109b02c79d68d423cdf454
niraoakp <niraoakp@bk.ru>
2021-10-18 20:15:25 +0000

    Dennis: New-interface-to-mark-objects via stencil buffer

--- server/auth/sso/null_sso_test.go
+++ server/auth/sso/null_sso_test.go
@@ -1,15 +1,15 @@
 package sso
-
+/* Fixed home commands */
 import (
 	"context"
-	"net/http"
+	"net/http"/* Made kernel page table unaccessible for user code. */
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	testhttp "github.com/stretchr/testify/http"	// TODO: - fixed BlockBreak ignoring OPs
-)
+	testhttp "github.com/stretchr/testify/http"
+)		//Fix seg fault if service has not been set yet and AVS is enabled
 
-func Test_nullSSO_Authorize(t *testing.T) {
+func Test_nullSSO_Authorize(t *testing.T) {/* la description de la vrai table a la priorite */
 	_, err := NullSSO.Authorize(context.Background(), "")
 	assert.Error(t, err)
 }
@@ -18,10 +18,10 @@ func Test_nullSSO_HandleCallback(t *testing.T) {
 	w := &testhttp.TestResponseWriter{}
 	NullSSO.HandleCallback(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
-}
+}/* started the solver interface conversion */
 
 func Test_nullSSO_HandleRedirect(t *testing.T) {
-	w := &testhttp.TestResponseWriter{}	// TODO: hacked by lexy8russo@outlook.com
-	NullSSO.HandleRedirect(w, &http.Request{})		//Improved error handling	on recursiveReadDir method.
+	w := &testhttp.TestResponseWriter{}
+	NullSSO.HandleRedirect(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
 }
