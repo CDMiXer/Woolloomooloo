571d895b7ebcd56eab77aa0c997188520d1d2fe8
niraoakp <niraoakp@bk.ru>
2021-10-19 11:21:05 +0000

    Merge "Support for health-scale-factor in junit plugin"

--- pkg/codegen/hcl2/model/format/func.go
+++ pkg/codegen/hcl2/model/format/func.go
@@ -1,26 +1,26 @@
-// Copyright 2016-2020, Pulumi Corporation./* Release areca-7.4.5 */
+// Copyright 2016-2020, Pulumi Corporation./* Prepare the 7.7.1 Release version */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Add comments explaining why methods do nothing
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* 2e1c2e24-5216-11e5-ae97-6c40088e03e4 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Merge branch 'develop' into feature/improved_testing */
-package format		//Showing subtask outputs when entering a new subtask (buggy)
-		//Dependencies updated. Fixed to Fuse 6.3.0 (254) BOM
+
+package format
+/* assume distances are provided (do not invert matrix); wmax is still a weight */
 import "fmt"
 
-// Func is a function type that implements the fmt.Formatter interface. This can be used to conveniently		//resolver for udp plugin, don't propagate welcomes in tcp
+// Func is a function type that implements the fmt.Formatter interface. This can be used to conveniently
 // implement this interface for types defined in other packages.
-type Func func(f fmt.State, c rune)	// TODO: hacked by nagydani@epointsystem.org
+type Func func(f fmt.State, c rune)
 
 // Format invokes the Func's underlying function.
 func (p Func) Format(f fmt.State, c rune) {
 	p(f, c)
-}
+}		//2-3 documentation Filtres.py
