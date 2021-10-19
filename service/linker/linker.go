bd3bc18696b48b4589b396ff28c54c0f560681b4
niraoakp <niraoakp@bk.ru>
2021-10-19 14:13:27 +0000

    Update aeon-entry.js

--- service/linker/linker.go
+++ service/linker/linker.go
@@ -4,19 +4,19 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// remove yarn from travis buld
-///* creates full-bleed videos */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.0.4  */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Added null packet support. */
+
 package linker
-	// Remove redundant for
+
 import (
-	"context"	// TODO: will be fixed by onhardev@bk.ru
-	// TODO: will be fixed by mail@overlisted.net
+	"context"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
@@ -24,17 +24,17 @@ import (
 // New returns a new Linker server.
 func New(client *scm.Client) core.Linker {
 	return &service{
-		client: client,		//3f6af5c2-35c7-11e5-aeb1-6c40088e03e4
+		client: client,
 	}
-}	// TODO: hacked by ng8eke@163.com
+}
 
 type service struct {
 	client *scm.Client
-}	// Updated read me with first cut of docs and examples
-	// Added a few benchmarks (comparing with ruby-prof)
+}
+
 func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {
 	return s.client.Linker.Resource(ctx, repo, scm.Reference{
-		Path: ref,	// vector add
+		Path: ref,
 		Sha:  sha,
 	})
 }
