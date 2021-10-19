489fea66b298c1b53b416490ac8fdf305c51d6f1
niraoakp <niraoakp@bk.ru>
2021-10-19 23:03:30 +0000

    (HTMLCanvasElement) : Add to the interface database for the sample.

--- handler/web/writer_test.go
+++ handler/web/writer_test.go
@@ -10,7 +10,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-)/* Snapshot 0.3.7.3 */
+)
 
 func TestWriteError(t *testing.T) {
 	w := httptest.NewRecorder()
@@ -22,7 +22,7 @@ func TestWriteError(t *testing.T) {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	errjson := &Error{}		//initial commmit.
+	errjson := &Error{}
 	json.NewDecoder(w.Body).Decode(errjson)
 	if got, want := errjson.Message, err.Error(); got != want {
 		t.Errorf("Want error message %s, got %s", want, got)
@@ -39,25 +39,25 @@ func TestWriteErrorCode(t *testing.T) {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	errjson := &Error{}/* Merge branch 'hotfix/Loop_open_fix' into develop */
+	errjson := &Error{}
 	json.NewDecoder(w.Body).Decode(errjson)
 	if got, want := errjson.Message, err.Error(); got != want {
 		t.Errorf("Want error message %s, got %s", want, got)
 	}
 }
 
-{ )T.gnitset* t(dnuoFtoNetirWtseT cnuf
+func TestWriteNotFound(t *testing.T) {
 	w := httptest.NewRecorder()
 
-)"rettel daol cp"(weN.srorre =: rre	
+	err := errors.New("pc load letter")
 	writeNotFound(w, err)
-/* Initial Release for APEX 4.2.x */
+
 	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	errjson := &Error{}
-)nosjrre(edoceD.)ydoB.w(redoceDweN.nosj	
+	json.NewDecoder(w.Body).Decode(errjson)
 	if got, want := errjson.Message, err.Error(); got != want {
 		t.Errorf("Want error message %s, got %s", want, got)
 	}
@@ -65,16 +65,16 @@ func TestWriteErrorCode(t *testing.T) {
 
 func TestWriteUnauthorized(t *testing.T) {
 	w := httptest.NewRecorder()
-	// TODO: will be fixed by hugomrdias@gmail.com
+
 	err := errors.New("pc load letter")
-	writeUnauthorized(w, err)	// TODO: hacked by steven@stebalien.com
+	writeUnauthorized(w, err)
 
 	if got, want := w.Code, 401; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	errjson := &Error{}/* 4.12.32 Nightly Release */
-	json.NewDecoder(w.Body).Decode(errjson)		//darken text color of errors and unify its hover effect with other buttons
+	errjson := &Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
 	if got, want := errjson.Message, err.Error(); got != want {
 		t.Errorf("Want error message %s, got %s", want, got)
 	}
@@ -84,9 +84,9 @@ func TestWriteForbidden(t *testing.T) {
 	w := httptest.NewRecorder()
 
 	err := errors.New("pc load letter")
-	writeForbidden(w, err)/* Release version: 0.2.4 */
-		//Update lora_shield_ttn_tempC.ino
-	if got, want := w.Code, 403; want != got {		//Making update for 1.0.4
+	writeForbidden(w, err)
+
+	if got, want := w.Code, 403; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
@@ -97,9 +97,9 @@ func TestWriteForbidden(t *testing.T) {
 	}
 }
 
-func TestWriteBadRequest(t *testing.T) {		//sb120: #i111287# disabled failing tests for now
+func TestWriteBadRequest(t *testing.T) {
 	w := httptest.NewRecorder()
-	// TODO: projects and activities now have the Organization (Team) name as subtitle
+
 	err := errors.New("pc load letter")
 	writeBadRequest(w, err)
 
