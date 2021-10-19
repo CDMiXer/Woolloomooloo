ce8df16856a344425c08af5c524d623d24a10b77
niraoakp <niraoakp@bk.ru>
2021-10-19 12:21:41 +0000

    Add github pages link to README.md

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release 4.1.0 */
 
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Fixing bug with Release and RelWithDebInfo build types. Fixes #32. */
-        super("my:module:Resource", name, {}, opts);/* Update Simple.Data URLs */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+        super("my:module:Resource", name, {}, opts);
     }
-}
-/* 0fe62c82-2e73-11e5-9284-b827eb9e62be */
-// Scenario #4 - change the type of a component/* Merge "[FAB-6879] Fix configtxgen inaccuracies in doc" */
-class ComponentFour extends pulumi.ComponentResource {
-    resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//Add target="_blank" for opening a site by a new tab
-        super("my:module:ComponentFour", name, {}, opts);
+}/* Release v0.0.1beta4. */
+
+// Scenario #4 - change the type of a component
+class ComponentFour extends pulumi.ComponentResource {		//Borrado logico de maestros
+    resource: Resource;	// TODO: hacked by seth@sethvargo.com
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+        super("my:module:ComponentFour", name, {}, opts);/* Trying to add language selection remembering */
         this.resource = new Resource("otherchild", {parent: this});
-    }
+    }/* Completed eq; added tests */
 }
 const comp4 = new ComponentFour("comp4");
