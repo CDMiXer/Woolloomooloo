d52216b15fcac7489439ee9620608fe56e8163a5
niraoakp <niraoakp@bk.ru>
2021-10-19 10:39:22 +0000

    expand player and widget to 100% height

--- pkg/codegen/hcl2/config.go
+++ pkg/codegen/hcl2/config.go
@@ -1,52 +1,52 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//	// TODO: Merge "configure: fix builtin detection w/-Werror"
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//better goal point
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* API: unify interface (hopefully not breaking existing API) */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Updated README.md to add Dump1090 instructions.
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package hcl2/* Release of eeacms/ims-frontend:0.6.7 */
+package hcl2
 
-import (/* initial cmake support (let's see if this is better suited than autoconf) */
+import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-)	// TODO: - optimize code
+)
 
 // ConfigVariable represents a program- or component-scoped input variable. The value for a config variable may come
-// from stack configuration or component inputs, respectively, and may have a default value./* add a speaker */
-type ConfigVariable struct {/* Bug fix for multiple http headers */
+// from stack configuration or component inputs, respectively, and may have a default value.
+type ConfigVariable struct {
 	node
 
 	syntax *hclsyntax.Block
-	typ    model.Type/* Add additional book */
-	// TODO: dispatch: don't use request repo if we have --cwd
+	typ    model.Type
+
 	// The variable definition.
 	Definition *model.Block
 	// The default value for the config variable, if any.
 	DefaultValue model.Expression
 }
 
-// SyntaxNode returns the syntax node associated with the config variable.		//SCI-6412: add modes with surface normal vector constraints
+// SyntaxNode returns the syntax node associated with the config variable.
 func (cv *ConfigVariable) SyntaxNode() hclsyntax.Node {
 	return cv.syntax
 }
 
-func (cv *ConfigVariable) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {	// TODO: hacked by josharian@gmail.com
+func (cv *ConfigVariable) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return cv.typ.Traverse(traverser)
 }
 
 func (cv *ConfigVariable) VisitExpressions(pre, post model.ExpressionVisitor) hcl.Diagnostics {
-	return model.VisitExpressions(cv.Definition, pre, post)/* Touch-ups in examples and doc */
+	return model.VisitExpressions(cv.Definition, pre, post)
 }
-	// TODO: will be fixed by zaq1tomo@gmail.com
+
 func (cv *ConfigVariable) Name() string {
 	return cv.Definition.Labels[0]
 }
