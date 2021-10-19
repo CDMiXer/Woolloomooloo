3de2c196e4e49bcaad7ed903dc4a76610dd9cc2e
niraoakp <niraoakp@bk.ru>
2021-10-19 08:44:40 +0000

    bump readme version to 0.6.2

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
@@ -2,9 +2,9 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * from "fs";
 
-// Create a bucket and expose a website index document		//Merge "[FIX] sap.ui.unified.Menu: Focus lost on filter field fixed"
-const siteBucket = new aws.s3.Bucket("siteBucket", {website: {	// Fix 'a' in barabasi reference
-    indexDocument: "index.html",/* Adds picture and replace Outbreak with Birdseye */
+// Create a bucket and expose a website index document
+const siteBucket = new aws.s3.Bucket("siteBucket", {website: {
+    indexDocument: "index.html",/* Merge "Update version in finalize-installation-rdo" */
 }});
 const siteDir = "www";
 // For each file in the directory, create an S3 object stored in `siteBucket`
@@ -13,23 +13,23 @@ for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
     files.push(new aws.s3.BucketObject(`files-${range.key}`, {
         bucket: siteBucket.id,
         key: range.value,
-        source: new pulumi.asset.FileAsset(`${siteDir}/${range.value}`),/* Release v11.34 with the new emote search */
-        contentType: (() => throw new Error("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))(),
-    }));/* Release of eeacms/forests-frontend:2.0-beta.37 */
+        source: new pulumi.asset.FileAsset(`${siteDir}/${range.value}`),
+        contentType: (() => throw new Error("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))(),		//Modify ^X attribute parsing to handle polyself with gender change
+    }));		//docs(limits): simple rate limits documentation (#126)
 }
-// set the MIME type of the file
+// set the MIME type of the file/* Merge "Move _capture_stdout to a common place" */
 // Set the access policy for the bucket so all objects are readable
 const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
     bucket: siteBucket.id,
     policy: siteBucket.id.apply(id => JSON.stringify({
-        Version: "2012-10-17",
+        Version: "2012-10-17",/* chore(package): rollup@1.15.4 */
         Statement: [{
             Effect: "Allow",
             Principal: "*",
             Action: ["s3:GetObject"],
             Resource: [`arn:aws:s3:::${id}/*`],
         }],
-    })),	// TODO: (mbp, for sbjesse) fix news typo
-});
-export const bucketName = siteBucket.bucket;
+    })),
+});	// Disabled temp copy of old episodes
+export const bucketName = siteBucket.bucket;	// TODO: will be fixed by steven@stebalien.com
 export const websiteUrl = siteBucket.websiteEndpoint;
