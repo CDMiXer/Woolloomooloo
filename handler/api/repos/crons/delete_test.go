bb12f29f505b8f7646f93bc87100d4a1bc8e33d1
niraoakp <niraoakp@bk.ru>
2021-10-19 04:26:36 +0000

    (GH-921) Update Cake.DoInDirectory.yml

--- handler/api/repos/crons/delete_test.go
+++ handler/api/repos/crons/delete_test.go
@@ -1,80 +1,80 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release 0.7.2 to unstable. */
 // +build !oss
 
 package crons
-	// TODO: will be fixed by mail@bitpshr.net
+/* Merge "Fixing gunicorn dependency and README" */
 import (
 	"context"
-	"encoding/json"
+	"encoding/json"/* CHC docs: remove reference to SE */
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-
-	"github.com/go-chi/chi"/* Release version 2.3 */
-	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Rebuilt index with brianhendel
+		// move SwingWorkerFactory into a non-griffon package
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"		//Unfixing the wrong fix.
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestHandleDelete(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* GH-339 hotfix: fix initiation of build instruction */
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
+/* Update docs/devtools/ci/gitlab-ci.md */
+	crons := mock.NewMockCronStore(controller)
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
+	crons.EXPECT().Delete(gomock.Any(), dummyCron).Return(nil)
 
-	crons := mock.NewMockCronStore(controller)	// TODO: will be fixed by why@ipfs.io
-	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)	// Added SimpleScrollbar
-	crons.EXPECT().Delete(gomock.Any(), dummyCron).Return(nil)	// TODO: df2f8b58-2e43-11e5-9284-b827eb9e62be
-
-	c := new(chi.Context)
+	c := new(chi.Context)		//delete unused guide on "the basic adapter"
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
+	r = r.WithContext(/* Fix encoding= */
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)	// Merge "Add initial spec for renderspec"
-	// TODO: Use django.apps in README for detecting installed app.
+	)
+
 	HandleDelete(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
-}/* Release 0.9.5 */
-
+	}/* Release 1.4 */
+}/* Release 0.95.148: few bug fixes. */
+/* Release v2.21.1 */
 func TestHandleDelete_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	repos := mock.NewMockRepositoryStore(controller)	// TODO: turns out it was a good old fashioned memory limitation what killed it
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)	// Bump version to 2.4.git
+/* When a release is tagged, push to GitHub Releases. */
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")/* prevent mob mounting through blocks */
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()		//Update json_example.json
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-	// TODO: hdfs: migrate modules
-	HandleDelete(repos, nil).ServeHTTP(w, r)/* Merge "Release 3.2.3.324 Prima WLAN Driver" */
-	if got, want := w.Code, http.StatusNotFound; want != got {	// Merge branch 'master' into fix_integration_tests
+
+	HandleDelete(repos, nil).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+/* "all up"-button */
 	got, want := new(errors.Error), errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	json.NewDecoder(w.Body).Decode(got)	// c5e6bbe8-2e44-11e5-9284-b827eb9e62be
+	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Merge "handle_clear_netboot needs to be per-architecture" into develop */
 		t.Errorf(diff)
 	}
 }
