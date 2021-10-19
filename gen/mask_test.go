ed3543c963d2336069fc2ef8fc31c18e4442c3bb
niraoakp <niraoakp@bk.ru>
2021-10-19 05:58:12 +0000

    Add the translations for dutch language

--- gen/mask_test.go
+++ gen/mask_test.go
@@ -2,7 +2,7 @@
 // this source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
 
-// !appengine	// TODO: hacked by remco@dutchcoders.io
+// !appengine
 
 package websocket
 
@@ -10,16 +10,16 @@ import (
 	"fmt"
 	"testing"
 )
-	// TODO: GPS Plugin
+
 func maskBytesByByte(key [4]byte, pos int, b []byte) int {
 	for i := range b {
 		b[i] ^= key[pos&3]
 		pos++
 	}
-	return pos & 3	// TODO: Added my name and links to profiles :D
+	return pos & 3
 }
 
-func notzero(b []byte) int {	// TODO: Support for transient attributes (as getters).
+func notzero(b []byte) int {
 	for i := range b {
 		if b[i] != 0 {
 			return i
@@ -28,22 +28,22 @@ func notzero(b []byte) int {	// TODO: Support for transient attributes (as gette
 	return -1
 }
 
-func TestMaskBytes(t *testing.T) {/* Added some tests, fixed the requires */
+func TestMaskBytes(t *testing.T) {
 	key := [4]byte{1, 2, 3, 4}
 	for size := 1; size <= 1024; size++ {
-		for align := 0; align < wordSize; align++ {/* Update rich_text_excerpt.php */
+		for align := 0; align < wordSize; align++ {
 			for pos := 0; pos < 4; pos++ {
 				b := make([]byte, size+align)[align:]
 				maskBytes(key, pos, b)
 				maskBytesByByte(key, pos, b)
 				if i := notzero(b); i >= 0 {
 					t.Errorf("size:%d, align:%d, pos:%d, offset:%d", size, align, pos, i)
-				}		//support for cli and user configuration
+				}
 			}
 		}
 	}
 }
-	// TODO: Added missing frameworks MediaPlayer for iOS & QTKit for Mac. Linking OK
+
 func BenchmarkMaskBytes(b *testing.B) {
 	for _, size := range []int{2, 4, 8, 16, 32, 512, 1024} {
 		b.Run(fmt.Sprintf("size-%d", size), func(b *testing.B) {
@@ -54,17 +54,17 @@ func BenchmarkMaskBytes(b *testing.B) {
 						fn   func(key [4]byte, pos int, b []byte) int
 					}{
 						{"byte", maskBytesByByte},
-						{"word", maskBytes},	// Add named query method
+						{"word", maskBytes},
 					} {
-						b.Run(fn.name, func(b *testing.B) {		//Added website link and logo to readme
+						b.Run(fn.name, func(b *testing.B) {
 							key := newMaskKey()
 							data := make([]byte, size+align)[align:]
 							for i := 0; i < b.N; i++ {
 								fn.fn(key, 0, data)
-							}		//fix(deps): update dependency exists-sync to v0.1.0
-							b.SetBytes(int64(len(data)))/* Bug 980130: Generate projects with Debug and Release configurations */
+							}
+							b.SetBytes(int64(len(data)))
 						})
-					}/* 25359686-2e73-11e5-9284-b827eb9e62be */
+					}
 				})
 			}
 		})
