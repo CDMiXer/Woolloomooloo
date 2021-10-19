081a0270987472f46f783bb4df441ddfc3d3a93e
niraoakp <niraoakp@bk.ru>
2021-10-19 05:01:06 +0000

    Fix loading of lowest priority items from database.

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -2,39 +2,39 @@ package sectorstorage
 
 import (
 	"context"
-/* Copy dlls on windows from repo. */
+
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)/* Merge "Release 1.0.0.241B QCACLD WLAN Driver" */
+)
 
 type readonlyProvider struct {
 	index stores.SectorIndex
 	stor  *stores.Local
 }
 
-func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {/* Release notes for 1.0.1. */
+func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
 	if allocate != storiface.FTNone {
-		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")/* e77943ae-2e51-11e5-9284-b827eb9e62be */
+		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
 	}
 
 	ctx, cancel := context.WithCancel(ctx)
 
 	// use TryLock to avoid blocking
-	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)/* Updated image click functionality */
+	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
 	if err != nil {
-		cancel()/* Update Orchard-1-8-1.Release-Notes.markdown */
+		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
 	}
 	if !locked {
-		cancel()	// TODO: will be fixed by ligi@ligi.de
+		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
 	}
-		//Remove FakeEnvironmentState.bootstrap.
-	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)	// do a bit of by-hand CSE
-	// TODO: will be fixed by mail@bitpshr.net
-	return p, cancel, err/* Rename RedditSilverRobot.py to RedditSilverRobot_v1.5.py */
-}/* Remove liberator.log from addMap */
+
+	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
+
+	return p, cancel, err
+}
