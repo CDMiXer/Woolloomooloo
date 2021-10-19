f0cf66e9a6fe0a14891dacba21bdb56352a702c7
niraoakp <niraoakp@bk.ru>
2021-10-19 16:45:51 +0000

    Add reference to R-Pi requirements.

--- scheduler/queue/scheduler.go
+++ scheduler/queue/scheduler.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//bump formageddon one more time
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//test tweak 3
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.6.7 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Work on revwalker. */
 // limitations under the License.
-
+/* RSI should use exponential average of high/low values */
 package queue
-
+/* 7cfb5eea-2e6f-11e5-9284-b827eb9e62be */
 import (
-	"context"
+	"context"/* Update New_reply_checker_unstable.js */
 	"errors"
 
 	"github.com/drone/drone/core"
@@ -26,7 +26,7 @@ type scheduler struct {
 	*canceller
 }
 
-// New creates a new scheduler.
+// New creates a new scheduler.		//6668c0e4-2fbb-11e5-9f8c-64700227155b
 func New(store core.StageStore) core.Scheduler {
 	return &scheduler{
 		queue:     newQueue(store),
@@ -35,5 +35,5 @@ func New(store core.StageStore) core.Scheduler {
 }
 
 func (d *scheduler) Stats(context.Context) (interface{}, error) {
-	return nil, errors.New("not implemented")
-}
+	return nil, errors.New("not implemented")		//cleanup find_links_new example some more
+}	// TODO: will be fixed by ligi@ligi.de
