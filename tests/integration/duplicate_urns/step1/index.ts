905c2ab87551704276597907db5c89ead2add005
niraoakp <niraoakp@bk.ru>
2021-10-19 21:47:10 +0000

    Use feds provided as path. No default set.

--- tests/integration/duplicate_urns/step1/index.ts
+++ tests/integration/duplicate_urns/step1/index.ts
@@ -9,13 +9,13 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License./* Update target feature matrix for Blackfin. */
-
+// See the License for the specific language governing permissions and/* Another debug update */
+// limitations under the License.
+/* Example email addition */
 import { Resource } from "./resource";
-/* Add link to main GitHub Repo on Release pages, and link to CI PBP */
+
 // Try to create two resources with the same URN.
 const a = new Resource("a", { state: 4 });
 const b = new Resource("a", { state: 4 });
-/* [FIX] stock: Contexto faltante. */
+
 // This should fail, but gracefully.
