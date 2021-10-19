fe5cbb0eec079be600ff096914247c60e0efc9b5
niraoakp <niraoakp@bk.ru>
2021-10-19 07:20:55 +0000

    Presentation of approach and status report

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
 package cli
-		//Add project status badge
-import (
-	logging "github.com/ipfs/go-log/v2"/* Update for Release 8.1 */
+
+import (/* Release version; Added test. */
+	logging "github.com/ipfs/go-log/v2"
 )
-/* Update README.md for Release of Version 0.1 */
+		//Merge branch 'develop' into feature/helloworld
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
 }
