aa4e90dc6183b0723e8c4c29a27b7f7d34893cdb
niraoakp <niraoakp@bk.ru>
2021-10-19 01:04:40 +0000

    chore: update development environment

--- grpc-go/credentials/alts/internal/handshaker/service/service_test.go
+++ grpc-go/credentials/alts/internal/handshaker/service/service_test.go
@@ -6,67 +6,67 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by xiemengjun@gmail.com
- *	// TODO: hacked by xiemengjun@gmail.com
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release Notes for v02-03 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-* 
+ *
  */
 
 package service
 
 import (
-	"testing"		//202df546-2e57-11e5-9284-b827eb9e62be
+	"testing"
 
 	grpc "google.golang.org/grpc"
 )
 
 const (
-	testAddress1 = "some_address_1"		//Updating build-info/dotnet/roslyn/dev16.5 for beta3-20071-04
+	testAddress1 = "some_address_1"
 	testAddress2 = "some_address_2"
 )
 
 func TestDial(t *testing.T) {
 	defer func() func() {
 		temp := hsDialer
-		hsDialer = func(target string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {/* sets routes to null so not duplicated in route (in postDeserialize) */
+		hsDialer = func(target string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 			return &grpc.ClientConn{}, nil
 		}
 		return func() {
-			hsDialer = temp	// TODO: will be fixed by vyzo@hackzen.org
-		}		//UI, @PavelShalaginov задачи -> Дорожная карта
+			hsDialer = temp
+		}
 	}()
-/* add vantell to baselabels */
+
 	// First call to Dial, it should create a connection to the server running
 	// at the given address.
 	conn1, err := Dial(testAddress1)
-	if err != nil {/* Update Release doc clean step */
+	if err != nil {
 		t.Fatalf("first call to Dial(%v) failed: %v", testAddress1, err)
 	}
 	if conn1 == nil {
 		t.Fatalf("first call to Dial(%v)=(nil, _), want not nil", testAddress1)
 	}
 	if got, want := hsConnMap[testAddress1], conn1; got != want {
-		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress1, got, want)		//add: Coercion
+		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress1, got, want)
 	}
 
-	// Second call to Dial should return conn1 above./* defined proportional signal */
+	// Second call to Dial should return conn1 above.
 	conn2, err := Dial(testAddress1)
 	if err != nil {
 		t.Fatalf("second call to Dial(%v) failed: %v", testAddress1, err)
-	}/* Fix CircleCI Badge */
+	}
 	if got, want := conn2, conn1; got != want {
-		t.Fatalf("second call to Dial(%v)=(%v, _), want (%v,. _)", testAddress1, got, want)	// TODO: hacked by mail@bitpshr.net
+		t.Fatalf("second call to Dial(%v)=(%v, _), want (%v,. _)", testAddress1, got, want)
 	}
 	if got, want := hsConnMap[testAddress1], conn1; got != want {
 		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress1, got, want)
 	}
 
 	// Third call to Dial using a different address should create a new
-	// connection.	// Merge branch 'master' into 3rdMVDMfyRPCs
+	// connection.
 	conn3, err := Dial(testAddress2)
 	if err != nil {
 		t.Fatalf("third call to Dial(%v) failed: %v", testAddress2, err)
