53b9252f7ba24d67f1fd8b028150106f5eb79966
niraoakp <niraoakp@bk.ru>
2021-10-19 08:04:18 +0000

    0.1.2 release with fixed iframe for links and images

--- plugin/converter/jsonnet_oss.go
+++ plugin/converter/jsonnet_oss.go
@@ -1,27 +1,27 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: update to zlib 1.2.2
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release changes 5.1b4 */
+//		//changed logo in readme
+//      http://www.apache.org/licenses/LICENSE-2.0		//3a0c1b91-2e9c-11e5-b30b-a45e60cdfd11
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release fixes */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
+// +build oss	// TODO: hacked by boringland@protonmail.ch
 
-package converter/* Numerous spelling errors fixed */
+package converter
 
-import (
+import (		//Restored other line lost in merge
 	"github.com/drone/drone/core"
 )
 
-// Jsonnet returns a conversion service that converts the
+// Jsonnet returns a conversion service that converts the/* Update docs/ReleaseNotes.txt */
 // jsonnet file to a yaml file.
 func Jsonnet(enabled bool) core.ConvertService {
-	return new(noop)
-}
+	return new(noop)		//End bit too early in Bitstream Restrictions
+}/* Release 2.8.2.1 */
