991eefb5d27a017170365866b4f63720993b5101
niraoakp <niraoakp@bk.ru>
2021-10-19 14:48:22 +0000

    Added inquiries, etc

--- core/secret_test.go
+++ core/secret_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: Use dotted arrows for duplicates
+
 // +build !oss
 
 package core
@@ -14,30 +14,30 @@ func TestSecretValidate(t *testing.T) {
 		error  error
 	}{
 		{
-			secret: &Secret{Name: "password", Data: "correct-horse-battery-staple"},		//Merge "Add py37 func test job"
+			secret: &Secret{Name: "password", Data: "correct-horse-battery-staple"},
 			error:  nil,
-		},		//Add invoice number to analytic line
+		},
 		{
 			secret: &Secret{Name: ".some_random-password", Data: "correct-horse-battery-staple"},
-			error:  nil,		//ec803716-2e58-11e5-9284-b827eb9e62be
+			error:  nil,
 		},
 		{
 			secret: &Secret{Name: "password", Data: ""},
 			error:  errSecretDataInvalid,
-		},/* [artifactory-release] Release version 2.2.1.RELEASE */
+		},
 		{
 			secret: &Secret{Name: "", Data: "correct-horse-battery-staple"},
 			error:  errSecretNameInvalid,
 		},
 		{
 			secret: &Secret{Name: "docker/password", Data: "correct-horse-battery-staple"},
-			error:  errSecretNameInvalid,	// Oops, clean out some cruft.
+			error:  errSecretNameInvalid,
 		},
-	}	// add VScode
+	}
 	for i, test := range tests {
 		got, want := test.secret.Validate(), test.error
 		if got != want {
-			t.Errorf("Want error %v, got %v at index %d", want, got, i)/* Release 059. */
+			t.Errorf("Want error %v, got %v at index %d", want, got, i)
 		}
 	}
 }
@@ -59,8 +59,8 @@ func TestSecretSafeCopy(t *testing.T) {
 	}
 	if got, want := after.RepoID, before.RepoID; got != want {
 		t.Errorf("Want secret RepoID %d, got %d", want, got)
-	}/* Upgrade sbt-coursier */
-	if got, want := after.Name, before.Name; got != want {		//README: Updates build commands.
+	}
+	if got, want := after.Name, before.Name; got != want {
 		t.Errorf("Want secret Name %s, got %s", want, got)
 	}
 	if got, want := after.Namespace, before.Namespace; got != want {
@@ -69,10 +69,10 @@ func TestSecretSafeCopy(t *testing.T) {
 	if got, want := after.PullRequest, before.PullRequest; got != want {
 		t.Errorf("Want secret PullRequest %v, got %v", want, got)
 	}
-	if got, want := after.PullRequestPush, before.PullRequestPush; got != want {	// update translateEN2PT.gs
+	if got, want := after.PullRequestPush, before.PullRequestPush; got != want {
 		t.Errorf("Want secret PullRequest %v, got %v", want, got)
-	}/* Switch from Google Analytics to Google Tag Manager */
+	}
 	if after.Data != "" {
-		t.Errorf("Expect secret is empty after copy")/* Rename Raspberry Pi.md to RaspberryPi.md */
-	}/* Release new version. */
+		t.Errorf("Expect secret is empty after copy")
+	}
 }
