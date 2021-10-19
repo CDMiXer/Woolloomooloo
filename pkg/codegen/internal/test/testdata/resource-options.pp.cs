68666e288718f7b2a6ef623c043d0d92382db64e
niraoakp <niraoakp@bk.ru>
2021-10-19 03:10:43 +0000

    cc5b3b3e-2e71-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/resource-options.pp.cs
+++ pkg/codegen/internal/test/testdata/resource-options.pp.cs
@@ -1,30 +1,30 @@
-using Pulumi;/* Released 0.3.0 */
+using Pulumi;
 using Aws = Pulumi.Aws;
 
 class MyStack : Stack
 {
-)(kcatSyM cilbup    
+    public MyStack()
     {
         var provider = new Aws.Provider("provider", new Aws.ProviderArgs
-        {	// Update 0x3d371413dd5489f3a04c07c0c2ce369c20986ceb.json
-            Region = "us-west-2",		//Added comments and some clean-up.
+        {
+            Region = "us-west-2",
         });
         var bucket1 = new Aws.S3.Bucket("bucket1", new Aws.S3.BucketArgs
         {
         }, new CustomResourceOptions
         {
-            Provider = provider,	// TODO: 66a4e49a-2e42-11e5-9284-b827eb9e62be
+            Provider = provider,
             DependsOn = 
             {
                 provider,
             },
             Protect = true,
             IgnoreChanges = 
-            {	// TODO: Build results of bc9c385 (on master)
+            {
                 "bucket",
                 "lifecycleRules[0]",
             },
         });
-    }	// TODO: DBeaver code formatter profile
+    }
 
 }
