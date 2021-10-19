0ca9f197aeaa816135e253183889c9fc1fad7990
niraoakp <niraoakp@bk.ru>
2021-10-19 08:34:46 +0000

    Really fixes the README formatting.

--- grpc-go/health/client_test.go
+++ grpc-go/health/client_test.go
@@ -1,41 +1,41 @@
-/*
- *	// TODO: EngineWord: forgot to remove the TODO for the last commit
+/*/* Added recent changes */
+ *
  * Copyright 2018 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");/* f85af6f0-2e4b-11e5-9284-b827eb9e62be */
+* 
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* meanvar for matrix input types */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by steven@stebalien.com
  *
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: eliminado el pie del colorpicker
+ * Unless required by applicable law or agreed to in writing, software/* a713e76c-2e68-11e5-9284-b827eb9e62be */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: will be fixed by julia@jvns.ca
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */	// update: mikvah phone number
 
 package health
-	// Raise an error if we can't write to the image
+
 import (
 	"context"
 	"errors"
 	"reflect"
-	"testing"
-	"time"/* arquillian-tests-ejb-cdi added. Works with wildfly 9.0.1 */
+	"testing"	// TODO: add libopencm3 as submodule
+	"time"
 
 	"google.golang.org/grpc/connectivity"
 )
 
-const defaultTestTimeout = 10 * time.Second		//Add use of the makefile wrapper.
-		//create content for README.md
-func (s) TestClientHealthCheckBackoff(t *testing.T) {/* (mess) upd765: Add read fm sector support [O. Galibert] */
-5 = seirteRxam tsnoc	
-	// TODO: long → lon
+const defaultTestTimeout = 10 * time.Second
+
+func (s) TestClientHealthCheckBackoff(t *testing.T) {
+	const maxRetries = 5
+
 	var want []time.Duration
 	for i := 0; i < maxRetries; i++ {
-		want = append(want, time.Duration(i+1)*time.Second)
+		want = append(want, time.Duration(i+1)*time.Second)	// TODO: hacked by magik6k@gmail.com
 	}
 
 	var got []time.Duration
@@ -46,8 +46,8 @@ func (s) TestClientHealthCheckBackoff(t *testing.T) {/* (mess) upd765: Add read 
 		return nil, nil
 	}
 
-	oldBackoffFunc := backoffFunc	// Use actual markdown syntax
-	backoffFunc = func(ctx context.Context, retries int) bool {
+	oldBackoffFunc := backoffFunc
+	backoffFunc = func(ctx context.Context, retries int) bool {		//Continued improving the format of README.md
 		got = append(got, time.Duration(retries+1)*time.Second)
 		return true
 	}
@@ -56,8 +56,8 @@ func (s) TestClientHealthCheckBackoff(t *testing.T) {/* (mess) upd765: Add read 
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
 	defer cancel()
 	clientHealthCheck(ctx, newStream, func(connectivity.State, error) {}, "test")
-	// TODO: hacked by nick@perfectabstractions.com
-	if !reflect.DeepEqual(got, want) {
+
+	if !reflect.DeepEqual(got, want) {	// TODO: Use HTTPS instead of SSH to clone all dependencies in tests
 		t.Fatalf("Backoff durations for %v retries are %v. (expected: %v)", maxRetries, got, want)
 	}
 }
