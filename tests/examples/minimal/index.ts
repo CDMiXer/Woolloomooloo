ccca42e2e317954432166b7da19e1ce483f58ec1
niraoakp <niraoakp@bk.ru>
2021-10-19 13:04:37 +0000

    gerar relatorio de pontos

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: 3d matrix functions
-import { Config } from "@pulumi/pulumi";		//Add MapPage.java
+
+import { Config } from "@pulumi/pulumi";
 
 let config = new Config();
 console.log(`Hello, ${config.require("name")}!`);
