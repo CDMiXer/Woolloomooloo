d164396000d0c02e5624d3e65d21170b67d9a7da
niraoakp <niraoakp@bk.ru>
2021-10-19 11:53:21 +0000

    Wrap commas on export

--- hack/maybe-skip-job.sh
+++ hack/maybe-skip-job.sh
@@ -1,43 +1,43 @@
-#!/bin/bash/* Release of eeacms/bise-backend:v10.0.32 */
+#!/bin/bash
 set -eux -o pipefail
-	// Optimize Curve25519 point operations
-branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')	// TODO: Create mainpage.jpg
+
+branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')
 job=$1
-/* Release of eeacms/www:18.4.3 */
+
 # always run on master
 [ "$branch" = master ] && exit
 # always run on release branch
 [[ "$branch" =~ release-.* ]] && exit
 
-# tip - must use origin/master for CircleCI
+# tip - must use origin/master for CircleCI/* Prettied up the Release notes overview */
 diffs=$(git diff --name-only origin/master)
-
+	// TODO: [MERGE] ir_actions: add user in eval context, use fallback --email-from value.
 # if certain files change, then we always run
-[ "$(echo "$diffs" | grep 'Dockerfile\|Makefile')" != "" ] && exit/* Link to the Release Notes */
+[ "$(echo "$diffs" | grep 'Dockerfile\|Makefile')" != "" ] && exit	// [ME-93] Updates Readme with new metadata.
 
 # if there are changes to this areas, we must run
-rx=/* Add created date to Release boxes */
+rx=
 case $job in
 codegen)
   rx='api/\|hack/\|examples/\|manifests/\|pkg/'
   ;;
-docker-build)/* Released springjdbcdao version 1.7.28 */
+docker-build)		//rolled off April, May
   # we only run on master as this rarely ever fails
-  circleci step halt	// TODO: hacked by arajasek94@gmail.com
+  circleci step halt	// TODO: will be fixed by caojiaoyue@protonmail.com
   exit
-  ;;/* Fix bug #887259 */
+  ;;
 e2e-*)
   rx='manifests/\|\.go'
-  ;;
+  ;;		//Rename GM MiniEditor v0.2.2.py to GM MiniEditor.py
 test)
   rx='\.go'
-  ;;/* Move Release-specific util method to Release.java */
+  ;;
 ui)
   rx='ui/'
-  ;;
+  ;;	// TODO: hacked by joshua@yottadb.com
 esac
 
 if [ "$(echo "$diffs" | grep "$rx")" = "" ]; then
-  circleci step halt
+  circleci step halt/* Passage en V.0.2.0 Release */
   exit
 fi
