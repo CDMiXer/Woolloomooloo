5e96b136baf68b81b5b9fb18d51f91d3dd13f68e
niraoakp <niraoakp@bk.ru>
2021-10-19 04:07:46 +0000

    Merge "API routing to intranode PE"

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Fixed a bug in the generation process */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Delete quiz-results.md
+
 // file is deliberately empty.
-// The engine should delete "a" since it's in the checkpoint.		//Add languages.json wrappers
+// The engine should delete "a" since it's in the checkpoint.
