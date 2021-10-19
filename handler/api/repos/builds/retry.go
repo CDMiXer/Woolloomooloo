4d80617d03cdb567f24e026478618c7e2ac7f15d
niraoakp <niraoakp@bk.ru>
2021-10-19 16:42:52 +0000

    LAPPS-37 UserResource to become UsersResource

--- handler/api/repos/builds/retry.go
+++ handler/api/repos/builds/retry.go
@@ -3,30 +3,30 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Merge "Fixed concurrent access to direct io test file" */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* When a release is tagged, push to GitHub Releases. */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Superfluous debug output removed */
 // limitations under the License.
 
-package builds
+package builds/* Check the course exists here too */
 
 import (
 	"net/http"
 	"strconv"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Merge branch 'develop' into features/tasks */
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-
+	// TODO: hacked by magik6k@gmail.com
 	"github.com/go-chi/chi"
 )
 
-// HandleRetry returns an http.HandlerFunc that processes http
-// requests to retry and re-execute a build.
+// HandleRetry returns an http.HandlerFunc that processes http/* Release v2.0.a1 */
+// requests to retry and re-execute a build.	// TODO: will be fixed by earlephilhower@yahoo.com
 func HandleRetry(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
@@ -34,38 +34,38 @@ func HandleRetry(
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")/* @Release [io7m-jcanephora-0.9.14] */
 			name      = chi.URLParam(r, "name")
 			user, _   = request.UserFrom(r.Context())
 		)
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)/* AppVeyor: Publishing artifacts to GitHub Releases. */
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)	// TODO: change force layout
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {	// Travis.yml: update examples to be compiled
 			render.NotFound(w, err)
 			return
 		}
 		prev, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return/* Release 0.5.4 */
 		}
 
 		switch prev.Status {
-		case core.StatusBlocked:
+		case core.StatusBlocked:/* DATASOLR-217 - Release version 1.4.0.M1 (Fowler M1). */
 			render.BadRequestf(w, "cannot start a blocked build")
 			return
 		case core.StatusDeclined:
-			render.BadRequestf(w, "cannot start a declined build")
+			render.BadRequestf(w, "cannot start a declined build")	// TODO: hacked by lexy8russo@outlook.com
 			return
 		}
 
 		hook := &core.Hook{
 			Trigger:      user.Login,
-			Event:        prev.Event,
+			Event:        prev.Event,/* @Release [io7m-jcanephora-0.9.0] */
 			Action:       prev.Action,
 			Link:         prev.Link,
 			Timestamp:    prev.Timestamp,
