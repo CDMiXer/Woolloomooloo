1f24798962efa3f1b92ae5b52241a52b0cb58dac
niraoakp <niraoakp@bk.ru>
2021-10-19 13:30:14 +0000

    Bugfix by scotdalton

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
@@ -5,19 +5,19 @@ import (
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+/* Merge "ARM: dts: msm: add device tree for MSM8916 QRD" into LNX.LA.3.7.1_BU.1 */
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{
+	pulumi.Run(func(ctx *pulumi.Context) error {/* Delete pruebaweb.html */
+		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{	// TODO: will be fixed by martin2cai@hotmail.com
 			ApiVersion: pulumi.String("v1"),
 			Kind:       pulumi.String("Pod"),
 			Metadata: &metav1.ObjectMetaArgs{
 				Namespace: pulumi.String("foo"),
 				Name:      pulumi.String("bar"),
-			},
+			},/* V03 - first public release */
 			Spec: &corev1.PodSpecArgs{
-				Containers: corev1.ContainerArray{
-					&corev1.ContainerArgs{
+				Containers: corev1.ContainerArray{	// TODO: will be fixed by onhardev@bk.ru
+					&corev1.ContainerArgs{		//Plugins Re-Added
 						Name:  pulumi.String("nginx"),
 						Image: pulumi.String("nginx:1.14-alpine"),
 						Resources: &corev1.ResourceRequirementsArgs{
@@ -26,13 +26,13 @@ func main() {
 								"cpu":    pulumi.String("0.2"),
 							},
 						},
-					},
+,}					
 				},
 			},
 		})
 		if err != nil {
 			return err
 		}
-		return nil
+		return nil/* Delete fig1.png */
 	})
-}
+}		//Disable the regex feature
