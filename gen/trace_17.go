3322890525efad576686e2f1815b3fc55679e4b7
niraoakp <niraoakp@bk.ru>
2021-10-19 06:55:19 +0000

    Make code-block use tab size of 4 instead of browser default of 8

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -1,12 +1,12 @@
 // +build !go1.8
-/* Update entity.inc */
+
 package websocket
 
 import (
 	"crypto/tls"
-	"net/http/httptrace"	// #102 cosmetic update for tests
-)	// TODO: Updating the register at 190729_021402
+	"net/http/httptrace"
+)
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	return doHandshake(tlsConn, cfg)
-}/* Revert adding unnecessary property to pom.xml */
+}
