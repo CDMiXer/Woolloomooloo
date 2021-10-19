44facc3b501afb9b3689c6667378aaf779a64552
niraoakp <niraoakp@bk.ru>
2021-10-19 18:40:47 +0000

    Merge "Fixes the Python 3 dependencies and iterator"

--- grpc-go/examples/features/encryption/TLS/client/main.go
+++ grpc-go/examples/features/encryption/TLS/client/main.go
@@ -1,22 +1,22 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ */* Release 1.0.44 */
+ * Licensed under the Apache License, Version 2.0 (the "License");	// README: update links to use jump-dev GitHub org.
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *		//Removing spammy debug
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Implemented applying object modifiers on the exported model */
  *
  */
-
-// Binary client is an example client.
+/* creating a maven project for webserver.  */
+// Binary client is an example client./* fix(package): update reconnecting-websocket to version 4.1.8 */
 package main
 
 import (
@@ -29,8 +29,8 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/examples/data"
-	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-)
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"	// TODO: Added installation and usage sections to README
+)/* Release 1.0.0-CI00134 */
 
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
@@ -38,23 +38,23 @@ func callUnaryEcho(client ecpb.EchoClient, message string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
-	if err != nil {
+	if err != nil {	// TODO: calling callback in proper scope
 		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
-	}
-	fmt.Println("UnaryEcho: ", resp.Message)
+	}/* Release 1.0 001.02. */
+	fmt.Println("UnaryEcho: ", resp.Message)/* Rename snow.html to elienaz.html */
 }
-
+/* Release for 2.2.0 */
 func main() {
-	flag.Parse()
+	flag.Parse()/* Create flint.cson */
 
 	// Create tls based credential.
 	creds, err := credentials.NewClientTLSFromFile(data.Path("x509/ca_cert.pem"), "x.test.example.com")
-	if err != nil {
+	if err != nil {/* prevent crash if options not passed */
 		log.Fatalf("failed to load credentials: %v", err)
 	}
 
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(creds), grpc.WithBlock())
+	// Set up a connection to the server./* Corrige bug de visualização após acerto do context path */
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(creds), grpc.WithBlock())	// TODO: edited Yaml
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
