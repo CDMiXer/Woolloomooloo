7384b7f4b40d46f55f8cd4c9aae2f177149da9cf
niraoakp <niraoakp@bk.ru>
2021-10-19 04:22:24 +0000

    Added Kent Nguyen's article on burnout.

--- handler/web/logout_test.go
+++ handler/web/logout_test.go
@@ -2,24 +2,24 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package web	// Update PNL_sniffer.py
-
-import (		//Upgrade isbinaryfile to 2.0.1
+package web
+		//Take it easy on the logging. 
+import (
 	"net/http/httptest"
-	"testing"
-)	// add apache 2.0 license
-	// TODO: Update kolabnotes-fx.desktop
+	"testing"/* Release v2.0 */
+)
+	// TODO: will be fixed by hugomrdias@gmail.com
 func TestLogout(t *testing.T) {
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/logout", nil)
+	r := httptest.NewRequest("GET", "/logout", nil)	// TODO: styling translation pages refs #20355
 
 	HandleLogout().ServeHTTP(w, r)
-
-	if got, want := w.Code, 200; want != got {
+	// TODO: will be fixed by souzau@yandex.com
+	if got, want := w.Code, 200; want != got {/* Merge "Let get-prebuilt-src-arch return empty if the input is empty" */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-		//Fixing tests for validations
+
 	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {
-		t.Errorf("Want response code %q, got %q", want, got)
+		t.Errorf("Want response code %q, got %q", want, got)		//7b1c13c0-2e52-11e5-9284-b827eb9e62be
 	}
 }
