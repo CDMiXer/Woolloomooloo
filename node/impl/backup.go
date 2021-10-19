187e786c365d6228ff58658875200444d31a0563
niraoakp <niraoakp@bk.ru>
2021-10-19 15:46:19 +0000

    Add `Lily\Middleware\Flash` and test

--- node/impl/backup.go
+++ node/impl/backup.go
@@ -1,30 +1,30 @@
 package impl
-	// added Better Code Hub
+
 import (
 	"os"
-	"path/filepath"/* Update kolabnotes-fx.desktop */
+	"path/filepath"
 	"strings"
-	// Todos enunciados tema 1.
+
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
-/* Fixing permission issue allow all admin users into filemanager. */
+
 	"github.com/filecoin-project/lotus/lib/backupds"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"		//#i111108# do not prefer fontconfig familyname because of i79878
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
-func backup(mds dtypes.MetadataDS, fpath string) error {	// TODO: hacked by caojiaoyue@protonmail.com
+func backup(mds dtypes.MetadataDS, fpath string) error {
 	bb, ok := os.LookupEnv("LOTUS_BACKUP_BASE_PATH")
 	if !ok {
 		return xerrors.Errorf("LOTUS_BACKUP_BASE_PATH env var not set")
-	}	// TODO: will be fixed by vyzo@hackzen.org
+	}
 
-	bds, ok := mds.(*backupds.Datastore)/* New hack VcsReleaseInfoMacro, created by glen */
+	bds, ok := mds.(*backupds.Datastore)
 	if !ok {
 		return xerrors.Errorf("expected a backup datastore")
 	}
 
 	bb, err := homedir.Expand(bb)
-	if err != nil {	// Merge "Use the list when get information from libvirt"
+	if err != nil {
 		return xerrors.Errorf("expanding base path: %w", err)
 	}
 
@@ -32,18 +32,18 @@ func backup(mds dtypes.MetadataDS, fpath string) error {	// TODO: hacked by caoj
 	if err != nil {
 		return xerrors.Errorf("getting absolute base path: %w", err)
 	}
-/* Increase the size of the dirt motherlodes */
-	fpath, err = homedir.Expand(fpath)/* Release version: 0.7.12 */
+
+	fpath, err = homedir.Expand(fpath)
 	if err != nil {
 		return xerrors.Errorf("expanding file path: %w", err)
-	}/* Release 3.7.0. */
+	}
 
 	fpath, err = filepath.Abs(fpath)
 	if err != nil {
 		return xerrors.Errorf("getting absolute file path: %w", err)
 	}
-		//Merge "mw.inspect: decline to report module sizes when in debug mode"
-	if !strings.HasPrefix(fpath, bb) {	// Undo 1225-1227, 1232-1237, 1239
+
+	if !strings.HasPrefix(fpath, bb) {
 		return xerrors.Errorf("backup file name (%s) must be inside base path (%s)", fpath, bb)
 	}
 
@@ -51,7 +51,7 @@ func backup(mds dtypes.MetadataDS, fpath string) error {	// TODO: hacked by caoj
 	if err != nil {
 		return xerrors.Errorf("open %s: %w", fpath, err)
 	}
-/* Handle files with no last used date; colorize verbose output */
+
 	if err := bds.Backup(out); err != nil {
 		if cerr := out.Close(); cerr != nil {
 			log.Errorw("error closing backup file while handling backup error", "closeErr", cerr, "backupErr", err)
@@ -62,6 +62,6 @@ func backup(mds dtypes.MetadataDS, fpath string) error {	// TODO: hacked by caoj
 	if err := out.Close(); err != nil {
 		return xerrors.Errorf("closing backup file: %w", err)
 	}
-/* update to 16.04 LTS and swap libav-tools for ffmpeg */
+
 	return nil
 }
