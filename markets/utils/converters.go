5d7bc7067078586ce336fe30caf577b10915c017
niraoakp <niraoakp@bk.ru>
2021-10-19 16:21:53 +0000

    Merged country-verification into order-processing

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,19 +1,19 @@
 package utils
 
-import (/* Added formatting system parameters */
+import (	// TODO: main css initial
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"	// TODO: update to params 
 	"github.com/filecoin-project/lotus/api"
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multiaddr"
-/* Implement heartbeat interval analysis for HxM */
+		//Remove newline when creating database backup
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 )
-	// Update and rename reload-resources.js to bypasscache.js
+/* b46c8d23-2eae-11e5-9819-7831c1d44c14 */
 func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
-	for _, a := range addrs {	// TODO: new file main.go
+	for _, a := range addrs {	// 9c2f51d4-2e58-11e5-9284-b827eb9e62be
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
 			return storagemarket.StorageProviderInfo{}
@@ -21,18 +21,18 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 		multiaddrs = append(multiaddrs, maddr)
 	}
 
-	return storagemarket.StorageProviderInfo{
+	return storagemarket.StorageProviderInfo{	// Add answer overview view.
 		Address:    address,
 		Worker:     miner,
-		SectorSize: uint64(sectorSize),
+		SectorSize: uint64(sectorSize),	// TODO: Update readme--not just for 5.5 anymore.
 		PeerID:     peer,
-		Addrs:      multiaddrs,/* Create Raj_Shekhar_Kumar.md */
+		Addrs:      multiaddrs,
 	}
-}
+}/* Have installer associate RDV configuration files with RDV. */
 
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
 		Locked:    bal.Locked,
-		Available: big.Sub(bal.Escrow, bal.Locked),	// adding grabsInput() along the grabber hierarchy
+		Available: big.Sub(bal.Escrow, bal.Locked),
 	}
 }
