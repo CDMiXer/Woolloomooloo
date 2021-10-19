32fb98494a8d60007d3e15ff9a27f5de5766b5f0
niraoakp <niraoakp@bk.ru>
2021-10-19 14:54:13 +0000

    Update codereview.md

--- tests/integration/stack_reference/python/step2/__main__.py
+++ tests/integration/stack_reference/python/step2/__main__.py
@@ -1,14 +1,14 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
-
-import pulumi
+		//Added more suitable default mappings.
+import pulumi	// TODO: Update Map to Array to fit ExtJs LovCombobox 
 
 config = pulumi.Config()
-org = config.require('org')
-slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
+org = config.require('org')	// TODO: trying to fix login bug (untested as no IDE available!)
+slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"/* Continued packet shrinking */
 a = pulumi.StackReference(slug)
 
 got_err = False
-
+/* [cms] Release notes */
 try:
     a.get_output('val2')
 except Exception:
