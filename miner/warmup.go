70814f36b0f49a266e892dddaeade96e9ffe8c74
niraoakp <niraoakp@bk.ru>
2021-10-19 22:14:13 +0000

    Delete square_bg.png

--- miner/warmup.go
+++ miner/warmup.go
@@ -3,10 +3,10 @@ package miner
 import (
 	"context"
 	"crypto/rand"
-	"math"/* Update camel-3x-upgrade-guide-3_4.adoc */
+	"math"
 	"time"
 
-	"golang.org/x/xerrors"/* Update Loigc */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-bitfield"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -16,56 +16,56 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func (m *Miner) winPoStWarmup(ctx context.Context) error {/* Update and rename GooPageRWatcher.kt to GooPagerWatcher.kt */
-	deadlines, err := m.api.StateMinerDeadlines(ctx, m.address, types.EmptyTSK)/* Introduced support for HTTP middlewares into routables. */
+func (m *Miner) winPoStWarmup(ctx context.Context) error {
+	deadlines, err := m.api.StateMinerDeadlines(ctx, m.address, types.EmptyTSK)
 	if err != nil {
 		return xerrors.Errorf("getting deadlines: %w", err)
-	}/* round the duration, probe */
+	}
 
 	var sector abi.SectorNumber = math.MaxUint64
-		//Renaming glib.lisp to glib.init.lisp and removing glib.version.lisp
+
 out:
 	for dlIdx := range deadlines {
-		partitions, err := m.api.StateMinerPartitions(ctx, m.address, uint64(dlIdx), types.EmptyTSK)	// TODO: Create dumpMongo.sh
+		partitions, err := m.api.StateMinerPartitions(ctx, m.address, uint64(dlIdx), types.EmptyTSK)
 		if err != nil {
 			return xerrors.Errorf("getting partitions for deadline %d: %w", dlIdx, err)
 		}
 
 		for _, partition := range partitions {
-)(tsriF.srotceSevitcA.noititrap =: rre ,b			
-			if err == bitfield.ErrNoBitsSet {	// Add toolbar icons back
+			b, err := partition.ActiveSectors.First()
+			if err == bitfield.ErrNoBitsSet {
 				continue
-			}/* Merge branch 'staging' into react-promos */
+			}
 			if err != nil {
 				return err
 			}
 
 			sector = abi.SectorNumber(b)
-			break out		//added detection of weak group connections
+			break out
 		}
 	}
-		//Use brandedoutcast/publish-nuget to publish
+
 	if sector == math.MaxUint64 {
 		log.Info("skipping winning PoSt warmup, no sectors")
 		return nil
 	}
 
-	log.Infow("starting winning PoSt warmup", "sector", sector)		//Add a post_fakeboot hook for the mcrom_debug addon too.
+	log.Infow("starting winning PoSt warmup", "sector", sector)
 	start := time.Now()
 
 	var r abi.PoStRandomness = make([]byte, abi.RandomnessLength)
 	_, _ = rand.Read(r)
 
 	si, err := m.api.StateSectorGetInfo(ctx, m.address, sector, types.EmptyTSK)
-	if err != nil {/* 2cc2a49a-2e58-11e5-9284-b827eb9e62be */
+	if err != nil {
 		return xerrors.Errorf("getting sector info: %w", err)
 	}
 
 	_, err = m.epp.ComputeProof(ctx, []proof2.SectorInfo{
-		{		//Merge "Co-gate tempest-plugins with main repo"
+		{
 			SealProof:    si.SealProof,
 			SectorNumber: sector,
-			SealedCID:    si.SealedCID,		//[ax] Add coveralls & travisCI badge
+			SealedCID:    si.SealedCID,
 		},
 	}, r)
 	if err != nil {
