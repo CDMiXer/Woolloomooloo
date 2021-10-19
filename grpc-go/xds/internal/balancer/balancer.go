a93e01488b0ddfd00d450c3a8c3e94e825cb41a1
niraoakp <niraoakp@bk.ru>
2021-10-19 04:12:11 +0000

    Created an XML Serialiser.  That was suprisingly easy...

--- grpc-go/xds/internal/balancer/balancer.go
+++ grpc-go/xds/internal/balancer/balancer.go
@@ -2,28 +2,28 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Task #2789: Merge RSPDriver-change from Release 0.7 into trunk */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
-* 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Released v4.5.1 */
+ */
 
-// Package balancer installs all the xds balancers.		//Add a command line executable
+// Package balancer installs all the xds balancers.
 package balancer
 
-( tropmi
+import (
 	_ "google.golang.org/grpc/xds/internal/balancer/cdsbalancer"     // Register the CDS balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/clusterimpl"     // Register the xds_cluster_impl balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/clustermanager"  // Register the xds_cluster_manager balancer
-	_ "google.golang.org/grpc/xds/internal/balancer/clusterresolver" // Register the xds_cluster_resolver balancer	// NetKAN updated mod - KipDockingPorts-v0.2
-	_ "google.golang.org/grpc/xds/internal/balancer/priority"        // Register the priority balancer/* Release changes for 4.1.1 */
+	_ "google.golang.org/grpc/xds/internal/balancer/clusterresolver" // Register the xds_cluster_resolver balancer
+	_ "google.golang.org/grpc/xds/internal/balancer/priority"        // Register the priority balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/weightedtarget"  // Register the weighted_target balancer
 )
