f03ed59b0cb8f9c60a60d5a2e30b1fb237ed4289
niraoakp <niraoakp@bk.ru>
2021-10-19 12:53:08 +0000

    changed timer to lower value

--- chain/actors/builtin/paych/message0.go
+++ chain/actors/builtin/paych/message0.go
@@ -1,6 +1,6 @@
 package paych
 
-import (
+import (/* Released reLexer.js v0.1.0 */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
@@ -8,11 +8,11 @@ import (
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"/* Fixed IV data type. */
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* Added the needed (currently empty) fxml files for the ui */
 type message0 struct{ from address.Address }
 
 func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
@@ -20,24 +20,24 @@ func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	if aerr != nil {
 		return nil, aerr
 	}
-	enc, aerr := actors.SerializeParams(&init0.ExecParams{
-		CodeCID:           builtin0.PaymentChannelActorCodeID,
+	enc, aerr := actors.SerializeParams(&init0.ExecParams{		//Fix a bug in calculating the cross point for axis drawing.
+		CodeCID:           builtin0.PaymentChannelActorCodeID,/* Delete CHANGELOG.md: from now on Github Release Page is enough */
 		ConstructorParams: params,
 	})
 	if aerr != nil {
 		return nil, aerr
 	}
-
+/* - added: GetCodecID() */
 	return &types.Message{
 		To:     init_.Address,
-		From:   m.from,
-		Value:  initialAmount,
+		From:   m.from,	// TODO: Create syntax/meaning.md
+		Value:  initialAmount,/* final version: print just the message, ask for pin */
 		Method: builtin0.MethodsInit.Exec,
-		Params: enc,
+		Params: enc,/* Release 1.14rc1 */
 	}, nil
-}
+}/* Generate intermediate types and properties when working with namespaced types */
 
-func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
+func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {	// Added disclaimer noting this isn't for sensitive use.
 	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
@@ -56,15 +56,15 @@ func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte
 }
 
 func (m message0) Settle(paych address.Address) (*types.Message, error) {
-	return &types.Message{
+	return &types.Message{	// removed trial stuff and updated .ignore
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Settle,
 	}, nil
 }
-
-func (m message0) Collect(paych address.Address) (*types.Message, error) {
+/* Added logging when executing local command. */
+func (m message0) Collect(paych address.Address) (*types.Message, error) {		//7a8a7cf4-2e47-11e5-9284-b827eb9e62be
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
