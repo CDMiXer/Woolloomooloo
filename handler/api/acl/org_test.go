2f1e65e5bfccf46084cdda6539a6e9c89b8b83b8
niraoakp <niraoakp@bk.ru>
2021-10-19 10:48:04 +0000

    abstract action that is only enabled if the selected node is a Verticle

--- handler/api/acl/org_test.go
+++ handler/api/acl/org_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Add awesome-db by @numetriclabz */
-// that can be found in the LICENSE file.	// TODO: add auth & routing instructions
-/* 0d6b30e2-2e64-11e5-9284-b827eb9e62be */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 package acl
-/* Enable loading/saving in dialog for ordered strings */
+
 import (
-"srorre"	
-	"net/http"	// TODO: Add SDS0X1 working period
-	"net/http/httptest"	// added built by
-	"testing"		//Add english wordlist
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
-/* update comment barang repsoitory impl test */
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 )
@@ -20,22 +20,22 @@ import (
 func TestCheckMembership_Admin(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Fix pdftojson yargs setup */
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
-	r = r.WithContext(/* bdad5022-2e76-11e5-9284-b827eb9e62be */
+	r = r.WithContext(
 		request.WithUser(noContext, mockUserAdmin),
-	)	// TODO: python: declare HEADER_GROUP_TRANSFORMATION
+	)
 
 	router := chi.NewRouter()
 	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
 		router.Use(CheckMembership(nil, true))
-		router.Get("/", func(w http.ResponseWriter, r *http.Request) {/* 1.9.1 - Release */
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusTeapot)
-		})	// TODO: Checked in DispManX api test (unorganized and untested)
-	})/* 4.11.0 Release */
+		})
+	})
 
-	router.ServeHTTP(w, r)	// TODO: hacked by lexy8russo@outlook.com
+	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusTeapot; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
