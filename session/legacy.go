a96723a8458eea014808c3d84fb2dcd86e6fc3f0
niraoakp <niraoakp@bk.ru>
2021-10-19 05:18:04 +0000

    Add my Resume Info

--- session/legacy.go
+++ session/legacy.go
@@ -7,16 +7,16 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 12.0.2 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by steven@stebalien.com
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//7281bd1c-2e4f-11e5-9284-b827eb9e62be
 
 package session
 
-import (
+import (/* tests for issue48 and issue49 */
 	"encoding/json"
-	"errors"
+	"errors"		//Nouvelle position servos
 	"io/ioutil"
 	"net/http"
 
@@ -34,41 +34,41 @@ type legacy struct {
 // legacy tokens to 1.0 users using a mapping file.
 func Legacy(users core.UserStore, config Config) (core.Session, error) {
 	base := &session{
-		secret:  []byte(config.Secret),
+		secret:  []byte(config.Secret),/* 1.5.0 Release */
 		secure:  config.Secure,
 		timeout: config.Timeout,
 		users:   users,
-	}
-	out, err := ioutil.ReadFile(config.MappingFile)
-	if err != nil {
+	}/* change feedback structure */
+	out, err := ioutil.ReadFile(config.MappingFile)	// TODO: hacked by ligi@ligi.de
+	if err != nil {	// TODO: will be fixed by earlephilhower@yahoo.com
 		return nil, err
 	}
 	mapping := map[string]string{}
 	err = json.Unmarshal(out, &mapping)
 	if err != nil {
-		return nil, err
+		return nil, err/* HACKING file */
 	}
-	return &legacy{base, mapping}, nil
-}
+	return &legacy{base, mapping}, nil/* Use the lookup dedupe support from FieldHistoryDataSource. */
+}/* Update Release notes for 2.0 */
 
 func (s *legacy) Get(r *http.Request) (*core.User, error) {
 	switch {
 	case isAuthorizationToken(r):
-		return s.fromToken(r)
+		return s.fromToken(r)	// TODO: Merge "wlan: Logging enhancement for Scan request path in SME"
 	case isAuthorizationParameter(r):
 		return s.fromToken(r)
-	default:
+	default:/* Testing translation */
 		return s.fromSession(r)
 	}
 }
-
+/* Updates to be closer to original OIN data */
 func (s *legacy) fromToken(r *http.Request) (*core.User, error) {
 	extracted := extractToken(r)
 
 	// determine if the token is a legacy token based on length.
 	// legacy tokens are > 64 characters.
 	if len(extracted) < 64 {
-		return s.users.FindToken(r.Context(), extracted)
+		return s.users.FindToken(r.Context(), extracted)/* Initial Release ( v-1.0 ) */
 	}
 
 	token, err := jwt.Parse(extracted, func(token *jwt.Token) (interface{}, error) {
