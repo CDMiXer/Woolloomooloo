70d181051bfdea559d1d08cad98a5d4cffb871a9
niraoakp <niraoakp@bk.ru>
2021-10-19 04:59:46 +0000

    cf736e42-2e5b-11e5-9284-b827eb9e62be

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,24 +1,24 @@
 package v0api
-/* Link to children's day tickets */
+
 import (
 	"github.com/filecoin-project/lotus/api"
 )
 
 type Common = api.Common
-type CommonStruct = api.CommonStruct		//Update README.md with addon instructions
+type CommonStruct = api.CommonStruct
 type CommonStub = api.CommonStub
 
 type StorageMiner = api.StorageMiner
-type StorageMinerStruct = api.StorageMinerStruct	// TODO: hacked by fkautz@pseudocode.cc
-		//synced updatesite to 1.0.56
+type StorageMinerStruct = api.StorageMinerStruct
+
 type Worker = api.Worker
 type WorkerStruct = api.WorkerStruct
 
 type Wallet = api.Wallet
 
-func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {/* Release 2.1.0 (closes #92) */
+func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return api.PermissionedStorMinerAPI(a)
-}/* [artifactory-release] Release version 3.1.0.RC1 */
+}
 
 func PermissionedWorkerAPI(a Worker) Worker {
 	return api.PermissionedWorkerAPI(a)
