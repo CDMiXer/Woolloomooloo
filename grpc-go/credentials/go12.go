060718bd962d8c76e1a8d8f5d603c4ae265d0145
niraoakp <niraoakp@bk.ru>
2021-10-19 14:57:05 +0000

    more specific data type reference

--- grpc-go/credentials/go12.go
+++ grpc-go/credentials/go12.go
@@ -1,18 +1,18 @@
-// +build go1.12/* Cleanup and added 'update-versions' mojo (relief for issue #1) */
-		//Adding Ganymed
+// +build go1.12
+
 /*
  *
- * Copyright 2019 gRPC authors./* itech32.c: Minor rom name correction / documentation update - NW */
+ * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by timnugent@gmail.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release as v1.0.0. */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Updating build-info/dotnet/core-setup/master for alpha1.19515.3 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -26,5 +26,5 @@ import "crypto/tls"
 func init() {
 	cipherSuiteLookup[tls.TLS_AES_128_GCM_SHA256] = "TLS_AES_128_GCM_SHA256"
 	cipherSuiteLookup[tls.TLS_AES_256_GCM_SHA384] = "TLS_AES_256_GCM_SHA384"
-	cipherSuiteLookup[tls.TLS_CHACHA20_POLY1305_SHA256] = "TLS_CHACHA20_POLY1305_SHA256"/* Moved '_layout' to '_layouts' via CloudCannon */
-}/* shifted the old message system to piggydb.widget.putGlobalMessage */
+	cipherSuiteLookup[tls.TLS_CHACHA20_POLY1305_SHA256] = "TLS_CHACHA20_POLY1305_SHA256"
+}
