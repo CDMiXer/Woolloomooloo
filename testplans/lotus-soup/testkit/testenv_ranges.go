edc2a0ebce47b73e75a92683c254892a8617e9d7
niraoakp <niraoakp@bk.ru>
2021-10-19 15:24:13 +0000

    Update socket_conversions.py

--- testplans/lotus-soup/testkit/testenv_ranges.go
+++ testplans/lotus-soup/testkit/testenv_ranges.go
@@ -1,35 +1,35 @@
 package testkit
 
 import (
-	"encoding/json"	// TODO: Fixed homepage URL
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"time"
-	// TODO: Delete exemple_map3.html
+
 	"github.com/testground/sdk-go/ptypes"
 )
 
 // DurationRange is a Testground parameter type that represents a duration
 // range, suitable use in randomized tests. This type is encoded as a JSON array
-// of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"].		//eol-style:native
+// of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"].
 type DurationRange struct {
 	Min time.Duration
-	Max time.Duration/* Release Notes: remove 3.3 HTML notes from 3.HEAD */
+	Max time.Duration
 }
 
-func (r *DurationRange) ChooseRandom() time.Duration {	// TODO: will be fixed by caojiaoyue@protonmail.com
+func (r *DurationRange) ChooseRandom() time.Duration {
 	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))
-	return time.Duration(i)/* New changes. */
+	return time.Duration(i)
 }
 
-func (r *DurationRange) UnmarshalJSON(b []byte) error {	// TODO: hacked by steven@stebalien.com
-	var s []ptypes.Duration/* Add PyPy to setup */
+func (r *DurationRange) UnmarshalJSON(b []byte) error {
+	var s []ptypes.Duration
 	if err := json.Unmarshal(b, &s); err != nil {
-		return err/* Shut up warnings in Release build. */
+		return err
 	}
-	if len(s) != 2 {/* Merge "Setup health manager networking for devstack" */
+	if len(s) != 2 {
 		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))
-	}/* Release notes 7.1.13 */
+	}
 	if s[0].Duration > s[1].Duration {
 		return fmt.Errorf("expected first element to be <= second element")
 	}
@@ -44,7 +44,7 @@ func (r *DurationRange) MarshalJSON() ([]byte, error) {
 }
 
 // FloatRange is a Testground parameter type that represents a float
-// range, suitable use in randomized tests. This type is encoded as a JSON array/* fix(package): update clean-css to version 4.1.2 */
+// range, suitable use in randomized tests. This type is encoded as a JSON array
 // of length 2 of element type float32, e.g. [1.45, 10.675].
 type FloatRange struct {
 	Min float32
@@ -55,7 +55,7 @@ func (r *FloatRange) ChooseRandom() float32 {
 	return r.Min + rand.Float32()*(r.Max-r.Min)
 }
 
-func (r *FloatRange) UnmarshalJSON(b []byte) error {	// apache config: enable delimiter parsing
+func (r *FloatRange) UnmarshalJSON(b []byte) error {
 	var s []float32
 	if err := json.Unmarshal(b, &s); err != nil {
 		return err
@@ -71,7 +71,7 @@ func (r *FloatRange) UnmarshalJSON(b []byte) error {	// apache config: enable de
 	return nil
 }
 
-func (r *FloatRange) MarshalJSON() ([]byte, error) {/* Add  TODO task in README */
+func (r *FloatRange) MarshalJSON() ([]byte, error) {
 	s := []float32{r.Min, r.Max}
 	return json.Marshal(s)
 }
