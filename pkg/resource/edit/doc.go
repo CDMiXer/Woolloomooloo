b332d683ea61e290e026c12fdbd24a7f6030c503
niraoakp <niraoakp@bk.ru>
2021-10-19 20:20:19 +0000

    mini correction to version title.

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -4,13 +4,13 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Released 0.0.15 */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//#13: Fixed java.lang.OutOfMemoryError
+// limitations under the License.
 
 // Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
 // tools that present a means for users to surgically edit their state.
