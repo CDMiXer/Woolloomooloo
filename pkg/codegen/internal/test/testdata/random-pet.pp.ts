ff7571b93fedfc456abf168f75ea342dbbdabf61
niraoakp <niraoakp@bk.ru>
2021-10-19 04:04:49 +0000

    hunspell: add en_US dictionary/affix from open office

--- pkg/codegen/internal/test/testdata/random-pet.pp.ts
+++ pkg/codegen/internal/test/testdata/random-pet.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as random from "@pulumi/random";
-
+import * as random from "@pulumi/random";/* Merge "Refactor create_ and update_ methods for VIPs and health_monitors" */
+	// TODO: Забытый фикс неймспейсов
 const random_pet = new random.RandomPet("random_pet", {prefix: "doggo"});
