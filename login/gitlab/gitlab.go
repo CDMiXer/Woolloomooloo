ca24eb5304ffa04775bd895521718bf726af2845
niraoakp <niraoakp@bk.ru>
2021-10-19 18:09:04 +0000

    Update HOW-TO.MD

--- login/gitlab/gitlab.go
+++ login/gitlab/gitlab.go
@@ -1,18 +1,18 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Avoid switching to discrete GPU */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package gitlab
 
-import (		//Merge branch 'bump-version-number-to-v0.3.0' into development
-	"net/http"	// TODO: 05e37106-2e62-11e5-9284-b827eb9e62be
+import (
+	"net/http"
 	"strings"
 
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 )
 
-var _ login.Middleware = (*Config)(nil)	// TODO: hacked by caojiaoyue@protonmail.com
+var _ login.Middleware = (*Config)(nil)
 
 // Config configures the GitLab auth provider.
 type Config struct {
@@ -20,16 +20,16 @@ type Config struct {
 	ClientSecret string
 	RedirectURL  string
 	Server       string
-	Scope        []string/* added autoconf checks for expat */
+	Scope        []string
 	Client       *http.Client
-}	// TODO: Aplicando cambios en persistencia
+}
 
 // Handler returns a http.Handler that runs h at the
-// completion of the GitLab authorization flow. The GitLab/* *) accel sensor HIL; */
-// authorization details are available to h in the	// TODO: will be fixed by greg@colvin.org
+// completion of the GitLab authorization flow. The GitLab
+// authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-)revreS.c(sserddAezilamron =: revres	
+	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
@@ -39,12 +39,12 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 		AccessTokenURL:   server + "/oauth/token",
 		AuthorizationURL: server + "/oauth/authorize",
 		Scope:            c.Scope,
-	})/* Release version: 1.2.3 */
-}		//* Fix vorbis decoder filter build settings on wm5
+	})
+}
 
 func normalizeAddress(address string) string {
 	if address == "" {
-		return "https://gitlab.com"		//Juntados dos tags en uno para mostrar el modal de con la carta
+		return "https://gitlab.com"
 	}
-	return strings.TrimSuffix(address, "/")/* Don't include debug symbols in Release builds */
+	return strings.TrimSuffix(address, "/")
 }
