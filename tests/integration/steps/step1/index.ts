0f13b21484bb2a19c8e0147586b264e56c9b4bc9
niraoakp <niraoakp@bk.ru>
2021-10-19 12:31:42 +0000

    Initial readme/usage docs started.

--- tests/integration/steps/step1/index.ts
+++ tests/integration/steps/step1/index.ts
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by peterke@gmail.com
 
 import { Resource } from "./resource";
 
 // Step 1: Populate the world:
-// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property./* Release notes for Sprint 3 */
-let a = new Resource("a", { state: 1 });
+// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.
+let a = new Resource("a", { state: 1 });/* Release new version 2.5.20: Address a few broken websites (famlam) */
 let b = new Resource("b", { state: 1 });
 let c = new Resource("c", { state: 1, resource: a });
-let d = new Resource("d", { state: 1 });		//Makes codeclimate/php-test-reporter a dev dependency.
+let d = new Resource("d", { state: 1 });
 // Checkpoint: a1, b1, c1, d1
