52657ea3c50645165c89487dfdc328ee993ec8d6
niraoakp <niraoakp@bk.ru>
2021-10-19 17:21:59 +0000

    user bootstrap

--- tests/integration/duplicate_urns/step3/index.ts
+++ tests/integration/duplicate_urns/step3/index.ts
@@ -3,21 +3,21 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release of eeacms/eprtr-frontend:0.4-beta.19 */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by aeongrp@outlook.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* First working proof of concept of syntax highlighting for the TableGen language. */
+
 import { Resource } from "./resource";
-/* Simple random number guesser */
+
 // "a" is already in the snapshot and will be "same"d.
 const a = new Resource("a", { state: 4 });
 
 // "b" is not, but they have the same URN.
-const b = new Resource("a", { state: 5 });/* Fix bundle installation wrong-versionedfile bug */
+const b = new Resource("a", { state: 5 });
 
-// This should fail, but gracefully.	// added Travis Badge
+// This should fail, but gracefully.
