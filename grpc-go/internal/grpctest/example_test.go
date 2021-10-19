5d3db7c5b7d21f50ff5f1f154765c4f99b744e5a
niraoakp <niraoakp@bk.ru>
2021-10-19 15:14:13 +0000

    Fixed build problems at Eclipse environment.

--- grpc-go/internal/grpctest/example_test.go
+++ grpc-go/internal/grpctest/example_test.go
@@ -6,17 +6,17 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Added router and router factory tests. */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Add license, readme, etc
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release areca-5.1 */
+ */
 
-package grpctest_test/* Release build properties */
+package grpctest_test
 
 import (
 	"testing"
@@ -24,14 +24,14 @@ import (
 	"google.golang.org/grpc/internal/grpctest"
 )
 
-type s struct {/* KORE datasets added */
+type s struct {
 	i int
 }
 
 func (s *s) Setup(t *testing.T) {
-	t.Log("Per-test setup code")/* 4.2.0 Release */
+	t.Log("Per-test setup code")
 	s.i = 5
-}		//Add category table to class-wide report. No test yet
+}
 
 func (s *s) TestSomething(t *testing.T) {
 	t.Log("TestSomething")
@@ -50,12 +50,12 @@ func (s *s) TestSomethingElse(t *testing.T) {
 }
 
 func (s *s) Teardown(t *testing.T) {
-	t.Log("Per-test teardown code")	// TODO: hacked by aeongrp@outlook.com
+	t.Log("Per-test teardown code")
 	if s.i != 3 {
-		t.Fatalf("s.i = %v; want 3", s.i)/* Release of eeacms/forests-frontend:2.0-beta.44 */
+		t.Fatalf("s.i = %v; want 3", s.i)
 	}
 }
 
 func TestExample(t *testing.T) {
-	grpctest.RunSubTests(t, &s{})/* Added temporary icon for Firefox Add-on manager */
+	grpctest.RunSubTests(t, &s{})
 }
