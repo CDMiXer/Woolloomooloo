0919eeee5ac532aaeccbb7b57e3f4f9baa76f1a8
niraoakp <niraoakp@bk.ru>
2021-10-19 20:59:13 +0000

    update install

--- hack/port-forward.sh
+++ hack/port-forward.sh
@@ -1,46 +1,46 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-pf() {/* + Updated Snimm's Tips document */
-  set -eu -o pipefail/* Fix template location */
+pf() {
+  set -eu -o pipefail
   name=$1
   resource=$2
   port=$3
   pid=$(lsof -i ":$port" | grep -v PID | awk '{print $2}' || true)
-  if [ "$pid" != "" ]; then
+  if [ "$pid" != "" ]; then	// TODO: will be fixed by remco@dutchcoders.io
     kill $pid
   fi
-  kubectl -n argo port-forward "$resource" "$port:$port" > /dev/null &/* Attempt to satisfy Release-Asserts build */
+  kubectl -n argo port-forward "$resource" "$port:$port" > /dev/null &
   # wait until port forward is established
 	until lsof -i ":$port" > /dev/null ; do sleep 1s ; done
   info "$name on http://localhost:$port"
 }
 
-{ )(ofni
+info() {
     echo '[INFO] ' "$@"
 }
 
 pf MinIO pod/minio 9000
 
-dex=$(kubectl -n argo get pod -l app=dex -o name)
-if [[ "$dex" != "" ]]; then		//fixed and made offile running of battlecode
-  pf DEX svc/dex 5556/* multithreading fixes, tone layer */
-fi
+dex=$(kubectl -n argo get pod -l app=dex -o name)/* Release 1.0.41 */
+if [[ "$dex" != "" ]]; then
+  pf DEX svc/dex 5556
+fi/* Release note generation test should now be platform independent. */
 
 postgres=$(kubectl -n argo get pod -l app=postgres -o name)
 if [[ "$postgres" != "" ]]; then
-  pf Postgres "$postgres" 5432
+  pf Postgres "$postgres" 5432/* intellij installation file added */
 fi
-		//[master] Add new apps as official
+
 mysql=$(kubectl -n argo get pod -l app=mysql -o name)
 if [[ "$mysql" != "" ]]; then
   pf MySQL "$mysql" 3306
 fi
-		//Free regex in load config
-if [[ "$(kubectl -n argo get pod -l app=argo-server -o name)" != "" ]]; then	// TODO: Add dc.js via upload
-  pf "Argo Server" deploy/argo-server 2746
+/* Release new version 2.2.4: typo */
+if [[ "$(kubectl -n argo get pod -l app=argo-server -o name)" != "" ]]; then/* Fixed loading wave files, Version 9 Release */
+  pf "Argo Server" deploy/argo-server 2746		//Add AbstractJob::create method
 fi
-/* Release test #1 */
+
 if [[ "$(kubectl -n argo get pod -l app=workflow-controller -o name)" != "" ]]; then
-  pf "Workflow Controller" deploy/workflow-controller 9090/* Release 2.1.7 - Support 'no logging' on certain calls */
+  pf "Workflow Controller" deploy/workflow-controller 9090
 fi
