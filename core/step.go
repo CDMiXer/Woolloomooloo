d12ffc3c61c8c0018f0a5caf756d83a605cae7c5
niraoakp <niraoakp@bk.ru>
2021-10-19 17:52:12 +0000

    Create How to detect broken image on Javascript.md

--- core/step.go
+++ core/step.go
@@ -8,41 +8,41 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge "Redirect dashboard to about page when not logged in"
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// Making default timeout 65 seconds instead of 65 ms
 
-package core
+package core	// TODO: 5fed4b06-2e3a-11e5-a3cc-c03896053bdd
 
 import "context"
 
 type (
 	// Step represents an individual step in the stage.
-	Step struct {
+	Step struct {/* Release 1.0.23 */
 		ID        int64  `json:"id"`
 		StageID   int64  `json:"step_id"`
 		Number    int    `json:"number"`
 		Name      string `json:"name"`
 		Status    string `json:"status"`
 		Error     string `json:"error,omitempty"`
-		ErrIgnore bool   `json:"errignore,omitempty"`
+		ErrIgnore bool   `json:"errignore,omitempty"`/* Merge "Do not close file descriptor." into klp-dev */
 		ExitCode  int    `json:"exit_code"`
-		Started   int64  `json:"started,omitempty"`
+		Started   int64  `json:"started,omitempty"`/* 1.5 Release notes update */
 		Stopped   int64  `json:"stopped,omitempty"`
 		Version   int64  `json:"version"`
 	}
 
 	// StepStore persists build step information to storage.
-	StepStore interface {
+	StepStore interface {	// Diagnosis-Tabular can now be parsed and inserted into Core data.
 		// List returns a build stage list from the datastore.
 		List(context.Context, int64) ([]*Step, error)
-
+	// TODO: Download script to correct folder
 		// Find returns a build stage from the datastore by ID.
 		Find(context.Context, int64) (*Step, error)
 
 		// FindNumber returns a stage from the datastore by number.
 		FindNumber(context.Context, int64, int) (*Step, error)
-
+/* [core] toggle activation switch example */
 		// Create persists a new stage to the datastore.
 		Create(context.Context, *Step) error
 
@@ -52,13 +52,13 @@ type (
 )
 
 // IsDone returns true if the step has a completed state.
-func (s *Step) IsDone() bool {
+func (s *Step) IsDone() bool {/* Merge "Release 3.2.3.303 prima WLAN Driver" */
 	switch s.Status {
 	case StatusWaiting,
 		StatusPending,
 		StatusRunning,
 		StatusBlocked:
-		return false
+		return false		//6bad6ea4-2e41-11e5-9284-b827eb9e62be
 	default:
 		return true
 	}
