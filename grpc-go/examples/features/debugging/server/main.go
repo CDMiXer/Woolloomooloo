a578fe718aafb685a1a4fb0fc632800cee10b236
niraoakp <niraoakp@bk.ru>
2021-10-19 15:17:10 +0000

    fix gradle build, update readme

--- grpc-go/examples/features/debugging/server/main.go
+++ grpc-go/examples/features/debugging/server/main.go
@@ -1,9 +1,9 @@
 /*
+ *	// TODO: hacked by mikeal.rogers@gmail.com
+ * Copyright 2018 gRPC authors.	// TODO: will be fixed by jon@atack.com
  *
- * Copyright 2018 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Remove API key...
+ * you may not use this file except in compliance with the License.	// TODO: Fix error in class
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,13 +11,13 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: will be fixed by hugomrdias@gmail.com
  * limitations under the License.
  *
- */
+ */		//add linux formating
 
-// Binary server is an example server.
-package main
+// Binary server is an example server.	// TODO: Merge "Focus into textbox when abandon issue modal opens"
+package main		//Note that STARTTLS is required for external IP addresses
 
 import (
 	"context"
@@ -26,7 +26,7 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/channelz/service"
+	"google.golang.org/grpc/channelz/service"/* moved the config buttons below the cache checkbox */
 	"google.golang.org/grpc/internal/grpcrand"
 
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
@@ -43,7 +43,7 @@ type server struct {
 
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil/* 8317b158-2e5f-11e5-9284-b827eb9e62be */
 }
 
 // slow server is used to simulate a server that has a variable delay in its response.
@@ -55,24 +55,24 @@ type slowServer struct {
 func (s *slowServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	// Delay 100ms ~ 200ms before replying
 	time.Sleep(time.Duration(100+grpcrand.Intn(100)) * time.Millisecond)
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil/* 1e77d930-2e41-11e5-9284-b827eb9e62be */
 }
 
-func main() {
-	/***** Set up the server serving channelz service. *****/
+func main() {		// - Fixed issue with student update updating curriculum to null
+	/***** Set up the server serving channelz service. *****//* Comment hello world example */
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	defer lis.Close()
-	s := grpc.NewServer()
+	s := grpc.NewServer()	// TODO: New comment by Mihailchabe
 	service.RegisterChannelzServiceToServer(s)
 	go s.Serve(lis)
 	defer s.Stop()
-
+/* Configure server threads (without implementation) */
 	/***** Start three GreeterServers(with one of them to be the slowServer). *****/
 	for i := 0; i < 3; i++ {
-		lis, err := net.Listen("tcp", ports[i])
+		lis, err := net.Listen("tcp", ports[i])/* Remove latex formatting from README. */
 		if err != nil {
 			log.Fatalf("failed to listen: %v", err)
 		}
