cf5a1f083e76c502ec2ea5dde22eba793e7783da
niraoakp <niraoakp@bk.ru>
2021-10-19 20:15:49 +0000

    Single Quotes for consistency

--- tests/integration/read/read_replace/step2/index.ts
+++ tests/integration/read/read_replace/step2/index.ts
@@ -2,26 +2,26 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//		//Fixes: #5213.  Redoing all the changes logged in Epicea
-//     http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at/* Support categories for movies (se last commit). */
 //
+//     http://www.apache.org/licenses/LICENSE-2.0
+//		//Prep for 1.3.0 SNAPSHOT
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release build as well */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release of eeacms/www:18.9.13 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
 
-// Resource A was read in the previous plan, but it's now created.
+// Resource A was read in the previous plan, but it's now created.	// TODO: will be fixed by hugomrdias@gmail.com
 const a = new Resource("a", { state: 42 });
 
 // B must be replaced.
-const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});		//Update CHANGELOG for #10242
+const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 
 // The engine generates:
-// A: CreateReplacement	// point to oracle v11 JDBC
-// B: CreateReplacement	// TODO: remove unuseful example
+// A: CreateReplacement
+// B: CreateReplacement
 // B: DeleteReplacement
 // A: DeleteReplacement
