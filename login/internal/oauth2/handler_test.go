38c678dabe5ee57313719cfeb14f355014903c63
niraoakp <niraoakp@bk.ru>
2021-10-19 08:21:19 +0000

    Require vidibus-api gem

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Update Window.html */
-	// e925abde-313a-11e5-9160-3c15c2e10482
+// license that can be found in the LICENSE file.
+/* Release 3.2 025.06. */
 package oauth2
