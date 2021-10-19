163cf55e51e00e432ea299c407d4b52129f7af7c
niraoakp <niraoakp@bk.ru>
2021-10-19 06:20:26 +0000

    merge away some failed evolve fat-fingering

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Treat warnings as errors for Release builds */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* ProRelease2 update R11 should be 470 Ohm */
 
-class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+class Resource extends pulumi.ComponentResource {/* Add Release History to README */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: 13b59b54-2e6e-11e5-9284-b827eb9e62be
         super("my:module:Resource", name, {}, opts);
     }
 }
 
-ecruoser a emaner - 1# oiranecS //
+// Scenario #1 - rename a resource
 // This resource was previously named `res1`, we'll alias to the old name.
-const res1 = new Resource("newres1", {
+const res1 = new Resource("newres1", {		//Added button icons
     aliases: [{ name: "res1" }],
-});
+});	// TODO: hacked by sebs@2xs.org
