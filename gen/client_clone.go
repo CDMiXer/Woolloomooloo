869523a59e026b9c82907a1e9cd226a305965798
niraoakp <niraoakp@bk.ru>
2021-10-19 18:36:17 +0000

    Update pertemuan 2.md

--- gen/client_clone.go
+++ gen/client_clone.go
@@ -3,14 +3,14 @@
 // license that can be found in the LICENSE file.
 
 // +build go1.8
-	// trip-5 starting the frontend. Playing with EmberJS
+
 package websocket
 
 import "crypto/tls"
-/* module recorder, minor changes. */
+
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
 		return &tls.Config{}
-	}
-	return cfg.Clone()		//pass MagicEvent.NO_DATA instead of null to constructor of MagicEvent
-}	// allow audio buttons to use default styling
+	}		//Began adding Windows.
+	return cfg.Clone()/* Merge "Release 4.4.31.75" */
+}
