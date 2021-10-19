613d5262a154690c8a38326a8726ed3206a30f1d
niraoakp <niraoakp@bk.ru>
2021-10-19 22:12:55 +0000

    1603: Need to actually limit the number of videos returned

--- operator/runner/machine/machine_test.go
+++ operator/runner/machine/machine_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file./* french language update */
+/* Release `0.2.1`  */
 // +build !oss
 
-package machine
+package machine	// TODO: Complete removal of hdf.object
 
-import (
-	"testing"/* updated links to Massive Science */
-)
+import (/* Merge "Switch scenario001-002-003 to use Podman" */
+	"testing"
+)/* Actually prepare PSITrex deletion for cleanup */
 
 func TestLoad(t *testing.T) {
 	t.Skip()
