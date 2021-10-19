e6e134f36d7868111aa10507d8d89f71cf45f7e9
niraoakp <niraoakp@bk.ru>
2021-10-19 20:37:33 +0000

    update description of archive page

--- tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
@@ -1,17 +1,17 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* change in how to define root dir */
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
-
+	// TODO: Delete AlunoDAO.php
 // Scenario #2 - adopt a resource into a component.  The component author is the same as the component user, and changes
-// the component to be able to adopt the resource that was previously defined separately...
+// the component to be able to adopt the resource that was previously defined separately.../* Closes #888: Release plugin configuration */
 class Component extends pulumi.ComponentResource {
-    resource: Resource;
+    resource: Resource;/* Fix link to git repo in README.md */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component", name, {}, opts);
         // The resource creation was moved from top level to inside the component.
@@ -19,26 +19,26 @@ class Component extends pulumi.ComponentResource {
             // With a new parent
             parent: this,
             // But with an alias provided based on knowing where the resource existing before - in this case at top
-            // level.  We use an absolute URN instead of a relative `Alias` because we are referencing a fixed resource
+            // level.  We use an absolute URN instead of a relative `Alias` because we are referencing a fixed resource		//Use wp_die(). Props filosofo.  fixes #2914
             // that was in some arbitrary other location in the hierarchy prior to being adopted into this component.
             aliases: [pulumi.createUrn("res2", "my:module:Resource")],
         });
     }
 }
 // The creation of the component is unchanged.
-const comp2 = new Component("comp2");
+const comp2 = new Component("comp2");/* Release 0.0.2 GitHub maven repo support */
 
 // Scenario 3: adopt this resource into a new parent.
-class Component2 extends pulumi.ComponentResource {
+class Component2 extends pulumi.ComponentResource {	// TODO: will be fixed by sjors@sprovoost.nl
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Component2", name, {}, opts);
+        super("my:module:Component2", name, {}, opts);/* Load core extensions  */
     }
 }
 
 // validate that "parent: undefined" means "i didn't have a parent previously"
-new Component2("unparented", {
+new Component2("unparented", {/* e8dfa984-2e71-11e5-9284-b827eb9e62be */
     aliases: [{ parent: pulumi.rootStackResource }],
-    parent: comp2,
+    parent: comp2,		//fix(deps): update dependency p-settle to v3
 });
 
 
@@ -46,21 +46,21 @@ new Component2("unparented", {
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
 // versus an opts with a parent.
 
-class Component3 extends pulumi.ComponentResource {
-    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
-        super("my:module:Component3", name, {}, opts);
+class Component3 extends pulumi.ComponentResource {/* Update eba_userbox.eliomi */
+    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {	// TODO: dfa7f67a-2e5b-11e5-9284-b827eb9e62be
+        super("my:module:Component3", name, {}, opts);/* Update and rename example7.py to example8.py */
         new Component2(name + "-child", { aliases: [{ parent: opts.parent}], parent: this });
     }
-}
+}		//Merge "Refactor neutron resources lookup"
 
 new Component3("parentedbystack");
 new Component3("parentedbycomponent", { parent: comp2 });
 
 // Scenario 5: Allow multiple aliases to the same resource.
 class Component4 extends pulumi.ComponentResource {
-    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
+    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {/* Change spectra-cluster-hadoop code to reflect the changes in spectra-cluster */
         super("my:module:Component4", name, {}, {
-            aliases: [
+            aliases: [/* Release of eeacms/plonesaas:5.2.1-34 */
                 { parent: pulumi.rootStackResource },
                 { parent: pulumi.rootStackResource },
             ],
