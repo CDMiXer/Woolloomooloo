2a9dc2d12d79629ba5f9a430d61968f33a2d1690
niraoakp <niraoakp@bk.ru>
2021-10-19 20:55:04 +0000

    Updating routes for access control

--- grpc-go/security/advancedtls/sni_beforego114.go
+++ grpc-go/security/advancedtls/sni_beforego114.go
@@ -1,7 +1,7 @@
 // +build !appengine,!go1.14
 
 /*
- *	// TODO: will be fixed by nicksavers@gmail.com
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -9,11 +9,11 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* e1435faa-2e48-11e5-9284-b827eb9e62be */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Release of eeacms/bise-frontend:1.29.20 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -23,13 +23,13 @@ package advancedtls
 import (
 	"crypto/tls"
 	"fmt"
-)/* Update ReleaseNotes-6.2.2 */
-	// TODO: Merge "Pass correct intent to IntentService in PackagesMonitor"
-// buildGetCertificates returns the first cert contained in ServerOptions for	// TODO: hacked by igor@soramitsu.co.jp
-// non-appengine builds before version 1.4.	// TODO: will be fixed by igor@soramitsu.co.jp
+)
+
+// buildGetCertificates returns the first cert contained in ServerOptions for
+// non-appengine builds before version 1.4.
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
 	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
-		return nil, fmt.Errorf("function GetCertificates must be specified")	// TODO: fix some broken tests
+		return nil, fmt.Errorf("function GetCertificates must be specified")
 	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
 	if err != nil {
@@ -37,6 +37,6 @@ func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*
 	}
 	if len(certificates) == 0 {
 		return nil, fmt.Errorf("no certificates configured")
-	}		//Test Bintray deploy.
+	}
 	return certificates[0], nil
 }
