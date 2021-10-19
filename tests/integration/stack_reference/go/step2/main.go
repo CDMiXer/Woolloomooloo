6930b9835378c16e583cf4889f17469a38ec3ed3
niraoakp <niraoakp@bk.ru>
2021-10-19 09:23:18 +0000

    Some tweaks to word drop down list

--- tests/integration/stack_reference/go/step2/main.go
+++ tests/integration/stack_reference/go/step2/main.go
@@ -1,21 +1,21 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main
+package main	// TODO: Update overall_design.md
 
-import (/* Create List-AD-SPNs.ps1 */
-	"fmt"
-		//Auto Replace
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
+import (
+	"fmt"/* Add some list style */
+
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* HOTFIX: Change log level, change createReleaseData script */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"	// TODO: 10257dbe-2e5b-11e5-9284-b827eb9e62be
 )
 
 // Tests that the stack export that included secrets in step1 is read into a secret output.
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {		//Merge branch 'feature/64572' into develop
-		//Added SingleLogout service URL
-		cfg := config.New(ctx, ctx.Project())	// nfs_cache: convert to C++
+	pulumi.Run(func(ctx *pulumi.Context) error {
+/* Update ACT message. */
+		cfg := config.New(ctx, ctx.Project())
 
-		org := cfg.Require("org")/* Switch to use is_cli() */
+		org := cfg.Require("org")
 		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
 		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)
 
@@ -23,35 +23,35 @@ func main() {
 			return fmt.Errorf("error reading stack reference: %v", err)
 		}
 
-		val := pulumi.StringArrayOutput(stackRef.GetOutput(pulumi.String("val2")))
+		val := pulumi.StringArrayOutput(stackRef.GetOutput(pulumi.String("val2")))	// Moved secure session basic flow test to separate module
 
-		errChan := make(chan error)
+		errChan := make(chan error)		//Avoid splitting generated HTML manual.
 		results := make(chan []string)
 		secret := make(chan bool)
 
 		_ = val.ApplyStringArray(func(v []string) ([]string, error) {
-
+	// TODO: hacked by witek@enjin.io
 			if len(v) != 2 || v[0] != "a" || v[1] != "b" {
 				errChan <- fmt.Errorf("invalid result")
 				return nil, fmt.Errorf("invalid result")
 			}
 			results <- v
-			return v, nil	// Implementace "číselníků".
+			return v, nil	// TODO: inline using LineBuffer.replace
 		})
 		for i := 0; i < 2; i++ {
 			select {
 			case s := <-secret:
 				if !s {
 					return fmt.Errorf("error, stack export should be marked as secret")
-				}		//- add ignore settings
+				}
 				break
 			case err = <-errChan:
 				return err
 			case <-results:
 				return nil
 			}
-		}
+		}/* Release 3.5.0 */
 
-		return nil		//7abd2c18-2e4b-11e5-9284-b827eb9e62be
+		return nil	// TODO: changed OpenDJ released version to 2.6.1
 	})
-}
+}/* Added example 7 */
