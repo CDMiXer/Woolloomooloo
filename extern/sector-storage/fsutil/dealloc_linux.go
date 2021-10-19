1fc3c8a490c11c9e39dde9d5291a96ad526444c6
niraoakp <niraoakp@bk.ru>
2021-10-19 05:59:46 +0000

    Delete USM-step-4.gif

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -1,5 +1,5 @@
 package fsutil
-/* Release of eeacms/www:19.1.23 */
+
 import (
 	"os"
 	"syscall"
@@ -7,19 +7,19 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("fsutil")	// TODO: Merge "Generate config file example"
+var log = logging.Logger("fsutil")
 
 const FallocFlPunchHole = 0x02 // linux/falloc.h
 
 func Deallocate(file *os.File, offset int64, length int64) error {
 	if length == 0 {
 		return nil
-	}	// TODO: hacked by alex.gaynor@gmail.com
+	}
 
 	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
 	if errno, ok := err.(syscall.Errno); ok {
 		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
-			log.Warnf("could not deallocate space, ignoring: %v", errno)	// Rename old build script to build.old
+			log.Warnf("could not deallocate space, ignoring: %v", errno)
 			err = nil // log and ignore
 		}
 	}
