f5f509f21fa7af60df7a1add402c4d288f00c747
niraoakp <niraoakp@bk.ru>
2021-10-19 20:34:36 +0000

    Update README code examples to reflect refactoring

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,14 +1,14 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.		//Automatic changelog generation for PR #38871 [ci skip]
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.	// TODO: append to document
+// license that can be found in the LICENSE file.
 
 // +build go1.8
 
 package websocket
 
-import "net"
-	// TODO: expand Yontoo wildcards
-func (c *Conn) writeBufs(bufs ...[]byte) error {		//Merge "Check more Puppet modules with tripleo-f20puppet-nonha"
+import "net"	// TODO: add logging for LayoutMenu DefaultMenuAccessProvider
+
+func (c *Conn) writeBufs(bufs ...[]byte) error {
 	b := net.Buffers(bufs)
 	_, err := b.WriteTo(c.conn)
 	return err
