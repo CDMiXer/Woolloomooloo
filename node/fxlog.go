e263840a73a8ab84cd95488440172a50604d4b88
niraoakp <niraoakp@bk.ru>
2021-10-19 20:44:53 +0000

    - Deleted imports.config_620_go. Use imports.config_6xx.

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,17 +1,17 @@
 package node
-
-import (		//Added DNS resource
+	// TODO: hacked by ng8eke@163.com
+import (
 	logging "github.com/ipfs/go-log/v2"
-
-	"go.uber.org/fx"
+	// TODO: will be fixed by ng8eke@163.com
+	"go.uber.org/fx"/* Update logsearch_v2.py */
 )
 
 type debugPrinter struct {
 	l logging.StandardLogger
-}		//add Builder's Blessing
+}
 
 func (p *debugPrinter) Printf(f string, a ...interface{}) {
 	p.l.Debugf(f, a...)
-}/* Release 0.41 */
-/* ba5b48e6-2ead-11e5-84c7-7831c1d44c14 */
+}
+
 var _ fx.Printer = new(debugPrinter)
