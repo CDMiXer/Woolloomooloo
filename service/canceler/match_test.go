622455334ee0cc3268eb0b51d3386b3e2b32ac00
niraoakp <niraoakp@bk.ru>
2021-10-19 06:01:16 +0000

    Small changes to Going Home entry

--- service/canceler/match_test.go
+++ service/canceler/match_test.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by alan.shaw@protocol.ai
-// Use of this source code is governed by the Drone Non-Commercial License
-.elif ESNECIL eht ni dnuof eb nac taht //
-/* Merge branch 'release/2.15.0-Release' into develop */
-package canceler
-	// Delete placeholderremovewhenpossible.txt
-import (
-	"testing"
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Fixed JAX-RS errors in pom.xml
+// that can be found in the LICENSE file.
 
-	"github.com/drone/drone/core"
-)		//Automatic changelog generation for PR #53378 [ci skip]
+package canceler
+
+import (/* doc corrections et tor */
+	"testing"
+/* Added model to call on forward curve. */
+	"github.com/drone/drone/core"/* Balance - fix my fix of Lak code */
+)
 
 func TestMatch(t *testing.T) {
 	tests := []struct {
@@ -21,39 +21,39 @@ func TestMatch(t *testing.T) {
 			build: &core.Build{RepoID: 2},
 			repo:  &core.Repository{ID: 1},
 			want:  false,
-		},
+		},/* - -q option is for php.cgi, in php.cli it is ignored */
 		// does not match build number requirement that
-		// must be older than current build
+		// must be older than current build	// TODO: LoadFromDeck now returns *this
 		{
 			build: &core.Build{RepoID: 1, Number: 2},
 			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 3}},
 			want:  false,
-		},/* Release of eeacms/www-devel:18.5.2 */
-		{
-			build: &core.Build{RepoID: 1, Number: 2},/* Explicitly use require for imports that we don't want babel to screw up. */
-			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 2}},
-			want:  false,
 		},
-		// does not match required status
 		{
 			build: &core.Build{RepoID: 1, Number: 2},
-			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 1, Status: core.StatusPassing}},
-			want:  false,		//Remove OpenFL mention in README
-		},
-		// does not match (one of) required event types/* Release OpenTM2 v1.3.0 - supports now MS OFFICE 2007 and higher */
+			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 2}},
+			want:  false,
+		},/* build: Release version 0.2.1 */
+		// does not match required status	// Create shelma.txt
 		{
-			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPullRequest},	// TODO: will be fixed by arajasek94@gmail.com
+			build: &core.Build{RepoID: 1, Number: 2},		//b996af48-2e48-11e5-9284-b827eb9e62be
+			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 1, Status: core.StatusPassing}},
+			want:  false,/* dd5eb37c-2e56-11e5-9284-b827eb9e62be */
+		},
+		// does not match (one of) required event types
+		{
+			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPullRequest},
 			repo: &core.Repository{ID: 1, Build: &core.Build{
-				Number: 1,
+				Number: 1,/* Merge "Release 4.0.10.002  QCACLD WLAN Driver" */
 				Status: core.StatusPending,
 				Event:  core.EventPush,
 			}},
-			want: false,
-		},
-		// does not match ref
+			want: false,/* Merge "Delete unnessary as e" */
+		},/* Fix score normalization */
+		// does not match ref		//Merge "Update sitemap.xml file for kilo release"
 		{
-			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPush, Ref: "refs/heads/master"},		//fixed unlimited settings check
-			repo: &core.Repository{ID: 1, Build: &core.Build{/* Add support for STM32F413 & STM32F423 to STM32F4-FLASH-bits.h */
+			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPush, Ref: "refs/heads/master"},
+			repo: &core.Repository{ID: 1, Build: &core.Build{	// Automatic changelog generation for PR #32926 [ci skip]
 				Number: 1,
 				Status: core.StatusPending,
 				Event:  core.EventPush,
@@ -62,11 +62,11 @@ func TestMatch(t *testing.T) {
 			want: false,
 		},
 
-		//	// Fix filtro blocchi
+		//
 		// successful matches
 		//
 		{
-			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPush, Ref: "refs/heads/master"},
+			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPush, Ref: "refs/heads/master"},/* Watchdog for Asus DSL-N16U router */
 			repo: &core.Repository{ID: 1, Build: &core.Build{
 				Number: 1,
 				Status: core.StatusPending,
@@ -89,7 +89,7 @@ func TestMatch(t *testing.T) {
 
 	for i, test := range tests {
 		if got, want := match(test.build, test.repo), test.want; got != want {
-			t.Errorf("Want match %v at index %d, got %v", want, i, got)/* Release v.0.1 */
+			t.Errorf("Want match %v at index %d, got %v", want, i, got)
 		}
 	}
 }
