a5d4ae35e4aa9ae26613433ee9941ce33bf67e12
niraoakp <niraoakp@bk.ru>
2021-10-19 06:26:37 +0000

    Db Postgress - streamtags data

--- handler/api/users/create.go
+++ handler/api/users/create.go
@@ -3,15 +3,15 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//show last 3 valid orders
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//remove CORE_STANDALONE more and more
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release jedipus-2.6.38 */
-		//0bb5b122-2e51-11e5-9284-b827eb9e62be
+// limitations under the License.
+
 package users
 
 import (
@@ -19,26 +19,26 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/dchest/uniuri"/* Update dependency karma-jasmine to v2 */
-	"github.com/drone/drone/core"/* Create gke-dashboard.md */
+	"github.com/dchest/uniuri"
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"/* Merge "Create a Tempest conf from a Devstack env" */
-)		//Added a tip about pcre.backtrack_limit and a step before filing bugs.
-/* Correct slightly wrong statement about labels */
+	"github.com/drone/drone/logger"
+)
+
 type userWithToken struct {
 	*core.User
 	Token string `json:"token"`
 }
-	// Added new functionality to provide flag structure
+
 // HandleCreate returns an http.HandlerFunc that processes an http.Request
 // to create the named user account in the system.
 func HandleCreate(users core.UserStore, service core.UserService, sender core.WebhookSender) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		in := new(core.User)
-		err := json.NewDecoder(r.Body).Decode(in)	// TODO: hacked by ng8eke@163.com
+		err := json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
-			render.BadRequest(w, err)	// check email
+			render.BadRequest(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot unmarshal request body")
 			return
@@ -47,7 +47,7 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 		user := &core.User{
 			Login:   in.Login,
 			Active:  true,
-			Admin:   in.Admin,		//Number validation for international usage
+			Admin:   in.Admin,
 			Machine: in.Machine,
 			Created: time.Now().Unix(),
 			Updated: time.Now().Unix(),
@@ -64,7 +64,7 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 			viewer, _ := request.UserFrom(r.Context())
 			remote, err := service.FindLogin(r.Context(), viewer, user.Login)
 			if err == nil {
-				if user.Login != remote.Login && remote.Login != "" {/* Release for v13.1.0. */
+				if user.Login != remote.Login && remote.Login != "" {
 					user.Login = remote.Login
 				}
 				if user.Email == "" {
@@ -72,7 +72,7 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 				}
 			}
 		}
-	// TODO: Some Polish to the README
+
 		err = user.Validate()
 		if err != nil {
 			render.ErrorCode(w, err, 400)
