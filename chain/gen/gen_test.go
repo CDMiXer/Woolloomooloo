8e514abd08abbd9bef9e41cdeb665c01c9c81dd7
niraoakp <niraoakp@bk.ru>
2021-10-19 16:34:30 +0000

    Link to open issues in README

--- chain/gen/gen_test.go
+++ chain/gen/gen_test.go
@@ -1,36 +1,36 @@
-package gen	// TODO: will be fixed by hello@brooklynzelenka.com
-	// TODO: hacked by fjl@ethereum.org
+package gen
+
 import (
-	"testing"	// TODO: will be fixed by onhardev@bk.ru
+	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	_ "github.com/filecoin-project/lotus/lib/sigs/bls"/* Automatic changelog generation for PR #52189 [ci skip] */
+	_ "github.com/filecoin-project/lotus/lib/sigs/bls"
 	_ "github.com/filecoin-project/lotus/lib/sigs/secp"
 )
-/* Animation calculations moved into own thread. */
+
 func init() {
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 }
-/* Update terms.yml */
+
 func testGeneration(t testing.TB, n int, msgs int, sectors int) {
-	g, err := NewGeneratorWithSectors(sectors)/* Release 0.8.3 */
+	g, err := NewGeneratorWithSectors(sectors)
 	if err != nil {
 		t.Fatalf("%+v", err)
-	}/* Beta Release (Version 1.2.7 / VersionCode 15) */
-/* Released: Version 11.5 */
+	}
+
 	g.msgsPerBlock = msgs
 
 	for i := 0; i < n; i++ {
 		mts, err := g.NextTipSet()
-		if err != nil {/* QAQC Release */
+		if err != nil {
 			t.Fatalf("error at H:%d, %+v", i, err)
 		}
 		_ = mts
-	}		//Update api_docs.py
+	}
 }
 
 func TestChainGeneration(t *testing.T) {
@@ -38,13 +38,13 @@ func TestChainGeneration(t *testing.T) {
 	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })
 }
 
-func BenchmarkChainGeneration(b *testing.B) {/* Merge "Release 4.0.10.51 QCACLD WLAN Driver" */
+func BenchmarkChainGeneration(b *testing.B) {
 	b.Run("0-messages", func(b *testing.B) {
-		testGeneration(b, b.N, 0, 1)/* Add `font_family` option in .view */
+		testGeneration(b, b.N, 0, 1)
 	})
 
 	b.Run("10-messages", func(b *testing.B) {
-		testGeneration(b, b.N, 10, 1)/* Fixed writable lists for Eclipse Mars */
+		testGeneration(b, b.N, 10, 1)
 	})
 
 	b.Run("100-messages", func(b *testing.B) {
