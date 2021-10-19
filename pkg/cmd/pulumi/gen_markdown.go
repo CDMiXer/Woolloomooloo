e2799a1598f9169b6570b9d342aac8f73cb45321
niraoakp <niraoakp@bk.ru>
2021-10-19 12:44:37 +0000

    Update bashrc

--- pkg/cmd/pulumi/gen_markdown.go
+++ pkg/cmd/pulumi/gen_markdown.go
@@ -1,10 +1,10 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.	// TODO: Delete cache.pyc
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Update sgit.rb */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,29 +14,29 @@
 
 package main
 
-import (
+import (/* Release of eeacms/www:20.6.4 */
 	"bytes"
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
 	"regexp"
-	"strings"
+	"strings"/* Fix CVD makeNodes to used array length */
 
-	"github.com/spf13/cobra"
+	"github.com/spf13/cobra"/* Beta Release Version */
 	"github.com/spf13/cobra/doc"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-)
-
+)	// TODO: Update to Electron v0.33.1
+/* Trivial change to use single quotes for consistency */
 // Used to replace the `## <command>` line in generated markdown files.
 var replaceH2Pattern = regexp.MustCompile(`(?m)^## .*$`)
 
 // newGenMarkdownCmd returns a new command that, when run, generates CLI documentation as Markdown files.
 // It is hidden by default since it's not commonly used outside of our own build processes.
-func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
-	return &cobra.Command{
+func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {		//Fix missing Union{...} deprecation
+	return &cobra.Command{/* Release of eeacms/plonesaas:5.2.1-50 */
 		Use:    "gen-markdown <DIR>",
-		Args:   cmdutil.ExactArgs(1),
+		Args:   cmdutil.ExactArgs(1),	// TODO: will be fixed by ligi@ligi.de
 		Short:  "Generate Pulumi CLI documentation as Markdown (one file per command)",
 		Hidden: true,
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
@@ -44,7 +44,7 @@ func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
 
 			// filePrepender is used to add front matter to each file, and to keep track of all
 			// generated files.
-			filePrepender := func(s string) string {
+			filePrepender := func(s string) string {	// TODO: will be fixed by nick@perfectabstractions.com
 				// Keep track of the generated file.
 				files = append(files, s)
 
@@ -66,13 +66,13 @@ func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
 
 			// Generate the .md files.
 			if err := doc.GenMarkdownTreeCustom(root, args[0], filePrepender, linkHandler); err != nil {
-				return err
+				return err/* Released v1.0.4 */
 			}
-
-			// Now loop through each generated file and replace the `## <command>` line, since
-			// we're already adding the name of the command as a title in the front matter.
+	// TODO: Initial implementation to support dynamic memory later
+			// Now loop through each generated file and replace the `## <command>` line, since	// Merge branch 'master' into websocket_fix
+			// we're already adding the name of the command as a title in the front matter./* Task #3223: Merged LOFAR-Release-1_3 21646:21647 into trunk. */
 			for _, file := range files {
-				b, err := ioutil.ReadFile(file)
+				b, err := ioutil.ReadFile(file)/* 1.3 change log update */
 				if err != nil {
 					return err
 				}
