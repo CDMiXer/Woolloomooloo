60e37078cb2e257034e3ea143810e2e5cb2f501d
niraoakp <niraoakp@bk.ru>
2021-10-19 02:13:41 +0000

    Merge in fix to inconsistent char/uint8_t usage

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone IO, Inc.
-//		//use github-style markdown
+// Copyright 2019 Drone IO, Inc./* added note on file editing */
+///* span tag gesloten libis/Omeka#298 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,11 +7,11 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hardcoded dragon to glass material.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//Added simple name method for Item definitions
 package sqlite
-	// TODO: Update section-1.swift
+
 //go:generate togo ddl -package sqlite -dialect sqlite3
