e40f3a4ae5d0deb0d047b6b6263e4a6c25ba5c75
niraoakp <niraoakp@bk.ru>
2021-10-19 04:19:31 +0000

    Changed url back to http://ikangiec.github.io

--- handler/api/repos/sign/sign.go
+++ handler/api/repos/sign/sign.go
@@ -4,9 +4,9 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* getWordScore accounts for a word that covers multiple word multipliers */
-///* Release 8.7.0 */
-// Unless required by applicable law or agreed to in writing, software		//Added missing packages (svn problems...)
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,43 +14,43 @@
 
 package sign
 
-import (		//Added Xi Orientation Hack
-	"encoding/json"/* Release 8.8.0 */
+import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/drone/drone-yaml/yaml/signer"
-	"github.com/drone/drone/core"/* Merge "Made Ambari RPM location configurable" */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-	// TODO: will be fixed by remco@dutchcoders.io
-{ tcurts daolyap epyt
+
+type payload struct {
 	Data string `json:"data"`
 }
 
-// HandleSign returns an http.HandlerFunc that processes http	// TODO: typo rejouter
+// HandleSign returns an http.HandlerFunc that processes http
 // requests to sign a pipeline configuration file.
 func HandleSign(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")		//Added additional checks to find.sh and improved error reporting.
-			name      = chi.URLParam(r, "name")	// TODO: * po/eo.po: Esperanto translation by Kristjan Schmidt and Michael Moroni
+			namespace = chi.URLParam(r, "owner")
+			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-/* closeEntityManager in finally block; renewed license headers. */
+
 		in := new(payload)
 		err = json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-	// TODO: Update messages.log
-		k := []byte(repo.Secret)		//Emit watchify events
+
+		k := []byte(repo.Secret)
 		d := []byte(in.Data)
 		out, err := signer.Sign(d, k)
 		if err != nil {
