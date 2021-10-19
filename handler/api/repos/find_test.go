874f7477ce2b8f1c96a57da8a206088408ba8c33
niraoakp <niraoakp@bk.ru>
2021-10-19 13:31:39 +0000

    We don't use Apache

--- handler/api/repos/find_test.go
+++ handler/api/repos/find_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Ajout license
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -6,17 +6,17 @@ package repos
 
 import (
 	"context"
-	"encoding/json"		//d432219c-2e3f-11e5-9284-b827eb9e62be
+	"encoding/json"
 	"io/ioutil"
-	"net/http/httptest"/* stopwatch: optimize MakeStopwatchName() */
-	"testing"
+	"net/http/httptest"
+	"testing"/* Release of version 0.1.1 */
 
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/core"
-	"github.com/sirupsen/logrus"/* [FEATURE] Add basic support for media output via MRCPSynth on Asterisk */
+	"github.com/sirupsen/logrus"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"		//patchbomb: use a list instead of indented paragraphs
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -25,16 +25,16 @@ func init() {
 }
 
 var (
-	mockRepo = &core.Repository{		//Ability to export a simple view
-		ID:        1,		//Rename images/Slider/2.jpg to images/slider/2.jpg
+	mockRepo = &core.Repository{
+		ID:        1,	// 09c51472-2e4f-11e5-9284-b827eb9e62be
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
-		Counter:   42,	// TODO: Delete EVAL.md
+		Counter:   42,
 		Branch:    "master",
 	}
-		//Update HtmlStringUtilities.cs
-	mockRepos = []*core.Repository{/* Update Release Date. */
+
+	mockRepos = []*core.Repository{
 		{
 			ID:        1,
 			Namespace: "octocat",
@@ -42,7 +42,7 @@ var (
 			Slug:      "octocat/hello-world",
 		},
 		{
-			ID:        1,
+			ID:        1,/* testing the jenkins github hook trigger */
 			Namespace: "octocat",
 			Name:      "spoon-knife",
 			Slug:      "octocat/spoon-knife",
@@ -51,26 +51,26 @@ var (
 )
 
 func TestFind(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Merge "Release 1.0.0.220 QCACLD WLAN Driver" */
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)
 	r = r.WithContext(request.WithRepo(
-		context.Background(), mockRepo,	// TODO: Create liz-date.html
+		context.Background(), mockRepo,
 	))
-		//Update titanic_test.py
+
 	router := chi.NewRouter()
-	router.Get("/api/repos/{owner}/{name}", HandleFind())	// Updating build-info/dotnet/corefx/master for preview.19103.1
-	router.ServeHTTP(w, r)
+	router.Get("/api/repos/{owner}/{name}", HandleFind())
+	router.ServeHTTP(w, r)/* Added sk to messages command */
 
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
+		t.Errorf("Want response code %d, got %d", want, got)/* (jam) Release 2.0.3 */
+	}/* deploys WARs & some housekeeping */
 
 	got, want := new(core.Repository), mockRepo
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Release new version 2.4.25:  */
 		t.Errorf(diff)
-	}/* Fix a memcached inconsistency */
-}
+	}
+}		//Updated 5link-external.md
