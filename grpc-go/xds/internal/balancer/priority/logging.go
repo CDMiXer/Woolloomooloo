4d66b02be9f646de2820e124bef066268fe5e6b4
niraoakp <niraoakp@bk.ru>
2021-10-19 23:40:10 +0000

    Added url to badge

--- grpc-go/xds/internal/balancer/priority/logging.go
+++ grpc-go/xds/internal/balancer/priority/logging.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2021 gRPC authors.	// TODO: Updating to tasklist
+ * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,27 +8,27 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by julia@jvns.ca
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Release: Making ready for next release iteration 6.0.1 */
+
 package priority
 
 import (
 	"fmt"
 
 	"google.golang.org/grpc/grpclog"
-	internalgrpclog "google.golang.org/grpc/internal/grpclog"	// fix apt headers
+	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
-const prefix = "[priority-lb %p] "	// TODO: will be fixed by antao2002@gmail.com
-		//prototypes/translate.py, demo/cgi-bin: add namespace demo script
+const prefix = "[priority-lb %p] "
+
 var logger = grpclog.Component("xds")
-	// TODO: Create activity_example.xml
-func prefixLogger(p *priorityBalancer) *internalgrpclog.PrefixLogger {	// TODO: Merge "Add re-tries to Nailgun client"
+
+func prefixLogger(p *priorityBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
