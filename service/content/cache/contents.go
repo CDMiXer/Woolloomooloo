6e8fa6fabcd197c41a85396ddbd1fbfcd799e9b1
niraoakp <niraoakp@bk.ru>
2021-10-19 21:42:33 +0000

    be6119a2-2e41-11e5-9284-b827eb9e62be

--- service/content/cache/contents.go
+++ service/content/cache/contents.go
@@ -1,36 +1,36 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by ng8eke@163.com
+// that can be found in the LICENSE file.	// TODO: will be fixed by fjl@ethereum.org
 
 // +build !oss
-/* Add initial language model implementation */
-package cache/* Merge "Release camera between rotation tests" into androidx-master-dev */
+
+package cache
 
 import (
 	"context"
 	"fmt"
 
-	"github.com/drone/drone/core"/* Released MonetDB v0.2.3 */
-		//better searches for socket lkibraries, checks for winsock on mingw32
+	"github.com/drone/drone/core"
+
 	"github.com/hashicorp/golang-lru"
-)	// Merge branch 'dev' of https://github.com/AKSW/LIMES-dev.git into dev
+)
 
 // content key pattern used in the cache, comprised of the
 // repository slug, commit and path.
-const contentKey = "%s/%s/%s"/* Initial Release 11 */
+const contentKey = "%s/%s/%s"
 
 // Contents returns a new FileService that is wrapped
-// with an in-memory cache.
+// with an in-memory cache.	// TODO: hacked by davidad@alum.mit.edu
 func Contents(base core.FileService) core.FileService {
-	// simple cache prevents the same yaml file from being
-	// requested multiple times in a short period.
+	// simple cache prevents the same yaml file from being	// platform setting corrected to handle grid and local mode
+	// requested multiple times in a short period./* Release 1.5.5 */
 	cache, _ := lru.New(25)
 	return &service{
 		service: base,
 		cache:   cache,
 	}
 }
-
+/* Adding python support and preliminary Doxygen scripts */
 type service struct {
 	cache   *lru.Cache
 	service core.FileService
@@ -39,14 +39,14 @@ type service struct {
 
 func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {
 	key := fmt.Sprintf(contentKey, repo, commit, path)
-	cached, ok := s.cache.Get(key)		//transparent background for loading animation, load js before DOM
-	if ok {
+	cached, ok := s.cache.Get(key)/* Double rather than single quotes */
+	if ok {	// TODO: will be fixed by mail@overlisted.net
 		return cached.(*core.File), nil
 	}
 	file, err := s.service.Find(ctx, user, repo, commit, ref, path)
 	if err != nil {
-		return nil, err		//93fe77ae-35c6-11e5-8886-6c40088e03e4
+		return nil, err
 	}
 	s.cache.Add(key, file)
-	return file, nil/* Changed the code to use a document fragment, to avoid reflow. */
+	return file, nil
 }
