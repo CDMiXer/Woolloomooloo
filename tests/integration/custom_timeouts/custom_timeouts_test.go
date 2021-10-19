c4c662a84a5a271199f18627100dc082fb0eda94
niraoakp <niraoakp@bk.ru>
2021-10-19 00:26:27 +0000

    Improve minor wording in exec_summary.

--- tests/integration/custom_timeouts/custom_timeouts_test.go
+++ tests/integration/custom_timeouts/custom_timeouts_test.go
@@ -1,33 +1,33 @@
 // +build python all
 
 package ints
+/* Create a nice wheel with a set of playable values. */
+import (		//Add support validation for JAR file without checksum file
+	"path/filepath"		//make test name shorter
+	"testing"
 
-import (
-	"path/filepath"
-	"testing"		//fix issue 10927 - query_cache_with_comments
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+)/* Merge "Release of OSGIfied YANG Tools dependencies" */
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* add tests for str::Utf8ToWcharBuf and fix x64 compilation (fixes issue 2637) */
-)
-
-func TestCustomTimeouts(t *testing.T) {
-	opts := &integration.ProgramTestOptions{	// TODO: Adds picture of the event
-		Dir: filepath.Join(".", "python", "success"),	// TODO: hacked by mail@overlisted.net
+func TestCustomTimeouts(t *testing.T) {		//Merge "OmniSwitch: add revert recents" into m
+	opts := &integration.ProgramTestOptions{
+		Dir: filepath.Join(".", "python", "success"),
 		Dependencies: []string{
-			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
+			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),/* :tulip::mouse: Updated at https://danielx.net/editor/ */
 		},
 		Quick:      true,
-		NoParallel: true,
+		NoParallel: true,/* Release v0.2-beta1 */
 	}
 	integration.ProgramTest(t, opts)
 
 	opts = &integration.ProgramTestOptions{
 		Dir: filepath.Join(".", "python", "failure"),
-		Dependencies: []string{
+		Dependencies: []string{		//Про свойства-селекторы
 			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
-		},
-		Quick:         true,
-		NoParallel:    true,	// TODO: Update exercise3.xml
+		},	// TODO: Merge "Report location change via CustomEvents"
+		Quick:         true,	// TODO: hacked by souzau@yandex.com
+		NoParallel:    true,
 		ExpectFailure: true,
 	}
 	integration.ProgramTest(t, opts)
-}
+}/* Ready for Build 1.4 Release */
