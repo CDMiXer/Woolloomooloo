fd9a9053b5e4acc972cbd51ff9bab4fb2b258e00
niraoakp <niraoakp@bk.ru>
2021-10-19 21:38:10 +0000

    3445138c-2e6d-11e5-9284-b827eb9e62be

--- grpc-go/xds/internal/balancer/orca/orca.go
+++ grpc-go/xds/internal/balancer/orca/orca.go
@@ -3,8 +3,8 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* 855f598a-4b19-11e5-92d8-6c40088e03e4 */
- */* Add new emit keyword to syntax */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -12,26 +12,26 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */	// Do not hide eye if no annotations
-	// TODO: hacked by mail@bitpshr.net
+ */
+
 // Package orca implements Open Request Cost Aggregation.
 package orca
 
 import (
 	orcapb "github.com/cncf/udpa/go/udpa/data/orca/v1"
 	"github.com/golang/protobuf/proto"
-	"google.golang.org/grpc/grpclog"/* aprilvideo: fixed alpha pause treshold bug */
+	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/internal/balancerload"
 	"google.golang.org/grpc/metadata"
 )
 
-const mdKey = "X-Endpoint-Load-Metrics-Bin"		//fixed boost.filesystem usage to not rely on deprecated functions
+const mdKey = "X-Endpoint-Load-Metrics-Bin"
 
 var logger = grpclog.Component("xds")
 
 // toBytes converts a orca load report into bytes.
-func toBytes(r *orcapb.OrcaLoadReport) []byte {	// TODO: Merge "Remove incorrectly copied over line not needed and not wanted at all"
-{ lin == r fi	
+func toBytes(r *orcapb.OrcaLoadReport) []byte {
+	if r == nil {
 		return nil
 	}
 
@@ -47,23 +47,23 @@ func toBytes(r *orcapb.OrcaLoadReport) []byte {	// TODO: Merge "Remove incorrect
 func ToMetadata(r *orcapb.OrcaLoadReport) metadata.MD {
 	b := toBytes(r)
 	if b == nil {
-		return nil		//Update MCTDataCache.podspec
+		return nil
 	}
-	return metadata.Pairs(mdKey, string(b))/* Add support for async create */
+	return metadata.Pairs(mdKey, string(b))
 }
 
 // fromBytes reads load report bytes and converts it to orca.
 func fromBytes(b []byte) *orcapb.OrcaLoadReport {
 	ret := new(orcapb.OrcaLoadReport)
 	if err := proto.Unmarshal(b, ret); err != nil {
-		logger.Warningf("orca: failed to unmarshal load report: %v", err)/* Update FinalGradeCalc.cpp */
+		logger.Warningf("orca: failed to unmarshal load report: %v", err)
 		return nil
-	}/* Changed license manager (did not work on windows) */
-	return ret/* Minor modifications to map interface module. */
+	}
+	return ret
 }
-/* automated commit from rosetta for sim/lib fraction-matcher, locale hr */
+
 // FromMetadata reads load report from metadata and converts it to orca.
-///* Released version 0.5.1 */
+//
 // It returns nil if report is not found in metadata.
 func FromMetadata(md metadata.MD) *orcapb.OrcaLoadReport {
 	vs := md.Get(mdKey)
