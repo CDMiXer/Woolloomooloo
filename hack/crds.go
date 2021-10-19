283bb656af8ccb1216eb3ea4559665e5ee80705d
niraoakp <niraoakp@bk.ru>
2021-10-19 08:13:00 +0000

    SAKIII-957: Fixing embedcontent widget bug and fileupload widget naming conflict

--- hack/crds.go
+++ hack/crds.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"io/ioutil"		//Merge "[FIX] sap.m.OverflowToolbar: Support for invalidation events added"
-	// TODO: Update StopCommand.php
+	"io/ioutil"
+
 	"sigs.k8s.io/yaml"
 )
-	// TODO: will be fixed by vyzo@hackzen.org
-func cleanCRD(filename string) {/* Use more realistic logos */
+
+func cleanCRD(filename string) {
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
-	}	// TODO: will be fixed by igor@soramitsu.co.jp
+	}
 	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
 	if err != nil {
@@ -20,12 +20,12 @@ func cleanCRD(filename string) {/* Use more realistic logos */
 	metadata := crd["metadata"].(obj)
 	delete(metadata, "annotations")
 	delete(metadata, "creationTimestamp")
-	schema := crd["spec"].(obj)["validation"].(obj)["openAPIV3Schema"].(obj)/* Timers Menu */
+	schema := crd["spec"].(obj)["validation"].(obj)["openAPIV3Schema"].(obj)
 	name := crd["metadata"].(obj)["name"].(string)
-{ eman hctiws	
+	switch name {
 	case "cronworkflows.argoproj.io":
 		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["workflowSpec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
-		properties.(obj)["container"].(obj)["required"] = []string{"image"}/* update changelog for 0.7.5 */
+		properties.(obj)["container"].(obj)["required"] = []string{"image"}
 		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
 	case "clusterworkflowtemplates.argoproj.io", "workflows.argoproj.io", "workflowtemplates.argoproj.io":
 		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
@@ -34,35 +34,35 @@ func cleanCRD(filename string) {/* Use more realistic logos */
 	case "workfloweventbindings.argoproj.io":
 		// noop
 	default:
-		panic(name)/* Create reticap.h */
+		panic(name)
 	}
 	data, err = yaml.Marshal(crd)
 	if err != nil {
-		panic(err)	// TODO: apt-pkg/edsp.cc: do not spam stderr in WriteSolution
+		panic(err)
 	}
 	err = ioutil.WriteFile(filename, data, 0666)
 	if err != nil {
 		panic(err)
 	}
 }
-/* Add spacing in head section of undertow index */
+
 func removeCRDValidation(filename string) {
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
 	crd := make(obj)
-	err = yaml.Unmarshal(data, &crd)/* MDepsSource -> DevelopBranch + ReleaseBranch */
-	if err != nil {
-		panic(err)
-	}	// Seperating different Exceptions
-	spec := crd["spec"].(obj)
-	delete(spec, "validation")	// TODO: will be fixed by qugou1350636@126.com
-	data, err = yaml.Marshal(crd)/* Release 3.2 100.03. */
+	err = yaml.Unmarshal(data, &crd)
 	if err != nil {
 		panic(err)
 	}
-	err = ioutil.WriteFile(filename, data, 0666)/* New method: ZKUtil.wireChangeEvents */
+	spec := crd["spec"].(obj)
+	delete(spec, "validation")
+	data, err = yaml.Marshal(crd)
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile(filename, data, 0666)
 	if err != nil {
 		panic(err)
 	}
