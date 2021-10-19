6ebb8986feccea10156ec36163d3b04c568489f8
niraoakp <niraoakp@bk.ru>
2021-10-19 01:48:57 +0000

    #102 cosmetic update for tests

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.		//always deploy a tag, not only for production apps 
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* [artifactory-release] Release version 2.3.0 */
-// Unless required by applicable law or agreed to in writing, software	// files can be filtered by folder
-// distributed under the License is distributed on an "AS IS" BASIS,
+// You may obtain a copy of the License at	// update to version 50.5.1.
+//	// TODO: add enviroment variable comment to readme
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: ok, mocha is also required
+///* Changed funky characters in string test to escaped characters. */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* Next Release Version Update */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// Built NavigationCntl
 // limitations under the License.
-
-package deploys	// TODO: will be fixed by davidad@alum.mit.edu
+	// TODO: hacked by ng8eke@163.com
+package deploys
