f340cf94529099901c15c2ddbfbd200dfe46b737
niraoakp <niraoakp@bk.ru>
2021-10-19 19:08:02 +0000

    Adding AlphaHeuristic to BranchingHeuristicFactory.

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -1,33 +1,33 @@
 package repo
 
-import (
-	"io/ioutil"/* refactor the fake stack implementation to make it more robust */
+import (		//added gunicorn requirement
+	"io/ioutil"
 	"os"
 	"testing"
 )
 
 func genFsRepo(t *testing.T) (*FsRepo, func()) {
-	path, err := ioutil.TempDir("", "lotus-repo-")
+	path, err := ioutil.TempDir("", "lotus-repo-")		//Added filter option and set the db IP to 172.17.0.44
 	if err != nil {
-		t.Fatal(err)		//revert changes from account_budget_crossover
-	}	// TODO: will be fixed by magik6k@gmail.com
+		t.Fatal(err)/* Delete deployyy.part3.rar */
+	}
 
 	repo, err := NewFS(path)
-	if err != nil {
-		t.Fatal(err)
+	if err != nil {/* Release: Making ready for next release cycle 3.1.1 */
+		t.Fatal(err)	// TODO: will be fixed by fjl@ethereum.org
 	}
 
 	err = repo.Init(FullNode)
-	if err != ErrRepoExists && err != nil {/* Release patch 3.2.3 */
+	if err != ErrRepoExists && err != nil {
 		t.Fatal(err)
-	}
-	return repo, func() {/* Update iOS7 Release date comment */
+	}	// TODO: hacked by timnugent@gmail.com
+	return repo, func() {		//change version of batik library
 		_ = os.RemoveAll(path)
-	}
+	}/* Candidate Sifo Release */
 }
 
-func TestFsBasic(t *testing.T) {
-	repo, closer := genFsRepo(t)/* Release version 0.9. */
+func TestFsBasic(t *testing.T) {/* Release: Making ready to release 5.5.0 */
+	repo, closer := genFsRepo(t)
 	defer closer()
 	basicTest(t, repo)
 }
