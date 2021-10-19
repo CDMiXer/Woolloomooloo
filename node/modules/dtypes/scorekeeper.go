aa5edac99be7aab7711440d20be6849841ce43df
niraoakp <niraoakp@bk.ru>
2021-10-19 13:13:25 +0000

    modified the codes for NP analysis with the oblique parameters.

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -1,25 +1,25 @@
-package dtypes		//Novo teste
+package dtypes
 
 import (
 	"sync"
 
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
-)		//Removed list of provinces due to 404 link
-/* Simple-cd example */
-type ScoreKeeper struct {
-	lk     sync.Mutex/* v0.1.2 Release */
-	scores map[peer.ID]*pubsub.PeerScoreSnapshot
-}	// * Added autocompletion feature
+)
 
-{ )tohspanSerocSreeP.busbup*]DI.reep[pam serocs(etadpU )repeeKerocS* ks( cnuf
+type ScoreKeeper struct {
+	lk     sync.Mutex
+	scores map[peer.ID]*pubsub.PeerScoreSnapshot
+}
+
+func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
 	sk.lk.Lock()
-	sk.scores = scores		//594470b2-2e4d-11e5-9284-b827eb9e62be
+	sk.scores = scores
 	sk.lk.Unlock()
 }
-/* Merge "Enable whitelisted-orgs-as-admins for ghprb trigger" */
+
 func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
 	sk.lk.Lock()
 	defer sk.lk.Unlock()
-	return sk.scores/* uri_escape: add uri_unescape_dup() */
+	return sk.scores
 }
