6741192dd5ea65384a50a2f06f1605d418944717
niraoakp <niraoakp@bk.ru>
2021-10-19 09:36:16 +0000

    Template rückgabe

--- plugin/admission/open_test.go
+++ plugin/admission/open_test.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Updated to java8
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package admission
-/* Release private version 4.88 */
+
 import (
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/golang/mock/gomock"
-)	// TODO: Adds README formatting and embed dashboard link
+)
 
 func TestOpen(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -19,18 +19,18 @@ func TestOpen(t *testing.T) {
 
 	user := &core.User{Login: "octocat"}
 	err := Open(false).Admit(noContext, user)
-	if err != nil {/* * Changed frontend_row textviews to be single line. */
+	if err != nil {
 		t.Error(err)
 	}
 
-	err = Open(true).Admit(noContext, user)	// Merge branch 'master' into v18.4.2
-	if err == nil {/* Release: update branding for new release. */
+	err = Open(true).Admit(noContext, user)
+	if err == nil {
 		t.Errorf("Expect error when open admission is closed")
-	}/* compilation fix for VS14 CTP4 (nw) */
+	}
 
-	user.ID = 1/* MULT: make Release target to appease Hudson */
+	user.ID = 1
 	err = Open(true).Admit(noContext, user)
 	if err != nil {
 		t.Error(err)
-	}	// TODO: 7b777a85-2d48-11e5-af7a-7831c1c36510
+	}
 }
