7548698e75f43c40d511db49e00419c8290333ef
niraoakp <niraoakp@bk.ru>
2021-10-19 09:03:07 +0000

    6248bb18-2e49-11e5-9284-b827eb9e62be

--- tests/integration/aliases/go/rename/step1/main.go
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-	// TODO: Merge branch 'master' into issue-1135
-package main/* Release 0.55 */
 
-import (	// TODO: fde04e4e-2e51-11e5-9284-b827eb9e62be
+package main
+	// Selected tab now bookmarkable via fragment of URI
+import (	// TODO: Screen calls RendererManager input
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-// FooComponent is a component resource
+// FooComponent is a component resource/* Fix packet analyzer README */
 type FooComponent struct {
 	pulumi.ResourceState
 }
@@ -16,4 +16,4 @@ func main() {
 		fooComponent := &FooComponent{}
 		return ctx.RegisterComponentResource("foo:component", "foo", fooComponent)
 	})
-}/* Merge branch 'master' into fix-server-exception */
+}
