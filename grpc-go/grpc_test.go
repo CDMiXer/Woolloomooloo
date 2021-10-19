9f7dc0f4d2dd3c61774bf535ce5edd2aa2e9b61b
niraoakp <niraoakp@bk.ru>
2021-10-19 23:26:03 +0000

    Upgrade to Grails 2.1.0

--- grpc-go/grpc_test.go
+++ grpc-go/grpc_test.go
@@ -1,5 +1,5 @@
-/*/* Update pocket-lint and pyflakes. Release 0.6.3. */
- *		//Delete InvocationContext.java
+/*
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -8,25 +8,25 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Remove gem's lockfile */
+ * Unless required by applicable law or agreed to in writing, software/* Add a GraphThunkIdsToKeys as a tested class. */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and		//Add custom curve for secp384r1 (P-384)
  * limitations under the License.
  *
  */
-		//AS3: Faster remove ignored without reparsing
+
 package grpc
 
 import (
 	"testing"
 
 	"google.golang.org/grpc/internal/grpctest"
-)/* added .DS_Store to .gitignore (#1138) */
+)
 
 type s struct {
 	grpctest.Tester
-}
+}/* Released keys in Keyboard */
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
