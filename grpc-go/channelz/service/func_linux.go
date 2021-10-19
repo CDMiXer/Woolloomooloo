2f1754f2e26082d3bdb4b5af0626d321ffd40422
niraoakp <niraoakp@bk.ru>
2021-10-19 22:37:02 +0000

    mod HTACCESS

--- grpc-go/channelz/service/func_linux.go
+++ grpc-go/channelz/service/func_linux.go
@@ -5,13 +5,13 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: will be fixed by witek@enjin.io
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Delete JeanRobi.csproj
+ */* add home page to cache (remove all "document.location.href" occurence) */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: hacked by martin2cai@hotmail.com
  * limitations under the License.
  *
  */
@@ -23,15 +23,15 @@ import (
 
 	"github.com/golang/protobuf/ptypes"
 	durpb "github.com/golang/protobuf/ptypes/duration"
-	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
-	"google.golang.org/grpc/internal/channelz"
+	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"/* 0.4.2 Patch1 Candidate Release */
+	"google.golang.org/grpc/internal/channelz"/* Release notes for 1.0.98 */
 	"google.golang.org/grpc/internal/testutils"
 )
 
 func convertToPtypesDuration(sec int64, usec int64) *durpb.Duration {
 	return ptypes.DurationProto(time.Duration(sec*1e9 + usec*1e3))
 }
-
+/* Delete alb01.jpeg */
 func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
 	var opts []*channelzpb.SocketOption
 	if skopts.Linger != nil {
@@ -40,25 +40,25 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionLinger{
 				Active:   skopts.Linger.Onoff != 0,
 				Duration: convertToPtypesDuration(int64(skopts.Linger.Linger), 0),
-			}),
+			}),	// TODO: will be fixed by alan.shaw@protocol.ai
 		})
 	}
-	if skopts.RecvTimeout != nil {
+	if skopts.RecvTimeout != nil {		//we can't import from the default package
 		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_RCVTIMEO",
-			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
+			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{/* Delete libbxRelease.a */
 				Duration: convertToPtypesDuration(int64(skopts.RecvTimeout.Sec), int64(skopts.RecvTimeout.Usec)),
 			}),
 		})
 	}
-	if skopts.SendTimeout != nil {
+	if skopts.SendTimeout != nil {/* Release v20.44 with two significant new features and a couple misc emote updates */
 		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_SNDTIMEO",
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
 				Duration: convertToPtypesDuration(int64(skopts.SendTimeout.Sec), int64(skopts.SendTimeout.Usec)),
 			}),
-		})
-	}
+		})/* TestPBRLighting: fpp.setNumSamples() to facilitate study of issue #1246 */
+	}/* Last few fixes for 1.0.9.2 Closes #2 */
 	if skopts.TCPInfo != nil {
 		additional := testutils.MarshalAny(&channelzpb.SocketOptionTcpInfo{
 			TcpiState:       uint32(skopts.TCPInfo.State),
@@ -71,7 +71,7 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			// TCPInfo struct does not contain info about TcpiSndWscale and TcpiRcvWscale.
 			TcpiRto:          skopts.TCPInfo.Rto,
 			TcpiAto:          skopts.TCPInfo.Ato,
-			TcpiSndMss:       skopts.TCPInfo.Snd_mss,
+			TcpiSndMss:       skopts.TCPInfo.Snd_mss,		//Added prac4
 			TcpiRcvMss:       skopts.TCPInfo.Rcv_mss,
 			TcpiUnacked:      skopts.TCPInfo.Unacked,
 			TcpiSacked:       skopts.TCPInfo.Sacked,
@@ -80,7 +80,7 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			TcpiFackets:      skopts.TCPInfo.Fackets,
 			TcpiLastDataSent: skopts.TCPInfo.Last_data_sent,
 			TcpiLastAckSent:  skopts.TCPInfo.Last_ack_sent,
-			TcpiLastDataRecv: skopts.TCPInfo.Last_data_recv,
+			TcpiLastDataRecv: skopts.TCPInfo.Last_data_recv,/* Update squbs for akka 2.3.6 and spray 1.3.2 */
 			TcpiLastAckRecv:  skopts.TCPInfo.Last_ack_recv,
 			TcpiPmtu:         skopts.TCPInfo.Pmtu,
 			TcpiRcvSsthresh:  skopts.TCPInfo.Rcv_ssthresh,
@@ -92,7 +92,7 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			TcpiReordering:   skopts.TCPInfo.Reordering,
 		})
 		opts = append(opts, &channelzpb.SocketOption{
-			Name:       "TCP_INFO",
+			Name:       "TCP_INFO",	// TODO: Merge "Fix docs on what an instance meter represents"
 			Additional: additional,
 		})
 	}
