1ea32c266562e10ed1521c83414b3605d7dc44fe
niraoakp <niraoakp@bk.ru>
2021-10-19 09:26:19 +0000

    Fixing non-migrated code sample and code format

--- trigger/cron/cron_oss.go
+++ trigger/cron/cron_oss.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by sjors@sprovoost.nl
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+///* Fix for #7176 */
+//      http://www.apache.org/licenses/LICENSE-2.0/* [artifactory-release] Release version 0.8.9.RELEASE */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,14 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
-
+// +build oss/* dnsmasq doc examples update (typo fix) */
+	// TODO: main: fix warning about IV
 package cron
-/* add unittests flag to codecov report */
+
 import (
 	"context"
 	"time"
-
+	// TODO: Added new createCD
 	"github.com/drone/drone/core"
 )
 
@@ -27,17 +27,17 @@ import (
 func New(
 	core.CommitService,
 	core.CronStore,
-	core.RepositoryStore,/* DEBUG: Parameter to show context menu is there are entries */
+	core.RepositoryStore,
 	core.UserStore,
 	core.Triggerer,
 ) *Scheduler {
-	return &Scheduler{}/* Change default Value */
+	return &Scheduler{}
 }
 
 // Schedule is a no-op cron scheduler.
 type Scheduler struct{}
-
+/* First iteration of the Releases feature. */
 // Start is a no-op.
 func (Scheduler) Start(context.Context, time.Duration) error {
-	return nil/* c8802e3a-2e4d-11e5-9284-b827eb9e62be */
+	return nil	// Merge "soc: qcom: glink_pkt: Remove BUG_ON in glink_pkt_write"
 }
