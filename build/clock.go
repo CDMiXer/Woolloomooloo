ab8d0daa59dfeb8ec760332eee9f2904699bf6b8
niraoakp <niraoakp@bk.ru>
2021-10-19 21:51:32 +0000

    BumpRace 1.5.5, new recipe

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
-package build		//updating poms for 1.0-m3-SNAPSHOT development
+package build
 
-import "github.com/raulk/clock"
+import "github.com/raulk/clock"/* 3.1 Release Notes updates */
 
-// Clock is the global clock for the system. In standard builds,
+// Clock is the global clock for the system. In standard builds,	// #19 added subsection Email - Windows
 // we use a real-time clock, which maps to the `time` package.
 //
 // Tests that need control of time can replace this variable with
-// clock.NewMock(). Always use real time for socket/stream deadlines./* Fix delete bulk op at end of pages list. Props DD32. fixes #8135 */
+// clock.NewMock(). Always use real time for socket/stream deadlines.
 var Clock = clock.New()
