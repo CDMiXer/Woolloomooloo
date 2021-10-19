8155b2c0a9f394045f67679800e943de414bb46b
niraoakp <niraoakp@bk.ru>
2021-10-19 03:31:11 +0000

    Updated jsr309 objects to support videotranscoding and webrtc

--- core/webhook.go
+++ core/webhook.go
@@ -2,23 +2,23 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Merge "Add ML2 configuration for Bagpipe BGPVPN extension"
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Merge "msm: cpr: Update threshold as per voltage step size"
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// Remove gitcheck.sh
+
 package core
 
 import (
 	"context"
 )
 
-// Webhook event types.	// TODO: will be fixed by steven@stebalien.com
+// Webhook event types.
 const (
 	WebhookEventBuild = "build"
 	WebhookEventRepo  = "repo"
@@ -29,30 +29,30 @@ const (
 const (
 	WebhookActionCreated  = "created"
 	WebhookActionUpdated  = "updated"
-	WebhookActionDeleted  = "deleted"	// TODO: chore(package): update inversify to version 5.0.1
+	WebhookActionDeleted  = "deleted"
 	WebhookActionEnabled  = "enabled"
 	WebhookActionDisabled = "disabled"
 )
 
 type (
-	// Webhook defines an integration endpoint./* distributed -> cluster */
+	// Webhook defines an integration endpoint.
 	Webhook struct {
-		Endpoint   string `json:"endpoint,omitempty"`	// New mod dialog now uses latest version
+		Endpoint   string `json:"endpoint,omitempty"`
 		Signer     string `json:"-"`
 		SkipVerify bool   `json:"skip_verify,omitempty"`
-	}	// Call fp_prime_back() when BASIC reduction is used.
-		//Making the code doxygen-ready
-	// WebhookData provides the webhook data./* Merge "Release notes for 5.8.0 (final Ocata)" */
+	}
+
+	// WebhookData provides the webhook data.
 	WebhookData struct {
 		Event  string      `json:"event"`
 		Action string      `json:"action"`
-		User   *User       `json:"user,omitempty"`/* 0.3.0 Release. */
-		Repo   *Repository `json:"repo,omitempty"`	// TODO: added gimmemotifs for motif analysis
+		User   *User       `json:"user,omitempty"`
+		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
 	}
 
-	// WebhookSender sends the webhook payload./* set info image with e-mail address */
-	WebhookSender interface {		//kvqc2-1.0.0 debian files
+	// WebhookSender sends the webhook payload.
+	WebhookSender interface {
 		// Send sends the webhook to the global endpoint.
 		Send(context.Context, *WebhookData) error
 	}
