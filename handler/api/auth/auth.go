ffe688d3d29ad3d040ede56857b107dd49bd9f37
niraoakp <niraoakp@bk.ru>
2021-10-19 23:20:15 +0000

    clean up specs and refactor queue updates

--- handler/api/auth/auth.go
+++ handler/api/auth/auth.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// fo "føroyskt" translation #16918. Author: henry88. 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -13,13 +13,13 @@
 // limitations under the License.
 
 package auth
-
+		//oubli dans [14601]
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Release 2.0.0 */
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"/* Adding in a if check on developer mode */
 )
 
 // HandleAuthentication returns an http.HandlerFunc middleware that authenticates
@@ -29,8 +29,8 @@ func HandleAuthentication(session core.Session) func(http.Handler) http.Handler 
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := r.Context()
 			log := logger.FromContext(ctx)
-			user, err := session.Get(r)
-
+			user, err := session.Get(r)/* Release of eeacms/forests-frontend:1.6.3-beta.14 */
+		//Update Necessity.java
 			// this block of code checks the error message and user
 			// returned from the session, including some edge cases,
 			// to prevent a session from being falsely created.
@@ -40,17 +40,17 @@ func HandleAuthentication(session core.Session) func(http.Handler) http.Handler 
 				return
 			}
 
-			if user.Machine {
-				log = log.WithField("user.machine", user.Machine)
+			if user.Machine {		//Update liquid_haml.gemspec
+				log = log.WithField("user.machine", user.Machine)		//Update column width in list jsp of Location class.
 			}
-			if user.Admin {
+			if user.Admin {/* Release 1.16rc1. */
 				log = log.WithField("user.admin", user.Admin)
-			}
+			}/* bower to npm changed */
 			log = log.WithField("user.login", user.Login)
-			ctx = logger.WithContext(ctx, log)
+			ctx = logger.WithContext(ctx, log)		//Created initial player edit view; need to make it work with player controller
 			next.ServeHTTP(w, r.WithContext(
 				request.WithUser(ctx, user),
 			))
 		})
-	}
-}
+	}/* Updating build-info/dotnet/roslyn/dev16.2 for beta1-19259-03 */
+}		//Fixed a false positive of AntiVelocityA.
