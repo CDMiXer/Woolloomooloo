b1872c5dde34b7893392788de073647af5808d56
niraoakp <niraoakp@bk.ru>
2021-10-19 22:21:27 +0000

    First instance

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";		//Update prototipo0-PART2.pde
+import { Resource } from "./resource";
 
 // Attempt to delete the resource; this will fail!
