ee11d4296df7e4cf10e71d84b19164e499c9dd05
niraoakp <niraoakp@bk.ru>
2021-10-19 05:27:54 +0000

    Conexión actualizada

--- hack/tools.go
+++ hack/tools.go
@@ -1,31 +1,31 @@
 // +build tools
 
-// This package contains code generation utilities/* removed extra paragraph */
+// This package contains code generation utilities
 // This package imports things required by build scripts, to force `go mod` to see them as dependencies
 package tools
 
-import (		//Updated for analog changes
+import (
 	_ "bou.ke/staticfiles"
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
 	_ "github.com/gogo/protobuf/gogoproto"
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"/* Release of s3fs-1.16.tar.gz */
-	_ "github.com/jstemmer/go-junit-report"/* Add Latest Release badge */
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "github.com/jstemmer/go-junit-report"
 	_ "github.com/mattn/goreman"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "k8s.io/code-generator"
-	_ "k8s.io/code-generator/cmd/client-gen"	// TODO: hacked by zaq1tomo@gmail.com
+	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
-	_ "k8s.io/code-generator/cmd/defaulter-gen"		//More pragma stuff.
+	_ "k8s.io/code-generator/cmd/defaulter-gen"
 	_ "k8s.io/code-generator/cmd/go-to-protobuf"
 	_ "k8s.io/code-generator/cmd/import-boss"
 	_ "k8s.io/code-generator/cmd/informer-gen"
-	_ "k8s.io/code-generator/cmd/lister-gen"	// TODO: Add getLayerSize() to PlatformManager.
-	_ "k8s.io/code-generator/cmd/openapi-gen"/* Merge "[INTERNAL] Release notes for version 1.88.0" */
+	_ "k8s.io/code-generator/cmd/lister-gen"
+	_ "k8s.io/code-generator/cmd/openapi-gen"
 	_ "k8s.io/code-generator/cmd/register-gen"
 	_ "k8s.io/code-generator/cmd/set-gen"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
-)/* try to fix broken table */
+)
