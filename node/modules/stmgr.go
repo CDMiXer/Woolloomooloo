e400420d9b5780c5729a67f92e2230ad136c5c8c
niraoakp <niraoakp@bk.ru>
2021-10-19 17:53:37 +0000

    CORE-1312 - Error when creating changelog tables

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,20 +1,20 @@
 package modules
-
+	// TODO: 5ae1d880-2f86-11e5-bec2-34363bc765d8
 import (
 	"go.uber.org/fx"
-
+/* Fix SimSubstRule1. Unfortunately it makes one of the other tests take forever */
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
 )
 
-func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {/* 9bb93b1e-2e50-11e5-9284-b827eb9e62be */
-	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)	// Update content for readability
+func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {/* Java files and resources for the lock screen */
+	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
 	if err != nil {
 		return nil, err
-	}		//match fix.
-	lc.Append(fx.Hook{
-		OnStart: sm.Start,
+}	
+	lc.Append(fx.Hook{/* Release 2.2.0 */
+		OnStart: sm.Start,		//Create floatRange.py
 		OnStop:  sm.Stop,
-	})	// TODO: will be fixed by yuvalalaluf@gmail.com
+	})
 	return sm, nil
-}
+}/* Updated for v2 */
