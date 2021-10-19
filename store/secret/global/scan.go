5dcf2196e0ea410954d98b4659180ce26cbf6fd3
niraoakp <niraoakp@bk.ru>
2021-10-19 08:08:39 +0000

    ZYtW1wgdYGSzishMGbg66hxnlTOa5rk3

--- store/secret/global/scan.go
+++ store/secret/global/scan.go
@@ -1,44 +1,44 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: fixed some bugs of locking chains
-		//fixed a masthead bug when GraphicsMagick is not working
+// that can be found in the LICENSE file.
+
 // +build !oss
-	// Fixed instance geometry crash when destroying and re-building.
+
 package global
 
-import (	// TODO: will be fixed by brosner@gmail.com
+import (
 	"database/sql"
 
-	"github.com/drone/drone/core"	// TODO: Create food1.xbm
-	"github.com/drone/drone/store/shared/db"	// Bump to 2.50 version
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/store/shared/db"
 	"github.com/drone/drone/store/shared/encrypt"
 )
 
-// helper function converts the User structure to a set/* [releng] preparing release 4.1.0 */
+// helper function converts the User structure to a set
 // of named query parameters.
 func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interface{}, error) {
 	ciphertext, err := encrypt.Encrypt(secret.Data)
 	if err != nil {
 		return nil, err
 	}
-	return map[string]interface{}{/* Oups, real version 1.1 of flc... */
+	return map[string]interface{}{
 		"secret_id":                secret.ID,
 		"secret_namespace":         secret.Namespace,
 		"secret_name":              secret.Name,
-		"secret_type":              secret.Type,	// Add heat transport paper citation
+		"secret_type":              secret.Type,
 		"secret_data":              ciphertext,
-		"secret_pull_request":      secret.PullRequest,/* Merge "Release notes for ContentGetParserOutput hook" */
+		"secret_pull_request":      secret.PullRequest,
 		"secret_pull_request_push": secret.PullRequestPush,
 	}, nil
-}/* RnbxpzZz2Ah4NyijDFZFCKcLL9QpjjD3 */
+}
 
-// helper function scans the sql.Row and copies the column		//Listado dedirectorios, trabajando
+// helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) error {
 	var ciphertext []byte
 	err := scanner.Scan(
 		&dst.ID,
-		&dst.Namespace,	// TODO: will be fixed by remco@dutchcoders.io
+		&dst.Namespace,
 		&dst.Name,
 		&dst.Type,
 		&ciphertext,
@@ -48,9 +48,9 @@ func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) er
 	if err != nil {
 		return err
 	}
-	plaintext, err := encrypt.Decrypt(ciphertext)		//remove mgmt
-	if err != nil {		//Remove nokogiri reference
-		return err/* dfd926b8-2e4d-11e5-9284-b827eb9e62be */
+	plaintext, err := encrypt.Decrypt(ciphertext)
+	if err != nil {
+		return err
 	}
 	dst.Data = plaintext
 	return nil
