0635171fb917a9935de7843c3e21665412eb4279
niraoakp <niraoakp@bk.ru>
2021-10-19 12:49:00 +0000

    ISSN added

--- handler/api/secrets/delete_test.go
+++ handler/api/secrets/delete_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Null default options */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -6,17 +6,17 @@
 
 package secrets
 
-import (
+( tropmi
 	"context"
-	"encoding/json"
+	"encoding/json"/* Release Scelight 6.3.0 */
 	"net/http"
 	"net/http/httptest"
-	"testing"
+	"testing"/* a92eb5a8-2e5a-11e5-9284-b827eb9e62be */
+		//[JS] bug fix
+	"github.com/drone/drone/handler/api/errors"/* ignore TAGS file */
+	"github.com/drone/drone/mock"/* Add a comment on how to build Release with GC support */
 
-	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
-
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"	// TODO: #19 creating instances only if are used
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
@@ -27,37 +27,37 @@ func TestHandleDelete(t *testing.T) {
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
-	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(nil)
+	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(nil)	// Completed refactoring of Ideal into its own independent module.
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
-	c.URLParams.Add("name", "github_password")
+	c.URLParams.Add("name", "github_password")	// TODO: hacked by martin2cai@hotmail.com
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
+	r := httptest.NewRequest("GET", "/", nil)	// TODO: will be fixed by arachnid@notdot.net
+	r = r.WithContext(/* Release 0.0.4 */
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleDelete(secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNoContent; want != got {
+	if got, want := w.Code, http.StatusNoContent; want != got {		//Missing initialization of StripChart format and layout
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
 
 func TestHandleDelete_SecretNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)		//Made elapsed time more robust, not NTP sensitive
 	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(nil, errors.ErrNotFound)
-
+/* Sticky header example */
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 	c.URLParams.Add("name", "github_password")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)/* Tasks on the page on load */
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
