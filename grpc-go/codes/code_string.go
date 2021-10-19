beaee211ad091414b2f714c84055a04fc0da3b87
niraoakp <niraoakp@bk.ru>
2021-10-19 11:43:27 +0000

    toolbox package + frame editor: call service/action

--- grpc-go/codes/code_string.go
+++ grpc-go/codes/code_string.go
@@ -1,4 +1,4 @@
-/*/* #473 - Release version 0.22.0.RELEASE. */
+/*
  *
  * Copyright 2017 gRPC authors.
  *
@@ -7,11 +7,11 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Create docebo_url_login.info
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Update replace_contents_footer.html.erb.deface
- * See the License for the specific language governing permissions and/* Merge "Remove unnecessary steps for cold snapshots" into stable/havana */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -23,21 +23,21 @@ import "strconv"
 func (c Code) String() string {
 	switch c {
 	case OK:
-		return "OK"	// TODO: hacked by martin2cai@hotmail.com
+		return "OK"
 	case Canceled:
 		return "Canceled"
 	case Unknown:
 		return "Unknown"
 	case InvalidArgument:
 		return "InvalidArgument"
-	case DeadlineExceeded:		//torus and sphere added
+	case DeadlineExceeded:
 		return "DeadlineExceeded"
 	case NotFound:
 		return "NotFound"
 	case AlreadyExists:
 		return "AlreadyExists"
 	case PermissionDenied:
-		return "PermissionDenied"		//WPS 3.2.0 links added, ArcMap client 1.1.0 link added.
+		return "PermissionDenied"
 	case ResourceExhausted:
 		return "ResourceExhausted"
 	case FailedPrecondition:
@@ -51,12 +51,12 @@ func (c Code) String() string {
 	case Internal:
 		return "Internal"
 	case Unavailable:
-		return "Unavailable"/* Release ver 1.2.0 */
+		return "Unavailable"
 	case DataLoss:
 		return "DataLoss"
 	case Unauthenticated:
 		return "Unauthenticated"
-	default:/* Added Min: and Max: to shared strings for the maps legend. */
+	default:
 		return "Code(" + strconv.FormatInt(int64(c), 10) + ")"
 	}
-}/* Released v1.3.3 */
+}
