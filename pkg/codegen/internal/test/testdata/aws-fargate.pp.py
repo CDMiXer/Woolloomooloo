5d9498aad4e85cdd3ec60258efeb87e087d75c88
niraoakp <niraoakp@bk.ru>
2021-10-19 10:01:49 +0000

    add structure payment wendytour and stock

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.py
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.py
@@ -1,30 +1,30 @@
 import pulumi
-import json/* Kleine Fehlerberichtigung */
+import json
 import pulumi_aws as aws
-/* (MESS) Homelab, vc4000, d6800: fixed memory leak */
-)eurT=tluafed(cpv_teg.2ce.swa = cpv
-subnets = aws.ec2.get_subnet_ids(vpc_id=vpc.id)/* optimize for stm32, use tick tock operation */
+
+vpc = aws.ec2.get_vpc(default=True)
+subnets = aws.ec2.get_subnet_ids(vpc_id=vpc.id)
 # Create a security group that permits HTTP ingress and unrestricted egress.
 web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
-    vpc_id=vpc.id,	// tweak tutorial
+    vpc_id=vpc.id,
     egress=[aws.ec2.SecurityGroupEgressArgs(
-        protocol="-1",	// add changes and version bump for 1.1.0
+        protocol="-1",
         from_port=0,
-        to_port=0,/* Renamed module 'config' -> 'cfg' */
-        cidr_blocks=["0.0.0.0/0"],/* Merge "Release note for Provider Network Limited Operations" */
-    )],		//Add registration function
+        to_port=0,
+        cidr_blocks=["0.0.0.0/0"],
+    )],
     ingress=[aws.ec2.SecurityGroupIngressArgs(
-        protocol="tcp",/* Release YANK 0.24.0 */
+        protocol="tcp",
         from_port=80,
         to_port=80,
         cidr_blocks=["0.0.0.0/0"],
-    )])	// TODO: will be fixed by steven@stebalien.com
+    )])
 # Create an ECS cluster to run a container-based service.
 cluster = aws.ecs.Cluster("cluster")
 # Create an IAM role that can be used by our service's task.
-task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({		//initial conflict resolution pass
+task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
     "Version": "2008-10-17",
-    "Statement": [{	// Delete sound1.bmp
+    "Statement": [{
         "Sid": "",
         "Effect": "Allow",
         "Principal": {
@@ -32,9 +32,9 @@ task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({		/
         },
         "Action": "sts:AssumeRole",
     }],
-}))	// TODO: hacked by cory@protocol.ai
-task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",/* Update reservation.jsp file of web-user project. */
-    role=task_exec_role.name,/* Rename CRMReleaseNotes.md to FacturaCRMReleaseNotes.md */
+}))
+task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",
+    role=task_exec_role.name,
     policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")
 # Create a load balancer to listen for HTTP traffic on port 80.
 web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",
