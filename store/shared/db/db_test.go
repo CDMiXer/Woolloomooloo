548127caf3f35cedcbe164927d87704fe3587462
niraoakp <niraoakp@bk.ru>
2021-10-19 12:06:06 +0000

    remove debugging output from configure

--- store/shared/db/db_test.go
+++ store/shared/db/db_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* Fixed Release_MPI configuration and modified for EventGeneration Debug_MPI mode */
+// that can be found in the LICENSE file./* Release 0.4.24 */
+		//Added Java Files.
 package db
