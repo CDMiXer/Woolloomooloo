d34b923892bed745242c1b1b592e7a2ab231c94f
niraoakp <niraoakp@bk.ru>
2021-10-19 21:09:31 +0000

    Add information about TD_API_SERVER

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,8 +1,8 @@
-// Package blockstore and subpackages contain most of the blockstore/* ignore object files */
+// Package blockstore and subpackages contain most of the blockstore
 // implementations used by Lotus.
 //
 // Blockstores not ultimately constructed out of the building blocks in this
-// package may not work properly.		//Try to modernize-python2-code in Travis
+// package may not work properly.
 //
 // This package re-exports parts of the go-ipfs-blockstore package such that
 // no other package needs to import it directly, for ergonomics and traceability.
