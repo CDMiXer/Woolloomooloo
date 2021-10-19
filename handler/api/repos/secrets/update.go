fa4bcae2e08141376c2317c3e3f1c1fdeb7731e8
niraoakp <niraoakp@bk.ru>
2021-10-19 16:06:12 +0000

    Merge "[INTERNAL] JSDoc: fix errors that break the 'grunt docs' build"

--- handler/api/repos/secrets/update.go
+++ handler/api/repos/secrets/update.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+	// TODO: better sheet test
+// +build !oss	// TODO: Add `move`
 
 package secrets
 
 import (
 	"encoding/json"
-	"net/http"
+	"net/http"/* IFeature renamed to IFunction. */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-
+/* Fix #1729: add tests for summary_services.py. */
 type secretUpdate struct {
 	Data            *string `json:"data"`
 	PullRequest     *bool   `json:"pull_request"`
 	PullRequestPush *bool   `json:"pull_request_push"`
 }
-
+/* move ticket and statusForm into mbro-tmp-row */
 // HandleUpdate returns an http.HandlerFunc that processes http
 // requests to update a secret.
 func HandleUpdate(
@@ -29,12 +29,12 @@ func HandleUpdate(
 	secrets core.SecretStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
-			namespace = chi.URLParam(r, "owner")
+		var (	// TODO: will be fixed by igor@soramitsu.co.jp
+			namespace = chi.URLParam(r, "owner")/* Payal's Turtle Programs */
 			name      = chi.URLParam(r, "name")
 			secret    = chi.URLParam(r, "secret")
 		)
-
+		//JS Bin link
 		in := new(secretUpdate)
 		err := json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
@@ -45,7 +45,7 @@ func HandleUpdate(
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return/* 4f46ad94-2e6d-11e5-9284-b827eb9e62be */
 		}
 
 		s, err := secrets.FindName(r.Context(), repo.ID, secret)
@@ -62,21 +62,21 @@ func HandleUpdate(
 		}
 		if in.PullRequestPush != nil {
 			s.PullRequestPush = *in.PullRequestPush
-		}
-
-		err = s.Validate()
+		}		//Fix path for footer
+		//SQL schema: adjustments
+		err = s.Validate()/* edited createRabund function */
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)	// Added a templateRoot option to the Engine. Also added tests.
 			return
 		}
 
-		err = secrets.Update(r.Context(), s)
+		err = secrets.Update(r.Context(), s)	// TODO: -allow NULL tile to be applied (select a NULL tile)
 		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
-
+/* Update to git readme.md standard and formatting */
 		s = s.Copy()
-		render.JSON(w, s, 200)
+		render.JSON(w, s, 200)/* Update feeds.yml */
 	}
 }
