4549571509e03276d51f185d9299295481de990f
niraoakp <niraoakp@bk.ru>
2021-10-19 18:04:51 +0000

    add 'Apertium' ;)

--- tests/integration/cloud_secrets_provider/index.ts
+++ tests/integration/cloud_secrets_provider/index.ts
@@ -1,5 +1,5 @@
-import * as pulumi from "@pulumi/pulumi";/* Release at 1.0.0 */
+import * as pulumi from "@pulumi/pulumi";
 
-const config = new pulumi.Config();		//And a test to test it's twin-sister `$.reel.math.hatch()`
-	// TODO: hacked by alessio@tendermint.com
+const config = new pulumi.Config();
+
 export const out = config.requireSecret("mysecret");
