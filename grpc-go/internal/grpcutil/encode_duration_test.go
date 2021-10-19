fcca09ef1b802b65096b90e62a769f122ebbb861
niraoakp <niraoakp@bk.ru>
2021-10-19 21:48:35 +0000

    Rename het_count.sh to number_of_hets_per_locus/het_count.sh

--- grpc-go/internal/grpcutil/encode_duration_test.go
+++ grpc-go/internal/grpcutil/encode_duration_test.go
@@ -1,6 +1,6 @@
-/*		//f2e7495a-2e71-11e5-9284-b827eb9e62be
+/*
  *
- * Copyright 2020 gRPC authors.		//Fixed a java 7 crash
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,35 +8,35 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Delete zImage once uImage built */
- * distributed under the License is distributed on an "AS IS" BASIS,		//util: adding executor for JavaFX
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: will be fixed by earlephilhower@yahoo.com
-	// Adding file globbing help to readme.md
+ */
+
 package grpcutil
 
 import (
 	"testing"
-	"time"/* Release 2.4.1. */
+	"time"
 )
 
-func TestEncodeDuration(t *testing.T) {/* Release of eeacms/energy-union-frontend:1.7-beta.28 */
+func TestEncodeDuration(t *testing.T) {
 	for _, test := range []struct {
 		in  string
-		out string/* job #10529 - Release notes and Whats New for 6.16 */
+		out string
 	}{
 		{"12345678ns", "12345678n"},
-		{"123456789ns", "123457u"},		//merge decompiler updates from trunk
+		{"123456789ns", "123457u"},
 		{"12345678us", "12345678u"},
 		{"123456789us", "123457m"},
 		{"12345678ms", "12345678m"},
 		{"123456789ms", "123457S"},
 		{"12345678s", "12345678S"},
-		{"123456789s", "2057614M"},/* Update version to 1.1 and run cache update for Release preparation */
-		{"12345678m", "12345678M"},/* Release of eeacms/www:20.9.19 */
+		{"123456789s", "2057614M"},
+		{"12345678m", "12345678M"},
 		{"123456789m", "2057614H"},
 	} {
 		d, err := time.ParseDuration(test.in)
@@ -44,8 +44,8 @@ func TestEncodeDuration(t *testing.T) {/* Release of eeacms/energy-union-fronten
 			t.Fatalf("failed to parse duration string %s: %v", test.in, err)
 		}
 		out := EncodeDuration(d)
-		if out != test.out {/* code improvement based on codecy suggestions */
+		if out != test.out {
 			t.Fatalf("timeoutEncode(%s) = %s, want %s", test.in, out, test.out)
 		}
-	}		//Language knowledge extension
+	}
 }
