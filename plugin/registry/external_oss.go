cc6d527c8cb62d26ed7e4f513deda524d07b1518
niraoakp <niraoakp@bk.ru>
2021-10-19 01:00:19 +0000

    Update syntax-highlight.html

--- plugin/registry/external_oss.go
+++ plugin/registry/external_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by why@ipfs.io
-///* Release for 2.9.0 */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by vyzo@hackzen.org
+// You may obtain a copy of the License at	// TODO: Merge "Add cache=swift.cache for authtoken example."
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Removed the encyclo page, it's a bit special
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by igor@soramitsu.co.jp
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release MailFlute-0.5.0 */
 // limitations under the License.
 
 // +build oss
 
-package registry/* add Python - Get Version - Universal.bes */
+package registry
 
 import "github.com/drone/drone/core"
 
-// External returns a no-op registry credential provider.	// Construct CEN urn
+// External returns a no-op registry credential provider.
 func External(string, string, bool) core.RegistryService {
-	return new(noop)/* added small wood gain per forage */
+	return new(noop)
 }
