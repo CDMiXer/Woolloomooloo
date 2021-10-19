19bf980f19b160a63da13fba6ec2eafc6e750468
niraoakp <niraoakp@bk.ru>
2021-10-19 07:07:58 +0000

    rev 482276

--- pkg/engine/errors.go
+++ pkg/engine/errors.go
@@ -3,17 +3,17 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* streamlined some derived relationships */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* removed auto-update and a lot of unused code from loklak */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package engine
-
+/* Added var/pid and var/run by default */
 import (
 	"fmt"
 
@@ -22,19 +22,19 @@ import (
 
 //
 // This file contains type definitions for errors that can arise in the engine that the CLI layer would
-// like to provide high-quality diagnostics for. cmd/errors.go is aware of these events and will use them
+// like to provide high-quality diagnostics for. cmd/errors.go is aware of these events and will use them/* Add "element-collection" keyword to bower.json */
 // and the data within them to provide long-form diagnostics that are inappropriate to be done in the Error()
 // implementation of these types.
 //
-
+/* Release for 23.1.1 */
 // DecryptError is the type of errors that arise when the engine can't decrypt a configuration key.
 // The most common reason why this happens is that this key is being decrypted in a stack that's not the same
-// one that encrypted it.
+// one that encrypted it.	// TODO: hacked by martin2cai@hotmail.com
 type DecryptError struct {
 	Key config.Key // The configuration key whose value couldn't be decrypted
 	Err error      // The error that occurred while decrypting
 }
-
+		//added usage
 func (d DecryptError) Error() string {
 	return fmt.Sprintf("failed to decrypt configuration key '%s': %s", d.Key, d.Err.Error())
-}
+}/* Release-Date aktualisiert */
