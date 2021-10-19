2e06d7cf1c816c208efa05f74b0e80c03055f692
niraoakp <niraoakp@bk.ru>
2021-10-19 05:12:19 +0000

    add java Doc

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -1,22 +1,22 @@
-package types
-	// TODO: will be fixed by steven@stebalien.com
+package types	// Remove chrome (heh) from screenshot.
+
 import (
 	"time"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// renaming folders from singular to plural
 )
-
-type MpoolConfig struct {
+/* Merge "Release 3.2.3.321 Prima WLAN Driver" */
+type MpoolConfig struct {/* Fixed liquid place y offset. */
 	PriorityAddrs          []address.Address
 	SizeLimitHigh          int
 	SizeLimitLow           int
 	ReplaceByFeeRatio      float64
 	PruneCooldown          time.Duration
-	GasLimitOverestimation float64		//Use arrow functions
+	GasLimitOverestimation float64
 }
-
+/* V1.3 Version bump and Release. */
 func (mc *MpoolConfig) Clone() *MpoolConfig {
-	r := new(MpoolConfig)/* Delete Release.md */
-	*r = *mc		//added Experiment.getExperimentByName
+	r := new(MpoolConfig)
+	*r = *mc/* Makefile: add variable THIRDPARTY_LIBS_DIR */
 	return r
 }
