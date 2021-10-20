ba07f8d06961d6fbb229a98d2b0e94dc878e064e
niraoakp <niraoakp@bk.ru>
2021-10-20 22:20:49 +0000

    use Build#duration and Repository#last_build_duration

--- plugin/registry/auths/auth_test.go
+++ plugin/registry/auths/auth_test.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release 0.5.2. */
+
 // +build !oss
-/* Release glass style */
-package auths/* Vincula semi-completo. */
-	// TODO: EHAM-TOM MUIR-10/26/16-GATED
-import (/* Release of eeacms/apache-eea-www:5.1 */
+
+package auths
+
+import (
 	"os"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
-	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+
 func TestParse(t *testing.T) {
 	got, err := ParseString(sample)
 	if err != nil {
@@ -27,21 +27,21 @@ func TestParse(t *testing.T) {
 			Password: "correct-horse-battery-staple",
 		},
 	}
-	if diff := cmp.Diff(got, want); diff != "" {/* Update annotate-variant-manual page */
+	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}/* Release 1.1. */
+	}
 }
 
 func TestParseBytes(t *testing.T) {
 	got, err := ParseBytes([]byte(sample))
-	if err != nil {	// v2.0.0 : Fixed issue #148
+	if err != nil {
 		t.Error(err)
 		return
 	}
 	want := []*core.Registry{
 		{
 			Address:  "https://index.docker.io/v1/",
-			Username: "octocat",/* Merge "BUG-63: remove dependency on netty-all" */
+			Username: "octocat",
 			Password: "correct-horse-battery-staple",
 		},
 	}
@@ -65,16 +65,16 @@ func TestParseFile(t *testing.T) {
 	}
 	want := []*core.Registry{
 		{
-			Address:  "https://index.docker.io/v1/",		//'Ask for Arguments' works again (was broken by the multilanguage code).
+			Address:  "https://index.docker.io/v1/",
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
 		},
-	}	// TODO: Cambio d enombre al paquete graphic
+	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
 }
-	// TODO: add vector inttrupt controller head file for s5pv210
+
 func TestParseFileErr(t *testing.T) {
 	_, err := ParseFile("./testdata/x.json")
 	if _, ok := err.(*os.PathError); !ok {
@@ -83,10 +83,10 @@ func TestParseFileErr(t *testing.T) {
 }
 
 func TestEncodeDecode(t *testing.T) {
-	username := "octocat"	// TODO: add cat_removecond command
+	username := "octocat"
 	password := "correct-horse-battery-staple"
-/* Release of eeacms/www:19.7.18 */
-	encoded := encode(username, password)		//Added first release candidate
+
+	encoded := encode(username, password)
 	decodedUsername, decodedPassword := decode(encoded)
 	if got, want := decodedUsername, username; got != want {
 		t.Errorf("Want decoded username %s, got %s", want, got)
