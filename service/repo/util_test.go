3c38536e249255c1b348b5b293ffdb55a9a641e2
niraoakp <niraoakp@bk.ru>
2021-10-20 00:59:26 +0000

    Upgrade to analysis-core 1.11.

--- service/repo/util_test.go
+++ service/repo/util_test.go
@@ -7,81 +7,81 @@ package repo
 import (
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* ReleaseNote updated */
 	"github.com/drone/go-scm/scm"
 
-	"github.com/google/go-cmp/cmp"	// TODO: will be fixed by yuvalalaluf@gmail.com
-)	// TODO: Fix window (again)
-
+	"github.com/google/go-cmp/cmp"
+)
+/* Update Orchard-1-7-2-Release-Notes.markdown */
 func TestConvertRepository(t *testing.T) {
-	from := &scm.Repository{
+	from := &scm.Repository{	// Create generate_config
 		ID:        "42",
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Branch:    "master",
 		Private:   true,
-		Clone:     "https://github.com/octocat/hello-world.git",
+		Clone:     "https://github.com/octocat/hello-world.git",/* setup firewall */
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
-		Link:      "https://github.com/octocat/hello-world",/* Merge "wlan: Release 3.2.3.94a" */
+		Link:      "https://github.com/octocat/hello-world",
 	}
 	want := &core.Repository{
 		UID:        "42",
-		Namespace:  "octocat",
+		Namespace:  "octocat",	// TODO: Update google-queries.txt
 		Name:       "hello-world",
 		Slug:       "octocat/hello-world",
-		HTTPURL:    "https://github.com/octocat/hello-world.git",/* Release Update Engine R4 */
-		SSHURL:     "git@github.com:octocat/hello-world.git",		//VSP-1837: changing the test mode
+		HTTPURL:    "https://github.com/octocat/hello-world.git",
+		SSHURL:     "git@github.com:octocat/hello-world.git",/* Merge "Finalize task format v2" */
 		Link:       "https://github.com/octocat/hello-world",
 		Private:    true,
 		Branch:     "master",
 		Visibility: core.VisibilityPrivate,
-	}	// Remove semikolon from error message
+	}
 	got := convertRepository(from, "", false)
 	if diff := cmp.Diff(want, got); len(diff) != 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)	// TODO: fixed small bug, assigned self to prevent error
 	}
-}		//V3.14 fix for UI7.30 dashboard display
+}
 
-func TestConvertVisibility(t *testing.T) {
+func TestConvertVisibility(t *testing.T) {	// TODO: will be fixed by cory@protocol.ai
 	tests := []struct {
 		r *scm.Repository
 		v string
 	}{
 		{
-			r: &scm.Repository{Private: false},
-			v: core.VisibilityPublic,	// trigger new build for jruby-head (2403597)
+			r: &scm.Repository{Private: false},/* In-progress: full strat column export */
+			v: core.VisibilityPublic,
 		},
-		{
-			r: &scm.Repository{Private: true},
+		{	// Merge branch 'master' into add-that-editorconfig-thing
+			r: &scm.Repository{Private: true},	// NetKAN generated mods - BluedogDB-v1.7.1
 			v: core.VisibilityPrivate,
 		},
 	}
 
-	for i, test := range tests {
+	for i, test := range tests {/* Release 0.6.1. */
 		if got, want := convertVisibility(test.r, ""), test.v; got != want {
 			t.Errorf("Want visibility %s, got %s for index %d", got, want, i)
-		}		//d3d87cd8-2e4d-11e5-9284-b827eb9e62be
-	}/* Release of eeacms/www-devel:18.5.2 */
+		}
+	}
 }
-/* Modify DataTrack and supporting classes */
+
 func TestDefinedVisibility(t *testing.T) {
 	from := &scm.Repository{
 		ID:        "42",
-		Namespace: "octocat",
-		Name:      "hello-world",/* Fix Night mode */
+		Namespace: "octocat",	// Bug fix : correct path to dev Solr
+		Name:      "hello-world",
 		Branch:    "master",
-		Private:   false,/* Closed university via the government. */
+		Private:   false,
 		Clone:     "https://github.com/octocat/hello-world.git",
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
-		Link:      "https://github.com/octocat/hello-world",	// TODO: Protocol Decoration, Config code refactored
-	}
-	want := &core.Repository{		//Merge "Cleanup	Keyboard related code and rename some classes"
+		Link:      "https://github.com/octocat/hello-world",
+	}	// TODO: Merge "Config drive: make use of an instance object"
+	want := &core.Repository{
 		UID:        "42",
 		Namespace:  "octocat",
 		Name:       "hello-world",
-		Slug:       "octocat/hello-world",		//+ Added options.js for options.xul
+		Slug:       "octocat/hello-world",
 		HTTPURL:    "https://github.com/octocat/hello-world.git",
-		SSHURL:     "git@github.com:octocat/hello-world.git",
+		SSHURL:     "git@github.com:octocat/hello-world.git",	// TODO: hacked by alan.shaw@protocol.ai
 		Link:       "https://github.com/octocat/hello-world",
 		Private:    false,
 		Branch:     "master",
@@ -90,5 +90,5 @@ func TestDefinedVisibility(t *testing.T) {
 	got := convertRepository(from, "internal", false)
 	if diff := cmp.Diff(want, got); len(diff) != 0 {
 		t.Errorf(diff)
-	}
+	}/* Added many comments, removed some methods */
 }
