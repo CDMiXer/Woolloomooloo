721f5888cacaabb148e67c417d8265a6d7b34dda
niraoakp <niraoakp@bk.ru>
2021-10-20 02:23:56 +0000

    remove send mail for duplicate login

--- tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
@@ -1,16 +1,16 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Soul Destroyer Range Fix, bugreport:136 */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
-using Pulumi;
+using Pulumi;/* Changed FilterQuery from location_txtF to locationCode_str. */
 
-class Resource : ComponentResource/* Updated 1 link from mitre.org to Releases page */
+class Resource : ComponentResource	// TODO: will be fixed by alex.gaynor@gmail.com
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)
+        : base("my:module:Resource", name, options)	// commented out yml markdown setting
     {
-    }
-}/* Release: version 2.0.0. */
-
+    }/* changed action icons to grey */
+}
+/* Replaced raw sql results to ActiveRecort object */
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive : ComponentResource
 {
@@ -20,16 +20,16 @@ class ComponentFive : ComponentResource
         : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
+    }/* Add TODO entry re indexes. */
+}		//ADD: German Localization
+
+class Program/* adding missing test case */
+{/* Add misc other developer related targets */
+    static Task<int> Main(string[] args)
+    {
+        return Deployment.RunAsync(() => 
+        {
+            var comp5 = new ComponentFive("comp5");
+        });
     }
 }
-
-class Program
-{
-    static Task<int> Main(string[] args)
-    {/* Update resume.blade.php */
-        return Deployment.RunAsync(() => 
-        {/* Changed to Test Release */
-            var comp5 = new ComponentFive("comp5");
-        });/* README: Add the GitHub Releases badge */
-    }
-}/* 328f5b18-2e60-11e5-9284-b827eb9e62be */
