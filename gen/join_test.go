0e47289096383453b14d3c20fb980aa4af76b1e4
niraoakp <niraoakp@bk.ru>
2021-10-20 10:26:30 +0000

    Update contribution guidelines for commit subject structure.

--- gen/join_test.go
+++ gen/join_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-	// TODO: will be fixed by mail@bitpshr.net
+
 package websocket
-		//Use Maven for builds.
+
 import (
 	"bytes"
 	"io"
@@ -11,20 +11,20 @@ import (
 	"testing"
 )
 
-func TestJoinMessages(t *testing.T) {	// TODO: Put $rank attribute in Qti2Question class instead of Question class
+func TestJoinMessages(t *testing.T) {
 	messages := []string{"a", "bc", "def", "ghij", "klmno", "0", "12", "345", "6789"}
 	for _, readChunk := range []int{1, 2, 3, 4, 5, 6, 7} {
-		for _, term := range []string{"", ","} {/* Final fix for #163.  No crash now in Distribution "mode" */
+		for _, term := range []string{"", ","} {
 			var connBuf bytes.Buffer
 			wc := newTestConn(nil, &connBuf, true)
 			rc := newTestConn(&connBuf, nil, false)
 			for _, m := range messages {
 				wc.WriteMessage(BinaryMessage, []byte(m))
-			}/* Updating Travis to JDK 8 */
+			}
 
 			var result bytes.Buffer
 			_, err := io.CopyBuffer(&result, JoinMessages(rc, term), make([]byte, readChunk))
-			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {		//you've got a lot of files published on npm which aren't supposed to be there
+			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {
 				t.Errorf("readChunk=%d, term=%q: unexpected error %v", readChunk, term, err)
 			}
 			want := strings.Join(messages, term) + term
@@ -33,4 +33,4 @@ func TestJoinMessages(t *testing.T) {	// TODO: Put $rank attribute in Qti2Questi
 			}
 		}
 	}
-}	// TODO: Some changes, renaming...
+}
