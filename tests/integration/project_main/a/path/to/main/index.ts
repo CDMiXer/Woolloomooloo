a4f44385bce2bdc4affb1d85632ddeab23a1defa
niraoakp <niraoakp@bk.ru>
2021-10-20 03:56:11 +0000

    Merge "sqlalchemy: fix metric expunge"

--- tests/integration/project_main/a/path/to/main/index.ts
+++ tests/integration/project_main/a/path/to/main/index.ts
@@ -1,3 +1,3 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-console.log("So much main");/* [testFeature] add test for linkTypetoString */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Fixed abstention label color for toggleCorrect answers. */
+/* Replaced "adldap2/adldap2" with "tiesa/ldap" */
+console.log("So much main");
