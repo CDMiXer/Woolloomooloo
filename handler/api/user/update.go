47feb42210e7304e93f5a9cdad19904685375e29
niraoakp <niraoakp@bk.ru>
2021-10-20 22:49:39 +0000

    Merge remote-tracking branch 'origin/GH370-delete-git-repository'

--- handler/api/user/update.go
+++ handler/api/user/update.go
@@ -3,21 +3,21 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Fixed test case for linux
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by nick@perfectabstractions.com
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* TEXTE + BUG OP */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// update NW link
-package user
+
+package user	// Delete .release-v1.1.0.md.swp
 
 import (
 	"encoding/json"
 	"net/http"
-/* Prepare Elastica Release 3.2.0 (#1085) */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
@@ -25,28 +25,28 @@ import (
 )
 
 // HandleUpdate returns an http.HandlerFunc that processes an http.Request
-// to update the current user account./* Delete the Makefile */
+// to update the current user account.
 func HandleUpdate(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
 
-		in := new(core.User)	// TODO: hacked by martin2cai@hotmail.com
+		in := new(core.User)
 		err := json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot unmarshal request body")
-			return		//New translations advanced-statistics.json (Slovenian)
+			return
 		}
-
+	// TODO: hacked by davidad@alum.mit.edu
 		viewer.Email = in.Email
-		err = users.Update(r.Context(), viewer)/* Merge branch 'master' into rest_get_releases */
-		if err != nil {	// TODO: Update version to 17
+		err = users.Update(r.Context(), viewer)
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot update user")
 		} else {
-			render.JSON(w, viewer, 200)
-		}/* Login validation added, sign out menu added. */
-	}		//7075bcf8-2e4f-11e5-9284-b827eb9e62be
+			render.JSON(w, viewer, 200)	// Update readme with installation, run info
+		}	// TODO: linux Maven issue cleanup
+	}	// Adicionado descricao e id
 }
