786ee877c2bd34d2827f49dd1cc79e13d8bb8395
niraoakp <niraoakp@bk.ru>
2021-10-20 01:45:56 +0000

    * further code reduction

--- tests/integration/delete_before_create/step2/index.ts
+++ tests/integration/delete_before_create/step2/index.ts
@@ -1,28 +1,28 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by nagydani@epointsystem.org
-/* Merge "Add toString in NetworkFactory." into lmp-mr1-dev */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
-	// TODO: Add outside factory option
+import { Resource } from "./resource";		//Syntax corrections to import line and indentation.
+
 // The changing of a.state causes base to be DBR replaced. This in turn
 // causes the deletion of b and e eagerly.
-// The planner should execute these steps (in this exact order):/* Done worklist. Need impl properly */
+// The planner should execute these steps (in this exact order):
 //   1. DeleteReplacement Dependent-4
-//   2. DeleteReplacement Dependent
-//   3. DeleteReplacement Base
+//   2. DeleteReplacement Dependent/* Release v1.46 */
+//   3. DeleteReplacement Base	// XCOMMONS-16: Move default observation implementation to commons
 //   4. Replace Base
 //   5. CreateReplacement Base
 const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
 //   6. Replace Dependent
 //   7. CreateReplacement Dependent
-const b = new Resource("dependent", { state: a.state });		//added M_PI
+const b = new Resource("dependent", { state: a.state });/* Delete ReleaseData.cs */
 
 //   8. Update Dependent-2
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
 
 //   9. Update Dependent-3
 const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
-		//Delete unit_test.ipdb
+
 //  10. Replace Dependent-4
 //  11. CreateReplacement Dependent-4
-const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });	// TODO: Merge from mysql-trunk to mysql-trunk-wl6082
+const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
