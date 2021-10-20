85f54020ccaf9717ecd5acf2b12c0b0ca99d8bc5
niraoakp <niraoakp@bk.ru>
2021-10-20 04:04:49 +0000

    Zoomable Panel refactoring + PreviewTransform Fix

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* 3b00e21e-2e6d-11e5-9284-b827eb9e62be */
 
 // +build !oss
 
-package mockscm
+package mockscm/* added Lizard Warrior */
 
 //go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
