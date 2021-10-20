e2c7a6481bed6c009030d7648c15b83e3cdff902
niraoakp <niraoakp@bk.ru>
2021-10-20 05:17:28 +0000

    el6Mc8xJqoVJ96myAILESwiJIgYSJGhC

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Release V.1.2 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* #137 Upgraded Spring Boot to 1.3.1.Release  */
 
 // +build oss
 
@@ -23,11 +23,11 @@ import (
 	"github.com/drone/drone/handler/api/render"
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {/* Version 016 from userscripts.org */
+	render.NotImplemented(w, render.ErrNotImplemented)		//Delete IStack.java
 }
 
 // HandleIncomplete returns a no-op http.HandlerFunc.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {		//Fixed Extension pointing to wrong redis memcache settings
 	return notImplemented
 }
