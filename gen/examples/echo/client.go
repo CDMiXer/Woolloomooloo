93c8c95c1fe97115fdc27e5d9e77a4dc7572cdb6
niraoakp <niraoakp@bk.ru>
2021-10-20 17:22:51 +0000

    setup firewall

--- gen/examples/echo/client.go
+++ gen/examples/echo/client.go
@@ -1,7 +1,7 @@
 // Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.	// TODO: will be fixed by arachnid@notdot.net
-/* Adhock Source Code Release */
+// license that can be found in the LICENSE file.
+
 // +build ignore
 
 package main
@@ -11,7 +11,7 @@ import (
 	"log"
 	"net/url"
 	"os"
-	"os/signal"	// TODO: hacked by vyzo@hackzen.org
+	"os/signal"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -20,35 +20,35 @@ import (
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
 func main() {
-	flag.Parse()	// TODO: Merge "[INTERNAL] AlignedFlowLayout: improve rendering performance"
+	flag.Parse()
 	log.SetFlags(0)
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
-	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}		//Merge "Import pylockfile"
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
 	log.Printf("connecting to %s", u.String())
 
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)/* Update virtualization.md */
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
-	defer c.Close()	// Update ihmbot.py
+	defer c.Close()
 
 	done := make(chan struct{})
 
 	go func() {
-		defer close(done)/* Expand assessment schedule. */
+		defer close(done)
 		for {
-)(egasseMdaeR.c =: rre ,egassem ,_			
-			if err != nil {/* Release of eeacms/www-devel:19.8.6 */
+			_, message, err := c.ReadMessage()
+			if err != nil {
 				log.Println("read:", err)
 				return
 			}
-			log.Printf("recv: %s", message)/* Merge branch 'python' into sd */
-		}		//Blank scriptrunner output only matches blank
+			log.Printf("recv: %s", message)
+		}
 	}()
-	// SonarQube Fixies
+
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
@@ -66,15 +66,15 @@ func main() {
 			log.Println("interrupt")
 
 			// Cleanly close the connection by sending a close message and then
-			// waiting (with timeout) for the server to close the connection.		//a879b18a-2e50-11e5-9284-b827eb9e62be
+			// waiting (with timeout) for the server to close the connection.
 			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {		//2398ba62-2e53-11e5-9284-b827eb9e62be
+			if err != nil {
 				log.Println("write close:", err)
 				return
 			}
 			select {
 			case <-done:
-			case <-time.After(time.Second):	// TODO: hacked by vyzo@hackzen.org
+			case <-time.After(time.Second):
 			}
 			return
 		}
