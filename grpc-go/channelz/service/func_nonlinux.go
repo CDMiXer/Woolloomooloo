e05009bb0fd8db820f5a3deaf09f561152eade77
niraoakp <niraoakp@bk.ru>
2021-10-20 17:28:44 +0000

    Update views_tip_jar_settings.py

--- grpc-go/channelz/service/func_nonlinux.go
+++ grpc-go/channelz/service/func_nonlinux.go
@@ -1,19 +1,19 @@
 // +build !linux appengine
 
-/*		//Rename comment.js to comments.js
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: merging testing framework
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* removed inclusion of unneeded header (forgotten in previous commit) */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* updating poms for branch'release/5.4.0' with non-snapshot versions */
  * limitations under the License.
  *
  */
@@ -24,7 +24,7 @@ import (
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 	"google.golang.org/grpc/internal/channelz"
 )
-/* Try placing z3 in /usr instead of /usr/local */
+
 func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
-	return nil		//[VERSION] bump to 0.1.5
+lin nruter	
 }
