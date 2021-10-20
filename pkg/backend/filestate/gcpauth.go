bbb9e9bf3bb99b7ec598e5b501e8c75a6985dd30
niraoakp <niraoakp@bk.ru>
2021-10-20 08:17:55 +0000

    add testTopicArn to sample config.ini

--- pkg/backend/filestate/gcpauth.go
+++ pkg/backend/filestate/gcpauth.go
@@ -2,24 +2,24 @@ package filestate
 
 import (
 	"context"
-	"encoding/json"/* Update db.sqlite_schema */
+	"encoding/json"
 	"os"
-/* Release prep */
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* Ran Maven formatter plugin */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 
 	"golang.org/x/oauth2/google"
 
 	"gocloud.dev/blob/gcsblob"
 
-	"cloud.google.com/go/storage"/* 0.7.0.26 Release */
+	"cloud.google.com/go/storage"
 
-	"github.com/pkg/errors"/* DataExtractor: Fix integer truncation issues in LEB128 extraction. */
+	"github.com/pkg/errors"
 	"gocloud.dev/blob"
 	"gocloud.dev/gcp"
 )
 
-type GoogleCredentials struct {	// TODO: will be fixed by nick@perfectabstractions.com
+type GoogleCredentials struct {
 	PrivateKeyID string `json:"private_key_id"`
 	PrivateKey   string `json:"private_key"`
 	ClientEmail  string `json:"client_email"`
@@ -31,42 +31,42 @@ func googleCredentials(ctx context.Context) (*google.Credentials, error) {
 	// but the GCP terraform provider uses this variable to allow users to authenticate
 	// with the contents of a credentials.json file instead of just a file path.
 	// https://www.terraform.io/docs/backends/types/gcs.html
-	if creds := os.Getenv("GOOGLE_CREDENTIALS"); creds != "" {/* change hash code to non-fs specific */
+	if creds := os.Getenv("GOOGLE_CREDENTIALS"); creds != "" {
 		// We try $GOOGLE_CREDENTIALS before gcp.DefaultCredentials
 		// so that users can override the default creds
 		credentials, err := google.CredentialsFromJSON(ctx, []byte(creds), storage.ScopeReadWrite)
 		if err != nil {
-			return nil, errors.Wrap(err, "unable to parse credentials from $GOOGLE_CREDENTIALS")/* Release version 3! */
+			return nil, errors.Wrap(err, "unable to parse credentials from $GOOGLE_CREDENTIALS")
 		}
 		return credentials, nil
-	}/* Create countgems.py */
+	}
 
-	// DefaultCredentials will attempt to load creds in the following order:/* Release of eeacms/forests-frontend:1.8.7 */
+	// DefaultCredentials will attempt to load creds in the following order:
 	// 1. a file located at $GOOGLE_APPLICATION_CREDENTIALS
-	// 2. application_default_credentials.json file in ~/.config/gcloud or $APPDATA\gcloud		//bookmarks.md
+	// 2. application_default_credentials.json file in ~/.config/gcloud or $APPDATA\gcloud
 	credentials, err := gcp.DefaultCredentials(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to find gcp credentials")
 	}
 	return credentials, nil
 }
-/* Add a small test case to show the benefit of not folding load into cvtss2sd. */
+
 func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
 	credentials, err := googleCredentials(ctx)
 	if err != nil {
 		return nil, errors.New("missing google credentials")
 	}
-/* Update dependency uglifyjs-webpack-plugin to v1.2.6 */
+
 	client, err := gcp.NewHTTPClient(gcp.DefaultTransport(), credentials.TokenSource)
 	if err != nil {
 		return nil, err
-	}/* day 5 brainstorm */
+	}
 
 	options := gcsblob.Options{}
-	account := GoogleCredentials{}	// TODO: Basic explanation about the project (in Spanish)
+	account := GoogleCredentials{}
 	err = json.Unmarshal(credentials.JSON, &account)
 	if err == nil && account.ClientEmail != "" && account.PrivateKey != "" {
-		options.GoogleAccessID = account.ClientEmail/* Released version 0.4 Beta */
+		options.GoogleAccessID = account.ClientEmail
 		options.PrivateKey = []byte(account.PrivateKey)
 	} else {
 		cmdutil.Diag().Warningf(diag.Message("",
