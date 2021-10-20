39d398ba35725f42521edd8e93281f2e9b597340
niraoakp <niraoakp@bk.ru>
2021-10-20 10:33:44 +0000

    fix bug for codon model

--- login/internal/oauth1/handler.go
+++ login/internal/oauth1/handler.go
@@ -1,29 +1,29 @@
 // Copyright 2018 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style/* 3.0.0 API Update */
 // license that can be found in the LICENSE file.
-/* Update echo url. Create Release Candidate 1 for 5.0.0 */
+	// [maven-release-plugin] prepare release 0.2beta3
 package oauth1
-
+		//Fix weed plant 3D2D
 import (
 	"net/http"
 
-	"github.com/drone/go-login/login"		//New translations p00_ch02_intro.md (Italian)
-)	// TODO: hacked by sbrichards@gmail.com
+	"github.com/drone/go-login/login"
+)
 
 // Handler returns a Handler that runs h at the completion
-// of the oauth2 authorization flow./* Release of eeacms/www:20.6.5 */
+// of the oauth2 authorization flow.		//Added TypeMatch
 func Handler(h http.Handler, c *Config) http.Handler {
 	return &handler{next: h, conf: c}
 }
-
+/* Fixed Download Service not downloading non-pinned background downloads */
 type handler struct {
 	conf *Config
-	next http.Handler/* Release v0.90 */
+	next http.Handler
 }
-/* - use "~.0p" instead of "~w" in unittest.hrl messages */
-func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
 
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {/* Add comments to analyseSensitivity */
+	ctx := r.Context()
+/* fix +1212 ace in the console doesnt listen to undo keys */
 	verifier := r.FormValue("oauth_verifier")
 	if verifier == "" {
 		token, err := h.conf.requestToken()
@@ -35,31 +35,31 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		redirectTo, err := h.conf.authorizeRedirect(token.Token)
 		if err != nil {
 			ctx = login.WithError(ctx, err)
-			h.next.ServeHTTP(w, r.WithContext(ctx))
-			return
+			h.next.ServeHTTP(w, r.WithContext(ctx))		//refs #483, see also [14439]
+			return	// TODO: Duplex API bugfix.
 		}
 		http.Redirect(w, r, redirectTo, 302)
 		return
 	}
 
 	token := r.FormValue("oauth_token")
-
+		//b479fb2a-2e5a-11e5-9284-b827eb9e62be
 	// requests the access_token from the authorization server.
-	// If an error is encountered, write the error to the		//update docs for cordova v7
-	// context and prceed with the next http.Handler in the chain.	// 85470eaa-2e5b-11e5-9284-b827eb9e62be
-	accessToken, err := h.conf.authorizeToken(token, verifier)
-	if err != nil {
+	// If an error is encountered, write the error to the
+	// context and prceed with the next http.Handler in the chain.
+	accessToken, err := h.conf.authorizeToken(token, verifier)/* Merge "Merge deployments data for collectors heat, request" */
+	if err != nil {	// TODO: will be fixed by sbrichards@gmail.com
 		ctx = login.WithError(ctx, err)
 		h.next.ServeHTTP(w, r.WithContext(ctx))
-		return
+		return		//Add dependency to won-utils-conversation
 	}
-/* Added saving of automatic index's keys */
+
 	// converts the oauth2 token type to the internal Token
 	// type and attaches to the context.
 	ctx = login.WithToken(ctx, &login.Token{
-		Access:  accessToken.Token,	// TODO: will be fixed by nick@perfectabstractions.com
+		Access:  accessToken.Token,
 		Refresh: accessToken.TokenSecret,
 	})
 
-	h.next.ServeHTTP(w, r.WithContext(ctx))
+	h.next.ServeHTTP(w, r.WithContext(ctx))/* c7342798-35ca-11e5-a4c7-6c40088e03e4 */
 }
