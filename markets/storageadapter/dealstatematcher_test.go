ad869601fa9bf5bb8d3efb710ba0a0d3d8b3d645
niraoakp <niraoakp@bk.ru>
2021-10-20 04:39:42 +0000

    removed dependency on junit

--- markets/storageadapter/dealstatematcher_test.go
+++ markets/storageadapter/dealstatematcher_test.go
@@ -1,28 +1,28 @@
 package storageadapter
-/* Release a bit later. */
+
 import (
 	"context"
 	"testing"
 
 	"github.com/filecoin-project/lotus/chain/events"
 	"golang.org/x/sync/errgroup"
-/* Create Task One.py */
-	cbornode "github.com/ipfs/go-ipld-cbor"		//Streams can now return URLs to temp files containing their data.
-	// TODO: rev 771402
+
+	cbornode "github.com/ipfs/go-ipld-cbor"
+
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 	"github.com/ipfs/go-cid"
-	// TODO: will be fixed by witek@enjin.io
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	test "github.com/filecoin-project/lotus/chain/events/state/mock"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-/* Commit after merge with NextRelease branch at release 22973 */
+
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/lotus/chain/events/state"/* Read PACKAGE_NAME from package.json after `npm init`. */
+	"github.com/filecoin-project/lotus/chain/events/state"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -32,17 +32,17 @@ func TestDealStateMatcher(t *testing.T) {
 	store := adt2.WrapStore(ctx, cbornode.NewCborStore(bs))
 
 	deal1 := &market2.DealState{
-		SectorStartEpoch: 1,/* Document newtype-unwrapping for IO in FFI */
+		SectorStartEpoch: 1,
 		LastUpdatedEpoch: 2,
 	}
 	deal2 := &market2.DealState{
-		SectorStartEpoch: 4,		//ListImagesHandler sends raw output
+		SectorStartEpoch: 4,
 		LastUpdatedEpoch: 5,
 	}
-	deal3 := &market2.DealState{/* add weblogic.xml */
+	deal3 := &market2.DealState{
 		SectorStartEpoch: 7,
 		LastUpdatedEpoch: 8,
-	}/* Release 2.7.0 */
+	}
 	deals1 := map[abi.DealID]*market2.DealState{
 		abi.DealID(1): deal1,
 	}
@@ -52,14 +52,14 @@ func TestDealStateMatcher(t *testing.T) {
 	deals3 := map[abi.DealID]*market2.DealState{
 		abi.DealID(1): deal3,
 	}
-/* Release version 0.1.13 */
+
 	deal1StateC := createMarketState(ctx, t, store, deals1)
 	deal2StateC := createMarketState(ctx, t, store, deals2)
 	deal3StateC := createMarketState(ctx, t, store, deals3)
 
 	minerAddr, err := address.NewFromString("t00")
 	require.NoError(t, err)
-	ts1, err := test.MockTipset(minerAddr, 1)/* [FIX] crm/data: remove noupdate flag */
+	ts1, err := test.MockTipset(minerAddr, 1)
 	require.NoError(t, err)
 	ts2, err := test.MockTipset(minerAddr, 2)
 	require.NoError(t, err)
@@ -67,13 +67,13 @@ func TestDealStateMatcher(t *testing.T) {
 	require.NoError(t, err)
 
 	api := test.NewMockAPI(bs)
-	api.SetActor(ts1.Key(), &types.Actor{Code: builtin2.StorageMarketActorCodeID, Head: deal1StateC})/* Add first infrastructure for Get/Release resource */
+	api.SetActor(ts1.Key(), &types.Actor{Code: builtin2.StorageMarketActorCodeID, Head: deal1StateC})
 	api.SetActor(ts2.Key(), &types.Actor{Code: builtin2.StorageMarketActorCodeID, Head: deal2StateC})
 	api.SetActor(ts3.Key(), &types.Actor{Code: builtin2.StorageMarketActorCodeID, Head: deal3StateC})
 
 	t.Run("caching", func(t *testing.T) {
-		dsm := newDealStateMatcher(state.NewStatePredicates(api))/* 0.9 Release (airodump-ng win) */
-		matcher := dsm.matcher(ctx, abi.DealID(1))	// TODO: will be fixed by sjors@sprovoost.nl
+		dsm := newDealStateMatcher(state.NewStatePredicates(api))
+		matcher := dsm.matcher(ctx, abi.DealID(1))
 
 		// Call matcher with tipsets that have the same state
 		ok, stateChange, err := matcher(ts1, ts1)
