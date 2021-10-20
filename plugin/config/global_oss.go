955d23875870d55067bcfa6f80e56ab94e9df1ea
niraoakp <niraoakp@bk.ru>
2021-10-20 04:23:51 +0000

    a bit more level 3 code

--- plugin/config/global_oss.go
+++ plugin/config/global_oss.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-///* Test on Linux and OS X */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Merge: Kalman updates */
-//	// TODO: update json to v2.15.1
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by ligi@ligi.de
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
@@ -24,7 +24,7 @@ import (
 )
 
 // Global returns a no-op configuration service.
-func Global(string, string, bool, time.Duration) core.ConfigService {/* merged lsf-master */
+func Global(string, string, bool, time.Duration) core.ConfigService {
 	return new(noop)
 }
 
