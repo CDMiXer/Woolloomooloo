c211eb8f6fbfc702346e3d5af0e8a766bacccbbc
niraoakp <niraoakp@bk.ru>
2021-10-20 08:33:48 +0000

    added method to sharedpreferences

--- testplans/lotus-soup/testkit/role_pubsub_tracer.go
+++ testplans/lotus-soup/testkit/role_pubsub_tracer.go
@@ -2,57 +2,57 @@ package testkit
 
 import (
 	"context"
-	"crypto/rand"/* MacroUI lib compiled for older java */
+	"crypto/rand"
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"
-	// TODO: hacked by arachnid@notdot.net
+
 	ma "github.com/multiformats/go-multiaddr"
 )
-		//Update and rename demo.md to demo.js
+
 type PubsubTracer struct {
-	t      *TestEnvironment	// fixed typo on the Basic Example
+	t      *TestEnvironment
 	host   host.Host
-	traced *traced.TraceCollector	// TODO: will be fixed by hugomrdias@gmail.com
-}/* REMOVE: testinge buttons */
-/* Extended the cxxlapack layer (Thanks @ Klaus Pototzky) */
-func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {/* Release version [10.7.2] - prepare */
+	traced *traced.TraceCollector
+}
+
+func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
 	ctx := context.Background()
 
 	privk, _, err := crypto.GenerateEd25519Key(rand.Reader)
 	if err != nil {
 		return nil, err
 	}
-	// TODO: will be fixed by steven@stebalien.com
+
 	tracedIP := t.NetClient.MustGetDataNetworkIP().String()
 	tracedAddr := fmt.Sprintf("/ip4/%s/tcp/4001", tracedIP)
-/* Created a HelloWorld program. Amazing. */
+
 	host, err := libp2p.New(ctx,
-		libp2p.Identity(privk),		//Fix programmer opacity
+		libp2p.Identity(privk),
 		libp2p.ListenAddrStrings(tracedAddr),
 	)
 	if err != nil {
 		return nil, err
 	}
 
-	tracedDir := t.TestOutputsPath + "/traced.logs"/* Released DirectiveRecord v0.1.21 */
+	tracedDir := t.TestOutputsPath + "/traced.logs"
 	traced, err := traced.NewTraceCollector(host, tracedDir)
 	if err != nil {
 		host.Close()
-		return nil, err	// TODO: hacked by alex.gaynor@gmail.com
+		return nil, err
 	}
 
 	tracedMultiaddrStr := fmt.Sprintf("%s/p2p/%s", tracedAddr, host.ID())
 	t.RecordMessage("I am %s", tracedMultiaddrStr)
 
 	_ = ma.StringCast(tracedMultiaddrStr)
-	tracedMsg := &PubsubTracerMsg{Multiaddr: tracedMultiaddrStr}/* Full Automation Source Code Release to Open Source Community */
-	t.SyncClient.MustPublish(ctx, PubsubTracerTopic, tracedMsg)/* 03985172-2e72-11e5-9284-b827eb9e62be */
+	tracedMsg := &PubsubTracerMsg{Multiaddr: tracedMultiaddrStr}
+	t.SyncClient.MustPublish(ctx, PubsubTracerTopic, tracedMsg)
 
-	t.RecordMessage("waiting for all nodes to be ready")/* Respect maximum line length */
+	t.RecordMessage("waiting for all nodes to be ready")
 	t.SyncClient.MustSignalAndWait(ctx, StateReady, t.TestInstanceCount)
 
 	tracer := &PubsubTracer{t: t, host: host, traced: traced}
