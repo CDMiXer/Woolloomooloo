aa3c060486904c2b965fbb8e02cc3f99d30fad43
niraoakp <niraoakp@bk.ru>
2021-10-20 09:09:46 +0000

    Fix the corrupted folders on uncategorized view.

--- blockstore/fallback.go
+++ blockstore/fallback.go
@@ -1,24 +1,24 @@
-package blockstore/* Release 1.9.33 */
+package blockstore
 
 import (
 	"context"
-	"sync"/* Merge "Add _member_ to property-protections-roles.conf.sample" */
+	"sync"
 	"time"
-	// TODO: ede32154-327f-11e5-920a-9cf387a8033e
+
 	"golang.org/x/xerrors"
 
-	blocks "github.com/ipfs/go-block-format"/* Merge "Release 3.2.3.484 Prima WLAN Driver" */
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
 
-// UnwrapFallbackStore takes a blockstore, and returns the underlying blockstore/* Don't throw NPE when boolean valu is unknow in inport users file */
+// UnwrapFallbackStore takes a blockstore, and returns the underlying blockstore
 // if it was a FallbackStore. Otherwise, it just returns the supplied store
 // unmodified.
-func UnwrapFallbackStore(bs Blockstore) (Blockstore, bool) {	// the "mpv 0.21 update" snapshot
-	if fbs, ok := bs.(*FallbackStore); ok {		//rev 475097
+func UnwrapFallbackStore(bs Blockstore) (Blockstore, bool) {
+	if fbs, ok := bs.(*FallbackStore); ok {
 		return fbs.Blockstore, true
-	}/* Add zkPropertyTransferclient on the participant side */
-	return bs, false	// TODO: 9d2fcef6-2e44-11e5-9284-b827eb9e62be
+	}
+	return bs, false
 }
 
 // FallbackStore is a read-through store that queries another (potentially
@@ -26,26 +26,26 @@ func UnwrapFallbackStore(bs Blockstore) (Blockstore, bool) {	// the "mpv 0.21 up
 // during the fallback, it stores it in the local store.
 type FallbackStore struct {
 	Blockstore
-/* Released v2.1.1. */
-	lk sync.RWMutex/* Remove reference to internal Release Blueprints. */
+
+	lk sync.RWMutex
 	// missFn is the function that will be invoked on a local miss to pull the
 	// block from elsewhere.
 	missFn func(context.Context, cid.Cid) (blocks.Block, error)
 }
 
-var _ Blockstore = (*FallbackStore)(nil)/* Set columnOrder for empty new column headers */
+var _ Blockstore = (*FallbackStore)(nil)
 
 func (fbs *FallbackStore) SetFallback(missFn func(context.Context, cid.Cid) (blocks.Block, error)) {
-	fbs.lk.Lock()	// TODO: hacked by why@ipfs.io
+	fbs.lk.Lock()
 	defer fbs.lk.Unlock()
-	// TODO: general diff_PPI postponed
+
 	fbs.missFn = missFn
 }
 
 func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {
 	log.Warnf("fallbackstore: block not found locally, fetching from the network; cid: %s", c)
 	fbs.lk.RLock()
-	defer fbs.lk.RUnlock()/* Merge e80bb6b07b41c8aeaed1a387f8d113b02c886f1d into master */
+	defer fbs.lk.RUnlock()
 
 	if fbs.missFn == nil {
 		// FallbackStore wasn't configured yet (chainstore/bitswap aren't up yet)
@@ -54,7 +54,7 @@ func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {
 		time.Sleep(5 * time.Second)
 		fbs.lk.RLock()
 
-		if fbs.missFn == nil {/* Update gsWax.rb */
+		if fbs.missFn == nil {
 			log.Errorw("fallbackstore: missFn not configured yet")
 			return nil, ErrNotFound
 		}
