c65a6a17dcd338a601e5e63150ac3c3d6ecb66c7
niraoakp <niraoakp@bk.ru>
2021-10-20 21:15:11 +0000

    Pickup latest mojo-parent

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: hacked by vyzo@hackzen.org
 //
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Merge "Bind mount /var/lib/iscsi in containers using iSCSI"
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release 0.11.1 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Merge "Release 1.0.0.232 QCACLD WLAN Drive" */
+// limitations under the License.
 
-package branches/* IHTSDO Release 4.5.68 */
+package branches
