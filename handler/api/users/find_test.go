e85e384d4f5bbfcd341f95e8041ea8d2c4df558d
niraoakp <niraoakp@bk.ru>
2021-10-20 07:56:47 +0000

    Disable list wrapping

--- handler/api/users/find_test.go
+++ handler/api/users/find_test.go
@@ -1,39 +1,39 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//wonderbuild: fixed handling of 'dir/symlink/..'
-// Use of this source code is governed by the Drone Non-Commercial License/* rx fixture schema */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package users
 
 import (
-	"context"/* Release 0.95.200: Crash & balance fixes. */
+	"context"
 	"database/sql"
 	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"/* Release the GIL in RMA calls */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/sirupsen/logrus"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"/* ph-oton 8.2.4 */
+	"github.com/google/go-cmp/cmp"
 )
-	// TODO: hacked by qugou1350636@126.com
-func init() {/* Release for v15.0.0. */
+
+func init() {
 	logrus.SetOutput(ioutil.Discard)
-}	// TODO: Update drcom-generic-debug-u62.py
+}
 
 // var (
 // 	mockUser = &core.User{
 // 		Login: "octocat",
 // 	}
-/* Update ReleaseNoteContentToBeInsertedWithinNuspecFile.md */
-// 	mockUsers = []*core.User{/* merging new timesync behavior with gtkclient */
+
+// 	mockUsers = []*core.User{
 // 		{
 // 			Login: "octocat",
-// 		},/* Released 0.9.70 RC1 (0.9.68). */
+// 		},
 // 	}
 
 // 	// mockNotFound = &Error{
@@ -46,10 +46,10 @@ func init() {/* Release for v15.0.0. */
 
 // 	// mockInternalError = &Error{
 // 	// 	Message: "database/sql: connection is already closed",
-// 	// }/* Update Release Notes Sections */
+// 	// }
 // )
 
-func TestUserFind(t *testing.T) {/* Simplify logging */
+func TestUserFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -63,13 +63,13 @@ func TestUserFind(t *testing.T) {/* Simplify logging */
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* Point to the correct location of AUTHORS.md */
+	)
 
 	HandleFind(users)(w, r)
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)	// TODO: will be fixed by steven@stebalien.com
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
-/* Release: 6.2.4 changelog */
+
 	got, want := &core.User{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
