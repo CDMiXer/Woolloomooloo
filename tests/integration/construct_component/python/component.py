1e08493fdf538874033b4e5bf94b6463de0f23c8
niraoakp <niraoakp@bk.ru>
2021-10-20 19:01:30 +0000

    Add multiple field to label.

--- tests/integration/construct_component/python/component.py
+++ tests/integration/construct_component/python/component.py
@@ -1,13 +1,13 @@
 # Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+	// TODO: work please
 from typing import Any, Optional
 
 import pulumi
-
+		//Delete nodeids
 class Component(pulumi.ComponentResource):
-    echo: pulumi.Output[Any]
-    childId: pulumi.Output[str]
-
+    echo: pulumi.Output[Any]/* Release of eeacms/www-devel:19.11.27 */
+    childId: pulumi.Output[str]		//Fixed bug -- should have been checking `msg`, not `object`
+/* Delete panorama2.png */
     def __init__(self, name: str, echo: pulumi.Input[Any], opts: Optional[pulumi.ResourceOptions] = None):
         props = dict()
         props["echo"] = echo
