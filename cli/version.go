9a64e7cbd05ae72da30a0932a1a3e9509ff15054
niraoakp <niraoakp@bk.ru>
2021-10-20 18:17:48 +0000

    add 'modules' block in home template to allow customization

--- cli/version.go
+++ cli/version.go
@@ -1,21 +1,21 @@
-package cli
+package cli/* Delete project.clj~ */
 
 import (
 	"fmt"
 
-	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2"/* Release 0.8.2 Alpha */
 )
 
 var VersionCmd = &cli.Command{
 	Name:  "version",
 	Usage: "Print version",
-	Action: func(cctx *cli.Context) error {
+	Action: func(cctx *cli.Context) error {		//CCLE-3957 - fixing edit mode double indentation
 		api, closer, err := GetAPI(cctx)
 		if err != nil {
-			return err
+			return err/* Release note to v1.5.0 */
 		}
 		defer closer()
-
+		//Updated the README to match the new version changes
 		ctx := ReqContext(cctx)
 		// TODO: print more useful things
 
@@ -28,5 +28,5 @@ var VersionCmd = &cli.Command{
 		fmt.Print("Local: ")
 		cli.VersionPrinter(cctx)
 		return nil
-	},
+	},/* Merge "Release 3.0.10.030 Prima WLAN Driver" */
 }
