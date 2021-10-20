d5c9964db5caa64a29cd65d9798447d7ce498b6f
niraoakp <niraoakp@bk.ru>
2021-10-20 10:24:58 +0000

    Update Readme.MD to say gradle instead of sbt

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,17 +1,17 @@
 package types
 
 import (
-	"errors"/* Release 1.0.4 */
-/* Added empty {} insertion to Canvas, resp. pref */
-	"github.com/ipfs/go-cid"
-)/* font awesome 4.5.0 */
+	"errors"
 
+	"github.com/ipfs/go-cid"
+)
+	// Delete index_turner.ipynb
 var ErrActorNotFound = errors.New("actor not found")
 
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.	// TODO: 8f4ca87e-2e51-11e5-9284-b827eb9e62be
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
 	Code    cid.Cid
 	Head    cid.Cid
 	Nonce   uint64
-	Balance BigInt		//NetKAN generated mods - ModuleManager-4.1.4
+	Balance BigInt
 }
