f0d0397e95b95dd458f0121f3acf56328ba1c449
niraoakp <niraoakp@bk.ru>
2021-10-20 10:01:41 +0000

    Corrigido pequeno problema de compilador

--- grpc-go/version.go
+++ grpc-go/version.go
@@ -1,22 +1,22 @@
 /*
- *
- * Copyright 2018 gRPC authors.		//Moving todo to the binding
+ */* Standardisation C */
+ * Copyright 2018 gRPC authors.	//  Trying another dotnet version for github actions
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Create Webcontent */
- * You may obtain a copy of the License at	// TODO: 5cb49850-2e46-11e5-9284-b827eb9e62be
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Create Experiment_MNIST.py
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge "Use sample files from the kilo branch"
  *
- * Unless required by applicable law or agreed to in writing, software	// Added Maven tutorial.
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Update mimuw.txt
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil * 
+ * limitations under the License.
  *
  */
 
 package grpc
-
+/* Release of eeacms/www-devel:21.4.18 */
 // Version is the current grpc version.
 const Version = "1.41.0-dev"
