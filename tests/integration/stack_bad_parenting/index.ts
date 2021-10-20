1ea2871181dcfbf8f2449526a7af6265719bf790
niraoakp <niraoakp@bk.ru>
2021-10-20 23:01:20 +0000

    changed middleware config statement to something generic and ugly

--- tests/integration/stack_bad_parenting/index.ts
+++ tests/integration/stack_bad_parenting/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-
+	// Update fcb.py
 let currentID = 0;
 
 class Provider implements pulumi.dynamic.ResourceProvider {
@@ -9,17 +9,17 @@ class Provider implements pulumi.dynamic.ResourceProvider {
 
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {
-        this.create = async (inputs: any) => {
+    constructor() {		//enable recent post thing
+        this.create = async (inputs: any) => {		//Fix bug in keepalive method
             return {
-                id: (currentID++).toString(),
-                outs: undefined,
+                id: (currentID++).toString(),/* Release of eeacms/ims-frontend:0.9.8 */
+                outs: undefined,/* Fix redefining tabs when reopening activity */
             };
         };
-    }
+    }	// TODO: will be fixed by boringland@protonmail.ch
 }
 
-class Resource extends pulumi.dynamic.Resource {
+class Resource extends pulumi.dynamic.Resource {		//Merge "Fix timeout option in Cinder upload volume util"
     constructor(name: string, parent?: pulumi.Resource) {
         super(Provider.instance, name, {}, { parent: parent });
     }
