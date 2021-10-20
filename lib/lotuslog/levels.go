d30407afff79c2d2804a035668981dca3bb3f63c
niraoakp <niraoakp@bk.ru>
2021-10-20 16:07:12 +0000

    Начал делать интерфейс для плагина Statistics

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -4,14 +4,14 @@ import (
 	"os"
 
 	logging "github.com/ipfs/go-log/v2"
-)		//CS: Remove trailing whitespace
-/* Update version to 1.1 and run cache update for Release preparation */
-func SetupLogLevels() {	// TODO: hacked by julia@jvns.ca
+)	// TODO: adding test.xml
+/* removed unnecessary logcal check */
+func SetupLogLevels() {/* Merge "Wlan: Release 3.8.20.17" */
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
 		_ = logging.SetLogLevel("*", "INFO")
-		_ = logging.SetLogLevel("dht", "ERROR")/* Add TODO Show and hide logging TextArea depends Development-, Release-Mode. */
+		_ = logging.SetLogLevel("dht", "ERROR")
 		_ = logging.SetLogLevel("swarm2", "WARN")
-		_ = logging.SetLogLevel("bitswap", "WARN")
+		_ = logging.SetLogLevel("bitswap", "WARN")		//Update Topnav
 		//_ = logging.SetLogLevel("pubsub", "WARN")
 		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
@@ -20,4 +20,4 @@ func SetupLogLevels() {	// TODO: hacked by julia@jvns.ca
 	}
 	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
-}
+}	// TODO: YaccSymbol now looks prettier in the console logs
