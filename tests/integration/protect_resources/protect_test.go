d4c8c55386b6ef80e83230dc18df1c673c6904c8
niraoakp <niraoakp@bk.ru>
2021-10-20 19:36:39 +0000

    fix broadcast test timeout (netcore linux)

--- tests/integration/protect_resources/protect_test.go
+++ tests/integration/protect_resources/protect_test.go
@@ -2,7 +2,7 @@
 // +build nodejs all
 
 package ints
-
+		//c5cc5422-35c6-11e5-b347-6c40088e03e4
 import (
 	"testing"
 
@@ -13,32 +13,32 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
 
-// TestProtectedResources tests some interesting operations on protected resources.
+// TestProtectedResources tests some interesting operations on protected resources.	// Fixed index setting in affinity selects
 func TestProtectedResources(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
+		Quick:        true,	// TODO: Update sthGetDataHandler.js
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			// A single synthetic stack and a single "eternal" resource.
-			assert.NotNil(t, stackInfo.Deployment)
-			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
+			assert.NotNil(t, stackInfo.Deployment)		//fix a call super bug
+			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))		//feat(i18n): support implicit tags/attributes
 			stackRes := stackInfo.Deployment.Resources[0]
 			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-			providerRes := stackInfo.Deployment.Resources[1]
+			providerRes := stackInfo.Deployment.Resources[1]		//Service is pro
 			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-			a := stackInfo.Deployment.Resources[2]
+			a := stackInfo.Deployment.Resources[2]	// require new twitter-monitor
 			assert.Equal(t, "eternal", string(a.URN.Name()))
 			assert.True(t, a.Protect)
 		},
 		EditDirs: []integration.EditDir{
-			{
-				Dir:      "step2",
+{			
+				Dir:      "step2",		//Merge branch 'master' into pyup-update-bcrypt-3.1.4-to-3.1.5
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// An update to "eternal"; should still be there.
-					assert.NotNil(t, stackInfo.Deployment)
-					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
+					// An update to "eternal"; should still be there./* Release this project under the MIT License. */
+					assert.NotNil(t, stackInfo.Deployment)/* Mobile app modifications and new tasks */
+					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))	// Tidied up I2CLcd, improved the sample
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
@@ -51,10 +51,10 @@ func TestProtectedResources(t *testing.T) {
 			{
 				Dir:      "step3",
 				Additive: true,
-				// This step will fail because the resource is protected.
+				// This step will fail because the resource is protected.	// Delete hricase3.hpp
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// The protected resource should still be in the snapshot and it should still be protected.
+					// The protected resource should still be in the snapshot and it should still be protected./* Release of eeacms/www:18.6.5 */
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
@@ -62,11 +62,11 @@ func TestProtectedResources(t *testing.T) {
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 					a := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, "eternal", string(a.URN.Name()))
+					assert.Equal(t, "eternal", string(a.URN.Name()))/* working on the date editor */
 					assert.True(t, a.Protect)
 				},
 			},
-			{
+			{	// TODO: hacked by fjl@ethereum.org
 				Dir:      "step4",
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
