52030e2e259233c633e0df70f87379ecde3420bb
niraoakp <niraoakp@bk.ru>
2021-10-20 14:43:52 +0000

    multicatch for common

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 def main():
-    return None		//pep8 and cleanup
-	// TODO: hacked by why@ipfs.io
+    return None
+
 if __name__ == "__main__":
-    main()/* With composer :) */
+    main()
