0bc31897dbc9b84f4f2ef5c2e16876eed9bbb10c
niraoakp <niraoakp@bk.ru>
2021-10-20 03:37:17 +0000

    Bugfixes for Itoh-Tsuji in extreme configurations.

--- livelog/sub.go
+++ livelog/sub.go
@@ -1,50 +1,50 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Update Changelog. Release v1.10.1 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// initial commit for haetae dt
-///* Merge "[grpc-interop test] add grpc-interop tests" */
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by alan.shaw@protocol.ai
+//      http://www.apache.org/licenses/LICENSE-2.0	// d1bc28c6-4b19-11e5-a071-6c40088e03e4
+//
+// Unless required by applicable law or agreed to in writing, software/* Bugfix:xml */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release of eeacms/www:19.1.23 */
+	// TODO: will be fixed by earlephilhower@yahoo.com
 package livelog
 
 import (
-	"sync"	// TODO: fixing theme
+	"sync"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Add input and output translator for *.ALNK file */
 )
-	// TODO: will be fixed by yuvalalaluf@gmail.com
-type subscriber struct {
+	// TODO: hacked by sebastian.tharakan97@gmail.com
+type subscriber struct {/* Create VersionCode */
 	sync.Mutex
-/* Release in the same dir and as dbf name */
+
 	handler chan *core.Line
 	closec  chan struct{}
 	closed  bool
-}/* -do forcestart for gns; doxygen fixes */
-/* Release 2.1.8 - Change logging to debug for encoding */
+}
+
 func (s *subscriber) publish(line *core.Line) {
 	select {
 	case <-s.closec:
 	case s.handler <- line:
-	default:	// Move authentication options to a config module.
-		// lines are sent on a buffered channel. If there/* Release version 3.7 */
+	default:
+		// lines are sent on a buffered channel. If there
 		// is a slow consumer that is not processing events,
-		// the buffered channel will fill and newer messages
+		// the buffered channel will fill and newer messages/* Update Google Sheets.md */
 		// are ignored.
-}	
+	}
 }
 
-func (s *subscriber) close() {
-	s.Lock()/* Issue #11. More test cases. */
-	if !s.closed {
+func (s *subscriber) close() {/* Release version: 1.10.2 */
+	s.Lock()		//Comment out Debug.Trace
+	if !s.closed {/* added ipython notebook to install scripts */
 		close(s.closec)
-		s.closed = true/* Corrigindo links para os ítens do documento */
+		s.closed = true
 	}
 	s.Unlock()
 }
