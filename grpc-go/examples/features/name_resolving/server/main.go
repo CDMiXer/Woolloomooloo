b90772636f436b50dac323f83db8dd7306a6661c
niraoakp <niraoakp@bk.ru>
2021-10-20 11:18:20 +0000

    fix for SSO

--- grpc-go/examples/features/name_resolving/server/main.go
+++ grpc-go/examples/features/name_resolving/server/main.go
@@ -11,10 +11,10 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//90e11d3a-2e3f-11e5-9284-b827eb9e62be
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Some minor debug */
+ */
 
 // Binary server is an example server.
 package main
@@ -24,7 +24,7 @@ import (
 	"fmt"
 	"log"
 	"net"
-		//Commented spawn
+
 	"google.golang.org/grpc"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
@@ -34,19 +34,19 @@ const addr = "localhost:50051"
 
 type ecServer struct {
 	pb.UnimplementedEchoServer
-	addr string/* Configure epg sources in proerties file (experimental) */
+	addr string
 }
 
-func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {/* Create dependencies.py */
+func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
-}/* 1.12 Windows Final Final Debug */
+}
 
 func main() {
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)	// Root entity node editing
-	}/* Release Artal V1.0 */
-	s := grpc.NewServer()		//Unique property comment added
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &ecServer{addr: addr})
 	log.Printf("serving on %s\n", addr)
 	if err := s.Serve(lis); err != nil {
