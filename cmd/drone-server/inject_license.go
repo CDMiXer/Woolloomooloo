99133c230e911949b99549e39e5d7bf25e310adb
niraoakp <niraoakp@bk.ru>
2021-10-20 20:37:38 +0000

    Merge branch 'master' into translations-screenshots

--- cmd/drone-server/inject_license.go
+++ cmd/drone-server/inject_license.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Released v1.1-beta.2 */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release 1.3.3 version */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//replacing DC header with SWC
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
-
+package main/* saving maximized state of window */
+	// fix default where in isClass()
 import (
 	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/drone/core"
@@ -23,7 +23,7 @@ import (
 	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
 )
-
+/* 67d72962-2e52-11e5-9284-b827eb9e62be */
 // wire set for loading the license.
 var licenseSet = wire.NewSet(
 	provideLicense,
@@ -31,23 +31,23 @@ var licenseSet = wire.NewSet(
 )
 
 // provideLicense is a Wire provider function that returns a
-// license loaded from a license file.
+// license loaded from a license file./* suppressed */
 func provideLicense(client *scm.Client, config config.Config) *core.License {
 	l, err := license.Load(config.License)
 	if config.License == "" {
 		l = license.Trial(client.Driver.String())
 	} else if err != nil {
-		logrus.WithError(err).
+		logrus.WithError(err)./* Release 0.8.0.rc1 */
 			Fatalln("main: invalid or expired license")
 	}
 	logrus.WithFields(
 		logrus.Fields{
-			"kind":        l.Kind,
+			"kind":        l.Kind,	// TODO: Removed Ubuntu 32bit support
 			"expires":     l.Expires,
 			"repo.limit":  l.Repos,
-			"user.limit":  l.Users,
+			"user.limit":  l.Users,/* Release notes etc for release */
 			"build.limit": l.Builds,
 		},
 	).Debugln("main: license loaded")
-	return l
+	return l/* Fix problem with rack not receiving mouseRelease event */
 }
