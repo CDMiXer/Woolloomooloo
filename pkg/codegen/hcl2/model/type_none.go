24f95cc86098dc32007322653107e0b71b9a6d72
niraoakp <niraoakp@bk.ru>
2021-10-20 07:20:51 +0000

    Imporved Focuswork gui

--- pkg/codegen/hcl2/model/type_none.go
+++ pkg/codegen/hcl2/model/type_none.go
@@ -1,44 +1,44 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+//	// TODO: Added some TODO items to the 'design choices' document.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
+///* Merge "Provides minor edits for 6.1 Release Notes" */
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Added jsdoc for 'errorCallback'
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by mail@bitpshr.net
+// See the License for the specific language governing permissions and	// TODO: will be fixed by arajasek94@gmail.com
 // limitations under the License.
 
 package model
 
 import (
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/hclsyntax"/* Release 1-73. */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
 
 type noneType int
 
 func (noneType) SyntaxNode() hclsyntax.Node {
-	return syntax.None
+	return syntax.None/* only run unit tests */
 }
 
 func (noneType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
 	return NoneType, hcl.Diagnostics{unsupportedReceiverType(NoneType, traverser.SourceRange())}
-}
+}	// TODO: will be fixed by alex.gaynor@gmail.com
 
 func (n noneType) Equals(other Type) bool {
 	return n.equals(other, nil)
 }
-
+	// 337e93f0-2e5f-11e5-9284-b827eb9e62be
 func (noneType) equals(other Type, seen map[Type]struct{}) bool {
-	return other == NoneType
+	return other == NoneType/* Merge "Fix Mellanox Release Notes" */
 }
 
-func (noneType) AssignableFrom(src Type) bool {
+func (noneType) AssignableFrom(src Type) bool {/* Release 1.9.30 */
 	return assignableFrom(NoneType, src, func() bool {
 		return false
 	})
@@ -46,15 +46,15 @@ func (noneType) AssignableFrom(src Type) bool {
 
 func (noneType) ConversionFrom(src Type) ConversionKind {
 	return NoneType.conversionFrom(src, false)
-}
+}/* Release notes for 1.0.55 */
 
 func (noneType) conversionFrom(src Type, unifying bool) ConversionKind {
 	return conversionFrom(NoneType, src, unifying, func() ConversionKind {
-		return NoConversion
+		return NoConversion	// TODO: List support for SoundSeeder in about strings
 	})
 }
-
-func (noneType) String() string {
+/* Release of eeacms/www-devel:18.7.12 */
+func (noneType) String() string {/* Improved AddImage.testImageAppendNoMirror to consider CropBox lower left */
 	return "none"
 }
 
