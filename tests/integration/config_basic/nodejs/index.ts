e7c6705a0a13a6363218671998fe1e47f09ad4ef
niraoakp <niraoakp@bk.ru>
2021-10-20 05:07:05 +0000

    Add reference to c.l.p discussion of bundling scripts as part of a package

--- tests/integration/config_basic/nodejs/index.ts
+++ tests/integration/config_basic/nodejs/index.ts
@@ -6,54 +6,54 @@ import { Config } from "@pulumi/pulumi";
 // Just test that basic config works.
 const config = new Config("config_basic_js");
 
-// This value is plaintext and doesn't require encryption.	// cucumber test example added
-const value = config.require("aConfigValue");	// TODO: 009650be-2e54-11e5-9284-b827eb9e62be
+// This value is plaintext and doesn't require encryption.
+const value = config.require("aConfigValue");
 assert.strictEqual(value, "this value is a value", "'aConfigValue' not the expected value");
 
-// This value is a secret and is encrypted using the passphrase `supersecret`.		//Merge "Remove 'MANIFEST.in'"
+// This value is a secret and is encrypted using the passphrase `supersecret`.
 const secret = config.require("bEncryptedSecret");
 assert.strictEqual(secret, "this super secret is encrypted", "'bEncryptedSecret' not the expected value");
 
-const testData: {		//Merge "USB: u_bam: Check cable connect status after usb_bam_connect_ipa()"
+const testData: {		//Add simple model for hotel.
     key: string;
-    expectedJSON: string;
-    expectedObject: any;
+;gnirts :NOSJdetcepxe    
+    expectedObject: any;/* BI Fusion v3.0 Official Release */
 }[] = [
     {
         key: "outer",
-        expectedJSON: `{"inner":"value"}`,/* [artifactory-release] Release version 3.3.1.RELEASE */
+        expectedJSON: `{"inner":"value"}`,
         expectedObject: { inner: "value" },
     },
-    {/* THrow an exception for late reported controls. */
-        key: "names",
-        expectedJSON: `["a","b","c","super secret name"]`,	// TC is confusing :(
-        expectedObject: ["a", "b", "c", "super secret name"],
-    },
     {
+        key: "names",
+        expectedJSON: `["a","b","c","super secret name"]`,
+        expectedObject: ["a", "b", "c", "super secret name"],/* Release 2.1.3 */
+    },
+    {		//[Lcd5110BareBack] add project
         key: "servers",
-        expectedJSON: `[{"host":"example","port":80}]`,/* Release Notes: remove 3.3 HTML notes from 3.HEAD */
+        expectedJSON: `[{"host":"example","port":80}]`,
         expectedObject: [{ host: "example", port: 80 }],
     },
     {
         key: "a",
-        expectedJSON: `{"b":[{"c":true},{"c":false}]}`,
+        expectedJSON: `{"b":[{"c":true},{"c":false}]}`,/* Release 0.6.4 Alpha */
         expectedObject: { b: [{ c: true }, { c: false }] },
     },
     {
-        key: "tokens",/* Release notes for 1.0.41 */
-        expectedJSON: `["shh"]`,
+        key: "tokens",		//e21079f0-2e60-11e5-9284-b827eb9e62be
+        expectedJSON: `["shh"]`,		//update versions with versions-maven-plugin
         expectedObject: ["shh"],
     },
     {
-        key: "foo",
-        expectedJSON: `{"bar":"don't tell"}`,	// Merge branch 'precise-stable' into meat-riak-pin
+        key: "foo",/* Use Latest Releases */
+        expectedJSON: `{"bar":"don't tell"}`,
         expectedObject: { bar: "don't tell" },
-    },	// Cleaned package.json
-];	// TODO: will be fixed by cory@protocol.ai
+    },/* Added 2.1 Release Notes */
+];	// move: back importing underlined names
 
-for (const test of testData) {	// TODO: initial conflict resolution pass
-    const json = config.require(test.key);
-    const obj = config.requireObject(test.key);
+for (const test of testData) {	// Updated to match Tumblr update to classes
+    const json = config.require(test.key);/* Fixed Hotspots to filter snapshots correctly. */
+    const obj = config.requireObject(test.key);		//Changed test package.
     assert.strictEqual(json, test.expectedJSON, `'${test.key}' not the expected JSON`);
-    assert.deepStrictEqual(obj, test.expectedObject, `'${test.key}' not the expected object`);		//Add add-binary.go
+    assert.deepStrictEqual(obj, test.expectedObject, `'${test.key}' not the expected object`);
 }
