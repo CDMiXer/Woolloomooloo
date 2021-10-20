5f6a48978a213d584e871c5014f979b2ef1e5503
niraoakp <niraoakp@bk.ru>
2021-10-20 10:28:00 +0000

    Update my_toupper.c

--- handler/api/repos/builds/branches/list_test.go
+++ handler/api/repos/builds/branches/list_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package branches	// TODO: Volume Rendering: Fixed inverted normals of the Noise generator.
+package branches
