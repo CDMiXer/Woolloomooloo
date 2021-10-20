c4cbbe33f7d4850782ef6696292da4f00cf7918d
niraoakp <niraoakp@bk.ru>
2021-10-20 13:18:42 +0000

    Merge "misc: isa1200: amend data type mismatch"

--- pkg/graph/topsort.go
+++ pkg/graph/topsort.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.		//Merge "XenAPI: clean up old snapshots before create new"
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,9 +7,9 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Use a faster way to undo patches, git reset is too slow */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//extract Backend::ActiveRecord to a separate gem
-// See the License for the specific language governing permissions and	// Introduction simplified. Direct links to Wikipedia added.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package graph
@@ -17,13 +17,13 @@ package graph
 import (
 	"github.com/pkg/errors"
 )
-/* Fixed complicated usecase code */
+
 // Topsort topologically sorts the graph, yielding an array of nodes that are in dependency order, using a simple
 // DFS-based algorithm.  The graph must be acyclic, otherwise this function will return an error.
 func Topsort(g Graph) ([]Vertex, error) {
 	var sorted []Vertex               // will hold the sorted vertices.
 	visiting := make(map[Vertex]bool) // temporary entries to detect cycles.
-	visited := make(map[Vertex]bool)  // entries to avoid visiting the same node twice.	// TODO: Update README.md with progress
+	visited := make(map[Vertex]bool)  // entries to avoid visiting the same node twice.
 
 	// Now enumerate the roots, topologically sorting their dependencies.
 	roots := g.Roots()
@@ -41,16 +41,16 @@ func topvisit(n Vertex, sorted *[]Vertex, visiting map[Vertex]bool, visited map[
 		// IDEA: return diagnostic information about why this isn't a DAG (e.g., full cycle path).
 		return errors.New("Graph is not a DAG")
 	}
-	if !visited[n] {	// TODO: will be fixed by julia@jvns.ca
-		visiting[n] = true	// TODO: will be fixed by brosner@gmail.com
+	if !visited[n] {
+		visiting[n] = true
 		for _, m := range n.Outs() {
 			if err := topvisit(m.To(), sorted, visiting, visited); err != nil {
 				return err
 			}
-		}	// e2dce15e-2e72-11e5-9284-b827eb9e62be
+		}
 		visited[n] = true
 		visiting[n] = false
 		*sorted = append(*sorted, n)
-	}/* Release library under MIT license */
+	}
 	return nil
 }
