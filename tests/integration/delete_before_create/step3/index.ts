8ea653f24181fcce1837c2221c1e9ce57ee59a0a
niraoakp <niraoakp@bk.ru>
2021-10-20 09:18:26 +0000

    NGS fixed frame rate

--- tests/integration/delete_before_create/step3/index.ts
+++ tests/integration/delete_before_create/step3/index.ts
@@ -1,20 +1,20 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// 940b4a6a-2e6e-11e5-9284-b827eb9e62be
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
 
 // The changing of a.state causes base to be DBR replaced. This in turn
-// causes the deletion of b and e eagerly. However, in this case, resource/* Released version 0.9.2 */
-// e does not exist in this file anymore and won't be recreated.		//BetterUnit after James feedback
-// The planner should execute these steps (in this exact order):		//Why is this here?
+// causes the deletion of b and e eagerly. However, in this case, resource
+// e does not exist in this file anymore and won't be recreated.
+// The planner should execute these steps (in this exact order):
 //   1. DeleteReplacement Dependent-2
 //   2. DeleteReplacement Dependent
-esaB tnemecalpeReteleD .3   //
+//   3. DeleteReplacement Base
 //   4. Replace Base
-//   5. CreateReplacement Base		//Fixed Spinner issues.
+//   5. CreateReplacement Base
 const a = new Resource("base", { uniqueKey: 1, state: 100 });
 
 //   6. Replace Dependent
 //   7. CreateReplacement Dependent
 const b = new Resource("dependent", { state: a.state });
-/* Merge "Release note for scheduler rework" */
+
 //   Done. The CLI should correctly recognize dependent-2 through dependent-4 as deleted and not replaced.
