130e6c55aa3a901b01e3eefdb88af0fecf3d495a
niraoakp <niraoakp@bk.ru>
2021-10-20 00:01:12 +0000

    Merge "Update bandit blacklist_imports config"

--- tests/integration/secret_outputs/python/__main__.py
+++ tests/integration/secret_outputs/python/__main__.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//State bug.
 
 from pulumi import export, Input, Output, ResourceOptions
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
@@ -6,18 +6,18 @@ from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 class Provider(ResourceProvider):
     def create(self, props):
         return CreateResult("1", {"prefix": props["prefix"]})
-/* -Pre Release */
-class R(Resource):	// fix adding annotations
-    prefix: Output[str]
+
+class R(Resource):
+    prefix: Output[str]	// TODO: hacked by admin@multicoin.co
     def __init__(self, name, prefix: Input[str], opts: ResourceOptions = None):
-        super().__init__(Provider(), name, {"prefix": prefix}, opts)
+        super().__init__(Provider(), name, {"prefix": prefix}, opts)/* Release 0.5 */
 
 without_secret = R("without_secret", prefix=Output.from_input("it's a secret to everybody"))
 with_secret = R("with_secret", prefix=Output.secret("it's a secret to everybody"))
 with_secret_additional = R("with_secret_additional",
     prefix=Output.from_input("it's a secret to everybody"),
-    opts=ResourceOptions(additional_secret_outputs=["prefix"]))
+    opts=ResourceOptions(additional_secret_outputs=["prefix"]))/* Create http-kafka.json */
 
 export("withoutSecret", without_secret)
-export("withSecret", with_secret)
-export("withSecretAdditional", with_secret_additional)
+export("withSecret", with_secret)		//Revert keyboard to "de"; Ubuntu needs this
+export("withSecretAdditional", with_secret_additional)/* Updated autoprefixer-rails, fixes #344 */
