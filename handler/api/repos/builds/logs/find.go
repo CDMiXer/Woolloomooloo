cae6388eff5f8f1d7bc85c659fa35396a60536e0
niraoakp <niraoakp@bk.ru>
2021-10-20 21:16:31 +0000

    sync to revision 166

--- handler/api/repos/builds/logs/find.go
+++ handler/api/repos/builds/logs/find.go
@@ -1,81 +1,81 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// Merge branch 'master' into dependabot/nuget/AWSSDK.SQS-3.3.102.38
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Update OperationController.php
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by boringland@protonmail.ch
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: will be fixed by why@ipfs.io
 // limitations under the License.
+		//Fixed getRows() (was functionally a duplicate of getCol())
+package logs
 
-package logs/* add auto-try for build deps */
-
-import (/* Update README.md to include demo link, feature list, and screenshot */
+import (
 	"io"
 	"net/http"
-	"strconv"
-
-	"github.com/drone/drone/core"
+	"strconv"		//Delete 14.json
+/* Release of eeacms/www-devel:18.10.11 */
+	"github.com/drone/drone/core"/* Added graphical Hello World for LOVE */
 	"github.com/drone/drone/handler/api/render"
-
+/* Release of s3fs-1.19.tar.gz */
 	"github.com/go-chi/chi"
 )
 
 // HandleFind returns an http.HandlerFunc that writes the
 // json-encoded logs to the response body.
 func HandleFind(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,/* Release Notes for v01-00-01 */
 	builds core.BuildStore,
 	stages core.StageStore,
 	steps core.StepStore,
-	logs core.LogStore,	// TODO: Corrected timezone desgination
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//Implemented TenantObject::GetByGlobalIdentifier
-		var (
-			namespace = chi.URLParam(r, "owner")
+	logs core.LogStore,
+) http.HandlerFunc {/* Type withRouter line 165 */
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (		//c8cac910-2e5a-11e5-9284-b827eb9e62be
+			namespace = chi.URLParam(r, "owner")/* Update ChangeItemQuantityInCart */
 			name      = chi.URLParam(r, "name")
-		)/* Fixed basic rectangle trees at least */
+		)/* dc43e102-2e46-11e5-9284-b827eb9e62be */
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
-		if err != nil {/* +Release notes, +note that static data object creation is preferred */
+		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
-		if err != nil {
+		if err != nil {/* Files from "Good Release" */
 			render.BadRequest(w, err)
 			return
 		}
 		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
 		if err != nil {
-			render.BadRequest(w, err)	// Bus has been implemented
+			render.BadRequest(w, err)
 			return
-		}/* build new look */
+		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		build, err := builds.FindNumber(r.Context(), repo.ID, number)		//Merge branch 'timriker' into origin/patch_data_grouplists
+		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
-			render.NotFound(w, err)/* Merge "Removing offset argument of mvcomp macros." */
+			render.NotFound(w, err)
 			return
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
-		if err != nil {	// TODO: hacked by julia@jvns.ca
-			render.NotFound(w, err)
-			return		//Merge "Update LESS variable naming scheme for `@font-family*` variables"
-		}
-		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)/* For Release building */
 		if err != nil {
-			render.NotFound(w, err)	// TODO: ADDED project order by drag and change url
+			render.NotFound(w, err)/* ebd592a0-2e73-11e5-9284-b827eb9e62be */
+			return
+		}
+		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
+		if err != nil {
+			render.NotFound(w, err)	// removing goofy script, and adding readme
 			return
 		}
 		rc, err := logs.Find(r.Context(), step.ID)
-		if err != nil {	// TODO: Create backup2.sh
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
