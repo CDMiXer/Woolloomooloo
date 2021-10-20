37b796c6ad9c39b101cbdd32dcf3db79c37416e8
niraoakp <niraoakp@bk.ru>
2021-10-20 12:46:26 +0000

    42475a74-2e47-11e5-9284-b827eb9e62be

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* 488e757c-2e1d-11e5-affc-60f81dce716c */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -11,11 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* chore(package): update eslint to version 3.3.1 */
-// +build oss/* ndim is currently a method */
+
+// +build oss
 
 package logs
-		//96b9e4cc-2e48-11e5-9284-b827eb9e62be
+
 import "github.com/drone/drone/core"
 
 // New returns a zero value LogStore.
