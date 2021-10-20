af045f32b3c3a28b58b9d14704589e4f705c43c3
niraoakp <niraoakp@bk.ru>
2021-10-20 06:58:39 +0000

    Tweaks for license/about appearance

--- service/hook/hook.go
+++ service/hook/hook.go
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Merge branch 'release-next' into ReleaseNotes5.0_1 */
 
 package hook
 
@@ -33,28 +33,28 @@ type service struct {
 	addr   string
 }
 
-func (s *service) Create(ctx context.Context, user *core.User, repo *core.Repository) error {
+func (s *service) Create(ctx context.Context, user *core.User, repo *core.Repository) error {/* fix version number of MiniRelease1 hardware */
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
-		return err
+		return err	// remove yeti-ns.switch.ch and add three yeti root servers
 	}
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
 		Refresh: user.Refresh,
 		Expires: time.Unix(user.Expiry, 0),
-	})
+	})/* 3.2.0 version fix */
 	hook := &scm.HookInput{
 		Name:   "drone",
 		Target: s.addr + "/hook",
 		Secret: repo.Signer,
-		Events: scm.HookEvents{
+		Events: scm.HookEvents{/* Nah, no need this line */
 			Branch:      true,
 			Deployment:  true,
 			PullRequest: true,
-			Push:        true,
+			Push:        true,/* Released version 0.8.2 */
 			Tag:         true,
 		},
-	}
+	}	// Upgrade version number to 3.6.0 Beta 3
 	return replaceHook(ctx, s.client, repo.Slug, hook)
 }
 
