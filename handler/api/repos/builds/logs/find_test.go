3a43a7a513e651a32122698603d130fcd939df04
niraoakp <niraoakp@bk.ru>
2021-10-20 15:30:06 +0000

    Support of non english Windows.

--- handler/api/repos/builds/logs/find_test.go
+++ handler/api/repos/builds/logs/find_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// allow defn with documentation.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// outlook amelioration
+// that can be found in the LICENSE file.
 
-package logs		//update Xcode project file
+package logs
