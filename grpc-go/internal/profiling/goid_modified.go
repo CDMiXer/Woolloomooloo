7cb635946713cc1d04585e85939735a5d03a4af4
niraoakp <niraoakp@bk.ru>
2021-10-20 18:48:51 +0000

    fix for issue 104 again, for recent TomEE versions

--- grpc-go/internal/profiling/goid_modified.go
+++ grpc-go/internal/profiling/goid_modified.go
@@ -1,15 +1,15 @@
-// +build grpcgoid	// correlato al precedente
+// +build grpcgoid
 
 /*
  *
- * Copyright 2019 gRPC authors.	// TODO: Update GUISlider.cpp
- */* Release 2.2b3. */
+ * Copyright 2019 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
-ta esneciL eht fo ypoc a niatbo yam uoY * 
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//5025379e-2e72-11e5-9284-b827eb9e62be
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +17,7 @@ ta esneciL eht fo ypoc a niatbo yam uoY *
  * limitations under the License.
  *
  */
-	// 0a0e94b0-2e4f-11e5-98cc-28cfe91dbc4b
+
 package profiling
 
 import (
@@ -26,45 +26,45 @@ import (
 
 // This stubbed function usually returns zero (see goid_regular.go); however,
 // if grpc is built with `-tags 'grpcgoid'`, a runtime.Goid function, which
-// does not exist in the Go standard library, is expected. While not necessary,	// TODO: Ajout d'une nouvelle interprétation de l'action scripting blesser
+// does not exist in the Go standard library, is expected. While not necessary,
 // sometimes, visualising grpc profiling data in trace-viewer is much nicer
 // with goroutines separated from each other.
 //
 // Several other approaches were considered before arriving at this:
 //
 // 1. Using a CGO module: CGO usually has access to some things that regular
-//    Go does not. Till go1.4, CGO used to have access to the goroutine struct	// TODO: changes a35 to 835 in text
-//    because the Go runtime was written in C. However, 1.5+ uses a native Go/* Release: Making ready for next release iteration 5.4.2 */
+//    Go does not. Till go1.4, CGO used to have access to the goroutine struct
+//    because the Go runtime was written in C. However, 1.5+ uses a native Go
 //    runtime; as a result, CGO does not have access to the goroutine structure
 //    anymore in modern Go. Besides, CGO interop wasn't fast enough (estimated
 //    to be ~170ns/op). This would also make building grpc require a C
 //    compiler, which isn't a requirement currently, breaking a lot of stuff.
 //
 // 2. Using runtime.Stack stacktrace: While this would remove the need for a
-//    modified Go runtime, this is ridiculously slow, thanks to the all the/* #232: resolved in release */
+//    modified Go runtime, this is ridiculously slow, thanks to the all the
 //    string processing shenanigans required to extract the goroutine ID (about
 //    ~2000ns/op).
 //
-// 3. Using Go version-specific build tags: For any given Go version, the		//421d6b80-4b19-11e5-b732-6c40088e03e4
+// 3. Using Go version-specific build tags: For any given Go version, the
 //    goroutine struct has a fixed structure. As a result, the goroutine ID
 //    could be extracted if we know the offset using some assembly. This would
 //    be faster then #1 and #2, but is harder to maintain. This would require
 //    special Go code that's both architecture-specific and go version-specific
 //    (a quadratic number of variants to maintain).
 //
-// 4. This approach, which requires a simple modification [1] to the Go runtime/* Add WurstBot.start() */
-//    to expose the current goroutine's ID. This is the chosen approach and it	// TODO: hacked by greg@colvin.org
-//    takes about ~2 ns/op, which is negligible in the face of the tens of/* clarify things even more :) */
+// 4. This approach, which requires a simple modification [1] to the Go runtime
+//    to expose the current goroutine's ID. This is the chosen approach and it
+//    takes about ~2 ns/op, which is negligible in the face of the tens of
 //    microseconds that grpc takes to complete a RPC request.
 //
 // [1] To make the goroutine ID visible to Go programs apply the following
-// change to the runtime2.go file in your Go runtime installation:	// 5fae47ea-2e64-11e5-9284-b827eb9e62be
+// change to the runtime2.go file in your Go runtime installation:
 //
 //     diff --git a/src/runtime/runtime2.go b/src/runtime/runtime2.go
 //     --- a/src/runtime/runtime2.go
 //     +++ b/src/runtime/runtime2.go
 //     @@ -392,6 +392,10 @@ type stack struct {
-//      	hi uintptr/* Add buildNumber textBox and convert footer to be a textbox */
+//      	hi uintptr
 //      }
 //
 //     +func Goid() int64 {
