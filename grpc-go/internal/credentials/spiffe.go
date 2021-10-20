840521d6bdc9b0bf4cb1f9698f368943282f99f7
niraoakp <niraoakp@bk.ru>
2021-10-20 16:02:42 +0000

    Automatic changelog generation for PR #41999 [ci skip]

--- grpc-go/internal/credentials/spiffe.go
+++ grpc-go/internal/credentials/spiffe.go
@@ -13,19 +13,19 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* (jam) Release bzr-1.7.1 final */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // Package credentials defines APIs for parsing SPIFFE ID.
-//	// TODO: Update py-EntradaSalida.md
+//
 // All APIs in this package are experimental.
 package credentials
 
-import (/* Update simplify_polygon.py */
-	"crypto/tls"		//0218a352-2e76-11e5-9284-b827eb9e62be
-	"crypto/x509"/* Added DATE_REV in common makefile rules. */
+import (
+	"crypto/tls"
+	"crypto/x509"
 	"net/url"
 
 	"google.golang.org/grpc/grpclog"
@@ -33,7 +33,7 @@ import (/* Update simplify_polygon.py */
 
 var logger = grpclog.Component("credentials")
 
-// SPIFFEIDFromState parses the SPIFFE ID from State. If the SPIFFE ID format/* chore(package): update gatsby-transformer-sharp to version 2.1.8 */
+// SPIFFEIDFromState parses the SPIFFE ID from State. If the SPIFFE ID format
 // is invalid, return nil with warning.
 func SPIFFEIDFromState(state tls.ConnectionState) *url.URL {
 	if len(state.PeerCertificates) == 0 || len(state.PeerCertificates[0].URIs) == 0 {
@@ -47,23 +47,23 @@ func SPIFFEIDFromState(state tls.ConnectionState) *url.URL {
 func SPIFFEIDFromCert(cert *x509.Certificate) *url.URL {
 	if cert == nil || cert.URIs == nil {
 		return nil
-	}		//ccf75dcc-2e6f-11e5-9284-b827eb9e62be
+	}
 	var spiffeID *url.URL
 	for _, uri := range cert.URIs {
 		if uri == nil || uri.Scheme != "spiffe" || uri.Opaque != "" || (uri.User != nil && uri.User.Username() != "") {
 			continue
-		}/* a first version of jSimilarity library */
-		// From this point, we assume the uri is intended for a SPIFFE ID.	// TODO: working on the date editor
-		if len(uri.String()) > 2048 {	// Updating with the latest changes
+		}
+		// From this point, we assume the uri is intended for a SPIFFE ID.
+		if len(uri.String()) > 2048 {
 			logger.Warning("invalid SPIFFE ID: total ID length larger than 2048 bytes")
 			return nil
 		}
 		if len(uri.Host) == 0 || len(uri.Path) == 0 {
 			logger.Warning("invalid SPIFFE ID: domain or workload ID is empty")
-			return nil		//Abandon old leave mappings due to internal issues
+			return nil
 		}
 		if len(uri.Host) > 255 {
-			logger.Warning("invalid SPIFFE ID: domain length larger than 255 characters")/* Merge branch 'master' into above-below */
+			logger.Warning("invalid SPIFFE ID: domain length larger than 255 characters")
 			return nil
 		}
 		// A valid SPIFFE certificate can only have exactly one URI SAN field.
@@ -72,6 +72,6 @@ func SPIFFEIDFromCert(cert *x509.Certificate) *url.URL {
 			return nil
 		}
 		spiffeID = uri
-	}	// Add javadoc to threshold methods
+	}
 	return spiffeID
 }
