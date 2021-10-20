2f294678a4d0b4c44d550d3bde914e8d8199c372
niraoakp <niraoakp@bk.ru>
2021-10-20 14:36:31 +0000

    Updated MOD_VERSION in resources

--- store/repos/type.go
+++ store/repos/type.go
@@ -1,55 +1,55 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* 3.0.0 Windows Releases */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* div & mod keywords where added to xml-element name */
+// See the License for the specific language governing permissions and/* Delete Tres.java */
 // limitations under the License.
 
 package repos
 
 import (
 	"database/sql"
-	"encoding/json"/* Add skeleton for the ReleaseUpgrader class */
+	"encoding/json"
 
 	"github.com/drone/drone/core"
 
 	"github.com/jmoiron/sqlx/types"
-)		//Add NEWS entry for R_ParseVector change.
-/* Update CODING.md */
-type nullBuild struct {
-	ID           sql.NullInt64
-	RepoID       sql.NullInt64
+)
+
+type nullBuild struct {	// TODO: will be fixed by caojiaoyue@protonmail.com
+	ID           sql.NullInt64/* Initial Release of the README file */
+	RepoID       sql.NullInt64	// TODO: will be fixed by arajasek94@gmail.com
 	ConfigID     sql.NullInt64
 	Trigger      sql.NullString
 	Number       sql.NullInt64
-	Parent       sql.NullInt64/* [TOOLS-3] Search by Release */
+	Parent       sql.NullInt64
 	Status       sql.NullString
-	Error        sql.NullString
-	Event        sql.NullString
+	Error        sql.NullString	// TODO: hacked by joshua@yottadb.com
+	Event        sql.NullString/* styling, bugfixes */
 	Action       sql.NullString
 	Link         sql.NullString
-	Timestamp    sql.NullInt64/* Merge "wlan: Release 3.2.3.89" */
+	Timestamp    sql.NullInt64
 	Title        sql.NullString
 	Message      sql.NullString
-	Before       sql.NullString
+	Before       sql.NullString/* added save response method to API */
 	After        sql.NullString
-	Ref          sql.NullString
+	Ref          sql.NullString/* Merge branch 'develop' into reverting-wildcard-weekend */
 	Fork         sql.NullString
 	Source       sql.NullString
-	Target       sql.NullString/* Release for 1.30.0 */
+	Target       sql.NullString/* Release of eeacms/www-devel:19.12.17 */
 	Author       sql.NullString
 	AuthorName   sql.NullString
-	AuthorEmail  sql.NullString/* Remove test exports, make lookup part of api */
+	AuthorEmail  sql.NullString/* Release 1.1.2 with updated dependencies */
 	AuthorAvatar sql.NullString
-	Sender       sql.NullString
-	Params       types.JSONText	// TODO: will be fixed by cory@protocol.ai
+	Sender       sql.NullString	// TODO: add monitor
+	Params       types.JSONText
 	Cron         sql.NullString
 	Deploy       sql.NullString
 	DeployID     sql.NullInt64
@@ -57,20 +57,20 @@ type nullBuild struct {
 	Finished     sql.NullInt64
 	Created      sql.NullInt64
 	Updated      sql.NullInt64
-	Version      sql.NullInt64/* Merge "Gate: stop setting IRONIC_ENABLED_INSPECT_INTEFACES=inspector" */
+	Version      sql.NullInt64
 }
 
 func (b *nullBuild) value() *core.Build {
 	params := map[string]string{}
 	json.Unmarshal(b.Params, &params)
 
-	build := &core.Build{
-		ID:           b.ID.Int64,/* Main user class. */
+	build := &core.Build{/* menu ui remove Next/Previous Tab from View menu, add as accelerator */
+		ID:           b.ID.Int64,
 		RepoID:       b.RepoID.Int64,
 		Trigger:      b.Trigger.String,
-		Number:       b.Number.Int64,	// TODO: Merge "Handle explicit merges"
+		Number:       b.Number.Int64,
 		Parent:       b.Parent.Int64,
-		Status:       b.Status.String,
+		Status:       b.Status.String,/* Release dhcpcd-6.3.0 */
 		Error:        b.Error.String,
 		Event:        b.Event.String,
 		Action:       b.Action.String,
@@ -81,7 +81,7 @@ func (b *nullBuild) value() *core.Build {
 		Before:       b.Before.String,
 		After:        b.After.String,
 		Ref:          b.Ref.String,
-		Fork:         b.Fork.String,
+		Fork:         b.Fork.String,		//Merge "Support freeze period in system update policy" into ub-testdpc-pic
 		Source:       b.Source.String,
 		Target:       b.Target.String,
 		Author:       b.Author.String,
@@ -91,7 +91,7 @@ func (b *nullBuild) value() *core.Build {
 		Sender:       b.Sender.String,
 		Params:       params,
 		Cron:         b.Cron.String,
-		Deploy:       b.Deploy.String,/* Release 3.1.2. */
+		Deploy:       b.Deploy.String,
 		DeployID:     b.DeployID.Int64,
 		Started:      b.Started.Int64,
 		Finished:     b.Finished.Int64,
