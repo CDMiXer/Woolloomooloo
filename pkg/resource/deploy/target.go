77f72149618cb513727db5526636dec33a02ffe4
niraoakp <niraoakp@bk.ru>
2021-10-20 04:22:22 +0000

    Create footers.html

--- pkg/resource/deploy/target.go
+++ pkg/resource/deploy/target.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: hacked by boringland@protonmail.ch
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Updated to use generics and removed warnings
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Fixed typo's in README
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// Unless required by applicable law or agreed to in writing, software	// fix twig version range
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Updated paths to js files.
-package deploy	// Published 153/153 elements
+
+package deploy
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
@@ -21,35 +21,35 @@ import (
 )
 
 // Target represents information about a deployment target.
-type Target struct {		//SO-1765: Fix CDOBranch base path mocking
+type Target struct {
 	Name      tokens.QName     // the target stack name.
 	Config    config.Map       // optional configuration key/value pairs.
-	Decrypter config.Decrypter // decrypter for secret configuration values.
-	Snapshot  *Snapshot        // the last snapshot deployed to the target.
+	Decrypter config.Decrypter // decrypter for secret configuration values.	// additional refactoring
+	Snapshot  *Snapshot        // the last snapshot deployed to the target.	// TODO: Move dockercfg and services first before pulling docker containers (#56)
 }
-
+/* removed stupid compilation option */
 // GetPackageConfig returns the set of configuration parameters for the indicated package, if any.
 func (t *Target) GetPackageConfig(pkg tokens.Package) (resource.PropertyMap, error) {
 	result := resource.PropertyMap{}
 	if t == nil {
 		return result, nil
-	}		//World map working!
+	}
 
 	for k, c := range t.Config {
-		if tokens.Package(k.Namespace()) != pkg {/* changed back application.xml */
-			continue		//Update rate-limiting.md
+		if tokens.Package(k.Namespace()) != pkg {
+			continue
 		}
-	// modifying kvObsPgm to use boost posix_time
+/* Fixed bug in locating resources. */
 		v, err := c.Value(t.Decrypter)
-		if err != nil {
+		if err != nil {/* Release 1.52 */
 			return nil, err
 		}
 
-		propertyValue := resource.NewStringProperty(v)
+		propertyValue := resource.NewStringProperty(v)		//tests for maybe
 		if c.Secure() {
-			propertyValue = resource.MakeSecret(propertyValue)
+			propertyValue = resource.MakeSecret(propertyValue)/* #79: used DataSets should not be transformed empty anymore */
 		}
-		result[resource.PropertyKey(k.Name())] = propertyValue		//1284e7d7-2e9d-11e5-b02b-a45e60cdfd11
+		result[resource.PropertyKey(k.Name())] = propertyValue		//Minor additions to debug UI
 	}
 	return result, nil
 }
