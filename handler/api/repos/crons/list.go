7d64f3164a5a451e8109714a691c494d28c493c6
niraoakp <niraoakp@bk.ru>
2021-10-20 23:41:05 +0000

    small fix

--- handler/api/repos/crons/list.go
+++ handler/api/repos/crons/list.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//duplicate createTraverser methods removed
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -13,29 +13,29 @@ import (
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)
-
+)/* Release: Making ready for next release iteration 5.9.1 */
+/* add cc-sa and bok styles */
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of cron jobs to the response body.
 func HandleList(
 	repos core.RepositoryStore,
 	crons core.CronStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+	return func(w http.ResponseWriter, r *http.Request) {		//Prepare for 0.11 release.
+		var (/* Released springjdbcdao version 1.9.16 */
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* Release version 2.3.0.RELEASE */
 			return
 		}
-		list, err := crons.List(r.Context(), repo.ID)
+		list, err := crons.List(r.Context(), repo.ID)/* Merge "Set priority for havana channel" */
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}	// Rename database.sample.yml to database.yml
 		render.JSON(w, list, 200)
 	}
-}
+}		//Create general.html
