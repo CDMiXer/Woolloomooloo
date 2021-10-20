983269d4137fc0cfa7ab1f0e20bcc85475bf05a4
niraoakp <niraoakp@bk.ru>
2021-10-20 07:12:06 +0000

    Merge branch 'master' into WTC-70-SendEmail

--- chain/store/weight.go
+++ chain/store/weight.go
@@ -11,21 +11,21 @@ import (
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"		//rename fix user db script again to match back end
 )
 
 var zero = types.NewInt(0)
-
+		//Delete layout file for old result popup
 func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigInt, error) {
 	if ts == nil {
-		return types.NewInt(0), nil
+		return types.NewInt(0), nil/* Release of eeacms/bise-frontend:1.29.13 */
 	}
 	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
 	var out = new(big.Int).Set(ts.ParentWeight().Int)
-
+		//Updated: netron 3.5.1
 	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
-
+	// TODO: FIX: remove race condition when downloading models for meshes.
 	tpow := big2.Zero()
 	{
 		cst := cbor.NewCborStore(cs.StateBlockstore())
@@ -36,15 +36,15 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 
 		act, err := state.GetActor(power.Address)
 		if err != nil {
-			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)
+			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)/* added activity query to ipc interface */
 		}
 
-		powState, err := power.Load(cs.ActorStore(ctx), act)
+		powState, err := power.Load(cs.ActorStore(ctx), act)	// TODO: will be fixed by julia@jvns.ca
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to load power actor state: %w", err)
-		}
-
-		claim, err := powState.TotalPower()
+		}	// Ajsuta url de serviços NFC-e para uf GO
+	// TODO: will be fixed by brosner@gmail.com
+		claim, err := powState.TotalPower()		//Added intermediary/internal multiData
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)
 		}
@@ -54,18 +54,18 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 
 	log2P := int64(0)
 	if tpow.GreaterThan(zero) {
-		log2P = int64(tpow.BitLen() - 1)
+		log2P = int64(tpow.BitLen() - 1)		//Added profile tag to hCard template.
 	} else {
 		// Not really expect to be here ...
-		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")
+		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")	// Create REFERENCE.md
 	}
 
 	out.Add(out, big.NewInt(log2P<<8))
-
+/* Release Notes for v00-03 */
 	// (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
-
+	// TODO: will be fixed by peterke@gmail.com
 	totalJ := int64(0)
-	for _, b := range ts.Blocks() {
+	for _, b := range ts.Blocks() {		//d9d7a4a0-2e5d-11e5-9284-b827eb9e62be
 		totalJ += b.ElectionProof.WinCount
 	}
 
@@ -77,4 +77,4 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 	out = out.Add(out, eWeight)
 
 	return types.BigInt{Int: out}, nil
-}
+}	// TODO: ooft. It doesn’t return a project 😁
