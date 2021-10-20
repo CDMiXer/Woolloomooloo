fe3a75eeb539342ada07c0a61ff880a19272518d
niraoakp <niraoakp@bk.ru>
2021-10-20 01:09:26 +0000

    ea0cddde-2e4b-11e5-9284-b827eb9e62be

--- tests/integration/query/step3/index.ts
+++ tests/integration/query/step3/index.ts
@@ -3,23 +3,23 @@
 import * as pulumi from "@pulumi/pulumi";
 
 // Step 3: Run a query during `pulumi query`.
-pulumi.runtime/* Merge "Release Notes 6.0 -- Networking -- LP1405477" */
+pulumi.runtime
     .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")
     .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)
     .all(async function(group) {
         const count = await group.count();
-        if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {
+        if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {/* Update _core18-security_takeover.html */
             throw Error(`Expected 2 registered resources, got ${count}`);
         }
-        console.log(group.key);
+        console.log(group.key);	// TODO: hacked by fjl@ethereum.org
         return (
-            group.key === "pulumi-nodejs:dynamic:Resource" ||/* Release new version 2.5.17: Minor bugfixes */
+            group.key === "pulumi-nodejs:dynamic:Resource" ||
             group.key === "pulumi:providers:pulumi-nodejs" ||
             group.key === "pulumi:pulumi:Stack"
         );
     })
-    .then(res => {
-        if (res !== true) {
+    .then(res => {		//1da14edc-2e60-11e5-9284-b827eb9e62be
+        if (res !== true) {	// Fixed segfault when new plot is created in case of new simulation.
             throw Error("Expected query to return dynamic resource, provider, and stack resource");
         }
     });
