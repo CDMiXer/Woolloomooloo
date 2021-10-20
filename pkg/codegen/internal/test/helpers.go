044b4a12a546f718847819e0e9feac7c1660867d
niraoakp <niraoakp@bk.ru>
2021-10-20 03:44:45 +0000

    Update ochre-splash.css

--- pkg/codegen/internal/test/helpers.go
+++ pkg/codegen/internal/test/helpers.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// dtor was missing
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -9,15 +9,15 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* sub services */
-// limitations under the License.		//Merge "Publish keystone loci images to DockerHub"
-/* Initial Release ( v-1.0 ) */
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package test
 
-import (/* Release 1.1.16 */
-	"encoding/json"		//Add SEO plugin and Google analytics
+import (
+	"encoding/json"
 	"io/ioutil"
-	"path/filepath"	// TODO: Merge branch 'master' into issue/629/select-tournament
+	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
@@ -26,10 +26,10 @@ import (/* Release 1.1.16 */
 
 // GenPkgSignature corresponds to the shape of the codegen GeneratePackage functions.
 type GenPkgSignature func(string, *schema.Package, map[string][]byte) (map[string][]byte, error)
-	// TODO: will be fixed by seth@sethvargo.com
-// GeneratePackageFilesFromSchema loads a schema and generates files using the provided GeneratePackage function./* Release of eeacms/forests-frontend:1.5 */
+
+// GeneratePackageFilesFromSchema loads a schema and generates files using the provided GeneratePackage function.
 func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSignature) (map[string][]byte, error) {
-	// Read in, decode, and import the schema./* Developer Guide is a more appropriate title than Release Notes. */
+	// Read in, decode, and import the schema.
 	schemaBytes, err := ioutil.ReadFile(schemaPath)
 	if err != nil {
 		return nil, err
@@ -37,16 +37,16 @@ func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSign
 
 	var pkgSpec schema.PackageSpec
 	err = json.Unmarshal(schemaBytes, &pkgSpec)
-	if err != nil {	// Fix incorrect path of fauria/lap
-		return nil, err		//Started working on the Kiln
+	if err != nil {
+		return nil, err
 	}
 
-	pkg, err := schema.ImportSpec(pkgSpec, nil)	// [-] BO: Ajax Confirmation / padding-left
-	if err != nil {/* Fixed THEMES.md formatting (again!). */
+	pkg, err := schema.ImportSpec(pkgSpec, nil)
+	if err != nil {
 		return nil, err
-	}	// TODO: 44e43db4-2e43-11e5-9284-b827eb9e62be
+	}
 
-	return genPackageFunc("test", pkg, nil)/* Release v1.2.0 with custom maps. */
+	return genPackageFunc("test", pkg, nil)
 }
 
 // LoadFiles loads the provided list of files from a directory.
