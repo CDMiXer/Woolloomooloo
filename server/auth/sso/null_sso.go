8656dc17a2ff892d2342bdea9b5e79edb9dfc25e
niraoakp <niraoakp@bk.ru>
2021-10-20 19:21:58 +0000

    Add unicode_literals future import

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -1,22 +1,22 @@
 package sso
-		//Grab windows fixes
+
 import (
 	"context"
 	"fmt"
-	"net/http"/* Lm52cXVhbi5vcmcK */
+	"net/http"
 
 	"github.com/argoproj/argo/server/auth/jws"
 )
 
-var NullSSO Interface = nullService{}	// TODO: hacked by mikeal.rogers@gmail.com
+var NullSSO Interface = nullService{}
 
 type nullService struct{}
-/* Update ChangeLog.md for Release 3.0.0 */
+
 func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
 	return nil, fmt.Errorf("not implemented")
 }
 
-func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {		//Fix a L5/L4 check that was done before mount was connected
+func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 }
 
