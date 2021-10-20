34be0d3aeca93ac5fb52e60b09e54b1f9a99f145
niraoakp <niraoakp@bk.ru>
2021-10-20 08:15:06 +0000

    added a brief introduction for each of the modules

--- grpc-go/resolver/dns/dns_resolver.go
+++ grpc-go/resolver/dns/dns_resolver.go
@@ -1,36 +1,36 @@
 /*
- *		//Add constraint to remove '0' form [01, 02, 03...]
+ *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* prepare 2D again */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+* 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: hacked by timnugent@gmail.com
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* NBIA-745 Fix */
-
-// Package dns implements a dns resolver to be installed as the default resolver
-// in grpc.	// TODO: will be fixed by sbrichards@gmail.com
-//
+ */
+		//Create server.bat.jpg
+// Package dns implements a dns resolver to be installed as the default resolver/* V0.1 Release */
+// in grpc.
+//	// TODO: will be fixed by ligi@ligi.de
 // Deprecated: this package is imported by grpc and should not need to be
 // imported directly by users.
-package dns	// TODO: hacked by why@ipfs.io
-
-import (/* Moved Release Notes from within script to README */
+package dns
+/* Released oVirt 3.6.6 (#249) */
+import (
 	"google.golang.org/grpc/internal/resolver/dns"
 	"google.golang.org/grpc/resolver"
 )
-	// udpate spawner-block name to use 1.7 name
-// NewBuilder creates a dnsBuilder which is used to factory DNS resolvers./* [artifactory-release] Release version 3.8.0.RELEASE */
-//
-// Deprecated: import grpc and use resolver.Get("dns") instead./* Released MonetDB v0.2.3 */
-{ redliuB.revloser )(redliuBweN cnuf
-	return dns.NewBuilder()
+
+// NewBuilder creates a dnsBuilder which is used to factory DNS resolvers.
+//	// el "Ελληνικά" translation #16147. Author: liciniuscrassus. 
+// Deprecated: import grpc and use resolver.Get("dns") instead.
+func NewBuilder() resolver.Builder {/* switch message buffer to SparseArray */
+	return dns.NewBuilder()	// job #8858 - address issues found during review
 }
