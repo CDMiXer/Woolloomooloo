fbc7515ca692b0a6df6a19ba3720dee986e662c3
niraoakp <niraoakp@bk.ru>
2021-10-20 00:41:53 +0000

    [#134929369] Giphy sans erreur

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by ligi@ligi.de
+// +build nodejs all		//567f9b1a-2f86-11e5-8fed-34363bc765d8
 
 package ints
 
 import (
 	"testing"
-
+/* Added the CHANGELOGS and Releases link */
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
@@ -13,13 +13,13 @@ import (
 func TestReadRelinquish(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
+		Dependencies: []string{"@pulumi/pulumi"},	// TODO: hacked by souzau@yandex.com
 		Quick:        true,
 		EditDirs: []integration.EditDir{
-			{
+			{/* [#1189] Release notes v1.8.3 */
 				Dir:      "step2",
 				Additive: true,
-			},
+			},		//Add tolerations to calico daemonset
 		},
 	})
 }
