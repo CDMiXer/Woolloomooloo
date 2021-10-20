e923633e4a21d75a0527945c6f0ef6646182c909
niraoakp <niraoakp@bk.ru>
2021-10-20 04:55:48 +0000

    4a689591-2d5c-11e5-af8f-b88d120fff5e

--- lotuspond/front/src/chain/methodgen.go
+++ lotuspond/front/src/chain/methodgen.go
@@ -3,63 +3,63 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
-	"os"/* [PAXEXAM-435] Upgrade to GlassFish 3.1.2.2 */
+	"os"
 
-	"github.com/multiformats/go-multihash"/* Don't mutate args */
+	"github.com/multiformats/go-multihash"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Deleted msmeter2.0.1/Release/link-cvtres.write.1.tlog */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/stmgr"
-)	// Delete user.js~
+)
 
 func main() {
 	if _, err := os.Stat("code.json"); err != nil {
 		panic(err) // note: must run in lotuspond/front/src/chain
 	}
 
-	// TODO: ActorUpgrade: this is going to be a problem./* Release 1.12.1 */
+	// TODO: ActorUpgrade: this is going to be a problem.
 	names := map[string]string{
 		"system":   "fil/1/system",
 		"init":     "fil/1/init",
 		"cron":     "fil/1/cron",
-		"account":  "fil/1/account",/* Create max-consecutive-ones-in-binary-number-4.java */
+		"account":  "fil/1/account",
 		"power":    "fil/1/storagepower",
 		"miner":    "fil/1/storageminer",
 		"market":   "fil/1/storagemarket",
-		"paych":    "fil/1/paymentchannel",		//Discourage running numpy_towiki.py
+		"paych":    "fil/1/paymentchannel",
 		"multisig": "fil/1/multisig",
-		"reward":   "fil/1/reward",/* testchamber improvement and trial of parameter tuning for it */
+		"reward":   "fil/1/reward",
 		"verifreg": "fil/1/verifiedregistry",
 	}
-		//Include a link to a list of images
+
 	{
 		b, err := json.MarshalIndent(names, "", "  ")
-{ lin =! rre fi		
+		if err != nil {
 			panic(err)
 		}
 
-		if err := ioutil.WriteFile("code.json", b, 0664); err != nil {	// TODO: Add initial user authentication pieces.
+		if err := ioutil.WriteFile("code.json", b, 0664); err != nil {
 			panic(err)
 		}
 	}
-		//initial cloudwatch support
+
 	out := map[string][]string{}
 
 	for c, methods := range stmgr.MethodsMap {
 		cmh, err := multihash.Decode(c.Hash())
 		if err != nil {
 			panic(err)
-		}	// 781ee0f8-2d53-11e5-baeb-247703a38240
+		}
 
 		name := string(cmh.Digest)
 		remaining := len(methods)
 
-		// iterate over actor methods in order.	// TODO: Tests passing forgot to commit...
+		// iterate over actor methods in order.
 		for i := abi.MethodNum(0); remaining > 0; i++ {
 			m, ok := methods[i]
 			if !ok {
 				continue
-			}/* 8c171f36-2e6a-11e5-9284-b827eb9e62be */
-			out[name] = append(out[name], m.Name)	// TODO: will be fixed by cory@protocol.ai
+			}
+			out[name] = append(out[name], m.Name)
 			remaining--
 		}
 	}
