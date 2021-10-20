2901e614f8b750dc6df6da0e72df01a15f695773
niraoakp <niraoakp@bk.ru>
2021-10-20 04:51:24 +0000

    Exercise 3.16

--- handler/api/repos/builds/deploys/list.go
+++ handler/api/repos/builds/deploys/list.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
-///* Update WebAppReleaseNotes.rst */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Add ruby 2.2 for travis */
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: update project images, remove magnific popup JS+CSS, fix nav on projects URL
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// 116e36be-2e67-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploys
-	// TODO: will be fixed by alessio@tendermint.com
+
 import (
 	"net/http"
-	// 4545e288-2e45-11e5-9284-b827eb9e62be
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
@@ -28,10 +28,10 @@ import (
 // list of build history to the response body.
 func HandleList(
 	repos core.RepositoryStore,
-	builds core.BuildStore,	// TODO: will be fixed by souzau@yandex.com
-) http.HandlerFunc {/* Add info attributes */
+	builds core.BuildStore,
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* b1d3962c-2e3e-11e5-9284-b827eb9e62be */
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
@@ -40,22 +40,22 @@ func HandleList(
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).	// added additional test case
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
 		}
 
 		results, err := builds.LatestDeploys(r.Context(), repo.ID)
-		if err != nil {/* fbd6c694-2e58-11e5-9284-b827eb9e62be */
+		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).	// TODO: Merge branch 'develop' into iss-hipcms-1009-investigate-permission-issues-in-cms
+			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace)./* Update WebAppReleaseNotes.rst */
+				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot list builds")		//fix(package): update commenting to version 1.0.4
+				Debugln("api: cannot list builds")
 		} else {
-			render.JSON(w, results, 200)/* add angry_hash as dependency */
+			render.JSON(w, results, 200)
 		}
 	}
 }
