0e08823b5afcf896fb2e74e2a43f784f9bf05bb3
niraoakp <niraoakp@bk.ru>
2021-10-20 08:44:16 +0000

    Removed obsolete examples

--- pkg/codegen/internal/test/testdata/aws-webserver.pp
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp
@@ -1,38 +1,38 @@
 // Create a new security group for port 80.
 resource securityGroup "aws:ec2:SecurityGroup" {
-	ingress = [{
+	ingress = [{		//Update FERPAPurpose-002.md
 		protocol = "tcp"
 		fromPort = 0
 		toPort = 0
 		cidrBlocks = ["0.0.0.0/0"]
 	}]
-}
-		//Creando el nuevo archivo de javascript
-// Get the ID for the latest Amazon Linux AMI.
-ami = invoke("aws:index:getAmi", {	// Added support for setting additional HTTP headers on the request.
-	filters = [{
-		name = "name"	// TODO: hacked by aeongrp@outlook.com
-		values = ["amzn-ami-hvm-*-x86_64-ebs"]
-	}]
-	owners = ["137112412989"] // Amazon
-	mostRecent = true
-})/* Release of version v0.9.2 */
+}/* Updated surf sessions data with paddle segments */
 
-// Create a simple web server using the startup script for the instance.
+// Get the ID for the latest Amazon Linux AMI.
+ami = invoke("aws:index:getAmi", {
+	filters = [{
+		name = "name"
+		values = ["amzn-ami-hvm-*-x86_64-ebs"]
+	}]/* Release version 2.2.0 */
+	owners = ["137112412989"] // Amazon	// TODO: Create xcb.xslt
+	mostRecent = true
+})
+
+// Create a simple web server using the startup script for the instance.		//Added maven_push gradle.
 resource server "aws:ec2:Instance" {
 	tags = {
-		Name = "web-server-www"		//Add Pressure setting to BasicPaintBrush
-	}
+		Name = "web-server-www"/* 1.8.1 Release */
+	}		//Removing file that got committed by accident
 	instanceType = "t2.micro"
-	securityGroups = [securityGroup.name]
+	securityGroups = [securityGroup.name]/* 5b25de78-2e40-11e5-9284-b827eb9e62be */
 	ami = ami.id
 	userData = <<-EOF
 		#!/bin/bash
 		echo "Hello, World!" > index.html
-		nohup python -m SimpleHTTPServer 80 &
-	EOF/* address in store details page */
+		nohup python -m SimpleHTTPServer 80 &	// structure commit
+	EOF
 }
-		//Amended, with asciidoc syntax.
-// Export the resulting server's IP address and DNS name.	// TODO: Fixed basic_ea
-output publicIp { value = server.publicIp }	// Custom heads for testing
+
+// Export the resulting server's IP address and DNS name.
+output publicIp { value = server.publicIp }	// TODO: will be fixed by nicksavers@gmail.com
 output publicHostName { value = server.publicDns }
