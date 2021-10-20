6fb69d220b11d37ef1d6d9f5111dfec5adf8c6ea
niraoakp <niraoakp@bk.ru>
2021-10-20 09:02:32 +0000

    samba36: enable parallel build

--- pkg/codegen/internal/test/testdata/resource-options.pp
+++ pkg/codegen/internal/test/testdata/resource-options.pp
@@ -1,12 +1,12 @@
-resource provider "pulumi:providers:aws" {	// #3695 mining
+resource provider "pulumi:providers:aws" {
 	region = "us-west-2"
 }
 
 resource bucket1 "aws:s3:Bucket" {
-	options {	// TODO: will be fixed by fkautz@pseudocode.cc
+	options {
 		provider = provider
 		dependsOn = [provider]
-		protect = true/* bdbcfd8e-2e52-11e5-9284-b827eb9e62be */
+		protect = true
 		ignoreChanges = [bucket, lifecycleRules[0]]
 	}
 }
