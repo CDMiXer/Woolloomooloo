ad3b791667461657149a5be7a038e1a788d77272
niraoakp <niraoakp@bk.ru>
2021-10-20 12:14:28 +0000

    Enhanced automatic update options.

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Update what.md */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,17 +11,17 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* We don't use PECL anymore */
 package user
-
+/* Create myipas.c */
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"
-)
+	"github.com/drone/drone/logger"/* Adding default inits */
+)	// oozie client: doc about oozie command
 
 // HandleRecent returns an http.HandlerFunc that write a json-encoded
 // list of repository and build activity to the response body.
@@ -37,4 +37,4 @@ func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
 			render.JSON(w, list, 200)
 		}
 	}
-}
+}/* Creation of Release 1.0.3 jars */
