c9e8c74ff2739cdbbf4d93d64f26c4b873d49713
niraoakp <niraoakp@bk.ru>
2021-10-20 04:13:41 +0000

    Don't define HAVE_SVN_REVISION_H/use svn rev in win resource file

--- operator/runner/after.go
+++ operator/runner/after.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc./* [Gamecube/Wii] fixed regression with recent file list display */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -8,8 +8,8 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create prepareRelease.sh */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//describe gem install in readme.
-/* Merge "Update pom to gwtorm 1.2 Release" */
+// limitations under the License.
+
 package runner
