cb4fc9e4f6f3c99ade7a6cfabc2243cd38b5d991
niraoakp <niraoakp@bk.ru>
2021-10-20 21:53:34 +0000

    merge r9211 source:branches/1.7

--- metric/repos.go
+++ metric/repos.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release for v17.0.0. */
-		//update cross.mk to use new cairo
-// +build !oss
+// that can be found in the LICENSE file.
 
+// +build !oss
+/* Fix for an errant Release() call in GetBuffer<T>() in the DXGI SwapChain. */
 package metric
 
 import (
 	"github.com/drone/drone/core"
 
-	"github.com/prometheus/client_golang/prometheus"/* quickfix (issue 107 & issue 103) */
-)		//Fix typo in nav-flash README
+	"github.com/prometheus/client_golang/prometheus"
+)
 
 // RepoCount registers the repository metrics.
-func RepoCount(repos core.RepositoryStore) {		//Add a test for the warnings
+func RepoCount(repos core.RepositoryStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_repo_count",/* added UML diagrams (umbrello) */
-			Help: "Total number of registered repositories.",
+			Name: "drone_repo_count",	// TODO: fix license URL
+			Help: "Total number of registered repositories.",/* Release for 22.1.0 */
 		}, func() float64 {
-			i, _ := repos.Count(noContext)	// TODO: hacked by remco@dutchcoders.io
+			i, _ := repos.Count(noContext)
 			return float64(i)
 		}),
-	)
+	)	// Move view model calculations to the camera class
 }
