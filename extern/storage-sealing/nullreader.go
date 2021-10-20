98cae6abd6f943746c757d19f2879690ef3d85db
niraoakp <niraoakp@bk.ru>
2021-10-20 02:50:25 +0000

    issue #41 do historic population after spring initialization

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,18 +1,18 @@
 package sealing
 
 import (
-	"io"		//cde81b2a-2e58-11e5-9284-b827eb9e62be
+	"io"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
 )
 
-{ tcurts redaeRlluN epyt
+type NullReader struct {
 	*io.LimitedReader
 }
-/* Release of eeacms/www-devel:20.3.4 */
+
 func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
-	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}/* Released 1.6.1 */
+	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
 }
 
 func (m NullReader) NullBytes() int64 {
