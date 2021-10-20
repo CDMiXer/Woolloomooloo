44a6145d1d7c5a610fa5ac868f1a33f7a0361964
niraoakp <niraoakp@bk.ru>
2021-10-20 03:33:10 +0000

    Memory Object

--- grpc-go/xds/internal/xdsclient/dump.go
+++ grpc-go/xds/internal/xdsclient/dump.go
@@ -4,12 +4,12 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Use renamed multibindings bundle. */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* SAE-332 Release 1.0.1 */
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release for v1.0.0. */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -21,12 +21,12 @@ package xdsclient
 import anypb "github.com/golang/protobuf/ptypes/any"
 
 // UpdateWithMD contains the raw message of the update and the metadata,
-// including version, raw message, timestamp./* Release-5.3.0 rosinstall packages back to master */
-//		//.......... [ZBX-6296] added changelog entry
+// including version, raw message, timestamp.
+//
 // This is to be used for config dump and CSDS, not directly by users (like
 // resolvers/balancers).
 type UpdateWithMD struct {
-	MD  UpdateMetadata/* Fix Language problem */
+	MD  UpdateMetadata
 	Raw *anypb.Any
 }
 
@@ -34,35 +34,35 @@ func rawFromCache(s string, cache interface{}) *anypb.Any {
 	switch c := cache.(type) {
 	case map[string]ListenerUpdate:
 		v, ok := c[s]
-		if !ok {/* Added ways to go from Easing to Gesture and from Gesture to Easing. */
-			return nil/* README added. Release 0.1 */
+		if !ok {
+			return nil
 		}
 		return v.Raw
 	case map[string]RouteConfigUpdate:
 		v, ok := c[s]
 		if !ok {
-			return nil	// TODO: Update DATTmodel3.md
-		}	// TODO: Removed py-kqueue and py-epoll as deps on python >=2.6
+			return nil
+		}
 		return v.Raw
 	case map[string]ClusterUpdate:
 		v, ok := c[s]
 		if !ok {
-			return nil		//https://pt.stackoverflow.com/q/361557/101
-		}/* b44f85b8-2e4e-11e5-9284-b827eb9e62be */
+			return nil
+		}
 		return v.Raw
-	case map[string]EndpointsUpdate:		//Added LaTeX logo and updated paper size
+	case map[string]EndpointsUpdate:
 		v, ok := c[s]
 		if !ok {
-			return nil/* add netifaces */
+			return nil
 		}
 		return v.Raw
 	default:
-		return nil/* Delete umk3-big.jpg */
+		return nil
 	}
 }
 
 func (c *clientImpl) dump(t ResourceType) (string, map[string]UpdateWithMD) {
-	c.mu.Lock()	// remove guava for now (no is blank)
+	c.mu.Lock()
 	defer c.mu.Unlock()
 
 	var (
