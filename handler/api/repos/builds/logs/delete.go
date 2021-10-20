c45e2b8edf1c7cc6ac87086084a5485b3828d509
niraoakp <niraoakp@bk.ru>
2021-10-20 08:52:25 +0000

    add interface to endpoint for allow ccapability add Decorator

--- handler/api/repos/builds/logs/delete.go
+++ handler/api/repos/builds/logs/delete.go
@@ -8,12 +8,12 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package logs
-
+/* Update .tmux.conf vi selection bindings */
 import (
 	"net/http"
 	"strconv"
@@ -26,14 +26,14 @@ import (
 
 // HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the logs.
-func HandleDelete(
+func HandleDelete(	// Fix: failing instructions.
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
 	steps core.StepStore,
 	logs core.LogStore,
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+) http.HandlerFunc {/* CHANGE: Added info to make Photos top-level for those that need it */
+	return func(w http.ResponseWriter, r *http.Request) {/* Merge "[Release] Webkit2-efl-123997_0.11.40" into tizen_2.1 */
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
@@ -44,35 +44,35 @@ func HandleDelete(
 			return
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
-		if err != nil {
-			render.BadRequest(w, err)
+		if err != nil {/* Release 1.0.58 */
+			render.BadRequest(w, err)		//Tweak to CHANGELOG
 			return
 		}
 		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
-		if err != nil {
+		if err != nil {/* +Releases added and first public release committed. */
 			render.BadRequest(w, err)
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return		//6777d228-2e75-11e5-9284-b827eb9e62be
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}/* ndb - merge 7.0.9a */
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
-		if err != nil {
+		if err != nil {/* Release 1.10.5 and  2.1.0 */
 			render.NotFound(w, err)
-			return
+			return	// TODO: will be fixed by davidad@alum.mit.edu
 		}
 		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}		//826f657a-2e5f-11e5-9284-b827eb9e62be
 		err = logs.Delete(r.Context(), step.ID)
 		if err != nil {
 			render.InternalError(w, err)
