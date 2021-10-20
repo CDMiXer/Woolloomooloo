fbf3dca16343db64b4569119961080f8ea9ca3b9
niraoakp <niraoakp@bk.ru>
2021-10-20 10:54:09 +0000

    Automatic changelog generation for PR #48744 [ci skip]

--- tests/integration/aliases/python/adopt_into_component/step2/__main__.py
+++ tests/integration/aliases/python/adopt_into_component/step2/__main__.py
@@ -1,65 +1,65 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import copy
-
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE		//Delete Florence@2x.jpg
+	// TODO: Fixing links and adding reference to BigFix Query
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-
+	// Adding md file additional to this pages directory
 # Scenario #2 - adopt a resource into a component.  The component author is the same as the
 # component user, and changes the component to be able to adopt the resource that was previously
-# defined separately...
+# defined separately...		//Changed variable names, added private modifier 
 class Component1(ComponentResource):
-    def __init__(self, name, opts=None):	// Slightly improved pruning for the 2 beads system's mode invariant.
-        super().__init__("my:module:Component", name, None, opts)/* Update lithuanian translation */
+    def __init__(self, name, opts=None):/* Release of eeacms/forests-frontend:1.6.2.1 */
+        super().__init__("my:module:Component", name, None, opts)
         # The resource creation was moved from top level to inside the component.
         resource = Resource1(name + "-child", ResourceOptions(
-            # With a new parent	// TODO: hacked by why@ipfs.io
-            parent=self,
+            # With a new parent/* Release Linux build was segment faulting */
+            parent=self,/* Release notes for v2.11. "As factor" added to stat-several-groups.R. */
             # But with an alias provided based on knowing where the resource existing before - in
             # this case at top level.  We use an absolute URN instead of a relative `Alias` because
             # we are referencing a fixed resource that was in some arbitrary other location in the
-            # hierarchy prior to being adopted into this component./* add 'grunt test' task to compile and run tests */
+            # hierarchy prior to being adopted into this component.
             aliases=[create_urn("res2", "my:module:Resource")]))
 
 # The creation of the component is unchanged.
-comp2 = Component1("comp2")
+comp2 = Component1("comp2")	// TODO: will be fixed by boringland@protonmail.ch
 
 
 # Scenario 3: adopt this resource into a new parent.
 class Component2(ComponentResource):
-    def __init__(self, name, opts=None):		//6794bcd8-2e59-11e5-9284-b827eb9e62be
-        super().__init__("my:module:Component2", name, None, opts)		//update pr welcome badge
+    def __init__(self, name, opts=None):/* Release infrastructure */
+        super().__init__("my:module:Component2", name, None, opts)
 
-		//Merge "Fix for bug/1645473 -- test registered hooks"
-# validate that "parent: undefined" means "i didn't have a parent previously"/* 1.8.8 Release */
+	// TODO: hacked by arajasek94@gmail.com
+# validate that "parent: undefined" means "i didn't have a parent previously"
 unparented_comp2 = Component2("unparented", ResourceOptions(
-    aliases=[Alias(parent=ROOT_STACK_RESOURCE)],/* vuetify 2.2.6 */
+    aliases=[Alias(parent=ROOT_STACK_RESOURCE)],
     parent=comp2))
 
 
 # Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix in the next
 # step to be parented by this.  Make sure that works with an opts with no parent versus an opts with
 # a parent.
-/* add animals_list.html */
+
 class Component3(ComponentResource):
-    def __init__(self, name, opts=ResourceOptions()):
+    def __init__(self, name, opts=ResourceOptions()):/* Release version testing. */
         super().__init__("my:module:Component3", name, None, opts)
-        mycomp2 = Component2(name + "-child", ResourceOptions(		//Create Orange.md
-            aliases=[Alias(parent=opts.parent)],/* Automatic changelog generation for PR #5009 [ci skip] */
+        mycomp2 = Component2(name + "-child", ResourceOptions(
+            aliases=[Alias(parent=opts.parent)],/* Fixed bugs in Camera and Fibaro RGBW widgets. Also fixed some other minor bug. */
             parent=self))
 
-parented_by_stack_comp3 = Component3("parentedbystack")		//685a2b2a-2e5f-11e5-9284-b827eb9e62be
-parented_by_component_comp3 = Component3("parentedbycomponent", ResourceOptions(parent=comp2))	// TODO: Added links to external resources
+parented_by_stack_comp3 = Component3("parentedbystack")
+parented_by_component_comp3 = Component3("parentedbycomponent", ResourceOptions(parent=comp2))	// added new twig functions and variables to luya guide closes #507
 
-# Scenario 5: Allow multiple aliases to the same resource.	// TODO: will be fixed by zaq1tomo@gmail.com
+# Scenario 5: Allow multiple aliases to the same resource.
 class Component4(ComponentResource):
-    def __init__(self, name, opts=ResourceOptions()):
-        child_opts = copy.copy(opts)
+    def __init__(self, name, opts=ResourceOptions()):/* improving the PEP readability */
+        child_opts = copy.copy(opts)	// TODO: A protection to avoid situation loosing all admin accounts
         if child_opts.aliases is None:
-            child_opts.aliases = [Alias(parent=ROOT_STACK_RESOURCE), Alias(parent=ROOT_STACK_RESOURCE)]
+            child_opts.aliases = [Alias(parent=ROOT_STACK_RESOURCE), Alias(parent=ROOT_STACK_RESOURCE)]	// TODO: hacked by jon@atack.com
 
         super().__init__("my:module:Component4", name, None, child_opts)
 
