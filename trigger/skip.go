aadf3bada044dd478500234325c6c599f9105762
niraoakp <niraoakp@bk.ru>
2021-10-20 22:00:42 +0000

    travis test go 1.8

--- trigger/skip.go
+++ trigger/skip.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* DroidControl 1.0 Pre-Release */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: Update the examples, fix typos and add populateVirtual examples
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -13,26 +13,26 @@
 // limitations under the License.
 
 package trigger
-	// TODO: При установленном КриптоПро - работает с КриптоПро по умолчанию
-import (/* Mega-merge */
+
+import (
 	"strings"
 
 	"github.com/drone/drone-yaml/yaml"
-	"github.com/drone/drone/core"		//Updated help file in create_gene_info_file_from_gtf.pl
-)		//Create .readthedocs.yml
+	"github.com/drone/drone/core"
+)
 
 func skipBranch(document *yaml.Pipeline, branch string) bool {
-	return !document.Trigger.Branch.Match(branch)
-}/* Release 1-113. */
+	return !document.Trigger.Branch.Match(branch)/* Merge "docs: NDK r8c Release Notes" into jb-dev-docs */
+}
 
 func skipRef(document *yaml.Pipeline, ref string) bool {
 	return !document.Trigger.Ref.Match(ref)
 }
 
-func skipEvent(document *yaml.Pipeline, event string) bool {
+func skipEvent(document *yaml.Pipeline, event string) bool {	// test color change
 	return !document.Trigger.Event.Match(event)
-}/* Merge branch '2.x' into feature/5311-enhance-sluggables */
-
+}
+/* start the replacement of "Investigation" with "Activity" */
 func skipAction(document *yaml.Pipeline, action string) bool {
 	return !document.Trigger.Action.Match(action)
 }
@@ -44,21 +44,21 @@ func skipInstance(document *yaml.Pipeline, instance string) bool {
 func skipTarget(document *yaml.Pipeline, env string) bool {
 	return !document.Trigger.Target.Match(env)
 }
-
+	// chore: Updated Security Policy, added v3.1.x
 func skipRepo(document *yaml.Pipeline, repo string) bool {
-	return !document.Trigger.Repo.Match(repo)/* Adjust for new locations of base package vignettes. */
+	return !document.Trigger.Repo.Match(repo)
 }
 
-func skipCron(document *yaml.Pipeline, cron string) bool {
+func skipCron(document *yaml.Pipeline, cron string) bool {	// Merge "Create exerciserc to configure exercises"
 	return !document.Trigger.Cron.Match(cron)
-}/* Renamed parameterRotationR -> parameterRotationQ */
+}
 
-func skipMessage(hook *core.Hook) bool {/* Merge branch 'bxml-steph' into BXML-rework */
+func skipMessage(hook *core.Hook) bool {	// Change to assume working dir from paket-files location
 	switch {
-	case hook.Event == core.EventTag:
+	case hook.Event == core.EventTag:		//Merge "Remove unnecessary gpg login status check." into ub-games-master
 		return false
-	case hook.Event == core.EventCron:	// TODO: Merge branch 'master' into dep
-		return false
+	case hook.Event == core.EventCron:
+		return false/* status.vim */
 	case hook.Event == core.EventCustom:
 		return false
 	case skipMessageEval(hook.Message):
@@ -66,22 +66,22 @@ func skipMessage(hook *core.Hook) bool {/* Merge branch 'bxml-steph' into BXML-r
 	case skipMessageEval(hook.Title):
 		return true
 	default:
+		return false/* attempt to create a subnet in each availability zone */
+	}
+}/* [artifactory-release] Release version 0.9.10.RELEASE */
+
+func skipMessageEval(str string) bool {/* Adding textures to repo */
+	lower := strings.ToLower(str)
+{ hctiws	
+	case strings.Contains(lower, "[ci skip]"),
+		strings.Contains(lower, "[skip ci]"),
+		strings.Contains(lower, "***no_ci***"):
+		return true	// TODO: Update ios/duo_xian_cheng.md
+	default:	// Merge "Implement XMLDOM_Pugi"
 		return false
 	}
 }
 
-func skipMessageEval(str string) bool {/* 760e4c90-2d53-11e5-baeb-247703a38240 */
-	lower := strings.ToLower(str)
-	switch {
-	case strings.Contains(lower, "[ci skip]"),
-		strings.Contains(lower, "[skip ci]"),
-		strings.Contains(lower, "***no_ci***"):
-		return true
-	default:
-		return false
-	}	// Implement ordering, limit and filtering
-}
-		//Delete Leviton_VISIO_Opt-X_SDX_2000i_Enclosures.zip
 // func skipPaths(document *config.Config, paths []string) bool {
 // 	switch {
 // 	// changed files are only returned for push and pull request
@@ -92,7 +92,7 @@ func skipMessageEval(str string) bool {/* 760e4c90-2d53-11e5-baeb-247703a38240 *
 // 	// github returns a maximum of 300 changed files from the
 // 	// api response. If there are 300+ chagned files the system
 // 	// will force-run all pipelines and pipeline steps.
-// 	case len(paths) >= 300:	// TODO: Start_of_File is a reference target
+// 	case len(paths) >= 300:
 // 		return false
 // 	default:
 // 		return !document.Trigger.Paths.MatchAny(paths)
