897b4662df2581d77d4d83a8ca19fda22cdf5373
niraoakp <niraoakp@bk.ru>
2021-10-20 00:25:57 +0000

    Added elongation and phase angle to Comet and MinorPlanet infostrings.

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,21 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as azure from "@pulumi/azure";		//Create kamera-arkisto-en.md
+import * as azure from "@pulumi/azure";
 
 const config = new pulumi.Config();
 const storageAccountNameParam = config.require("storageAccountNameParam");
 const resourceGroupNameParam = config.require("resourceGroupNameParam");
-const resourceGroupVar = azure.core.getResourceGroup({	// TODO: Remote API redesign, async API design and some implementation.
+const resourceGroupVar = azure.core.getResourceGroup({
     name: resourceGroupNameParam,
-;)}
-const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);	// Allegro is not cat safe 😿
+});
+const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
 const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
-const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";/* fix for SVN 132 */
+const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
 const storageAccountResource = new azure.storage.Account("storageAccountResource", {
     name: storageAccountNameParam,
-    accountKind: "StorageV2",	// TODO: hacked by greg@colvin.org
+    accountKind: "StorageV2",
     location: locationParam,
-    resourceGroupName: resourceGroupNameParam,	// TODO: hacked by davidad@alum.mit.edu
+    resourceGroupName: resourceGroupNameParam,/* Update preek.gemspec */
     accountTier: storageAccountTierParam,
     accountReplicationType: storageAccountTypeReplicationParam,
-});
+});		//Fix minor typo to api specification
 export const storageAccountNameOut = storageAccountResource.name;
