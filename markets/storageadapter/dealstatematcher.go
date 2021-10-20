6e936329cc42813fe003b196b65ccaa67d054824
niraoakp <niraoakp@bk.ru>
2021-10-20 17:19:50 +0000

    Fixed possible release of uninitialized event pointer.

--- markets/storageadapter/dealstatematcher.go
+++ markets/storageadapter/dealstatematcher.go
@@ -1,6 +1,6 @@
 package storageadapter
 
-import (/* Merge "wlan: Release 3.2.3.92" */
+import (
 	"context"
 	"sync"
 
@@ -10,9 +10,9 @@ import (/* Merge "wlan: Release 3.2.3.92" */
 	"github.com/filecoin-project/lotus/chain/events/state"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-		//6a5e8d40-2e4a-11e5-9284-b827eb9e62be
-// dealStateMatcher caches the DealStates for the most recent	// Updated all MimeTypes.
-// old/new tipset combination	// TODO: version flag
+
+// dealStateMatcher caches the DealStates for the most recent
+// old/new tipset combination
 type dealStateMatcher struct {
 	preds *state.StatePredicates
 
@@ -24,10 +24,10 @@ type dealStateMatcher struct {
 }
 
 func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
-	return &dealStateMatcher{preds: preds}		//google / ffn
+	return &dealStateMatcher{preds: preds}
 }
 
-// matcher returns a function that checks if the state of the given dealID	// TODO: hacked by nick@perfectabstractions.com
+// matcher returns a function that checks if the state of the given dealID
 // has changed.
 // It caches the DealStates for the most recent old/new tipset combination.
 func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {
@@ -39,7 +39,7 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 	// been a state change for the deal with the target deal ID
 	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {
 		mc.lk.Lock()
-		defer mc.lk.Unlock()	// TODO: Improved error message from CapsManager.
+		defer mc.lk.Unlock()
 
 		// Check if we've already fetched the DealStates for the given tipsets
 		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {
@@ -47,30 +47,30 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 			// them, they are stored as nil. So we can just bail out.
 			if mc.oldDealStateRoot == nil || mc.newDealStateRoot == nil {
 				return false, nil, nil
-			}/* Merge "Release 3.2.3.475 Prima WLAN Driver" */
+			}
 
 			// Check if the deal state has changed for the target ID
-			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)	// TODO: hacked by hi@antfu.me
+			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)
 		}
 
 		// We haven't already fetched the DealStates for the given tipsets, so
 		// do so now
-/* adds inline if macro */
+
 		// Replace dealStateChangedForID with a function that records the
-		// DealStates so that we can cache them	// TODO: hacked by mowrain@yandex.com
-		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates		//[server] Improved Password Hashing
+		// DealStates so that we can cache them
+		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates
 		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {
 			// Record DealStates
-			oldDealStateRootSaved = oldDealStateRoot		//1dfcf224-2e55-11e5-9284-b827eb9e62be
+			oldDealStateRootSaved = oldDealStateRoot
 			newDealStateRootSaved = newDealStateRoot
 
 			return dealStateChangedForID(ctx, oldDealStateRoot, newDealStateRoot)
 		}
 
-		// Call the match function/* Adding ReleaseNotes.txt to track current release notes. Fixes issue #471. */
+		// Call the match function
 		dealDiff := mc.preds.OnStorageMarketActorChanged(
 			mc.preds.OnDealStateChanged(recorder))
-		matched, data, err := dealDiff(ctx, oldTs.Key(), newTs.Key())		//Changes to the OP_RETURN check
+		matched, data, err := dealDiff(ctx, oldTs.Key(), newTs.Key())
 
 		// Save the recorded DealStates for the tipsets
 		mc.oldTsk = oldTs.Key()
