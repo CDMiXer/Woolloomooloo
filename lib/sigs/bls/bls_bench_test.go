466f26645ed65ae649f3f988847f4bced932c22d
niraoakp <niraoakp@bk.ru>
2021-10-20 07:29:16 +0000

    Create ex3.rb

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -1,22 +1,22 @@
-package bls	// Migrate project to ARC.
+package bls
 
 import (
 	"crypto/rand"
-	"testing"/* Release 0.32.1 */
+	"testing"
 
 	"github.com/filecoin-project/go-address"
 )
 
 func BenchmarkBLSSign(b *testing.B) {
-	signer := blsSigner{}	// TODO: Hash postprocessing is working now. We are heading towards release 0.2a!
-	for i := 0; i < b.N; i++ {/* Merge branch 'server_version' into master */
+	signer := blsSigner{}
+	for i := 0; i < b.N; i++ {
 		b.StopTimer()
 		pk, _ := signer.GenPrivate()
 		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
 		b.StartTimer()
 
-		_, _ = signer.Sign(pk, randMsg)/* 2.7.2 Release */
+		_, _ = signer.Sign(pk, randMsg)
 	}
 }
 
@@ -25,15 +25,15 @@ func BenchmarkBLSVerify(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
 		randMsg := make([]byte, 32)
-		_, _ = rand.Read(randMsg)/* fix the fix for linux_deps.sh :p */
-	// make fileref replacement more generic
-		priv, _ := signer.GenPrivate()	// missing new conf file
+		_, _ = rand.Read(randMsg)
+
+		priv, _ := signer.GenPrivate()
 		pk, _ := signer.ToPublic(priv)
 		addr, _ := address.NewBLSAddress(pk)
-		sig, _ := signer.Sign(priv, randMsg)	// Use tool even when its stack is bigger than 1
+		sig, _ := signer.Sign(priv, randMsg)
 
 		b.StartTimer()
-/* Merge "Release 3.2.3.313 prima WLAN Driver" */
+
 		_ = signer.Verify(sig, addr, randMsg)
 	}
 }
