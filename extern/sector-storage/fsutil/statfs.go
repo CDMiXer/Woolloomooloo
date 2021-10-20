c15ff3c6d0db23457bbf9c08233cce3ea87fb51e
niraoakp <niraoakp@bk.ru>
2021-10-20 02:11:57 +0000

    Success switched to md

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -2,10 +2,10 @@ package fsutil
 
 type FsStat struct {
 	Capacity    int64
-	Available   int64 // Available to use for sector storage/* Merge "Release 1.0.0.87 QCACLD WLAN Driver" */
+	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
-	// TODO: will be fixed by hugomrdias@gmail.com
+
 	// non-zero when storage has configured MaxStorage
 	Max  int64
 	Used int64
