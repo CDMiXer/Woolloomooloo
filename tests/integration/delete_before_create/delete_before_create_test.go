55ce011050aaf528d479f2dba5a6efaaae9683d8
niraoakp <niraoakp@bk.ru>
2021-10-20 03:28:50 +0000

    Add more sections to README

--- tests/integration/delete_before_create/delete_before_create_test.go
+++ tests/integration/delete_before_create/delete_before_create_test.go
@@ -2,41 +2,41 @@
 // +build nodejs all
 
 package ints
-		//Generated site for typescript-generator-spring 2.28.810
+
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// TestDeleteBeforeCreate tests a few different operational modes for/* Delete Release.rar */
-// replacements done by deleting before creating.		//Adds support for custom properties
+// TestDeleteBeforeCreate tests a few different operational modes for
+// replacements done by deleting before creating.
 func TestDeleteBeforeCreate(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Release 1-90. */
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
-		EditDirs: []integration.EditDir{		//[sprint 1] prueba alfonso
+		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
 			},
-			{/* only using exp of tag dict dist for unseen words */
+			{
 				Dir:      "step3",
-				Additive: true,	// TODO: Add test for AwtThread leak
+				Additive: true,
 			},
 			{
 				Dir:      "step4",
 				Additive: true,
-			},/* 19b16c30-4b19-11e5-8d8e-6c40088e03e4 */
+			},
 			{
-				Dir:      "step5",/* Updated to Post Release Version Number 1.31 */
+				Dir:      "step5",
 				Additive: true,
 			},
 			{
-				Dir:      "step6",/* Fixed gust */
+				Dir:      "step6",
 				Additive: true,
 			},
 		},
 	})
-}	// TODO: Renamed Toolchain in ToolchainWriter
+}
