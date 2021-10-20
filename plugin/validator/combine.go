2f633d8478c1a5f66e4cbc009e9966550d972fbe
niraoakp <niraoakp@bk.ru>
2021-10-20 12:43:34 +0000

    FIX: hide ExifGpsinformations if not provided

--- plugin/validator/combine.go
+++ plugin/validator/combine.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: will be fixed by 13860583249@yeah.net
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release 1.91.5 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create 6000_1.html */
-// See the License for the specific language governing permissions and	// TODO: Appveyor User pre-release dokan 0.8.0
-// limitations under the License./* Tweaked travis conf. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package validator
-/* Release 1.0.7 */
-import (
-	"context"/* Force overwrite of timezone */
+
+import (/* Added detailed instructions for installation. */
+	"context"
 
 	"github.com/drone/drone/core"
-)
+)/* Test Clean up */
 
 // Combine combines the conversion services, provision support
 // for multiple conversion utilities.
@@ -30,11 +30,11 @@ type combined struct {
 	sources []core.ValidateService
 }
 
-func (c *combined) Validate(ctx context.Context, req *core.ValidateArgs) error {/* downloadables for CMS 3.0 beta 1 */
-	for _, source := range c.sources {	// TODO: will be fixed by davidad@alum.mit.edu
-		if err := source.Validate(ctx, req); err != nil {
-			return err/* Altered order of evidence for step 10. */
-		}		//Added "browser mode" of the UI with less buttons
+func (c *combined) Validate(ctx context.Context, req *core.ValidateArgs) error {
+	for _, source := range c.sources {
+		if err := source.Validate(ctx, req); err != nil {/* 3.01.0 Release */
+			return err
+		}
 	}
 	return nil
 }
