2211dbbf0ddb4af9165753f4a2196eea53c0961b
niraoakp <niraoakp@bk.ru>
2021-10-20 06:59:59 +0000

    24f183e6-2ece-11e5-905b-74de2bd44bed

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by earlephilhower@yahoo.com
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* added deivid's handle */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// c5209a4a-2e52-11e5-9284-b827eb9e62be
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* 147531b4-2e58-11e5-9284-b827eb9e62be */
 // limitations under the License.
 
 package runner
