371d72ff34dbca2a4a0619ba7143f736698ee6a5
niraoakp <niraoakp@bk.ru>
2021-10-20 11:48:47 +0000

    [ASC] DDBDATA-1858 Geokoordinatenmapping

--- tests/integration/empty/gorun_main/gorun_main_src/main.go
+++ tests/integration/empty/gorun_main/gorun_main_src/main.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package main
-/* Fix HsqlJdbcLockProviderIntegrationTest */
+
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)/* Wheat_test_Stats_for_Release_notes */
-	// Cadastro de Clientes e melhoria na tela de vendas concluídos.
+)
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		return nil	// typo removal
+		return nil
 	})
 }
