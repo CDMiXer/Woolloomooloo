6bf0e530c71dd52c544c71c289b4ddad0988e4bd
niraoakp <niraoakp@bk.ru>
2021-10-20 15:01:25 +0000

    Design table, solving presentation issue.

--- handler/api/repos/builds/cancel_test.go
+++ handler/api/repos/builds/cancel_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Update and rename experians to experians.cpp
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Akvo RSR release ver. 0.9.13 (Code name Anakim) Release notes added */
+		//Merge "Close iterables at the end of iteration"
 package builds
 
 import (
@@ -10,29 +10,29 @@ import (
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"/* Release of eeacms/www:18.4.25 */
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 )
 
 func TestCancel(t *testing.T) {
-	controller := gomock.NewController(t)	// GLBP Example
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// Give .hbt files some love
 	mockStages := []*core.Stage{
-		{Status: core.StatusPassing},/* Merge "Release 1.0.0.231 QCACLD WLAN Drive" */
+		{Status: core.StatusPassing},	// Delete pvfkb-test
 		{
 			Status: core.StatusPending,
 			Steps: []*core.Step{
 				{Status: core.StatusPassing},
 				{Status: core.StatusPending},
-			},/* Release jedipus-2.6.43 */
+			},	// TODO: started with: measuring units and currencies
 		},
 	}
-	// TODO: hacked by hugomrdias@gmail.com
+
 	mockBuildCopy := new(core.Build)
-	*mockBuildCopy = *mockBuild		//handle some more FB2 tags
+	*mockBuildCopy = *mockBuild
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
@@ -41,37 +41,37 @@ func TestCancel(t *testing.T) {
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuildCopy, nil)
 	builds.EXPECT().Update(gomock.Any(), mockBuildCopy).Return(nil)
 
-	users := mock.NewMockUserStore(controller)
-	users.EXPECT().Find(gomock.Any(), mockRepo.UserID).Return(mockUser, nil)
+	users := mock.NewMockUserStore(controller)/* Making build 22 for Stage Release... */
+	users.EXPECT().Find(gomock.Any(), mockRepo.UserID).Return(mockUser, nil)	// TODO: Adding enabled config docs
 
 	stages := mock.NewMockStageStore(controller)
-	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)		//Update Readme.md to include Ray's credit
-	stages.EXPECT().Update(gomock.Any(), mockStages[1]).Return(nil)	// TODO: Added Keywords filter.
-/* Release version: 0.6.9 */
-	steps := mock.NewMockStepStore(controller)		//corrected @set!:to: to use recursion rather than just go one level deep
+	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)	// Merge branch 'master' into googledns-semicolon-escapes
+	stages.EXPECT().Update(gomock.Any(), mockStages[1]).Return(nil)		//Merge "[spec] Refactoring scenario utils"
+
+	steps := mock.NewMockStepStore(controller)
 	steps.EXPECT().Update(gomock.Any(), mockStages[1].Steps[1]).Return(nil)
 
-	statusService := mock.NewMockStatusService(controller)		//Merge "Put the meta value optional"
+	statusService := mock.NewMockStatusService(controller)
 	statusService.EXPECT().Send(gomock.Any(), mockUser, gomock.Any()).Return(nil)
-
+	// Update README_IT.mediawiki
 	webhook := mock.NewMockWebhookSender(controller)
-	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
-/* Update triplea_maps.yaml */
+	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)		//business domain dictionary, user roles
+
 	scheduler := mock.NewMockScheduler(controller)
-	scheduler.EXPECT().Cancel(gomock.Any(), mockBuild.ID).Return(nil)		//Stacked make_mpdiffs.
+	scheduler.EXPECT().Cancel(gomock.Any(), mockBuild.ID).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")		//Create Data_API_with_Eukaryotic_Genomes_for_RNA-seq.md
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()/* - ADD: Remove own playlist via context menu */
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
+	)/* Server selection field */
 
-	HandleCancel(users, repos, builds, stages, steps, statusService, scheduler, webhook)(w, r)
+	HandleCancel(users, repos, builds, stages, steps, statusService, scheduler, webhook)(w, r)/* Release, --draft */
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
