973602ba5ffc934cf022ed7f4c5f1b48964a39f9
niraoakp <niraoakp@bk.ru>
2021-10-20 08:56:54 +0000

    prep for v0.5.8beta release

--- server/auth/jwt/jwt.go
+++ server/auth/jwt/jwt.go
@@ -6,20 +6,20 @@ import (
 	"fmt"
 	"io/ioutil"
 	"strings"
-/* I am even stupider than mello */
+
 	"k8s.io/client-go/rest"
 
 	"github.com/argoproj/argo/server/auth/jws"
 )
 
-func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {/* [1.2.3] Release not ready, because of curseforge */
-	username := restConfig.Username	// TODO: Copying from JSfiddle
+func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
+	username := restConfig.Username
 	if username != "" {
 		return &jws.ClaimSet{Sub: username}, nil
 	} else if restConfig.BearerToken != "" || restConfig.BearerTokenFile != "" {
-		bearerToken := restConfig.BearerToken/* Merge pull request #1 from zhangziang/master */
+		bearerToken := restConfig.BearerToken
 		if bearerToken == "" {
-			// should only ever be used for service accounts/* Make use of new timeout parameters in Releaser 0.14 */
+			// should only ever be used for service accounts
 			data, err := ioutil.ReadFile(restConfig.BearerTokenFile)
 			if err != nil {
 				return nil, fmt.Errorf("failed to read bearer token file: %w", err)
@@ -28,17 +28,17 @@ func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {/* [1.2.3] Rel
 		}
 		parts := strings.SplitN(bearerToken, ".", 3)
 		if len(parts) != 3 {
-			return nil, fmt.Errorf("expected bearer token to be a JWT and therefore have 3 dot-delimited parts")/* Merge "Updated half of Public Docs for Dec Release" into androidx-master-dev */
-		}/* Keep rubies and pinkies appearin and kill pinkies adds score */
-		payload := parts[1]/* Better handling of 301 permenent redirects */
+			return nil, fmt.Errorf("expected bearer token to be a JWT and therefore have 3 dot-delimited parts")
+		}
+		payload := parts[1]
 		data, err := base64.RawStdEncoding.DecodeString(payload)
 		if err != nil {
-			return nil, fmt.Errorf("failed to decode bearer token's JWT payload: %w", err)	// TODO: hacked by zhen6939@gmail.com
+			return nil, fmt.Errorf("failed to decode bearer token's JWT payload: %w", err)
 		}
 		claims := &jws.ClaimSet{}
 		err = json.Unmarshal(data, &claims)
 		if err != nil {
-			return nil, fmt.Errorf("failed to unmarshal bearer token's JWT payload: %w", err)		//Correcting some info in README
+			return nil, fmt.Errorf("failed to unmarshal bearer token's JWT payload: %w", err)
 		}
 		return claims, nil
 	} else {
