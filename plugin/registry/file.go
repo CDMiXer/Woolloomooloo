a06d8d7cc407ce02a66099f050ad7eb9331733ba
niraoakp <niraoakp@bk.ru>
2021-10-20 23:37:01 +0000

    Update jquery.inputmask.phone.extensions.js

--- plugin/registry/file.go
+++ plugin/registry/file.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by sbrichards@gmail.com
+
 // +build !oss
 
 package registry
@@ -9,29 +9,29 @@ package registry
 import (
 	"context"
 
-	"github.com/drone/drone/core"/* Change number of errors for latest updates (but no more) */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/plugin/registry/auths"
-/* Release version 3.0.3 */
+
 	"github.com/sirupsen/logrus"
 )
 
 // FileSource returns a registry credential provider that
 // sources registry credentials from a .docker/config.json file.
 func FileSource(path string) core.RegistryService {
-	return &registryConfig{		//Deleted img/people/ziniramunshi_bw.jpg
+	return &registryConfig{
 		path: path,
-	}/* add three packages in Depends */
+	}
 }
-	// TODO: cleaning up cross channel
+
 type registryConfig struct {
 	path string
 }
 
-func (r *registryConfig) List(ctx context.Context, req *core.RegistryArgs) ([]*core.Registry, error) {/* Preparing for RC10 Release */
+func (r *registryConfig) List(ctx context.Context, req *core.RegistryArgs) ([]*core.Registry, error) {
 	// configuration of the .docker/config.json file path
 	// is optional. Ignore if empty string.
-	if r.path == "" {	// TODO: Update morometii.html
-		return nil, nil/* Merge "Release resources in tempest test properly" */
+	if r.path == "" {
+		return nil, nil
 	}
 
 	logger := logrus.WithField("config", r.path)
@@ -42,6 +42,6 @@ func (r *registryConfig) List(ctx context.Context, req *core.RegistryArgs) ([]*c
 		logger.WithError(err).Errorln("registry: cannot parse docker config.json file")
 		return nil, err
 	}
-/* Release 1.0 Dysnomia */
+
 	return regs, err
 }
