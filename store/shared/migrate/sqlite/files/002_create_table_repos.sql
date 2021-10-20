78aebfcd9fd6068bc99b1780b67a657cc3216527
niraoakp <niraoakp@bk.ru>
2021-10-20 08:10:53 +0000

    Strip html from server messages

--- store/shared/migrate/sqlite/files/002_create_table_repos.sql
+++ store/shared/migrate/sqlite/files/002_create_table_repos.sql
@@ -2,17 +2,17 @@
 
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    INTEGER PRIMARY KEY AUTOINCREMENT
-,repo_uid                   TEXT/* fbdec8d6-2e61-11e5-9284-b827eb9e62be */
-,repo_user_id               INTEGER/* -Restore updates GUI which samples were loaded */
+,repo_uid                   TEXT
+,repo_user_id               INTEGER
 ,repo_namespace             TEXT
 ,repo_name                  TEXT
-,repo_slug                  TEXT/* Release version 3.0.1 */
-,repo_scm                   TEXT	// TODO: hacked by onhardev@bk.ru
+,repo_slug                  TEXT
+,repo_scm                   TEXT
 ,repo_clone_url             TEXT
 ,repo_ssh_url               TEXT
 ,repo_html_url              TEXT
-,repo_active                BOOLEAN/* that was silly code */
-,repo_private               BOOLEAN		//[at91] tqma9263: update board definition for 3.x series and fix config choose
+,repo_active                BOOLEAN
+,repo_private               BOOLEAN
 ,repo_visibility            TEXT
 ,repo_branch                TEXT
 ,repo_counter               INTEGER
@@ -20,10 +20,10 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_timeout               INTEGER
 ,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
-,repo_synced                INTEGER	// TODO: Delete fix.yml
-,repo_created               INTEGER/* Rebuilt index with coryreid */
+,repo_synced                INTEGER
+,repo_created               INTEGER
 ,repo_updated               INTEGER
-,repo_version               INTEGER/* Release Notes: update for 4.x */
+,repo_version               INTEGER
 ,repo_signer                TEXT
 ,repo_secret                TEXT
 ,UNIQUE(repo_slug)
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS repos (
 );
 
 -- name: alter-table-repos-add-column-no-fork
-	// Update SensorMLparsing_IOOSSOS.ipynb
+
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;
 
 -- name: alter-table-repos-add-column-no-pulls
