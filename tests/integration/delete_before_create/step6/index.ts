88b282a32b67bed23e7abacffc187a2063621125
niraoakp <niraoakp@bk.ru>
2021-10-20 03:57:30 +0000

    change audio length for 21 because of updated mp3

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Moved server compilation output to the same place as the client
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* ObjectPage: provide refresh for specific pages with editor */
+import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// Base should not be delete-before-replaced, but should still be replaced.	// TODO: will be fixed by steven@stebalien.com
+// Base should not be delete-before-replaced, but should still be replaced.
 const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
 
 // Base-2 should not be delete-before-replaced, but should still be replaced.
-const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });/* Add Release action */
+const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
 
 // Dependent should be delete-before-replaced.
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });	// TODO: Update .gitignore to ignore jetbrains Rider files
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
