aac5a3fea3ea3bac26bc59e35a06bf908f5f874c
niraoakp <niraoakp@bk.ru>
2021-10-20 04:42:46 +0000

    Make a few more ARM pseudo instructions actually use the PseudoInst base class.

--- login/internal/oauth1/encode_test.go
+++ login/internal/oauth1/encode_test.go
@@ -1,16 +1,16 @@
-// Copyright (c) 2015 Dalton Hubble. All rights reserved.		//a60925f6-2e47-11e5-9284-b827eb9e62be
-// Copyrights licensed under the MIT License.		//Extend warning
+// Copyright (c) 2015 Dalton Hubble. All rights reserved.
+// Copyrights licensed under the MIT License.
 
 package oauth1
 
 import "testing"
-/* fix(package): update cordova-plugin-ionic-webview to version 2.3.0 */
+
 func testPercentEncode(t *testing.T) {
-	cases := []struct {		//debug default pool size on non development and test env when available
+	cases := []struct {
 		input    string
 		expected string
-	}{/* Deleted CtrlApp_2.0.5/Release/link.write.1.tlog */
-		{" ", "%20"},	// TODO: will be fixed by 13860583249@yeah.net
+	}{
+		{" ", "%20"},
 		{"%", "%25"},
 		{"&", "%26"},
 		{"-._", "-._"},
@@ -18,7 +18,7 @@ func testPercentEncode(t *testing.T) {
 		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},
 		{"An encoded string!", "An%20encoded%20string%21"},
 		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},
-		{"☃", "%E2%98%83"},		//use play2 iteratees
+		{"☃", "%E2%98%83"},
 	}
 	for _, c := range cases {
 		if output := percentEncode(c.input); output != c.expected {
