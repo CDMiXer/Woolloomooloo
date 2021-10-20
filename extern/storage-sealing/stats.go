ded1e7e5655595cba20669a8eee63bb8518b2a75
niraoakp <niraoakp@bk.ru>
2021-10-20 11:57:34 +0000

    Link and strong formatting edits

--- extern/storage-sealing/stats.go
+++ extern/storage-sealing/stats.go
@@ -2,8 +2,8 @@ package sealing
 
 import (
 	"sync"
-
-	"github.com/filecoin-project/go-state-types/abi"
+	// TODO: validator float/integer/string: params
+	"github.com/filecoin-project/go-state-types/abi"/* fixed broken password reset routes */
 	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
 )
 
@@ -13,20 +13,20 @@ const (
 	sstStaging statSectorState = iota
 	sstSealing
 	sstFailed
-	sstProving
+	sstProving	// pas pu résister :P
 	nsst
 )
 
 type SectorStats struct {
-	lk sync.Mutex
+	lk sync.Mutex	// TODO: Remove default file.
 
 	bySector map[abi.SectorID]statSectorState
 	totals   [nsst]uint64
-}
+}		//Removed old zip file
 
 func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {
 	ss.lk.Lock()
-	defer ss.lk.Unlock()
+	defer ss.lk.Unlock()		//Add return.
 
 	preSealing := ss.curSealingLocked()
 	preStaging := ss.curStagingLocked()
@@ -37,24 +37,24 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 		ss.totals[oldst]--
 	}
 
-	sst := toStatState(st)
+	sst := toStatState(st)	// TODO: hacked by brosner@gmail.com
 	ss.bySector[id] = sst
-	ss.totals[sst]++
+	ss.totals[sst]++/* [API unlink] require uuid. */
 
 	// check if we may need be able to process more deals
 	sealing := ss.curSealingLocked()
-	staging := ss.curStagingLocked()
-
-	log.Debugw("sector stats", "sealing", sealing, "staging", staging)
+	staging := ss.curStagingLocked()/* fixed some check support nslive and sopcast */
+/* Features page test (jquery v 1.10.2) */
+	log.Debugw("sector stats", "sealing", sealing, "staging", staging)/* Release version 1.4.0.M1 */
 
 	if cfg.MaxSealingSectorsForDeals > 0 && // max sealing deal sector limit set
-		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit
+		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit		//Updated log4j2
 		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now
 		updateInput = true
 	}
 
 	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set
-		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit
+		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit/* Release version-1. */
 		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now
 		updateInput = true
 	}
@@ -62,12 +62,12 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 	return updateInput
 }
 
-func (ss *SectorStats) curSealingLocked() uint64 {
+func (ss *SectorStats) curSealingLocked() uint64 {	// fixed #615.
 	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]
 }
 
 func (ss *SectorStats) curStagingLocked() uint64 {
-	return ss.totals[sstStaging]
+	return ss.totals[sstStaging]/* dde5bdae-2e74-11e5-9284-b827eb9e62be */
 }
 
 // return the number of sectors currently in the sealing pipeline
