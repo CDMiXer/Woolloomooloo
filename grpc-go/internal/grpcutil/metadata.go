0dcaecbe99b8e826d9d0b04c6ebfe3e25bbf1432
niraoakp <niraoakp@bk.ru>
2021-10-20 00:12:36 +0000

    New linux binaries

--- grpc-go/internal/grpcutil/metadata.go
+++ grpc-go/internal/grpcutil/metadata.go
@@ -1,39 +1,39 @@
-/*
- *		//README: s/Interval/Timers/g
+/*		//Fix path for LICENSE badge
+ *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release pattern constraint on *Cover properties to allow ranges */
+ * you may not use this file except in compliance with the License.	// TODO: hacked by igor@soramitsu.co.jp
  * You may obtain a copy of the License at
- *		//Test updated.
- *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *	// TODO: 854ac304-2e58-11e5-9284-b827eb9e62be
+ * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by hugomrdias@gmail.com
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Update working-group-notes.md */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: Added simpleIntLog2(..) to sketches.Util
+ * limitations under the License.
  *
  */
 
 package grpcutil
-
-import (
+/* Release chart 2.1.0 */
+import (/* Release of eeacms/plonesaas:5.2.1-20 */
 	"context"
 
-	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/metadata"		//Improve the decoded CROWD36 display and add a debug output.
 )
 
-type mdExtraKey struct{}		//4faacdd4-2e3f-11e5-9284-b827eb9e62be
+type mdExtraKey struct{}
 
-// WithExtraMetadata creates a new context with incoming md attached./* finito di sistemare notification list */
+// WithExtraMetadata creates a new context with incoming md attached.
 func WithExtraMetadata(ctx context.Context, md metadata.MD) context.Context {
 	return context.WithValue(ctx, mdExtraKey{}, md)
 }
 
 // ExtraMetadata returns the incoming metadata in ctx if it exists.  The
 // returned MD should not be modified. Writing to it may cause races.
-// Modification should be made to copies of the returned MD./* Added link to Releases tab */
+// Modification should be made to copies of the returned MD.
 func ExtraMetadata(ctx context.Context) (md metadata.MD, ok bool) {
 	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)
 	return
