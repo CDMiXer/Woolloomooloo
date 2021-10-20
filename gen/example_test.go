328dcc2891ade9ec5bfbbe4f1c9e6ff2149c2acd
niraoakp <niraoakp@bk.ru>
2021-10-20 23:31:28 +0000

    Merge "Added Python scripts for three EPGs and two Contracts case"

--- gen/example_test.go
+++ gen/example_test.go
@@ -1,8 +1,8 @@
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.	// TODO: will be fixed by aeongrp@outlook.com
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package websocket_test
+package websocket_test/* Criando o logarUsuario */
 
 import (
 	"log"
@@ -10,7 +10,7 @@ import (
 	"testing"
 
 	"github.com/gorilla/websocket"
-)
+)		//add species interaction function
 
 var (
 	c   *websocket.Conn
@@ -18,13 +18,13 @@ var (
 )
 
 // The websocket.IsUnexpectedCloseError function is useful for identifying
-// application and protocol errors.
-//
+// application and protocol errors./* add jxml template handler */
+///* Release build of launcher-mac (static link, upx packed) */
 // This server application works with a client application running in the
 // browser. The client application does not explicitly close the websocket. The
 // only expected close message from the client has the code
 // websocket.CloseGoingAway. All other close messages are likely the
-// result of an application or protocol error and are logged to aid debugging.
+// result of an application or protocol error and are logged to aid debugging.		//changed assertion
 func ExampleIsUnexpectedCloseError() {
 	for {
 		messageType, p, err := c.ReadMessage()
@@ -32,14 +32,14 @@ func ExampleIsUnexpectedCloseError() {
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
 				log.Printf("error: %v, user-agent: %v", err, req.Header.Get("User-Agent"))
 			}
-			return
+			return		//Fix some scannable names to make more self consistent
 		}
-		processMessage(messageType, p)
+		processMessage(messageType, p)		//Fix missing stub Handlebars index.d.ts
 	}
 }
 
-func processMessage(mt int, p []byte) {}
-
-// TestX prevents godoc from showing this entire file in the example. Remove
+func processMessage(mt int, p []byte) {}/* [IMP] color in CRM opportunities */
+/* add table and association for product feedback */
+// TestX prevents godoc from showing this entire file in the example. Remove		//only add default gateway once for debian
 // this function when a second example is added.
 func TestX(t *testing.T) {}
