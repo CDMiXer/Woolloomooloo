b4a8fc65a003baab5f8a1e5f0b193cec2a673fe0
niraoakp <niraoakp@bk.ru>
2021-10-20 20:33:41 +0000

    Rename Code to 1.0_Code

--- handler/api/queue/resume_test.go
+++ handler/api/queue/resume_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 3.0.4. */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+	// bundle-size: 57dbfa618b3456559f572c1f9a769e69ad84b711.json
 package queue
