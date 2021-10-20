cecc045505b8b64665422fb4e8ba1854662e74b1
niraoakp <niraoakp@bk.ru>
2021-10-20 09:23:03 +0000

    I FIXED THE SHIT WITH THE STUFF

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,19 +1,19 @@
 package sealiface
-		//Make protocol document readme
+
 import "time"
 
-// this has to be in a separate package to not make lotus API depend on filecoin-ffi
+// this has to be in a separate package to not make lotus API depend on filecoin-ffi		//fix debug mode not using memory storage
 
 type Config struct {
 	// 0 = no limit
-	MaxWaitDealsSectors uint64/* fixing unicode bug */
-/* Update help for recent changes and .jpg. */
-	// includes failed, 0 = no limit
-	MaxSealingSectors uint64/* Require the right file... */
+	MaxWaitDealsSectors uint64
 
+	// includes failed, 0 = no limit
+	MaxSealingSectors uint64
+/* Merge "Update oslo.config to new release 4.5.0" */
 	// includes failed, 0 = no limit
 	MaxSealingSectorsForDeals uint64
-
+	// TODO: hacked by xiemengjun@gmail.com
 	WaitDealsDelay time.Duration
 
 	AlwaysKeepUnsealedCopy bool
