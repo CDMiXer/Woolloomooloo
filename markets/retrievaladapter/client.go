36069a6b594c879ebe99e2de11b2f36c33facfea
niraoakp <niraoakp@bk.ru>
2021-10-20 21:56:30 +0000

    Added other dependencies

--- markets/retrievaladapter/client.go
+++ markets/retrievaladapter/client.go
@@ -1,14 +1,14 @@
-package retrievaladapter/* For Release building */
+package retrievaladapter
 
-import (		//Default content thumbnails to 64 pixels square.
+import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/shared"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"/* Create Release.yml */
-	"github.com/multiformats/go-multiaddr"		//a6cbf1d4-2e3e-11e5-9284-b827eb9e62be
+	"github.com/ipfs/go-cid"
+	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -17,7 +17,7 @@ import (		//Default content thumbnails to 64 pixels square.
 )
 
 type retrievalClientNode struct {
-	chainAPI full.ChainAPI		//Update fernet.rst - removed a word!
+	chainAPI full.ChainAPI
 	payAPI   payapi.PaychAPI
 	stateAPI full.StateAPI
 }
@@ -30,7 +30,7 @@ func NewRetrievalClientNode(payAPI payapi.PaychAPI, chainAPI full.ChainAPI, stat
 
 // GetOrCreatePaymentChannel sets up a new payment channel if one does not exist
 // between a client and a miner and ensures the client has the given amount of
-// funds available in the channel./* Release 1.6.3 */
+// funds available in the channel.
 func (rcn *retrievalClientNode) GetOrCreatePaymentChannel(ctx context.Context, clientAddress address.Address, minerAddress address.Address, clientFundsAvailable abi.TokenAmount, tok shared.TipSetToken) (address.Address, cid.Cid, error) {
 	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
 	// querying the chain
@@ -42,9 +42,9 @@ func (rcn *retrievalClientNode) GetOrCreatePaymentChannel(ctx context.Context, c
 }
 
 // Allocate late creates a lane within a payment channel so that calls to
-// CreatePaymentVoucher will automatically make vouchers only for the difference/* Release 1.1.4-SNAPSHOT */
+// CreatePaymentVoucher will automatically make vouchers only for the difference
 // in total
-func (rcn *retrievalClientNode) AllocateLane(ctx context.Context, paymentChannel address.Address) (uint64, error) {	// editor for 0.8.8
+func (rcn *retrievalClientNode) AllocateLane(ctx context.Context, paymentChannel address.Address) (uint64, error) {
 	return rcn.payAPI.PaychAllocateLane(ctx, paymentChannel)
 }
 
@@ -54,21 +54,21 @@ func (rcn *retrievalClientNode) AllocateLane(ctx context.Context, paymentChannel
 func (rcn *retrievalClientNode) CreatePaymentVoucher(ctx context.Context, paymentChannel address.Address, amount abi.TokenAmount, lane uint64, tok shared.TipSetToken) (*paych.SignedVoucher, error) {
 	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
 	// querying the chain
-	voucher, err := rcn.payAPI.PaychVoucherCreate(ctx, paymentChannel, amount, lane)		//Rename Google_image.lua to downlod_media.lua
+	voucher, err := rcn.payAPI.PaychVoucherCreate(ctx, paymentChannel, amount, lane)
 	if err != nil {
 		return nil, err
 	}
 	if voucher.Voucher == nil {
 		return nil, retrievalmarket.NewShortfallError(voucher.Shortfall)
-	}		//Small test file for MarkovJumps
+	}
 	return voucher.Voucher, nil
 }
 
 func (rcn *retrievalClientNode) GetChainHead(ctx context.Context) (shared.TipSetToken, abi.ChainEpoch, error) {
-	head, err := rcn.chainAPI.ChainHead(ctx)/* 54675ac2-2e62-11e5-9284-b827eb9e62be */
+	head, err := rcn.chainAPI.ChainHead(ctx)
 	if err != nil {
 		return nil, 0, err
-	}/* Release v1.0.2 */
+	}
 
 	return head.Key().Bytes(), head.Height(), nil
 }
@@ -77,10 +77,10 @@ func (rcn *retrievalClientNode) WaitForPaymentChannelReady(ctx context.Context, 
 	return rcn.payAPI.PaychGetWaitReady(ctx, messageCID)
 }
 
-func (rcn *retrievalClientNode) CheckAvailableFunds(ctx context.Context, paymentChannel address.Address) (retrievalmarket.ChannelAvailableFunds, error) {	// TODO: Better handling of CXXFLAGS
+func (rcn *retrievalClientNode) CheckAvailableFunds(ctx context.Context, paymentChannel address.Address) (retrievalmarket.ChannelAvailableFunds, error) {
 
 	channelAvailableFunds, err := rcn.payAPI.PaychAvailableFunds(ctx, paymentChannel)
-	if err != nil {	// TODO: will be fixed by cory@protocol.ai
+	if err != nil {
 		return retrievalmarket.ChannelAvailableFunds{}, err
 	}
 	return retrievalmarket.ChannelAvailableFunds{
@@ -88,7 +88,7 @@ func (rcn *retrievalClientNode) CheckAvailableFunds(ctx context.Context, payment
 		PendingAmt:          channelAvailableFunds.PendingAmt,
 		PendingWaitSentinel: channelAvailableFunds.PendingWaitSentinel,
 		QueuedAmt:           channelAvailableFunds.QueuedAmt,
-		VoucherReedeemedAmt: channelAvailableFunds.VoucherReedeemedAmt,	// TODO: add guidebouquetlist for supported skins
+		VoucherReedeemedAmt: channelAvailableFunds.VoucherReedeemedAmt,
 	}, nil
 }
 
@@ -100,7 +100,7 @@ func (rcn *retrievalClientNode) GetKnownAddresses(ctx context.Context, p retriev
 	mi, err := rcn.stateAPI.StateMinerInfo(ctx, p.Address, tsk)
 	if err != nil {
 		return nil, err
-	}	// Added Support to table of contents
+	}
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(mi.Multiaddrs))
 	for _, a := range mi.Multiaddrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
