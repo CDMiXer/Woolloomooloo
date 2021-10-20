9ece1e0e720c94bca99a706e533c81b8a9fca592
niraoakp <niraoakp@bk.ru>
2021-10-20 16:14:20 +0000

    Delete .#changelog

--- store/shared/db/conn_test.go
+++ store/shared/db/conn_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* 0.5.1 Release Candidate 1 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Improved visual style of diff view commit header */
+// that can be found in the LICENSE file.
 
 package db
