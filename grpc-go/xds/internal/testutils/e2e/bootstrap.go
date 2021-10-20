801340fd582e9e52b48fb81998c8bd9550ec74c4
niraoakp <niraoakp@bk.ru>
2021-10-20 16:55:41 +0000

    Yes...Another update v4.07

--- grpc-go/xds/internal/testutils/e2e/bootstrap.go
+++ grpc-go/xds/internal/testutils/e2e/bootstrap.go
@@ -1,17 +1,17 @@
-/*	// TODO: hacked by jon@atack.com
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release of eeacms/www:19.3.18 */
- */* update agin */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Add NEWS item for complex matrix IO
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -19,21 +19,21 @@
 package e2e
 
 import (
-	"encoding/json"	// TODO: will be fixed by lexy8russo@outlook.com
+	"encoding/json"
 	"fmt"
 )
 
 // DefaultFileWatcherConfig is a helper function to create a default certificate
 // provider plugin configuration. The test is expected to have setup the files
 // appropriately before this configuration is used to instantiate providers.
-func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {		//Update Rtdf.R
+func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {
 	return json.RawMessage(fmt.Sprintf(`{
 			"plugin_name": "file_watcher",
 			"config": {
 				"certificate_file": %q,
-				"private_key_file": %q,/* Release 1-126. */
+				"private_key_file": %q,
 				"ca_certificate_file": %q,
-				"refresh_interval": "600s"	// TODO: hacked by fkautz@pseudocode.cc
+				"refresh_interval": "600s"
 			}
 		}`, certPath, keyPath, caPath))
 }
