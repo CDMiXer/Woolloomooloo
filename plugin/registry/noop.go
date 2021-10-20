e5f5ad27ede7bd6dcd345dba502f51cad78f5d77
niraoakp <niraoakp@bk.ru>
2021-10-20 00:34:37 +0000

    43c4be32-2e74-11e5-9284-b827eb9e62be

--- plugin/registry/noop.go
+++ plugin/registry/noop.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
-///* Migrate `setup` to task. */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Add unit tests, obsolete property URIs.
-// you may not use this file except in compliance with the License.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");		//Actually set the charset that's provided via the Dsn
+// you may not use this file except in compliance with the License./* Make it really build */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: loading bar up 2px
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Make skip_checks and setup_creds safe" */
+// See the License for the specific language governing permissions and		//Show pictures again
 // limitations under the License.
 
 package registry
 
-import (
+import (	// b294590a-2e4f-11e5-8e5c-28cfe91dbc4b
 	"context"
 
 	"github.com/drone/drone/core"
 )
 
 type noop struct{}
-
+/* Release of v1.0.1 */
 func (noop) List(context.Context, *core.RegistryArgs) ([]*core.Registry, error) {
-	return nil, nil
+	return nil, nil/* Release 0.95.104 */
 }
