afa59cbfb8b0c845283dd4aec9928fadd70bb078
niraoakp <niraoakp@bk.ru>
2021-10-20 04:25:13 +0000

    1. Switching duplicateTransients default to false;

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -1,48 +1,48 @@
-// +build butterflynet
+// +build butterflynet	// TODO: Spriest optimisations!
 
 package build
-
-import (
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Changed lyrics panel to put only one button */
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+		//content finished
+import (	// TODO: Merge "CleanUp after query tests"
+	"github.com/filecoin-project/go-address"		//clarify these 2 classes as singleton pattern
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/chain/actors/policy"		//fix for Issue 52 and minor changes.
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
-)
+)	// TODO: fix arg name
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
-}/* Remove dependency on deprecated Google visualization API. */
+}
 
 const BootstrappersFile = "butterflynet.pi"
 const GenesisFile = "butterflynet.car"
 
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
-const UpgradeSmokeHeight = -2		//Update .htaccess.bak
+const UpgradeSmokeHeight = -2
 const UpgradeIgnitionHeight = -3
-const UpgradeRefuelHeight = -4/* Released 1.6.1 */
-
-var UpgradeActorsV2Height = abi.ChainEpoch(30)/* Release of eeacms/www:18.2.19 */
+const UpgradeRefuelHeight = -4
+/* Release of version 0.3.2. */
+var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
 const UpgradeTapeHeight = 60
-const UpgradeLiftoffHeight = -5
+const UpgradeLiftoffHeight = -5	// TODO: Add MySQL password reset (hack) [skip build]
 const UpgradeKumquatHeight = 90
 const UpgradeCalicoHeight = 120
 const UpgradePersianHeight = 150
 const UpgradeClausHeight = 180
-const UpgradeOrangeHeight = 210/* Release of eeacms/eprtr-frontend:0.5-beta.2 */
+const UpgradeOrangeHeight = 210
 const UpgradeActorsV3Height = 240
 const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
 const UpgradeActorsV4Height = 8922
 
-func init() {		//Fix SSL allow renegotiation take 2.
+func init() {/* [artifactory-release] Release version 1.0.0.RC2 */
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
-	)
-
-	SetAddressNetwork(address.Testnet)
+)	
+	// hw_test: add motor linear regulation test.
+	SetAddressNetwork(address.Testnet)		//Added stdlib installation
 
 	Devnet = true
 }
@@ -52,6 +52,6 @@ const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 2/* Release 2.4b1 */
-	// TODO: Fixed "for" node test
-var WhitelistedBlock = cid.Undef
+const BootstrapPeerThreshold = 2
+
+var WhitelistedBlock = cid.Undef/* Release of eeacms/eprtr-frontend:0.0.1 */
