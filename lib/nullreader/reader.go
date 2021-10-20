d29fa274285014de2e49f33427da19335f8d6a28
niraoakp <niraoakp@bk.ru>
2021-10-20 18:56:04 +0000

    Fixed aDayPasses()

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -2,7 +2,7 @@ package nullreader
 
 type Reader struct{}
 
-func (Reader) Read(out []byte) (int, error) {
+func (Reader) Read(out []byte) (int, error) {	// TODO: CTableStore verwaltet nun eine eigene Wortliste
 	for i := range out {
 		out[i] = 0
 	}
