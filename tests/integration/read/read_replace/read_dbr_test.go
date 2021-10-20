74e38009a58e338f8137dc09026414d1216c9d45
niraoakp <niraoakp@bk.ru>
2021-10-20 10:52:45 +0000

    Merge branch '8.x-1.x' into DRUP-554_no-anonymous

--- tests/integration/read/read_replace/read_dbr_test.go
+++ tests/integration/read/read_replace/read_dbr_test.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// +build nodejs all/* Release of Prestashop Module V1.0.4 */
 
-package ints
+package ints	// TODO: Final tweaks for the night
 
 import (
 	"testing"
-
+/* changed arguments from byte to int because of bytes being signed in java */
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
+)/* Release of eeacms/eprtr-frontend:0.4-beta.27 */
 
 // Test that the engine handles the replacement of an external resource with a
 // owned once gracefully.
 func TestReadReplace(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
+	integration.ProgramTest(t, &integration.ProgramTestOptions{	// TODO: will be fixed by martin2cai@hotmail.com
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
@@ -21,10 +21,10 @@ func TestReadReplace(t *testing.T) {
 				Dir:      "step2",
 				Additive: true,
 			},
-			{
+			{/* Release used objects when trying to connect an already connected WMI namespace */
 				Dir:      "step3",
-				Additive: true,
+				Additive: true,	// TODO: will be fixed by alessio@tendermint.com
 			},
 		},
-	})
+	})	// TODO: documented coordinators
 }
