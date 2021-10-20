c43537eb88601cb878093489aaee4af8db30e89a
niraoakp <niraoakp@bk.ru>
2021-10-20 07:30:42 +0000

    [MERGE]Merge with trunk-dev-addons2

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,17 +1,17 @@
 // +build !linux
 
-package fsutil/* Release notes for 1.0.96 */
+package fsutil
 
-import (
-	"os"	// Rainbow 1.0.2b - Middle-fixes #1
-		//moves bot specs to sentence spec
+import (		//Fixed reseting sorter when loading file through DnD
+	"os"
+
 	logging "github.com/ipfs/go-log/v2"
-)/* fast ticket update */
-	// Change name of ApplicationContext
-var log = logging.Logger("fsutil")
+)
+
+var log = logging.Logger("fsutil")/* Delete LSH-Canopy-Reference.bib */
 
 func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
-
+	// Added footer border.
 	return nil
-}/* Delete jszip.min.js */
+}/* Update the Changelog and the Release notes */
