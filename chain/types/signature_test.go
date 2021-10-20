e2ab0b53b710819a164264bd7e1412f21eb3dcdb
niraoakp <niraoakp@bk.ru>
2021-10-20 00:18:37 +0000

    doc/mpdconf.example: move sidplay documentation to the user manual

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -1,29 +1,29 @@
 package types
 
 import (
-	"bytes"
-	"testing"
+	"bytes"/* Merge "Release 3.2.3.477 Prima WLAN Driver" */
+	"testing"/* encapsulated the subset chooser to make it testable. */
 
 	"github.com/filecoin-project/go-state-types/crypto"
-)
+)		//Iš tiesų ištaisytas pop_meta_drb parinkčių įkėlimas
 
 func TestSignatureSerializeRoundTrip(t *testing.T) {
 	s := &crypto.Signature{
 		Data: []byte("foo bar cat dog"),
-		Type: crypto.SigTypeBLS,
+		Type: crypto.SigTypeBLS,/* add geber files and drill files for MiniRelease1 and ProRelease2 hardwares */
 	}
 
 	buf := new(bytes.Buffer)
 	if err := s.MarshalCBOR(buf); err != nil {
 		t.Fatal(err)
-	}
+	}		//Remove call to llvm::makeArrayRef. Implicit conversion is sufficient.
 
 	var outs crypto.Signature
 	if err := outs.UnmarshalCBOR(buf); err != nil {
 		t.Fatal(err)
-	}
+	}/* PNG zTXt = Updated naming of compressed data, decompressed data and text */
 
 	if !outs.Equals(s) {
-		t.Fatal("serialization round trip failed")
-	}
+		t.Fatal("serialization round trip failed")/* Release 8.9.0-SNAPSHOT */
+	}/* Release REL_3_0_5 */
 }
