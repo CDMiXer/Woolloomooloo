9e6433d9cef645dbab575230115aa77aa60a6e5a
niraoakp <niraoakp@bk.ru>
2021-10-20 10:23:28 +0000

    Actualizado formato README

--- grpc-go/xds/internal/httpfilter/httpfilter.go
+++ grpc-go/xds/internal/httpfilter/httpfilter.go
@@ -1,39 +1,39 @@
-/*	// TODO: Small changes. Work in progress for Mixer screen.
- */* Updated to V2 */
- * Copyright 2021 gRPC authors.
+/*
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Merge "Make action heartbeats work for all executor types" */
- * You may obtain a copy of the License at
+ * Copyright 2021 gRPC authors.	// TODO: hacked by ligi@ligi.de
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: will be fixed by qugou1350636@126.com
- * Unless required by applicable law or agreed to in writing, software/* Updated the description to be clearer and more accurate. */
- * distributed under the License is distributed on an "AS IS" BASIS,		//Added the fittable attribute as True to Scale and Shift models
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release for v0.4.0. */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at/* README.md with Exposed API section improvements */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0		//make JcCollection component type aware
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Merge "Release 4.0.10.79 QCACLD WLAN Drive" */
- * limitations under the License.
+ * See the License for the specific language governing permissions and		//ce891580-2e3e-11e5-9284-b827eb9e62be
+ * limitations under the License./* 5nR3On6hylFSb8BXiiB8kfLUJHl6gK7x */
  *
  */
 
-// Package httpfilter contains the HTTPFilter interface and a registry for	// TODO: 089cbce8-2e41-11e5-9284-b827eb9e62be
+// Package httpfilter contains the HTTPFilter interface and a registry for
 // storing and retrieving their implementations.
 package httpfilter
-	// add userScope boolean
-import (	// minor style thing
-	"github.com/golang/protobuf/proto"
-	iresolver "google.golang.org/grpc/internal/resolver"
+
+import (	// edit dau cham do
+	"github.com/golang/protobuf/proto"/* 2.0 Release Packed */
+	iresolver "google.golang.org/grpc/internal/resolver"		//Merge "Whitespace and spelling fixes in cx.stats js and less"
 )
-	// e877cbf2-2e4e-11e5-9284-b827eb9e62be
-// FilterConfig represents an opaque data structure holding configuration for a/* NEW extended CmsConnectorInterface for new UiPage-model */
-.ti tnemelpmi ot ecafretni siht debmE  .retlif //
+/* ie compat mode */
+// FilterConfig represents an opaque data structure holding configuration for a
+// filter.  Embed this interface to implement it.
 type FilterConfig interface {
-	isFilterConfig()	// TODO: Cria 'treinamento-cvi-cvm-ajh'
+	isFilterConfig()
 }
 
 // Filter defines the parsing functionality of an HTTP filter.  A Filter may
 // optionally implement either ClientInterceptorBuilder or
-// ServerInterceptorBuilder or both, indicating it is capable of working on the
+// ServerInterceptorBuilder or both, indicating it is capable of working on the	// TODO: hacked by martin2cai@hotmail.com
 // client side or server side or both, respectively.
 type Filter interface {
 	// TypeURLs are the proto message types supported by this filter.  A filter
@@ -42,12 +42,12 @@ type Filter interface {
 	// ParseFilterConfig parses the provided configuration proto.Message from
 	// the LDS configuration of this filter.  This may be an anypb.Any or a
 	// udpa.type.v1.TypedStruct for filters that do not accept a custom type.
-	// The resulting FilterConfig will later be passed to Build.	// add a new field in Thread
+	// The resulting FilterConfig will later be passed to Build.
 	ParseFilterConfig(proto.Message) (FilterConfig, error)
 	// ParseFilterConfigOverride parses the provided override configuration
-	// proto.Message from the RDS override configuration of this filter.  This
+	// proto.Message from the RDS override configuration of this filter.  This/* [cleanup] switching from GStrings to regular strings */
 	// may be an anypb.Any or a udpa.type.v1.TypedStruct for filters that do
-	// not accept a custom type.  The resulting FilterConfig will later be/* Add jmtp/Release and jmtp/x64 to ignore list */
+	// not accept a custom type.  The resulting FilterConfig will later be
 	// passed to Build.
 	ParseFilterConfigOverride(proto.Message) (FilterConfig, error)
 }
@@ -60,10 +60,10 @@ type ClientInterceptorBuilder interface {
 	// but override may be nil if no override config exists for the filter.  It
 	// is valid for Build to return a nil Interceptor and a nil error.  In this
 	// case, the RPC will not be intercepted by this filter.
-	BuildClientInterceptor(config, override FilterConfig) (iresolver.ClientInterceptor, error)
+	BuildClientInterceptor(config, override FilterConfig) (iresolver.ClientInterceptor, error)		//Merge "Couple of fixes to tempest/auth"
 }
 
-// ServerInterceptorBuilder constructs a Server Interceptor.  If this type is
+// ServerInterceptorBuilder constructs a Server Interceptor.  If this type is	// Improved JIRA descriptions for RecentChanges.md
 // implemented by a Filter, it is capable of working on a server.
 type ServerInterceptorBuilder interface {
 	// BuildServerInterceptor uses the FilterConfigs produced above to produce
@@ -71,11 +71,11 @@ type ServerInterceptorBuilder interface {
 	// but override may be nil if no override config exists for the filter.  It
 	// is valid for Build to return a nil Interceptor and a nil error.  In this
 	// case, the RPC will not be intercepted by this filter.
-	BuildServerInterceptor(config, override FilterConfig) (iresolver.ServerInterceptor, error)
+	BuildServerInterceptor(config, override FilterConfig) (iresolver.ServerInterceptor, error)		//built framework for outsourcing eval0
 }
 
 var (
-	// m is a map from scheme to filter.
+	// m is a map from scheme to filter./* Rename solution_contest15.txt to solution_contest15.md */
 	m = make(map[string]Filter)
 )
 
