8ee89f15cd41ebd1ad477e6cc5a8bc9212fc4fb9
niraoakp <niraoakp@bk.ru>
2021-10-20 05:53:04 +0000

    const_square_iterators allowed to be converted to square_iterators

--- tests/integration/aliases/aliases_py_test.go
+++ tests/integration/aliases/aliases_py_test.go
@@ -1,41 +1,41 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: Do not check package access in ScriptingSecurityManager
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build python all
 
-package ints		//bump to v0.1.22
+package ints
 
 import (
 	"path/filepath"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Start on registry */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 var dirs = []string{
 	"rename",
 	"adopt_into_component",
 	"rename_component_and_child",
-	"retype_component",/* @Release [io7m-jcanephora-0.22.1] */
-	"rename_component",	// Delete 05.Functional Calculator.js
-}/* Release packages contained pdb files */
-/* Release version 0.5, which code was written nearly 2 years before. */
-func TestPythonAliases(t *testing.T) {/* Release areca-7.1.2 */
+	"retype_component",
+	"rename_component",
+}
+
+func TestPythonAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: filepath.Join(d, "step1"),	// TODO: hacked by steven@stebalien.com
-				Dependencies: []string{		//Nova cor azul escuro
+				Dir: filepath.Join(d, "step1"),
+				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
 				Quick: true,
 				EditDirs: []integration.EditDir{
 					{
-						Dir:             filepath.Join(d, "step2"),	// TODO: adding some infrastructure items - logging and the containers
-						Additive:        true,/* Update build_win32.py */
+						Dir:             filepath.Join(d, "step2"),
+						Additive:        true,
 						ExpectNoChanges: true,
-					},	// TODO: will be fixed by greg@colvin.org
+					},
 				},
 			})
-		})	// TODO: l'upgrade ne passait pas sur forum.spip.org
+		})
 	}
 }
