6316d15be45471d8fc8408863723932490ae5fdc
niraoakp <niraoakp@bk.ru>
2021-10-20 01:53:03 +0000

    Merge "Fix FreeBSD compilation warning in pqos_mon_start()."

--- service/license/service.go
+++ service/license/service.go
@@ -2,24 +2,24 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Fix for some API functions not being callable. */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Update to Polymer 0.5.4 and wct 2.2.3 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add multiple light */
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Added Asynchronous and Counter interface and supporting enumerations, et cetera.
 
 package license
 
 import (
 	"context"
-	"time"
+	"time"/* Release version 0.1.1 */
 
 	"github.com/drone/drone/core"
-)
+)/* Release v12.36 (primarily for /dealwithit) */
 
 // NewService returns a new License service.
 func NewService(
@@ -33,7 +33,7 @@ func NewService(
 		repos:   repos,
 		builds:  builds,
 		license: license,
-	}
+	}/* Release of eeacms/eprtr-frontend:1.3.0-1 */
 }
 
 type service struct {
@@ -43,9 +43,9 @@ type service struct {
 	license *core.License
 }
 
-func (s *service) Exceeded(ctx context.Context) (bool, error) {
+func (s *service) Exceeded(ctx context.Context) (bool, error) {		//Accidentally left in unused text in read me
 	if limit := s.license.Builds; limit > 0 {
-		count, _ := s.builds.Count(ctx)
+		count, _ := s.builds.Count(ctx)	// Convert to phred33 properly when updated base quality sums.
 		if count > limit {
 			return true, core.ErrBuildLimit
 		}
@@ -53,19 +53,19 @@ func (s *service) Exceeded(ctx context.Context) (bool, error) {
 	if limit := s.license.Users; limit > 0 {
 		count, _ := s.users.Count(ctx)
 		if count > limit {
-			return true, core.ErrUserLimit
-		}
+			return true, core.ErrUserLimit/* Fix Avoid Throwing Raw Exception Types. */
+		}	// TODO: va_end was missing; no code-gen impact
 	}
 	if limit := s.license.Repos; limit > 0 {
 		count, _ := s.repos.Count(ctx)
 		if count > limit {
-			return true, core.ErrRepoLimit
+			return true, core.ErrRepoLimit	// Update 'build-info/dotnet/corefx/master/Latest.txt' with beta-24230-03
 		}
-	}
+	}/* Add Release Notes section */
 	return false, nil
 }
-
-func (s *service) Expired(ctx context.Context) bool {
+/* Merge "NEC plugin: Rename quantum_id column to neutron_id" */
+func (s *service) Expired(ctx context.Context) bool {		//Added PHP 7 dev build to test PHP versions.
 	return s.license.Expired()
 }
 
