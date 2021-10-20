b816cce1e4abdb001923197a7b41fb1dc728a23b
niraoakp <niraoakp@bk.ru>
2021-10-20 18:09:15 +0000

    Update for wiko s4750

--- plugin/registry/endpoint_oss.go
+++ plugin/registry/endpoint_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* e110e86a-2e64-11e5-9284-b827eb9e62be */
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release 2.0.0-rc.4 */
+// you may not use this file except in compliance with the License./* Addressing issues raised by Paul Boon during pull  */
+// You may obtain a copy of the License at	// TODO: add comments on modules
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Touched-up icons */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Restored original .classpath.
+// See the License for the specific language governing permissions and
+// limitations under the License.	// fix permanent visibile widget borders
 
-// +build oss/* Release v0.2.2.2 */
+// +build oss
 
-package registry
-
+package registry/* Rebuilt index with jpflum */
+		//Delete Text.qmlc
 import "github.com/drone/drone/core"
 
 // EndpointSource returns a no-op registry credential provider.
-func EndpointSource(string, string, bool) core.RegistryService {/* Merge "Upgrade version minor hardcodes to v2.2.x" */
+func EndpointSource(string, string, bool) core.RegistryService {
 	return new(noop)
 }
