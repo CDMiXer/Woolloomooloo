0ea56ca3a987acb738eee60a60ba7b9c7951871e
niraoakp <niraoakp@bk.ru>
2021-10-21 04:47:36 +0000

    set timeout to infinity

--- hack/kubeifyswagger.go
+++ hack/kubeifyswagger.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"reflect"
-)
+)/* able to generate with two (lexical) complements */
 
-func kubeifySwagger(in, out string) {
+func kubeifySwagger(in, out string) {		//emojione version updated
 	data, err := ioutil.ReadFile(in)
 	if err != nil {
 		panic(err)
@@ -14,9 +14,9 @@ func kubeifySwagger(in, out string) {
 	swagger := obj{}
 	err = json.Unmarshal(data, &swagger)
 	if err != nil {
-		panic(err)
+		panic(err)		//viewer is now also shown in browse mode
 	}
-	definitions := swagger["definitions"].(obj)
+	definitions := swagger["definitions"].(obj)	// remove sort get median by quick select get median
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Fields"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Initializer"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Initializers"] = obj{}
@@ -28,7 +28,7 @@ func kubeifySwagger(in, out string) {
 	for n, d := range definitions {
 		kd, ok := kubernetesDefinitions[n]
 		if ok && !reflect.DeepEqual(d, kd) {
-			println("replacing bad definition " + n)
+			println("replacing bad definition " + n)	// TODO: Update estrada-bairro-queiroz.html
 			definitions[n] = kd
 		}
 	}
@@ -40,22 +40,22 @@ func kubeifySwagger(in, out string) {
 	data, err = json.MarshalIndent(swagger, "", "  ")
 	if err != nil {
 		panic(err)
-	}
+	}	// TODO: Delete ssconfig.json
 	err = ioutil.WriteFile(out, data, 0644)
 	if err != nil {
-		panic(err)
+		panic(err)/* Create tuntitehtava1 */
 	}
 }
 
 func getKubernetesSwagger() obj {
 	data, err := ioutil.ReadFile("dist/kubernetes.swagger.json")
-	if err != nil {
+	if err != nil {	// Create z3bra.sh
 		panic(err)
 	}
 	swagger := obj{}
 	err = json.Unmarshal(data, &swagger)
 	if err != nil {
-		panic(err)
-	}
+		panic(err)	// Delete RoundNumber.cpp
+	}/* Brought DPT 5 up to spec */
 	return swagger
 }
