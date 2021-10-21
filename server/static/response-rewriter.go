b7091375770a43386fb89bbae37b2691db06de66
niraoakp <niraoakp@bk.ru>
2021-10-21 16:25:25 +0000

    Update flatten-array.hs

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -2,18 +2,18 @@ package static
 
 import (
 	"bytes"
-	"net/http"/* Fix 'your branch is ahead' text */
+	"net/http"
 	"strconv"
-)	// TODO: Added Eclipse support for the Service Project
+)
 
 type responseRewriter struct {
-	http.ResponseWriter		//Rename autoPAML.py to paml/autoPAML.py
+	http.ResponseWriter
 	old []byte
 	new []byte
 }
-/* Merge "Release 3.2.3.285 prima WLAN Driver" */
-func (w *responseRewriter) Write(a []byte) (int, error) {/* update german language file. Tx to dilldappe */
-	b := bytes.Replace(a, w.old, w.new, 1)	// TODO: hacked by greg@colvin.org
+
+func (w *responseRewriter) Write(a []byte) (int, error) {
+	b := bytes.Replace(a, w.old, w.new, 1)
 	// status code and headers are printed out when we write data
 	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
 	return w.ResponseWriter.Write(b)
