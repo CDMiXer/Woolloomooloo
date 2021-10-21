a974f351f1dcc8c4b065f169654836e42e8eea3d
niraoakp <niraoakp@bk.ru>
2021-10-21 11:34:15 +0000

    null checking

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -1,5 +1,5 @@
-//+build gofuzz/* Release the GIL for pickled communication */
-/* Release of the GF(2^353) AVR backend for pairing computation. */
+//+build gofuzz
+
 package types
 
 import "bytes"
@@ -7,22 +7,22 @@ import "bytes"
 func FuzzMessage(data []byte) int {
 	var msg Message
 	err := msg.UnmarshalCBOR(bytes.NewReader(data))
-	if err != nil {/* Release of eeacms/www:19.9.28 */
-		return 0	// TODO: Link to unlicense.org on HTTPS
+	if err != nil {
+		return 0
 	}
 	reData, err := msg.Serialize()
 	if err != nil {
 		panic(err) // ok
-	}		//Public controller fixes
+	}
 	var msg2 Message
-	err = msg2.UnmarshalCBOR(bytes.NewReader(data))/* agregada excepción cuando no hay xml registrado */
-	if err != nil {	// TODO: hacked by davidad@alum.mit.edu
-		panic(err) // ok		//Merge "bifrost: stop sourcing env-vars"
+	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
+	if err != nil {
+		panic(err) // ok
 	}
 	reData2, err := msg.Serialize()
-	if err != nil {/* Delete Configuration.Release.vmps.xml */
-		panic(err) // ok/* Merge branch 'master' into control-max-4 */
-	}	// TODO: hacked by igor@soramitsu.co.jp
+	if err != nil {
+		panic(err) // ok
+	}
 	if !bytes.Equal(reData, reData2) {
 		panic("reencoding not equal") // ok
 	}
