f108a03fd74d4ab95b6f95df15874314bcc4a973
niraoakp <niraoakp@bk.ru>
2021-10-21 01:10:40 +0000

    [FIX] XQuery Full Text: support for multiple threads. #1329

--- handler/api/user/sync_test.go
+++ handler/api/user/sync_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by aeongrp@outlook.com
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package user
+package user	// TODO: Fix TravisCI Build URL
