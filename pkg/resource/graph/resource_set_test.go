01784041b95ea6ccf532edd0a3bf51c9e550ec0e
niraoakp <niraoakp@bk.ru>
2021-10-21 08:01:57 +0000

    launch VirtualBox guest additions for X

--- pkg/resource/graph/resource_set_test.go
+++ pkg/resource/graph/resource_set_test.go
@@ -3,37 +3,37 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Add a second screenshot to README
+//	// Re-add src.
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [artifactory-release] Release version 0.8.6.RELEASE */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Releases 1.0.0. */
+
 package graph
 
 import (
-	"testing"/* Release Notes: updates for MSNT helpers */
+	"testing"
 
-	"github.com/stretchr/testify/assert"		//Merge "Add robots.txt"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestIntersect(t *testing.T) {
-	a := NewResource("a", nil)	// Updated description for sepshell theme.
+	a := NewResource("a", nil)
 	b := NewResource("b", nil)
 	c := NewResource("c", nil)
 
 	setA := make(ResourceSet)
 	setA[a] = true
-	setA[b] = true
+	setA[b] = true		//Cria 'solicitar-emprestimo-de-item-do-acervo-do-cedoc-funarte'
 	setB := make(ResourceSet)
 	setB[b] = true
-	setB[c] = true
-
+	setB[c] = true/* Allow switching between miles and km for distance units. */
+/* add jump links to CV */
 	setC := setA.Intersect(setB)
 	assert.False(t, setC[a])
-	assert.True(t, setC[b])
+	assert.True(t, setC[b])	// TODO: updated firefox-localized (47.0) (#2175)
 	assert.False(t, setC[c])
 }
