ea8020f2f7a2902169dc4933b4bb09cc694d735d
niraoakp <niraoakp@bk.ru>
2021-10-21 06:03:34 +0000

    Create rebuild

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
-package v1api
-/* Link to the shared libraries instead of the static ones. */
-import (
-	"github.com/filecoin-project/lotus/api"
+package v1api	// TODO: hacked by ligi@ligi.de
+
+import (	// TODO: 9a184830-2e4f-11e5-8969-28cfe91dbc4b
+	"github.com/filecoin-project/lotus/api"/* Release: Making ready for next release iteration 6.3.0 */
 )
-/* Add Drone 4 to related projects */
+
 type FullNode = api.FullNode
-type FullNodeStruct = api.FullNodeStruct		//Create conv_icon.sh
-	// TODO: will be fixed by ligi@ligi.de
+type FullNodeStruct = api.FullNodeStruct	// Create OLT-2.html
+
 func PermissionedFullAPI(a FullNode) FullNode {
 	return api.PermissionedFullAPI(a)
-}/* ActionInterface: move API documentation to the header */
+}/* fixed ranking */
