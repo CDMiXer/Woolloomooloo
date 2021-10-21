7e1e591efc520bfdd013058bd8749e84ba8c525b
niraoakp <niraoakp@bk.ru>
2021-10-21 16:01:42 +0000

    changing text for hero image

--- node/testopts.go
+++ node/testopts.go
@@ -1,20 +1,20 @@
 package node
-	// TODO: Delete LRsheeptemplate.nii.gz
-import (
+
+import (		//Better organization of client vs server side JS.
 	"errors"
 
-	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
-
+	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"/* Chore: Moving Paging.js to top in readme file */
+	// Update netutils.h
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-)/* add csv2ddl */
+)
 
 func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
-		ApplyIf(func(s *Settings) bool { return !s.Online },	// TODO: will be fixed by mail@bitpshr.net
+		ApplyIf(func(s *Settings) bool { return !s.Online },
 			Error(errors.New("MockHost must be specified after Online")),
 		),
-
+	// TODO: will be fixed by aeongrp@outlook.com
 		Override(new(lp2p.RawHost), lp2p.MockHost),
-		Override(new(mocknet.Mocknet), mn),
+		Override(new(mocknet.Mocknet), mn),/* Release dbpr  */
 	)
 }
