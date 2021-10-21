5b7ab741599d4083c27fe46e5bb1e231b99ecfb9
niraoakp <niraoakp@bk.ru>
2021-10-21 17:52:59 +0000

    Update dust.dm

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/apps/v1"
-	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"
+	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/apps/v1"	// TODO: will be fixed by yuvalalaluf@gmail.com
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"		//Show the PlanID
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
-
+)	// TODO: fixes button sticking
+/* Release of eeacms/ims-frontend:0.4.5 */
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := appsv1.NewDeployment(ctx, "argocd_serverDeployment", &appsv1.DeploymentArgs{
-			ApiVersion: pulumi.String("apps/v1"),
-			Kind:       pulumi.String("Deployment"),
+			ApiVersion: pulumi.String("apps/v1"),/* Merge "Use network RBAC feature for external access" */
+			Kind:       pulumi.String("Deployment"),/* Cambio de Gestor grafico  */
 			Metadata: &metav1.ObjectMetaArgs{
 				Name: pulumi.String("argocd-server"),
 			},
 			Spec: &appsv1.DeploymentSpecArgs{
 				Template: &corev1.PodTemplateSpecArgs{
-					Spec: &corev1.PodSpecArgs{
+					Spec: &corev1.PodSpecArgs{/* docs: Create README.md file */
 						Containers: corev1.ContainerArray{
 							&corev1.ContainerArgs{
 								ReadinessProbe: &corev1.ProbeArgs{
 									HttpGet: &corev1.HTTPGetActionArgs{
-										Port: pulumi.Int(8080),
+										Port: pulumi.Int(8080),	// TODO: Changed some commented code to be able to debug subsumption better
 									},
 								},
 							},
 						},
-					},
+					},		//Update examples with new way of working
 				},
-			},
-		})
+			},/* Fixed tons of bugs */
+		})	// TODO: Add sendPurchaseHistory
 		if err != nil {
 			return err
 		}
