78137478bb7c7b5e3692ec0cdd07350833785fab
niraoakp <niraoakp@bk.ru>
2021-10-21 01:28:24 +0000

    added locate

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,16 +1,16 @@
-package test
+package test/* Updated git URL in the Jenkins file to the Bcgov location */
 
-import (		//merge work on images, checkboxes and pathbars
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"
-)		//Downgraded jmzml, because new version requires java 8
-
+	"github.com/ipfs/go-cid"/* Merge "Don't delete objects twice..." into honeycomb */
+)
+/* @Release [io7m-jcanephora-0.23.1] */
 var dummyCid cid.Cid
 
-func init() {
-	dummyCid, _ = cid.Parse("bafkqaaa")
+func init() {	// TODO: add more missing files
+	dummyCid, _ = cid.Parse("bafkqaaa")/* Remove an id attribute from texts.json */
 }
 
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
@@ -19,9 +19,9 @@ func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, err
 		Height:                5,
 		ParentStateRoot:       dummyCid,
 		Messages:              dummyCid,
-		ParentMessageReceipts: dummyCid,
-		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
-		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},/* fix for issue 120 */
-		Timestamp:             timestamp,
+		ParentMessageReceipts: dummyCid,/* automated commit from rosetta for sim/lib resistance-in-a-wire, locale sq */
+		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},	// TODO: Add link to Cassini projection.
+		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
+		Timestamp:             timestamp,/* Release notes for Trimble.SQLite package */
 	}})
-}
+}		//Added bechmarks folder
