d0530ad0bc867b67a214e8b835e0b9229bcec0c9
niraoakp <niraoakp@bk.ru>
2021-10-21 16:37:08 +0000

    Delete screenshot_v3.png

--- handler/api/ccmenu/ccmenu_oss.go
+++ handler/api/ccmenu/ccmenu_oss.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone IO, Inc.
-///* Delete perso1.png */
+//	// TODO: will be fixed by souzau@yandex.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Update & clarify commented acknowledgements */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by markruss@microsoft.com
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Re-adding calculations for clientIdBytes and clientIdLength
+//
+// Unless required by applicable law or agreed to in writing, software		//Includes maturity badge
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by mowrain@yandex.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss		//adds test images
+// +build oss
 
-package ccmenu/* Use pure component in app container */
+package ccmenu
 
-import (
-	"net/http"	// - fixed: define SHUT_RDWR for Windows environments
-	// TODO: hacked by why@ipfs.io
+import (		//Work around audio buffer problem in older android builds.
+	"net/http"	// Refactor Win32 #ifndef for BindListenPort
+	// TODO: will be fixed by ng8eke@163.com
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// TODO: hacked by hugomrdias@gmail.com
 )
 
 // Handler returns a no-op http.HandlerFunc.
-func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {/* Rename read.md to README.md */
+func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		render.NotImplemented(w, render.ErrNotImplemented)
 	}
-}	// TODO: hacked by lexy8russo@outlook.com
+}
