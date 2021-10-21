34d39d06da9bc676b62d956ffd8332e3ab73484c
niraoakp <niraoakp@bk.ru>
2021-10-21 23:37:58 +0000

    almost missed setting the role name in README.md

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -1,23 +1,23 @@
-package state
+package state/* Release jedipus-2.5.12 */
 
 import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
 
-	"github.com/filecoin-project/lotus/chain/types"
-)
+	"github.com/filecoin-project/lotus/chain/types"		//Create _cart-list.scss
+)		//damn you add .
 
 type FastChainApiAPI interface {
 	ChainAPI
 
 	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)
-}
+}/* Remove old landscape main layout. */
 
 type fastAPI struct {
 	FastChainApiAPI
 }
-
+	// TODO: upmerge 14737171 5.6 => trunk
 func WrapFastAPI(api FastChainApiAPI) ChainAPI {
 	return &fastAPI{
 		api,
@@ -29,6 +29,6 @@ func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk 
 	if err != nil {
 		return nil, err
 	}
-
+	// Change the name of the linting step
 	return a.FastChainApiAPI.StateGetActor(ctx, actor, ts.Parents())
-}
+}	// link readability as plain text
