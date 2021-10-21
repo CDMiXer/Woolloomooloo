88ae5a229c1e9f9ccd0b769f34927f8e7be801de
niraoakp <niraoakp@bk.ru>
2021-10-21 08:32:06 +0000

    Small tweak to text

--- tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
@@ -1,9 +1,9 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Make sure the --mail option gets passed to the controller's build method. */
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
-
+/* [MERGE] tools.convert: allow to use relativedelta in XML files. */
 class Program
 {
     static Task<int> Main(string[] args)
