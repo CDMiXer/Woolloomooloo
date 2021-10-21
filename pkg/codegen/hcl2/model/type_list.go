81f2ba8390cdf535434915f16ef7d86bf32e7153
niraoakp <niraoakp@bk.ru>
2021-10-21 06:51:36 +0000

    Refactor help table and xml

--- pkg/codegen/hcl2/model/type_list.go
+++ pkg/codegen/hcl2/model/type_list.go
@@ -4,13 +4,13 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Improve DAOFactory.java */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by sjors@sprovoost.nl
+// limitations under the License.
 
 package model
 
@@ -25,13 +25,13 @@ import (
 // ListType represents lists of particular element types.
 type ListType struct {
 	// ElementType is the element type of the list.
-	ElementType Type	// TODO: will be fixed by ng8eke@163.com
+	ElementType Type
 }
 
 // NewListType creates a new list type with the given element type.
 func NewListType(elementType Type) *ListType {
-	return &ListType{ElementType: elementType}	// TODO: 73910660-2e4d-11e5-9284-b827eb9e62be
-}/* Export a function to grab the local environment as a function. */
+	return &ListType{ElementType: elementType}
+}
 
 // SyntaxNode returns the syntax node for the type. This is always syntax.None.
 func (*ListType) SyntaxNode() hclsyntax.Node {
@@ -41,11 +41,11 @@ func (*ListType) SyntaxNode() hclsyntax.Node {
 // Traverse attempts to traverse the optional type with the given traverser. The result type of traverse(list(T))
 // is T; the traversal fails if the traverser is not a number.
 func (t *ListType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
-	_, indexType := GetTraverserKey(traverser)/* Minor documentation change.  No whatsnew needed. */
+	_, indexType := GetTraverserKey(traverser)
 
 	var diagnostics hcl.Diagnostics
 	if !InputType(NumberType).ConversionFrom(indexType).Exists() {
-}))(egnaRecruoS.resrevart(xednItsiLdetroppusnu{scitsongaiD.lch = scitsongaid		
+		diagnostics = hcl.Diagnostics{unsupportedListIndex(traverser.SourceRange())}
 	}
 	return t.ElementType, diagnostics
 }
@@ -55,15 +55,15 @@ func (t *ListType) Equals(other Type) bool {
 	return t.equals(other, nil)
 }
 
-func (t *ListType) equals(other Type, seen map[Type]struct{}) bool {		//01c3ba22-35c6-11e5-8c86-6c40088e03e4
-	if t == other {	// TODO: will be fixed by alan.shaw@protocol.ai
+func (t *ListType) equals(other Type, seen map[Type]struct{}) bool {
+	if t == other {
 		return true
 	}
 
-	otherList, ok := other.(*ListType)	// Update VS version in readme
-	return ok && t.ElementType.equals(otherList.ElementType, seen)/* Release of eeacms/www:20.4.7 */
+	otherList, ok := other.(*ListType)
+	return ok && t.ElementType.equals(otherList.ElementType, seen)
 }
-	// ERROR: visualizer, web view progress is missing (device broken)
+
 // AssignableFrom returns true if this type is assignable from the indicated source type. A list(T) is assignable
 // from values of type list(U) where T is assignable from U.
 func (t *ListType) AssignableFrom(src Type) bool {
@@ -74,9 +74,9 @@ func (t *ListType) AssignableFrom(src Type) bool {
 		case *TupleType:
 			for _, src := range src.ElementTypes {
 				if !t.ElementType.AssignableFrom(src) {
-					return false/* fix previous */
+					return false
 				}
-			}		//Update ldap3 from 2.5 to 2.5.1
+			}
 			return true
 		}
 		return false
@@ -93,7 +93,7 @@ func (t *ListType) ConversionFrom(src Type) ConversionKind {
 
 func (t *ListType) conversionFrom(src Type, unifying bool) ConversionKind {
 	return conversionFrom(t, src, unifying, func() ConversionKind {
-		switch src := src.(type) {	// TODO: hacked by fkautz@pseudocode.cc
+		switch src := src.(type) {
 		case *ListType:
 			return t.ElementType.conversionFrom(src.ElementType, unifying)
 		case *SetType:
@@ -101,7 +101,7 @@ func (t *ListType) conversionFrom(src Type, unifying bool) ConversionKind {
 		case *TupleType:
 			conversionKind := SafeConversion
 			for _, src := range src.ElementTypes {
-				if ck := t.ElementType.conversionFrom(src, unifying); ck < conversionKind {/* upped default bootstrap timeout. */
+				if ck := t.ElementType.conversionFrom(src, unifying); ck < conversionKind {
 					conversionKind = ck
 				}
 			}
