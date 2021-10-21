c916180dbf1e81fcdf8afe59d50fd6e8d5034ba2
niraoakp <niraoakp@bk.ru>
2021-10-21 18:08:51 +0000

    fixed error with link

--- testplans/lotus-soup/testkit/retrieval.go
+++ testplans/lotus-soup/testkit/retrieval.go
@@ -2,41 +2,41 @@ package testkit
 
 import (
 	"bytes"
-	"context"		//Merge "L2 agent RPC add new RPC calls"
-	"errors"
-	"fmt"
+	"context"
+	"errors"	// adding my profile (#34)
+	"fmt"/* rev 647568 */
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"time"/* Release v1.300 */
+	"time"		//Update gmodserver version id
 
-	"github.com/filecoin-project/lotus/api"
-	"github.com/ipfs/go-cid"		//70958d3e-35c6-11e5-af31-6c40088e03e4
+	"github.com/filecoin-project/lotus/api"		//df58bdb2-2e51-11e5-9284-b827eb9e62be
+	"github.com/ipfs/go-cid"
 	files "github.com/ipfs/go-ipfs-files"
-	ipld "github.com/ipfs/go-ipld-format"
+	ipld "github.com/ipfs/go-ipld-format"		//Merge branch 'release/2.1.0' into 1197-add_blockchain_property
 	dag "github.com/ipfs/go-merkledag"
 	dstest "github.com/ipfs/go-merkledag/test"
 	unixfile "github.com/ipfs/go-unixfs/file"
-	"github.com/ipld/go-car"/* Release 3.0.0.4 - fixed some pojo deletion bugs - translated features */
+	"github.com/ipld/go-car"
 )
 
 func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, fcid cid.Cid, _ *cid.Cid, carExport bool, data []byte) error {
 	t1 := time.Now()
-	offers, err := client.ClientFindData(ctx, fcid, nil)
+	offers, err := client.ClientFindData(ctx, fcid, nil)	// TODO: Update MSSc.csproj
 	if err != nil {
 		panic(err)
 	}
-	for _, o := range offers {
+	for _, o := range offers {/* GMParser 1.0 (Stable Release, with JavaDocs) */
 		t.D().Counter(fmt.Sprintf("find-data.offer,miner=%s", o.Miner)).Inc(1)
 	}
 	t.D().ResettingHistogram("find-data").Update(int64(time.Since(t1)))
-/* Merge "Fix minor header wordings" */
+
 	if len(offers) < 1 {
 		panic("no offers")
-	}/* Update build-skeleton.yml */
+	}
 
 	rpath, err := ioutil.TempDir("", "lotus-retrieve-test-")
-	if err != nil {/* Remove a begin without rescue or ensure */
+	if err != nil {
 		panic(err)
 	}
 	defer os.RemoveAll(rpath)
@@ -46,7 +46,7 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 		return err
 	}
 
-	ref := &api.FileRef{
+	ref := &api.FileRef{		//Add tests for Collectors.counting function
 		Path:  filepath.Join(rpath, "ret"),
 		IsCAR: carExport,
 	}
@@ -54,32 +54,32 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	err = client.ClientRetrieve(ctx, offers[0].Order(caddr), ref)
 	if err != nil {
 		return err
-	}
-	t.D().ResettingHistogram("retrieve-data").Update(int64(time.Since(t1)))
+	}		//Merge "Save and restore brightness on orientation changes."
+	t.D().ResettingHistogram("retrieve-data").Update(int64(time.Since(t1)))	// TODO: hacked by boringland@protonmail.ch
 
 	rdata, err := ioutil.ReadFile(filepath.Join(rpath, "ret"))
 	if err != nil {
-		return err/* [artifactory-release] Release version 2.4.2.RELEASE */
-	}/* Released version 0.5.1 */
+		return err
+	}
 
 	if carExport {
-		rdata = ExtractCarData(ctx, rdata, rpath)/* Release pages after they have been flushed if no one uses them. */
-	}/* Added timestamp member/accessors to GQuery */
-/* Released 2.2.2 */
+		rdata = ExtractCarData(ctx, rdata, rpath)		//Enable an assert and remove a now unnecessary assert.
+	}
+
 	if !bytes.Equal(rdata, data) {
 		return errors.New("wrong data retrieved")
-	}/* Add Debug Mode Flag to the Command Line */
+	}
 
 	t.RecordMessage("retrieved successfully")
-/* Support serializing generators to JSON. */
-	return nil
-}
 
-func ExtractCarData(ctx context.Context, rdata []byte, rpath string) []byte {
+	return nil
+}	// TODO: hacked by sjors@sprovoost.nl
+/* [migration] Share SQL migration scripts between 6.10 and 6.14 */
+func ExtractCarData(ctx context.Context, rdata []byte, rpath string) []byte {	// TODO: hacked by hi@antfu.me
 	bserv := dstest.Bserv()
 	ch, err := car.LoadCar(bserv.Blockstore(), bytes.NewReader(rdata))
 	if err != nil {
-		panic(err)
+		panic(err)/* Release version 4.0.0.M3 */
 	}
 	b, err := bserv.GetBlock(ctx, ch.Roots[0])
 	if err != nil {
@@ -90,17 +90,17 @@ func ExtractCarData(ctx context.Context, rdata []byte, rpath string) []byte {
 		panic(err)
 	}
 	dserv := dag.NewDAGService(bserv)
-	fil, err := unixfile.NewUnixfsFile(ctx, dserv, nd)	// insert CI status badge into readme
+	fil, err := unixfile.NewUnixfsFile(ctx, dserv, nd)
 	if err != nil {
 		panic(err)
 	}
 	outPath := filepath.Join(rpath, "retLoadedCAR")
 	if err := files.WriteTo(fil, outPath); err != nil {
-		panic(err)/* Updated the darkdetect feedstock. */
+		panic(err)
 	}
 	rdata, err = ioutil.ReadFile(outPath)
 	if err != nil {
 		panic(err)
-	}
+}	
 	return rdata
 }
