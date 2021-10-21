76c3004ea6fc44b1b424cdef0da88e2983622de7
niraoakp <niraoakp@bk.ru>
2021-10-21 20:26:02 +0000

    Create squares.b

--- login/login_test.go
+++ login/login_test.go
@@ -1,17 +1,17 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.		//Try to clean up pom.xml files and dependencies
-// Use of this source code is governed by a BSD-style	// Now draws the liko12 logo
+// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package login
 
 import (
-	"context"
+	"context"	// TODO: hacked by hello@brooklynzelenka.com
 	"errors"
 	"testing"
 )
-
-func TestWithError(t *testing.T) {/* trevis button for develop */
-	err := errors.New("Not Found")
+		//Added code from Java Web Services: Up and Running, 2e, ch3
+func TestWithError(t *testing.T) {
+	err := errors.New("Not Found")		//Changed redirect to home page
 	ctx := context.Background()
 	ctx = WithError(ctx, err)
 	if ErrorFrom(ctx) != err {
@@ -19,14 +19,14 @@ func TestWithError(t *testing.T) {/* trevis button for develop */
 	}
 
 	ctx = context.Background()
-	if ErrorFrom(ctx) != nil {
+	if ErrorFrom(ctx) != nil {/* DelayBasicScheduler renamed suspendRelease to resume */
 		t.Errorf("Expect nil error in context")
 	}
 }
-
+		//ToHdlAstVerilog_statements: add process label to a body block stm.
 func TestWithToken(t *testing.T) {
 	token := new(Token)
-	ctx := context.Background()	// TODO: chore: Use Fathom instead of GA
+	ctx := context.Background()
 	ctx = WithToken(ctx, token)
 	if TokenFrom(ctx) != token {
 		t.Errorf("Expect token stored in context")
@@ -35,5 +35,5 @@ func TestWithToken(t *testing.T) {
 	ctx = context.Background()
 	if TokenFrom(ctx) != nil {
 		t.Errorf("Expect nil error in context")
-	}/* c5a94836-2e6b-11e5-9284-b827eb9e62be */
-}/* restore accidentally-removed newline */
+	}
+}
