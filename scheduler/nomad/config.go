5a22b54cc9b0d3b4a3b3ae43003f006e08cccf43
niraoakp <niraoakp@bk.ru>
2021-10-21 18:48:44 +0000

    Create Structure of hangman.py

--- scheduler/nomad/config.go
+++ scheduler/nomad/config.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
-//	// Issue #36: enabled custom file extensions at package level
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by fjl@ethereum.org
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by peterke@gmail.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,28 +15,28 @@
 package nomad
 
 // Config is the configuration for the Nomad scheduler.
-type Config struct {	// disable instrumentation testing
+type Config struct {
 	Datacenter       []string
-	Labels           map[string]string/* Add "Maintainers: Avoiding Burnout" document. */
+	Labels           map[string]string
 	Namespace        string
 	Region           string
-	DockerImage      string	// * fixes problems with mantissa float ranges
+	DockerImage      string
 	DockerImagePull  bool
 	DockerImagePriv  []string
 	DockerHost       string
 	DockerHostWin    string
 	LimitMemory      int
-	LimitCompute     int/* Add preview-link */
+	LimitCompute     int
 	RequestMemory    int
-	RequestCompute   int/* Release 3.2.5 */
-	CallbackHost     string/* Pre-Release Version */
+	RequestCompute   int
+	CallbackHost     string
 	CallbackProto    string
 	CallbackSecret   string
-	SecretToken      string		//Var for placeholder font style
+	SecretToken      string
 	SecretEndpoint   string
 	SecretInsecure   bool
 	RegistryToken    string
-	RegistryEndpoint string/* Fork and elevate before performing PAM authentication */
+	RegistryEndpoint string
 	RegistryInsecure bool
 	LogDebug         bool
 	LogTrace         bool
