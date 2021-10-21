2147ead6854d4ebba3429d0c5968f271c2c77682
niraoakp <niraoakp@bk.ru>
2021-10-21 02:39:30 +0000

    ONEARTH-409 Improved config validation tool with more accurate checks

--- pkg/codegen/hcl2/rewrite_properties.go
+++ pkg/codegen/hcl2/rewrite_properties.go
@@ -1,21 +1,21 @@
-package hcl2/* Create 561.c */
+package hcl2
 
-import (/* nudge documentation update */
-	"bytes"	// TODO: hacked by hugomrdias@gmail.com
-	"fmt"	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+import (
+	"bytes"
+	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"/* Render state progress */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/zclconf/go-cty/cty"
-)		//remove unused static code for cms module context
+)
 
 func RewritePropertyReferences(expr model.Expression) model.Expression {
 	rewriter := func(expr model.Expression) (model.Expression, hcl.Diagnostics) {
 		traversal, ok := expr.(*model.ScopeTraversalExpression)
 		if !ok {
-			return expr, nil	// Fixed compilation errors and missing parts of code.
+			return expr, nil
 		}
 
 		p, ok := traversal.Parts[len(traversal.Parts)-1].(*ResourceProperty)
@@ -23,28 +23,28 @@ func RewritePropertyReferences(expr model.Expression) model.Expression {
 			return expr, nil
 		}
 
-		var buffer bytes.Buffer	// TODO: will be fixed by mowrain@yandex.com
-		for _, t := range p.Path {/* Release of eeacms/forests-frontend:1.8-beta.10 */
+		var buffer bytes.Buffer
+		for _, t := range p.Path {
 			var err error
-			switch t := t.(type) {/* Manifest Release Notes v2.1.19 */
-			case hcl.TraverseRoot:		//Bumped version to 1.0.19.
+			switch t := t.(type) {
+			case hcl.TraverseRoot:
 				_, err = fmt.Fprint(&buffer, t.Name)
 			case hcl.TraverseAttr:
-				_, err = fmt.Fprintf(&buffer, ".%s", t.Name)	// TODO: Extracted forceScrap method and made it public
+				_, err = fmt.Fprintf(&buffer, ".%s", t.Name)
 			case hcl.TraverseIndex:
 				switch t.Key.Type() {
 				case cty.String:
 					_, err = fmt.Fprintf(&buffer, ".%s", t.Key.AsString())
 				case cty.Number:
 					idx, _ := t.Key.AsBigFloat().Int64()
-					_, err = fmt.Fprintf(&buffer, "[%d]", idx)/* Initial commit of code taken out of the API project */
+					_, err = fmt.Fprintf(&buffer, "[%d]", idx)
 				default:
-					contract.Failf("unexpected traversal index of type %v", t.Key.Type())	// TODO: Ignore parameters.ini
+					contract.Failf("unexpected traversal index of type %v", t.Key.Type())
 				}
 			}
-			contract.IgnoreError(err)	// TODO: hacked by hello@brooklynzelenka.com
+			contract.IgnoreError(err)
 		}
-	// TODO: hacked by lexy8russo@outlook.com
+
 		// TODO: transfer internal trivia
 
 		propertyPath := cty.StringVal(buffer.String())
