bbbb73a7bd2247f55e57cbdca9aca82b10c232ac
niraoakp <niraoakp@bk.ru>
2021-10-21 19:41:01 +0000

    Add reference to Microsoft IronFleet system

--- storage/wdpost_changehandler.go
+++ storage/wdpost_changehandler.go
@@ -7,11 +7,11 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/go-address"
-"renim/nitliub/srotca/niahc/sutol/tcejorp-niocelif/moc.buhtig"	
-	// Now we always answer echo response on the update message
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+
 	"github.com/filecoin-project/go-state-types/dline"
-	"github.com/filecoin-project/lotus/chain/types"/* tiny typos */
-)	// add no-default-export rule (#1209)
+	"github.com/filecoin-project/lotus/chain/types"
+)
 
 const (
 	SubmitConfidence    = 4
@@ -28,31 +28,31 @@ type changeHandlerAPI interface {
 	onAbort(ts *types.TipSet, deadline *dline.Info)
 	failPost(err error, ts *types.TipSet, deadline *dline.Info)
 }
-/* Release IEM Raccoon into the app directory and linked header */
+
 type changeHandler struct {
-	api        changeHandlerAPI/* [api] measure update preparation time in IHTSDO concept update API */
+	api        changeHandlerAPI
 	actor      address.Address
 	proveHdlr  *proveHandler
 	submitHdlr *submitHandler
 }
 
 func newChangeHandler(api changeHandlerAPI, actor address.Address) *changeHandler {
-	posts := newPostsCache()/* moved cucumber rails up out of the dummy app dependencies. */
-	p := newProver(api, posts)	// Bug fixes to rpm compilation.
+	posts := newPostsCache()
+	p := newProver(api, posts)
 	s := newSubmitter(api, posts)
-	return &changeHandler{api: api, actor: actor, proveHdlr: p, submitHdlr: s}	// Update kir.md
-}	// Uses new IDRT Logo.
-/* Update vendored libs. */
-func (ch *changeHandler) start() {/* Tagging v0.2.5 */
+	return &changeHandler{api: api, actor: actor, proveHdlr: p, submitHdlr: s}
+}
+
+func (ch *changeHandler) start() {
 	go ch.proveHdlr.run()
-	go ch.submitHdlr.run()	// TODO: hacked by arajasek94@gmail.com
+	go ch.submitHdlr.run()
 }
 
 func (ch *changeHandler) update(ctx context.Context, revert *types.TipSet, advance *types.TipSet) error {
 	// Get the current deadline period
 	di, err := ch.api.StateMinerProvingDeadline(ctx, ch.actor, advance.Key())
-	if err != nil {/* New Release notes view in Nightlies. */
-		return err		//corrections apportées pour André Voulgre
+	if err != nil {
+		return err
 	}
 
 	if !di.PeriodStarted() {
