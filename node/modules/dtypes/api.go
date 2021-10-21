319f47e5b0a58a58dc13594c31a7c77906e4c61a
niraoakp <niraoakp@bk.ru>
2021-10-21 20:21:35 +0000

    zrf-model (drop moves support)

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -1,10 +1,10 @@
 package dtypes
 
-import (/* Add DNS addon */
+import (	// TODO: will be fixed by steven@stebalien.com
 	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/multiformats/go-multiaddr"
-)/* Fixed typo in DisplayChanger.ConfigDialog */
-/* Release 0.7.2 to unstable. */
+)
+
 type APIAlg jwt.HMACSHA
 
 type APIEndpoint multiaddr.Multiaddr
