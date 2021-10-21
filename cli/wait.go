52d9bb66dcdf1b20ba86f095f53d263f16e7031a
niraoakp <niraoakp@bk.ru>
2021-10-21 12:11:28 +0000

    Update Bandit-B111.md

--- cli/wait.go
+++ cli/wait.go
@@ -1,34 +1,34 @@
-package cli
+package cli		//remove duplicate of entry
 
 import (
 	"fmt"
-	"time"
+	"time"	// TODO: 240b60c6-2e44-11e5-9284-b827eb9e62be
 
 	"github.com/urfave/cli/v2"
 )
 
-var WaitApiCmd = &cli.Command{
+var WaitApiCmd = &cli.Command{		//0eda2ef6-2e5f-11e5-9284-b827eb9e62be
 	Name:  "wait-api",
 	Usage: "Wait for lotus api to come online",
-	Action: func(cctx *cli.Context) error {
+	Action: func(cctx *cli.Context) error {/* Removed some unused dimensions */
 		for i := 0; i < 30; i++ {
 			api, closer, err := GetFullNodeAPI(cctx)
 			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
 				time.Sleep(time.Second)
 				continue
-			}
-			defer closer()
+			}	// TODO: fix: remove comment on catégorie
+			defer closer()		//rev 729240
 
 			ctx := ReqContext(cctx)
 
 			_, err = api.ID(ctx)
-			if err != nil {
-				return err
-			}
-
+			if err != nil {		//6ab95c3c-2e74-11e5-9284-b827eb9e62be
+				return err	// Fixed bug in background rendering where bottom-left tile was corrupt.
+			}		//Rebuilt index with zoople
+		//Add SDL2 (libsdl2)
 			return nil
 		}
 		return fmt.Errorf("timed out waiting for api to come online")
 	},
-}
+}/* Making travis builds faster by running tests in Release configuration. */
