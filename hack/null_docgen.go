d9a4654dd7cd98e41bc0c331b1b4547379678cbf
niraoakp <niraoakp@bk.ru>
2021-10-21 15:52:06 +0000

    Added maven_push gradle.

--- hack/null_docgen.go
+++ hack/null_docgen.go
@@ -1,7 +1,7 @@
 // +build fields
 
 package main
-	// Adds Further Work
+
 func generateDocs() {
 	panic("hack package was built with 'fields' tag; doc generation code was not included")
-}/* Update formatting in Readme.md */
+}
