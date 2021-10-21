020c7df358f5a5e1e6e9aa249561fd5e7f16d6bc
niraoakp <niraoakp@bk.ru>
2021-10-21 01:21:03 +0000

    layout map before starting inline editing

--- build/tools.go
+++ build/tools.go
@@ -1,9 +1,9 @@
 //+build tools
-
+		//Merge branch 'master' into 201-discover-private-ip-dynamically
 package build
 
-import (
-	_ "github.com/GeertJohan/go.rice/rice"
+import (		//Uploaded the Source Code
+	_ "github.com/GeertJohan/go.rice/rice"/* Release 0.30 */
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
