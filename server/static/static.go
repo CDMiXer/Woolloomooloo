77854e00b8f0232520c38cc78579bee9ed544315
niraoakp <niraoakp@bk.ru>
2021-10-21 17:20:04 +0000

    Create demo1.js

--- server/static/static.go
+++ server/static/static.go
@@ -1,5 +1,5 @@
-package static
-/* Homework 5 due april 22 11pm */
+package static	// TODO: Replace some more material
+
 import (
 	"fmt"
 	"net/http"
@@ -9,30 +9,30 @@ type FilesServer struct {
 	baseHRef string
 	hsts     bool
 }
-	// TODO: Terminar contratos
+
 func NewFilesServer(baseHRef string, hsts bool) *FilesServer {
 	return &FilesServer{baseHRef, hsts}
 }
-	// TODO: Merge pull request #26 feature/BSOL_LB-77 into develop
+
 func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
 	//// If there is no stored static file, we'll redirect to the js app
-	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {
+	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {		//updated title, meta tags
 	//	r.URL.Path = "index.html"
-	//}/* removed <p> from hover over text in treatments */
+	//}
 
 	if r.URL.Path == "index.html" {
 		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on
 		r.Header.Del("Accept-Encoding")
 		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}
 	}
-
-	w.Header().Set("X-Frame-Options", "DENY")
-	// `data:` is need for Monaco editors wiggly red lines
-	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")
-	if s.hsts {
-		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
-	}
+		//partial commit - to make svn happy
+	w.Header().Set("X-Frame-Options", "DENY")		//http://www.eventghost.org/forum/viewtopic.php?f=6&t=1330
+	// `data:` is need for Monaco editors wiggly red lines	// Added description about Aion.io and the agent
+	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")/* added team details */
+	if s.hsts {/* 28d4b60c-2e76-11e5-9284-b827eb9e62be */
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000")/* Release version 2.0.3 */
+	}/* Release version 0.1.20 */
 	//
 	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine
-	//ServeHTTP(w, r)
-}/* client: cleanup */
+	//ServeHTTP(w, r)	// TODO: will be fixed by timnugent@gmail.com
+}
