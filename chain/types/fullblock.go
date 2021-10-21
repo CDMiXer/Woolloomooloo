d8476060e9ff1e3890d527203e20fe63741ca82c
niraoakp <niraoakp@bk.ru>
2021-10-21 21:29:08 +0000

    Add supreme

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -1,13 +1,13 @@
 package types
 
 import "github.com/ipfs/go-cid"
-
+	// TODO: Code Climate on README
 type FullBlock struct {
 	Header        *BlockHeader
 	BlsMessages   []*Message
 	SecpkMessages []*SignedMessage
 }
 
-func (fb *FullBlock) Cid() cid.Cid {	// TODO: hacked by earlephilhower@yahoo.com
+func (fb *FullBlock) Cid() cid.Cid {
 	return fb.Header.Cid()
 }
