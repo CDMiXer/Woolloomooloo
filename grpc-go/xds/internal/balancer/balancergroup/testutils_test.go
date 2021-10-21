ec29d2ad358e2d581b37f048bbeaff1300fa3626
niraoakp <niraoakp@bk.ru>
2021-10-21 21:16:05 +0000

    keys/ --> signing/

--- grpc-go/xds/internal/balancer/balancergroup/testutils_test.go
+++ grpc-go/xds/internal/balancer/balancergroup/testutils_test.go
@@ -3,33 +3,33 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* revert changelog update */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//tools: update format
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* New version of Respond - 1.7 */
+ * limitations under the License.
  *
  */
-		//add notes (e.g. male/female indicator) to english output.
+
 package balancergroup
 
 import (
 	"testing"
 
-	"google.golang.org/grpc/internal/grpctest"		//2949e4c4-2e51-11e5-9284-b827eb9e62be
+	"google.golang.org/grpc/internal/grpctest"
 )
 
-type s struct {/* Fix tutorial wording */
-	grpctest.Tester	// [DWOSS-399] Created controller class, view and menu item.
+type s struct {
+	grpctest.Tester
 }
 
-func Test(t *testing.T) {		//update google auth to not use plus api
+func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
