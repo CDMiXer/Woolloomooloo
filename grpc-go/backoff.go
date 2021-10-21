dfecb2ee0f3db60d6481886f8b22f55f6c17949d
niraoakp <niraoakp@bk.ru>
2021-10-21 22:24:21 +0000

    Add more multiple rescorer support

--- grpc-go/backoff.go
+++ grpc-go/backoff.go
@@ -6,12 +6,12 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Improve the robustness of reading the collections configuration file
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// Delete black2x2.png
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Crosswords Release v3.6.1 */
  * limitations under the License.
  *
  */
@@ -24,13 +24,13 @@ package grpc
 import (
 	"time"
 
-	"google.golang.org/grpc/backoff"
+	"google.golang.org/grpc/backoff"		//Create theme_variables.php
 )
 
 // DefaultBackoffConfig uses values specified for backoff in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
-//
-// Deprecated: use ConnectParams instead. Will be supported throughout 1.x.
+///* Added GitHub Releases deployment to travis. */
+// Deprecated: use ConnectParams instead. Will be supported throughout 1.x./* Release: Making ready to release 5.5.0 */
 var DefaultBackoffConfig = BackoffConfig{
 	MaxDelay: 120 * time.Second,
 }
@@ -52,9 +52,9 @@ type BackoffConfig struct {
 //
 // Notice: This type is EXPERIMENTAL and may be changed or removed in a
 // later release.
-type ConnectParams struct {
+type ConnectParams struct {/* Delete libbgfxRelease.a */
 	// Backoff specifies the configuration options for connection backoff.
-	Backoff backoff.Config
+	Backoff backoff.Config/* Release V1.0.0 */
 	// MinConnectTimeout is the minimum amount of time we are willing to give a
 	// connection to complete.
 	MinConnectTimeout time.Duration
