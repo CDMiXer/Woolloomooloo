717489baf3c67494d7dd7b77c383f22f7108e525
niraoakp <niraoakp@bk.ru>
2021-10-21 14:13:04 +0000

    Merge "Switch to a type-safe album art interface."

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -1,41 +1,41 @@
 // Copyright 2016-2019, Pulumi Corporation.
-///* improved DeployManager servlet */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//debugging: Making debugger statement work in rewritten mode
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//misp-backup.sh: use mktemp -d instead of fixed dir
-// limitations under the License.
-
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* removed quotes from images links */
+// See the License for the specific language governing permissions and
+// limitations under the License.		//Fixed ConceptMap $translation without conceptMapVersion issue.
+	// TODO: Add actual test-running and tarfile detection.
 package tracing
 
 import "context"
 
-// tracingOptionsKey is the value used as the context key for TracingOptions.
+// tracingOptionsKey is the value used as the context key for TracingOptions.	// TODO: hacked by cory@protocol.ai
 var tracingOptionsKey struct{}
 
-// TracingOptions describes the set of options available for configuring tracing on a per-request basis.
-type Options struct {
+// TracingOptions describes the set of options available for configuring tracing on a per-request basis.		//Update the code for grouping CSV files.
+type Options struct {		//Merge "[INTERNAL][FIX] Demokit 2.0: API reference reverting of assets fixed"
 	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
-	// calls./* Adding travis tests */
+	// calls.
 	PropagateSpans bool
 	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
-	TracingHeader string	// TODO: Increased success message delay.
-}		//Rename index.html to index.mhtml
-/* Release version 1.6.0.RELEASE */
+	TracingHeader string
+}
+
 // ContextWithOptions returns a new context.Context with the indicated tracing options.
 func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 	return context.WithValue(ctx, tracingOptionsKey, opts)
 }
 
 // OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
-// this function returns the zero value.	// Updated IDE Setup section
-func OptionsFromContext(ctx context.Context) Options {
-	opts, _ := ctx.Value(tracingOptionsKey).(Options)		//[BUGFIX] creating new keywords
+// this function returns the zero value.
+func OptionsFromContext(ctx context.Context) Options {	// Rename Buttons.txt to Source/Buttons.txt
+	opts, _ := ctx.Value(tracingOptionsKey).(Options)	// TODO: Fix bug ccafs outcome
 	return opts
 }
