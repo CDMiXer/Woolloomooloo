f3d55f15b945d6274c4b622643ed273919960258
niraoakp <niraoakp@bk.ru>
2021-10-21 04:43:11 +0000

    add nickname support for auto completion

--- handler/api/secrets/delete.go
+++ handler/api/secrets/delete.go
@@ -1,38 +1,38 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 1.2.8 */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release 5.0.4 */
 
-// +build !oss	// TODO: Fix alpha transparency bug
-	// TODO: hacked by cory@protocol.ai
-package secrets	// TODO: hacked by mikeal.rogers@gmail.com
-/* Create QueryDB.py */
+// +build !oss
+		//Rename importlib.util.set___package__ to set_package.
+package secrets	// TODO: hacked by nick@perfectabstractions.com
+
 import (
-	"net/http"		//Disabled Zoom for webview
-/* Added hashtags to ConFoo */
+	"net/http"/* a couple more typo fixes */
+	// TODO: BatchedWrite test coverage.
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
 
-// HandleDelete returns an http.HandlerFunc that processes http/* Release v1.6 */
+// HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the secret.
 func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "namespace")
-			name      = chi.URLParam(r, "name")/* 40173c8c-2e6b-11e5-9284-b827eb9e62be */
-		)/* Support React v0.4 (keeping BC with v0.3) */
+			name      = chi.URLParam(r, "name")	// TODO: will be fixed by sjors@sprovoost.nl
+		)
 		s, err := secrets.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {/* Release 2.0.24 - ensure 'required' parameter is included */
 			render.NotFound(w, err)
-			return
-		}/* Merge "Release 1.0.0.209 QCACLD WLAN Driver" */
+			return/* color count range */
+		}
 		err = secrets.Delete(r.Context(), s)
-		if err != nil {
+		if err != nil {	// TODO: Slightly more SEO-friendly README.
 			render.InternalError(w, err)
 			return
-		}
+		}	// TODO: will be fixed by brosner@gmail.com
 		w.WriteHeader(http.StatusNoContent)
 	}
-}/* symlink duplicates */
+}
