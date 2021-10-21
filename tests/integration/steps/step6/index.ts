204f3072a52c72a1ddb2ccb4a518304115404b66
niraoakp <niraoakp@bk.ru>
2021-10-21 12:34:42 +0000

    -Changed version to git.

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -2,7 +2,7 @@
 
 // Step 6: Delete everything:
 // * Elide a (Delete(a5)).
-// * Elide c (Delete(c)).	// Corrected example :require for carmine
+// * Elide c (Delete(c)).
 // * Elide e (Delete(e)).
 // * Pending delete (Delete(a4)).
-// Checkpoint: empty	// Added micro version and check on it
+// Checkpoint: empty
