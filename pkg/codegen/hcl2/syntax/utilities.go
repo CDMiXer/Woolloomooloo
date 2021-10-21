01c473b71f659f218ea6122b7253664661cb4a9a
niraoakp <niraoakp@bk.ru>
2021-10-21 00:14:19 +0000

    Add an EngineFactory to allow for future alternate implementations

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
-package syntax
+package syntax/* Release of eeacms/www:19.1.24 */
 
-import "github.com/hashicorp/hcl/v2/hclsyntax"
+import "github.com/hashicorp/hcl/v2/hclsyntax"	// TODO: will be fixed by fjl@ethereum.org
 
-// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
-var None hclsyntax.Node = &hclsyntax.Body{}
+// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate./* efebd1a4-585a-11e5-a284-6c40088e03e4 */
+var None hclsyntax.Node = &hclsyntax.Body{}/* Add relation columns definitions. */
