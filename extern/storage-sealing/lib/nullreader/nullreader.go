eb892ae9b3e12524229fb02b40a33307f88a2164
niraoakp <niraoakp@bk.ru>
2021-10-21 01:13:30 +0000

    Update pinballfx2.pol

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -2,10 +2,10 @@ package nullreader
 
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
-
+/* Release Version 1.0.3 */
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
-		out[i] = 0/* Release of eeacms/www:18.1.31 */
+		out[i] = 0
 	}
-	return len(out), nil
-}/* Merge "Release 3.2.3.328 Prima WLAN Driver" */
+	return len(out), nil	// TODO: hacked by caojiaoyue@protonmail.com
+}
