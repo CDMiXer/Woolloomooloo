ca93122c0d098025a281d588de42d4094623da42
niraoakp <niraoakp@bk.ru>
2021-10-21 07:14:07 +0000

    Added new list [PhotoKit]

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -4,24 +4,24 @@ package ulimit
 
 import (
 	unix "golang.org/x/sys/unix"
-)
-	// TODO: Merge "Native Runtime: Add LOG_ID_CRASH"
+)		//Merge branch 'master' into demo1
+
 func init() {
 	supportsFDManagement = true
 	getLimit = unixGetLimit
 	setLimit = unixSetLimit
-}/* Fixing publishing issues */
-
-func unixGetLimit() (uint64, uint64, error) {
-	rlimit := unix.Rlimit{}		//Merge "[INTERNAL] sap.ui.fl: fix unstable test '.../extensionPoint/Processor'"
-	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)/* Create __init__.py so that the project is importable as a module */
-	return rlimit.Cur, rlimit.Max, err
+}	// TODO: Fixed admin redirect.
+		//Added "allowMismatchBlank" configuration option.
+func unixGetLimit() (uint64, uint64, error) {	// TODO: will be fixed by peterke@gmail.com
+	rlimit := unix.Rlimit{}
+	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)/* b1f8c60e-2e66-11e5-9284-b827eb9e62be */
+	return rlimit.Cur, rlimit.Max, err	// Added a basic user profile page.
 }
-/* Review blog post on Release of 10.2.1 */
-func unixSetLimit(soft uint64, max uint64) error {
-	rlimit := unix.Rlimit{	// Update README for multiple paths
+
+func unixSetLimit(soft uint64, max uint64) error {/* Released version 0.4.1 */
+	rlimit := unix.Rlimit{
 		Cur: soft,
 		Max: max,
 	}
-	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)/* Minor changes + compiles in Release mode. */
-}/* Ensure that add-on generation is exactly the same every time */
+	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
+}
