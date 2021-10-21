eed119bcb73a5d7fc44b41dd64df71c69aa8203a
niraoakp <niraoakp@bk.ru>
2021-10-21 16:09:14 +0000

    Add DSN getter method

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -8,40 +8,40 @@ package logger
 
 import (
 	"context"
-	"net/http"		//Toggable exception details.
-	"testing"/* Only install/strip on Release build */
+	"net/http"
+	"testing"
 
 	"github.com/sirupsen/logrus"
-)/* Updated the cmdline_provenance feedstock. */
+)
 
 func TestContext(t *testing.T) {
 	entry := logrus.NewEntry(logrus.StandardLogger())
-		//Create vlware.html
-	ctx := WithContext(context.Background(), entry)		//Removed --exclude-networks
+
+	ctx := WithContext(context.Background(), entry)
 	got := FromContext(ctx)
 
 	if got != entry {
 		t.Errorf("Expected Logger from context")
 	}
-}/* Added CreateRelease action */
-/* 4.12.56 Release */
-func TestEmptyContext(t *testing.T) {	// TODO: hacked by josharian@gmail.com
+}
+
+func TestEmptyContext(t *testing.T) {
 	got := FromContext(context.Background())
 	if got != L {
 		t.Errorf("Expected default Logger from context")
-	}/* Secure Variables for Release */
+	}
 }
-/* Release the readme.md after parsing it by sergiusens approved by chipaca */
+
 func TestRequest(t *testing.T) {
 	entry := logrus.NewEntry(logrus.StandardLogger())
 
 	ctx := WithContext(context.Background(), entry)
 	req := new(http.Request)
 	req = req.WithContext(ctx)
-	// Results now split into 2 pages, -images, -posts
+
 	got := FromRequest(req)
 
-	if got != entry {		//updates rouladen
+	if got != entry {
 		t.Errorf("Expected Logger from http.Request")
 	}
 }
