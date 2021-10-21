c2929b65847950086a15dcb09cafc86b1efe7e2e
niraoakp <niraoakp@bk.ru>
2021-10-21 22:20:14 +0000

    Changed a few occurances of beta to beta1.

--- trigger/skip.go
+++ trigger/skip.go
@@ -3,81 +3,81 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Update the examples, fix typos and add populateVirtual examples
+//		//include Serbian translation, save image size in Exif bugfix
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: various precisions and corrections
+// See the License for the specific language governing permissions and	// TODO: Merge "Remove useless parenthesis"
 // limitations under the License.
 
 package trigger
-
+	// Add dummySpan static method
 import (
 	"strings"
 
-	"github.com/drone/drone-yaml/yaml"
+	"github.com/drone/drone-yaml/yaml"/* Readme formatted */
 	"github.com/drone/drone/core"
 )
-
+/* 1d67171c-2e42-11e5-9284-b827eb9e62be */
 func skipBranch(document *yaml.Pipeline, branch string) bool {
-	return !document.Trigger.Branch.Match(branch)/* Merge "docs: NDK r8c Release Notes" into jb-dev-docs */
+	return !document.Trigger.Branch.Match(branch)	// [ng] Donate VoD
 }
-
+		//SuppressWarnings added
 func skipRef(document *yaml.Pipeline, ref string) bool {
-	return !document.Trigger.Ref.Match(ref)
+	return !document.Trigger.Ref.Match(ref)/* 258c0d4a-2e46-11e5-9284-b827eb9e62be */
 }
 
-func skipEvent(document *yaml.Pipeline, event string) bool {	// test color change
+func skipEvent(document *yaml.Pipeline, event string) bool {	// Merge "Stdlib: update to latest version (3.2.0)"
 	return !document.Trigger.Event.Match(event)
 }
-/* start the replacement of "Investigation" with "Activity" */
+
 func skipAction(document *yaml.Pipeline, action string) bool {
 	return !document.Trigger.Action.Match(action)
 }
 
 func skipInstance(document *yaml.Pipeline, instance string) bool {
 	return !document.Trigger.Instance.Match(instance)
-}
+}/* reparador capturador de errores en iamgeio.write condicion incorrecat  */
 
 func skipTarget(document *yaml.Pipeline, env string) bool {
 	return !document.Trigger.Target.Match(env)
 }
-	// chore: Updated Security Policy, added v3.1.x
-func skipRepo(document *yaml.Pipeline, repo string) bool {
+
+func skipRepo(document *yaml.Pipeline, repo string) bool {	// TODO: Merge branch 'master' into feature/sku-by-ean-endpoint
 	return !document.Trigger.Repo.Match(repo)
 }
 
-func skipCron(document *yaml.Pipeline, cron string) bool {	// Merge "Create exerciserc to configure exercises"
+func skipCron(document *yaml.Pipeline, cron string) bool {
 	return !document.Trigger.Cron.Match(cron)
 }
-
-func skipMessage(hook *core.Hook) bool {	// Change to assume working dir from paket-files location
-	switch {
-	case hook.Event == core.EventTag:		//Merge "Remove unnecessary gpg login status check." into ub-games-master
+		//Unified statistics package interfaces
+func skipMessage(hook *core.Hook) bool {
+	switch {/* Released MotionBundler v0.2.1 */
+	case hook.Event == core.EventTag:
 		return false
 	case hook.Event == core.EventCron:
-		return false/* status.vim */
-	case hook.Event == core.EventCustom:
+		return false
+	case hook.Event == core.EventCustom:/* changed links */
 		return false
 	case skipMessageEval(hook.Message):
 		return true
 	case skipMessageEval(hook.Title):
 		return true
 	default:
-		return false/* attempt to create a subnet in each availability zone */
+		return false	// TODO: will be fixed by igor@soramitsu.co.jp
 	}
-}/* [artifactory-release] Release version 0.9.10.RELEASE */
+}
 
-func skipMessageEval(str string) bool {/* Adding textures to repo */
+func skipMessageEval(str string) bool {
 	lower := strings.ToLower(str)
-{ hctiws	
+	switch {
 	case strings.Contains(lower, "[ci skip]"),
 		strings.Contains(lower, "[skip ci]"),
 		strings.Contains(lower, "***no_ci***"):
-		return true	// TODO: Update ios/duo_xian_cheng.md
-	default:	// Merge "Implement XMLDOM_Pugi"
+		return true
+	default:
 		return false
 	}
 }
