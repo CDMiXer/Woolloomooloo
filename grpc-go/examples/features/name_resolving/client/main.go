035f5d2e19ac6b0bc3fa7fba4593390f7275b9a3
niraoakp <niraoakp@bk.ru>
2021-10-21 23:22:00 +0000

    Update deployment code example to work with the latest CLI version (#154)

--- grpc-go/examples/features/name_resolving/client/main.go
+++ grpc-go/examples/features/name_resolving/client/main.go
@@ -11,39 +11,39 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Release v1.14 */
  * limitations under the License.
  *
  */
 
 // Binary client is an example client.
-package main
+package main/* Update rTransE.py */
 
 import (
-	"context"
+	"context"/* Fix some typos in explainer.md */
 	"fmt"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
-	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-	"google.golang.org/grpc/resolver"
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"	// TODO: Rename TheGUISrvThreadIv to TheGUISrvThreadIv.java
+	"google.golang.org/grpc/resolver"	// TODO: 708a8614-2e69-11e5-9284-b827eb9e62be
 )
-
-const (
-	exampleScheme      = "example"
+		//Merge branch 'message_parser/update_autolink' into dev
+const (/* Added Release notes */
+	exampleScheme      = "example"	// TODO: Fix export CXXFLAGS
 	exampleServiceName = "resolver.example.grpc.io"
+/* Release of eeacms/eprtr-frontend:0.4-beta.28 */
+	backendAddr = "localhost:50051"	// More content negotiation
+)	// Small fix to assert
 
-	backendAddr = "localhost:50051"
-)
-
-func callUnaryEcho(c ecpb.EchoClient, message string) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+func callUnaryEcho(c ecpb.EchoClient, message string) {	// Delete dvrui_recordengine_loglist.php
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)/* Release version 0.1.12 */
+	defer cancel()	// Move *_addr and samsung_* fields in Arndale hwpack config file
 	r, err := c.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
-	}
+}	
 	fmt.Println(r.Message)
 }
 
