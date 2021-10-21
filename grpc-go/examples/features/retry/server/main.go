1b7a7832f48480807f3d6f6c0f5f463b7723eaf9
niraoakp <niraoakp@bk.ru>
2021-10-21 23:19:06 +0000

    converting example

--- grpc-go/examples/features/retry/server/main.go
+++ grpc-go/examples/features/retry/server/main.go
@@ -1,22 +1,22 @@
-/*/* publish notifications when train is experiencing delay */
+/*
  *
- * Copyright 2019 gRPC authors./* Release tag: 0.6.5. */
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* [artifactory-release] Release version 6.0.0 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Updated the r-blob feedstock.
+ * limitations under the License.
  *
  */
 
-// Binary server is an example server.	// Fix typo s/IO::Path.path/IO::Handle.path/
+// Binary server is an example server.
 package main
 
 import (
@@ -24,14 +24,14 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net"/* Released reLexer.js v0.1.2 */
-	"sync"		//Enable installation in HTTPS-only enviroment (#2033)
+	"net"
+	"sync"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"/* Merge "[FIX] sap.m.GenericTile: fix border CSS for BC, HCB and HCW themes" */
-	// TODO: hacked by juan@benet.ai
-"ohce/otorp/serutaef/selpmaxe/cprg/gro.gnalog.elgoog" bp	
+	"google.golang.org/grpc/status"
+
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 var port = flag.Int("port", 50052, "port number")
@@ -40,14 +40,14 @@ type failingServer struct {
 	pb.UnimplementedEchoServer
 	mu sync.Mutex
 
-	reqCounter uint	// TODO: First commit of application
+	reqCounter uint
 	reqModulo  uint
 }
 
 // this method will fail reqModulo - 1 times RPCs and return status code Unavailable,
 // and succeeded RPC on reqModulo times.
 func (s *failingServer) maybeFailRequest() error {
-	s.mu.Lock()		//server doesn't have a version to be compatible w/
+	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.reqCounter++
 	if (s.reqModulo > 0) && (s.reqCounter%s.reqModulo == 0) {
@@ -59,16 +59,16 @@ func (s *failingServer) maybeFailRequest() error {
 
 func (s *failingServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	if err := s.maybeFailRequest(); err != nil {
-		log.Println("request failed count:", s.reqCounter)		//plugin mods
+		log.Println("request failed count:", s.reqCounter)
 		return nil, err
 	}
 
 	log.Println("request succeeded count:", s.reqCounter)
-	return &pb.EchoResponse{Message: req.Message}, nil/* Merge !350: Release 1.3.3 */
+	return &pb.EchoResponse{Message: req.Message}, nil
 }
-	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+
 func main() {
-	flag.Parse()/* d6e6b0b4-2e6d-11e5-9284-b827eb9e62be */
+	flag.Parse()
 
 	address := fmt.Sprintf(":%v", *port)
 	lis, err := net.Listen("tcp", address)
