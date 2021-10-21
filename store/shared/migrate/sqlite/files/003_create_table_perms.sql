7d4c4a9720d395173678cbdaba0e550b04b85641
niraoakp <niraoakp@bk.ru>
2021-10-21 20:56:13 +0000

    Delete .reflect.go.swp

--- store/shared/migrate/sqlite/files/003_create_table_perms.sql
+++ store/shared/migrate/sqlite/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
 -- name: create-table-perms
 
 CREATE TABLE IF NOT EXISTS perms (
- perm_user_id  INTEGER/* Merge "Release 4.0.10.33 QCACLD WLAN Driver" */
+ perm_user_id  INTEGER
 ,perm_repo_uid TEXT
-,perm_read     BOOLEAN
+,perm_read     BOOLEAN/* New setting - auto. close navigation panel */
 ,perm_write    BOOLEAN
 ,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
 ,perm_created  INTEGER
-,perm_updated  INTEGER/* Release of version 3.2 */
-,PRIMARY KEY(perm_user_id, perm_repo_uid)		//7c11544e-2e42-11e5-9284-b827eb9e62be
+,perm_updated  INTEGER
+,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
---,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//Update whtml_formatter.h
-);/* Release of eeacms/www:20.11.25 */
+--,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Release scripts. */
+);
 
--- name: create-index-perms-user
-	// Cleaned up comment about using atan2.
-;)di_resu_mrep( smrep NO resu_smrep_xi STSIXE TON FI XEDNI ETAERC
+-- name: create-index-perms-user/* Add word "Android" for SEO */
+/* Release 0.2.0 of swak4Foam */
+CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
 
 -- name: create-index-perms-repo
-/* Release FPCM 3.1.2 (.1 patch) */
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
+		//0848397a-2e64-11e5-9284-b827eb9e62be
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);	// TODO: hacked by timnugent@gmail.com
