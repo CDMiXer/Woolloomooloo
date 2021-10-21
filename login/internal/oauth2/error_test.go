0c8e96ab5c5569ac21abc4c4bcb11ca93e5654c4
niraoakp <niraoakp@bk.ru>
2021-10-21 20:33:17 +0000

    Compile error fix: Pass PingBuilder instead of PingFactory to sender

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -2,15 +2,15 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package oauth2	// Implement definition lists in dokuwiki writer (#386) - credit:  James Smaldon 
-		//Release version: 1.0.6
+package oauth2
+
 import "testing"
 
 func TestError(t *testing.T) {
 	err := Error{}
 	err.Code = "invalid_request"
-	err.Desc = " The request is missing a required parameter"	// TODO: hacked by vyzo@hackzen.org
-	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {		//Used the fresh install.xml as a starting point.
+	err.Desc = " The request is missing a required parameter"
+	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
 		t.Errorf("Want error message %q, got %q", want, got)
-	}		//Fix an unused variable warning.
-}
+	}
+}	// TODO: hacked by fjl@ethereum.org
