7810246e96717fd4e890fbc5e591f6e23618701e
niraoakp <niraoakp@bk.ru>
2021-10-21 16:12:07 +0000

    Merge "irqchip: gic-v3: Fix out of bounds access to cpu_logical_map"

--- tests/integration/dependency_steps/dependency_steps_test.go
+++ tests/integration/dependency_steps/dependency_steps_test.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-
+/* Release of eeacms/eprtr-frontend:0.3-beta.9 */
 package ints
-
+	// TODO: Initial import from old repository with incremented version number to 2.2.
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
-	// TODO: Fix zoom issue
+)/* ✏️ Update with our new logo */
+
 // TestDependencySteps tests a case where the dependency graph between two
-// resources is inverted between updates. The snapshot should be robust to this
+// resources is inverted between updates. The snapshot should be robust to this/* Fix some build_dir issues when the package was at the leaf of the ackage tree. */
 // case and still produce a snapshot in a valid topological sorting of the dependency graph.
 func TestDependencySteps(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Fin de journée */
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},	// Enable the timeless skin on metawiki
+		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
 			},
-		},/* Release should run also `docu_htmlnoheader` which is needed for the website */
+		},
 	})
-}	// TODO: will be fixed by onhardev@bk.ru
+}/* Debugged overlap function between tree masks */
