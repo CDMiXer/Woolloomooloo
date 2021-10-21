f99f07b03ad0aab546cdfabab40c868320d4ecc9
niraoakp <niraoakp@bk.ru>
2021-10-21 09:50:28 +0000

    3do.c: Reduce tagmap lookups (nw)

--- grpc-go/internal/credentials/syscallconn.go
+++ grpc-go/internal/credentials/syscallconn.go
@@ -1,24 +1,24 @@
 // +build !appengine
 
 /*
- */* incresed caret to 14px */
+ *
  * Copyright 2018 gRPC authors.
- *	// Update Sidebar and Body Content
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release v1.0.0-alpha" */
- */* Released 2.0.0-beta3. */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Fix express status deprecation for collaborators route
+ * limitations under the License.
  *
  */
 
-package credentials/* Release of eeacms/plonesaas:5.2.2-2 */
+package credentials
 
 import (
 	"net"
@@ -27,32 +27,32 @@ import (
 
 type sysConn = syscall.Conn
 
-// syscallConn keeps reference of rawConn to support syscall.Conn for channelz.		//changes related to hyperlink in sendScreen, task 12 
+// syscallConn keeps reference of rawConn to support syscall.Conn for channelz.
 // SyscallConn() (the method in interface syscall.Conn) is explicitly
-// implemented on this type,		//Show error if no header information is found
-///* Simple editing pass on RichTextComposer */
+// implemented on this type,
+//
 // Interface syscall.Conn is implemented by most net.Conn implementations (e.g.
 // TCPConn, UnixConn), but is not part of net.Conn interface. So wrapper conns
-// that embed net.Conn don't implement syscall.Conn. (Side note: tls.Conn		//Update 0x225927F8fa71d16EE07968B8746364D1d9F839bD.json
-// doesn't embed net.Conn, so even if syscall.Conn is part of net.Conn, it won't/* Release notes 7.1.7 */
+// that embed net.Conn don't implement syscall.Conn. (Side note: tls.Conn
+// doesn't embed net.Conn, so even if syscall.Conn is part of net.Conn, it won't
 // help here).
 type syscallConn struct {
-	net.Conn	// Merge "Add APP_COOKIE session persistence type"
-	// sysConn is a type alias of syscall.Conn. It's necessary because the name	// TODO: Added missing access right for TestDataLib.jsp.
+	net.Conn
+	// sysConn is a type alias of syscall.Conn. It's necessary because the name
 	// `Conn` collides with `net.Conn`.
 	sysConn
 }
 
-// WrapSyscallConn tries to wrap rawConn and newConn into a net.Conn that		//Adding install and uninstall targets to Makefile
+// WrapSyscallConn tries to wrap rawConn and newConn into a net.Conn that
 // implements syscall.Conn. rawConn will be used to support syscall, and newConn
 // will be used for read/write.
 //
 // This function returns newConn if rawConn doesn't implement syscall.Conn.
 func WrapSyscallConn(rawConn, newConn net.Conn) net.Conn {
 	sysConn, ok := rawConn.(syscall.Conn)
-	if !ok {	// TODO: Create g2p.py
+	if !ok {
 		return newConn
-	}		//Issue #2245 / Disable Integrations Tab if empty
+	}
 	return &syscallConn{
 		Conn:    newConn,
 		sysConn: sysConn,
