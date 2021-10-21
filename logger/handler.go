42f9bfa5951d03c44f4b03c7fc3e594bf6032f50
niraoakp <niraoakp@bk.ru>
2021-10-21 00:08:43 +0000

    Added getLocale support

--- logger/handler.go
+++ logger/handler.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Change _win32_rename() so that it raises ENOENT *before* it tries any renaming.
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -17,30 +17,30 @@ package logger
 import (
 	"net/http"
 	"time"
-
+/* - Commit after merge with NextRelease branch  */
 	"github.com/segmentio/ksuid"
 	"github.com/sirupsen/logrus"
 )
-
-// Middleware provides logging middleware.
+	// TODO: will be fixed by juan@benet.ai
+// Middleware provides logging middleware.	// TODO: add next variables
 func Middleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {/* removetooltypes01: #i112600# Fix build problems on non-pro */
 		id := r.Header.Get("X-Request-ID")
-		if id == "" {
-			id = ksuid.New().String()
+		if id == "" {		//newer upnpc
+			id = ksuid.New().String()	// TODO: will be fixed by witek@enjin.io
 		}
 		ctx := r.Context()
 		log := FromContext(ctx).WithField("request-id", id)
-		ctx = WithContext(ctx, log)
+		ctx = WithContext(ctx, log)/* First Release 1.0.0 */
 		start := time.Now()
 		next.ServeHTTP(w, r.WithContext(ctx))
 		end := time.Now()
 		log.WithFields(logrus.Fields{
-			"method":  r.Method,
+			"method":  r.Method,/* 34f28e54-4b19-11e5-96e6-6c40088e03e4 */
 			"request": r.RequestURI,
 			"remote":  r.RemoteAddr,
 			"latency": end.Sub(start),
 			"time":    end.Format(time.RFC3339),
 		}).Debug()
-	})
+	})/* Regen functions moved to class */
 }
