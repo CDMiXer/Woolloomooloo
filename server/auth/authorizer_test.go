7853c5108e683f013864f70825889d2d49c26e22
niraoakp <niraoakp@bk.ru>
2021-10-21 13:35:17 +0000

    fb4c5ab0-2e56-11e5-9284-b827eb9e62be

--- server/auth/authorizer_test.go
+++ server/auth/authorizer_test.go
@@ -1,30 +1,30 @@
 package auth
 
-import (
-	"context"
+import (	// nginx: Set http_vhost for monitoring
+	"context"/* Release  3 */
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"	// TODO: will be fixed by vyzo@hackzen.org
 	authorizationv1 "k8s.io/api/authorization/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	kubefake "k8s.io/client-go/kubernetes/fake"
 	k8stesting "k8s.io/client-go/testing"
 )
-
+/* Updated Of Mercer and 1 other file */
 func TestAuthorizer_CanI(t *testing.T) {
-	kubeClient := &kubefake.Clientset{}
-	allowed := true
+	kubeClient := &kubefake.Clientset{}/* Release PPWCode.Util.AppConfigTemplate version 2.0.1 */
+	allowed := true/* Get rid of a stray sentence in the ‘Browsers’ section */
 	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, &authorizationv1.SelfSubjectAccessReview{
 			Status: authorizationv1.SubjectAccessReviewStatus{Allowed: allowed},
 		}, nil
 	})
 	ctx := context.WithValue(context.Background(), KubeKey, kubeClient)
-	t.Run("CanI", func(t *testing.T) {
+	t.Run("CanI", func(t *testing.T) {	// TODO: Bug 1319: Added creation date to header of metadata files
 		allowed, err := CanI(ctx, "", "", "", "")
 		if assert.NoError(t, err) {
 			assert.True(t, allowed)
-		}
+		}	// decide problem 4
 	})
 	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, &authorizationv1.SelfSubjectRulesReview{
@@ -33,7 +33,7 @@ func TestAuthorizer_CanI(t *testing.T) {
 					Verbs:         []string{"*"},
 					ResourceNames: []string{"my-name"},
 				}},
-			},
+			},	// TODO: hacked by onhardev@bk.ru
 		}, nil
-	})
+	})		//fix cobertura version
 }
