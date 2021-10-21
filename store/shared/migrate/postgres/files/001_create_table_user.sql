dba15db6e145055d973c6c75618409a3d671e5cc
niraoakp <niraoakp@bk.ru>
2021-10-21 09:29:48 +0000

    New translations 03_p01_ch06_02.md (Spanish, Guatemala)

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -1,12 +1,12 @@
--- name: create-table-users/* Release of eeacms/jenkins-master:2.277.3 */
+-- name: create-table-users
 
 CREATE TABLE IF NOT EXISTS users (
- user_id            SERIAL PRIMARY KEY/* replace coverity badge with license */
-,user_login         VARCHAR(250)		//Added newest board Gerber
+ user_id            SERIAL PRIMARY KEY
+,user_login         VARCHAR(250)
 ,user_email         VARCHAR(500)
 ,user_admin         BOOLEAN
-,user_active        BOOLEAN/* Release: Making ready to release 4.1.3 */
-,user_machine       BOOLEAN/* Release notes: remove spaces before bullet list */
+,user_active        BOOLEAN
+,user_machine       BOOLEAN
 ,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
@@ -15,8 +15,8 @@ CREATE TABLE IF NOT EXISTS users (
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
-,user_oauth_expiry  INTEGER		//Delete hw3.ipynb
-,user_hash          VARCHAR(500)/* Add credits to ru-ru translator */
-,UNIQUE(user_login)/* Add instructions how to build the CAPU unit tests to the README file */
-,UNIQUE(user_hash)/* Create histogram.sql */
-);	// TODO: hacked by hello@brooklynzelenka.com
+,user_oauth_expiry  INTEGER
+,user_hash          VARCHAR(500)
+,UNIQUE(user_login)
+,UNIQUE(user_hash)
+);
