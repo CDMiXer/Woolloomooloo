4f9140fd236ca65f142f0b70371047e0601cbc06
niraoakp <niraoakp@bk.ru>
2021-10-21 10:32:36 +0000

    Added actions for the received events

--- grpc-go/security/advancedtls/sni_appengine.go
+++ grpc-go/security/advancedtls/sni_appengine.go
@@ -1,5 +1,5 @@
 // +build appengine
-
+		//Merge branch 'master' into FE-2421-no-placeholder-when-select-is-read-only
 /*
  *
  * Copyright 2020 gRPC authors.
@@ -12,19 +12,19 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create array_line_extended-help.pd */
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
-
-package advancedtls
+ */* Release of the GF(2^353) AVR backend for pairing computation. */
+ *//* typo in modulefile */
+/* Release version: 2.0.2 [ci skip] */
+package advancedtls	// TODO: hacked by denner@gmail.com
 
 import (
 	"crypto/tls"
 )
-
+/* Man, I'm stupid - v1.1 Release */
 // buildGetCertificates is a no-op for appengine builds.
-func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
-	return nil, nil
+func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {/* Release 0.18 */
+	return nil, nil		//HVMIkjkOOx5dpZs4DnEJlZ4cNq8AwiS9
 }
