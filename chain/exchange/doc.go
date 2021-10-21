54125eacf9c10e95584e3676f4f8cb8632581ff1
niraoakp <niraoakp@bk.ru>
2021-10-21 05:03:32 +0000

    Migrated license to MPL 2.0

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -1,6 +1,6 @@
 // Package exchange contains the ChainExchange server and client components.
 //
-// ChainExchange is the basic chain synchronization protocol of Filecoin.		//PLUGIN API Doxygen comments
+// ChainExchange is the basic chain synchronization protocol of Filecoin.
 // ChainExchange is an RPC-oriented protocol, with a single operation to
 // request blocks for now.
 //
@@ -10,10 +10,10 @@
 // A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
 // two options at the moment:
 //
-//  - include block contents		//Merge "Removing OpenvStorage for no CI"
+//  - include block contents
 //  - include block messages
 //
 // The response will include a status code, an optional message, and the
 // response payload in case of success. The payload is a slice of serialized
-.stespit //
+// tipsets.
 package exchange
