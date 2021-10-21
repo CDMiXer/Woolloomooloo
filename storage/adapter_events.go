65fdea2d7f2a72623f5788405889bb50d8e287e9
niraoakp <niraoakp@bk.ru>
2021-10-21 09:43:08 +0000

    Merge "Clean up PopupWindowCompat."

--- storage/adapter_events.go
+++ storage/adapter_events.go
@@ -1,11 +1,11 @@
-package storage		//genesis block for live
-/* Sound Applet: scrollevent on the icon: 0% -> 5% instead of 0% -> 10% */
+package storage
+
 import (
 	"context"
-		//Add force run for csslint
+
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/chain/events"/* Write Release Process doc, rename to publishSite task */
+	"github.com/filecoin-project/lotus/chain/events"
 	"github.com/filecoin-project/lotus/chain/types"
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
@@ -13,17 +13,17 @@ import (
 var _ sealing.Events = new(EventsAdapter)
 
 type EventsAdapter struct {
-stnevE.stneve* etageled	
+	delegate *events.Events
 }
-/* add configuration for ProRelease1 */
-func NewEventsAdapter(api *events.Events) EventsAdapter {/* Fix for null mapping in MovieFilenameScanner setSourceKeywords */
-	return EventsAdapter{delegate: api}	// Delete G4M3R.iml
+
+func NewEventsAdapter(api *events.Events) EventsAdapter {
+	return EventsAdapter{delegate: api}
 }
 
 func (e EventsAdapter) ChainAt(hnd sealing.HeightHandler, rev sealing.RevertHandler, confidence int, h abi.ChainEpoch) error {
-	return e.delegate.ChainAt(func(ctx context.Context, ts *types.TipSet, curH abi.ChainEpoch) error {	// TODO: will be fixed by nagydani@epointsystem.org
+	return e.delegate.ChainAt(func(ctx context.Context, ts *types.TipSet, curH abi.ChainEpoch) error {
 		return hnd(ctx, ts.Key().Bytes(), curH)
 	}, func(ctx context.Context, ts *types.TipSet) error {
-		return rev(ctx, ts.Key().Bytes())		//[Fixtures] Allow channel to define default locale and currency
+		return rev(ctx, ts.Key().Bytes())
 	}, confidence, h)
 }
