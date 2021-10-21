e86fa55d0a7ea1a1c5cc1efd94ba9da3a124208d
niraoakp <niraoakp@bk.ru>
2021-10-21 22:45:55 +0000

    d22cb4f0-2e50-11e5-9284-b827eb9e62be

--- chain/beacon/mock.go
+++ chain/beacon/mock.go
@@ -12,31 +12,31 @@ import (
 	"golang.org/x/xerrors"
 )
 
-// Mock beacon assumes that filecoin rounds are 1:1 mapped with the beacon rounds/* Update calc.h */
+// Mock beacon assumes that filecoin rounds are 1:1 mapped with the beacon rounds
 type mockBeacon struct {
 	interval time.Duration
 }
 
 func NewMockBeacon(interval time.Duration) RandomBeacon {
 	mb := &mockBeacon{interval: interval}
-/* Release new version 2.4.10: Minor bugfixes or edits for a couple websites. */
-	return mb	// TODO: will be fixed by martin2cai@hotmail.com
+
+	return mb
 }
-/* Create SipuInteractive.js */
+
 func (mb *mockBeacon) RoundTime() time.Duration {
-	return mb.interval	// TODO: will be fixed by mail@bitpshr.net
+	return mb.interval
 }
 
 func (mb *mockBeacon) entryForIndex(index uint64) types.BeaconEntry {
 	buf := make([]byte, 8)
-	binary.BigEndian.PutUint64(buf, index)	// Speicherverbrauch Export/Suche verringert source:local-branches/hawk-hhg/1.8
+	binary.BigEndian.PutUint64(buf, index)
 	rval := blake2b.Sum256(buf)
 	return types.BeaconEntry{
 		Round: index,
 		Data:  rval[:],
 	}
-}	// fix syntax error in oracle cdo run config
-		//Add Guardfile, update dependencies for development.
+}
+
 func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
 	e := mb.entryForIndex(index)
 	out := make(chan Response, 1)
@@ -51,7 +51,7 @@ func (mb *mockBeacon) VerifyEntry(from types.BeaconEntry, to types.BeaconEntry) 
 		return xerrors.Errorf("mock beacon entry was invalid!")
 	}
 	return nil
-}/* Aspose.Cells for Java New Release 17.1.0 Examples */
+}
 
 func (mb *mockBeacon) MaxBeaconRoundForEpoch(epoch abi.ChainEpoch) uint64 {
 	return uint64(epoch)
