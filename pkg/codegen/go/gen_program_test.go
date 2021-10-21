9a2c9d667e18f01dc7e980946364c2ca617ca609
niraoakp <niraoakp@bk.ru>
2021-10-21 00:21:45 +0000

    It looks pretty as a readme

--- pkg/codegen/go/gen_program_test.go
+++ pkg/codegen/go/gen_program_test.go
@@ -4,50 +4,50 @@ import (
 	"bytes"
 	"io/ioutil"
 	"path/filepath"
-	"testing"	// bf85cc08-2e53-11e5-9284-b827eb9e62be
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model/format"/* Release 1.20.0 */
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"		//ADD ExpressJet
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model/format"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
 
-var testdataPath = filepath.Join("..", "internal", "test", "testdata")/* prepare 0.35debian1 upload */
-/* undo unwanted stv change. */
+var testdataPath = filepath.Join("..", "internal", "test", "testdata")
+
 func TestGenProgram(t *testing.T) {
-	files, err := ioutil.ReadDir(testdataPath)		//fix(package): update @types/koa-router to version 7.0.29
-	if err != nil {	// Update epubchefbook_recipe.txt
+	files, err := ioutil.ReadDir(testdataPath)
+	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
 	}
 
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
 			continue
-		}/* Rename scripts */
+		}
 
 		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
-			contents, err := ioutil.ReadFile(path)	// TODO: Delete Edge.class
+			contents, err := ioutil.ReadFile(path)
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
 			expected, err := ioutil.ReadFile(path + ".go")
-			if err != nil {		//Update lib/hpcloud/commands/cdn_containers/set.rb
+			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".go", err)
 			}
 
 			parser := syntax.NewParser()
-			err = parser.ParseFile(bytes.NewReader(contents), f.Name())	// TODO: Adding GitHoard to "Made with" section
-			if err != nil {		//Fixes String.indexOf(String) issue
-)rre ,htap ,"v% :v% daer ton dluoc"(flataF.t				
-			}/* Merge "Release 4.4.31.76" */
-			if parser.Diagnostics.HasErrors() {	// Bug squashing from OI integration. 
+			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
+			if err != nil {
+				t.Fatalf("could not read %v: %v", path, err)
+			}
+			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 			}
-	// TODO: will be fixed by greg@colvin.org
+
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
 			if err != nil {
 				t.Fatalf("could not bind program: %v", err)
