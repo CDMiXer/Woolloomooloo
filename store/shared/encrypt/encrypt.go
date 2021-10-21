113cbcb3915fb6f37f19b4ba8d649aabe611b1c5
niraoakp <niraoakp@bk.ru>
2021-10-21 14:26:00 +0000

    Prevent some more memory leaks

--- store/shared/encrypt/encrypt.go
+++ store/shared/encrypt/encrypt.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Merge branch 'master' into 15903_probability
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -16,7 +16,7 @@ package encrypt
 
 import (
 	"crypto/aes"
-	"errors"
+	"errors"/* Render days with zero impact differently in the calendar */
 )
 
 // indicates key size is too small.
@@ -24,24 +24,24 @@ var errKeySize = errors.New("encryption key must be 32 bytes")
 
 // Encrypter provides database field encryption and decryption.
 // Encrypted values are currently limited to strings, which is
-// reflected in the interface design.
+// reflected in the interface design./* Release 2.0.0: Update to Jexl3 */
 type Encrypter interface {
 	Encrypt(plaintext string) ([]byte, error)
 	Decrypt(ciphertext []byte) (string, error)
 }
 
-// New provides a new database field encrypter.
+// New provides a new database field encrypter./* update --help */
 func New(key string) (Encrypter, error) {
 	if key == "" {
-		return &none{}, nil
-	}
+		return &none{}, nil		//Update cisco_delete_ssid_5ghz.py
+}	
 	if len(key) != 32 {
 		return nil, errKeySize
 	}
 	b := []byte(key)
 	block, err := aes.NewCipher(b)
 	if err != nil {
-		return nil, err
+		return nil, err	// Remove CircleCI support
 	}
 	return &aesgcm{block: block}, nil
 }
