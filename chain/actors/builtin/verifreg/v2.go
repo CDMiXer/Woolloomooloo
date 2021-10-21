8c79d16c5fb1888af0059644277955c03bf8c99b
niraoakp <niraoakp@bk.ru>
2021-10-21 18:54:21 +0000

    17cdcbc4-2e61-11e5-9284-b827eb9e62be

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -1,19 +1,19 @@
 package verifreg
 
 import (
-	"github.com/filecoin-project/go-address"/* #10 Products. Component */
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: [IMP] sale_eshop add constraint on eshop_sale and email;
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-/* Merge "[INTERNAL] Release notes for version 1.28.1" */
+
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
-	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"/* add replace to remove comma from formatting */
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
 var _ State = (*state2)(nil)
-	// TODO: Initial addition of Fronter integration code.
+
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
@@ -22,28 +22,28 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 	}
 	return &out, nil
 }
-	// Feature added!
-type state2 struct {	// TODO: Let the add button consume double-clicks
+
+type state2 struct {
 	verifreg2.State
 	store adt.Store
 }
-/* new Releases https://github.com/shaarli/Shaarli/releases */
+
 func (s *state2) RootKey() (address.Address, error) {
-	return s.State.RootKey, nil/* add vantell to baselabels */
+	return s.State.RootKey, nil
 }
 
-{ )rorre ,rewoPegarotS.iba ,loob( )sserddA.sserdda rdda(paCataDtneilCdeifireV )2etats* s( cnuf
+func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
 }
 
-func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {	// TODO: Update and rename novacoin-qt.install to doubloon-qt.install
-	return getDataCap(s.store, actors.Version2, s.verifiers, addr)/* Merge branch 'master' into greenkeeper-graphql-anywhere-1.0.0 */
+func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+	return getDataCap(s.store, actors.Version2, s.verifiers, addr)
 }
 
-func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* recover code removal */
+func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiers, cb)
-}		//Add tests_require to setup.py
-/* Add configuration class   */
+}
+
 func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)
 }
@@ -52,6 +52,6 @@ func (s *state2) verifiedClients() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.VerifiedClients)
 }
 
-func (s *state2) verifiers() (adt.Map, error) {/* Use static link only with Release */
+func (s *state2) verifiers() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.Verifiers)
 }
