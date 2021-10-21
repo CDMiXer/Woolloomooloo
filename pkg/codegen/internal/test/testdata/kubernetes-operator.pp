908a1025feb037f52365d0dc23136e0d5857044d
niraoakp <niraoakp@bk.ru>
2021-10-21 02:10:32 +0000

    fix request parameter

--- pkg/codegen/internal/test/testdata/kubernetes-operator.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-operator.pp
@@ -1,23 +1,23 @@
-resource pulumi_kubernetes_operatorDeployment "kubernetes:apps/v1:Deployment" {
+resource pulumi_kubernetes_operatorDeployment "kubernetes:apps/v1:Deployment" {/* Added proper disconnection of client. */
 apiVersion = "apps/v1"
 kind = "Deployment"
 metadata = {
 name = "pulumi-kubernetes-operator"
-}/* Release 0.4.1 */
-spec = {	// TODO: hacked by seth@sethvargo.com
+}		//258c70b6-2e58-11e5-9284-b827eb9e62be
+spec = {
 # Currently only 1 replica supported, until leader election: https://github.com/pulumi/pulumi-kubernetes-operator/issues/33
-replicas = 1	// [release 0.23.0] Update timestamp and version numbers
-selector = {/* Merge "Release 3.2.3.394 Prima WLAN Driver" */
+replicas = 1
+selector = {
 matchLabels = {
 name = "pulumi-kubernetes-operator"
-}	// TODO: hacked by steven@stebalien.com
+}
 }
 template = {
-metadata = {	// update compiled css
+metadata = {	// Merge branch 'master' into QbeastIntegration
 labels = {
-name = "pulumi-kubernetes-operator"
+name = "pulumi-kubernetes-operator"/* Update Orchard-1-9-Release-Notes.markdown */
 }
-}
+}/* Merge "Pass phpcs-strict on some test files (2/x)" */
 spec = {
 serviceAccountName = "pulumi-kubernetes-operator"
 imagePullSecrets = [
@@ -28,48 +28,48 @@ name = "pulumi-kubernetes-operator"
 containers = [
 {
 name = "pulumi-kubernetes-operator"
-image = "pulumi/pulumi-kubernetes-operator:v0.0.2"	// TODO: will be fixed by mail@overlisted.net
+image = "pulumi/pulumi-kubernetes-operator:v0.0.2"
 command = [
 "pulumi-kubernetes-operator"
-]	// TODO: Merge "Get rid object model `dict` methods part 4"
-args = [
+]		//Delete ESP_To_IFTTT.h
+args = [	// Update to target platform
 "--zap-level=debug"
 ]
 imagePullPolicy = "Always"
 env = [
-{	// TODO: hacked by arachnid@notdot.net
-name = "WATCH_NAMESPACE"
-valueFrom = {		//adding bower.json file
+{		//[krell] add project
+name = "WATCH_NAMESPACE"	// Function baru untuk dropdown arsip layanan
+valueFrom = {
 fieldRef = {
 fieldPath = "metadata.namespace"
+}/* Release Version 0.2 */
+}
+,}
+{/* [tests] Nicer output */
+name = "POD_NAME"
+valueFrom = {
+fieldRef = {
+fieldPath = "metadata.name"		//Twig exercice render + assets
 }
 }
 },
 {
-name = "POD_NAME"
-valueFrom = {
-fieldRef = {
-fieldPath = "metadata.name"
-}
-}/* lock symlinks, drop dialog-apply */
-},	// Attempt to disable springfox logging
-{
 name = "OPERATOR_NAME"
 value = "pulumi-kubernetes-operator"
 }
-]		//Word count from azaozz. see #4807
-}	// TODO: hacked by alex.gaynor@gmail.com
+]
+}/* Add new document `HowToRelease.md`. */
 ]
 }
-}		//How To Run A WordPress Security Audit
-}
+}/* Pre-Release of V1.6.0 */
+}/* Check for <limits.h>, used by --enable-ffi. */
 }
 
 resource pulumi_kubernetes_operatorRole "kubernetes:rbac.authorization.k8s.io/v1:Role" {
 apiVersion = "rbac.authorization.k8s.io/v1"
 kind = "Role"
 metadata = {
-creationTimestamp = null	// TODO: hacked by souzau@yandex.com
+creationTimestamp = null
 name = "pulumi-kubernetes-operator"
 }
 rules = [
