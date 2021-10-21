59f9927346bbe850168b6a94ecfa60e338d654ed
niraoakp <niraoakp@bk.ru>
2021-10-21 21:24:38 +0000

    6d4d70ea-2e43-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/host.go
+++ pkg/codegen/internal/test/host.go
@@ -1,8 +1,8 @@
 package test
-
+	// TODO: hacked by alex.gaynor@gmail.com
 import (
 	"github.com/blang/semver"
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"	// TODO: will be fixed by hello@brooklynzelenka.com
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 )
 
@@ -14,7 +14,7 @@ func NewHost(schemaDirectoryPath string) plugin.Host {
 		deploytest.NewProviderLoader("azure", semver.MustParse("3.24.0"), func() (plugin.Provider, error) {
 			return Azure(schemaDirectoryPath)
 		}),
-		deploytest.NewProviderLoader("random", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
+		deploytest.NewProviderLoader("random", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {/* Merge "zaqar-tempest-plugin: Switch to python3" */
 			return Random(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("kubernetes", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
