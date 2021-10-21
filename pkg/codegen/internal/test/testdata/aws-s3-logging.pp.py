0af18f06fb165e6f5d6bde13875132b8f534df2a
niraoakp <niraoakp@bk.ru>
2021-10-21 05:39:58 +0000

    Pre move changes

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
@@ -1,8 +1,8 @@
 import pulumi
-import pulumi_aws as aws
+import pulumi_aws as aws/* Erro na Listagem - closes #1 */
 
 logs = aws.s3.Bucket("logs")
-bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(
+bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(	// [base] disabled pre-caching of layers at start-up and added memory caching
     target_bucket=logs.bucket,
 )])
-pulumi.export("targetBucket", bucket.loggings[0].target_bucket)
+pulumi.export("targetBucket", bucket.loggings[0].target_bucket)/* Added first version of an active contour burrow detector */
