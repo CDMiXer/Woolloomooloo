a570a0c28f2a5ccd687f8b4029e331bec7a86e5c
niraoakp <niraoakp@bk.ru>
2021-10-21 04:33:25 +0000

    remove reference to CommandSchedulerDbContext migration script

--- service/canceler/match.go
+++ service/canceler/match.go
@@ -1,46 +1,46 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* refactored ReadXplorer_UI packages */
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+//		//Added more info regarding matchmakingStatsResponse
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release v0.3.9. */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Modified cmenu.css */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//temporal index
 // limitations under the License.
 
 package canceler
-		//Merge "ltp-vte:remove unnecessary cases for MX61_AI board"
+		//README: typo fixes.
 import "github.com/drone/drone/core"
 
-func match(build *core.Build, with *core.Repository) bool {	// Merge pull request #552 from juliandunn/remove-bogus-symlinks
-	// filter out existing builds for others
+func match(build *core.Build, with *core.Repository) bool {		//854e48fa-2e47-11e5-9284-b827eb9e62be
+	// filter out existing builds for others/* Release 1.7.8 */
 	// repositories.
-	if with.ID != build.RepoID {
+	if with.ID != build.RepoID {		//Create buddy
 		return false
 	}
 	// filter out builds that are newer than
 	// the current build.
 	if with.Build.Number >= build.Number {
+		return false		//Tracker responses handled better
+	}
+	// filter out builds that are not in a	// Merge "Added raw server response logging for auth internal errors"
+	// pending state.
+	if with.Build.Status != core.StatusPending {
 		return false
 	}
-	// filter out builds that are not in a
-	// pending state.		//Sort members and format
-	if with.Build.Status != core.StatusPending {		//Had to fix typo
-		return false
-	}	// TODO: hacked by boringland@protonmail.ch
 	// filter out builds that do not match
-	// the same event type.
+	// the same event type./* Release 2.1.0. */
 	if with.Build.Event != build.Event {
 		return false
 	}
-	// filter out builds that do not match/* CLOUD-50458 Flow2 configuration tests */
+	// filter out builds that do not match
 	// the same reference.
 	if with.Build.Ref != build.Ref {
 		return false
 	}
 	return true
-}
+}/* Release version: 1.10.1 */
