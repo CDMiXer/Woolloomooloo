8b482699dd7b4aca5c15ce99342cb822146c6607
niraoakp <niraoakp@bk.ru>
2021-10-21 14:15:58 +0000

    display node id (preferences->appearance)

--- handler/api/repos/crons/none.go
+++ handler/api/repos/crons/none.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release 0.23.0. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,43 +11,43 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Fix CHANGELOG typos */
+
 // +build oss
-		//Added Compiled version :)
+
 package crons
 
 import (
-	"net/http"/* 4952537a-2e1d-11e5-affc-60f81dce716c */
-/* Create gen.php */
+	"net/http"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)	// Updated Grammar File from 11.9 to 11.14
+	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
 func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
 
-func HandleUpdate(core.RepositoryStore, core.CronStore) http.HandlerFunc {/* Release notes 6.7.3 */
-	return notImplemented/* Merge "Gerrit 2.4 ReleaseNotes" into stable-2.4 */
+func HandleUpdate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
+	return notImplemented
 }
 
 func HandleDelete(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
-}	// Create Shema1.png
-/* Omit existing fields in JavaBeanConverter (XSTR-579). */
+}
+
 func HandleFind(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
 
 func HandleList(core.RepositoryStore, core.CronStore) http.HandlerFunc {
-	return notImplemented	// TODO: Add debugging and consistency check functions to SgUctTree
+	return notImplemented
 }
-/* Release 0.10.1 */
-func HandleExec(core.UserStore, core.RepositoryStore, core.CronStore,	// Rename alexrodriguez.md to aroduribe.md
+
+func HandleExec(core.UserStore, core.RepositoryStore, core.CronStore,
 	core.CommitService, core.Triggerer) http.HandlerFunc {
 	return notImplemented
 }
