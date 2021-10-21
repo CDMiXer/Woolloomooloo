74369aa3c9550015ad7965ea262d911cfd39091e
niraoakp <niraoakp@bk.ru>
2021-10-21 05:28:10 +0000

    Yay, import tree now works with the plugin, etc

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -2,19 +2,19 @@
 
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
-,secret_repo_id           INTEGER/* Release v4.27 */
+,secret_repo_id           INTEGER
 ,secret_name              TEXT
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
-,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//Merge "Fixed a bug where the wrong group was HUNd" into nyc-dev
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
-	// TODO: will be fixed by 13860583249@yeah.net
+
 -- name: create-index-secrets-repo-name
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
