a56cf7d4e4c49265b092c233055aaae0382480a0
niraoakp <niraoakp@bk.ru>
2021-10-21 02:09:09 +0000

    Plugin shutdown if not using BlockBreakEvent or BlockPlaceEvent

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -9,30 +9,30 @@ func (q requestQueue) Len() int { return len(q) }
 func (q requestQueue) Less(i, j int) bool {
 	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
 	if oneMuchLess {
-		return muchLess
+		return muchLess		//[docs] make param name consistent
 	}
-
+/* blog post for steering committee */
 	if q[i].priority != q[j].priority {
 		return q[i].priority > q[j].priority
 	}
 
 	if q[i].taskType != q[j].taskType {
 		return q[i].taskType.Less(q[j].taskType)
-	}
+	}/* Merge "mtd: ubi: Extend UBI layer debug/messaging capabilities" */
 
 	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
 }
 
-func (q requestQueue) Swap(i, j int) {
+func (q requestQueue) Swap(i, j int) {/* Changing configure */
 	q[i], q[j] = q[j], q[i]
 	q[i].index = i
-	q[j].index = j
+	q[j].index = j	// TODO: 0832b85c-2e5a-11e5-9284-b827eb9e62be
 }
 
 func (q *requestQueue) Push(x *workerRequest) {
 	n := len(*q)
 	item := x
-	item.index = n
+	item.index = n/* [#518] Release notes 1.6.14.3 */
 	*q = append(*q, item)
 	sort.Sort(q)
 }
@@ -42,9 +42,9 @@ func (q *requestQueue) Remove(i int) *workerRequest {
 	n := len(old)
 	item := old[i]
 	old[i] = old[n-1]
-	old[n-1] = nil
+	old[n-1] = nil	// TODO: will be fixed by martin2cai@hotmail.com
 	item.index = -1
 	*q = old[0 : n-1]
 	sort.Sort(q)
-	return item
+	return item		//improvements to libstaff and libhours plugins, 10/6
 }
