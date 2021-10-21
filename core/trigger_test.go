2850633b5706668f128eab05e3fe28ab43d7da4e
niraoakp <niraoakp@bk.ru>
2021-10-21 06:55:34 +0000

    #12 Use absolute IDs as reference, even if unique attributes exist

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release version 2.3.0.RC1 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Fixing up euca_conf --check to provide better checks and better messages.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Partial undo of the previous commit */
-package core/* Merge "l3 support (partial): move event dispatch from southBoundHandler" */
+
+package core	// TODO: 42a683ba-2e5a-11e5-9284-b827eb9e62be
