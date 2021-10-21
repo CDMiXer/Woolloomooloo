1bd36a9b4a6ac4783d017bdf4bd77f80c7bd2523
niraoakp <niraoakp@bk.ru>
2021-10-21 10:07:48 +0000

    Agora com whitelist tbm

--- store/shared/migrate/mysql/files/007_create_table_logs.sql
+++ store/shared/migrate/mysql/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
 -- name: create-table-logs
-
+		//Added extension download link.
 CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
-,log_data  MEDIUMBLOB
+,log_data  MEDIUMBLOB	// TODO: async -> run_async
 );
