f0c10d766b9971ff0023b15933717e7d95566763
niraoakp <niraoakp@bk.ru>
2021-10-21 17:42:38 +0000

    Added safety null check

--- store/shared/migrate/postgres/files/015_create_table_refs.sql
+++ store/shared/migrate/postgres/files/015_create_table_refs.sql
@@ -1,11 +1,11 @@
--- name: create-table-latest/* Release jedipus-2.6.10 */
+-- name: create-table-latest
 
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
-,latest_type     VARCHAR(50)		//Merge "Add releasenotes jobs to murano"
+)05(RAHCRAV     epyt_tsetal,
 ,latest_name     VARCHAR(500)
-,latest_created  INTEGER
+,latest_created  INTEGER/* e7112666-2e65-11e5-9284-b827eb9e62be */
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
