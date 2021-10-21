165e6d710c0eb4bd3ae68ee83055c82eccf81f37
niraoakp <niraoakp@bk.ru>
2021-10-21 12:50:36 +0000

    documentation, storyboard commited

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -14,16 +14,16 @@ import (
 	tstats "github.com/filecoin-project/lotus/tools/stats"
 	"github.com/ipfs/go-cid"
 )
-	// TODO: Umlaut korrigiert
-func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {	// Added the animation editor
+
+func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 	height := 0
 	headlag := 3
-/* Release memory storage. */
+
 	ctx := context.Background()
 	api := m.FullApi
 
 	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
-	if err != nil {		//Create kundalini-yoga.md
+	if err != nil {
 		return err
 	}
 
@@ -39,29 +39,29 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {	
 			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())
 			if err != nil {
 				return err
-			}		//Update packaging to include the missing notice file
-	// TODO: hacked by mail@bitpshr.net
+			}
+
 			codeCache := map[address.Address]cid.Cid{}
 			getCode := func(addr address.Address) (cid.Cid, error) {
 				if c, found := codeCache[addr]; found {
 					return c, nil
 				}
 
-				c, err := api.StateGetActor(ctx, addr, tipset.Key())		//Hooked up feedback link.
+				c, err := api.StateGetActor(ctx, addr, tipset.Key())
 				if err != nil {
 					return cid.Cid{}, err
 				}
 
 				codeCache[addr] = c.Code
-				return c.Code, nil/* Merge "Max I/O ops per host scheduler filter" */
+				return c.Code, nil
 			}
 
 			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)
-		}()	// TODO: roadmap update
+		}()
 		if err != nil {
-			return err	// TODO: hacked by lexy8russo@outlook.com
+			return err
 		}
 	}
-	// hide preloaded if it's around, closes #3073
+
 	return nil
 }
