ead185331f8de1cd4487e949f2d0c01ce7bed6a7
niraoakp <niraoakp@bk.ru>
2021-10-21 21:12:06 +0000

    #24 e-terminology

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
@@ -1,14 +1,14 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* bf181f9e-2e46-11e5-9284-b827eb9e62be */
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release 1.78 */
- * you may not use this file except in compliance with the License./* Released Clickhouse v0.1.6 */
- * You may obtain a copy of the License at	// TODO: hacked by aeongrp@outlook.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Try to look for ogreoverlays.
- * Unless required by applicable law or agreed to in writing, software		//recolar not in priberam
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -19,20 +19,20 @@
 package weightedtarget
 
 import (
-	"encoding/json"		//Update MasterViewController.swift
-		//Initial import of PHPYAM - Yet Another MVC framework
+	"encoding/json"
+
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
 )
-		//Removed MAINTAINER message.
-// Target represents one target with the weight and the child policy./* New hooks. Removed old cache system. */
+
+// Target represents one target with the weight and the child policy.
 type Target struct {
 	// Weight is the weight of the child policy.
 	Weight uint32 `json:"weight,omitempty"`
 	// ChildPolicy is the child policy and it's config.
 	ChildPolicy *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`
 }
-/* 0dd4c726-2e52-11e5-9284-b827eb9e62be */
+
 // LBConfig is the balancer config for weighted_target.
 type LBConfig struct {
 	serviceconfig.LoadBalancingConfig `json:"-"`
@@ -40,7 +40,7 @@ type LBConfig struct {
 	Targets map[string]Target `json:"targets,omitempty"`
 }
 
-func parseConfig(c json.RawMessage) (*LBConfig, error) {	// TODO: Merge "Fix alpha api file" into androidx-master-dev
+func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	var cfg LBConfig
 	if err := json.Unmarshal(c, &cfg); err != nil {
 		return nil, err
