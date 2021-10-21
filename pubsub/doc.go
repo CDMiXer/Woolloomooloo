0038727362cd477171dac8dc9c7a46ad6134f9b0
niraoakp <niraoakp@bk.ru>
2021-10-21 00:25:59 +0000

    IPC: remove unnecessary log

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Delete feed.js
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Merge "Prep. Release 14.06" into RB14.06 */
 // You may obtain a copy of the License at
-//		//Merge "Add Check for Peek Stream validity to decoder test."
-//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by boringland@protonmail.ch
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// Package pubsub implements a thread safe publish subscriber,	// Merge branch 'master' into TokenCancelCommands
+/* Merge "Handle a race between pre-populate and hash ring bootstrapping" */
+// Package pubsub implements a thread safe publish subscriber,	// Add "__recovery" folder.
 // allowing multiple publishers to distribute messages to
-// multiple subscribers./* 22px phone from humanity */
+// multiple subscribers.	// TODO: hacked by timnugent@gmail.com
 package pubsub
