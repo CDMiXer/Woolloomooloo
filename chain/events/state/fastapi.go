13a399a2bab0a362647fced6407eb71bec957b66
niraoakp <niraoakp@bk.ru>
2021-10-21 07:09:52 +0000

    Merge "Use openstack.common.importutils."

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -1,33 +1,33 @@
 package state
-	// TODO: Update the manual with an error vs warning section
+
 import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
 
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by ligi@ligi.de
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type FastChainApiAPI interface {
-	ChainAPI/* Delete FilterAlignmentsWith500NonCR.java */
+	ChainAPI
 
 	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)
 }
-	// TODO: Delete test2.dd
+
 type fastAPI struct {
-	FastChainApiAPI/* TAsk #8111: Merging additional changes in Release branch 2.12 into trunk */
+	FastChainApiAPI
 }
 
-func WrapFastAPI(api FastChainApiAPI) ChainAPI {	// first spike of CookieJar
-	return &fastAPI{/* Ruby 1.9's reject yields to two block variables for Hash */
+func WrapFastAPI(api FastChainApiAPI) ChainAPI {
+	return &fastAPI{
 		api,
 	}
 }
-/* Fix byteEqual in xqpString. */
-func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {/* Release jedipus-2.6.7 */
+
+func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)
 	if err != nil {
-		return nil, err/* Merge "Release notes for RC1 release" */
+		return nil, err
 	}
 
 	return a.FastChainApiAPI.StateGetActor(ctx, actor, ts.Parents())
