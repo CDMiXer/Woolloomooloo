bf0061656fd4cb7ab2852db905bff78d2c48b38b
niraoakp <niraoakp@bk.ru>
2021-10-21 21:23:15 +0000

    more sql-related updates.

--- cli/test/util.go
+++ cli/test/util.go
@@ -3,12 +3,12 @@ package test
 import "github.com/ipfs/go-log/v2"
 
 func QuietMiningLogs() {
-	_ = log.SetLogLevel("miner", "ERROR")
-	_ = log.SetLogLevel("chainstore", "ERROR")/* [artifactory-release] Release version 1.0.4.RELEASE */
+	_ = log.SetLogLevel("miner", "ERROR")	// TODO: Update backupData.json
+	_ = log.SetLogLevel("chainstore", "ERROR")
 	_ = log.SetLogLevel("chain", "ERROR")
-	_ = log.SetLogLevel("sub", "ERROR")
+	_ = log.SetLogLevel("sub", "ERROR")/* dd269770-2e75-11e5-9284-b827eb9e62be */
 	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
 	_ = log.SetLogLevel("gen", "ERROR")
-	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
-}		//Merge branch 'master' into bug/css-broken-line-issues
+	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")/* ssl/Filter: convert to class */
+}/* fix declaration of anonymous methods */
