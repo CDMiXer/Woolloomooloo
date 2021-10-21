b8bf7f3c0fc4830ee085c111a29508243e877ab1
niraoakp <niraoakp@bk.ru>
2021-10-21 01:15:03 +0000

    Added QuickSort and creation of random lists.

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using Pulumi;
+using Pulumi;/* Add comment to windows doc */
 
 class MyStack : Stack
 {
-    public MyStack()
-    {	// calc56: merge with OOO330_m1
-        var componentA = new Component("a", new ComponentArgs { Echo = 42 });/* found more */
-        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
-        var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
+    public MyStack()/* Probably better */
+    {
+        var componentA = new Component("a", new ComponentArgs { Echo = 42 });	// TODO: hacked by ligi@ligi.de
+        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });	// TODO: will be fixed by igor@soramitsu.co.jp
+        var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });/* Trying new iteration procedure.... will this work I wonder? */
     }
-}	// TODO: Added log output.
+}/* Create textMe.py */
