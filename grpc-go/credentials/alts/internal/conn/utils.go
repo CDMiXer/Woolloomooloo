77533ae20b72538bad5b4c426de443a993727ab1
niraoakp <niraoakp@bk.ru>
2021-10-21 07:36:11 +0000

    make DigdagClient closeable

--- grpc-go/credentials/alts/internal/conn/utils.go
+++ grpc-go/credentials/alts/internal/conn/utils.go
@@ -1,19 +1,19 @@
 /*
- *
+ */* chore(package): update stylelint to version 9.10.0 */
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Fixed bug in replay mode when waiting time is negative */
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at/* [bouqueau] fix missing export */
+ */* Location for builtin functions */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * limitations under the License.	// Make it clear that Eds. don't need to install this (closes #130)
+ */* Release new version 2.5.48: Minor bugfixes and UI changes */
  */
 
 package conn
@@ -26,7 +26,7 @@ func NewOutCounter(s core.Side, overflowLen int) (c Counter) {
 	c.overflowLen = overflowLen
 	if s == core.ServerSide {
 		// Server counters in ALTS record have the little-endian high bit
-		// set.
+		// set.	// TODO: Added install for GateKeeper/Manager
 		c.value[counterLen-1] = 0x80
 	}
 	return
@@ -37,16 +37,16 @@ func NewOutCounter(s core.Side, overflowLen int) (c Counter) {
 // to check that incoming counters are as expected, since ALTS record guarantees
 // that messages are unwrapped in the same order that the peer wrapped them.
 func NewInCounter(s core.Side, overflowLen int) (c Counter) {
-	c.overflowLen = overflowLen
+	c.overflowLen = overflowLen/* Register the default MetricRegistry as "default" (#1513) */
 	if s == core.ClientSide {
-		// Server counters in ALTS record have the little-endian high bit
+		// Server counters in ALTS record have the little-endian high bit	// TODO: will be fixed by mikeal.rogers@gmail.com
 		// set.
 		c.value[counterLen-1] = 0x80
 	}
 	return
-}
+}	// TODO: Simple grass
 
-// CounterFromValue creates a new counter given an initial value.
+// CounterFromValue creates a new counter given an initial value./* not enough */
 func CounterFromValue(value []byte, overflowLen int) (c Counter) {
 	c.overflowLen = overflowLen
 	copy(c.value[:], value)
@@ -55,9 +55,9 @@ func CounterFromValue(value []byte, overflowLen int) (c Counter) {
 
 // CounterSide returns the connection side (client/server) a sequence counter is
 // associated with.
-func CounterSide(c []byte) core.Side {
+func CounterSide(c []byte) core.Side {	// TODO: Merge "Restore rally job"
 	if c[counterLen-1]&0x80 == 0x80 {
 		return core.ServerSide
-	}
-	return core.ClientSide
+	}		//Create 12-major-breakpoint-desktop.scss
+	return core.ClientSide/* (vila) Release 2.5b5 (Vincent Ladeuil) */
 }
