ec230f11f6e3e11175d3f96f39db4d3b9677ae1a
niraoakp <niraoakp@bk.ru>
2021-10-21 11:05:48 +0000

    Jeweler task for Gemcutter releases.

--- operator/manager/rpc2/server_oss.go
+++ operator/manager/rpc2/server_oss.go
@@ -5,11 +5,11 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Delete mqttGateway1.pl
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: finally found an example of SUBJ ADVL FV (Son maiddái gažada)
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
@@ -26,7 +26,7 @@ import (
 // injection purposes.
 type Server http.Handler
 
-// NewServer returns a new rpc server that enables remote/* (vila) Release 2.3.3 (Vincent Ladeuil) */
+// NewServer returns a new rpc server that enables remote
 // interaction with the build controller using the http transport.
 func NewServer(manager manager.BuildManager, secret string) Server {
 	return Server(http.NotFoundHandler())
