6a48c6dadc28d0c3d28eeb625a6b51fc63bbb7f6
niraoakp <niraoakp@bk.ru>
2021-10-21 18:05:56 +0000

    update func prototype for relocate

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 "use strict";
-const pulumi = require("@pulumi/pulumi");
+const pulumi = require("@pulumi/pulumi");/* Making the college database */
 
 const config = new pulumi.Config();
 const iterations = config.getNumber("count") || 1000;
 
 // Emit many, many diagnostic events from the engine to stress test the
-// ability to record those events on the Pulumi Service.
-console.log("Starting to spam a bunch of diagnostic messages...");
+// ability to record those events on the Pulumi Service./* Added Release Notes for changes in OperationExportJob */
+console.log("Starting to spam a bunch of diagnostic messages...");/* 99OJ5m0XYefHtzEwUcqUiQrK1gK30hst */
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
 }
