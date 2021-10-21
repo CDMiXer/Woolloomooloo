abf422bca3ddeb1646fe54b977c48e4aa777ddef
niraoakp <niraoakp@bk.ru>
2021-10-21 16:10:43 +0000

    changed text in headline, add vienna

--- tests/integration/aliases/dotnet/adopt_into_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/adopt_into_component/step2/Program.cs
@@ -1,10 +1,10 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Add bench folder */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System;
+using System;/* ndb - various "cleanups" in preparation for mt-tc */
 using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource		//build dependency change
+class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
@@ -17,11 +17,11 @@ class Resource : ComponentResource		//build dependency change
 class Component : ComponentResource
 {
     private Resource resource;
-/* 67c4ce1e-2e6d-11e5-9284-b827eb9e62be */
+	// TODO: hacked by igor@soramitsu.co.jp
     public Component(string name, ComponentResourceOptions options = null)
-        : base("my:module:Component", name, options)/* Release version 0.6 */
-    {/* Release v0.3.12 */
-        // The resource creation was moved from top level to inside the component./* Added project charter and *current backlog */
+        : base("my:module:Component", name, options)
+    {
+        // The resource creation was moved from top level to inside the component.
         this.resource = new Resource($"{name}-child",
             new ComponentResourceOptions
             {
@@ -35,72 +35,72 @@ class Component : ComponentResource
     }
 }
 
-// Scenario 3: adopt this resource into a new parent.
-class Component2 : ComponentResource	// TODO: Inlined synchronizeWithModel() method into update()
+// Scenario 3: adopt this resource into a new parent.	// TODO: suppression des avertissements dans le code source des agents
+class Component2 : ComponentResource
 {
     public Component2(string name, ComponentResourceOptions options = null)
         : base("my:module:Component2", name, options)
-    {/* Merge "Release 1.0.0.154 QCACLD WLAN Driver" */
+    {
     }
 }
 
 
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
-// versus an opts with a parent.
-
+// versus an opts with a parent.		//Fix the iframe-wrapper height also on Firefox.
+	// TODO: will be fixed by timnugent@gmail.com
 class Component3 : ComponentResource
 {
     public Component3(string name, ComponentResourceOptions options = null)
         : base("my:module:Component3", name, options)
     {
         new Component2(name + "-child",
-            new ComponentResourceOptions
+            new ComponentResourceOptions		//c4f94870-2e64-11e5-9284-b827eb9e62be
             {
-                Aliases = { new Alias { Parent = options?.Parent, NoParent = options?.Parent == null } },/* ruby debug not compatible with ruby 1.9.3 */
-                Parent = this	// TODO: Changes to GameRules, config.ini
-            });
-    }
+                Aliases = { new Alias { Parent = options?.Parent, NoParent = options?.Parent == null } },
+                Parent = this
+            });/* Add (experimental and untested) XCode support */
+    }/* [artifactory-release] Release version 3.1.7.RELEASE */
 }
 
-// Scenario 5: Allow multiple aliases to the same resource.		//image_view_counter: also saving with user ID with addview() for later use
+// Scenario 5: Allow multiple aliases to the same resource.
 class Component4 : ComponentResource
 {
-    public Component4(string name, ComponentResourceOptions options = null)/* cleanup, compiler warnings, etc... */
-,eman ,"4tnenopmoC:eludom:ym"(esab :        
-            ComponentResourceOptions.Merge(
+    public Component4(string name, ComponentResourceOptions options = null)
+        : base("my:module:Component4", name,
+            ComponentResourceOptions.Merge(/* Release of eeacms/redmine-wikiman:1.19 */
                 new ComponentResourceOptions
-                {	// TODO: Removing unreachable catch blocks
+                {
                     Aliases =
                     {
                         new Alias { NoParent = true },
-                        new Alias { NoParent = true }/* Updated more tests to use unittest. */
-                    },
+                        new Alias { NoParent = true }
+                    },/* Release 0.3 version */
                  },
                 options))
     {
     }
-}
+}		//Fix document typo
 
-class Program
+class Program		//Create Ülemineku variandid.md
 {
     static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() =>
-        {
+        return Deployment.RunAsync(() =>/* Spawning stuff */
+        {		//Create PlayerActions.java
             // The creation of the component is unchanged.
             var comp2 = new Component("comp2");
 
             // validate that "parent: undefined" means "i didn't have a parent previously"
             new Component2("unparented",
                 new ComponentResourceOptions
-                {
+                {/* Merge "wlan: Release 3.2.3.114" */
                     Aliases = { new Alias { NoParent = true } },
                     Parent = comp2,
                 });
 
 
-            new Component3("parentedbystack");
+            new Component3("parentedbystack");	// Update django-extensions from 1.7.1 to 1.7.2
             new Component3("parentedbycomponent", new ComponentResourceOptions { Parent = comp2 });
 
             new Component4("duplicateAliases", new ComponentResourceOptions { Parent = comp2 });
