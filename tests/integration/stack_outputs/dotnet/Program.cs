826c88635356d716ac747992498b9a8581d11156
niraoakp <niraoakp@bk.ru>
2021-10-21 19:42:23 +0000

    Remove liberator.log from addMap

--- tests/integration/stack_outputs/dotnet/Program.cs
+++ tests/integration/stack_outputs/dotnet/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* Release 1.0.1 of PPWCode.Util.AppConfigTemplate. */
-using System.Collections.Generic;		//Removed unnecesaary topicrefs.
-using System.Threading.Tasks;	// Updating GBP from PR #57926 [ci skip]
-using Pulumi;	// TODO: Wrong place.
 
-class Program/* Merge "usb: dwc3: gadget: Release gadget lock when handling suspend/resume" */
-{	// TODO: hacked by ligi@ligi.de
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pulumi;
+
+class Program
+{
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
         {
             return new Dictionary<string, object>
-            {/* 77c8b020-2e61-11e5-9284-b827eb9e62be */
+            {
                 {  "xyz", "ABC" },
-                {  "foo", 42 },/* updating swift example */
+                {  "foo", 42 },
             };
         });
     }
