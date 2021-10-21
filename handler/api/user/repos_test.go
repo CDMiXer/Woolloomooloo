d48a122602aadb06b81cbbd90fd1e960b4dbb05f
niraoakp <niraoakp@bk.ru>
2021-10-21 18:21:44 +0000

    Fixed issue were the same host could be included twice from different ips

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -1,55 +1,55 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Add a start button.
 
 package user
 
 import (
-	"encoding/json"
+	"encoding/json"		//Delete naorobot.cpp~
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"testing"
+	"testing"	// TODO: will be fixed by igor@soramitsu.co.jp
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"/* 1.0.0 Production Ready Release */
 	"github.com/drone/drone/core"
-
+		//Some Windows Controller fixes.
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/sirupsen/logrus"
 )
 
-func init() {
+func init() {/* Added entry points to intraday research screen */
 	logrus.SetOutput(ioutil.Discard)
-}
+}	// TODO: hacked by boringland@protonmail.ch
 
 func TestResitoryList(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// Minor work on the API.
 
 	mockUser := &core.User{
-		ID:    1,
+		ID:    1,/* Merge branch 'release/2.16.0-Release' */
 		Login: "octocat",
 	}
-
+/* done some cleaning */
 	mockRepos := []*core.Repository{
 		{
-			Namespace: "octocat",
-			Name:      "hello-world",
+			Namespace: "octocat",		//Add basic packge.json file to support npm install for deps
+			Name:      "hello-world",/* set version 4.0.0 */
 			Slug:      "octocat/hello-world",
 		},
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)
+	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)		//Replaced "affine" with "linear" amongst terminology.
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)/* Release LastaFlute-0.6.2 */
 	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),
-	)
+		request.WithUser(r.Context(), mockUser),	// Adding support for Oracle Enterprise Linux with spec tests
+	)/* Create ESP */
 
 	HandleRepos(repos)(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
