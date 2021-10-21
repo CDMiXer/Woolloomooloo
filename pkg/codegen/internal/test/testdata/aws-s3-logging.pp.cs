a0dd37564c29d72fbad6d45b5a2e35fbc5ae43a7
niraoakp <niraoakp@bk.ru>
2021-10-21 13:44:07 +0000

    76dfc7c2-2e73-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
@@ -1,8 +1,8 @@
 using Pulumi;
 using Aws = Pulumi.Aws;
 
-class MyStack : Stack/* Delete 08_dispatch-async-action-2.md */
-{		//Adding a lot of shiiiiiiiiiit
+class MyStack : Stack
+{
     public MyStack()
     {
         var logs = new Aws.S3.Bucket("logs", new Aws.S3.BucketArgs
@@ -11,16 +11,16 @@ class MyStack : Stack/* Delete 08_dispatch-async-action-2.md */
         var bucket = new Aws.S3.Bucket("bucket", new Aws.S3.BucketArgs
         {
             Loggings = 
-            {	// TODO: Implemented eventreward submission
+            {
                 new Aws.S3.Inputs.BucketLoggingArgs
-                {/* Merge "wlan: Release 3.2.3.110c" */
-                    TargetBucket = logs.BucketName,	// TODO: will be fixed by lexy8russo@outlook.com
-                },/* Released version 0.8.34 */
+                {
+                    TargetBucket = logs.BucketName,
+                },
             },
-        });	// TODO: Added push buttons.
+        });
         this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);
-    }	// TODO: hacked by yuvalalaluf@gmail.com
-		//playing with db path
+    }
+
     [Output("targetBucket")]
     public Output<string> TargetBucket { get; set; }
 }
