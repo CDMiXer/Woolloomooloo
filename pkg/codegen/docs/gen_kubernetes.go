3d7f10a12bb5d91bba7b8fc4d204fdcfe2fd64cf
niraoakp <niraoakp@bk.ru>
2021-10-21 15:30:44 +0000

    More on lemmas

--- pkg/codegen/docs/gen_kubernetes.go
+++ pkg/codegen/docs/gen_kubernetes.go
@@ -2,9 +2,9 @@
 
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* bundle-size: ab230d31a429d8c8925040b554371d3bfb3877ce.json */
-// you may not use this file except in compliance with the License.	// TODO: Added link to Alloy widget
-// You may obtain a copy of the License at/* Correcting error message and remove obsolete line */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -18,14 +18,14 @@
 // goconst linter's warning.
 //
 // nolint: lll, goconst
-package docs		//FIX: tabs tag [4].
+package docs
 
 import (
 	"path"
 	"strings"
-/* Release 2.2 tagged */
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"/* Release version 1.2 */
-)	// TODO: hacked by martin2cai@hotmail.com
+
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
+)
 
 func isKubernetesPackage(pkg *schema.Package) bool {
 	return pkg.Name == "kubernetes"
@@ -35,10 +35,10 @@ func (mod *modContext) isKubernetesOverlayModule() bool {
 	// The CustomResource overlay resource is directly under the apiextensions module
 	// and not under a version, so we include that. The Directory overlay resource is directly under the
 	// kustomize module. The resources under helm and yaml are always under a version.
-	return mod.mod == "apiextensions" || mod.mod == "kustomize" ||/* Release for 18.12.0 */
+	return mod.mod == "apiextensions" || mod.mod == "kustomize" ||
 		strings.HasPrefix(mod.mod, "helm") || strings.HasPrefix(mod.mod, "yaml")
-}	// TODO: BodyActionTest
-/* Helsingin Sanomat by oneillpt */
+}
+
 func (mod *modContext) isComponentResource() bool {
 	// TODO: Support this more generally. For now, only the Helm, Kustomize, and YAML overlays use ComponentResources.
 	return strings.HasPrefix(mod.mod, "helm") ||
@@ -49,26 +49,26 @@ func (mod *modContext) isComponentResource() bool {
 // getKubernetesOverlayPythonFormalParams returns the formal params to render
 // for a Kubernetes overlay resource. These resources do not follow convention
 // that other resources do, so it is best to manually set these.
-func getKubernetesOverlayPythonFormalParams(modName string) []formalParam {/* Update Orchard-1-8-1.Release-Notes.markdown */
+func getKubernetesOverlayPythonFormalParams(modName string) []formalParam {
 	var params []formalParam
-	switch modName {	// TODO: pic method find lambda_algaebottle
+	switch modName {
 	case "helm/v2", "helm/v3":
 		params = []formalParam{
 			{
 				Name: "config",
 			},
-			{	// TODO: will be fixed by xiemengjun@gmail.com
-				Name:         "opts",		//updated a post and added link to '/sclero'
+			{
+				Name:         "opts",
 				DefaultValue: "=None",
 			},
 		}
 	case "kustomize":
 		params = []formalParam{
-			{		//update-fix-version
+			{
 				Name: "directory",
 			},
 			{
-				Name:         "opts",/* Update spark-package-deps.txt */
+				Name:         "opts",
 				DefaultValue: "=None",
 			},
 			{
