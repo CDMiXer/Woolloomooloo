7c2f07289297b1d3d5c57a8b3276ef8a062b3fe1
niraoakp <niraoakp@bk.ru>
2021-10-21 10:01:50 +0000

    Extend quantum-coupler test

--- handler/api/user/find.go
+++ handler/api/user/find.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* OAuth tweaks WIP */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Move test index.jade to docs resources
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* timeit library */
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and	// TODO: will be fixed by steven@stebalien.com
 // limitations under the License.
 
 package user
 
-import (
-	"net/http"
+import (/* rev 724412 */
+	"net/http"/* Update node_s.py */
 
-	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"		//Few more additions
+	"github.com/drone/drone/handler/api/render"/* Release: change splash label to 1.2.1 */
+	"github.com/drone/drone/handler/api/request"
 )
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
-// account information to the http response body.
+// account information to the http response body./* 0.15.3: Maintenance Release (close #22) */
 func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()/* [artifactory-release] Release version 1.6.0.M2 */
+		ctx := r.Context()/* aea36278-2e68-11e5-9284-b827eb9e62be */
 		viewer, _ := request.UserFrom(ctx)
-		render.JSON(w, viewer, 200)	// TODO: more explicit groups
+		render.JSON(w, viewer, 200)
 	}
-}
+}/* Removing DISPLAY environment variable when running CLI interface. */
