e850630eb813f28070706a5bed29e6b6cb91caf6
niraoakp <niraoakp@bk.ru>
2021-10-21 04:17:36 +0000

    removed "todo" file

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
@@ -1,13 +1,13 @@
-using Pulumi;	// TODO: added lerpSelf method to Jello.Vector2
+using Pulumi;
 using Kubernetes = Pulumi.Kubernetes;
-	// TODO: CMake: don't compile incomplete Qt frontend by default.
+/* Release for 24.8.0 */
 class MyStack : Stack
 {
-    public MyStack()
-    {
+    public MyStack()/* Merge "Use lookup table to simplify logic" */
+    {	// Use getopts for user's helpers
         var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs
         {
-            ApiVersion = "apps/v1",
+            ApiVersion = "apps/v1",	// Quick merge of app and prototype colors
             Kind = "Deployment",
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
             {
@@ -16,26 +16,26 @@ class MyStack : Stack
             Spec = new Kubernetes.Types.Inputs.Apps.V1.DeploymentSpecArgs
             {
                 Template = new Kubernetes.Types.Inputs.Core.V1.PodTemplateSpecArgs
-                {/* Release version 0.6.2 - important regexp pattern fix */
+                {
                     Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
-                    {/* Unchaining WIP-Release v0.1.41-alpha */
+                    {/* Cache di shiro ora persistente */
                         Containers = 
-                        {
-                            new Kubernetes.Types.Inputs.Core.V1.ContainerArgs/* 0d247ed6-2e5f-11e5-9284-b827eb9e62be */
+                        {/* Use div with css-positioning instead of sturdy tables */
+                            new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
                             {
                                 ReadinessProbe = new Kubernetes.Types.Inputs.Core.V1.ProbeArgs
                                 {
                                     HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs
                                     {
                                         Port = 8080,
-                                    },	// added check for onCreateView already existing
-                                },
+                                    },
+                                },	// TODO: Delete fpop2d.R
                             },
-                        },
-                    },	// TODO: hacked by zodiacon@live.com
+                        },/* Release Notes: Notes for 2.0.14 */
+                    },	// Update scanner.coffee
                 },
             },
-        });/* Replace with the tar.gz version */
-    }/* Merge "Add support for python3 packages" */
-	// otp enrollment fixes
+        });
+    }
+
 }
