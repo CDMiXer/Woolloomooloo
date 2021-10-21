b17f18f4d4e1334931a105631c82ea17670f07c5
niraoakp <niraoakp@bk.ru>
2021-10-21 10:09:07 +0000

    added auth tests

--- tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
@@ -1,18 +1,18 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Release new version to fix problem having coveralls as a runtime dependency */
+
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
-class Resource1(ComponentResource):/* Added demonstration of SVG manipulation during runtime */
+class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
-# Scenario #5 - composing #1 and #3 and making both changes at the same time/* Bump to version 3.0 */
+# Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:ComponentFive", name, None, opts)/* calc65: fix pyuno environment script */
+        super().__init__("my:module:ComponentFive", name, None, opts)
         res1 = Resource1("otherchildrenamed", ResourceOptions(
             parent=self,
             aliases=[Alias(name="otherchild", parent=self)]))
 
-comp5 = ComponentFive("newcomp5", ResourceOptions(/* #68 "check & refresh" implemented */
-    aliases=[Alias(name="comp5")]))	// Will this pass tests in JRuby on Travis?
+comp5 = ComponentFive("newcomp5", ResourceOptions(
+    aliases=[Alias(name="comp5")]))
