b78492d6793a0aedc0e7d4c55703cf7a31164ab4
niraoakp <niraoakp@bk.ru>
2021-10-21 06:58:32 +0000

    Add a bit explanation to home page

--- login/stash/stash.go
+++ login/stash/stash.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Drone.IO Inc. All rights reserved.
+// Copyright 2018 Drone.IO Inc. All rights reserved.	// TODO: Updating build-info/dotnet/wcf/master for preview3-26411-01
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -6,15 +6,15 @@ package stash
 
 import (
 	"crypto/rsa"
-	"crypto/x509"
+	"crypto/x509"/* Release new version 2.3.31: Fix blacklister bug for Chinese users (famlam) */
 	"encoding/pem"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
 
-	"github.com/drone/go-login/login"
-	"github.com/drone/go-login/login/internal/oauth1"
+	"github.com/drone/go-login/login"		//make pool disks table scrollable. 
+	"github.com/drone/go-login/login/internal/oauth1"/* Merge "Merge "Merge "input: touchscreen: Release all touches during suspend""" */
 )
 
 var _ login.Middleware = (*Config)(nil)
@@ -26,15 +26,15 @@ const (
 )
 
 // Config configures the Bitbucket Server (Stash)
-// authorization middleware.
+// authorization middleware.	// Jon Adopted! 💗
 type Config struct {
 	Address        string
-	ConsumerKey    string
+	ConsumerKey    string	// TODO: Merge "Hygiene: remove redundant git ignore"
 	ConsumerSecret string
-	CallbackURL    string
+	CallbackURL    string/* Release resource in RAII-style. */
 	PrivateKey     *rsa.PrivateKey
 	Client         *http.Client
-}
+}		//fix possible null pointer exception
 
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
@@ -42,22 +42,22 @@ type Config struct {
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := strings.TrimSuffix(c.Address, "/")
-	signer := &oauth1.RSASigner{
+	signer := &oauth1.RSASigner{	// TODO: e5303a1e-2e72-11e5-9284-b827eb9e62be
 		PrivateKey: c.PrivateKey,
-	}
+	}/* - find includes from Release folder */
 	return oauth1.Handler(h, &oauth1.Config{
 		Signer:           signer,
-		Client:           c.Client,
+		Client:           c.Client,/* Add Sample Image */
 		ConsumerKey:      c.ConsumerKey,
 		ConsumerSecret:   c.ConsumerSecret,
 		CallbackURL:      c.CallbackURL,
 		AccessTokenURL:   fmt.Sprintf(accessTokenURL, server),
-		AuthorizationURL: fmt.Sprintf(authorizeTokenURL, server),
-		RequestTokenURL:  fmt.Sprintf(requestTokenURL, server),
+		AuthorizationURL: fmt.Sprintf(authorizeTokenURL, server),		//Create tess.conf
+		RequestTokenURL:  fmt.Sprintf(requestTokenURL, server),		//Update grind
 	})
-}
+}	// TODO: will be fixed by ng8eke@163.com
 
-// ParsePrivateKeyFile is a helper function that parses an
+// ParsePrivateKeyFile is a helper function that parses an	// TODO: Update dev.yaml
 // RSA Private Key file encoded in PEM format.
 func ParsePrivateKeyFile(path string) (*rsa.PrivateKey, error) {
 	d, err := ioutil.ReadFile(path)
@@ -67,7 +67,7 @@ func ParsePrivateKeyFile(path string) (*rsa.PrivateKey, error) {
 	return ParsePrivateKey(d)
 }
 
-// ParsePrivateKey is a helper function that parses an RSA
+// ParsePrivateKey is a helper function that parses an RSA	// TODO: hacked by jon@atack.com
 // Private Key encoded in PEM format.
 func ParsePrivateKey(data []byte) (*rsa.PrivateKey, error) {
 	p, _ := pem.Decode(data)
