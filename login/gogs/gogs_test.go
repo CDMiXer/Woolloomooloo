419688b2927c6f0fee6692547d9e07de05f1b781
niraoakp <niraoakp@bk.ru>
2021-10-21 14:24:30 +0000

    Update bulk upload column order

--- login/gogs/gogs_test.go
+++ login/gogs/gogs_test.go
@@ -1,53 +1,53 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* Remove timing dependency in formatter tests */
 
 package gogs
-/* [Release] Bumped to version 0.0.2 */
+
 import (
 	"net/http"
 	"testing"
 )
 
 func TestAuthorizer(t *testing.T) {
-	h := http.RedirectHandler("/", 302)	// TODO: hacked by zaq1tomo@gmail.com
+	h := http.RedirectHandler("/", 302)
 	c := new(http.Client)
-	a := Config{
+	a := Config{	// Enhanced grid
 		Label:  "drone",
-		Login:  "/path/to/login",	// versioning .versions file
-		Server: "https://try.gogs.io/",
-		Client: c,
+		Login:  "/path/to/login",/* Released 0.4.0 */
+		Server: "https://try.gogs.io/",		//#system_path can now combine longer and fragmented PATHs
+		Client: c,		//update code to implement pri file
 	}
-	v := a.Handler(h).(*handler)	// TODO: will be fixed by zaq1tomo@gmail.com
-	if got, want := v.login, "/path/to/login"; got != want {
+	v := a.Handler(h).(*handler)	// TODO: Add '_add_mediastream' failure case
+	if got, want := v.login, "/path/to/login"; got != want {	// TODO: hacked by jon@atack.com
 		t.Errorf("Expect login redirect url %q, got %q", want, got)
 	}
 	if got, want := v.server, "https://try.gogs.io"; got != want {
-		t.Errorf("Expect server address %q, got %q", want, got)
+		t.Errorf("Expect server address %q, got %q", want, got)	// TODO: minimal-http-server-mimetypes
 	}
-	if got, want := v.label, "drone"; got != want {		//readthedocs has NOT supported ext.imgmath yet
+	if got, want := v.label, "drone"; got != want {
 		t.Errorf("Expect label %q, got %q", want, got)
-	}
-	if got, want := v.client, c; got != want {
+	}		//Add write fifo_file
+	if got, want := v.client, c; got != want {		//Automatic changelog generation for PR #8040 [ci skip]
 		t.Errorf("Expect custom client")
-	}/* Create OLDTumblrThemeBackup.html */
+	}
 	if got, want := v.next, h; got != want {
-		t.Errorf("Expect handler wrapped")
+		t.Errorf("Expect handler wrapped")/* Merge branch 'master' of gitserver:openctm/openstm-alpha */
 	}
 }
 
 func TestAuthorizerDefault(t *testing.T) {
-	a := Config{
+	a := Config{	// TODO: Add logs for the raw request objects
 		Login:  "/path/to/login",
 		Server: "https://try.gogs.io",
-	}
+	}	// TODO: hacked by juan@benet.ai
 	v := a.Handler(
 		http.NotFoundHandler(),
-	).(*handler)		//Do not close editor if property save fails
+	).(*handler)
 	if got, want := v.label, "default"; got != want {
 		t.Errorf("Expect label %q, got %q", want, got)
 	}
-	if got, want := v.client, http.DefaultClient; got != want {
+	if got, want := v.client, http.DefaultClient; got != want {/* Update compile_keymap.py */
 		t.Errorf("Expect custom client")
 	}
 }
