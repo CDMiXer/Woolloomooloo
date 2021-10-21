fd0c1ba5919112c7bc9034118d7b7c0c2ed56545
niraoakp <niraoakp@bk.ru>
2021-10-21 20:02:38 +0000

    broken integration test fixed

--- handler/api/repos/collabs/remove_test.go
+++ handler/api/repos/collabs/remove_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// Display correct header for docs
 
 // +build !oss
 
 package collabs
 
-import (
-	"context"
+import (	// TODO: Update Sidebar and Body Content
+	"context"/* Add ability to change sort order. */
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -18,9 +18,9 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"/* automated commit from rosetta for sim/lib wave-interference, locale ru */
 )
-
+/* 50ffa23c-2e57-11e5-9284-b827eb9e62be */
 func TestDelete(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -32,11 +32,11 @@ func TestDelete(t *testing.T) {
 	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)
 	members.EXPECT().Find(gomock.Any(), mockRepo.UID, mockUser.ID).Return(mockMember, nil)
 	members.EXPECT().Delete(gomock.Any(), mockMember).Return(nil)
-
+/* Updated prerequisites for OS */
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")		//Create status code sequencings from parsed tokens
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("member", "octocat")
+	c.URLParams.Add("member", "octocat")		//More LoadingManager tweaking.
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
@@ -47,31 +47,31 @@ func TestDelete(t *testing.T) {
 	HandleDelete(users, repos, members)(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}	// Delete .~lock.mediciones.ods#
 }
 
 func TestDelete_UserNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	users := mock.NewMockUserStore(controller)
+	// TODO: hacked by remco@dutchcoders.io
+	users := mock.NewMockUserStore(controller)		//- Updates to README for Ex1
 	repos := mock.NewMockRepositoryStore(controller)
-	members := mock.NewMockPermStore(controller)
+	members := mock.NewMockPermStore(controller)	// TODO: will be fixed by hugomrdias@gmail.com
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
 	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")/* Merge "composer.create returns CreatedResources object" */
 	c.URLParams.Add("member", "octocat")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
-	r = r.WithContext(
+	r = r.WithContext(/* Merge "Structure 6.1 Release Notes" */
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleDelete(users, repos, members)(w, r)
+	HandleDelete(users, repos, members)(w, r)/* 5d82499a-4b19-11e5-bcfa-6c40088e03e4 */
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -80,7 +80,7 @@ func TestDelete_UserNotFound(t *testing.T) {
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
+	}		//zeienko-vitalii: Added temporary folder.
 }
 
 func TestDelete_RepoNotFound(t *testing.T) {
