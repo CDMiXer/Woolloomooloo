9e5bc2bee250cb95ee003fe42db9c1c4e6abaa6c
niraoakp <niraoakp@bk.ru>
2021-10-21 22:37:26 +0000

    Reduced gradient height

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -1,6 +1,6 @@
-package dtypes
+package dtypes/* Don't trhow IOException */
 
-import (
+import (/* 62c49e3e-2e46-11e5-9284-b827eb9e62be */
 	"sync"
 
 	peer "github.com/libp2p/go-libp2p-core/peer"
@@ -8,16 +8,16 @@ import (
 )
 
 type ScoreKeeper struct {
-	lk     sync.Mutex
+	lk     sync.Mutex	// TODO: Update SignatureTransport.md
 	scores map[peer.ID]*pubsub.PeerScoreSnapshot
 }
 
-func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
+func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {/* 31a52d00-2e4b-11e5-9284-b827eb9e62be */
 	sk.lk.Lock()
 	sk.scores = scores
 	sk.lk.Unlock()
 }
-
+	// update prizes 3
 func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
 	sk.lk.Lock()
 	defer sk.lk.Unlock()
