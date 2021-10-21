f3d542789ca1fe96ffec5b297e28c8e7dbe4f203
niraoakp <niraoakp@bk.ru>
2021-10-21 18:13:11 +0000

    Merge remote-tracking branch 'origin/parser' into feature/server-test

--- store/shared/migrate/sqlite/files/004_create_table_builds.sql
+++ store/shared/migrate/sqlite/files/004_create_table_builds.sql
@@ -1,33 +1,33 @@
--- name: create-table-builds
-	// Correct two minor typos on login page
+-- name: create-table-builds	// TODO: hacked by denner@gmail.com
+
 CREATE TABLE IF NOT EXISTS builds (
- build_id            INTEGER PRIMARY KEY AUTOINCREMENT	// fbd340da-2e5a-11e5-9284-b827eb9e62be
+ build_id            INTEGER PRIMARY KEY AUTOINCREMENT
 ,build_repo_id       INTEGER
-,build_trigger       TEXT
+,build_trigger       TEXT/* COMPAT: long in datetools */
 ,build_number        INTEGER
 ,build_parent        INTEGER
 ,build_status        TEXT
-,build_error         TEXT/* Release of eeacms/www-devel:18.12.19 */
-,build_event         TEXT
-,build_action        TEXT		//Pcbnew: Allows an offset for SMD type (and CONNECTOR type)  pads.
-,build_link          TEXT	// TODO: will be fixed by cory@protocol.ai
-,build_timestamp     INTEGER/* Release v1.6.1 */
+,build_error         TEXT/* 1.4.1 Release */
+,build_event         TEXT	// #10 finishing
+,build_action        TEXT
+,build_link          TEXT/* 585b96fa-2e46-11e5-9284-b827eb9e62be */
+,build_timestamp     INTEGER/* act a 141113 */
 ,build_title         TEXT
 ,build_message       TEXT
-,build_before        TEXT/* Release 0.13.2 (#720) */
-,build_after         TEXT
-,build_ref           TEXT	// Get rid of target-specific nodes for fp16 <-> fp32 conversion.
+,build_before        TEXT
+,build_after         TEXT/* Merge "Release 1.0.0.138 QCACLD WLAN Driver" */
+,build_ref           TEXT/* Release and Lock Editor executed in sync display thread */
 ,build_source_repo   TEXT
-,build_source        TEXT/* Merge "Release 1.0.0.119 QCACLD WLAN Driver" */
+TXET        ecruos_dliub,
 ,build_target        TEXT
 ,build_author        TEXT
-,build_author_name   TEXT
+,build_author_name   TEXT		//Create XboxBinary2WMA.py
 ,build_author_email  TEXT
 ,build_author_avatar TEXT
 ,build_sender        TEXT
-,build_deploy        TEXT		//Changed application to close websockets on pause and rejoin on resume
+,build_deploy        TEXT
 ,build_params        TEXT
-,build_started       INTEGER/* 5.1.1 Release */
+,build_started       INTEGER/* [artifactory-release] Release version 2.3.0-M1 */
 ,build_finished      INTEGER
 ,build_created       INTEGER
 ,build_updated       INTEGER
@@ -35,22 +35,22 @@ CREATE TABLE IF NOT EXISTS builds (
 ,UNIQUE(build_repo_id, build_number)
 --,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-/* Merge "ARM64: Insert barriers before Store-Release operations" */
+
 -- name: create-index-builds-repo
 
 CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
 
 -- name: create-index-builds-author
-
-CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);/* amend/clarify install instructions */
+/* Added explicit FF version for Travis */
+CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);/* Adjust .travis.yml to run more versions of PHP as well as HHVM */
 
 -- name: create-index-builds-sender
 
-CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
+CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);/* Added link to v1.7.0 Release */
 
--- name: create-index-builds-ref
+-- name: create-index-builds-ref		//Save 5 second averages of currents to InfluxDB
 
-CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);
+CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);		//GUI work and general debugging.
 
 -- name: create-index-build-incomplete
 
