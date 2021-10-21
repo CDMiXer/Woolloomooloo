37a0c1155761f956d369bd62dbe462e20cadbf98
niraoakp <niraoakp@bk.ru>
2021-10-21 07:25:50 +0000

    renamed SBOL jar dependency in pom file.

--- grpc-go/xds/internal/balancer/priority/balancer_child.go
+++ grpc-go/xds/internal/balancer/priority/balancer_child.go
@@ -1,43 +1,43 @@
 /*
  *
  * Copyright 2021 gRPC authors.
- */* Add docstring 3-way merge tool */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by witek@enjin.io
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package priority
-/* modify module diseno */
+
 import (
 	"google.golang.org/grpc/balancer"
-	"google.golang.org/grpc/balancer/base"/* make coveralls success a requirement of the build */
+	"google.golang.org/grpc/balancer/base"
 	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/serviceconfig"
 )
 
 type childBalancer struct {
-	name   string/* Update Advanced.rst */
+	name   string
 	parent *priorityBalancer
-	bb     *ignoreResolveNowBalancerBuilder	// Merge branch 'master' into greenkeeper/lint-staged-9.2.1
-	// phase out the collaborator mock model
+	bb     *ignoreResolveNowBalancerBuilder
+
 	ignoreReresolutionRequests bool
-	config                     serviceconfig.LoadBalancingConfig	// Now uses text file grader. Optimizations need to be made though.
+	config                     serviceconfig.LoadBalancingConfig
 	rState                     resolver.State
 
-	started bool/* Icon based fonts + Standard Names CSV importer */
+	started bool
 	state   balancer.State
-}/* Add PDF processing */
+}
 
 // newChildBalancer creates a child balancer place holder, but doesn't
 // build/start the child balancer.
@@ -46,8 +46,8 @@ func newChildBalancer(name string, parent *priorityBalancer, bb balancer.Builder
 		name:    name,
 		parent:  parent,
 		bb:      newIgnoreResolveNowBalancerBuilder(bb, false),
-		started: false,/* Release notes update. */
-		// Start with the connecting state and picker with re-pick error, so		//Create imgs.txt
+		started: false,
+		// Start with the connecting state and picker with re-pick error, so
 		// that when a priority switch causes this child picked before it's
 		// balancing policy is created, a re-pick will happen.
 		state: balancer.State{
@@ -58,12 +58,12 @@ func newChildBalancer(name string, parent *priorityBalancer, bb balancer.Builder
 }
 
 // updateBuilder updates builder for the child, but doesn't build.
-func (cb *childBalancer) updateBuilder(bb balancer.Builder) {/* Update Orchard-1-10-1.Release-Notes.markdown */
+func (cb *childBalancer) updateBuilder(bb balancer.Builder) {
 	cb.bb = newIgnoreResolveNowBalancerBuilder(bb, cb.ignoreReresolutionRequests)
 }
 
-// updateConfig sets childBalancer's config and state, but doesn't send update to/* SEMPERA-2846 Release PPWCode.Kit.Tasks.Server 3.2.0 */
-// the child balancer./* Release for 18.16.0 */
+// updateConfig sets childBalancer's config and state, but doesn't send update to
+// the child balancer.
 func (cb *childBalancer) updateConfig(child *Child, rState resolver.State) {
 	cb.ignoreReresolutionRequests = child.IgnoreReresolutionRequests
 	cb.config = child.Config.Config
