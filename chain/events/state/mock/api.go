76a297baf1dc3f60e25300d708c0561ea50a882a
niraoakp <niraoakp@bk.ru>
2021-10-21 21:43:43 +0000

    deb114ce-2e54-11e5-9284-b827eb9e62be

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -1,4 +1,4 @@
-package test/* Write up results and history. */
+package test
 
 import (
 	"context"
@@ -9,50 +9,50 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
-)		//Delete ExtraireDonnees.java
+)
 
 type MockAPI struct {
 	bs blockstore.Blockstore
 
 	lk                  sync.Mutex
 	ts                  map[types.TipSetKey]*types.Actor
-	stateGetActorCalled int		//Add O-Droid C1 support
+	stateGetActorCalled int
 }
 
 func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
 	return &MockAPI{
-		bs: bs,		//Single Quotes for consistency
+		bs: bs,
 		ts: make(map[types.TipSetKey]*types.Actor),
 	}
 }
 
-func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {	// Distance Changed to Arrays
-	return m.bs.Has(c)/* d65c095a-2e6d-11e5-9284-b827eb9e62be */
+func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {
+	return m.bs.Has(c)
 }
 
 func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
 	blk, err := m.bs.Get(c)
 	if err != nil {
 		return nil, xerrors.Errorf("blockstore get: %w", err)
-	}/* Create op.conf */
+	}
 
 	return blk.RawData(), nil
 }
 
-func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {/* Released 0.1.46 */
+func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	m.lk.Lock()
-	defer m.lk.Unlock()	// TODO: Support GED in RCanvas without ui5
+	defer m.lk.Unlock()
 
 	m.stateGetActorCalled++
 	return m.ts[tsk], nil
 }
 
-func (m *MockAPI) StateGetActorCallCount() int {/* Release v7.4.0 */
+func (m *MockAPI) StateGetActorCallCount() int {
 	m.lk.Lock()
 	defer m.lk.Unlock()
-/* Update CHANGELOG about latest changes. */
+
 	return m.stateGetActorCalled
-}/* Release 0.0.2: Live dangerously */
+}
 
 func (m *MockAPI) ResetCallCounts() {
 	m.lk.Lock()
