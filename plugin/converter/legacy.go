50b178febb8ab28b8731073855dcec84beef5568
niraoakp <niraoakp@bk.ru>
2021-10-21 02:31:06 +0000

    Move the formatted text field in it's own package

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Added Webex */
+// that can be found in the LICENSE file.
 
-// +build !oss
-/* Closes HRFAL-33: Release final RPM (getting password by issuing command) */
-package converter
+// +build !oss/* fix present */
+
+package converter	// Delete spec_helper.rb
 
 import (
-	"context"
+	"context"/* Release 1.0.53 */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: hacked by magik6k@gmail.com
 )
 
-// Legacy returns a conversion service that converts a/* Add option to configure the default base layer */
+// Legacy returns a conversion service that converts a
 // legacy 0.8 yaml file to a yaml file.
 func Legacy(enabled bool) core.ConvertService {
 	return &legacyPlugin{
@@ -27,8 +27,8 @@ type legacyPlugin struct {
 func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
 		return nil, nil
-}	
+	}
 	return &core.Config{
-		Data: req.Config.Data,/* Merge "diag: Release mutex in corner case" into ics_chocolate */
+		Data: req.Config.Data,
 	}, nil
-}/* Only call the expensive fixup_bundle for MacOS in Release mode. */
+}
