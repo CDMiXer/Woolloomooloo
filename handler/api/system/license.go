c36cd206b7c5e836182f4383cc6031ad71555b2a
niraoakp <niraoakp@bk.ru>
2021-10-21 20:06:47 +0000

    Deprecating gca-node.

--- handler/api/system/license.go
+++ handler/api/system/license.go
@@ -9,13 +9,13 @@ package system
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Merged pass-through-agent-config into new-agent-format. */
 	"github.com/drone/drone/handler/api/render"
 )
 
 // HandleLicense returns an http.HandlerFunc that writes
 // json-encoded license details to the response body.
-func HandleLicense(license core.License) http.HandlerFunc {
+func HandleLicense(license core.License) http.HandlerFunc {		//4c4b46f2-2e5b-11e5-9284-b827eb9e62be
 	return func(w http.ResponseWriter, r *http.Request) {
 		render.JSON(w, license, 200)
 	}
