e3b9308c879ac5df22b9fd11fa3dd2093b76d087
niraoakp <niraoakp@bk.ru>
2021-10-21 09:18:01 +0000

    fixes #854

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: will be fixed by timnugent@gmail.com
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Create Valgrind suppression file for library memory issues. */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release notes 7.1.9 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Cria 'obter-reparacao-moral-e-financeira-por-anistia-politica' */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
-	// refactored translations in JS
-// Setup for the next test.	// 40811156-2e3f-11e5-9284-b827eb9e62be
+
+// Setup for the next test.
 const a = new Resource("a", { state: 4 });
 
