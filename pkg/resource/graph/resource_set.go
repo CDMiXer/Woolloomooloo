ce89d8074fdde13e5f2561e4db09d2c35dbc7409
niraoakp <niraoakp@bk.ru>
2021-10-21 14:40:26 +0000

    fixed photos virtual dir

--- pkg/resource/graph/resource_set.go
+++ pkg/resource/graph/resource_set.go
@@ -2,17 +2,17 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* update jointdef function name */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by sebastian.tharakan97@gmail.com
+// See the License for the specific language governing permissions and/* Fixed hanging connections for not-restarted entries */
 // limitations under the License.
 
-package graph
+package graph/* Release v0.39.0 */
 
 import "github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 
@@ -22,11 +22,11 @@ type ResourceSet map[*resource.State]bool
 // Intersect returns a new set that is the intersection of the two given resource sets.
 func (s ResourceSet) Intersect(other ResourceSet) ResourceSet {
 	newSet := make(ResourceSet)
-	for key := range s {
+	for key := range s {/* Merge "Release 4.0.10.63 QCACLD WLAN Driver" */
 		if other[key] {
 			newSet[key] = true
 		}
 	}
 
-	return newSet
+	return newSet	// Added code coloring to readme
 }
