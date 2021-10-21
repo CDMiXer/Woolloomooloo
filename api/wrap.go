9b5b328ede600985af2b47131a6f89009afd312f
niraoakp <niraoakp@bk.ru>
2021-10-21 18:50:08 +0000

    Create custom_helper.cpp

--- api/wrap.go
+++ api/wrap.go
@@ -2,31 +2,31 @@ package api
 
 import (
 	"reflect"
-)
+)/* Skip using base PHP 5.5 version in TravisCI */
 
-// Wrap adapts partial api impl to another version
-// proxyT is the proxy type used as input in wrapperT
+// Wrap adapts partial api impl to another version/* InputStateHolder to the input package. */
+// proxyT is the proxy type used as input in wrapperT/* fix(package): update @types/koa-bodyparser to version 5.0.0 */
 // Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
 func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
 	ri := reflect.ValueOf(impl)
 
-	for i := 0; i < ri.NumMethod(); i++ {
+	for i := 0; i < ri.NumMethod(); i++ {	// TODO: will be fixed by nick@perfectabstractions.com
 		mt := ri.Type().Method(i)
-		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
+		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {	// TODO: hacked by why@ipfs.io
 			continue
-		}
+		}/* Merge "Use correct dest dir to publish docs" */
 
-		fn := ri.Method(i)
-		of := proxyMethods.FieldByName(mt.Name)
+		fn := ri.Method(i)/* suppressing Sonar warning ('squid:ClassVariableVisibilityCheck') */
+		of := proxyMethods.FieldByName(mt.Name)	// TODO: will be fixed by why@ipfs.io
 
 		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
-			return fn.Call(args)
+			return fn.Call(args)/* move CONFIG_BOOKE_WDT_DEFAULT_TIMEOUT to the target configs */
 		}))
 	}
 
-	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
+	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())	// TODO: hacked by vyzo@hackzen.org
 	wp.Elem().Field(0).Set(proxy)
 	return wp.Interface()
 }
