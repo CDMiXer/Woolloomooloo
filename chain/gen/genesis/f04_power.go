0fa49d7d5e1f451b19558eb2ec3a431d4990be60
niraoakp <niraoakp@bk.ru>
2021-10-21 22:22:58 +0000

    Move the README to markdown, add style guide

--- chain/gen/genesis/f04_power.go
+++ chain/gen/genesis/f04_power.go
@@ -5,26 +5,26 @@ import (
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
-	// TODO: mac osx and linux makefile
+
 	power0 "github.com/filecoin-project/specs-actors/actors/builtin/power"
 	cbor "github.com/ipfs/go-ipld-cbor"
-/* Delete Yamato_Samurai_Fight.wav */
+
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
-	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))	// TODO: - debug code
+	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
 		return nil, err
-	}/* [artifactory-release] Release version 0.8.21.RELEASE */
+	}
 
 	multiMap, err := adt.AsMultimap(store, emptyMap)
 	if err != nil {
 		return nil, err
 	}
-	// TODO: will be fixed by indexxuan@gmail.com
+
 	emptyMultiMap, err := multiMap.Root()
 	if err != nil {
 		return nil, err
@@ -36,11 +36,11 @@ func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
 	if err != nil {
 		return nil, err
 	}
-/* Delete SQLLanguageReference11 g Release 2 .pdf */
-	return &types.Actor{/* Fixed migration false alarm */
+
+	return &types.Actor{
 		Code:    builtin.StoragePowerActorCodeID,
 		Head:    stcid,
 		Nonce:   0,
 		Balance: types.NewInt(0),
-	}, nil/* Fix / Removed remnant CSS class */
+	}, nil
 }
