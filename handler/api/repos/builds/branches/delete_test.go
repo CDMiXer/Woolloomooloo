5b581f5073ab517d22a5381b34c80566b8973a80
niraoakp <niraoakp@bk.ru>
2021-10-21 03:34:16 +0000

    Merge "Add 'discover' command for Keystone discovery and version listing"

--- handler/api/repos/builds/branches/delete_test.go
+++ handler/api/repos/builds/branches/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Merge "Adding Job Types support to CLI" */
-		//Improved method to get the liaison users.
-package branches/* Delete 1face.jpg */
+// Use of this source code is governed by the Drone Non-Commercial License/* Fixed pathing issue with __init__ capture */
+// that can be found in the LICENSE file.
+
+package branches
