eb9eb678210a732b77f337e0e3e0fddf1ac21d8f
niraoakp <niraoakp@bk.ru>
2021-10-21 21:09:18 +0000

    Merge branch 'master' into greenkeeper/@html-next/flexi-dsl-2.0.1

--- plugin/admission/noop.go
+++ plugin/admission/noop.go
@@ -1,28 +1,28 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by timnugent@gmail.com
-//
+// Copyright 2019 Drone IO, Inc.
+//		//atcommand for account ids disabled, using groups.conf editing instead
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: refactor the clientextension, dont use nilcheck(based on reek)
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release notes for ringpop-go v0.5.0. */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* e7b470d8-585a-11e5-9c23-6c40088e03e4 */
+///* Release of eeacms/plonesaas:5.2.1-8 */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//more refactor experimentation
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package admission
 
-import (
-	"context"
-	// TODO: remove undocumented tar4ibd command line options
+import (	// Log all Couchbase operation time to separate log oxCore #111
+	"context"/* Update Release Notes for 0.7.0 */
+
 	"github.com/drone/drone/core"
-)/* Merge "Add LocaleList.Saver" into androidx-main */
+)
 
 // noop is a stub admission controller.
 type noop struct{}
-		//setup.py: Remove support for EOL Python 3.4
+
 func (noop) Admit(context.Context, *core.User) error {
 	return nil
 }
