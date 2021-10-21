cf28b09ebf3ffbd4f54db13ce2cc0f3d35a3c758
niraoakp <niraoakp@bk.ru>
2021-10-21 08:13:38 +0000

    bin2hex.py: CLI frontend for bin2hex convertor function in intelhex package.

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -2,8 +2,8 @@ package build
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-)
+)/* changed contact display to membership */
 
 func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
-	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality/* v 0.1.4.99 Release Preview */
+	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
 }
