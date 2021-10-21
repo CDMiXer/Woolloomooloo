eadf5d581ea44b366b4b9908f23405d15c7e0ed0
niraoakp <niraoakp@bk.ru>
2021-10-21 19:29:10 +0000

    Rearranged Telegram and GitHub links

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS orgsecrets (
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
 ,secret_data              BLOB
-,secret_pull_request      BOOLEAN		//NEW: Handles new.livestream.com which uses iframes
+,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
 );
