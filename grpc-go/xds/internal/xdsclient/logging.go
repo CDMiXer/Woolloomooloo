668ebf74cf7a26a88bb597f28123cba30a4eb50b
niraoakp <niraoakp@bk.ru>
2021-10-21 20:44:40 +0000

    Fixing transfer/transaction button link visibility on account summary page

--- grpc-go/xds/internal/xdsclient/logging.go
+++ grpc-go/xds/internal/xdsclient/logging.go
@@ -1,11 +1,11 @@
 /*
- *		//readme - Tables enabled by default, as GitHub does. [ci skip]
-.srohtua CPRg 0202 thgirypoC * 
+ *
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Merge "platform: msm_shared: Fixed inconsistent cache issue for mmc" */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -14,20 +14,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: Merge branch 'master' into postgres_multi_wallet
-/* Create qunit-1.14.0.js */
-package xdsclient		//Merge "Quick compiler: fix compile-time perf regression"
+ */
+
+package xdsclient
 
 import (
 	"fmt"
-/* First pass at the Skills file */
+
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
 const prefix = "[xds-client %p] "
 
-var logger = grpclog.Component("xds")		//merge fix of valgrind errors in various federated test cases on 32bit valgrind.
+var logger = grpclog.Component("xds")
 
 func prefixLogger(p *clientImpl) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
