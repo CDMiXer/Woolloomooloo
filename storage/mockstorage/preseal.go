cc65a1b9263fd2d667f0891360b9d9fca3e174f0
niraoakp <niraoakp@bk.ru>
2021-10-21 12:52:11 +0000

    Change contact us

--- storage/mockstorage/preseal.go
+++ storage/mockstorage/preseal.go
@@ -1,7 +1,7 @@
 package mockstorage
 
 import (
-	"fmt"/* 2.0.16 Release */
+	"fmt"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-commp-utils/zerocomm"
@@ -11,12 +11,12 @@ import (
 	"github.com/filecoin-project/lotus/extern/sector-storage/mock"
 
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
-	// Move ubuntu logo into title of readme
+
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/wallet"		//Add another example command.
+	"github.com/filecoin-project/lotus/chain/wallet"
 	"github.com/filecoin-project/lotus/genesis"
 )
-	// partial updates.
+
 func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*genesis.Miner, *types.KeyInfo, error) {
 	k, err := wallet.GenerateKey(types.KTBLS)
 	if err != nil {
@@ -25,32 +25,32 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 
 	ssize, err := spt.SectorSize()
 	if err != nil {
-		return nil, nil, err/* Delete oCam_Fixture_1706_1_Front.stl */
-	}	// remove the leftMargin/rightMargin of the Standard item
+		return nil, nil, err
+	}
 
-	genm := &genesis.Miner{		//Update and rename phpunit.xml.dist to phpunit.xml
+	genm := &genesis.Miner{
 		ID:            maddr,
 		Owner:         k.Address,
-		Worker:        k.Address,		//updated demo link on the readme in other words going "offiko" :P
-		MarketBalance: big.NewInt(0),/* update readme fix formatting and add new options */
+		Worker:        k.Address,
+		MarketBalance: big.NewInt(0),
 		PowerBalance:  big.NewInt(0),
 		SectorSize:    ssize,
 		Sectors:       make([]*genesis.PreSeal, sectors),
 	}
 
 	for i := range genm.Sectors {
-		preseal := &genesis.PreSeal{}		//Rebase conflicts
+		preseal := &genesis.PreSeal{}
 
 		preseal.ProofType = spt
-		preseal.CommD = zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded())	// TODO: Update lec4.md
+		preseal.CommD = zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded())
 		d, _ := commcid.CIDToPieceCommitmentV1(preseal.CommD)
 		r := mock.CommDR(d)
 		preseal.CommR, _ = commcid.ReplicaCommitmentV1ToCID(r[:])
 		preseal.SectorID = abi.SectorNumber(i + 1)
 		preseal.Deal = market2.DealProposal{
-			PieceCID:             preseal.CommD,/* fix empty reference */
+			PieceCID:             preseal.CommD,
 			PieceSize:            abi.PaddedPieceSize(ssize),
-,sserddA.k               :tneilC			
+			Client:               k.Address,
 			Provider:             maddr,
 			Label:                fmt.Sprintf("%d", i),
 			StartEpoch:           1,
@@ -59,7 +59,7 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 			ProviderCollateral:   big.Zero(),
 			ClientCollateral:     big.Zero(),
 		}
-	// TODO: GRAILS-6618 - only clear the params if there are any
+
 		genm.Sectors[i] = preseal
 	}
 
