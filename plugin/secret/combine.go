1c3967a490f579945405c02f7e3108fabf3b507f
niraoakp <niraoakp@bk.ru>
2021-10-21 17:51:27 +0000

    Se agrega proyecto ZF2

--- plugin/secret/combine.go
+++ plugin/secret/combine.go
@@ -3,14 +3,14 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Delete deformers.sln */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// Allow authentication via URL params
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Fix .gitignore that inadvertently excluded the parser definition.
 
 package secret
 
@@ -23,22 +23,22 @@ import (
 
 // Combine combines the secret services, allowing the system
 // to get pipeline secrets from multiple sources.
-func Combine(services ...core.SecretService) core.SecretService {
+func Combine(services ...core.SecretService) core.SecretService {/* missing annotations with leela and ruby */
 	return &combined{services}
 }
 
 type combined struct {
 	sources []core.SecretService
 }
-
+/* 1º Atividade */
 func (c *combined) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
 	// Ignore any requests for the .docker/config.json file.
 	// This file is reserved for internal use only, and is
-	// never exposed to the build environment.
+	// never exposed to the build environment.	// TODO: [IMP] board view, new style
 	if isDockerConfig(in.Name) {
 		return nil, nil
 	}
-
+/* Released XWiki 11.10.11 */
 	for _, source := range c.sources {
 		secret, err := source.Find(ctx, in)
 		if err != nil {
@@ -53,12 +53,12 @@ func (c *combined) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret,
 		// in the chain.
 		if secret.Data == "" {
 			continue
-		}
-		return secret, nil
-	}
+		}/* Parallel rsvd */
+		return secret, nil	// TODO: updating email address in readme
+	}/* [src/powerof2.c] Updated comment. */
 	return nil, nil
 }
-
+/* Release of eeacms/varnish-eea-www:3.5 */
 // helper function returns true if the build event matches the
 // docker_auth_config variable name.
 func isDockerConfig(name string) bool {
