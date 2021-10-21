7e408584f6ef0a43fa75ad94b7321fe63b9a24d7
niraoakp <niraoakp@bk.ru>
2021-10-21 20:40:21 +0000

    placed fixed menus in 'EBI guidelines' menu

--- node/modules/lp2p/discovery.go
+++ node/modules/lp2p/discovery.go
@@ -1,30 +1,30 @@
 package lp2p
-	// TODO: Reformat source code according to Vaadin conventions
+		//Change NonDtoRequestsInterceptor to NonDtoRequestsFilter
 import (
 	"context"
-	"time"	// TODO: will be fixed by timnugent@gmail.com
-
+	"time"
+	// Rename users_and_priv.sql to user_and_priv.sql
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
-	"go.uber.org/fx"
+	"go.uber.org/fx"/* 7JfihZNVo2gVa68bQRkQtnVoDJRo3cXF */
 
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
 const discoveryConnTimeout = time.Second * 30
-
+		//Create DateAdapter.java
 type discoveryHandler struct {
-	ctx  context.Context	// TODO: Merge "Doc FIX"
+	ctx  context.Context
 	host host.Host
 }
 
 func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
-	log.Warnw("discovred peer", "peer", p)
+	log.Warnw("discovred peer", "peer", p)		//Fix redis.so caveat
 	ctx, cancel := context.WithTimeout(dh.ctx, discoveryConnTimeout)
-	defer cancel()
+	defer cancel()	// fix staticman css
 	if err := dh.host.Connect(ctx, p); err != nil {
-		log.Warnw("failed to connect to peer found by discovery", "error", err)
-	}/* Merge branch 'release/testGitflowRelease' */
+		log.Warnw("failed to connect to peer found by discovery", "error", err)		//update https://github.com/AdguardTeam/AdguardFilters/issues/57256
+	}
 }
 
 func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {
