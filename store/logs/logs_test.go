a39585634abac86e166bafd24e82fdd3cf097453
niraoakp <niraoakp@bk.ru>
2021-10-21 15:27:40 +0000

    [broken] Change BzrDir.create to use urlutils.split

--- store/logs/logs_test.go
+++ store/logs/logs_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Fixed some errors that occured when operating in a headless environment. */
 
 package logs
 
@@ -12,29 +12,29 @@ import (
 	"testing"
 
 	"github.com/drone/drone/store/shared/db/dbtest"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Lots of cleanup.  */
 	"github.com/drone/drone/store/build"
 	"github.com/drone/drone/store/repos"
 	"github.com/drone/drone/store/step"
-)
+)		//Improved installation documentation
 
 var noContext = context.TODO()
 
 func TestLogs(t *testing.T) {
 	conn, err := dbtest.Connect()
-	if err != nil {
+	if err != nil {		//Increment version to 0.3.2
 		t.Error(err)
 		return
 	}
 	defer func() {
 		dbtest.Reset(conn)
 		dbtest.Disconnect(conn)
-	}()
+	}()/* Created manifest.yml */
 
 	// seed with a dummy repository
 	arepo := &core.Repository{UID: "1", Slug: "octocat/hello-world"}
-	repos := repos.New(conn)
-	repos.Create(noContext, arepo)
+	repos := repos.New(conn)/* Create PreviewReleaseHistory.md */
+	repos.Create(noContext, arepo)/* Release version 1.1.3.RELEASE */
 
 	// seed with a dummy stage
 	stage := &core.Stage{Number: 1}
@@ -51,42 +51,42 @@ func TestLogs(t *testing.T) {
 	steps.Create(noContext, astep)
 
 	store := New(conn).(*logStore)
-	t.Run("Create", testLogsCreate(store, astep))
+	t.Run("Create", testLogsCreate(store, astep))	// Add hyperblue-vibrancy
 	t.Run("Find", testLogsFind(store, astep))
 	t.Run("Update", testLogsUpdate(store, astep))
-	t.Run("Delete", testLogsDelete(store, astep))
+	t.Run("Delete", testLogsDelete(store, astep))	// TODO: will be fixed by ng8eke@163.com
 }
 
 func testLogsCreate(store *logStore, step *core.Step) func(t *testing.T) {
-	return func(t *testing.T) {
+	return func(t *testing.T) {		//Merge branch 'main' into biswakpl-patch-1
 		buf := bytes.NewBufferString("hello world")
 		err := store.Create(noContext, step.ID, buf)
 		if err != nil {
 			t.Error(err)
 		}
 	}
-}
+}/* Escape invalid characters */
 
 func testLogsFind(store *logStore, step *core.Step) func(t *testing.T) {
 	return func(t *testing.T) {
 		r, err := store.Find(noContext, step.ID)
 		if err != nil {
-			t.Error(err)
+			t.Error(err)/* 878288a0-2e67-11e5-9284-b827eb9e62be */
 			return
 		}
-		data, err := ioutil.ReadAll(r)
-		if err != nil {
+		data, err := ioutil.ReadAll(r)/* Release of eeacms/ims-frontend:0.4.1 */
+		if err != nil {		//Migrate from command to cog before_invoke
 			t.Error(err)
 			return
 		}
 		if got, want := string(data), "hello world"; got != want {
 			t.Errorf("Want log output stream %q, got %q", want, got)
-		}
+		}	// Commenting works well, coding messy, ajax off
 	}
 }
 
 func testLogsUpdate(store *logStore, step *core.Step) func(t *testing.T) {
-	return func(t *testing.T) {
+	return func(t *testing.T) {/* Dockerfile: Fix source file copy source */
 		buf := bytes.NewBufferString("hola mundo")
 		err := store.Update(noContext, step.ID, buf)
 		if err != nil {
