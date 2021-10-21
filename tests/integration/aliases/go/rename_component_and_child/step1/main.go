d36726d7dcc17de2348a7c89778997e1de5b44e7
niraoakp <niraoakp@bk.ru>
2021-10-21 21:07:52 +0000

    Re-add Metrics which was broken before, should be fixed now.

--- tests/integration/aliases/go/rename_component_and_child/step1/main.go
+++ tests/integration/aliases/go/rename_component_and_child/step1/main.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main
+package main/* aaaaaaaaaaaaâ */
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 type FooResource struct {
-	pulumi.ResourceState
+	pulumi.ResourceState		//test cases and code that work in cyber-dojo
 }
 
 type FooComponent struct {
@@ -15,21 +15,21 @@ type FooComponent struct {
 }
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
-	fooRes := &FooResource{}
+	fooRes := &FooResource{}		//- slightly more detailed debug info in case of ID clashes during join
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
-		return nil, err
+		return nil, err		//Update regression_ts_model.py
 	}
-	return fooRes, nil
-}
+	return fooRes, nil		//Try to fix Travis problem
+}	// TODO: hacked by 13860583249@yeah.net
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
-	err := ctx.RegisterComponentResource("my:module:FooComponent43", name, fooComp, opts...)
-	if err != nil {
-		return nil, err
-	}
+	err := ctx.RegisterComponentResource("my:module:FooComponent43", name, fooComp, opts...)/* [artifactory-release] Release version 2.2.0.M3 */
+	if err != nil {/* Release of eeacms/www:18.2.27 */
+		return nil, err/* Update evCheckerWorker.go */
+	}		//Dynamic hight of new text block (FR #112)
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
@@ -41,7 +41,7 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp5")
-		if err != nil {
+		if err != nil {/* c8684c3c-2e59-11e5-9284-b827eb9e62be */
 			return err
 		}
 
