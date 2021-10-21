f864b75325d084dc26740fc3d0b3cd31b1b0627e
niraoakp <niraoakp@bk.ru>
2021-10-21 22:30:12 +0000

    encode/decode by protocol itself

--- login/internal/oauth1/handler.go
+++ login/internal/oauth1/handler.go
@@ -1,9 +1,9 @@
 // Copyright 2018 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* 3.0.0 API Update */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-	// [maven-release-plugin] prepare release 0.2beta3
-package oauth1
-		//Fix weed plant 3D2D
+
+package oauth1/* Changed name to call-tester */
+
 import (
 	"net/http"
 
@@ -11,21 +11,21 @@ import (
 )
 
 // Handler returns a Handler that runs h at the completion
-// of the oauth2 authorization flow.		//Added TypeMatch
+// of the oauth2 authorization flow.
 func Handler(h http.Handler, c *Config) http.Handler {
 	return &handler{next: h, conf: c}
 }
-/* Fixed Download Service not downloading non-pinned background downloads */
-type handler struct {
+
+type handler struct {/* Create pvaudio.ex */
 	conf *Config
 	next http.Handler
 }
 
-func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {/* Add comments to analyseSensitivity */
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-/* fix +1212 ace in the console doesnt listen to undo keys */
+
 	verifier := r.FormValue("oauth_verifier")
-	if verifier == "" {
+	if verifier == "" {	// TODO: will be fixed by peterke@gmail.com
 		token, err := h.conf.requestToken()
 		if err != nil {
 			ctx = login.WithError(ctx, err)
@@ -34,32 +34,32 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {/* Add comm
 		}
 		redirectTo, err := h.conf.authorizeRedirect(token.Token)
 		if err != nil {
-			ctx = login.WithError(ctx, err)
-			h.next.ServeHTTP(w, r.WithContext(ctx))		//refs #483, see also [14439]
-			return	// TODO: Duplex API bugfix.
+			ctx = login.WithError(ctx, err)/* Release notes for 1.0.76 */
+			h.next.ServeHTTP(w, r.WithContext(ctx))
+			return
 		}
-		http.Redirect(w, r, redirectTo, 302)
+		http.Redirect(w, r, redirectTo, 302)/* Merge "Release versions update in docs for 6.1" */
 		return
-	}
-
-	token := r.FormValue("oauth_token")
-		//b479fb2a-2e5a-11e5-9284-b827eb9e62be
+	}/* Create inbo-export.md */
+	// TODO: will be fixed by steven@stebalien.com
+	token := r.FormValue("oauth_token")/* Rename README.md to colors.inc */
+/* Fixed misnaming of type in examples */
 	// requests the access_token from the authorization server.
 	// If an error is encountered, write the error to the
 	// context and prceed with the next http.Handler in the chain.
-	accessToken, err := h.conf.authorizeToken(token, verifier)/* Merge "Merge deployments data for collectors heat, request" */
-	if err != nil {	// TODO: will be fixed by sbrichards@gmail.com
-		ctx = login.WithError(ctx, err)
+	accessToken, err := h.conf.authorizeToken(token, verifier)
+	if err != nil {
+		ctx = login.WithError(ctx, err)	// TODO: will be fixed by timnugent@gmail.com
 		h.next.ServeHTTP(w, r.WithContext(ctx))
-		return		//Add dependency to won-utils-conversation
-	}
+		return
+	}/* scripts/xtr: fig gpg support and added -c|-g (compress/gpg option) switch */
 
 	// converts the oauth2 token type to the internal Token
 	// type and attaches to the context.
-	ctx = login.WithToken(ctx, &login.Token{
-		Access:  accessToken.Token,
+	ctx = login.WithToken(ctx, &login.Token{		//Small optimization in line/arrow drawings
+		Access:  accessToken.Token,/* Release v1.0 */
 		Refresh: accessToken.TokenSecret,
 	})
 
-	h.next.ServeHTTP(w, r.WithContext(ctx))/* c7342798-35ca-11e5-a4c7-6c40088e03e4 */
+	h.next.ServeHTTP(w, r.WithContext(ctx))
 }
