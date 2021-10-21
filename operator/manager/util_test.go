ac46c20169c26fd1b618d2c5906e3f98137fca7a
niraoakp <niraoakp@bk.ru>
2021-10-21 11:13:04 +0000

    Merge pull request #11 from nasa-gibs/lerc

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package manager
+package manager/* 426c5f9c-2e59-11e5-9284-b827eb9e62be */
