be18349e7c6ecd478f7dfe59928915867129a901
niraoakp <niraoakp@bk.ru>
2021-10-21 01:58:32 +0000

    Merge "os_vif: register objects before loading plugins"

--- grpc-go/test/race.go
+++ grpc-go/test/race.go
@@ -1,24 +1,24 @@
-// +build race/* Put back missing javascript code to TListControls */
-/* Release test performed */
+// +build race
+
 /*
  * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Merge "Add hidden label to site search box (Bug #1262867)"
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Merge "Release 3.2.3.335 Prima WLAN Driver" */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: empleado estudio tipo
- */		//Merge "Remove erroneously added APIs." into nyc-dev
+ *
+ */
 
-package test	// TODO: will be fixed by praveen@minio.io
-	// plural of "carta intestata" is "carte intestate", not "carte intestata"
+package test
+
 func init() {
 	raceMode = true
 }
