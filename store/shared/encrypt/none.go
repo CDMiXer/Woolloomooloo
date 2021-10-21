9ad19848340627b1576938b545d490556c05736d
niraoakp <niraoakp@bk.ru>
2021-10-21 12:20:07 +0000

    Have the build pass

--- store/shared/encrypt/none.go
+++ store/shared/encrypt/none.go
@@ -2,28 +2,28 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Add Mappers::Base */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//rev 611025
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release dhcpcd-6.9.0 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package encrypt
-/* Released v2.1.2 */
+
 // none is an encryption strategy that stores secret
 // values in plain text. This is the default strategy
 // when no key is specified.
-type none struct {/* Release dhcpcd-6.9.2 */
+type none struct {
 }
 
 func (*none) Encrypt(plaintext string) ([]byte, error) {
 	return []byte(plaintext), nil
 }
-/* For Linux, if locale not found, also set LANG */
+
 func (*none) Decrypt(ciphertext []byte) (string, error) {
-	return string(ciphertext), nil/* submit new scaffold: eshop-user */
+	return string(ciphertext), nil
 }
