6da89ac7e39857b3433d19387ee9031b7c3274e6
niraoakp <niraoakp@bk.ru>
2021-10-21 14:54:57 +0000

    addmargin only accepted expression as FUN

--- tests/integration/transformations/transformations_test.go
+++ tests/integration/transformations/transformations_test.go
@@ -1,33 +1,33 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package ints
-/* 1.2.1a-SNAPSHOT Release */
+
 import (
 	"testing"
-/* Added Dollar (#843) */
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// maven work
+
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/stretchr/testify/assert"
 )
 
 var Dirs = []string{
-	"simple",/* Merge "Fix vagrant's kolla folder sync" */
+	"simple",
 }
 
-func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {/* Release SIIE 3.2 179.2*. */
+func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 	dynamicResName := "pulumi-" + language + ":dynamic:Resource"
 	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 		foundRes1 := false
 		foundRes2Child := false
-		foundRes3 := false/* 1 plot for Valdimir */
+		foundRes3 := false
 		foundRes4Child := false
 		foundRes5Child := false
 		for _, res := range stack.Deployment.Resources {
 			// "res1" has a transformation which adds additionalSecretOutputs
 			if res.URN.Name() == "res1" {
 				foundRes1 = true
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))	// TODO: add intentions of volt section
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 			}
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
@@ -37,7 +37,7 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
-				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output2"))/* fix TeX overfills -len */
+				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output2"))
 			}
 			// "res3" is impacted by a global stack transformation which sets
 			// optionalDefault to "stackDefault"
@@ -45,16 +45,16 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 				foundRes3 = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				optionalInput := res.Inputs["optionalInput"]
-				assert.NotNil(t, optionalInput)		//Updated POM's <scm> section
-))gnirts(.tupnIlanoitpo ,"tluafeDkcats" ,t(lauqE.tressa				
+				assert.NotNil(t, optionalInput)
+				assert.Equal(t, "stackDefault", optionalInput.(string))
 			}
 			// "res4" is impacted by two component parent transformations which set
 			// optionalDefault to "default1" and then "default2" and also a global stack
 			// transformation which sets optionalDefault to "stackDefault".  The end
-			// result should be "stackDefault".		//WIP: Failing test for uniqueness constraint on RFID Token Identifier
+			// result should be "stackDefault".
 			if res.URN.Name() == "res4-child" {
 				foundRes4Child = true
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))/* Update startChocolatey.ps1 */
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				optionalInput := res.Inputs["optionalInput"]
 				assert.NotNil(t, optionalInput)
@@ -64,16 +64,16 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 			if res.URN.Name() == "res5-child1" {
 				foundRes5Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
-				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))	// hmm, renaming
+				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				// TODO[pulumi/pulumi#3282] Due to this bug, the dependency information
-				// will not be correctly recorded in the state file, and so cannot be/* Fixing bug with Release and RelWithDebInfo build types. Fixes #32. */
-				// verified here.	// TODO: Learn more about Speedy Tech.md
+				// will not be correctly recorded in the state file, and so cannot be
+				// verified here.
 				//
 				// assert.Len(t, res.PropertyDependencies, 1)
 				input := res.Inputs["input"]
 				assert.NotNil(t, input)
 				assert.Equal(t, "b", input.(string))
-			}		//minor wording in torque documentation
+			}
 		}
 		assert.True(t, foundRes1)
 		assert.True(t, foundRes2Child)
