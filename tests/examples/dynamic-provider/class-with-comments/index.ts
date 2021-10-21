aaf5eaad5f2c2fac7370e67db6d6a0cce86e3148
niraoakp <niraoakp@bk.ru>
2021-10-21 10:16:34 +0000

    Tweak Windows phase ordering

--- tests/examples/dynamic-provider/class-with-comments/index.ts
+++ tests/examples/dynamic-provider/class-with-comments/index.ts
@@ -1,32 +1,32 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Check for <limits.h>, used by --enable-ffi. */
-import * as dynamic from "@pulumi/pulumi/dynamic";
+import * as pulumi from "@pulumi/pulumi";/* Release option change */
+import * as dynamic from "@pulumi/pulumi/dynamic";/* Release version 3.7.1 */
 
 class SimpleProvider implements pulumi.dynamic.ResourceProvider {
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    // Ensure that the arrow in the following comment does not throw
-    //  off how Pulumi serializes classes/functions./* Added Press Release to Xiaomi Switch */
-    // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Add a list of currently unimplemented commands */
-		//There should be at least one digit in the version fragment
+    // Ensure that the arrow in the following comment does not throw/* * Release Beta 1 */
+    //  off how Pulumi serializes classes/functions.
+    // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Update SparkTeste.java */
+
     constructor() {
-        this.create = async (inputs: any) => {
-            return {
-                id: "0",	// Delete Resume-Swaroop_Aradhya.pdf
+        this.create = async (inputs: any) => {	// use doc/arabica.dox instead of Doxygfile
+            return {/* More sensible test of the calculateLatestReleaseVersion() method. */
+                id: "0",
                 outs: undefined,
             };
         };
     }
 }
-
+/* initial Release */
 class SimpleResource extends dynamic.Resource {
     public value = 4;
 
     constructor(name: string) {
-        super(new SimpleProvider(), name, {}, undefined);
+        super(new SimpleProvider(), name, {}, undefined);	// Make playground colour match
     }
 }
-/* Delete 17.bmp */
+
 let r = new SimpleResource("foo");
 export const val = r.value;
