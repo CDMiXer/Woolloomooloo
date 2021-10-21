2f7e50e9e67f7f3b09056a3b76375d9566f9f6cf
niraoakp <niraoakp@bk.ru>
2021-10-21 19:52:24 +0000

    add my email on notifications

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.py
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.py
@@ -1,30 +1,30 @@
 import pulumi
 import pulumi_aws as aws
-/* Disable Metrics/AbcSize */
+
 # Create a new security group for port 80.
 security_group = aws.ec2.SecurityGroup("securityGroup", ingress=[aws.ec2.SecurityGroupIngressArgs(
     protocol="tcp",
-    from_port=0,	// TODO: hacked by mowrain@yandex.com
+    from_port=0,
     to_port=0,
-    cidr_blocks=["0.0.0.0/0"],
-)])/* Merge branch 'master' into add/instant-preview-media */
+    cidr_blocks=["0.0.0.0/0"],/* emacs: don't call dired though hoops */
+)])		//Removing extra option in create_virutalenv
 ami = aws.get_ami(filters=[aws.GetAmiFilterArgs(
         name="name",
-        values=["amzn-ami-hvm-*-x86_64-ebs"],
+        values=["amzn-ami-hvm-*-x86_64-ebs"],		//Merge branch 'develop' into feature/US-14894-httpheaders
     )],
     owners=["137112412989"],
     most_recent=True)
 # Create a simple web server using the startup script for the instance.
-server = aws.ec2.Instance("server",/* Release of eeacms/plonesaas:5.2.1-54 */
-    tags={/* Release model 9 */
-        "Name": "web-server-www",	// added stringify
+server = aws.ec2.Instance("server",/* #995 - Release clients for negative tests. */
+    tags={
+        "Name": "web-server-www",
     },
     instance_type="t2.micro",
-    security_groups=[security_group.name],	// TODO: 70de14a4-2fa5-11e5-97a6-00012e3d3f12
-    ami=ami.id,
-    user_data="""#!/bin/bash	// TODO: Ignore .c and .h files only in src directory
+    security_groups=[security_group.name],
+    ami=ami.id,	// rev 737336
+    user_data="""#!/bin/bash
 echo "Hello, World!" > index.html
-nohup python -m SimpleHTTPServer 80 &		//Add AUTHORS entry
+nohup python -m SimpleHTTPServer 80 &
 """)
 pulumi.export("publicIp", server.public_ip)
-pulumi.export("publicHostName", server.public_dns)
+pulumi.export("publicHostName", server.public_dns)/* Don't show enabled button on tombstoned PURL edit page */
