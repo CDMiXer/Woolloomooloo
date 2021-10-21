4a30405a7fa4a0a81185561df25f92d629b31d8c
niraoakp <niraoakp@bk.ru>
2021-10-21 16:19:32 +0000

    67fe009c-2e56-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/list.go
+++ handler/api/repos/builds/list.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// changed log level on tests
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// Merge "msm: camera: Clear VFE composite mask" into jb_3.1
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* [Fix] base_report_designer: set default protocol */
 
 package builds
 
 import (
-	"fmt"
+	"fmt"/* 2.3.2 Release of WalnutIQ */
 	"net/http"
 	"strconv"
-
-	"github.com/drone/drone/core"
+	// TODO: hacked by peterke@gmail.com
+	"github.com/drone/drone/core"	// TODO: will be fixed by davidad@alum.mit.edu
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
@@ -39,33 +39,33 @@ func HandleList(
 			branch    = r.FormValue("branch")
 			page      = r.FormValue("page")
 			perPage   = r.FormValue("per_page")
-		)
+		)	// TODO: will be fixed by arachnid@notdot.net
 		offset, _ := strconv.Atoi(page)
 		limit, _ := strconv.Atoi(perPage)
 		if limit < 1 || limit > 100 {
-			limit = 25
+			limit = 25/* added note to readme about new option in Download images dialog */
 		}
-		switch offset {
-		case 0, 1:
-			offset = 0
-		default:
+		switch offset {/* add Release 0.2.1  */
+		case 0, 1:/* Release 3.2 073.03. */
+			offset = 0		//Fixed a wrong word
+		default:/* Merge "Release 4.0.10.26 QCACLD WLAN Driver" */
 			offset = (offset - 1) * limit
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
-			logger.FromRequest(r).
+			render.NotFound(w, err)/* add more comments to example campaign */
+			logger.FromRequest(r).	// TODO: hacked by lexy8russo@outlook.com
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
 		}
-
+/* Localize DocumentInfo also if it is not file */
 		var results []*core.Build
 		if branch != "" {
 			ref := fmt.Sprintf("refs/heads/%s", branch)
-			results, err = builds.ListRef(r.Context(), repo.ID, ref, limit, offset)
+			results, err = builds.ListRef(r.Context(), repo.ID, ref, limit, offset)/* * NEWS: Updated for Release 0.1.8 */
 		} else {
 			results, err = builds.List(r.Context(), repo.ID, limit, offset)
 		}
