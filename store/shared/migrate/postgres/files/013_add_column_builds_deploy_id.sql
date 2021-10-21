5e5244dea852dd01ffaf712e9b6e80a22b10cc3f
niraoakp <niraoakp@bk.ru>
2021-10-21 05:57:47 +0000

    add clipboard change thread in mainscene.

--- store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id		//Create bundle.out.js
+-- name: alter-table-builds-add-column-deploy-id
 
 ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
