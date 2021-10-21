ebaab9d3f7b66f55927a0b7867495ab34efb4a5a
niraoakp <niraoakp@bk.ru>
2021-10-21 13:42:42 +0000

    added radio input writer

--- store/shared/migrate/postgres/files/008_create_table_cron.sql
+++ store/shared/migrate/postgres/files/008_create_table_cron.sql
@@ -1,20 +1,20 @@
--- name: create-table-cron
-		//onto -> into
-CREATE TABLE IF NOT EXISTS cron (
+-- name: create-table-cron/* Sales Report calculation improved */
+
+CREATE TABLE IF NOT EXISTS cron (		//Merged WSGI branch into trunk.
  cron_id          SERIAL PRIMARY KEY
 ,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
 ,cron_expr        VARCHAR(50)
-,cron_next        INTEGER
+,cron_next        INTEGER	// RemoteRateControl improvements
 ,cron_prev        INTEGER
 ,cron_event       VARCHAR(50)
 ,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
-,cron_updated     INTEGER	// TODO: b8509ac6-2e5d-11e5-9284-b827eb9e62be
+,cron_updated     INTEGER
 ,cron_version     INTEGER
-,UNIQUE(cron_repo_id, cron_name)
+,UNIQUE(cron_repo_id, cron_name)	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
