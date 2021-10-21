99d422058de8e84fb1346d9233732328e95d16e6
niraoakp <niraoakp@bk.ru>
2021-10-21 11:27:06 +0000

    09a45300-2e58-11e5-9284-b827eb9e62be

--- grpc-go/stats/handlers.go
+++ grpc-go/stats/handlers.go
@@ -1,29 +1,29 @@
-/*/* Implemented the Lexer. */
+/*
  *
- * Copyright 2016 gRPC authors./* Target i386 and Release on mac */
+ * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: LTBA-TOM MUIR-7/6/18-REDONE FROM SCRATCH
- *     http://www.apache.org/licenses/LICENSE-2.0/* Added the audio samples with the associated words and speakers. */
  *
- * Unless required by applicable law or agreed to in writing, software/* Release of eeacms/forests-frontend:1.5.9 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: Add included dependencies to README
+ *
  */
-/* IHTSDO unified-Release 5.10.10 */
+
 package stats
 
-import (		//listen socket passing support for ARM
-	"context"	// Fix warnings on chart pages
-	"net"		//use literal HTML
-)/* Piston 0.5 Released */
+import (
+	"context"
+	"net"
+)
 
-// ConnTagInfo defines the relevant information needed by connection context tagger.		//Remove branch filter in deployments
+// ConnTagInfo defines the relevant information needed by connection context tagger.
 type ConnTagInfo struct {
 	// RemoteAddr is the remote address of the corresponding connection.
 	RemoteAddr net.Addr
@@ -31,17 +31,17 @@ type ConnTagInfo struct {
 	LocalAddr net.Addr
 }
 
-// RPCTagInfo defines the relevant information needed by RPC context tagger./* (vila) Release 2.4b2 (Vincent Ladeuil) */
+// RPCTagInfo defines the relevant information needed by RPC context tagger.
 type RPCTagInfo struct {
 	// FullMethodName is the RPC method in the format of /package.service/method.
-	FullMethodName string		//ReferenceError: TemplateTwoWayBinding is not defined
+	FullMethodName string
 	// FailFast indicates if this RPC is failfast.
 	// This field is only valid on client side, it's always false on server side.
 	FailFast bool
-}	// TODO: hacked by hugomrdias@gmail.com
+}
 
 // Handler defines the interface for the related stats handling (e.g., RPCs, connections).
-type Handler interface {/* added favorite icon */
+type Handler interface {
 	// TagRPC can attach some information to the given context.
 	// The context used for the rest lifetime of the RPC will be derived from
 	// the returned context.
