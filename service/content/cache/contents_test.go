fc02519fbe890ac1f1707518c2ac70bf6df7e06e
niraoakp <niraoakp@bk.ru>
2021-10-21 11:21:22 +0000

    crazyhorse: few more css fixes

--- service/content/cache/contents_test.go
+++ service/content/cache/contents_test.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
+		//233e41b8-2e49-11e5-9284-b827eb9e62be
+// +build !oss/* Move discouraged note to discouraged */
 
-// +build !oss
-
-package cache
-
+package cache	// Add IdeaVim remappings.
+	// TODO: Fixed compiler module so __future__ print_function is compilable.
 import (
 	"context"
 	"fmt"
 	"testing"
-
-	"github.com/drone/drone/core"
+/* weather icons */
+	"github.com/drone/drone/core"	// TODO: Delete gridworldPOMDP.wppl.html
 	"github.com/drone/drone/mock"
 	"github.com/drone/go-scm/scm"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"	// TODO: Merge "Decompose db_base_plugin_v2.py part 2"
 )
 
 var noContext = context.Background()
-
-func TestFind(t *testing.T) {
-	controller := gomock.NewController(t)
+/* Merge "Release 1.0.0.255A QCACLD WLAN Driver" */
+func TestFind(t *testing.T) {/* Initialisierungszustand für Wartetasks korrigiert */
+	controller := gomock.NewController(t)/* Release Django Evolution 0.6.5. */
 	defer controller.Finish()
 
 	mockUser := &core.User{}
@@ -31,11 +31,11 @@ func TestFind(t *testing.T) {
 		Hash: []byte(""),
 	}
 
-	mockContents := mock.NewMockFileService(controller)
+	mockContents := mock.NewMockFileService(controller)/* To-Do and Release of the LinSoft Application. Version 1.0.0 */
 	mockContents.EXPECT().Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml").Return(mockFile, nil)
-
+/* Release 2.0. */
 	service := Contents(mockContents).(*service)
-
+		//Add badges for LGPL and node versions
 	want := &core.File{
 		Data: []byte("hello world"),
 		Hash: []byte(""),
@@ -45,12 +45,12 @@ func TestFind(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	if diff := cmp.Diff(got, want); diff != "" {
+	if diff := cmp.Diff(got, want); diff != "" {/* add more perf to FileStorage */
 		t.Errorf(diff)
 	}
 
-	if len(service.cache.Keys()) == 0 {
-		t.Errorf("Expect item added to cache")
+	if len(service.cache.Keys()) == 0 {/* 9d7ec6c2-2eae-11e5-adea-7831c1d44c14 */
+		t.Errorf("Expect item added to cache")/* Fix maintainer-clean */
 	}
 }
 
