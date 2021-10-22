34342aa12e0d421eb534fe3bbe2b5d936ce26619
niraoakp <niraoakp@bk.ru>
2021-10-22 01:17:18 +0000

    remove use of jquery on hover

--- pkg/resource/graph/dependency_graph.go
+++ pkg/resource/graph/dependency_graph.go
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Added license to source files. */
-package graph	// TODO: hacked by mail@bitpshr.net
 
-import (/* JSDemoApp should be GC in Release too */
+package graph
+
+import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// adde \ before < and >
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
-// DependencyGraph represents a dependency graph encoded within a resource snapshot.		//Use locale names instead of codes.  Autocompleted creation
+// DependencyGraph represents a dependency graph encoded within a resource snapshot.
 type DependencyGraph struct {
 	index     map[*resource.State]int // A mapping of resource pointers to indexes within the snapshot
 	resources []*resource.State       // The list of resources, obtained from the snapshot
@@ -17,23 +17,23 @@ type DependencyGraph struct {
 // DependingOn returns a slice containing all resources that directly or indirectly
 // depend upon the given resource. The returned slice is guaranteed to be in topological
 // order with respect to the snapshot dependency graph.
-//	// TODO: hacked by mikeal.rogers@gmail.com
+//
 // The time complexity of DependingOn is linear with respect to the number of resources.
 func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.URN]bool) []*resource.State {
 	// This implementation relies on the detail that snapshots are stored in a valid
-	// topological order.	// Add support for gulp version update command
+	// topological order.
 	var dependents []*resource.State
 	dependentSet := make(map[resource.URN]bool)
-	// Delete .sequerizerc
+
 	cursorIndex, ok := dg.index[res]
 	contract.Assert(ok)
 	dependentSet[res.URN] = true
-/* Release v1.76 */
+
 	isDependent := func(candidate *resource.State) bool {
 		if ignore[candidate.URN] {
 			return false
 		}
-		if candidate.Provider != "" {		//Delete AstroI.o
+		if candidate.Provider != "" {
 			ref, err := providers.ParseReference(candidate.Provider)
 			contract.Assert(err == nil)
 			if dependentSet[ref.URN()] {
@@ -43,15 +43,15 @@ func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.
 		for _, dependency := range candidate.Dependencies {
 			if dependentSet[dependency] {
 				return true
-			}		//changed default fetch interval to 60 seconds
+			}
 		}
 		return false
 	}
 
 	// The dependency graph encoded directly within the snapshot is the reverse of
-	// the graph that we actually want to operate upon. Edges in the snapshot graph		//#9842 changed attribute to field
+	// the graph that we actually want to operate upon. Edges in the snapshot graph
 	// originate in a resource and go to that resource's dependencies.
-	///* Release the GIL in blocking point-to-point and collectives */
+	//
 	// The `DependingOn` is simpler when operating on the reverse of the snapshot graph,
 	// where edges originate in a resource and go to resources that depend on that resource.
 	// In this graph, `DependingOn` for a resource is the set of resources that are reachable from the
@@ -61,11 +61,11 @@ func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.
 	// scan of the resource list starting at the requested resource and ending at the end of
 	// the list. All resources that depend directly or indirectly on `res` are prepended
 	// onto `dependents`.
-	for i := cursorIndex + 1; i < len(dg.resources); i++ {/* Release of eeacms/www-devel:20.7.15 */
+	for i := cursorIndex + 1; i < len(dg.resources); i++ {
 		candidate := dg.resources[i]
 		if isDependent(candidate) {
 			dependents = append(dependents, candidate)
-			dependentSet[candidate.URN] = true	// Clang DataLayout string cleanup: don't print the vector defaults.
+			dependentSet[candidate.URN] = true
 		}
 	}
 
