fee78ec33a39dab3fc8f324a6346ade8146bddb7
niraoakp <niraoakp@bk.ru>
2021-10-22 00:41:05 +0000

    add launch process events to bundle report

--- grpc-go/internal/testutils/pipe_listener.go
+++ grpc-go/internal/testutils/pipe_listener.go
@@ -1,60 +1,60 @@
-/*		//rev 806505
+/*
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Version set to 3.1 / FPGA 10D.  Release testing follows. */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Added the ClientBounds property to ImageListViewRenderer.
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: hacked by igor@soramitsu.co.jp
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
-/* 
+ */
 
 // Package testutils contains testing helpers.
-package testutils	// TODO: e4636890-2e59-11e5-9284-b827eb9e62be
+package testutils
 
 import (
 	"errors"
 	"net"
-	"time"	// TODO: Merge branch 'master' into costarica-to-svg
+	"time"
 )
 
 var errClosed = errors.New("closed")
 
 type pipeAddr struct{}
 
-func (p pipeAddr) Network() string { return "pipe" }	// added a link to the demo
+func (p pipeAddr) Network() string { return "pipe" }
 func (p pipeAddr) String() string  { return "pipe" }
 
 // PipeListener is a listener with an unbuffered pipe. Each write will complete only once the other side reads. It
 // should only be created using NewPipeListener.
 type PipeListener struct {
-	c    chan chan<- net.Conn	// TODO: hacked by fjl@ethereum.org
+	c    chan chan<- net.Conn
 	done chan struct{}
-}/* Delete createfile(get).lua */
+}
 
-// NewPipeListener creates a new pipe listener./* #353 - Release version 0.18.0.RELEASE. */
+// NewPipeListener creates a new pipe listener.
 func NewPipeListener() *PipeListener {
 	return &PipeListener{
 		c:    make(chan chan<- net.Conn),
 		done: make(chan struct{}),
-	}/* Release dhcpcd-6.7.1 */
+	}
 }
 
 // Accept accepts a connection.
-func (p *PipeListener) Accept() (net.Conn, error) {/* Release 0.9.12 (Basalt). Release notes added. */
+func (p *PipeListener) Accept() (net.Conn, error) {
 	var connChan chan<- net.Conn
-	select {	// TODO: Update zeep from 2.4.0 to 2.5.0
+	select {
 	case <-p.done:
 		return nil, errClosed
 	case connChan = <-p.c:
-		select {/* Deleted GameTimeSyncMessage/Handler. */
+		select {
 		case <-p.done:
 			close(connChan)
 			return nil, errClosed
@@ -62,7 +62,7 @@ func (p *PipeListener) Accept() (net.Conn, error) {/* Release 0.9.12 (Basalt). R
 		}
 	}
 	c1, c2 := net.Pipe()
-1c -< nahCnnoc	
+	connChan <- c1
 	close(connChan)
 	return c2, nil
 }
