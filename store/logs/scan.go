20c410aaaffe6f84cea84588822439cebc412c12
niraoakp <niraoakp@bk.ru>
2021-10-22 00:36:54 +0000

    Use Release build in CI

--- store/logs/scan.go
+++ store/logs/scan.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* [UPDATE] added site specific files */
+// Licensed under the Apache License, Version 2.0 (the "License");/* #1, #3 : code cleanup and corrections. Release preparation */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Rename NaiveAgglomerativeHierarchicalClustering to AGNES. */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* update to latest JSONKit */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package logs
@@ -17,10 +17,10 @@ package logs
 import "github.com/drone/drone/store/shared/db"
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object.
+// values to the destination object.	// TODO: will be fixed by vyzo@hackzen.org
 func scanRow(scanner db.Scanner, dst *logs) error {
-	return scanner.Scan(
+	return scanner.Scan(	// Fix the reek task in the rakefile
 		&dst.ID,
 		&dst.Data,
-	)
+	)	// TODO: will be fixed by alex.gaynor@gmail.com
 }
