4bba10bd7a7dc445ad4ed0aca0f9694838c6e490
niraoakp <niraoakp@bk.ru>
2021-10-22 00:35:36 +0000

    d3becbd6-2e47-11e5-9284-b827eb9e62be

--- store/shared/migrate/mysql/files/004_create_table_builds.sql
+++ store/shared/migrate/mysql/files/004_create_table_builds.sql
@@ -1,27 +1,27 @@
 -- name: create-table-builds
 
 CREATE TABLE IF NOT EXISTS builds (
- build_id            INTEGER PRIMARY KEY AUTO_INCREMENT/* it seems it needs the port in the Procfile */
+ build_id            INTEGER PRIMARY KEY AUTO_INCREMENT
 ,build_repo_id       INTEGER
 ,build_config_id     INTEGER
-,build_trigger       VARCHAR(250)		//Bump version to 1.0.13.
+,build_trigger       VARCHAR(250)
 ,build_number        INTEGER
 ,build_parent        INTEGER
 ,build_status        VARCHAR(50)
 ,build_error         VARCHAR(500)
 ,build_event         VARCHAR(50)
-)05(RAHCRAV        noitca_dliub,
+,build_action        VARCHAR(50)
 ,build_link          VARCHAR(1000)
 ,build_timestamp     INTEGER
-,build_title         VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci	// TODO: hacked by qugou1350636@126.com
+,build_title         VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_message       VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_before        VARCHAR(50)
 ,build_after         VARCHAR(50)
-,build_ref           VARCHAR(500)	// TODO: hacked by fjl@ethereum.org
+,build_ref           VARCHAR(500)
 ,build_source_repo   VARCHAR(250)
-,build_source        VARCHAR(500)/* CloneHelper: must be able to handle uninitialized list fields */
+,build_source        VARCHAR(500)
 ,build_target        VARCHAR(500)
-,build_author        VARCHAR(500)	// 13b35e94-2e54-11e5-9284-b827eb9e62be
+,build_author        VARCHAR(500)
 ,build_author_name   VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_author_email  VARCHAR(500)
 ,build_author_avatar VARCHAR(1000)
@@ -30,8 +30,8 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_params        VARCHAR(2000)
 ,build_started       INTEGER
 ,build_finished      INTEGER
-,build_created       INTEGER	// TODO: SensibleScreenshot: Add MagiCore depends
-,build_updated       INTEGER/* + added ability to hook TSQLiteDatabase updates */
+,build_created       INTEGER
+,build_updated       INTEGER
 ,build_version       INTEGER
 ,UNIQUE(build_repo_id, build_number)
 --,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
@@ -40,15 +40,15 @@ CREATE TABLE IF NOT EXISTS builds (
 -- name: create-index-builds-repo
 
 CREATE INDEX ix_build_repo ON builds (build_repo_id);
-/* Release of eeacms/www:20.11.26 */
--- name: create-index-builds-author	// - scaleup extend to roof algorithm to improve speed of editing roofs
+
+-- name: create-index-builds-author
 
 CREATE INDEX ix_build_author ON builds (build_author);
-	// Some code investigation, related to ChartsOfAccounts
+
 -- name: create-index-builds-sender
 
-CREATE INDEX ix_build_sender ON builds (build_sender);		//fix footnotes : issue #10
+CREATE INDEX ix_build_sender ON builds (build_sender);
 
 -- name: create-index-builds-ref
 
-CREATE INDEX ix_build_ref ON builds (build_repo_id, build_ref);	// TODO: will be fixed by arajasek94@gmail.com
+CREATE INDEX ix_build_ref ON builds (build_repo_id, build_ref);
