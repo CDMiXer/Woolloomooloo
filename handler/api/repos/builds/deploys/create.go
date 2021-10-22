3cefd1c83df4be0df36b2dadce509a84f767f59f
niraoakp <niraoakp@bk.ru>
2021-10-22 03:44:43 +0000

    fix: remove comment on catégorie

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.		//chore(package): update yargs to version 7.1.0
+///* removed use of _.isUndefined */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// update to version 50.5.1.
-//	// TODO: add enviroment variable comment to readme
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: ok, mocha is also required
-///* Changed funky characters in string test to escaped characters. */
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Next Release Version Update */
+// you may not use this file except in compliance with the License./* Release Notes for v01-00-02 */
+// You may obtain a copy of the License at		//[-dev] spelling fix: "Ldap" to "LDAP".
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software	// Delete Gradle__org_lwjgl_lwjgl_lwjgl_platform_natives_windows_2_9_1.xml
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Built NavigationCntl
+// See the License for the specific language governing permissions and/* Create EvenFibonacciNumbers.java */
 // limitations under the License.
-	// TODO: hacked by ng8eke@163.com
+		//fix jshint errors
 package deploys
