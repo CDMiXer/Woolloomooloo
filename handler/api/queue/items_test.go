377eac523c5228d64f91741832acfac441190b88
niraoakp <niraoakp@bk.ru>
2021-10-22 03:09:50 +0000

    Update active_record_hash.gemspec

--- handler/api/queue/items_test.go
+++ handler/api/queue/items_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release version 0.3.8 */
 
 // +build !oss
 
 package queue
-		//Set 'OK' defaults for acquire dialogs.
-import (/* Fix Build Page -> Submit Release */
-	"io/ioutil"/* eed5653e-2e55-11e5-9284-b827eb9e62be */
-		//Add the documentation for the source code release
+
+import (
+	"io/ioutil"
+
 	"github.com/sirupsen/logrus"
 )
-/* fixing inmate-application for #2906 */
-func init() {/* Release of eeacms/www-devel:19.7.24 */
+
+func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
