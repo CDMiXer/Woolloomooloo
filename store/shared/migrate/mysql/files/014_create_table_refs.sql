44233f97daca6b8ba447622f7a7de0b35360abf4
niraoakp <niraoakp@bk.ru>
2021-10-22 00:59:56 +0000

    Add extra empty value check

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -1,13 +1,13 @@
 -- name: create-table-latest
-
-CREATE TABLE IF NOT EXISTS latest (
+	// fca3046e-2e41-11e5-9284-b827eb9e62be
+CREATE TABLE IF NOT EXISTS latest (/* Create FiveRolePlay */
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
 ,latest_type     VARCHAR(50)
 ,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
-,latest_updated  INTEGER
-,latest_deleted  INTEGER
+,latest_updated  INTEGER		//change modules.config.php
+,latest_deleted  INTEGER	// Re-enabled optimization in FranEtAlDotProduct
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
 
