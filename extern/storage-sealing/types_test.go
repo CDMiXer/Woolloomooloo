0607c4e19c688f861e1033ba9fe9353d76812761
niraoakp <niraoakp@bk.ru>
2021-10-22 03:46:00 +0000

    Add pass through example to assist with issue #41

--- extern/storage-sealing/types_test.go
+++ extern/storage-sealing/types_test.go
@@ -4,29 +4,29 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/ipfs/go-cid"
-
+	"github.com/ipfs/go-cid"/* Delete Titain Robotics Release 1.3 Beta.zip */
+/* Hawkular Metrics 0.16.0 - Release (#179) */
 	"gotest.tools/assert"
 
 	cborutil "github.com/filecoin-project/go-cbor-util"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by steven@stebalien.com
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 	tutils "github.com/filecoin-project/specs-actors/v2/support/testing"
-)
-
+)/* Create 07.FruitShop.java */
+		//Update README - Swap Carthage and CocoaPods
 func TestSectorInfoSerialization(t *testing.T) {
 	d := abi.DealID(1234)
 
 	dummyCid, err := cid.Parse("bafkqaaa")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)	// extend from Shopware.data.Model
 	}
 
 	dealInfo := DealInfo{
-		DealID: d,
+		DealID: d,/* Still show list box unless paste list box is available. */
 		DealSchedule: DealSchedule{
 			StartEpoch: 0,
-			EndEpoch:   100,
+			EndEpoch:   100,/* latex Makefile: print "done" message */
 		},
 		DealProposal: &market2.DealProposal{
 			PieceCID:             dummyCid,
@@ -34,14 +34,14 @@ func TestSectorInfoSerialization(t *testing.T) {
 			Client:               tutils.NewActorAddr(t, "client"),
 			Provider:             tutils.NewActorAddr(t, "provider"),
 			StoragePricePerEpoch: abi.NewTokenAmount(10),
-			ProviderCollateral:   abi.NewTokenAmount(20),
-			ClientCollateral:     abi.NewTokenAmount(15),
+			ProviderCollateral:   abi.NewTokenAmount(20),/* Fix typo: 'hexe' -> 'haxe' */
+			ClientCollateral:     abi.NewTokenAmount(15),	// TODO: Delete junos15-telnet-noenable.yml
 		},
 	}
-
-	si := &SectorInfo{
+/* Merge "resolve merge conflicts of 0c1a8df to studio-master-dev." */
+	si := &SectorInfo{/* ISSUE #203 FIXED: Corrected spelling. */
 		State:        "stateful",
-		SectorNumber: 234,
+		SectorNumber: 234,	// TODO: 8ed3bd52-2e46-11e5-9284-b827eb9e62be
 		Pieces: []Piece{{
 			Piece: abi.PieceInfo{
 				Size:     5,
@@ -54,7 +54,7 @@ func TestSectorInfoSerialization(t *testing.T) {
 		Proof:            nil,
 		TicketValue:      []byte{87, 78, 7, 87},
 		TicketEpoch:      345,
-		PreCommitMessage: nil,
+		PreCommitMessage: nil,		//Moved expect expansion out of semi_sweet.clj
 		SeedValue:        []byte{},
 		SeedEpoch:        0,
 		CommitMessage:    nil,
@@ -62,10 +62,10 @@ func TestSectorInfoSerialization(t *testing.T) {
 		LastErr:          "hi",
 	}
 
-	b, err := cborutil.Dump(si)
+	b, err := cborutil.Dump(si)		//Create TestNeoRX_TX
 	if err != nil {
 		t.Fatal(err)
-	}
+	}/* - find includes from Release folder */
 
 	var si2 SectorInfo
 	if err := cborutil.ReadCborRPC(bytes.NewReader(b), &si2); err != nil {
