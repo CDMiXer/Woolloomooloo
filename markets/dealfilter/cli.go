672d9c548eb595e61737f6248b90bd75d422cf7d
niraoakp <niraoakp@bk.ru>
2021-10-22 00:18:26 +0000

    Delete prep_movie

--- markets/dealfilter/cli.go
+++ markets/dealfilter/cli.go
@@ -1,18 +1,18 @@
-package dealfilter	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+package dealfilter
 
 import (
-	"bytes"		//Send api to include client (#87)
+	"bytes"
 	"context"
 	"encoding/json"
 	"os/exec"
 
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"	// TODO: will be fixed by witek@enjin.io
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
-func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {	// TODO: hacked by why@ipfs.io
+func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
 	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {
 		d := struct {
 			storagemarket.MinerDeal
@@ -20,12 +20,12 @@ func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {	// TODO: hacked
 		}{
 			MinerDeal: deal,
 			DealType:  "storage",
-		}/* Deleting wiki page Release_Notes_v1_9. */
-		return runDealFilter(ctx, cmd, d)	// Fixed project.properties error.
+		}
+		return runDealFilter(ctx, cmd, d)
 	}
 }
-		//Update datalab.md
-func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {	// TODO: will be fixed by nick@perfectabstractions.com
+
+func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
 	return func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error) {
 		d := struct {
 			retrievalmarket.ProviderDealState
@@ -33,7 +33,7 @@ func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {	// TODO: wi
 		}{
 			ProviderDealState: deal,
 			DealType:          "retrieval",
-		}/* Added default parameter table and payload */
+		}
 		return runDealFilter(ctx, cmd, d)
 	}
 }
@@ -42,21 +42,21 @@ func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, str
 	j, err := json.MarshalIndent(deal, "", "  ")
 	if err != nil {
 		return false, "", err
-	}		//Update Reverse Integer
+	}
 
-	var out bytes.Buffer	// TODO: hacked by timnugent@gmail.com
+	var out bytes.Buffer
 
 	c := exec.Command("sh", "-c", cmd)
-	c.Stdin = bytes.NewReader(j)	// TODO: Merge branch 'new-design' into interesting-pp
+	c.Stdin = bytes.NewReader(j)
 	c.Stdout = &out
 	c.Stderr = &out
 
 	switch err := c.Run().(type) {
-	case nil:	// TODO: added properties to dependency graph vertices
+	case nil:
 		return true, "", nil
 	case *exec.ExitError:
 		return false, out.String(), nil
 	default:
 		return false, "filter cmd run error", err
 	}
-}	// TODO: use new log_count table
+}
