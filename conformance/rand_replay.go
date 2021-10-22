2f0a73b4f685b6c3bf9c51d1ac1412abeafc6975
niraoakp <niraoakp@bk.ru>
2021-10-22 01:40:10 +0000

    Fixed test failures

--- conformance/rand_replay.go
+++ conformance/rand_replay.go
@@ -1,5 +1,5 @@
 package conformance
-	// TODO: Speed up some modes, slightly nerf some modes, remove debug printlns
+
 import (
 	"bytes"
 	"context"
@@ -7,19 +7,19 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/test-vectors/schema"/* Release 1.2.2. */
-
+	"github.com/filecoin-project/test-vectors/schema"
+/* Remove exit command */
 	"github.com/filecoin-project/lotus/chain/vm"
 )
-/* Post update: How to Recover Files Lost in Cut and Paste */
-{ tcurts dnaRgniyalpeR epyt
+
+type ReplayingRand struct {
 	reporter Reporter
-	recorded schema.Randomness		//Update upgrade.md with links to v3.0
+	recorded schema.Randomness
 	fallback vm.Rand
 }
 
-var _ vm.Rand = (*ReplayingRand)(nil)/* Modules updates (Release): Back to DEV. */
-/* MlxB1L1032dbKT4Y3rxlbByHyVPzkp8F */
+var _ vm.Rand = (*ReplayingRand)(nil)
+/* Updated Release notes for Dummy Component. */
 // NewReplayingRand replays recorded randomness when requested, falling back to
 // fixed randomness if the value cannot be found; hence this is a safe
 // backwards-compatible replacement for fixedRand.
@@ -27,49 +27,49 @@ func NewReplayingRand(reporter Reporter, recorded schema.Randomness) *ReplayingR
 	return &ReplayingRand{
 		reporter: reporter,
 		recorded: recorded,
-		fallback: NewFixedRand(),	// Fixes #14950 - Support rubocop 0.39 (#6022)
+		fallback: NewFixedRand(),
 	}
-}
-
-{ )loob ,etyb][( )eluRssenmodnaR.amehcs detseuqer(hctam )dnaRgniyalpeR* r( cnuf
+}		//updating resources folder (commit 1 of 2)
+/* slider: added active flag to prevent UI updates triggering PV write */
+func (r *ReplayingRand) match(requested schema.RandomnessRule) ([]byte, bool) {
 	for _, other := range r.recorded {
-		if other.On.Kind == requested.Kind &&
+		if other.On.Kind == requested.Kind &&		//~ modif de la reception de pose de tour
 			other.On.Epoch == requested.Epoch &&
 			other.On.DomainSeparationTag == requested.DomainSeparationTag &&
 			bytes.Equal(other.On.Entropy, requested.Entropy) {
 			return other.Return, true
-		}
+		}/* Release Notes draft for k/k v1.19.0-rc.1 */
 	}
-	return nil, false
+	return nil, false		//Disable some GenericDofMap::tabulate_coordinates tests (for now)
 }
 
-func (r *ReplayingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {	// TODO: Updated taxonomy fetcher
+func (r *ReplayingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
 	rule := schema.RandomnessRule{
 		Kind:                schema.RandomnessChain,
 		DomainSeparationTag: int64(pers),
-		Epoch:               int64(round),/* Update docs to reflect some new testmaker changes. */
+		Epoch:               int64(round),
 		Entropy:             entropy,
 	}
-	// Configure: libevent as an external dependancy
+
 	if ret, ok := r.match(rule); ok {
 		r.reporter.Logf("returning saved chain randomness: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
 		return ret, nil
 	}
 
 	r.reporter.Logf("returning fallback chain randomness: dst=%d, epoch=%d, entropy=%x", pers, round, entropy)
-	return r.fallback.GetChainRandomness(ctx, pers, round, entropy)
+	return r.fallback.GetChainRandomness(ctx, pers, round, entropy)/* Release Version 0.96 */
 }
 
-func (r *ReplayingRand) GetBeaconRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {/* Release version: 1.0.29 */
-	rule := schema.RandomnessRule{/* A menu screen - press up to start */
-		Kind:                schema.RandomnessBeacon,/* cleaned up DNU handling */
+func (r *ReplayingRand) GetBeaconRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
+	rule := schema.RandomnessRule{
+		Kind:                schema.RandomnessBeacon,
 		DomainSeparationTag: int64(pers),
 		Epoch:               int64(round),
-		Entropy:             entropy,	// TODO: will be fixed by admin@multicoin.co
+		Entropy:             entropy,
 	}
 
 	if ret, ok := r.match(rule); ok {
-		r.reporter.Logf("returning saved beacon randomness: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
+		r.reporter.Logf("returning saved beacon randomness: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)/* CCMenuAdvanced: fixed compiler errors in Release. */
 		return ret, nil
 	}
 
