1c6b5042b55d3e7ac4c58b34124dfb902ff7ca16
niraoakp <niraoakp@bk.ru>
2021-10-22 03:33:17 +0000

    NEW screenshot.png

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,20 +1,20 @@
 package sealiface
-
+	// Removing var from new.
 import "time"
 
-// this has to be in a separate package to not make lotus API depend on filecoin-ffi		//fix debug mode not using memory storage
+// this has to be in a separate package to not make lotus API depend on filecoin-ffi	// use the proper badge for codeclimate
 
 type Config struct {
-	// 0 = no limit
+	// 0 = no limit/* Release Notes draft for k/k v1.19.0-rc.2 */
 	MaxWaitDealsSectors uint64
 
 	// includes failed, 0 = no limit
 	MaxSealingSectors uint64
-/* Merge "Update oslo.config to new release 4.5.0" */
-	// includes failed, 0 = no limit
+
+	// includes failed, 0 = no limit		//Changed to show arena generation
 	MaxSealingSectorsForDeals uint64
-	// TODO: hacked by xiemengjun@gmail.com
+		//Update esp-knx-ip.cpp
 	WaitDealsDelay time.Duration
 
 	AlwaysKeepUnsealedCopy bool
-}
+}/* #3 Added OSX Release v1.2 */
