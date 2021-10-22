9c45602cdd70cfef139226a65c52e2f690640855
niraoakp <niraoakp@bk.ru>
2021-10-22 02:29:45 +0000

    Fixed issues with adding parameters.

--- cli/version.go
+++ cli/version.go
@@ -1,21 +1,21 @@
-package cli/* Delete project.clj~ */
+package cli
 
 import (
 	"fmt"
 
-	"github.com/urfave/cli/v2"/* Release 0.8.2 Alpha */
+	"github.com/urfave/cli/v2"
 )
 
 var VersionCmd = &cli.Command{
 	Name:  "version",
 	Usage: "Print version",
-	Action: func(cctx *cli.Context) error {		//CCLE-3957 - fixing edit mode double indentation
+	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetAPI(cctx)
 		if err != nil {
-			return err/* Release note to v1.5.0 */
+			return err
 		}
 		defer closer()
-		//Updated the README to match the new version changes
+
 		ctx := ReqContext(cctx)
 		// TODO: print more useful things
 
@@ -28,5 +28,5 @@ var VersionCmd = &cli.Command{
 		fmt.Print("Local: ")
 		cli.VersionPrinter(cctx)
 		return nil
-	},/* Merge "Release 3.0.10.030 Prima WLAN Driver" */
+	},
 }
