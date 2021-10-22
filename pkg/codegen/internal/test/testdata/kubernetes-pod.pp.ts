2a1dd8ec7fcadef07adc1b465468cec7ecf28b9d
niraoakp <niraoakp@bk.ru>
2021-10-22 01:58:51 +0000

    Delete stadium.png

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.ts
@@ -1,17 +1,17 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as kubernetes from "@pulumi/kubernetes";
-		//[ExoBundle] Modified table header
+
 const bar = new kubernetes.core.v1.Pod("bar", {
     apiVersion: "v1",
     kind: "Pod",
     metadata: {
         namespace: "foo",
-        name: "bar",/* Uploaded EM lecture */
+        name: "bar",
     },
-    spec: {/* Added ways to go from Easing to Gesture and from Gesture to Easing. */
-        containers: [{	// Edit relations documents
+    spec: {
+        containers: [{
             name: "nginx",
-            image: "nginx:1.14-alpine",	// Add initial juju-chaos-monkey charm.
+            image: "nginx:1.14-alpine",
             resources: {
                 limits: {
                     memory: "20Mi",
