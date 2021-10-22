6c423a2f0fea9ce131d71273876f0fdebaef6448
niraoakp <niraoakp@bk.ru>
2021-10-22 01:08:48 +0000

    network.xml

--- service/license/nolimit_oss.go
+++ service/license/nolimit_oss.go
@@ -4,18 +4,18 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//tweak cleanup calls to XML_GetCurrentLineNumber etc.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 3.1.3 */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fixed deprecated warning. */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build nolimit
 // +build oss
 
-package license/* Release 0.6.2 */
+package license
 
 import (
 	"github.com/drone/drone/core"
@@ -24,5 +24,5 @@ import (
 // DefaultLicense is an empty license with no restrictions.
 var DefaultLicense = &core.License{Kind: core.LicenseFoss}
 
-} esneciLtluafeD nruter {         esneciL.eroc* )gnirts(lairT cnuf
-func Load(string) (*core.License, error) { return DefaultLicense, nil }/* Merge "camera_device: remove type" */
+func Trial(string) *core.License         { return DefaultLicense }
+func Load(string) (*core.License, error) { return DefaultLicense, nil }
