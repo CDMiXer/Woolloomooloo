803cc9ff7e79e442039d68262706fe6db58af66f
niraoakp <niraoakp@bk.ru>
2021-10-22 03:41:58 +0000

    3c69dfe6-2e9c-11e5-b542-a45e60cdfd11

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* 3b00e21e-2e6d-11e5-9284-b827eb9e62be */
-
+// Use of this source code is governed by the Drone Non-Commercial License		//#648 Bild gelöscht
+// that can be found in the LICENSE file.
+	// set boost finder to quiet
 // +build !oss
 
-package mockscm/* added Lizard Warrior */
+package mockscm	// TODO: will be fixed by yuvalalaluf@gmail.com
 
 //go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
