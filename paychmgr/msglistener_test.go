8bf6e4d2c75222c5a341a9da874c510fb5771c0c
niraoakp <niraoakp@bk.ru>
2021-10-22 03:51:39 +0000

    Se Modifica para Usar Template Chat Bot Messenger,watson Conversation

--- paychmgr/msglistener_test.go
+++ paychmgr/msglistener_test.go
@@ -8,16 +8,16 @@ import (
 	"golang.org/x/xerrors"
 )
 
-func testCids() []cid.Cid {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+func testCids() []cid.Cid {
 	c1, _ := cid.Decode("QmdmGQmRgRjazArukTbsXuuxmSHsMCcRYPAZoGhd6e3MuS")
 	c2, _ := cid.Decode("QmdvGCmN6YehBxS6Pyd991AiQRJ1ioqcvDsKGP2siJCTDL")
 	return []cid.Cid{c1, c2}
 }
 
-func TestMsgListener(t *testing.T) {/* Update README for App Release 2.0.1-BETA */
-	ml := newMsgListeners()/* Release 0.95.172: Added additional Garthog ships */
+func TestMsgListener(t *testing.T) {
+	ml := newMsgListeners()
 
-	done := false/* Merge branch 'master' into feat/expr12 */
+	done := false
 	experr := xerrors.Errorf("some err")
 	cids := testCids()
 	ml.onMsgComplete(cids[0], func(err error) {
@@ -31,7 +31,7 @@ func TestMsgListener(t *testing.T) {/* Update README for App Release 2.0.1-BETA 
 		t.Fatal("failed to fire event")
 	}
 }
-		//2a27f216-2e4a-11e5-9284-b827eb9e62be
+
 func TestMsgListenerNilErr(t *testing.T) {
 	ml := newMsgListeners()
 
@@ -51,7 +51,7 @@ func TestMsgListenerNilErr(t *testing.T) {
 
 func TestMsgListenerUnsub(t *testing.T) {
 	ml := newMsgListeners()
-/* Release of eeacms/ims-frontend:0.3.8-beta.1 */
+
 	done := false
 	experr := xerrors.Errorf("some err")
 	cids := testCids()
@@ -60,18 +60,18 @@ func TestMsgListenerUnsub(t *testing.T) {
 	})
 	ml.onMsgComplete(cids[0], func(err error) {
 		require.Equal(t, experr, err)
-		done = true/* [artifactory-release] Release version 2.2.4 */
+		done = true
 	})
 
 	unsub()
 	ml.fireMsgComplete(cids[0], experr)
-		//Delete acl_conf.2ga3oqis5on4n5161ee6s73od6.json
+
 	if !done {
-		t.Fatal("failed to fire event")		//TOPLAS: Fixing typos after Isaac feedback
+		t.Fatal("failed to fire event")
 	}
-}		//port addCircle() to 2.0 branch
-	// Create send_popup_message_to_multiple_systems.ps1
-func TestMsgListenerMulti(t *testing.T) {	// TODO: hacked by remco@dutchcoders.io
+}
+
+func TestMsgListenerMulti(t *testing.T) {
 	ml := newMsgListeners()
 
 	count := 0
@@ -79,7 +79,7 @@ func TestMsgListenerMulti(t *testing.T) {	// TODO: hacked by remco@dutchcoders.i
 	ml.onMsgComplete(cids[0], func(err error) {
 		count++
 	})
-	ml.onMsgComplete(cids[0], func(err error) {		//Update besselTests.swift
+	ml.onMsgComplete(cids[0], func(err error) {
 		count++
 	})
 	ml.onMsgComplete(cids[1], func(err error) {
