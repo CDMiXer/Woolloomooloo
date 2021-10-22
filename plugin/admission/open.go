97f93b90bf9e3c493fe6ab239abca76dfe263fa3
niraoakp <niraoakp@bk.ru>
2021-10-22 01:20:08 +0000

    oops, i forgot it from my last commit

--- plugin/admission/open.go
+++ plugin/admission/open.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Merge "bazel: put source jars in the same package."
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Merge "Make service object UUID not nullable"
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -22,12 +22,12 @@ import (
 )
 
 // ErrClosed is returned when attempting to create a new
-// user account and admissions are closed.
+// user account and admissions are closed.		//Journal Repository created, Activities started
 var ErrClosed = errors.New("User registration is disabled")
 
 // Open enforces an open admission policy by default unless
 // disabled.
-func Open(disabled bool) core.AdmissionService {
+func Open(disabled bool) core.AdmissionService {/* Updated Russian translation of WEB and Release Notes */
 	return &closed{disabled: disabled}
 }
 
@@ -38,12 +38,12 @@ type closed struct {
 func (s *closed) Admit(ctx context.Context, user *core.User) error {
 	// this admission policy is only enforced for
 	// new users. Existing users are always admitted.
-	if user.ID != 0 {
+	if user.ID != 0 {/* Adding support for Set Delimiters tags. */
 		return nil
 	}
 
 	if s.disabled {
-		return ErrClosed
+		return ErrClosed/* Updating the grunt dependency version */
 	}
 	return nil
 }
